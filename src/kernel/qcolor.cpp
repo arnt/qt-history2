106bdc8e717c5fc2939cc972d376bbf03152c101
Haavard Nord <hanord@trolltech.com>
1994-09-29 22:04:15 +0100

    HSV<->RGB conversion finished. Color allocation flag.
    [imported from CVS by cvs2p4 at 1999/10/31 16:56:27]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 81]

--- src/kernel/qcolor.cpp
+++ src/kernel/qcolor.cpp
@@ -1,23 +1,20 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qcolor.cpp#3 $
+** $Id: //depot/qt/main/src/kernel/qcolor.cpp#4 $
 **
 ** Implementation of QColor class
 **
 ** Author  : Haavard Nord
 ** Created : 940112
 **
-** Copyright (C) 1994 by Troll Tech as.	 All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.	 All rights reserved.
 **
-** --------------------------------------------------------------------------
-** This file containts the platform independent implementation of the QColor
-** class.  Platform dependent functions are found in the qcol_xxx.C files.
 *****************************************************************************/
 
 #include "qcolor.h"
 #include "qdstream.h"
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qcolor.cpp#3 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qcolor.cpp#4 $";
 #endif
 
 
@@ -48,6 +45,110 @@ const QColor darkYellow;
 // QColor member functions
 //
 
+bool QColor::aalloc = FALSE;			// auto-alloc colors flag
+
+void QColor::setAutoAlloc( bool onOff )		// set auto-alloc flag
+{
+    aalloc = onOff;
+}
+
+#undef max
+#undef min
+
+//
+// Heavily optimized integer-based RGB<-->HSV conversion algorithms.
+// These algorithms are tuned for efficiency and accuracy, and might
+// not appear very readable!
+//
+void QColor::getHSV( int *h, int *s, int *v ) const
+{						// get HSV value
+    int r = (int)(rgb & 0xff);
+    int g = (int)((rgb >> 8) & 0xff);
+    int b = (int)((rgb >> 16) & 0xff);
+    uint max = r;				// maximum RGB component
+    int whatmax = 0;				// r=>0, g=>1, b=>2
+    if ( g > max ) {
+	max = g;
+	whatmax = 1;
+    }
+    if ( b > max ) {
+	max = b;
+	whatmax = 2;
+    }
+    uint min = r;				// find minimum value
+    if ( g < min ) min = g;
+    if ( b < min ) min = b;
+    int delta = max-min;
+    *v = max;					// calc value
+    *s = max ? (int)((long)(510L*delta+max)/(2L*max)) : 0;
+    if ( *s == 0 )
+	*h = -1;				// undefined hue
+    else {
+	switch ( whatmax ) {
+	    case 0:				// red is max component
+	        if ( g >= b )
+		    *h = (120*(g-b)+delta)/(2*delta);
+		else
+		    *h = (120*(g-b+delta)+delta)/(2*delta) + 300;
+	        break;
+	    case 1:				// green is max component
+	        if ( b > r )
+		    *h = 120 + (120*(b-r)+delta)/(2*delta);
+		else
+		    *h = 60 + (120*(b-r+delta)+delta)/(2*delta);
+		break;
+	    case 2:				// blue is max component
+	        if ( r > g )
+		    *h = 240 + (120*(r-g)+delta)/(2*delta);
+		else
+		    *h = 180 + (120*(r-g+delta)+delta)/(2*delta);
+		break;
+	}
+    }
+}
+
+bool QColor::setHSV( int h, int s, int v )	// set HSV value
+{
+    int r, g, b;
+#if defined(CHECK_RANGE)
+    if ( h < -1 || (uint)s > 255 || (uint)v > 255 ) {
+	warning( "QColor::setHSV:  HSV parameters out of range" );
+	return FALSE;
+    }
+#endif
+    if ( s == 0 || h == -1 )	       		// achromatic case
+	r = g = b = v;
+    else {					// chromatic case
+	if ( (uint)h >= 360 )
+	    h %= 360;
+	uint f = h%60;
+	h /= 60;
+	uint p = (uint)((ulong)(2L*v*(255L-s)+255L)/510L);
+	uint q, t;
+	if ( h&1 )				// do only when necessary
+	    q = (uint)((ulong)(2L*v*(15300L-s*f)+15300L)/30600L);
+	else
+	    t = (uint)((ulong)(2L*v*(15300L-(s*(60L-f)))+15300L)/30600L);
+	switch( h ) {
+	    case 0: r=(int)v; g=(int)t, b=(int)p; break;
+	    case 1: r=(int)q; g=(int)v, b=(int)p; break;
+	    case 2: r=(int)p; g=(int)v, b=(int)t; break;
+	    case 3: r=(int)p; g=(int)q, b=(int)v; break;
+	    case 4: r=(int)t; g=(int)p, b=(int)v; break;
+	    case 5: r=(int)v; g=(int)p, b=(int)q; break;
+	}
+    }
+    return setRGB( r, g, b );
+}
+
+
+void QColor::getRGB( int *r, int *g, int *b ) const
+{						// get RGB value
+    *r = (int)(rgb & 0xff);
+    *g = (int)((rgb >> 8) & 0xff);
+    *b = (int)((rgb >> 16) & 0xff);
+}
+
 bool QColor::setRGB( ulong rgb )		// set RGB value directly
 {
     int r = (int)(rgb & 0xff);
@@ -57,37 +158,24 @@ bool QColor::setRGB( ulong rgb )		// set RGB value directly
 }
 
 
+//
+// The trick with the light() and dark() functions is to transform the RGB
+// color into HSV, multiply/divide V by a factor, and then transform back.
+//
 QColor QColor::light( double factor ) const	// get light color
 {
     if ( factor <= 0.0 )			// invalid lightness factor
 	return *this;
     else
     if ( factor < 1.0 )				// makes color darker
-	return dark( 1/factor );
-    int r=red(), g=green(), b=blue();
-    r = (int)(r*factor);			// multiply intensities
-    g = (int)(g*factor);
-    b = (int)(b*factor);
-    int m = r;					// find maximum value
-    if ( g > m ) m = g;
-    if ( b > m ) m = b;
-    if ( m == 0 )				// oops, black!
-	m++;
-    if ( r > 255 || g > 255 || b > 255 ) {	// handle out-of-range
-	r = 255*r/m;
-	r += (255 - r) >> 1;
-	g = 255*g/m;
-	g += (255 - g) >> 1;
-	b = 255*b/m;
-	b += (255 - b) >> 1;
-    }
-    else
-    if ( r < 102 && g < 102 && b < 102 ) {	// too dark
-	r = 102*r/m;
-	g = 102*g/m;
-	b = 102*b/m;
-    }
-    QColor c( r, g, b );
+	return dark( 1.0/factor );
+    int h, s, v;
+    getHSV( &h, &s, &v );
+    v = (int)(factor*v);
+    if ( v > 255 )				// avoid overflow
+	v = 255;
+    QColor c;
+    c.setHSV( h, s, v );
     return c;
 }
 
@@ -98,9 +186,11 @@ QColor QColor::dark( double factor ) const	// get dark color
     else
     if ( factor < 1.0 )				// makes color lighter
 	return light( 1/factor );
-    QColor c( (int)(red()/factor),		// divide intensities
-	      (int)(green()/factor),
-	      (int)(blue()/factor) );
+    int h, s, v;
+    getHSV( &h, &s, &v );
+    v = (int)((double)v/factor);
+    QColor c;
+    c.setHSV( h, s, v );
     return c;
 }
 
--- src/kernel/qcolor.h
+++ src/kernel/qcolor.h
@@ -1,12 +1,12 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qcolor.h#3 $
+** $Id: //depot/qt/main/src/kernel/qcolor.h#4 $
 **
 ** Definition of QColor class
 **
 ** Author  : Haavard Nord
 ** Created : 940112
 **
-** Copyright (C) 1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.  All rights reserved.
 **
 *****************************************************************************/
 
@@ -16,8 +16,9 @@
 #include "qwindefs.h"
 
 
-const ulong RGB_INVALID = 0x80000000;		// flags invalid color
-
+const ulong RGB_DIRTY = 0x80000000;		// flags unset color
+const ulong RGB_INVALID = 0x40000000;		// flags invalid color
+const ulong RGB_MASK = 0x00ffffff;		// masks RGB values
 
 class QColor					// RGB based color
 {
@@ -28,33 +29,42 @@ public:
     QColor( const char *name );			// load color from database
    ~QColor();
 
+    static void setAutoAlloc( bool );
+    static bool autoAlloc()	{ return aalloc; }
+
+    bool   alloc();				// allocate color
+
+    bool   setNamedColor( const char *name );	// load color from database
+
+    void   getRGB( int *r, int *g, int *b ) const; // get RGB value
+    ulong  getRGB() const { return rgb & 0x7fffffff; }
     bool   setRGB( int r, int g, int b );	// set RGB value
     bool   setRGB( ulong rgb );
-    ulong  getRGB() const { return rgb & 0x7fffffff; }
 
     int	   red()    const { return (int)(rgb & 0xff); }
     int	   green()  const { return (int)((rgb >> 8) & 0xff); }
     int	   blue()   const { return (int)((rgb >> 16) & 0xff); }
 
+    void   getHSV( int *h, int *s, int *v ) const; // get HSV value
+    bool   setHSV( int h, int s, int v );	// set HSV value
+    
     bool   isValid()const { return !(rgb & 0x80000000); }
 
     QColor light( double f = 1.12) const;	// get lighter color
     QColor dark( double f = 2.0 )  const;	// get darker color
 
+    ulong  pixel()  const;			// get pixel value
+
 #if defined(_WS_WIN_) || defined(_WS_WIN32_)
-    ulong  pixel()  const { return pix; }
     static HANDLE hPal()  { return hpal; }
     static uint	  realizePal( QWidget * );
-#elif defined(_WS_PM_)
-    ulong  pixel()  const { return pix; }
-#elif defined(_WS_X11_)
-    ulong  pixel()  const { return pix; }	// get X11 pixel value
 #endif
 
     static void initialize();			// initialize color system
     static void cleanup();			// cleanup color system
 
 private:
+    static bool aalloc;
 #if defined(_WS_WIN_) || defined(_WS_WIN32_)
     static HANDLE hpal;
     ulong  pix;
@@ -65,6 +75,14 @@ private:
 #endif
     ulong  rgb;					// RGB value
 };
+
+
+inline ulong QColor::pixel() const
+{
+    if ( rgb & RGB_DIRTY )
+	((QColor*)this)->alloc();
+    return pix;
+}
 
 
 // --------------------------------------------------------------------------
