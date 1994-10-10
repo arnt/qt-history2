d9395a13f55064957bc2cf009b19bfc2c6d34fec
Haavard Nord <hanord@trolltech.com>
1994-10-10 14:40:55 +0100

    Almost finished implementation Lacks >> string and double
    [imported from CVS by cvs2p4 at 1999/10/31 16:56:40]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 96]

--- src/tools/qtstream.cpp
+++ src/tools/qtstream.cpp
@@ -1,12 +1,12 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qtstream.cpp#1 $
+** $Id: //depot/qt/main/src/tools/qtstream.cpp#2 $
 **
 ** Implementation of QTextStream class
 **
 ** Author  : Haavard Nord
-** Created : 990922
+** Created : 940922
 **
-** Copyright (C) 1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.  All rights reserved.
 **
 *****************************************************************************/
 
@@ -17,7 +17,7 @@
 #include <stdlib.h>
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/tools/qtstream.cpp#1 $";
+static char ident[] = "$Id: //depot/qt/main/src/tools/qtstream.cpp#2 $";
 #endif
 
 
@@ -34,36 +34,50 @@ static char ident[] = "$Id: //depot/qt/main/src/tools/qtstream.cpp#1 $";
 #endif
 
 
-#define I_BASE_2	0x0001
-#define I_BASE_8	0x0002
-#define I_BASE_10	0x0003
-#define I_BASE_16	0x0004
-#define I_BASE_MASK	0x000f
-
 #define I_SHORT		0x0010
 #define I_INT		0x0020
 #define I_LONG		0x0030
 #define I_TYPE_MASK	0x00f0
+
+#define I_BASE_2	QTS::bin
+#define I_BASE_8	QTS::oct
+#define I_BASE_10	QTS::dec
+#define I_BASE_16	QTS::hex
+#define I_BASE_MASK	(QTS::bin | QTS::oct | QTS::dec | QTS::hex)
 
 #define I_SIGNED	0x0100
 #define I_UNSIGNED	0x0200
 #define I_SIGN_MASK	0x0f00
 
 
+const long QTS::basefield   = I_BASE_MASK;
+const long QTS::adjustfield = QTS::left | QTS::right | QTS::internal;
+const long QTS::floatfield  = QTS::scientific | QTS::fixed;
+
+
 QTextStream::QTextStream()
 {
     dev = 0;					// no device set
-    base = I_BASE_10;
+    reset();
 }
 
 QTextStream::QTextStream( QIODevice *d )
 {
     dev = d;					// set device
-    base = I_BASE_10;
+    reset();
 }
 
 QTextStream::~QTextStream()
 {
+}
+
+
+void QTextStream::reset()
+{
+    fflags = 0;
+    fwidth = 0;
+    fillchar = ' ';
+    fprec = 6;
 }
 
 
@@ -72,7 +86,193 @@ QTextStream::~QTextStream()
 //
 
 
-// !!! NOT IMPLEMENTED !!!
+QTextStream &QTextStream::operator>>( char &c )
+{
+    CHECK_STREAM_PRECOND
+    if ( flags() & skipws )
+	ws( *this );
+    c = dev->getch();
+    return *this;
+}
+
+
+static ulong input_bin( QTextStream *s )
+{
+    ulong val = 0;
+    register int c = s->device()->getch();
+    while ( c == '0' || c == '1' ) {
+	val <<= 1;
+	val += c - '0';
+	c = s->device()->getch();
+    }
+    s->device()->ungetch( c );
+    return val;
+}
+
+static ulong input_oct( QTextStream *s )
+{
+    ulong val = 0;
+    register int c = s->device()->getch();
+    while ( c >= '0' && c <= '7' ) {
+	val <<= 3;
+	val += c - '0';
+	c = s->device()->getch();
+    }
+    s->device()->ungetch( c );
+    return val;
+}
+
+static ulong input_dec( QTextStream *s )
+{
+    ulong val = 0;
+    register int c = s->device()->getch();
+    while ( isdigit(c) ) {
+	val *= 10;
+	val += c - '0';
+	c = s->device()->getch();
+    }
+    s->device()->ungetch( c );
+    return val;
+}
+
+static ulong input_hex( QTextStream *s )
+{
+    ulong val = 0;
+    register int c = s->device()->getch();
+    while ( isxdigit(c) ) {
+	val <<= 4;
+	if ( isdigit(c) )
+	    val += c - '0';
+	else
+	    val += 10 + tolower(c) - 'a';
+	c = s->device()->getch();
+    }
+    s->device()->ungetch( c );
+    return val;
+}
+
+long QTextStream::input_int()
+{
+    long val;
+    int c;
+    if ( flags() & skipws )			// skip whitespace
+	ws( *this );
+    switch ( flags() & basefield ) {
+	case bin:
+	    val = (long)input_bin( this );
+	    break;
+	case oct:
+	    val = (long)input_oct( this );
+	    break;
+	case dec:
+	    c = dev->getch();
+	    if ( !(c == '-' || c == '+') )
+		dev->ungetch( c );
+	    val = (long)input_dec( this );
+	    if ( c == '-' )
+		val = -val;
+	    break;
+	case hex:
+	    val = (long)input_hex( this );
+	    break;
+	default:
+	    val = 0;
+	    c = dev->getch();
+	    if ( c == '0' ) {		// bin, oct or hex
+		c = dev->getch();
+		if ( tolower(c) == 'x' )
+		    val = (long)input_hex( this );
+		else if ( tolower(c) == 'b' )
+		    val = (long)input_bin( this );
+		else {			// octal
+		    dev->ungetch( c );
+		    val = (long)input_oct( this );
+		}
+	    }
+	    else if ( c >= '1' && c <= '9' ) {
+		dev->ungetch( c );
+		val = (long)input_dec( this );
+	    }
+	    else if ( c == '-' || c == '+' ) {
+		val = (long)input_dec( this );
+		if ( c == '-' )
+		    val = -val;
+	    }
+    }
+    return val;
+}
+
+
+QTextStream &QTextStream::operator>>( signed short &i )
+{
+    CHECK_STREAM_PRECOND
+    i = (signed short)input_int();
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( unsigned short &i )
+{
+    CHECK_STREAM_PRECOND
+    i = (unsigned short)input_int();
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( signed int &i )
+{
+    CHECK_STREAM_PRECOND
+    i = (signed int)input_int();
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( unsigned int &i )
+{
+    CHECK_STREAM_PRECOND
+    i = (unsigned int)input_int();
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( signed long &i )
+{
+    CHECK_STREAM_PRECOND
+    i = (signed long)input_int();
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( unsigned long &i )
+{
+    CHECK_STREAM_PRECOND
+    i = (unsigned long)input_int();
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( float &f )
+{
+    CHECK_STREAM_PRECOND
+    double f2;
+    *this >> f2;
+    f = (float)f2;
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( double & )
+{
+    CHECK_STREAM_PRECOND
+    return *this;
+}
+
+
+QTextStream &QTextStream::operator>>( char *& )
+{
+    CHECK_STREAM_PRECOND
+    return *this;
+}
 
 
 // --------------------------------------------------------------------------
@@ -92,39 +292,60 @@ QTextStream &QTextStream::output_int( int format, ulong n, bool neg )
     static char hexdigits_lower[] = "0123456789abcdef";
     static char hexdigits_upper[] = "0123456789ABCDEF";
     CHECK_STREAM_PRECOND
-    char buf[72];
-    register int len;
+    char buf[76];
     register char *p;
+    int   len;
     char *hexdigits;
-    switch ( base & I_BASE_MASK ) {
-	case I_BASE_2:				// output bits
+
+    switch ( flags() & I_BASE_MASK ) {
+
+	case I_BASE_2:				// output binary number
 	    switch ( format & I_TYPE_MASK ) {
 		case I_SHORT: len=16; break;
 		case I_INT:   len=sizeof(int)*8; break;
 		case I_LONG:  len=32; break;
 		default:      len = 0;
 	    }
-	    p = &buf[len];
-	    *p-- = '\0';
+	    p = &buf[74];			// go reverse order
+	    *p = '\0';
 	    while ( len-- ) {
-		*p-- = ((n & 1) + '0');
+		*--p = ((n & 1) + '0');
 		n >>= 1;
 	    }
-	    p = buf;
+	    if ( flags() & showbase ) {		// show base
+		*--p = (flags() & uppercase) ? 'B' : 'b';
+		*--p = '0';
+	    }
 	    break;
-	case I_BASE_8:
-	    p = &buf[64];			// go in reverse order
+
+	case I_BASE_8:				// output octal number
+	    p = &buf[74];
 	    *p = '\0';
 	    do {
 		*--p = (n&7) + '0';
-		n /= 8;
+		n >>= 3;
 	    } while ( n );
+	    if ( flags() & showbase )
+		*--p = '0';
 	    break;
-	case I_BASE_10:
-	    sprintf( buf, "%d", n );
-	    p = buf;
+
+	case I_BASE_16:				// output hexadecimal number
+	    p = &buf[74];
+	    *p = '\0';
+	    hexdigits = (flags() & uppercase) ?
+		hexdigits_upper : hexdigits_lower;
+	    do {
+		*--p = hexdigits[(int)n&0xf];
+		n >>= 4;
+	    } while ( n );
+	    if ( flags() & showbase ) {
+		*--p = (flags() & uppercase) ? 'X' : 'x';
+		*--p = '0';
+	    }
 	    break;
-	    p = &buf[64];			// go in reverse order
+
+	default:				// decimal base is default
+	    p = &buf[74];
 	    *p = '\0';
 	    if ( neg )
 		n = (ulong)(-(long)n);
@@ -134,20 +355,34 @@ QTextStream &QTextStream::output_int( int format, ulong n, bool neg )
 	    } while ( n );
 	    if ( neg )
 		*--p = '-';
-	    break;
-	case I_BASE_16:
-	    p = &buf[64];			// go in reverse order
-	    *p = '\0';
-	    hexdigits = hexdigits_lower;
-	    do {
-		*--p = hexdigits[(int)n&0xf];
-		n /= 16;
-	    } while ( n );
-	    break;
-	default:				// unsupported base -> ignore
-	    return *this;
+	    else if ( flags() & showpos )
+		*--p = '+';
+	    if ( (flags() & internal) && fwidth && !isdigit(*p) ) {
+		dev->putch( *p );		// special case for internal
+		++p;				//   padding
+		fwidth--;
+		return *this << (const char *)p;
+	    }
     }
-    dev->writeBlock( p, strlen(p) );
+    if ( fwidth ) {				// adjustment required
+	if ( !(flags() & left) ) {		// but NOT left adjustment
+	    len = strlen(p);
+	    int padlen = fwidth - len;
+	    if ( padlen <= 0 )			// no padding required
+		dev->writeBlock( p, len );
+	    else if ( padlen < (int)(p-buf) ) {	// speeds up padding
+		memset( p-padlen, fillchar, padlen );
+		dev->writeBlock( p-padlen, padlen+len );
+	    }
+	    else				// standard padding
+		*this << (const char *)p;
+	}
+	else
+	    *this << (const char *)p;
+	fwidth = 0;				// reset field width
+    }
+    else
+	dev->writeBlock( p, strlen(p) );
     return *this;
 }
 
@@ -190,20 +425,42 @@ QTextStream &QTextStream::operator<<( unsigned long i )
 
 QTextStream &QTextStream::operator<<( float f )	// write float value
 {
-    CHECK_STREAM_PRECOND
-    char buf[32];
-    sprintf( buf, "%g", f );
-    dev->writeBlock( buf, strlen(buf) );
-    return *this;
+    return *this << (double)f;
 }
 
 
 QTextStream &QTextStream::operator<<( double f )// write double value
 {
     CHECK_STREAM_PRECOND
-    char buf[32];
-    sprintf( buf, "%g", f );
-    dev->writeBlock( buf, strlen(buf) );
+    char buf[64];
+    char f_char;
+    char format[16];
+    if ( (flags()&floatfield) == fixed )
+	f_char = 'f';
+    else if ( (flags()&floatfield) == scientific )
+	f_char = (flags() & uppercase) ? 'E' : 'e';
+    else
+	f_char = (flags() & uppercase) ? 'G' : 'g';
+    register char *fs = format;			// generate format string
+    *fs++ = '%';				//   "%.<prec>l<f_char>"
+    *fs++ = '.';
+    int prec = precision();
+    if ( prec > 99 )
+	prec = 99;
+    if ( prec >= 10 ) {
+	*fs++ = prec / 10 + '0';
+	*fs++ = prec % 10 + '0';
+    }
+    else
+	*fs++ = prec + '0';
+    *fs++ = 'l';
+    *fs++ = f_char;
+    *fs = '\0';
+    sprintf( buf, format, f );			// the easy way out...
+    if ( fwidth )				// padding
+	*this << (const char *)buf;
+    else					// just write it
+	dev->writeBlock( buf, strlen(buf) );
     return *this;
 }
 
@@ -211,56 +468,103 @@ QTextStream &QTextStream::operator<<( double f )// write double value
 QTextStream &QTextStream::operator<<( const char *s )
 {						// write 0-term char array
     CHECK_STREAM_PRECOND
+    char padbuf[48];
     uint len = strlen( s );			// don't write null terminator
+    if ( fwidth ) {				// field width set
+	int padlen = fwidth - len;
+	fwidth = 0;				// reset width
+	if ( padlen > 0 ) {
+	    char *ppad;
+	    if ( padlen > 46 ) {		// create extra big fill buffer
+		ppad = new char[padlen];
+		CHECK_PTR( ppad );
+	    }
+	    else
+		ppad = padbuf;
+	    memset( ppad, fillchar, padlen );	// fill with fillchar
+	    if ( !(flags() & left) ) {
+		dev->writeBlock( ppad, padlen );
+		padlen = 0;
+	    }
+	    dev->writeBlock( s, len );
+	    if ( padlen )
+		dev->writeBlock( ppad, padlen );
+	    if ( ppad != padbuf )		// delete extra big fill buf
+		delete[] ppad;
+	    return *this;
+	}
+    }
     dev->writeBlock( s, len );
     return *this;
 }
 
 
-QTextStream &QTextStream::operator<<( QTSMF mf )
+QTextStream &QTextStream::operator<<( void *ptr )// any pointer
 {
-    (this->*mf)();
+    long f = flags();
+    setf( hex, basefield );
+    setf( showbase );
+    unsetf( uppercase );
+    output_int( I_LONG | I_UNSIGNED, (ulong)ptr, FALSE );
+    flags( f );
     return *this;
 }
 
 
-QTextStream &QTextStream::operator<<( QTSFUNC f )
+// --------------------------------------------------------------------------
+// QTextStream manipulators
+//
+
+QTextStream &bin( QTextStream &s )
 {
-    return (*f)( *this );
+    s.setf(QTS::bin,QTS::basefield);
+    return s;
 }
 
-
-void QTextStream::bin()
+QTextStream &oct( QTextStream &s )
 {
-    base = I_BASE_2;
+    s.setf(QTS::oct,QTS::basefield);
+    return s;
 }
 
-void QTextStream::oct()
+QTextStream &dec( QTextStream &s )
 {
-    base = I_BASE_8;
+    s.setf(QTS::dec,QTS::basefield);
+    return s;
 }
 
-void QTextStream::dec()
+QTextStream &hex( QTextStream &s )
 {
-    base = I_BASE_10;
+    s.setf(QTS::hex,QTS::basefield);
+    return s;
 }
 
-void QTextStream::hex()
+QTextStream &endl( QTextStream &s )
 {
-    base = I_BASE_16;
+    return s << '\n';
 }
 
-void QTextStream::endl()
+QTextStream &flush( QTextStream &s )
 {
-    if ( dev )
-	dev->putch( '\n' );
+    if ( s.device() )
+	s.device()->flush();
+    return s;
 }
 
-void QTextStream::flush()
+QTextStream &ws( QTextStream &s )
 {
-    if ( dev )
-	dev->flush();
+    register QIODevice *d = s.device();
+    if ( d ) {
+	int ch = d->getch();
+	while ( isspace(ch) )
+	    ch = d->getch();
+	d->ungetch( ch );
+    }
+    return s;
 }
 
-
-QTextStream &hex(QTextStream &s) { s.hex(); return s; }
+QTextStream &reset( QTextStream &s )
+{
+    s.reset();
+    return s;
+}
--- src/tools/qtstream.h
+++ src/tools/qtstream.h
@@ -1,12 +1,12 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qtstream.h#1 $
+** $Id: //depot/qt/main/src/tools/qtstream.h#2 $
 **
 ** Definition of QTextStream class
 **
 ** Author  : Haavard Nord
 ** Created : 940922
 **
-** Copyright (C) 1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.  All rights reserved.
 **
 *****************************************************************************/
 
@@ -15,11 +15,6 @@
 
 #include "qiodev.h"
 
-
-class QTextStream;
-
-typedef void (QTextStream::*QTSMF)();		// member function pointer
-typedef QTextStream & (*QTSFUNC)(QTextStream &);
 
 class QTextStream				// text stream class
 {
@@ -30,7 +25,7 @@ public:
 
     QIODevice 	*device() const;		// get current stream device
     void	 setDevice( QIODevice * );	// set stream device
-    void	 resetDevice();			// set NULL stream device
+    void	 unsetDevice();			// set NULL stream device
 
     QTextStream &operator>>( char & );
     QTextStream &operator>>( signed short & );
@@ -53,35 +48,146 @@ public:
     QTextStream &operator<<( float );
     QTextStream &operator<<( double );
     QTextStream &operator<<( const char * );
-    QTextStream &operator<<( QTSMF );
-    QTextStream &operator<<( QTSFUNC );
+    QTextStream &operator<<( void * );		// any pointer
 
-    void	 bin();
-    void	 oct();
-    void	 dec();
-    void	 hex();
-    void	 endl();
-    void	 flush();
+    enum {
+	skipws	  = 0x0001,			// skip whitespace on input
+	left	  = 0x0002,			// left-adjust output
+	right	  = 0x0004,			// right-adjust output
+	internal  = 0x0008,			// pad after sign
+	bin	  = 0x0010,			// binary format integer
+	oct	  = 0x0020,			// octal format integer
+	dec	  = 0x0040,			// decimal format integer
+	hex	  = 0x0080,			// hex format integer
+	showbase  = 0x0100,			// show base indicator
+	showpoint = 0x0200,			// force decimal point (float)
+	uppercase = 0x0400,			// upper-case hex output
+	showpos	  = 0x0800,			// add '+' to positive integers
+	scientific= 0x1000,			// scientific float output
+	fixed	  = 0x2000,			// fixed float output
+    };
+    
+    static const long basefield;		// bin | oct | dec | hex
+    static const long adjustfield;		// left | right | internal
+    static const long floatfield;		// scientific | fixed
+
+    long  flags() const;			// get/set flags
+    long  flags( long f );
+    long  setf( long bits );
+    long  setf( long bits, long mask );
+    long  unsetf( long bits );
+
+    void  reset();				// set default flags
+
+    int   width() const;			// get/set field width
+    int   width( int );
+    int   fill() const;				// get/set fill char
+    int   fill( int );
+    int   precision() const;			// get/set float precision
+    int   precision( int );
 
 private:
+    long	 input_int();
     QTextStream &output_int( int, ulong, bool );
     QIODevice   *dev;				// I/O device
-    int		 base;
+    long	 fflags;			// formatting flags
+    int		 fwidth;			// field width
+    int		 fillchar;			// fill char
+    int		 fprec;				// float precision
 };
 
-/*
 typedef QTextStream QTS;
 
-QTextStream &hex( QTextStream &s );
-*/
 
 // --------------------------------------------------------------------------
 // QTextStream inline functions
 //
 
-inline QIODevice *QTextStream::device() const	  { return dev; }
-inline void QTextStream::setDevice(QIODevice *d ) { dev = d; }
-inline void QTextStream::resetDevice()		  { dev = 0; }
+inline QIODevice *QTextStream::device() const
+{ return dev; }
+
+inline void QTextStream::setDevice(QIODevice *d )
+{ dev = d; }
+
+inline void QTextStream::unsetDevice()
+{ dev = 0; }
+
+inline long QTextStream::flags() const
+{ return fflags; }
+
+inline long QTextStream::flags( long newf )
+{ long f = fflags;  fflags = newf;  return f; }
+
+inline long QTextStream::setf( long bits )
+{ long f = fflags;  fflags |= bits;  return f; }
+
+inline long QTextStream::setf( long bits, long mask )
+{ long f = fflags;  fflags = (fflags & ~mask) | (bits & mask);  return f; }
+
+inline long QTextStream::unsetf( long mask )
+{ long f = fflags;  fflags &= ~mask;  return f; }
+
+inline int QTextStream::width() const
+{ return fwidth; }
+
+inline int QTextStream::width( int w )
+{ int oldw = fwidth;  fwidth = w;  return oldw;  }
+
+inline int QTextStream::fill() const
+{ return fillchar; }
+
+inline int QTextStream::fill( int f )
+{ int oldc = fillchar;  fillchar = f;  return oldc;  }
+
+inline int QTextStream::precision() const
+{ return fprec; }
+
+inline int QTextStream::precision( int p )
+{ int oldp = fprec;  fprec = p;  return oldp;  }
+
+
+// --------------------------------------------------------------------------
+// QTextStream manipulators
+//
+
+typedef QTextStream & (*QTSFUNC)(QTextStream &);// manipulator function
+typedef int (QTextStream::*QTSMFI)(int);	// manipulator w/int argument
+
+class QTSManip {				// text stream manipulator
+public:
+    QTSManip( QTSMFI m, int a )	{ mf=m; arg=a; }
+    void exec( QTextStream &s ) { (s.*mf)(arg); }
+private:
+    QTSMFI mf;					// QTextStream member function
+    int	   arg;					// member function argument
+};
+
+inline QTextStream &operator>>( QTextStream &s, QTSFUNC f )
+{ return (*f)( s ); }
+
+inline QTextStream &operator<<( QTextStream &s, QTSFUNC f )
+{ return (*f)( s ); }
+
+inline QTextStream &operator<<( QTextStream &s, QTSManip m )
+{ m.exec(s); return s; }
+
+QTextStream &bin( QTextStream &s );		// set bin notation
+QTextStream &oct( QTextStream &s );		// set oct notation
+QTextStream &dec( QTextStream &s );		// set dec notation
+QTextStream &hex( QTextStream &s );		// set hex notation
+QTextStream &endl( QTextStream &s );		// insert EOL ('\n')
+QTextStream &flush( QTextStream &s );		// flush output
+QTextStream &ws( QTextStream &s );		// eat whitespace on input
+QTextStream &reset( QTextStream &s );		// set default flags
+
+inline QTSManip setw( int w )
+{ return QTSManip(&QTextStream::width,w); }
+
+inline QTSManip setfill( int f )
+{ return QTSManip(&QTextStream::fill,f); }
+
+inline QTSManip setprecision( int p )
+{ return QTSManip(&QTextStream::precision,p); }
 
 
 #endif // QTSTREAM_H
