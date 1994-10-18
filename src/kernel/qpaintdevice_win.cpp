75d26ca5f9ec325c90e67d816aac8d50200ef9bb
Haavard Nord <hanord@trolltech.com>
1994-10-18 09:31:31 +0100

    bitBlt implemented
    [imported from CVS by cvs2p4 at 1999/10/31 16:57:05]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 126]

--- src/kernel/qpaintdevice_win.cpp
+++ src/kernel/qpaintdevice_win.cpp
@@ -1,19 +1,22 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpaintdevice_win.cpp#2 $
+** $Id: //depot/qt/main/src/kernel/qpaintdevice_win.cpp#3 $
 **
 ** Implementation of QPaintDevice class for Windows + NT
 **
 ** Author  : Haavard Nord
 ** Created : 940801
 **
-** Copyright (C) 1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.	 All rights reserved.
 **
 *****************************************************************************/
 
 #include "qpaintd.h"
+#include "qwidget.h"
+#include "qpixmap.h"
+#include <windows.h>
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qpaintdevice_win.cpp#2 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qpaintdevice_win.cpp#3 $";
 #endif
 
 
@@ -24,4 +27,83 @@ QPaintDevice::QPaintDevice()
 
 QPaintDevice::~QPaintDevice()
 {
+}
+
+
+void QPaintDevice::bitBlt( int sx, int sy, int sw, int sh, QPaintDevice *dest,
+			   int dx, int dy, RasterOp rop )
+{
+    int ts = devType();				// from device type
+    int td = dest->devType();			// to device type
+
+// NOTE!!!  PS printer/metafile pixmap output not yet supported...
+    if ( (td == PDT_PRINTER || td == PDT_METAFILE) && dest->paintingActive() ){
+//	cmd( PDC_DRAWPIXMAP, ... );   // !!!!!!!!!!
+	return;
+    }
+
+    if ( ts == PDT_PRINTER || ts == PDT_METAFILE ) {
+#if defined(CHECK_STATE)
+	warning( "QPaintDevice::bitBlt: Cannot bitBlt from device" );
+#endif
+	return;
+    }
+    static long ropCodes[] =			// ROP translation table
+	{ SRCCOPY, SRCPAINT, SRCINVERT, 0x00220326 /* DSna */,
+	  NOTSRCCOPY, MERGEPAINT, 0x00990066 /* DSnx */,
+	  SRCAND, DSTINVERT };
+    if ( !(rop >= CopyROP && rop <= NotROP) ) {
+#if defined(CHECK_RANGE)
+	warning( "QPaintDevice::bitBlt: Invalid ROP code" );
+#endif
+	return;
+    }
+    HDC src_dc = hdc, dest_dc = dest->hdc;
+    bool src_tmp = FALSE, dest_tmp = FALSE;
+    if ( !src_dc ) {
+	switch ( ts ) {
+	    case PDT_WIDGET:
+		src_dc = GetDC( ((QWidget*)this)->id() );
+		break;
+	    case PDT_PIXMAP:
+		src_dc = ((QPixMap*)this)->allocMemDC();
+		break;
+	}
+	src_tmp = TRUE;
+    }
+    if ( !dest_dc ) {
+	switch ( td ) {
+	    case PDT_WIDGET:
+		dest_dc = GetDC( ((QWidget*)dest)->id() );
+		break;
+	    case PDT_PIXMAP:
+		dest_dc = ((QPixMap*)dest)->allocMemDC();
+		break;
+	}
+	dest_tmp = TRUE;
+    }
+    if ( !(src_dc && dest_dc) )			// not ready, (why?)
+	return;
+    BitBlt( dest_dc, dx, dy, sw, sh, src_dc, sx, sy, ropCodes[rop] );
+    if ( src_tmp ) {
+	switch ( ts ) {
+	    case PDT_WIDGET:
+		ReleaseDC( ((QWidget*)this)->id(), src_dc );
+		break;
+	    case PDT_PIXMAP:
+		((QPixMap*)this)->freeMemDC();
+		break;
+	}
+    }
+    if ( dest_tmp ) {
+	switch ( td ) {
+	    case PDT_WIDGET:
+		ReleaseDC( ((QWidget*)dest)->id(), dest_dc );
+		break;
+	    case PDT_PIXMAP:
+		((QPixMap*)dest)->freeMemDC();
+		break;
+	}
+    }
+
 }
--- src/kernel/qptd_x11.cpp
+++ src/kernel/qptd_x11.cpp
@@ -1,23 +1,25 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qptd_x11.cpp#2 $
+** $Id: //depot/qt/main/src/kernel/qptd_x11.cpp#3 $
 **
 ** Implementation of QPaintDevice class for X11
 **
 ** Author  : Haavard Nord
 ** Created : 940721
 **
-** Copyright (C) 1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.  All rights reserved.
 **
 *****************************************************************************/
 
 #include "qpaintd.h"
+#include "qwidget.h"
+#include "qpixmap.h"
 #define	 GC GC_QQQ
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qptd_x11.cpp#2 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qptd_x11.cpp#3 $";
 #endif
 
 
@@ -30,6 +32,67 @@ QPaintDevice::QPaintDevice()
 
 QPaintDevice::~QPaintDevice()
 {
+}
+
+
+void QPaintDevice::bitBlt( int sx, int sy, int sw, int sh, QPaintDevice *dest,
+			   int dx, int dy, RasterOp rop )
+{
+    int ts = devType();				// from device type
+    int td = dest->devType();			// to device type
+
+/*
+    NOTE!!!  PS printer pixmap output not yet supported...
+    if ( td == PDT_PRINTER && paintingActive() )
+    	cmd( PDC_DRAWPIXMAP, ... );   // or something similar...
+*/
+
+    if ( !(ts <= PDT_PIXMAP && td <= PDT_PIXMAP) ) {
+#if defined(CHECK_STATE)
+	warning( "QPaintDevice::bitBlt: Cannot bitBlt to or from device" );
+#endif
+	return;
+    }
+    static short ropCodes[] =			// ROP translation table
+	{ GXcopy, GXor, GXxor, GXandInverted,
+	  GXcopyInverted, GXorInverted, GXequiv, GXand, GXinvert };
+    if ( !(rop >= CopyROP && rop <= NotROP) ) {
+#if defined(CHECK_RANGE)
+	warning( "QPaintDevice::bitBlt: Invalid ROP code" );
+#endif
+	return;
+    }
+    GC gc;
+    bool new_gc = FALSE;
+    bool copy_plane = FALSE;
+    if ( td == PDT_WIDGET )			// borrow dest widget's gc
+	gc = ((QWidget*)dest)->getGC();
+    else {					// create dedicated GC
+	gc = XCreateGC( dpy, hd, 0, 0 );
+	new_gc = TRUE;
+    }
+    if ( rop != CopyROP )
+	XSetFunction( dpy, gc, ropCodes[rop] );
+    if ( ts == PDT_PIXMAP )
+	copy_plane = ((QPixMap*)this)->planes() == 1;
+    if ( td == PDT_PIXMAP ) {
+	bool singleplane = ((QPixMap*)dest)->planes() == 1;
+	if ( singleplane && !copy_plane ) {	
+#if defined(CHECK_RANGE)
+		warning( "QPaintDevice::bitBlt: Incompatible destination pixmap" );
+#endif
+		return;			// dest is 1-bit pixmap, source is not
+	}
+	copy_plane ^= singleplane;
+    }
+    if ( copy_plane )
+	XCopyPlane( dpy, hd, dest->hd, gc, sx, sy, sw, sh, dx, dy, 1 );
+    else
+	XCopyArea( dpy, hd, dest->hd, gc, sx, sy, sw, sh, dx, dy );
+    if ( new_gc )
+	XFreeGC( dpy, gc );
+    else if ( rop != CopyROP )			// restore ROP for borrowed gc
+	XSetFunction( dpy, gc, ropCodes[CopyROP] );
 }
 
 
