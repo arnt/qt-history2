a140576ee3722b2221bd94d29e95f29752bcc5bf
Haavard Nord <hanord@trolltech.com>
1994-11-08 16:41:32 +0100

    Give pdc argc better names
    [imported from CVS by cvs2p4 at 1999/10/31 16:57:50]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 200]

--- src/kernel/qpaintdevice.h
+++ src/kernel/qpaintdevice.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpaintdevice.h#8 $
+** $Id: //depot/qt/main/src/kernel/qpaintdevice.h#9 $
 **
 ** Definition of QPaintDevice class
 **
@@ -38,52 +38,55 @@
 #define PDC_RESERVED_START      0		// codes 0-99 are reserved
 #define PDC_RESERVED_STOP	99		//   for Troll Tech
 
-#define PDC_NOP			0		// void
-#define PDC_DRAWPOINT		1		// p
-#define PDC_MOVETO		2		// p
-#define PDC_LINETO		3		// p
-#define PDC_DRAWLINE		4		// p,p
-#define PDC_DRAWRECT		5		// r
-#define PDC_DRAWROUNDRECT	6		// r,i,i
-#define PDC_DRAWELLIPSE		7		// r
-#define PDC_DRAWARC		8		// r,i,i
-#define PDC_DRAWPIE		9		// r,i,i
-#define PDC_DRAWCHORD		10		// r,i,i
-#define PDC_DRAWLINESEGS	11		// a
-#define PDC_DRAWPOLYLINE	12		// a
-#define PDC_DRAWPOLYGON		13		// a,i
-#define PDC_DRAWTEXT		14		// p,s
+#define PDC_NOP			0		//  <void>
+#define PDC_DRAWPOINT		1		// point
+#define PDC_MOVETO		2		// point
+#define PDC_LINETO		3		// point
+#define PDC_DRAWLINE		4		// point,point
+#define PDC_DRAWRECT		5		// rect
+#define PDC_DRAWROUNDRECT	6		// rect,ival,ival
+#define PDC_DRAWELLIPSE		7		// rect
+#define PDC_DRAWARC		8		// rect,ival,ival
+#define PDC_DRAWPIE		9		// rect,ival,ival
+#define PDC_DRAWCHORD		10		// rect,ival,ival
+#define PDC_DRAWLINESEGS	11		// ptarr
+#define PDC_DRAWPOLYLINE	12		// ptarr
+#define PDC_DRAWPOLYGON		13		// ptarr,ival
+#define PDC_DRAWTEXT		14		// point,str
 #define PDC_DRAWTEXTALIGN	15		// NI
 #define PDC_DRAWPIXMAP		16		// NI
-#define PDC_BEGIN		20		// void
-#define PDC_END			21		// void
-#define PDC_SAVE		22		// void
-#define PDC_RESTORE		23		// void
-#define PDC_SETBKCOLOR		30		// l
-#define PDC_SETBKMODE		31		// i
-#define PDC_SETROP		32		// i
-#define PDC_SETBRUSHORIGIN	33		// p
-#define PDC_SETFONT		35		// s	NOTE!!! more details
-#define PDC_SETPEN		36		// i,i,ul
-#define PDC_SETBRUSH		37		// i,ul	NOTE!!! bitmap support
-#define PDC_SETUNIT		40		// i
-#define PDC_SETVXFORM		41		// i
-#define PDC_SETSOURCEVIEW	42		// r
-#define PDC_SETTARGETVIEW	43		// r
-#define PDC_SETWXFORM		44		// i
-#define PDC_SETWXFMATRIX	45		// m,i
-#define PDC_SETCLIP		50		// i
+#define PDC_BEGIN		20		//  <void>
+#define PDC_END			21		//  <void>
+#define PDC_SAVE		22		//  <void>
+#define PDC_RESTORE		23		//  <void>
+#define PDC_SETBKCOLOR		30		// color
+#define PDC_SETBKMODE		31		// ival
+#define PDC_SETROP		32		// ival
+#define PDC_SETBRUSHORIGIN	33		// point
+#define PDC_SETFONT		35		// font
+#define PDC_SETPEN		36		// pen
+#define PDC_SETBRUSH		37		// brush
+#define PDC_SETUNIT		40		// ival
+#define PDC_SETVXFORM		41		// ival
+#define PDC_SETSOURCEVIEW	42		// rect
+#define PDC_SETTARGETVIEW	43		// rect
+#define PDC_SETWXFORM		44		// ival
+#define PDC_SETWXFMATRIX	45		// matrix,ival
+#define PDC_SETCLIP		50		// ival
 #define PDC_SETCLIPRGN		51		// rgn
 
 union QPDevCmdParam {
-    int		 i;
-    ulong	 ul;
+    int		 ival;
     char	*str;
-    QPoint	*p;
-    QRect	*r;
-    QPointArray *a;
+    QPoint	*point;
+    QRect	*rect;
+    QPointArray *ptarr;
+    QColor	*color;
+    QFont	*font;
+    QPen	*pen;
+    QBrush	*brush;
     QRegion	*rgn;
-    QWXFMatrix  *m;
+    QWXFMatrix  *matrix;
 };
 
 
