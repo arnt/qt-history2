f89856cad5614fa4713844c3a41117bee4cc2ee5
Haavard Nord <hanord@trolltech.com>
1994-11-08 18:44:20 +0100

    cosmetics
    [imported from CVS by cvs2p4 at 1999/10/31 16:57:53]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 205]

--- src/tools/qstring.cpp
+++ src/tools/qstring.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qstring.cpp#5 $
+** $Id: //depot/qt/main/src/tools/qstring.cpp#6 $
 **
 ** Implementation of extended char array operations, and QByteArray and
 ** QString classes
@@ -21,7 +21,7 @@
 #include <ctype.h>
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/tools/qstring.cpp#5 $";
+static char ident[] = "$Id: //depot/qt/main/src/tools/qstring.cpp#6 $";
 #endif
 
 
@@ -149,7 +149,7 @@ QString::QString( const char *str )		// deep copy
 }
 
 
-QString& QString::operator=( const char *str )	// deep copy
+QString &QString::operator=( const char *str )	// deep copy
 {
     return (QString &)duplicate( str, strlen(str)+1 );
 }
