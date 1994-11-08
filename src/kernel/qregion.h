9c89170aacfeb6794e852d0e57c53e942c0a76fc
Haavard Nord <hanord@trolltech.com>
1994-11-08 15:56:47 +0100

    Serializing, history etc.
    [imported from CVS by cvs2p4 at 1999/10/31 16:57:48]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 196]

--- src/kernel/qregion.h
+++ src/kernel/qregion.h
@@ -1,12 +1,12 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qregion.h#2 $
+** $Id: //depot/qt/main/src/kernel/qregion.h#3 $
 **
 ** Definition of QRegion class
 **
 ** Author  : Haavard Nord
 ** Created : 940514
 **
-** Copyright (C) 1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.  All rights reserved.
 **
 *****************************************************************************/
 
@@ -15,6 +15,7 @@
 
 #include "qshared.h"
 #include "qrect.h"
+#include "qstring.h"
 
 
 class QRegion
@@ -30,6 +31,8 @@ public:
    ~QRegion();
     QRegion &operator=( const QRegion & );
 
+    QRegion copy() const;
+
     bool    isNull()  const;
 
     bool    contains( const QPoint &p ) const;
@@ -42,8 +45,18 @@ public:
     QRegion subtract( const QRegion & ) const;
     QRegion xor( const QRegion & )	const;
 
+    bool    operator==( const QRegion & );
+    bool    operator!=( const QRegion &r )
+    			{ return !(operator==(r)); }
+
+    friend QDataStream &operator<<( QDataStream &, const QRegion & );
+    friend QDataStream &operator>>( QDataStream &, QRegion & );
+
 private:
+    void    cmd( int id, void *, const QRegion * = 0, const QRegion * = 0 );
+    void    exec();
     struct QRegionData : QShared {		// region data
+	QByteArray bop;
 #if defined(_WS_WIN_)
 	HANDLE rgn;
 #elif defined(_WS_PM_)
@@ -56,6 +69,17 @@ private:
     static HPS hps;
 #endif
 };
+
+
+#define QRGN_NOP	0			// region stream commands
+#define QRGN_SETRECT	1
+#define QRGN_SETELLIPSE	2
+#define QRGN_SETPTARRAY	3
+#define QRGN_MOVE	4
+#define QRGN_OR		5
+#define QRGN_AND	6
+#define QRGN_SUB	7
+#define QRGN_XOR	8
 
 
 #endif // QREGION_H
