f039810c5957cbd98f2fd5255930ffa5f68385f2
Haavard Nord <hanord@trolltech.com>
1994-11-10 07:30:01 +0100

    Moved constants into qpaintdc.h
    [imported from CVS by cvs2p4 at 1999/10/31 16:58:00]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 219]

--- src/kernel/qpaintd.h
+++ src/kernel/qpaintd.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpaintd.h#9 $
+** $Id: //depot/qt/main/src/kernel/qpaintd.h#10 $
 **
 ** Definition of QPaintDevice class
 **
@@ -33,66 +33,15 @@
 #define PDF_PAINTACTIVE	0x20
 
 
-// Painter device commands (for programmable, extended devices)
+// Painter device command param (defined in qpaintdc.h)
 
-#define PDC_RESERVED_START      0		// codes 0-99 are reserved
-#define PDC_RESERVED_STOP	99		//   for Troll Tech
-
-#define PDC_NOP			0		//  <void>
-#define PDC_DRAWPOINT		1		// point
-#define PDC_MOVETO		2		// point
-#define PDC_LINETO		3		// point
-#define PDC_DRAWLINE		4		// point,point
-#define PDC_DRAWRECT		5		// rect
-#define PDC_DRAWROUNDRECT	6		// rect,ival,ival
-#define PDC_DRAWELLIPSE		7		// rect
-#define PDC_DRAWARC		8		// rect,ival,ival
-#define PDC_DRAWPIE		9		// rect,ival,ival
-#define PDC_DRAWCHORD		10		// rect,ival,ival
-#define PDC_DRAWLINESEGS	11		// ptarr
-#define PDC_DRAWPOLYLINE	12		// ptarr
-#define PDC_DRAWPOLYGON		13		// ptarr,ival
-#define PDC_DRAWTEXT		14		// point,str
-#define PDC_DRAWTEXTALIGN	15		// NI
-#define PDC_DRAWPIXMAP		16		// NI
-#define PDC_BEGIN		20		//  <void>
-#define PDC_END			21		//  <void>
-#define PDC_SAVE		22		//  <void>
-#define PDC_RESTORE		23		//  <void>
-#define PDC_SETBKCOLOR		30		// color
-#define PDC_SETBKMODE		31		// ival
-#define PDC_SETROP		32		// ival
-#define PDC_SETBRUSHORIGIN	33		// point
-#define PDC_SETFONT		35		// font
-#define PDC_SETPEN		36		// pen
-#define PDC_SETBRUSH		37		// brush
-#define PDC_SETUNIT		40		// ival
-#define PDC_SETVXFORM		41		// ival
-#define PDC_SETSOURCEVIEW	42		// rect
-#define PDC_SETTARGETVIEW	43		// rect
-#define PDC_SETWXFORM		44		// ival
-#define PDC_SETWXFMATRIX	45		// matrix,ival
-#define PDC_SETCLIP		50		// ival
-#define PDC_SETCLIPRGN		51		// rgn
-
-union QPDevCmdParam {
-    int		 ival;
-    char	*str;
-    QPoint	*point;
-    QRect	*rect;
-    QPointArray *ptarr;
-    QColor	*color;
-    QFont	*font;
-    QPen	*pen;
-    QBrush	*brush;
-    QRegion	*rgn;
-    QWXFMatrix  *matrix;
-};
+union QPDevCmdParam;
 
 
 class QPaintDevice				// device for QPainter
 {
 friend class QPainter;
+friend class QPaintDeviceMetrics;
 public:
     QPaintDevice();
     virtual ~QPaintDevice();
@@ -129,9 +78,8 @@ protected:
 
 private:
     uint     devFlags;				// device flags
-#if defined(_WS_X11_)
     virtual bool cmd( int, QPDevCmdParam * );
-#endif
+    virtual int  metric( int );
 };
 
 
