b04803763ed99fe7fdd2f8ff5a671c327b84d16a
Haavard Nord <hanord@trolltech.com>
1994-11-30 15:42:14 +0100

    Converted to int functions
    [imported from CVS by cvs2p4 at 1999/10/31 16:59:19]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 341]

--- src/kernel/qpaintdevicemetrics.h
+++ src/kernel/qpaintdevicemetrics.h
@@ -1,12 +1,12 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpaintdevicemetrics.h#2 $
+** $Id: //depot/qt/main/src/kernel/qpaintdevicemetrics.h#3 $
 **
 ** Definition of QPaintDeviceMetrics class
 **
 ** Author  : Haavard Nord
 ** Created : 941109
 **
-** Copyright (C) 1994 by Troll Tech AS.  All rights reserved.
+** Copyright (C) 1994 by Troll Tech AS.	 All rights reserved.
 **
 *****************************************************************************/
 
@@ -23,10 +23,10 @@ class QPaintDeviceMetrics			// paint device metrics
 public:
     QPaintDeviceMetrics( const QPaintDevice * );
 
-    uint    width()	const	{ return (int)pdev->metric(PDM_WIDTH); }
-    uint    height()	const	{ return (int)pdev->metric(PDM_HEIGHT); }
-    uint    widthMM()	const	{ return (int)pdev->metric(PDM_WIDTHMM); }
-    uint    heightMM()	const	{ return (int)pdev->metric(PDM_HEIGHTMM); }
+    int	  width()	const	{ return (int)pdev->metric(PDM_WIDTH); }
+    int	  height()	const	{ return (int)pdev->metric(PDM_HEIGHT); }
+    int	  widthMM()	const	{ return (int)pdev->metric(PDM_WIDTHMM); }
+    int	  heightMM()	const	{ return (int)pdev->metric(PDM_HEIGHTMM); }
 
 private:
     QPaintDevice *pdev;
