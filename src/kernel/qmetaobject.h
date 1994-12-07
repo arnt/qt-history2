a3653c8a1985bac9d41322c541e5bba2b213de12
Haavard Nord <hanord@trolltech.com>
1994-12-07 11:23:53 +0100

    Reorganized
    [imported from CVS by cvs2p4 at 1999/10/31 16:59:45]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 387]

--- src/kernel/qmetaobject.h
+++ src/kernel/qmetaobject.h
@@ -1,15 +1,13 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qmetaobject.h#1 $
+** $Id: //depot/qt/main/src/kernel/qmetaobject.h#2 $
 **
 ** Definition of QMetaObject class
 **
 ** Author  : Haavard Nord
 ** Created : 930419
 **
-** Copyright (C) 1993,1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1993,1994 by Troll Tech AS.  All rights reserved.
 **
-** --------------------------------------------------------------------------
-** The QMetaObject class contains meta information about existing objects.
 *****************************************************************************/
 
 #ifndef QMETAOBJ_H
@@ -19,12 +17,10 @@
 
 
 struct QMetaData				// member function meta data
-{						//   for methods, signal, slots
+{						//   for signal and slots
     char   *name;				// - member name
     QMember ptr;				// - member pointer
 };
-
-class QStrList;					// defined in: qstrlist.h
 
 
 class QMetaObject				// meta object class
@@ -32,7 +28,6 @@ class QMetaObject				// meta object class
 friend class QObject;
 public:
     QMetaObject( const char *class_name, const char *superclass_name,
-		 QMetaData *method_data, int n_methods,
 		 QMetaData *slot_data,	int n_slots,
 		 QMetaData *signal_data, int n_signals );
    ~QMetaObject();
@@ -40,28 +35,23 @@ public:
     const char *getClassName()	       const { return className; }
     const char *getSuperclassName()    const { return superclassName; }
 
-    int		nMethods( bool=FALSE ) const;
     int		nSlots( bool=FALSE )   const;
     int		nSignals( bool=FALSE ) const;
 
-    QMember    *method( const char *, bool=FALSE )  const;
-    QMember    *slot( const char *, bool=FALSE )    const;
-    QMember    *signal( const char *, bool=FALSE )  const;
+    QMetaData  *slot( const char *, bool=FALSE )   const;
+    QMetaData  *signal( const char *, bool=FALSE ) const;
 
-    void	getMethods( QStrList *, bool=FALSE) const;
-    void	getSlots( QStrList *,bool=FALSE )   const;
-    void	getSignals( QStrList *, bool=FALSE) const;
+    QMetaData  *slot( int index, bool=FALSE )	   const;
+    QMetaData  *signal( int index, bool=FALSE )	   const;
 
 private:
     QMemberDict*init( QMetaData *, int );
-    QMember    *member( int code, const char *, bool ) const;
-    void	getNames( int code, QStrList *, bool ) const;
+    QMetaData  *mdata( int code, const char *, bool ) const;
+    QMetaData  *mdata( int code, int, bool ) const;
 
     char       *className;			// class name
     char       *superclassName;			// super class name
     QMetaObject*superMetaObject;		// super meta object
-    QMetaData  *methodData;			// method meta data
-    QMemberDict*methodDict;			// method dictionary
     QMetaData  *slotData;			// slot meta data
     QMemberDict*slotDict;			// slot dictionary
     QMetaData  *signalData;			// signal meta data
