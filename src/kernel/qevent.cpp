ba15af33ad1a2c7accceb6f62b352894a2eb118e
Haavard Nord <hanord@trolltech.com>
1994-12-16 11:29:42 +0100

    Initial revision
    [imported from CVS by cvs2p4 at 1999/10/31 17:00:26]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 457]

---
+++ src/kernel/qevent.cpp
@@ -0,0 +1,97 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/kernel/qevent.cpp#1 $
+**
+** Implementation of QEvent classes
+**
+** Author  : Haavard Nord
+** Created : 931029
+**
+** Copyright (C) 1993,1994 by Troll Tech AS.  All rights reserved.
+**
+*****************************************************************************/
+
+#include "qevent.h"
+
+#if defined(DEBUG)
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qevent.cpp#1 $";
+#endif
+
+
+void qRemovePostedEvent( QEvent * );		// defined in qapp_xxx.cpp
+
+
+void QEvent::peErrMsg()				// posted event error message
+{
+#if defined(CHECK_STATE)
+    char *n = 0;
+    switch ( t ) {				// convert type to msg string
+	case Event_Timer:
+	    n = "Timer";
+	    break;
+	case Event_MouseButtonPress:
+	    n = "MouseButtonPress";
+	    break;
+	case Event_MouseButtonRelease:
+	    n = "MouseButtonRelease";
+	    break;
+	case Event_MouseButtonDblClick:
+	    n = "MouseButtonDblClick";
+	    break;
+	case Event_MouseMove:
+	    n = "MouseMove";
+	    break;
+	case Event_KeyPress:
+	    n = "KeyPress";
+	    break;
+	case Event_KeyRelease:
+	    n = "KeyRelease";
+	    break;
+	case Event_FocusIn:
+	    n = "FocusIn";
+	    break;
+	case Event_FocusOut:
+	    n = "FocusOut";
+	    break;
+	case Event_Enter:
+	    n = "Enter";
+	    break;
+	case Event_Leave:
+	    n = "Leave";
+	    break;
+	case Event_Paint:
+	    n = "Paint";
+	    break;
+	case Event_Move:
+	    n = "Move";
+	    break;
+	case Event_Resize:
+	    n = "Resize";
+	    break;
+	case Event_Create:
+	    n = "Create";
+	    break;
+	case Event_Destroy:
+	    n = "Destroy";
+	    break;
+	case Event_Show:
+	    n = "Show";
+	    break;
+	case Event_Hide:
+	    n = "Hide";
+	    break;
+	case Event_Close:
+	    n = "Close";
+	    break;
+	case Event_Quit:
+	    n = "Quit";
+	    break;
+    }
+    if ( n )
+	warning( "QEvent: Posted event %s cannot be stack variable, ignored",
+		 n );
+    else
+	warning( "QEvent: Posted event %d cannot be stack variable, ignored",
+		 t );
+#endif
+    qRemovePostedEvent( this );
+}
