c755c95a0aca8973d6cb640d5a1299cc67823fea
Haavard Nord <hanord@trolltech.com>
1995-01-18 13:15:36 +0100

    Initial revision
    [imported from CVS by cvs2p4 at 1999/10/31 17:01:54]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 575]

---
+++ src/kernel/qwidcoll.h
@@ -0,0 +1,28 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/kernel/qwidcoll.h#1 $
+**
+** Definition of QWidget collections
+**
+** Author  : Haavard Nord
+** Created : 950116
+**
+** Copyright (C) 1995 by Troll Tech AS.  All rights reserved.
+**
+*****************************************************************************/
+
+#ifndef QWIDCOLL_H
+#define QWIDCOLL_H
+
+#include "qwidget.h"
+#include "qlist.h"
+#include "qintdict.h"
+
+
+typedef declare(QListM,QWidget)		QWidgetList;
+typedef declare(QListIteratorM,QWidget) QWidgetListIt;
+
+typedef declare(QIntDictM,QWidget)	   QWidgetIntDict;
+typedef declare(QIntDictIteratorM,QWidget) QWidgetIntDictIt;
+
+
+#endif // QWIDCOLL_H
