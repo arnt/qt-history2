ab37ef3af8bed5cad5945a03a7e35c522bcf93f0
Haavard Nord <hanord@trolltech.com>
1995-01-18 16:04:04 +0100

    Serialization format changed
    [imported from CVS by cvs2p4 at 1999/10/31 17:01:56]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 579]

--- src/kernel/qcursor.cpp
+++ src/kernel/qcursor.cpp
@@ -1,36 +1,32 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qcursor.cpp#2 $
+** $Id: //depot/qt/main/src/kernel/qcursor.cpp#3 $
 **
 ** Implementation of QCursor class
 **
 ** Author  : Haavard Nord
 ** Created : 940220
 **
-** Copyright (C) 1994 by Troll Tech as.  All rights reserved.
+** Copyright (C) 1994,1995 by Troll Tech AS.  All rights reserved.
 **
-** --------------------------------------------------------------------------
-** This file containts the platform independent implementation of the QCursor
-** class, which is mainly stream functions.  Platform dependent functions are
-** found in the qcur_xxx.C files.
 *****************************************************************************/
 
 #include "qcursor.h"
 #include "qdstream.h"
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qcursor.cpp#2 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qcursor.cpp#3 $";
 #endif
 
 
 QDataStream &operator<<( QDataStream &s, const QCursor &c )
 {
-    return s << c.shape();			// write shape id to stream
+    return s << (INT16)c.shape();		// write shape id to stream
 }
 
 QDataStream &operator>>( QDataStream &s, QCursor &c )
 {
-    int shape;
+    INT16 shape;
     s >> shape;					// read shape id from stream
-    c.setShape( shape );			// create cursor with shape
+    c.setShape( (int)shape );			// create cursor with shape
     return s;
 }
