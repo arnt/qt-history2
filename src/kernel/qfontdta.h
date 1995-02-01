0c2689d0fe7c26169db55f72a9bc21e6f37f2187
Eirik Eng <eiriken@trolltech.com>
1995-02-01 17:23:59 +0100

    Changed data structure of font system. All data now centralized in the QFont class. QFontMetrics split into QFontMetrics and  QFontInfo. These 2 classes contain a referenced QFont object only. QFont now has a default font that can be set.  Underline and strikeout added. Redefined QFont::operator== to use adress of data pointer. Added stylehint aliases ( SansSerif, Serif, TypeWriter and Decorative ) defaultFamily now uses StyleHint.  lastResortFont searches for a font when all else fails.  Added QFontMetrics::maxWidth
    [imported from CVS by cvs2p4 at 1999/10/31 17:02:13]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 607]

--- src/kernel/qfontdta.h
+++ src/kernel/qfontdta.h
@@ -1,16 +1,16 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qfontdta.h#2 $
+** $Id: //depot/qt/main/src/kernel/qfontdta.h#3 $
 **
 ** Definition of QFontData struct
 **
 ** Author  : Eirik Eng
 ** Created : 941229
 **
-** Copyright (C) 1994 by Troll Tech AS.	 All rights reserved.
+** Copyright (C) 1994, 1995 by Troll Tech AS.	 All rights reserved.
 **
 ** --------------------------------------------------------------------------
-** Internal header file containing private data common to QFont and
-** QFontMetrics.
+** Internal header file containing private data common to QFont,
+** QFontInfo and QFontMetrics.
 **
 ** Uses definitions found in qfont.h, must always be included after qfont.h.
 *****************************************************************************/
@@ -19,25 +19,35 @@
 #define QFONTDTA_H
 
 
-struct QFontData : QShared {	// font data, used by QFont and QFontMetrics
+struct QFontDef  {
 	QString	  family;
 	short	  pointSize;
-#if defined(_WS_WIN_)
-	HANDLE	hfont;
-#elif defined(_WS_PM_)
-#elif defined(_WS_X11_)
-	QXFontStruct *f;
-	QString	  xFontName;
-#endif
 	uint	  styleHint	: 8;
 	uint	  charSet	: 8;
 	uint	  weight	: 8;
 	uint	  italic	: 1;
+	uint	  underline	: 1;
+	uint	  strikeOut	: 1;
 	uint	  fixedPitch	: 1;
 	uint	  hintSetByUser : 1;
 	uint	  rawMode	: 1;
 	uint	  dirty		: 1;
 	uint	  exactMatch	: 1;
+};
+
+struct QFontData : QShared {
+    QFontDef        req;                // requested
+    QFontDef        act;                // actual
+    short           lineW;              // width of underline and strikeOut
+    short           refCount;           // number of QFontInfo and QFontMetrics
+                                        // referencing this font.
+#if defined(_WS_WIN_)
+	HANDLE	hfont;
+#elif defined(_WS_PM_)
+#elif defined(_WS_X11_)
+	QXFontStruct *xfont;
+	QString	  xFontName;
+#endif
 };
 
 
