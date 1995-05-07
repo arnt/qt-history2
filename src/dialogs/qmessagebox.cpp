1d1e78009be0eeb591a87d37d353749d522ebbb2
Haavard Nord <hanord@trolltech.com>
1995-05-07 15:58:15 +0100

    Initial revision
    [imported from CVS by cvs2p4 at 1999/10/31 17:08:46]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 1092]

---
+++ src/dialogs/qmessagebox.cpp
@@ -0,0 +1,91 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/dialogs/qmessagebox.cpp#1 $
+**
+** Implementation of QMessageBox class
+**
+** Author  : Haavard Nord
+** Created : 950503
+**
+** Copyright (C) 1995 by Troll Tech AS.  All rights reserved.
+**
+*****************************************************************************/
+
+#include "qmsgbox.h"
+#include "qlabel.h"
+#include "qpushbt.h"
+
+#if defined(DEBUG)
+static char ident[] = "$Id: //depot/qt/main/src/dialogs/qmessagebox.cpp#1 $";
+#endif
+
+
+/*!
+\class QMessageBox qmsgbox.h
+\brief The QMessageBox is a modal view that displays a text and contains a
+push button.
+The default push button text is "Ok". This can be changed with setButtonText().
+*/
+
+QMessageBox::QMessageBox( QWidget *parent, const char *name )
+	: QDialog( parent, name )
+{
+    label = new QLabel( this, "text" );
+    CHECK_PTR( label );
+    button = new QPushButton( this, "button" );
+    CHECK_PTR( button );
+    button->setLabel( "Ok" );
+    connect( button, SIGNAL(clicked()), SLOT(accept()) );
+}
+
+
+/*!
+Returns the message box text currently set, or null if no text has been set.
+\sa setText().
+*/
+
+const char *QMessageBox::text() const
+{
+    return label->label();
+}
+
+/*!
+Sets the message box text to be displayed.
+\sa text().
+*/
+
+void QMessageBox::setText( const char *text )
+{
+    label->setLabel( text );
+    resize( size() );
+}
+
+/*!
+Returns the push button text currently set, or null if no text has been set.
+Initially, the push button text is "Ok".
+\sa setButtonText().
+*/
+
+const char *QMessageBox::buttonText() const
+{
+    return button->label();
+}
+
+/*!
+Sets the push button text to be displayed.
+\sa buttonText().
+*/
+
+void QMessageBox::setButtonText( const char *text )
+{
+    label->setLabel( text );
+    resize( size() );
+}
+
+
+/*!
+Does simple geometry management.
+*/
+
+void QMessageBox::resizeEvent( QResizeEvent * )
+{
+}
---
+++ src/dialogs/qmsgbox.h
@@ -0,0 +1,42 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/dialogs/qmsgbox.h#1 $
+**
+** Definition of QMessageBox class
+**
+** Author  : Haavard Nord
+** Created : 950503
+**
+** Copyright (C) 1995 by Troll Tech AS.  All rights reserved.
+**
+*****************************************************************************/
+
+#ifndef QMSGBOX_H
+
+#include "qdialog.h"
+
+class QLabel;
+class QPushButton;
+
+
+class QMessageBox : public QDialog
+{
+    Q_OBJECT
+public:
+    QMessageBox( QWidget *parent=0, const char *name=0 );
+
+    const char  *text()		const;
+    void	 setText( const char * );
+
+    const char  *buttonText()	const;
+    void	 setButtonText( const char * );
+
+protected:
+    void	 resizeEvent( QResizeEvent * );
+
+private:
+    QLabel      *label;
+    QPushButton *button;
+};
+
+
+#endif // QMSGBOX_H
