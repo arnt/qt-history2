a10e048215deef84ae59a578ea01d33a3b47f545
qtdist <qtdist@trolltech.com>
1995-05-29 15:51:45 +0100

    fixed bool problem
    [imported from CVS by cvs2p4 at 1999/10/31 17:12:03]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 1342]

--- src/kernel/qimage.cpp
+++ src/kernel/qimage.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qimage.cpp#28 $
+** $Id: //depot/qt/main/src/kernel/qimage.cpp#29 $
 **
 ** Implementation of QImage and QImageIO classes
 **
@@ -21,7 +21,7 @@
 #include <ctype.h>
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qimage.cpp#28 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qimage.cpp#29 $";
 #endif
 
 /*!
@@ -431,7 +431,8 @@ int QImage::systemByteOrder()			// determine system byte order
 {
     static int sbo = IgnoreEndian;
     if ( sbo == IgnoreEndian ) {		// initialize
-	int ws, be;
+	int  ws;
+	bool be;
 	qSysInfo( &ws, &be );
 	sbo = be ? BigEndian : LittleEndian;
     }
