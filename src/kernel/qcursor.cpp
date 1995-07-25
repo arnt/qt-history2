89d64ab47d2279eb009a18cc29e3aedeeb5bd6eb
Haavard Nord <hanord@trolltech.com>
1995-07-25 16:00:33 +0100

    moved some platform independent functions here
    [imported from CVS by cvs2p4 at 1999/10/31 17:15:58]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 1584]

--- src/kernel/qcursor.cpp
+++ src/kernel/qcursor.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qcursor.cpp#4 $
+** $Id: //depot/qt/main/src/kernel/qcursor.cpp#5 $
 **
 ** Implementation of QCursor class
 **
@@ -11,11 +11,98 @@
 *****************************************************************************/
 
 #include "qcursor.h"
+#include "qbitmap.h"
 #include "qdstream.h"
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qcursor.cpp#4 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qcursor.cpp#5 $";
 #endif
 
 
-/* TO BE REMOVED */
+/*!
+  \class QCursor qcursor.h
+  \brief The QCursor class provides a mouse cursor with an arbitrary shape.
+
+  This class is used for mainly two things; to create mouse cursors to be
+  associated with widgets and to get and set the position of the mouse
+  cursor.
+
+  Qt has a number of standard cursor shapes, but you can also make a custom
+  cursor shape based on a bitmap, a mask and a hotspot.
+
+  To associate a cursor with a widget, use QWidget::setCursor(). <br>
+  To associate a cursor with all widgets (maybe for a short period of time),
+  use QApplication::setCursor().
+
+  The <a name=cursors>predefined cursor</a> objects are:
+  <dl compact>
+  <dt> arrowCursor <dd> standard arrow cursor
+  <dt> upArrowCursor <dd> upwards arrow
+  <dt> crossCursor <dd> crosshair
+  <dt> hourGlassCursor <dd> hourglass/watch
+  <dt> ibeamCursor <dd> ibeam/text entry
+  <dt> sizeVerCursor <dd> vertical resize
+  <dt> sizeHorCursor <dd> horizontal resize
+  <dt> sizeBDiagCursor <dd> diagonal resize (/)
+  <dt> sizeFDiagCursor <dd> diagonal resize (\)
+  <dt> sizeAllCursor <dd> all directions resize
+  </dl>
+
+  QCursor objects make use of implicit sharing.
+*/
+
+
+// --------------------------------------------------------------------------
+// Global cursors
+//
+
+const QCursor arrowCursor;
+const QCursor upArrowCursor;
+const QCursor crossCursor;
+const QCursor hourGlassCursor;
+const QCursor ibeamCursor;
+const QCursor sizeVerCursor;
+const QCursor sizeHorCursor;
+const QCursor sizeBDiagCursor;
+const QCursor sizeFDiagCursor;
+const QCursor sizeAllCursor;
+
+
+// --------------------------------------------------------------------------
+// QCursor stream functions
+//
+
+/*!
+  \relates QCursor
+  Writes a cursor to the stream.
+*/
+
+QDataStream &operator<<( QDataStream &s, const QCursor &c )
+{
+    s << (INT16)c.shape();			// write shape id to stream
+    if ( c.shape() == BitmapCursor ) {		// bitmap cursor
+	s << *c.bitmap() << *c.mask();
+	s << c.hotSpot();
+    }
+    return s;
+}
+
+/*!
+  \relates QCursor
+  Reads a cursor from the stream.
+*/
+
+QDataStream &operator>>( QDataStream &s, QCursor &c )
+{
+    INT16 shape;
+    s >> shape;					// read shape id from stream
+    if ( shape == BitmapCursor ) {		// read bitmap cursor
+	QBitmap bm, bmm;
+	QPoint  hot;
+	s >> bm >> bmm >> hot;
+	c = QCursor( bm, bmm, hot.x(), hot.y() );
+    }
+    else
+	c.setShape( (int)shape );		// create cursor with shape
+    return s;
+}
