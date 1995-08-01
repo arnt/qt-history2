5cc2fc3de0ca70048be46d4c463541862cf04ab8
Haavard Nord <hanord@trolltech.com>
1995-08-01 16:54:48 +0100

    using another way of getting font metrics
    [imported from CVS by cvs2p4 at 1999/10/31 17:18:10]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 1669]

--- src/kernel/qpainter.h
+++ src/kernel/qpainter.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpainter.h#46 $
+** $Id: //depot/qt/main/src/kernel/qpainter.h#47 $
 **
 ** Definition of QPainter class
 **
@@ -54,8 +54,8 @@ public:
 
   // Drawing tools
 
-    QFontMetrics fontMetrics()	const { return QFontMetrics(pdev); }
-    QFontInfo	 fontInfo()	const { return QFontInfo(pdev); }
+    QFontMetrics fontMetrics()	const { return QFontMetrics(this); }
+    QFontInfo	 fontInfo()	const { return QFontInfo(this); }
 
     const QFont &font();			// get/set font
     void	setFont( const QFont & );
