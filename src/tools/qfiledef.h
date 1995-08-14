20c0abe80a9f677577032576e9aa0b03ed6a558d
Eirik Eng <eiriken@trolltech.com>
1995-08-14 13:03:39 +0100

    Initial revision
    [imported from CVS by cvs2p4 at 1999/10/31 17:21:58]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 1807]

---
+++ src/tools/qfiledef.h
@@ -0,0 +1,95 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/tools/qfiledef.h#1 $
+**
+**                    ***   INTERNAL HEADER FILE   ***
+**
+**              This file is NOT a part of the Qt interface!
+**
+** QFile, QFileInfo and QDir related macros and includes,
+** used by qfile.cpp, qfileinf.cpp and qdir.cpp
+**
+** Author  : Eirik Eng
+** Created : 950809
+**
+** Copied from the QFile class written by Haavad Nord.
+**
+** Copyright (C) 1995 by Troll Tech AS.  All rights reserved.
+**
+*****************************************************************************/
+
+#if !defined(_OS_MAC_)
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
+#include <fcntl.h>
+#include <errno.h>
+#if defined(UNIX)
+#include <unistd.h>
+#endif
+#if defined(_OS_MSDOS_) || defined(_OS_WIN32_) || defined(_OS_OS2_)
+#include <io.h>
+#endif
+#include <limits.h>
+
+#undef STATBUF
+#undef STAT
+#undef STAT_REG
+#undef STAT_DIR
+#undef STAT_LNK
+#undef OPEN
+#undef CLOSE
+#undef LSEEK
+#undef READ
+#undef WRITE
+#undef OPEN_RDONLY
+#undef OPEN_WRONLY
+#undef OPEN_CREAT
+#undef OPEN_TRUNC
+#undef OPEN_APPEND
+#undef OPEN_TEXT
+
+#if defined(_CC_MSC_) || defined(_CC_SYM_)
+#define STATBUF	 _stat				// non-ANSI defs
+#define STAT	 _stat
+#define STAT_REG _S_IFREG
+#define STAT_DIR _S_IFDIR
+#if defined(_S_IFLNK)
+#define STAT_LNK _S_IFLNK
+#endif
+#define OPEN	::_open
+#define CLOSE	::_close
+#define LSEEK	_lseek
+#define READ	_read
+#define WRITE	_write
+#define OPEN_RDONLY	_O_RDONLY
+#define OPEN_WRONLY	_O_WRONLY
+#define OPEN_RDWR	_O_RDWR
+#define OPEN_CREAT	_O_CREAT
+#define OPEN_TRUNC	_O_TRUNC
+#define OPEN_APPEND	_O_APPEND
+#define OPEN_TEXT	_O_TEXT
+
+#else						// all other systems
+
+#define STATBUF	 stat
+#define STAT	 ::stat
+#define STAT_REG S_IFREG
+#define STAT_DIR S_IFDIR
+#if defined(S_IFLNK)
+#define STAT_LNK S_IFLNK
+#endif
+#define OPEN	::open
+#define CLOSE	::close
+#define LSEEK	::lseek
+#define READ	::read
+#define WRITE	::write
+#define OPEN_RDONLY	O_RDONLY
+#define OPEN_WRONLY	O_WRONLY
+#define OPEN_RDWR	O_RDWR
+#define OPEN_CREAT	O_CREAT
+#define OPEN_TRUNC	O_TRUNC
+#define OPEN_APPEND	O_APPEND
+#if defined(O_TEXT)
+#define OPEN_TEXT	O_TEXT
+#endif
+#endif
