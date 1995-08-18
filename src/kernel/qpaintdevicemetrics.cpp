c57074fe2bcc183176ee1700065f6aa11944875d
Haavard Nord <hanord@trolltech.com>
1995-08-18 19:08:56 +0100

    added doc
    [imported from CVS by cvs2p4 at 1999/10/31 17:24:04]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 1905]

--- src/kernel/qpaintdevicemetrics.cpp
+++ src/kernel/qpaintdevicemetrics.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpaintdevicemetrics.cpp#4 $
+** $Id: //depot/qt/main/src/kernel/qpaintdevicemetrics.cpp#5 $
 **
 ** Implementation of QPaintDeviceMetrics class
 **
@@ -13,19 +13,60 @@
 #include "qpdevmet.h"
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qpaintdevicemetrics.cpp#4 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qpaintdevicemetrics.cpp#5 $";
 #endif
 
-/*! \class QPaintDeviceMetrics qpdevmet.h
 
+/*!
+  \class QPaintDeviceMetrics qpdevmet.h
   \brief The QPaintDeviceMetrics class provides information about a
   paint device.
 
-  This class is not yet documented.  Our <a
-  href=http://www.troll.no/>home page</a> contains a pointer to the
-  current version of Qt. */
+  \ingroup paintdevice
+
+  Sometimes it is necessary to obtain information about the
+  physical size of a paint device when drawing graphics, especially
+  for \link QPrinter printing\endlink.
+  
+  Example:
+  \code
+    QPaintDeviceMetrics pdm( myWidget );
+    float aspect = (float)pdm.widthMM / (float)pdm.heightMM();
+  \endcode
+*/
 
 QPaintDeviceMetrics::QPaintDeviceMetrics( const QPaintDevice *pd )
 {
     pdev = (QPaintDevice *)pd;
 }
+
+
+/*!
+  \fn int QPaintDeviceMetrics::width() const
+  Returns the width of the paint device, in pixels.
+*/
+
+/*!
+  \fn int QPaintDeviceMetrics::height() const
+  Returns the height of the paint device, in pixels.
+*/
+
+/*!
+  \fn int QPaintDeviceMetrics::widthMM() const
+  Returns the width of the paint device, measured in millimeters.
+*/
+
+/*!
+  \fn int QPaintDeviceMetrics::heightMM() const
+  Returns the height of the paint device, measured in millimeters.
+*/
+
+/*!
+  \fn int QPaintDeviceMetrics::numColors() const
+  Returns the number of different colors available for the paint device.
+*/
+
+/*!
+  \fn int QPaintDeviceMetrics::depth() const
+  Returns the bit depth (number of bit planes) of the paint device.
+*/
