7b0b38c7f7e8715d41c0063825a4bad323f4b861
Haavard Nord <hanord@trolltech.com>
1995-08-22 19:46:11 +0100

    Working event filter for QApplication
    [imported from CVS by cvs2p4 at 1999/10/31 17:24:56]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 1953]

--- src/kernel/qapp.cpp
+++ src/kernel/qapp.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qapp.cpp#55 $
+** $Id: //depot/qt/main/src/kernel/qapp.cpp#56 $
 **
 ** Implementation of QApplication class
 **
@@ -17,11 +17,11 @@
 #include "qpalette.h"
 
 #if defined(DEBUG)
-static char ident[] = "$Id: //depot/qt/main/src/kernel/qapp.cpp#55 $";
+static char ident[] = "$Id: //depot/qt/main/src/kernel/qapp.cpp#56 $";
 #endif
 
 
-/*!
+/*----------------------------------------------------------------------------
   \class QApplication qapp.h
   \brief The QApplication class manages the application event queue.
 
@@ -58,7 +58,8 @@ static char ident[] = "$Id: //depot/qt/main/src/kernel/qapp.cpp#55 $";
 
   \header qkeycode.h
   \header qwindefs.h
-  \header qglobal.h */
+  \header qglobal.h
+ ----------------------------------------------------------------------------*/
 
 
 void qt_init( int *, char ** );			// defined in qapp_???.cpp
@@ -107,7 +108,7 @@ static void destroy_palettes()
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   Creates an application object with the command line parameters \e argc
   and \e argv.
 
@@ -141,7 +142,8 @@ static void destroy_palettes()
   <li> -name \e name, Sets the application name.
   <li> -title \e title, Sets the application title (caption).
   <li> -sync, Turns X into synchronous mode for debugging.
-  </ul> */
+  </ul>
+ ----------------------------------------------------------------------------*/
 
 QApplication::QApplication( int &argc, char **argv )
 {
@@ -168,10 +170,10 @@ QApplication::QApplication( int &argc, char **argv )
     starting_up = FALSE;			// no longer starting up
 }
 
-/*!
+/*----------------------------------------------------------------------------
   Closes all widgets and cleans up all window system resources.
   Sets \c qApp to 0.
-*/
+ ----------------------------------------------------------------------------*/
 
 QApplication::~QApplication()
 {
@@ -190,23 +192,23 @@ QApplication::~QApplication()
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   \fn GUIStyle QApplication::style()
   Returns the GUI style of the application.
 
-  \sa setStyle().
-*/
+  \sa setStyle()
+ ----------------------------------------------------------------------------*/
 
 
-/*!
+/*----------------------------------------------------------------------------
   Sets the application GUI style to \e style.
 
   The style parameter can be \c MacStyle, \c WindowsStyle, \c PMStyle or
   \c MotifStyle.
   Only \c MotifStyle is supported in the current version of Qt.
 
-  \sa style().
-*/
+  \sa style()
+ ----------------------------------------------------------------------------*/
 
 void QApplication::setStyle( GUIStyle style )	// set application GUI style
 {
@@ -220,24 +222,24 @@ void QApplication::setStyle( GUIStyle style )	// set application GUI style
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   Returns a pointer to the default application palette.	 There will always
   be an application palette, i.e. the returned pointer will never be 0.
-*/
+ ----------------------------------------------------------------------------*/
 
 QPalette *QApplication::palette()
 {
     return app_pal;
 }
 
-/*!
+/*----------------------------------------------------------------------------
   Changes the default application palette to \e palette.
 
   If \e updateAllWidgets is TRUE, then this palette will be set for
   all existing widgets.
   If \e updateAllWidgets is FALSE (default), then only widgets that
   are created after this call will have the palette.
-*/
+ ----------------------------------------------------------------------------*/
 
 void QApplication::setPalette( const QPalette &palette, bool updateAllWidgets )
 {
@@ -256,54 +258,54 @@ void QApplication::setPalette( const QPalette &palette, bool updateAllWidgets )
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   \fn QCursor *QApplication::cursor()
   Returns the application cursor.
   This function returns 0 if no application cursor has been defined.
-  \sa setCursor().
-*/
+  \sa setCursor()
+ ----------------------------------------------------------------------------*/
 
-/*!
+/*----------------------------------------------------------------------------
   \fn void QApplication::setCursor( const QCursor &c )
   Sets the application cursor to \e c.
 
   This cursor will be displayed in all application widgets until
   restoreCursor() is called.
 
-  Example of use:
+  Example:
   \code
-    QApplication::setCursor( hourGlassCursor );
-    calculate_mandelbrot();		// lunch time...
+    QApplication::setCursor( waitCursor );
+    calculate_mandelbrot();			// lunch time...
     QApplication::restoreCursor();
   \endcode
 
-  \sa cursor(), restoreCursor().
-*/
+  \sa cursor(), restoreCursor()
+ ----------------------------------------------------------------------------*/
 
-/*!
+/*----------------------------------------------------------------------------
   \fn void QApplication::restoreCursor()
   Restores after some application cursor was set.
 
-  \sa setCursor().
-*/
+  \sa setCursor()
+ ----------------------------------------------------------------------------*/
 
 
-/*!
+/*----------------------------------------------------------------------------
   \fn QFont *QApplication::font()
   Returns the default application font.	 There will always be an application
   font, i.e. the returned pointer will never be 0.
-  \sa setFont().
-*/
+  \sa setFont()
+ ----------------------------------------------------------------------------*/
 
 
-/*!
+/*----------------------------------------------------------------------------
   Changes the default application font to \e font.
 
   If \e updateAllWidgets is TRUE, then this font will be set for
   all existing widgets.
   If \e updateAllWidgets is FALSE (default), then only widgets
   created after this call will have this font.
-*/
+ ----------------------------------------------------------------------------*/
 
 void QApplication::setFont( const QFont &font,	bool updateAllWidgets )
 {						// set application font
@@ -324,7 +326,7 @@ void QApplication::setFont( const QFont &font,	bool updateAllWidgets )
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   \fn QWidget *QApplication::desktop()
   Returns the desktop widget (also called root window).
 
@@ -337,31 +339,31 @@ void QApplication::setFont( const QFont &font,	bool updateAllWidgets )
     d->height();			// returns screen height
     d->setBackgroundColor( red );	// makes desktop red
   \endcode
-*/
+ ----------------------------------------------------------------------------*/
 
-/*!
+/*----------------------------------------------------------------------------
   \fn int QApplication::enter_loop()
   This function enters the main event loop (recursively).
   Do not call it unless you are an expert.
-  \sa exit_loop().
-*/
+  \sa exit_loop()
+ ----------------------------------------------------------------------------*/
 
-/*!
+/*----------------------------------------------------------------------------
   \fn void QApplication::exit_loop()
   This function leaves from a recursive call to the main event loop.
   Do not call it unless you are an expert.
-  \sa enter_loop().
-*/
+  \sa enter_loop()
+ ----------------------------------------------------------------------------*/
 
 
-/*!
+/*----------------------------------------------------------------------------
   \fn QWidget *QApplication::mainWidget() const
   Returns the main application widget.
   \sa setMainWidget()
-*/
+ ----------------------------------------------------------------------------*/
 
 
-/*!
+/*----------------------------------------------------------------------------
   Tells the application to quit.
 
   After quit has been called, the application leaves the main event
@@ -371,7 +373,8 @@ void QApplication::setFont( const QFont &font,	bool updateAllWidgets )
   By convention, \e retcode 0 means success, any non-zero value indicates
   an error.
 
-  \sa quitApp(), exec() */
+  \sa quitApp(), exec()
+ ----------------------------------------------------------------------------*/
 
 void QApplication::quit( int retcode )		// quit application
 {
@@ -384,7 +387,7 @@ void QApplication::quit( int retcode )		// quit application
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   Tells the application to quit with exit code 0 (success).
   Equivalent to calling QApplication::quit(0).
 
@@ -393,12 +396,12 @@ void QApplication::quit( int retcode )		// quit application
 
   Example:
   \code
-    QPushButton *quitButton = new QPushButton( ... );
+    QPushButton *quitButton = new QPushButton;
     connect( quitButton, SIGNAL(clicked()), qApp, SLOT(quitApp()) );
   \endcode  
 
   \sa quit()
-*/
+ ----------------------------------------------------------------------------*/
 
 void QApplication::quitApp()			// quit application
 {
@@ -406,26 +409,25 @@ void QApplication::quitApp()			// quit application
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   \fn bool QApplication::sendEvent( QObject *receiver, QEvent *event )
   Sends an event directly to a receiver, using the notify() function.
   Returns the value that was returned from the event handler.
+  \sa postEvent()
+ ----------------------------------------------------------------------------*/
 
-  \sa postEvent().
-*/
-
-/*!
+/*----------------------------------------------------------------------------
   \fn void QApplication::postEvent( QObject *receiver, QEvent *event )
   Stores the event in a queue and returns immediatly.
 
   Back in the main event loop, all events that are stored in the queue
   will be sent using the notify() function.
 
-  \sa sendEvent().
-*/
+  \sa sendEvent()
+ ----------------------------------------------------------------------------*/
 
 
-/*!
+/*----------------------------------------------------------------------------
   Sends \e event to \e receiver: <code>receiver->event( event );</code>
   Returns the value that was returned from the event handler.
 
@@ -433,8 +435,11 @@ void QApplication::quitApp()			// quit application
   is virtual, you can make a subclass of QApplication and reimplement
   notify() to get total control of Qt events.
 
-  \sa QObject::event().
-*/
+  Installing an event filter on \c qApp is another way of making an
+  application-global event hook.
+
+  \sa QObject::event(), installEventFilter()
+ ----------------------------------------------------------------------------*/
 
 bool QApplication::notify( QObject *receiver, QEvent *event )
 {						// send event to object
@@ -442,22 +447,34 @@ bool QApplication::notify( QObject *receiver, QEvent *event )
     if ( receiver == 0 )			// fatal error
 	warning( "QApplication::notify: Unexpected null receiver" );
 #endif
+    if ( eventFilters ) {
+	QObjectListIt it( *eventFilters );
+	register QObject *obj = it.current();
+	while ( obj ) {				// send to all filters
+	    ++it;				//   until one returns TRUE
+	    if ( obj->eventFilter(receiver,event) )
+		return TRUE;
+	    obj = it.current();
+	}
+    }
     return receiver->event( event );
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   Returns TRUE if an application object has not been created yet.
-*/
+  \sa closingDown()
+ ----------------------------------------------------------------------------*/
 
 bool QApplication::startingUp()			// is application starting up?
 {
     return starting_up;
 }
 
-/*!
+/*----------------------------------------------------------------------------
   Returns TRUE if the application objects is being destroyed.
-*/
+  \sa startingUp()
+ ----------------------------------------------------------------------------*/
 
 bool QApplication::closingDown()		// is application closing down?
 {
@@ -465,17 +482,17 @@ bool QApplication::closingDown()		// is application closing down?
 }
 
 
-/*!
+/*----------------------------------------------------------------------------
   \fn void QApplication::flushX()
   Flushes the X event queue in the X-Windows implementation.
   Does nothing on other platforms.
-*/
+ ----------------------------------------------------------------------------*/
 
-/*!
+/*----------------------------------------------------------------------------
   \fn void QApplication::syncX()
   Synchronizes with the X server in the X-Windows implementation.
   Does nothing on other platforms.
-*/
+ ----------------------------------------------------------------------------*/
 
 
 #if !defined(_WS_X11_)
