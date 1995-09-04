aeb4413c2aef0d8431cba558be37ffc196ba9469
Haavard Nord <hanord@trolltech.com>
1995-09-04 15:43:11 +0100

    removed init()
    [imported from CVS by cvs2p4 at 1999/10/31 17:29:23]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2157]

--- src/widgets/qcombo.h
+++ src/widgets/qcombo.h
@@ -1,5 +1,5 @@
 /**********************************************************************
-** $Id: //depot/qt/main/src/widgets/qcombo.h#10 $
+** $Id: //depot/qt/main/src/widgets/qcombo.h#11 $
 **
 ** Definition of QComboBox class
 **
@@ -77,7 +77,6 @@ protected:
     void	popup();
 
 private:
-    void	init();
     void	reIndex();
     void	currentChanged();
 
