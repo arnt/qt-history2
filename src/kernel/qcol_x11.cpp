bde56195a07c4f8d68540b576fdc0ffad5c0e9ba
Haavard Nord <hanord@trolltech.com>
1995-12-07 12:11:25 +0100

    tiny optimization
    [imported from CVS by cvs2p4 at 1999/10/31 17:34:43]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2477]

--- src/kernel/qcol_x11.cpp
+++ src/kernel/qcol_x11.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qcol_x11.cpp#32 $
+** $Id: //depot/qt/main/src/kernel/qcol_x11.cpp#33 $
 **
 ** Implementation of QColor class for X11
 **
@@ -17,7 +17,7 @@
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qcol_x11.cpp#32 $")
+RCSTAG("$Id: //depot/qt/main/src/kernel/qcol_x11.cpp#33 $")
 
 
 /*****************************************************************************
@@ -332,9 +332,10 @@ void QColor::setRgb( int r, int g, int b )
     if ( (uint)r > 255 || (uint)g > 255 || (uint)b > 255 )
 	warning( "QColor::setRgb: RGB parameter(s) out of range" );
 #endif
-    rgbVal = QRGB(r,g,b);
-    if ( lazyAlloc() || !g_cmap )
-	rgbVal |= RGB_DIRTY;			// alloc later
-    else
+    if ( lalloc || g_cmap == 0 )
+	rgbVal = QRGB(r,g,b) | RGB_DIRTY;	// alloc later
+    else {
+	rgbVal = QRGB(r,g,b);
 	alloc();				// alloc now
+    }
 }
