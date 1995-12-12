3e79707ebf97586fca0634c1d6f50a8bc8fdff21
Haavard Nord <hanord@trolltech.com>
1995-12-12 17:07:31 +0100

    caption etc. moved to QWidget
    [imported from CVS by cvs2p4 at 1999/10/31 17:35:12]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2526]

--- src/kernel/qwindow.cpp
+++ src/kernel/qwindow.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qwindow.cpp#3 $
+** $Id: //depot/qt/main/src/kernel/qwindow.cpp#4 $
 **
 ** Implementation of QWindow class
 **
@@ -13,19 +13,19 @@
 #include "qwindow.h"
 #include "qpixmap.h"
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qwindow.cpp#3 $")
+RCSTAG("$Id: //depot/qt/main/src/kernel/qwindow.cpp#4 $")
 
 
-/*!
+/*----------------------------------------------------------------------------
   \class QWindow qwindow.h
-  \brief The QWindow class is a widget with a caption and an icon.
+  \brief The QWindow class is reserved for future extensions.
 
-  A window is a widget that can have a caption (window title), an
-  icon and an icon text.
-*/
+  This class is not yet finished.  It will contain intelligent handling of
+  menus etc. in a future Qt release.
+ ----------------------------------------------------------------------------*/
 
 
-/*!
+/*----------------------------------------------------------------------------
   Constructs a window named \e name, which will be a child widget of
   \e parent.
 
@@ -33,42 +33,18 @@ RCSTAG("$Id: //depot/qt/main/src/kernel/qwindow.cpp#3 $")
   are doing.
 
   These arguments are sent to the QWidget constructor.
-*/
+ ----------------------------------------------------------------------------*/
 
 QWindow::QWindow( QWidget *parent, const char *name, WFlags f )
     : QWidget( parent, name, f )
 {
-    initMetaObject();				// initialize meta object
-    ctext = qAppName();				// default caption
-    ipm = 0;
+    initMetaObject();
 }
 
-/*!
+/*----------------------------------------------------------------------------
   Destroys the window.
-*/
+ ----------------------------------------------------------------------------*/
 
 QWindow::~QWindow()
 {
-    delete ipm;
-}
-
-
-/*!
-  Returns the window caption (title).
-  \sa setCaption.
-*/
-
-char *QWindow::caption() const			// get caption text
-{
-    return (char *)((const char*)ctext);
-}
-
-/*!
-  Returns the icon text.
-  \sa setIcon(), setIconText()
-*/
-
-char *QWindow::iconText() const			// get icon text
-{
-    return (char *)((const char*)itext);
 }
--- src/kernel/qwindow.h
+++ src/kernel/qwindow.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qwindow.h#1 $
+** $Id: //depot/qt/main/src/kernel/qwindow.h#2 $
 **
 ** Definition of QWindow class
 **
@@ -18,22 +18,10 @@
 
 class QWindow : public QWidget			// window widget class
 {
-friend class QObject;
     Q_OBJECT
 public:
     QWindow( QWidget *parent=0, const char *name=0, WFlags f=0 );
    ~QWindow();
-
-    char   *caption() const;			// get caption text
-    void    setCaption( const char * );		// set caption text
-    char   *iconText() const;			// get icon text
-    void    setIconText( const char * );	// set icon text
-    void    setIcon( QPixmap * );		// set icon pixmap
-
-private:
-    QString ctext;
-    QString itext;
-    QPixmap *ipm;
 };
 
 
