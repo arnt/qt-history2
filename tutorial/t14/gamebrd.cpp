df53d0d1b598137f7778682a06bb8c592bd9ca43
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1995-12-15 10:40:33 +0100

    cleanup
    [imported from CVS by cvs2p4 at 1999/10/31 17:35:52]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2565]

--- tutorial/t14/gamebrd.cpp
+++ tutorial/t14/gamebrd.cpp
@@ -8,7 +8,6 @@
 #include <qapp.h>
 #include <qlabel.h>
 #include <qaccel.h>
-#include <qkeycode.h>
 #include <qpushbt.h>
 #include <qlcdnum.h>
 
@@ -22,25 +21,19 @@ GameBoard::GameBoard( QWidget *parent, const char *name )
     setMaximumSize( 500, 355 );
 
     quit = new QPushButton( "Quit", this, "quit" );
-    quit->setGeometry( 10, 10, 75, 30 );
     quit->setFont( QFont( "Times", 18, QFont::Bold ) );
     connect( quit, SIGNAL(clicked()), qApp, SLOT(quitApp()) );
 
     angle  = new LCDRange( "ANGLE", this, "angle" );
     angle->setRange( 5, 70 );
-    angle->setGeometry( 10, 45, 75, 130 );
 
     force  = new LCDRange( "FORCE", this, "force" );
     force->setRange( 10, 50 );
-    force->setGeometry( 10, 180, 75, 130 );
 
     QFrame *frame = new QFrame( this, "cannonFrame" );
-    frame->setGeometry( angle->x() + angle->width() + 5, 45, 400, 300 );
     frame->setFrameStyle( QFrame::WinPanel | QFrame::Sunken );
 
     cannon = new CannonField( this, "canonfield" );
-    cannon->setGeometry( frame->x() + 2, frame->y() + 2,
-			 frame->width() - 4, frame->height() - 4 );
     cannon->setBackgroundColor( QColor( 250, 250, 200) );
     cannon->raise();
 
@@ -55,31 +48,38 @@ GameBoard::GameBoard( QWidget *parent, const char *name )
     force->setValue( 25 );
 
     shoot = new QPushButton( "Shoot", this, "shoot" );
-    shoot->setGeometry( 10, 315, 75, 30 );
     shoot->setFont( QFont( "Times", 18, QFont::Bold ) );
     connect( shoot, SIGNAL(clicked()), SLOT(fire()) );
 
     reStart = new QPushButton( "New Game", this, "newgame" );
-    reStart->setGeometry( 380, 10, 110, 30 );
     reStart->setFont( QFont( "Times", 18, QFont::Bold ) );
     connect( reStart, SIGNAL(clicked()), SLOT(newGame()) );
 
     hits  = new QLCDNumber( 2, this, "hits" );
-    hits->setGeometry( 130, 10, 40, 30 );
 
     QLabel *hitsL  = new QLabel( "HITS", this, "hitsLabel" );
-    hitsL->setGeometry( hits->x() + hits->width() + 5, 10, 60, 30 );
 
     shotsLeft  = new QLCDNumber( 2, this, "shotsleft" );
-    shotsLeft->setGeometry( 240, 10, 40, 30 );
 
     QLabel *shotsLeftL  = new QLabel( "SHOTS LEFT", this, "shotsleftLabel" );
-    shotsLeftL->setGeometry( shotsLeft->x() + shotsLeft->width() + 5, 10, 
-                             60, 30);
 
     QAccel *accel = new QAccel( this );
     accel->connectItem( accel->insertItem( Key_Space), this, SLOT(fire()) );
     accel->connectItem( accel->insertItem( Key_Q), qApp, SLOT(quitApp()) );
+
+    quit->setGeometry( 10, 10, 75, 30 );
+    angle->setGeometry( 10, 45, 75, 130 );
+    force->setGeometry( 10, 180, 75, 130 );
+    frame->setGeometry( angle->x() + angle->width() + 5, 45, 400, 300 );
+    cannon->setGeometry( frame->x() + 2, frame->y() + 2,
+			 frame->width() - 4, frame->height() - 4 );
+    shoot->setGeometry( 10, 315, 75, 30 );
+    reStart->setGeometry( 380, 10, 110, 30 );
+    hits->setGeometry( 130, 10, 40, 30 );
+    hitsL->setGeometry( hits->x() + hits->width() + 5, 10, 60, 30 );
+    shotsLeft->setGeometry( 240, 10, 40, 30 );
+    shotsLeftL->setGeometry( shotsLeft->x() + shotsLeft->width() + 5, 10, 
+                             60, 30);
 
     newGame();
 }
