377de21e38c569a808afcc84a0d79b9d93728103
Haavard Nord <hanord@trolltech.com>
1995-12-18 11:14:37 +0100

    sets seed
    [imported from CVS by cvs2p4 at 1999/10/31 17:36:40]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2614]

--- tutorial/t14/main.cpp
+++ tutorial/t14/main.cpp
@@ -4,14 +4,19 @@
 **
 ****************************************************************/
 
-#include <qapp.h>
-
 #include "gamebrd.h"
+#include <qapp.h>
+#include <qdatetm.h>
+
+#include <stdlib.h>
 
 
 int main( int argc, char **argv )
 {
     QApplication a( argc, argv );
+
+    QTime midnight( 0, 0, 0 );
+    srand( midnight.secsTo(QTime::currentTime()) );
 
     GameBoard gb;
     gb.setGeometry( 100, 100, 500, 355 );
