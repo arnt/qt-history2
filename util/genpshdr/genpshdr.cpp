fd8573bcb654afa7b8ad8a7b6e500d638050746c
Haavard Nord <hanord@trolltech.com>
1996-01-05 13:24:12 +0100

    fixed
    [imported from CVS by cvs2p4 at 1999/10/31 17:39:30]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2738]

--- util/genpshdr/genpshdr.cpp
+++ util/genpshdr/genpshdr.cpp
@@ -1,37 +1,74 @@
 #include <qstring.h>
-#include <stdio.h>
-#include <ctype.h>
+#include <qfile.h>
 
-#define BUFSIZE 8192
+
+char *outputstr( char *p, int i )
+{
+    while ( p[i] != ' ' )
+	i--;
+    p[i] = '\0';
+
+    char *x = strchr(p,'%');
+    if ( x ) {					// special treatment for %%
+	char *start = p;
+	p[i] = ' ';				// terminate elsewhere
+	p = x + 2;
+	while ( *p != '%' )			// find %% terminator, %
+	    *p++;
+	*p = '\0';
+	if ( x > start ) {	
+	    x[-1] = '\0';			// terminate before '%%'
+	    printf( "\"%s\\n\"\n", start );
+	    printf( "\"%s\\n\"\n", x );
+	}
+	return p+2;
+    }
+    else {
+	printf( "\"%s\\n\"\n", p );
+	return p+i+1;
+    }
+}
 
 
 int main( int argc, char **argv )
 {
-    FILE *f;
-    int len, i;
-    static char buf[BUFSIZE];
-
     if ( argc != 2 ) {
 	fprintf( stderr, "Usage:\n%s file\n", argv[0] );
 	exit( 1 );
     }
-    f = fopen( argv[1], "r" );
-    if ( !f ) {
+    QFile f( argv[1] );
+    if ( !f.open(IO_ReadOnly) ) {
 	fprintf( stderr, "%s: No such file: %s\n", argv[0], argv[1] );
 	exit( 1 );
     }
-    QString s;
-    while ( !feof(f) ) {			// read to end of file
-	char *p;
-	if ( !fgets(buf, BUFSIZE, f) )		// read line
-	    break;				// EOF reached
-	s = buf;
-	int index = s.find( '%' );
-	if ( index >= 0 && s[index+1] != '%' )
-	    s.truncate( index );
-	s = s.simplifyWhiteSpace();
-	if ( !s.isEmpty() )
-	    printf( "    \"%s\\n\"\n", s.data() );
+    QString buf( f.size()+1 );
+    f.readBlock( buf.data(), f.size() );	// read the file into buf
+    f.close();
+
+    char *p = buf.data();
+
+    while ( *p ) {				// strip comments
+	if ( *p == '%' ) {
+	    if ( *(p+1) == '%' && p > buf.data() && *(p-1) == '\n' ) {
+		while ( *p && *p != '\n' )	// not a comment
+		    p++;
+		if ( *p )			// \n -> %
+		    *p++ = '%';
+	    }
+	    else {
+		while ( *p && *p != '\n' )	// replace with white space
+		    *p++ = ' ';
+	    }
+	}
+	else
+	    p++;
     }
-    fclose( f );
+
+    buf = buf.simplifyWhiteSpace();
+    p = buf.data();
+    char *last = &p[buf.length()];
+
+    while ( p+74<last )
+	p = outputstr( p, 74 );
+    outputstr( p, 0 );
 }
