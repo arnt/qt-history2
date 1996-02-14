f17217303e3d0d780e08320fa06b10f067bf748f
Haavard Nord <hanord@trolltech.com>
1996-02-14 12:58:25 +0100

    Added external file flag
    [imported from CVS by cvs2p4 at 1999/10/31 17:41:49]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2897]

--- src/tools/qfile.h
+++ src/tools/qfile.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qfile.h#9 $
+** $Id: //depot/qt/main/src/tools/qfile.h#10 $
 **
 ** Definition of QFile class
 **
@@ -33,29 +33,31 @@ public:
     bool	exists()   const;
     static bool exists( const char *fileName );
 
-    bool	open( int );			// open file
-    bool	open( int, FILE * );		// open file, using file handle
-    bool	open( int, int );		// open file, using file descr
-    void	close();			// close file
-    void	flush();			// flush file
+    bool	open( int );
+    bool	open( int, FILE * );
+    bool	open( int, int );
+    void	close();
+    void	flush();
 
-    long	size()	const;			// get file size
-    long	at()	const;			// get file pointer
-    bool	at( long );			// set file pointer
+    long	size()	const;
+    long	at()	const;
+    bool	at( long );
+    bool	atEnd() const;
 
     int		readBlock( char *data, uint len );
     int		writeBlock( const char *data, uint len );
     int		readLine( char *data, uint maxlen );
 
-    int		getch();			// get next char
-    int		putch( int );			// put char
-    int		ungetch( int ) ;		// put back char
+    int		getch();
+    int		putch( int );
+    int		ungetch( int );
 
 protected:
-    QString	fn;				// file name
-    FILE       *fh;				// file handle (buffered)
-    int		fd;				// file descriptor (raw)
-    long	length;				// file length
+    QString	fn;
+    FILE       *fh;
+    int		fd;
+    long	length;
+    bool	ext_f;
 
 private:
     void	init();
