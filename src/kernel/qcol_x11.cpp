ce59b5f045a1b15d6a98612e17c4bc0d752d1462
Haavard Nord <hanord@trolltech.com>
1996-03-01 14:02:42 +0100

    Initializes the pix value in setRgb()
    [imported from CVS by cvs2p4 at 1999/10/31 17:42:45]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 2972]

--- src/kernel/qcol_x11.cpp
+++ src/kernel/qcol_x11.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qcol_x11.cpp#37 $
+** $Id: //depot/qt/main/src/kernel/qcol_x11.cpp#38 $
 **
 ** Implementation of QColor class for X11
 **
@@ -17,7 +17,7 @@
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qcol_x11.cpp#37 $")
+RCSTAG("$Id: //depot/qt/main/src/kernel/qcol_x11.cpp#38 $")
 
 
 /*****************************************************************************
@@ -334,8 +334,10 @@ void QColor::setRgb( int r, int g, int b )
     if ( (uint)r > 255 || (uint)g > 255 || (uint)b > 255 )
 	warning( "QColor::setRgb: RGB parameter(s) out of range" );
 #endif
-    if ( lalloc || g_cmap == 0 )
+    if ( lalloc || g_cmap == 0 ) {
 	rgbVal = QRGB(r,g,b) | RGB_DIRTY;	// alloc later
+	pix = 0;
+    }
     else {
 	rgbVal = QRGB(r,g,b);
 	alloc();				// alloc now
