5a83d46395b16715d8c1e82892f14c2e496e5389
Haavard Nord <hanord@trolltech.com>
1996-05-05 15:03:30 +0100

    more inline functions
    [imported from CVS by cvs2p4 at 1999/10/31 17:46:26]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 3195]

--- src/kernel/qpoint.cpp
+++ src/kernel/qpoint.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpoint.cpp#16 $
+** $Id: //depot/qt/main/src/kernel/qpoint.cpp#17 $
 **
 ** Implementation of QPoint class
 **
@@ -14,369 +14,305 @@
 #include "qpoint.h"
 #include "qdstream.h"
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qpoint.cpp#16 $")
+RCSTAG("$Id: //depot/qt/main/src/kernel/qpoint.cpp#17 $")
 
 
-/*!
-\class QPoint qpoint.h
-\brief The QPoint class defines a point in the plane.
+/*----------------------------------------------------------------------------
+  \class QPoint qpoint.h
+  \brief The QPoint class defines a point in the plane.
 
-A point is specified by an x coordinate and a y coordinate.
+  A point is specified by an x coordinate and a y coordinate.
 
-The coordinate type is QCOORD (defined as <code>short</code>). The minimum
-value of QCOORD is -32768 and the maximum value is 32767.
+  The coordinate type is QCOORD (defined as <code>short</code>). The minimum
+  value of QCOORD is -32768 and the maximum value is 32767.
 
-We have defined many operator functions that make arithmetic on points
-simple and intuitive:
-\code
-  QPoint p(  1, 2 );
-  QPoint q( -8, 5 );
-  QPoint r(  9, 7 );
-  QPoint x = 2*p + (q-r)*5.5 - (r+p/1.5);
-\endcode
+  We have defined many operator functions that make arithmetic on points
+  simple and intuitive:
+  \code
+    QPoint p(  1, 2 );
+    QPoint q( -8, 5 );
+    QPoint r(  9, 7 );
+    QPoint x = 2*p + (q-r)*5.5 - (r+p/1.5);
+  \endcode
 
-\sa QSize and QRect.
-*/
-
-
-// --------------------------------------------------------------------------
-// QPoint member functions
-//
-
-/*!
-\fn QPoint::QPoint()
-Constructs a point with undefined x and y values.
-*/
-
-/*!
-Constructs a point with the x value  \e xpos and y value \e ypos.
-*/
-
-QPoint::QPoint( int xpos, int ypos )
-{
-    xp=(QCOORD)xpos; yp=(QCOORD)ypos;
-}
+  \sa QSize, QRect
+ ----------------------------------------------------------------------------*/
 
 
-/*!
-\fn bool QPoint::isNull() const
-Returns TRUE if both the x value and the y value are 0.
-*/
+/*****************************************************************************
+  QPoint member functions
+ *****************************************************************************/
+
+/*----------------------------------------------------------------------------
+  \fn QPoint::QPoint()
+  Constructs a point with undefined x and y values.
+ ----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+  \fn QPoint::QPoint( int xpos, int ypos )
+  Constructs a point with the x value  \e xpos and y value \e ypos.
+ ----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+  \fn bool QPoint::isNull() const
+  Returns TRUE if both the x value and the y value are 0.
+ ----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+  \fn int QPoint::x() const
+  Returns the x coordinate of the point.
+  \sa y()
+ ----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+  \fn int QPoint::y() const
+  Returns the y coordinate of the point.
+  \sa x()
+ ----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+  \fn void QPoint::setX( int x )
+  Sets the x coordinate of the point to \e x.
+  \sa setY()
+ ----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+  \fn void QPoint::setY( int y )
+  Sets the y coordinate of the point to \e y.
+  \sa setX()
+ ----------------------------------------------------------------------------*/
 
 
-/*!
-\fn int QPoint::x() const
-Returns the x coordinate of the point.
+/*----------------------------------------------------------------------------
+  \fn QCOORD &QPoint::rx()
+  Returns a reference to the x coordinate of the point.
 
-\sa y().
-*/
+  Using a reference makes it possible to directly manipulate x:
+  \code
+    QPoint p( 1, 2 );
+    p.rx()--;			// p becomes (0,2)
+  \endcode
 
-/*!
-\fn int QPoint::y() const
-Returns the y coordinate of the point.
+  \sa ry()
+ ----------------------------------------------------------------------------*/
 
-\sa x().
-*/
+/*----------------------------------------------------------------------------
+  \fn QCOORD &QPoint::ry()
+  Returns a reference to the y coordinate of the point.
 
-/*!
-\fn void QPoint::setX( int x )
-Sets the x coordinate of the point to \e x.
+  Using a reference makes it possible to directly manipulate y:
+  \code
+    QPoint p( 1, 2 );
+    p.ry()++;			// p becomes (1,3)
+  \endcode
 
-\sa setY().
-*/
-
-/*!
-\fn void QPoint::setY( int y )
-Sets the y coordinate of the point to \e y.
-
-\sa setX().
-*/
+  \sa rx()
+ ----------------------------------------------------------------------------*/
 
 
-/*!
-\fn QCOORD &QPoint::rx()
-Returns a reference to the x coordinate of the point.
+/*----------------------------------------------------------------------------
+  \fn QPoint &QPoint::operator+=( const QPoint &p )
+  Adds \e p to the point and returns a reference to this point.
 
-Using a reference makes it possible to directly manipulate x:
-\code
-  QPoint p( 1, 2 );
-  p.rx()--;			\/ p becomes (0,2)
-\endcode
+  \code
+    QPoint p(  3, 7 );
+    QPoint q( -1, 4 );
+    p += q;			// p becomes (2,11)
+  \endcode
+ ----------------------------------------------------------------------------*/
 
-\sa ry().
-*/
+/*----------------------------------------------------------------------------
+  \fn QPoint &QPoint::operator-=( const QPoint &p )
+  Subtracts \e p from the point and returns a reference to this point.
 
-/*!
-\fn QCOORD &QPoint::ry()
-Returns a reference to the y coordinate of the point.
+  \code
+    QPoint p(  3, 7 );
+    QPoint q( -1, 4 );
+    p -= q;			// p becomes (4,3)
+  \endcode
+ ----------------------------------------------------------------------------*/
 
-Using a reference makes it possible to directly manipulate y:
-\code
-  QPoint p( 1, 2 );
-  p.ry()++;			\/ p becomes (1,3)
-\endcode
+/*----------------------------------------------------------------------------
+  \fn QPoint &QPoint::operator*=( int c )
+  Multiplies both x and y with \e c, and return a reference to this point.
 
-\sa rx().
-*/
+  \code
+    QPoint p( -1, 4 );
+    p *= 2;			// p becomes (-2,8)
+  \endcode
+ ----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+  \fn QPoint &QPoint::operator*=( double c )
+  Multiplies both x and y with \e c, and return a reference to this point.
+
+  \code
+    QPoint p( -1, 4 );
+    p *= 2.5;			// p becomes (-3,10)
+  \endcode
+
+  Notice that the result is truncated.
+ ----------------------------------------------------------------------------*/
 
 
-/*!
-Adds \e p to the point and returns a reference to this point.
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn bool operator==( const QPoint &p1, const QPoint &p2 )
+  Returns TRUE if \e p1 and \e p2 are equal, or FALSE if they are different.
+ ----------------------------------------------------------------------------*/
 
-\code
-  QPoint p(  3, 7 );
-  QPoint q( -1, 4 );
-  p += q;			\/ p becomes (2,11)
-\endcode
-*/
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn bool operator!=( const QPoint &p1, const QPoint &p2 )
+  Returns TRUE if \e p1 and \e p2 are different, or FALSE if they are equal.
+ ----------------------------------------------------------------------------*/
 
-QPoint &QPoint::operator+=( const QPoint &p )
-{
-    xp+=p.xp; yp+=p.yp; return *this;
-}
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn QPoint operator+( const QPoint &p1, const QPoint &p2 )
+  Returns the sum of \e p1 and \e p2; each component is added separately.
+ ----------------------------------------------------------------------------*/
 
-/*!
-Subtracts \e p from the point and returns a reference to this point.
-\code
-  QPoint p(  3, 7 );
-  QPoint q( -1, 4 );
-  p -= q;			\/ p becomes (4,3)
-\endcode
-*/
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn QPoint operator-( const QPoint &p1, const QPoint &p2 )
+  Returns \e p2 subtracted from \e p1; each component is
+  subtracted separately.
+ ----------------------------------------------------------------------------*/
 
-QPoint &QPoint::operator-=( const QPoint &p )
-{
-    xp-=p.xp; yp-=p.yp; return *this;
-}
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn QPoint operator*( const QPoint &p, int c )
+  Multiplies both of \e p's components by \e c and returns the result.
+ ----------------------------------------------------------------------------*/
 
-/*!
-Multiplies both x and y with \e c, and return a reference to this point.
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn QPoint operator*( int c, const QPoint &p )
+  Multiplies both of \e p's components by \e c and returns the result.
+ ----------------------------------------------------------------------------*/
 
-\code
-  QPoint p( -1, 4 );
-  p *= 2;			\/ p becomes (-2,8)
-\endcode
-*/
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn QPoint operator*( const QPoint &p, double c )
+  Multiplies both of \e p's components by \e c and returns the
+  result.
+ ----------------------------------------------------------------------------*/
 
-QPoint &QPoint::operator*=( int c )
-{
-    xp*=(QCOORD)c; yp*=(QCOORD)c; return *this;
-}
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn QPoint operator*( double c, const QPoint &p )
+  Multiplies both of \e p's components by \e c and returns the
+  result.
+ ----------------------------------------------------------------------------*/
 
-/*!
-Multiplies both x and y with \e c, and return a reference to this point.
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  \fn QPoint operator-( const QPoint &p )
+  Returns \e p where x and y have opposite signs.
+ ----------------------------------------------------------------------------*/
 
-\code
-  QPoint p( -1, 4 );
-  p *= 2.5;			\/ p becomes (-3,10)
-\endcode
 
-Notice that the result is truncated.
-*/
+/*----------------------------------------------------------------------------
+  Divides both x and y by \e c, and return a reference to this point.
 
-QPoint &QPoint::operator*=( double c )
-{
-    xp=(QCOORD)(c*xp); yp=(QCOORD)(c*yp); return *this;
-}
+  The division will not be performed if \e c is 0.
 
-/*!
-Divides both x and y by \e c, and return a reference to this point.
-
-The division will not be performed if \e c is 0.
-
-\code
-  QPoint p( -2, 8 );
-  p /= 2;			\/ p becomes (-1,4)
-\endcode
-*/
+  \code
+    QPoint p( -2, 8 );
+    p /= 2;			// p becomes (-1,4)
+  \endcode
+ ----------------------------------------------------------------------------*/
 
 QPoint &QPoint::operator/=( int c )
 {
-    if ( c == 0 ) {
+    if ( c == 0 )
 #if defined(CHECK_MATH)
 	warning( "QPoint: Attempt to divide point by zero" );
 #endif
-	return *this;
-    }
-    xp/=(QCOORD)c; yp/=(QCOORD)c; return *this;
+    xp/=(QCOORD)c; yp/=(QCOORD)c;
+    return *this;
 }
 
-/*!
-Divides both x and y by \e c, and return a reference to this point.
+/*----------------------------------------------------------------------------
+  Divides both x and y by \e c, and return a reference to this point.
 
-The division will not be performed if \e c is 0.
+  The division will not be performed if \e c is 0.
 
-\code
-  QPoint p( -3, 10 );
-  p /= 2.5;			\/ p becomes (-1,4)
-\endcode
+  \code
+    QPoint p( -3, 10 );
+    p /= 2.5;			// p becomes (-1,4)
+  \endcode
 
-Notice that the result is truncated.
-*/
+  Notice that the result is truncated.
+ ----------------------------------------------------------------------------*/
 
 QPoint &QPoint::operator/=( double c )
 {
-    if ( c == 0.0 ) {
+    if ( c == 0.0 )
 #if defined(CHECK_MATH)
 	warning( "QPoint: Attempt to divide point by zero" );
 #endif
-	return *this;
-    }
-    xp=(QCOORD)(xp/c); yp=(QCOORD)(yp/c); return *this;
+    xp=(QCOORD)(xp/c); yp=(QCOORD)(yp/c);
+    return *this;
 }
 
-/*!
-\relates QPoint
-Returns TRUE if \e p1 and \e p2 are equal, or FALSE if they are different.
-*/
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  Divides both of \e p's components by \e c and returns the result.
 
-bool operator==( const QPoint & p1, const QPoint & p2 )
-{
-    return p1.xp == p2.xp && p1.yp == p2.yp;
-}
-
-/*!
-\relates QPoint
-Returns TRUE if \e p1 and \e p2 are different, or FALSE if they are equal.
-*/
-
-bool operator!=( const QPoint & p1, const QPoint & p2 )
-{
-    return p1.xp != p2.xp || p1.yp != p2.yp;
-}
-
-/*!
-\relates QPoint
-Returns the sum of \e p1 and \e p2; each component is added separately.
-*/
-
-QPoint operator+( const QPoint & p1, const QPoint & p2 )
-{
-    return QPoint( p1.xp+p2.xp, p1.yp+p2.yp );
-}
-
-/*!
-\relates QPoint
-Returns \e p2 subtracted from \e p1; each component is
-subtracted separately.
-*/
-
-QPoint operator-( const QPoint & p1, const QPoint & p2 )
-{
-    return QPoint( p1.xp-p2.xp, p1.yp-p2.yp );
-}
-
-/*!
-\relates QPoint
-Multiplies both of \e p's components by \e c and returns the result.
-*/
-
-QPoint operator*( const QPoint &p, int c )
-{
-    return QPoint( p.xp*c, p.yp*c );
-}
-
-/*!
-\relates QPoint
-Multiplies both of \e p's components by \e c and returns the result.
-*/
-
-QPoint operator*( int c, const QPoint &p )
-{
-    return QPoint( p.xp*c, p.yp*c );
-}
-
-/*!
-\relates QPoint
-Multiplies both of \e p's components by \e c and returns the
-result.
-*/
-
-QPoint operator*( const QPoint &p, double c )
-{
-    return QPoint( (QCOORD)(p.xp*c), (QCOORD)(p.yp*c) );
-}
-
-/*!
-\relates QPoint
-Multiplies both of \e p's components by \e c and returns the
-result.
-*/
-
-QPoint operator*( double c, const QPoint &p )
-{
-    return QPoint( (QCOORD)(p.xp*c), (QCOORD)(p.yp*c) );
-}
-
-/*!
-\relates QPoint
-Divides both of \e p's components by \e c and returns the result.
-
-This function returns \e p if \e c is 0.
-*/
+  This function returns \e p if \e c is 0.
+ ----------------------------------------------------------------------------*/
 
 QPoint operator/( const QPoint &p, int c )
 {
-    if ( c == 0 ) {
+    if ( c == 0 )
 #if defined(CHECK_MATH)
 	warning( "QPoint: Attempt to divide point by zero" );
 #endif
-	return p;
-    }
     return QPoint( p.xp/c, p.yp/c );
 }
 
-/*!
-\relates QPoint
-Divides both of \e p's components by \e c and returns the result.
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  Divides both of \e p's components by \e c and returns the result.
 
-This function returns \e p if \e c is 0.
+  This function returns \e p if \e c is 0.
 
-Notice that the result is truncated.
-*/
+  Notice that the result is truncated.
+ ----------------------------------------------------------------------------*/
 
 QPoint operator/( const QPoint &p, double c )
 {
-    if ( c == 0.0 ) {
+    if ( c == 0.0 )
 #if defined(CHECK_MATH)
 	warning( "QPoint: Attempt to divide point by zero" );
 #endif
-	return p;
-    }
     return QPoint( (QCOORD)(p.xp/c), (QCOORD)(p.yp/c) );
 }
 
-/*!
-\relates QPoint
-Returns \e p where x and y have opposite signs.
-*/
 
-QPoint operator-( const QPoint &p )
-{
-    return QPoint( -p.xp, -p.yp );
-}
+/*****************************************************************************
+  QPoint stream functions
+ *****************************************************************************/
 
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  Writes a QPoint to the stream and returns a reference to the stream.
 
-// --------------------------------------------------------------------------
-// QPoint stream functions
-//
-
-/*!
-\relates QPoint
-Writes a QPoint to the stream and returns a reference to the stream.
-
-First \e p.x() and then p.y() are written, both as signed 16-bit
-numbers. */
+  First \e p.x() and then p.y() are written, both as signed 16-bit
+  numbers.
+ ----------------------------------------------------------------------------*/
 
 QDataStream &operator<<( QDataStream &s, const QPoint &p )
 {
     return s << (INT16)p.x() << (INT16)p.y();
 }
 
-/*!
-\relates QPoint
-Reads a QPoint from the stream and returns a reference to the stream.
-*/
+/*----------------------------------------------------------------------------
+  \relates QPoint
+  Reads a QPoint from the stream and returns a reference to the stream.
+ ----------------------------------------------------------------------------*/
 
 QDataStream &operator>>( QDataStream &s, QPoint &p )
 {
--- src/kernel/qpoint.h
+++ src/kernel/qpoint.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpoint.h#9 $
+** $Id: //depot/qt/main/src/kernel/qpoint.h#10 $
 **
 ** Definition of QPoint class
 **
@@ -20,24 +20,24 @@ class QPoint					// point class
 {
 public:
     QPoint()	{}				// undefined init values
-    QPoint( int xpos, int ypos );		// set x=xpos, y=ypos
+    QPoint( int xpos, int ypos );
 
-    bool   isNull()	const	{ return xp==0 && yp==0; }
+    bool   isNull()	const;
 
-    int	   x()		const	{ return xp; }	// get x
-    int	   y()		const	{ return yp; }	// get y
-    void   setX( int x )	{ xp=(QCOORD)x;}// set x
-    void   setY( int y )	{ yp=(QCOORD)y;}// set y
+    int	   x()		const;
+    int	   y()		const;
+    void   setX( int x );
+    void   setY( int y );
 
-    QCOORD &rx()		{ return xp; }	// get reference to x
-    QCOORD &ry()		{ return yp; }	// get reference to y
+    QCOORD &rx();
+    QCOORD &ry();
 
-    QPoint &operator+=( const QPoint &p );	// add point
-    QPoint &operator-=( const QPoint &p );	// subtract point
-    QPoint &operator*=( int c );		// multiply with scalar
-    QPoint &operator*=( double c );		// multiply with scalar double
-    QPoint &operator/=( int c );		// divide by scalar
-    QPoint &operator/=( double c );		// divide by scalar double
+    QPoint &operator+=( const QPoint &p );
+    QPoint &operator-=( const QPoint &p );
+    QPoint &operator*=( int c );
+    QPoint &operator*=( double c );
+    QPoint &operator/=( int c );
+    QPoint &operator/=( double c );
 
     friend bool	  operator==( const QPoint &, const QPoint & );
     friend bool	  operator!=( const QPoint &, const QPoint & );
@@ -53,11 +53,11 @@ public:
 
 private:
 #if defined(_OS_MAC_)
-    QCOORD yp;					// y position
-    QCOORD xp;					// x position
+    QCOORD yp;
+    QCOORD xp;
 #else
-    QCOORD xp;					// x position
-    QCOORD yp;					// y position
+    QCOORD xp;
+    QCOORD yp;
 #endif
 };
 
@@ -70,8 +70,6 @@ QDataStream &operator<<( QDataStream &, const QPoint & );
 QDataStream &operator>>( QDataStream &, QPoint & );
 
 
-#if !(defined(QPOINT_C) || defined(DEBUG))
-
 // --------------------------------------------------------------------------
 // QPoint member functions
 //
@@ -79,6 +77,41 @@ QDataStream &operator>>( QDataStream &, QPoint & );
 inline QPoint::QPoint( int xpos, int ypos )
 {
     xp=(QCOORD)xpos; yp=(QCOORD)ypos;
+}
+
+inline bool QPoint::isNull() const
+{
+    return xp == 0 && yp == 0;
+}
+
+inline int QPoint::x() const
+{
+    return xp;
+}
+
+inline int QPoint::y() const
+{
+    return yp;
+}
+
+inline void QPoint::setX( int x )
+{
+    xp = (QCOORD)x;
+}
+
+inline void QPoint::setY( int y )
+{
+    yp = (QCOORD)y;
+}
+
+inline QCOORD &QPoint::rx()
+{
+    return xp;
+}
+
+inline QCOORD &QPoint::ry()
+{
+    return yp;
 }
 
 inline QPoint &QPoint::operator+=( const QPoint &p )
@@ -101,32 +134,22 @@ inline QPoint &QPoint::operator*=( double c )
     xp=(QCOORD)(xp*c); yp=(QCOORD)(yp*c); return *this;
 }
 
-inline QPoint &QPoint::operator/=( int c )
-{
-    xp/=(QCOORD)c; yp/=(QCOORD)c; return *this;
-}
-
-inline QPoint &QPoint::operator/=( double c )
-{
-    xp=(QCOORD)(xp/c); yp=(QCOORD)(yp/c); return *this;
-}
-
-inline bool operator==( const QPoint & p1, const QPoint & p2 )
+inline bool operator==( const QPoint &p1, const QPoint &p2 )
 {
     return p1.xp == p2.xp && p1.yp == p2.yp;
 }
 
-inline bool operator!=( const QPoint & p1, const QPoint & p2 )
+inline bool operator!=( const QPoint &p1, const QPoint &p2 )
 {
     return p1.xp != p2.xp || p1.yp != p2.yp;
 }
 
-inline QPoint operator+( const QPoint & p1, const QPoint & p2 )
+inline QPoint operator+( const QPoint &p1, const QPoint &p2 )
 {
     return QPoint( p1.xp+p2.xp, p1.yp+p2.yp );
 }
 
-inline QPoint operator-( const QPoint & p1, const QPoint & p2 )
+inline QPoint operator-( const QPoint &p1, const QPoint &p2 )
 {
     return QPoint( p1.xp-p2.xp, p1.yp-p2.yp );
 }
@@ -151,6 +174,29 @@ inline QPoint operator*( double c, const QPoint &p )
     return QPoint( (QCOORD)(p.xp*c), (QCOORD)(p.yp*c) );
 }
 
+inline QPoint operator-( const QPoint &p )
+{
+    return QPoint( -p.xp, -p.yp );
+}
+
+#if !(defined(QPOINT_C) || defined(DEBUG))
+
+//
+// These "dangerous" QPoint functions are inline if DEBUG is not defined.
+// The debug implementation in qpoint.cpp checks c and gives a warning
+// before dividing by zero.
+//
+
+inline QPoint &QPoint::operator/=( int c )
+{
+    xp/=(QCOORD)c; yp/=(QCOORD)c; return *this;
+}
+
+inline QPoint &QPoint::operator/=( double c )
+{
+    xp=(QCOORD)(xp/c); yp=(QCOORD)(yp/c); return *this;
+}
+
 inline QPoint operator/( const QPoint &p, int c )
 {
     return QPoint( p.xp/c, p.yp/c );
@@ -161,12 +207,8 @@ inline QPoint operator/( const QPoint &p, double c )
     return QPoint( (QCOORD)(p.xp/c), (QCOORD)(p.yp/c) );
 }
 
-inline QPoint operator-( const QPoint &p )
-{
-    return QPoint( -p.xp, -p.yp );
-}
 
-#endif // inline functions
+#endif // non-debug inline functions
 
 
 #endif // QPOINT_H
