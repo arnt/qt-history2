a0d3293530f5f29ed6893da7f359c2f1f4295b52
Haavard Nord <hanord@trolltech.com>
1996-06-18 12:51:55 +0100

    Win32 based directory search
    [imported from CVS by cvs2p4 at 1999/10/31 17:51:28]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 3500]

--- src/tools/qdir.cpp
+++ src/tools/qdir.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qdir.cpp#18 $
+** $Id: //depot/qt/main/src/tools/qdir.cpp#19 $
 **
 ** Implementation of QDir class
 **
@@ -15,8 +15,17 @@
 #include "qfiledef.h"
 #include "qregexp.h"
 #include <stdlib.h>
+#if defined(_OS_WIN32_)
+#if defined(_CC_BOOL_DEF_)
+#undef	bool
+#include <windows.h>
+#define bool int
+#else
+#include <windows.h>
+#endif
+#endif
 
-RCSTAG("$Id: //depot/qt/main/src/tools/qdir.cpp#18 $")
+RCSTAG("$Id: //depot/qt/main/src/tools/qdir.cpp#19 $")
 
 
 #if !defined(PATH_MAX)
@@ -1057,7 +1066,9 @@ QString QDir::currentDirPath()
 	    }
 	}
     } else {
+#if defined(DEBUG)
 	warning( "QDir::currentDirPath: stat(\".\") failed" );
+#endif
 	currentName = 0;
 	forcecwd    = TRUE;
     }
@@ -1291,15 +1302,49 @@ bool QDir::readDirEntries( const QString &nameFilter,
 	CHECK_PTR( dList );
     }
 
-#if defined(_OS_MSDOS_) || defined(_OS_WIN32_)
+#if defined(_OS_WIN32_) || defined(_OS_MSDOS_)
 
     QRegExp   wc( nameFilter, FALSE, TRUE );	// wild card, case insensitive
     bool      first = TRUE;
     QString   p = dPath.copy();
     int	      plen = p.length();
+#if defined(_OS_WIN32_)
+    HANDLE    ff;
+    WIN32_FIND_DATA finfo;
+#else
     long      ff;
     _finddata_t finfo;
+#endif
     QFileInfo fi;
+
+#undef	IS_SUBDIR
+#undef	IS_RDONLY
+#undef	IS_ARCH
+#undef	IS_HIDDEN
+#undef	IS_SYSTEM
+#undef	FF_GETFIRST
+#undef	FF_GETNEXT
+#undef	FF_ERROR
+
+#if defined(_OS_WIN32_)
+#define IS_SUBDIR   FILE_ATTRIBUTE_DIRECTORY
+#define IS_RDONLY   FILE_ATTRIBUTE_READONLY
+#define IS_ARCH	    FILE_ATTRIBUTE_ARCHIVE
+#define IS_HIDDEN   FILE_ATTRIBUTE_HIDDEN
+#define IS_SYSTEM   FILE_ATTRIBUTE_SYSTEM
+#define FF_GETFIRST FindFirstFile
+#define FF_GETNEXT  FindNextFile
+#define FF_ERROR    INVALID_HANDLE_VALUE
+#else
+#define IS_SUBDIR   _A_SUBDIR
+#define IS_RDONLY   _A_RDONLY
+#define IS_ARCH	    _A_ARCH
+#define IS_HIDDEN   _A_HIDDEN
+#define IS_SYSTEM   _A_SYSTEM
+#define FF_GETFIRST _findfirst
+#define FF_GETNEXT  _findnext
+#define FF_ERROR    -1
+#endif
 
     if ( plen == 0 ) {
 #if defined(CHECK_NULL)
@@ -1311,9 +1356,9 @@ bool QDir::readDirEntries( const QString &nameFilter,
 	p += '/';
     p += "*.*";
 
-    ff = _findfirst( p.data(), &finfo );
-    if ( ff == -1 ) {
-#if defined(CHECK_NULL)
+    ff = FF_GETFIRST( p.data(), &finfo );
+    if ( ff == FF_ERROR ) {
+#if defined(DEBUG)
 	warning( "QDir::readDirEntries: Cannot read the directory: %s",
 		 (const char *)dPath );
 #endif
@@ -1324,23 +1369,38 @@ bool QDir::readDirEntries( const QString &nameFilter,
 	if ( first )
 	    first = FALSE;
 	else {
-	    if ( _findnext(ff,&finfo) == -1 )
+#if defined(_OS_WIN32_)
+	    if ( !FF_GETNEXT(ff,&finfo) )
 		break;
+#else
+	    if ( FF_GETNEXT(ff,&finfo) == -1 )
+		break;
+#endif
 	}
-	bool isDir	= (finfo.attrib & _A_SUBDIR) != 0;
+#if defined(_OS_WIN32_)
+	int  attrib = finfo.dwFileAttributes;
+#else
+	int  attrib = finfo.attrib;
+#endif
+	bool isDir	= (attrib & IS_SUBDIR) != 0;
 	bool isFile	= !isDir;
 	bool isSymLink	= FALSE;
 	bool isReadable = TRUE;
-	bool isWritable = (finfo.attrib & _A_RDONLY) == 0;
+	bool isWritable = (attrib & IS_RDONLY) == 0;
 	bool isExecable = FALSE;
-	bool isModified = (finfo.attrib & _A_ARCH)   != 0;
-	bool isHidden	= (finfo.attrib & _A_HIDDEN) != 0;
-	bool isSystem	= (finfo.attrib & _A_SYSTEM) != 0;
+	bool isModified = (attrib & IS_ARCH)   != 0;
+	bool isHidden	= (attrib & IS_HIDDEN) != 0;
+	bool isSystem	= (attrib & IS_SYSTEM) != 0;
 
-	if ( wc.match(finfo.name) == -1 && !(allDirs && isDir) )
+#if defined(_OS_WIN32_)
+	const char *fname = finfo.cFileName;
+#else
+	const char *fname = finfo.name;
+#endif
+	if ( wc.match(fname) == -1 && !(allDirs && isDir) )
 	    continue;
 
-	QString name = finfo.name;
+	QString name = fname;
 	if ( doExecable ) {
 	    QString ext = name.right(4).lower();
 	    if ( ext == ".exe" || ext == ".com" || ext == ".bat" ||
@@ -1369,7 +1429,21 @@ bool QDir::readDirEntries( const QString &nameFilter,
 		dirInSort( fList, fiList, name, fi, sortSpec );
 	}
     }
+#if defined(_OS_WIN32_)
+    FindClose( ff );
+#else
     _findclose( ff );
+#endif
+
+#undef	IS_SUBDIR
+#undef	IS_RDONLY
+#undef	IS_ARCH
+#undef	IS_HIDDEN
+#undef	IS_SYSTEM
+#undef	FF_GETFIRST
+#undef	FF_GETNEXT
+#undef	FF_ERROR
+
 
 #elif defined(UNIX)
 
