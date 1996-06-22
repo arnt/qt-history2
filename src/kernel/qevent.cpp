c535da628a456c3092151d501ae71b8c62a0eac7
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1996-06-22 19:35:25 +0100

    syntax error fixed
    [imported from CVS by cvs2p4 at 1999/10/31 17:51:43]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 3515]

--- src/kernel/qevent.cpp
+++ src/kernel/qevent.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qevent.cpp#31 $
+** $Id: //depot/qt/main/src/kernel/qevent.cpp#32 $
 **
 ** Implementation of event classes
 **
@@ -12,7 +12,7 @@
 
 #include "qevent.h"
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qevent.cpp#31 $")
+RCSTAG("$Id: //depot/qt/main/src/kernel/qevent.cpp#32 $")
 
 
 void qRemovePostedEvent( QEvent * );		// defined in qapp_xxx.cpp
@@ -129,7 +129,7 @@ void QEvent::peErrMsg()				// posted event error message
   \fn QEvent::~QEvent()
   Destroys the event.  Reports an error if the event has been
   \link QApplication::postEvent() posted. \endlink
- ----------------------------------------------------------------------------. */
+  */
 
 /*----------------------------------------------------------------------------
   \fn int QEvent::type() const
