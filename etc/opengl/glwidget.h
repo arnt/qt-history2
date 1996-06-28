b67994303da3f079eb0f3f3f1f89524254c7ead9
Haavard Nord <hanord@trolltech.com>
1996-06-28 07:04:01 +0100

    Added (c)
    [imported from CVS by cvs2p4 at 1999/10/31 17:52:23]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 3555]

--- etc/opengl/glwidget.h
+++ etc/opengl/glwidget.h
@@ -1,6 +1,18 @@
 /****************************************************************************
+** $Id: //depot/qt/main/etc/opengl/glwidget.h#2 $
 **
 ** Definition of GLWidget class
+**
+** Author  : Haavard Nord (hanord@troll.no)
+** Created : 960620
+**
+** Copyright (C) 1996 by Troll Tech AS.
+**
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted, provided
+** that the above copyright notice appear in all copies.
+** No representations are made about the suitability of this software for any
+** purpose. It is provided "as is" without express or implied warranty.
 **
 *****************************************************************************/
 
