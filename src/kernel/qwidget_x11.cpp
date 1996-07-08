e8dc6253ba74d0f2311547ff0321276430b42997
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1996-07-08 15:50:54 +0100

    baktabbing er tabbing det og, og krever mer enn bare return TRUE;
    [imported from CVS by cvs2p4 at 1999/10/31 17:54:18]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 3666]

--- src/kernel/qwidget_x11.cpp
+++ src/kernel/qwidget_x11.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#136 $
+** $Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#137 $
 **
 ** Implementation of QWidget and QWindow classes for X11
 **
@@ -22,7 +22,7 @@
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#136 $")
+RCSTAG("$Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#137 $")
 
 
 void qt_enter_modal( QWidget * );		// defined in qapp_x11.cpp
@@ -734,6 +734,24 @@ bool QWidget::focusNextChild()
 
 bool QWidget::focusPrevChild()
 {
+    QWidget *p = parentWidget();
+    if ( !p )
+	return FALSE;
+    QObjectList *c = (QObjectList *)p->children();
+    if ( c->findRef(this) < 0 )			// why, not found?
+	return FALSE;
+    while ( TRUE ) {
+	c->prev();
+	if ( !c->current() )
+	    c->last();
+	if ( c->current()->isWidgetType() ) {
+	    QWidget *w = (QWidget*)c->current();
+	    if ( w->acceptFocus() ) {
+		w->setFocus();
+		return TRUE;
+	    }
+	}
+    }
     return TRUE;
 }
 
