e6128da4f5a2c705ad7ba7af5110204515d061de
Paul Olav Tvete <paul@trolltech.com>
1997-04-02 13:20:44 +0100

    mouseRelease fix
    [imported from CVS by cvs2p4 at 1999/10/31 18:23:28]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 4985]

--- src/widgets/qslider.cpp
+++ src/widgets/qslider.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/widgets/qslider.cpp#28 $
+** $Id: //depot/qt/main/src/widgets/qslider.cpp#29 $
 **
 ** Implementation of QSlider class
 **
@@ -15,7 +15,7 @@
 #include "qtimer.h"
 #include "qkeycode.h"
 
-RCSTAG("$Id: //depot/qt/main/src/widgets/qslider.cpp#28 $");
+RCSTAG("$Id: //depot/qt/main/src/widgets/qslider.cpp#29 $");
 
 
 static const int motifBorder = 2;
@@ -684,8 +684,8 @@ void QSlider::mouseMoveEvent( QMouseEvent *e )
 
 void QSlider::mouseReleaseEvent( QMouseEvent *e )
 {
-    if ( !(e->state() & LeftButton) )
-	return;					// left mouse button is up
+    if ( !(e->button() & LeftButton) )
+	return;
     resetState();
 }
 
