5cee5733a210541137deb0f2c3651ad3e49acac1
Warwick Allison <warwick@trolltech.com>
1997-07-24 14:50:34 +0100

    docs
    [imported from CVS by cvs2p4 at 1999/10/31 18:35:02]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 5603]

--- src/dialogs/qmessagebox.cpp
+++ src/dialogs/qmessagebox.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/dialogs/qmessagebox.cpp#39 $
+** $Id: //depot/qt/main/src/dialogs/qmessagebox.cpp#40 $
 **
 ** Implementation of QMessageBox class
 **
@@ -16,7 +16,7 @@
 #include "qkeycode.h"
 #include "qapp.h"
 
-RCSTAG("$Id: //depot/qt/main/src/dialogs/qmessagebox.cpp#39 $");
+RCSTAG("$Id: //depot/qt/main/src/dialogs/qmessagebox.cpp#40 $");
 
 // Message box icons, from page 210 of the Windows style guide.
 
@@ -152,8 +152,6 @@ static const unsigned char critical_gif_data[] = {
 	    break;
     }
   \endcode
-
-  \bugs Icons in the message box does not work yet.
 */
 
 
