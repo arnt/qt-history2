c6a89ec770a3a9cfae46b4b38b380b57f5dac3d4
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1997-07-29 16:04:13 +0100

    nutha tweak
    [imported from CVS by cvs2p4 at 1999/10/31 18:35:39]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 5639]

--- src/kernel/qwidget.cpp
+++ src/kernel/qwidget.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qwidget.cpp#194 $
+** $Id: //depot/qt/main/src/kernel/qwidget.cpp#195 $
 **
 ** Implementation of QWidget class
 **
@@ -19,7 +19,7 @@
 #include "qkeycode.h"
 #include "qapp.h"
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qwidget.cpp#194 $");
+RCSTAG("$Id: //depot/qt/main/src/kernel/qwidget.cpp#195 $");
 
 
 /*!
@@ -1776,7 +1776,7 @@ void QWidget::setFocus()
 	return;
 
     QFocusData * f = focusData(TRUE);
-    if ( f->it.current() == this )
+    if ( f->it.current() == this && qApp->focusWidget() == this )
 	return;
     else if ( f->it.current() )
 	f->it.current()->clearFocus();
