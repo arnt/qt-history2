90be4160a75d48ddb59e660fccdf41d6cd8049d4
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1997-09-10 13:58:40 +0100

    blush
    [imported from CVS by cvs2p4 at 1999/10/31 18:44:03]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 5984]

--- src/widgets/qcombobox.cpp
+++ src/widgets/qcombobox.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-** $Id: //depot/qt/main/src/widgets/qcombobox.cpp#106 $
+** $Id: //depot/qt/main/src/widgets/qcombobox.cpp#107 $
 **
 ** Implementation of QComboBox widget class
 **
@@ -23,7 +23,7 @@
 #include "qlined.h"
 #include <limits.h>
 
-RCSTAG("$Id: //depot/qt/main/src/widgets/qcombobox.cpp#106 $");
+RCSTAG("$Id: //depot/qt/main/src/widgets/qcombobox.cpp#107 $");
 
 
 /*!
@@ -301,7 +301,7 @@ QComboBox::QComboBox( bool rw, QWidget *parent, const char *name )
 	d->ed->installEventFilter( this );
 	setFocusProxy( d->ed );
 
-	setBackgroundMode( NoBackgroun );
+	setBackgroundMode( NoBackground );
 
 	connect( d->ed, SIGNAL(returnPressed()), SLOT(returnPressed()) );
     } else {
