754b9c96d4ac3bee2ef6efee448c9d7d2d13e7fc
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1997-09-10 13:27:08 +0100

    make qcombo compile with the latest qwidget.h
    [imported from CVS by cvs2p4 at 1999/10/31 18:43:59]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 5981]

--- src/widgets/qcombobox.cpp
+++ src/widgets/qcombobox.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-** $Id: //depot/qt/main/src/widgets/qcombobox.cpp#105 $
+** $Id: //depot/qt/main/src/widgets/qcombobox.cpp#106 $
 **
 ** Implementation of QComboBox widget class
 **
@@ -23,7 +23,7 @@
 #include "qlined.h"
 #include <limits.h>
 
-RCSTAG("$Id: //depot/qt/main/src/widgets/qcombobox.cpp#105 $");
+RCSTAG("$Id: //depot/qt/main/src/widgets/qcombobox.cpp#106 $");
 
 
 /*!
@@ -301,7 +301,7 @@ QComboBox::QComboBox( bool rw, QWidget *parent, const char *name )
 	d->ed->installEventFilter( this );
 	setFocusProxy( d->ed );
 
-	setBackgroundEmpty();
+	setBackgroundMode( NoBackgroun );
 
 	connect( d->ed, SIGNAL(returnPressed()), SLOT(returnPressed()) );
     } else {
