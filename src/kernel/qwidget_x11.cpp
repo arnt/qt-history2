6a3a43fa3c094b3509e941083a60505575b3a692
Warwick Allison <warwick@trolltech.com>
1997-09-17 14:25:17 +0100

    Fix update()ing when bg pixmap changes.
    [imported from CVS by cvs2p4 at 1999/10/31 18:45:39]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6075]

--- src/kernel/qwidget_x11.cpp
+++ src/kernel/qwidget_x11.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#204 $
+** $Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#205 $
 **
 ** Implementation of QWidget and QWindow classes for X11
 **
@@ -22,7 +22,7 @@
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#204 $");
+RCSTAG("$Id: //depot/qt/main/src/kernel/qwidget_x11.cpp#205 $");
 
 
 void qt_enter_modal( QWidget * );		// defined in qapp_x11.cpp
@@ -557,7 +557,7 @@ void QWidget::setBackgroundPixmap( const QPixmap &pixmap )
 	if ( testWFlags(WType_Desktop) )	// save rootinfo later
 	    qt_updated_rootinfo();
     }
-    if ( allow_null_pixmaps ) {
+    if ( !allow_null_pixmaps ) {
 	setBackgroundModeDirect( FixedPixmap );
 	backgroundPixmapChange( old );
     }
