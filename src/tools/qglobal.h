4d978e1ba2e527f9a52244d4b3603c641f090774
Paul Olav Tvete <paul@trolltech.com>
1997-09-22 15:06:12 +0100

    New version number
    [imported from CVS by cvs2p4 at 1999/10/31 18:46:20]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6102]

--- src/tools/qglobal.h
+++ src/tools/qglobal.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qglobal.h#94 $
+** $Id: //depot/qt/main/src/tools/qglobal.h#95 $
 **
 ** Global type declarations and definitions
 **
@@ -14,7 +14,7 @@
 
 
 #define QT_VERSION	130
-#define QT_VERSION_STR	"1.3beta4"
+#define QT_VERSION_STR	"1.3beta5"
 
 
 //
