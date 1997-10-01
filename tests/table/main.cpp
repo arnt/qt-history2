5a6ae668f424b2bba7579bc7f84cf31a3b8f584d
Paul Olav Tvete <paul@trolltech.com>
1997-10-01 16:03:32 +0100

    New table example. Will hopefully be a part of Qt 1.40
    [imported from CVS by cvs2p4 at 1999/10/31 18:50:03]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6337]

---
+++ tests/table/main.cpp
@@ -0,0 +1,63 @@
+//
+// Qt example: Table
+//
+// A simple, spreadsheet-like widget, made by inheriting  QTableView.
+// 
+// File: main.cpp
+//
+// The main() function, showing the use of the Table widget
+// 
+
+#include "table.h"
+#include <qpushbt.h>
+#include <qapp.h>
+#include <qlayout.h>
+#include <qheader.h>
+
+/*
+  Constants
+*/
+
+const int numRows = 20;				// Tablesize: number of rows
+const int numCols = 20;				// Tablesize: number of columns
+
+/*
+  The program starts here. 
+*/
+
+int main( int argc, char **argv )
+{
+    QApplication a(argc,argv);			
+
+    QWidget w;
+
+    QVBoxLayout l(&w);
+
+    QHeader h( numCols, &w );
+    h.setFixedHeight( h.sizeHint().height() );
+
+    l.addWidget( &h );
+    Table v( numRows, numCols, &w );
+    v.setHeader( &h );
+
+    l.addWidget( &v );
+
+    //    QObject::connect( &b, SIGNAL(clicked()), &v, SLOT(increaseWidth()) );
+    /*
+      Fill the table with default content: a coordinate string.
+    */
+    QString s ;
+    for( int i = 0; i < numRows; i++ ) {
+	for( int j = 0; j < numCols; j++ ) {
+	    s.setNum(j);
+	    s += ' ';
+	    s += 'A' + ( i % 26 );		// Wrap if necessary
+	    v.setCellContent( i, j, s );
+	}
+    }
+
+    a.setMainWidget( &w );
+    w.show();
+
+    return a.exec();
+}
---
+++ tests/table/table.h
@@ -0,0 +1,50 @@
+//
+// Qt example: Table
+//
+// A simple, spreadsheet-like widget, made by inheriting  QTableView.
+// 
+// File: table.h
+//
+// Definition of the Table widget.
+//
+
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <qtablevw.h>
+
+class QHeader;
+
+class Table : public QTableView
+{
+    Q_OBJECT
+public:
+    Table( int numRows, int numCols, QWidget* parent=0, const char* name=0 );
+    ~Table();
+    
+    const char* cellContent( int row, int col ) const;
+    void setCellContent( int row, int col, const char* );
+
+    void setHeader( QHeader * );
+
+public slots:
+    void update();
+
+protected:
+    void paintCell( QPainter*, int row, int col );
+    void mousePressEvent( QMouseEvent* );
+    void keyPressEvent( QKeyEvent* );
+    void focusInEvent( QFocusEvent* );
+    void focusOutEvent( QFocusEvent* );
+
+    int cellWidth( int);
+    
+private:
+    int indexOf( int row, int col ) const;
+    QString* contents;
+    QHeader *head;
+    int curRow;
+    int curCol;
+};
+
+#endif // TABLE_H
