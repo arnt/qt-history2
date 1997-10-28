ae1d2d49db19441f2c964af774d0649b5236f3b7
Haavard Nord <hanord@trolltech.com>
1997-10-28 16:13:04 +0100

    Added cast to make example code work
    [imported from CVS by cvs2p4 at 1999/10/31 18:53:07]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6548]

--- src/kernel/qobject.cpp
+++ src/kernel/qobject.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qobject.cpp#139 $
+** $Id: //depot/qt/main/src/kernel/qobject.cpp#140 $
 **
 ** Implementation of QObject class
 **
@@ -17,7 +17,7 @@
 #include "qapp.h"
 #endif
 
-RCSTAG("$Id: //depot/qt/main/src/kernel/qobject.cpp#139 $");
+RCSTAG("$Id: //depot/qt/main/src/kernel/qobject.cpp#140 $");
 
 
 /*!
@@ -791,7 +791,7 @@ static void objSearch( QObjectList *result,
     QObject * obj;
     while ( (obj=it.current()) != 0 ) {	// for each found object...
 	++it;
-	obj->setFont( newFont );
+	((QWidget *)obj)->setFont( newFont );
     }
     delete list;			// delete the list, not the objects
   \endcode
