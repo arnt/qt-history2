7e22d92e10bbbf26b196de51e43fab870258db3d
Paul Olav Tvete <paul@trolltech.com>
1997-11-17 19:04:35 +0100

    fixing bug when setting value in constructor followed by resetting value to zero.
    [imported from CVS by cvs2p4 at 1999/10/31 18:54:32]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6624]

--- src/widgets/qslider.cpp
+++ src/widgets/qslider.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/widgets/qslider.cpp#44 $
+** $Id: //depot/qt/main/src/widgets/qslider.cpp#45 $
 **
 ** Implementation of QSlider class
 **
@@ -15,7 +15,7 @@
 #include "qtimer.h"
 #include "qkeycode.h"
 
-RCSTAG("$Id: //depot/qt/main/src/widgets/qslider.cpp#44 $");
+RCSTAG("$Id: //depot/qt/main/src/widgets/qslider.cpp#45 $");
 
 
 static const int motifBorder = 2;
@@ -97,6 +97,7 @@ QSlider::QSlider( int minValue, int maxValue, int step,
 {
     orient = orientation;
     init();
+    sliderVal = value;
 }
 
 
