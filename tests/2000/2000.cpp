6c91efd1547daeb458ceb76ee465f01c17d368d9
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1998-02-04 18:43:43 +0100

    qt is y2k compliant and this is how we know it.  sort of.
    [imported from CVS by cvs2p4 at 1999/10/31 18:59:18]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6891]

---
+++ tests/2000/2000.cpp
@@ -0,0 +1,64 @@
+#include <qapp.h>
+#include <qdatetm.h>
+
+RCSTAG("$Id: //depot/qt/main/tests/2000/2000.cpp#1 $");
+
+int main( int, char ** )
+{
+    QDate d;
+
+    debug( "new year" );
+    d.setYMD( 1999, 12, 31 );
+    debug( "d %d %d %d/%d-%d",
+	   QDate::currentDate().daysTo( d ),
+	   d.dayOfWeek(), d.day(), d.month(), d.year() );
+    d = d.addDays( 1 );
+    debug( "d %d %d %d/%d-%d",
+	   QDate::currentDate().daysTo( d ),
+	   d.dayOfWeek(), d.day(), d.month(), d.year() );
+
+    int i;
+
+    debug( "feb" );
+    for( i=1; i < 30; i++ ) {
+	d.setYMD( 2000, 2, i );
+	debug( "d %d %d %d/%d-%d",
+	       QDate::currentDate().daysTo( d ),
+	       d.dayOfWeek(), d.day(), d.month(), d.year() );
+    }
+
+    debug( "feb 26 + 5" );
+    d.setYMD( 2000, 2, 26 );
+    for( i=0; i < 6; i++ ) {
+	debug( "d %d %d %d/%d-%d",
+	       QDate::currentDate().daysTo( d ),
+	       d.dayOfWeek(), d.day(), d.month(), d.year() );
+	d = d.addDays( 1 );
+    }
+
+    debug( "2001" );
+    d.setYMD( 2001, 1, 1 );
+    debug( "d %d %d %d/%d-%d",
+	   QDate::currentDate().daysTo( d ),
+	   d.dayOfWeek(), d.day(), d.month(), d.year() );
+
+    debug( "feb 2001 26 + 5" );
+    d.setYMD( 2001, 2, 26 );
+    for( i=0; i < 6; i++ ) {
+	debug( "d %d %d %d/%d-%d",
+	       QDate::currentDate().daysTo( d ),
+	       d.dayOfWeek(), d.day(), d.month(), d.year() );
+	d = d.addDays( 1 );
+    }
+
+    d.setYMD( 2000, 3, 1 );
+    debug( "d %d %d %d/%d-%d",
+	   QDate::currentDate().daysTo( d ),
+	   d.dayOfWeek(), d.day(), d.month(), d.year() );
+    d.setYMD( 2001, 3, 1);
+    debug( "d %d %d %d/%d-%d",
+	   QDate::currentDate().daysTo( d ),
+	   d.dayOfWeek(), d.day(), d.month(), d.year() );
+
+
+}
---
+++ tests/2000/2000.pro
@@ -0,0 +1,5 @@
+TEMPLATE    =	app
+CONFIG      =	qt warn_on release
+SOURCES	    =	2000.cpp
+HEADERS	    =	2000.h
+TARGET      =	2000
