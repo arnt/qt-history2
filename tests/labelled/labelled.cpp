3c14291216b67b91980c12c8e5dd627376df7131
Paul Olav Tvete <paul@trolltech.com>
1998-02-23 15:28:50 +0100

    Use new layout widgets
    [imported from CVS by cvs2p4 at 1999/10/31 19:00:52]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6974]

--- tests/labelled/labelled.cpp
+++ tests/labelled/labelled.cpp
@@ -2,6 +2,10 @@
 #include <qlabelled.h>
 #include <qlabel.h>
 #include <qapp.h>
+#include <qgrid.h>
+#include <qvbox.h>
+#include <qchkbox.h>
+#include <qpushbt.h>
 
 main(int argc, char** argv)
 {
@@ -9,12 +13,23 @@ main(int argc, char** argv)
     QApplication app(argc, argv);
     QApplication::setFont( QFont("Helvetica") );
 
-    QLabelled m("Labelled widget");
-    //m.setAlignment(AlignLeft);
-    QLabel l("Label for the guts",&m);
-    l.setMinimumSize(l.sizeHint());
+    QVBox b;
 
-    m.show();
+    QLabelled m("Labelled widget", &b);
+    //m.setAlignment(AlignLeft);
+    QGrid g( 2, &m);
+    for ( int i = 1; i < 10; i++ ) 
+	new QCheckBox( "Press Me", &g );
+
+
+    QHBox h( &b );
+    new QPushButton( "OK", &h );
+    new QPushButton( "Cancel", &h );
+
+    b.resize(0,0); //############
+
+    b.show();
+
 
     QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
 
