33426b0cbf050a5b35547dff2594e3cdba0f4727
Paul Olav Tvete <paul@trolltech.com>
1998-02-25 10:10:34 +0100

    use buttonrow
    [imported from CVS by cvs2p4 at 1999/10/31 19:01:06]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6990]

--- tests/labelled/labelled.cpp
+++ tests/labelled/labelled.cpp
@@ -6,134 +6,8 @@
 #include <qvbox.h>
 #include <qchkbox.h>
 #include <qpushbt.h>
+#include <qbuttonrow.h>
 
-
-
-#include <qlayout.h>
-#include <qobjcoll.h>
-/*!
-  Constructs an buttonrow widget with parent \a parent and name \a name
- */
-QButtonRow::QButtonRow( QWidget *parent, const char *name )
-    :QWidget( parent, name )
-{
-    lay = new QHBoxLayout( this, parent?0:5, 5, name ); //### border
-    first = TRUE;
-    prefSize = QSize(0,0);
-}
-
-void QButtonRow::recalc()
-{
-    //    const int border = 5;
-    int maxw=0;
-    int miny=0;
-
-    debug( "QButtonRow::recalc" );
-
-    QObjectListIt it(*children());
-    QObject *o;
-    while ( (o=it.current()) ) {
-	++it;
-	if ( o-> isWidgetType() ) {
-	    QWidget *w = (QWidget*) o;
-	    maxw = QMAX( maxw, w->minimumSize().width() );
-	    miny = QMAX( miny, w->minimumSize().height() );
-	}
-    }
-
-    if ( maxw == prefSize.width() && miny == prefSize.height() )
-	return;
-    prefSize = QSize( maxw, miny );
-    it.toFirst();
-    while ( (o=it.current()) ) {
-	++it;
-	if ( o->isWidgetType() ) {
-	    QWidget *w = (QWidget*) o;
-	    w->setMaximumSize( prefSize );
-	    debug( "recalc setting %p to (%d,%d)", w, prefSize.width(), 
-		   prefSize.height() );
-	}
-    }
-}
-
-/*!
-  This function is called when a child changes min/max size.
- */
-void QButtonRow::layoutEvent( QEvent * )
-{
-    recalc();
-}
-
-/*!
-  This function is called when the widget gets a new child or loses an old one.
- */
-void QButtonRow::childEvent( QChildEvent *c )
-{
-    QWidget *w = c->child();
-
-    if ( !c->inserted() ) {
-	///recalc();  //############# removed event comes while child is still there!
-	return;
-    }
-
-    QSize sh = w->sizeHint();
-    w->setAutoMinimumSize( TRUE );
-    w->setMinimumSize( sh );
-
-    if ( first )
-	first = FALSE;
-    else
-	lay->addStretch( 0 );
-    lay->addWidget( w, 1 );
-
-    if ( sh.width() > prefSize.width() || sh.height() > prefSize.height() ) 
-	recalc();
-    else {
-	w->setMaximumSize( prefSize );
-	debug( "childEvent setting %p to (%d,%d)", w, prefSize.width(), prefSize.height() );
-    }
-    if ( isVisible() )
-	lay->activate();
-}
-
-
-void QButtonRow::dump()
-{
-    QObjectListIt it(*children());
-    QObject *o;
-    while ( (o=it.current()) ) {
-	++it;
-	if ( o-> isWidgetType() ) {
-	    QWidget *w = (QWidget*) o;
-	    debug( "%s/%s, max (%d,%d), min (%d,%d)", w->className(), w->name(),
-		   w->maximumSize().width(), w->maximumSize().height(),
-		   w->minimumSize().width(), w->minimumSize().height() );
-	}
-    }
-    QWidget *w = this;
-    debug( "%s/%s, max (%d,%d), min (%d,%d)", w->className(), w->name(),
-	   w->maximumSize().width(), w->maximumSize().height(),
-	   w->minimumSize().width(), w->minimumSize().height() );
-
-}
-
-
-/*!
-  Provides childEvent() while waiting for Qt 2.0.
- */
-bool QButtonRow::event( QEvent *e ) {
-    switch ( e->type() ) {
-    case Event_ChildInserted:
-    case Event_ChildRemoved:
-	childEvent( (QChildEvent*)e );
-	return TRUE;
-    case Event_LayoutHint:
-	layoutEvent( e );
-	return TRUE;
-    default:
-	return QWidget::event( e );
-    }
-}
 
 main(int argc, char** argv)
 {
@@ -158,7 +32,7 @@ main(int argc, char** argv)
     QObject::connect( &p2, SIGNAL(clicked()), qApp, SLOT(quit()));
     //    b.resize(0,0); //############
 
-    QObject::connect( &pp, SIGNAL(clicked()), &h, SLOT(dump()));
+    //QObject::connect( &pp, SIGNAL(clicked()), &h, SLOT(dump()));
 
     b.show();
 
