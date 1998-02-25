209aa113113162171339cb769517c405acc85869
Paul Olav Tvete <paul@trolltech.com>
1998-02-25 08:36:34 +0100

    testing
    [imported from CVS by cvs2p4 at 1999/10/31 19:01:05]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 6989]

--- tests/labelled/labelled.h
+++ tests/labelled/labelled.h
@@ -1,2 +1,29 @@
 #include <qwidget.h>
 
+
+
+#include <qlist.h>
+
+class QBoxLayout;
+
+class QButtonRow : public QWidget
+{
+    Q_OBJECT
+public:
+    QButtonRow( QWidget *parent=0, const char *name=0 );
+    bool event( QEvent * );
+protected:
+    //    QHBox( bool horizontal, QWidget *parent=0, const char *name=0 );
+    virtual void childEvent( QChildEvent * );
+    virtual void layoutEvent( QEvent * );
+private:
+    void recalc();
+    QBoxLayout *lay;
+    QSize prefSize;
+    bool first;
+public slots:
+    void dump();
+};
+
+
+
