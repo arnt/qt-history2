e88c4eda72a95a1ce325ee2d3ae0719ab909db4b
Warwick Allison <warwick@trolltech.com>
1998-02-26 17:05:42 +0100

    Test bounding-rectangle code in QFontMetrics and QPainter.
    [imported from CVS by cvs2p4 at 1999/10/31 19:02:39]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 7045]

---
+++ tests/fontextent/fontextent.cpp
@@ -0,0 +1,94 @@
+#include "fontextent.h"
+#include <qpainter.h>
+#include <qapp.h>
+#include <qkeycode.h>
+#include <qlabel.h>
+
+Main::Main(QWidget* parent, const char* name, int f) :
+    QWidget(parent, name, f)
+{
+    line="type";
+    align=AlignVCenter|AlignLeft;
+    setBackgroundColor(white);
+}
+
+void Main::bang()
+{
+}
+
+void Main::resizeEvent(QResizeEvent*)
+{
+}
+
+void Main::keyPressEvent(QKeyEvent* ke)
+{
+    if ( ke->key() == Key_Backspace ) {
+	if ( line.length() )
+	    line = line.left(line.length()-1);
+    } else if ( ke->key() == Key_Return ) {
+	line += '\n';
+    } else if ( ke->key() == Key_Left ) {
+	align = AlignVCenter|AlignLeft;
+    } else if ( ke->key() == Key_Right ) {
+	align = AlignVCenter|AlignRight;
+    } else if ( ke->key() == Key_Up ||  ke->key() == Key_Down ) {
+	align = AlignCenter;
+    } else if ( ke->key() == Key_Escape ) {
+	QLabel *l = new QLabel(line);
+	l->resize(l->sizeHint());
+	l->setAlignment(align);
+	l->show();
+    } else {
+	line += ke->ascii();
+    }
+    update();
+}
+
+void Main::keyReleaseEvent(QKeyEvent*)
+{
+}
+
+void Main::paintEvent(QPaintEvent* e)
+{
+    QPainter p(this);
+    p.setClipRect(e->rect());
+
+    QFontMetrics fm = fontMetrics();
+    QRect br = fm.boundingRect(line);
+    int w = width()/2;
+    int h = height()/2;
+    int vmarg = h*3/4;
+    int hmarg = w/2;
+    br.moveBy(hmarg,vmarg);
+    QRect r(hmarg,vmarg+h-80,w/2,80);
+    QRect pbr = p.boundingRect(r,align,line);
+
+    p.setPen(yellow);
+    p.drawLine(hmarg,0,hmarg,height());
+    p.drawLine(0,vmarg,width(),vmarg);
+    p.drawLine(0,vmarg+h,width(),vmarg+h);
+
+    p.setPen(black);
+    p.drawRect(br);
+    p.setPen(blue);
+    p.drawText(hmarg,vmarg,line);
+
+    p.setPen(yellow);
+    p.drawRect(r);
+    p.setPen(magenta);
+    p.drawRect(pbr);
+    p.setPen(blue);
+    p.drawText(pbr,align,line);
+}
+
+main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    Main m;
+    m.show();
+
+    QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
+
+    return app.exec();
+}
---
+++ tests/fontextent/fontextent.pro
@@ -0,0 +1,5 @@
+TEMPLATE    =	app
+CONFIG      =	qt warn_on release
+SOURCES	    =	fontextent.cpp
+HEADERS	    =	fontextent.h
+TARGET      =	fontextent
