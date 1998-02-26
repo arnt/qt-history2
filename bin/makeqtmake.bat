5bbac1aa3be4174cfcfe64114f34247505a2db2b
Haavard Nord <hanord@trolltech.com>
1998-02-26 09:37:12 +0100

    Ran pl2bat
    [imported from CVS by cvs2p4 at 1999/10/31 19:02:07]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 7037]

--- bin/makeqtmake.bat
+++ bin/makeqtmake.bat
@@ -15,12 +15,12 @@ goto endofperl
 @rem ';
 #!/usr/bin/perl
 ############################################################################
-# $Id: //depot/qt/main/bin/makeqtmake.bat#1 $
+# $Id: //depot/qt/main/bin/makeqtmake.bat#2 $
 #
 # Makes Qt makefiles and a project file - internal Troll Tech tool.
 #
 # Usage:
-#    makeqtmake [-lib] [-cvs] projectname
+#    makeqtmake [-lib] [-cvs] [-nopro] projectname
 #
 # Copyright (C) 1998 by Troll Tech AS.  All rights reserved.
 #
@@ -29,6 +29,7 @@ goto endofperl
 $libtarget = "";
 $project = "";
 $cvs = 0;
+$nopro = 0;
 
 while ( @ARGV ) {				# parse command line args
     $_ = shift @ARGV;
@@ -39,6 +40,8 @@ while ( @ARGV ) {				# parse command line args
 	    $cvs = 1;
 	} elsif ( $_ eq "lib" ) {
 	    $libtarget = "LIBTARGET = 1\n";
+	} elsif ( $_ eq "nopro" ) {
+	    $nopro = 1;
 	} else {
 	    die "Invalid option";
 	}
@@ -51,7 +54,7 @@ $project || die "No project name specified";
 
 $win32make =
 "#############################################################################
-# \$Id: //depot/qt/main/bin/makeqtmake.bat#1 $
+# \$Id: //depot/qt/main/bin/makeqtmake.bat#2 $
 #
 # Win32 Makefile, requires Microsoft nmake.
 #
@@ -66,7 +69,7 @@ $libtarget
 
 $unixmake =
 "#############################################################################
-# \$Id: //depot/qt/main/bin/makeqtmake.bat#1 $
+# \$Id: //depot/qt/main/bin/makeqtmake.bat#2 $
 #
 # Unix Makefile, requires GNU make (gmake).
 #
@@ -88,7 +91,46 @@ if ( open( F, "> GNUmakefile" ) ) {
     close F;
 }
 $libtarget && ($libtarget = "-t lib");
-`progen -n $project $libtarget -o $project.pro *.cpp *.h`;
+if ( ! $nopro ) {
+    $files = join(" ",find_files(".",'\.(h|cpp)$',0));
+    `progen -n $project $libtarget -o $project.pro $files`;
+}
 $cvs && `cvs add $project.pro Makefile GNUmakefile`;
+exit 0;
+
+
+
+#
+# Finds files.
+#
+# Examples:
+#   find_files("/usr","\.cpp$",1)   - finds .cpp files in /usr and below
+#   find_files("/tmp","^#",0)	    - finds #* files in /tmp
+#
+
+sub find_files {
+    my($dir,$match,$descend) = @_;
+    my($file,$p,@files);
+    local(*D);
+    $dir =~ s=\\=/=g;
+    ($dir eq "") && ($dir = ".");
+    if ( opendir(D,$dir) ) {
+	if ( $dir eq "." ) {
+	    $dir = "";
+	} else {
+	    ($dir =~ /\/$/) || ($dir .= "/");
+	}
+	foreach $file ( readdir(D) ) {
+	    next if ( $file  =~ /^\.\.?$/ );
+	    $p = $dir . $file;
+	    ($file =~ /$match/) && (push @files, $p);
+	    if ( $descend && -d $p && ! -l $p ) {
+		push @files, &find_files($p,$match,$descend);
+	    }
+	}
+	closedir(D);
+    }
+    return @files;
+}
 __END__
 :endofperl
