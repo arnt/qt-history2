c14e97744960a637494ccc8660cbc7b6ef8f74a5
Paul Olav Tvete <paul@trolltech.com>
1998-03-11 12:34:05 +0100

    testing messageboxes when iconified/hidden
    [imported from CVS by cvs2p4 at 1999/10/31 19:05:39]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 7139]

--- tests/iconify/iconify.cpp
+++ tests/iconify/iconify.cpp
@@ -2,7 +2,12 @@
 #include <qpainter.h>
 #include <qapp.h>
 #include <qtimer.h>
+
+#include <qmsgbox.h>
+
+#ifdef WIN_SOMETHING
 #include <windows.h>
+#endif
 
 Main::Main(QWidget* parent, const char* name, int f) :
     QWidget(parent, name, f)
@@ -12,20 +17,39 @@ Main::Main(QWidget* parent, const char* name, int f) :
    t->start( 500 );
 }
 
+static int counter;
+
 void Main::bang()
 {
+
+counter++;
+#ifdef WIN_SOMETHING
    debug( " Windows thinks the widget is %svisible", IsWindowVisible(winId())?"":"not " );
+#endif
    debug( " Qt thinks the widget is %svisible", isVisible()?"":"not " );
-	show();
+   if ( counter % 30 == 0 ) {
+       show();
+       debug( "                  ***showing" );
+   }
+   if ( counter % 8 == 0 ) {
+       debug( "   ***MessageBox" );
+       QMessageBox msg( "Hello", "Hi There!", QMessageBox::Information, 
+			QMessageBox::Ok+QMessageBox::Default, 0, 0 );
+       msg.exec();
+   }
 }
 
 void Main::resizeEvent(QResizeEvent*)
 {
 }
 
-void Main::keyPressEvent(QKeyEvent*)
+void Main::keyPressEvent(QKeyEvent*k)
 {
+    if ( k->ascii() == 'i' || k->ascii() == 'I' )
+	iconify();
+    else
 	hide();
+
 }
 
 void Main::keyReleaseEvent(QKeyEvent*)
