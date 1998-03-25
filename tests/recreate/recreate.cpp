980a46e0766e6580c5d5d3c1f0e27e9d3027b95d
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1998-03-25 20:58:32 +0100

    more recreate testing
    [imported from CVS by cvs2p4 at 1999/10/31 19:10:12]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 7344]

--- tests/recreate/recreate.cpp
+++ tests/recreate/recreate.cpp
@@ -1,18 +1,35 @@
 #include "recreate.h"
+#include <qpushbt.h>
 #include <qpainter.h>
 #include <qapp.h>
 
 Main::Main(QWidget* parent, const char* name, int f) :
     QLabel("Press key to recreate", parent, name, f)
 {
+    f = new QFrame( this );
+    f->setFrameStyle( QFrame::Raised  + QFrame::Box );
 }
 
 void Main::bang()
 {
 }
 
-void Main::resizeEvent(QResizeEvent*)
+
+void Main::pang()
 {
+    
+}
+
+
+void Main::ting()
+{
+    
+}
+
+
+void Main::resizeEvent(QResizeEvent* )
+{
+    f->setGeometry( 10, 10, width()-20, height()-20 );
 }
 
 void Main::keyPressEvent(QKeyEvent*)
@@ -20,19 +37,7 @@ void Main::keyPressEvent(QKeyEvent*)
     recreate(0,0,QPoint(0,0),isVisible());
 }
 
-void Main::keyReleaseEvent(QKeyEvent*)
-{
-}
 
-void Main::paintEvent(QPaintEvent* e)
-{
-    QLabel::paintEvent(e);
-
-    //QPainter p(this);
-    //p.setClipRect(e->rect());
-
-    // ...
-}
 
 main(int argc, char** argv)
 {
@@ -41,6 +46,10 @@ main(int argc, char** argv)
     QApplication::setFont( QFont("Helvetica") );
 
     Main m;
+    
+    QPushButton * p;
+    p = new QPushButton( 
+    
     m.show();
 
     QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
--- tests/recreate/recreate.h
+++ tests/recreate/recreate.h
@@ -4,10 +4,8 @@ class Main : public QLabel {
     Q_OBJECT
 public:
     Main(QWidget* parent=0, const char* name=0, int f=0);
-    void resizeEvent(QResizeEvent*);
-    void keyPressEvent(QKeyEvent*);
-    void keyReleaseEvent(QKeyEvent*);
-    void paintEvent(QPaintEvent* e);
 public slots:
     void bang();
+    void pang();
+    void ting();
 };
