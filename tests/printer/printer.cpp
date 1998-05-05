4146d0ffefb20a745ad40c171d32f64a8d315ae9
Eirik Eng <eiriken@trolltech.com>
1998-05-05 15:20:38 +0100

    Simple printer test.
    [imported from CVS by cvs2p4 at 1999/10/31 19:15:25]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 7610]

---
+++ tests/printer/printer.cpp
@@ -0,0 +1,32 @@
+#include <qprinter.h>
+#include <qapp.h>
+#include <qpainter.h>
+
+#include <qpixmap.h>
+#include <qimage.h>
+
+int main( int argc, char **argv )
+{
+    QApplication a( argc, argv );
+    
+    QImage  img("ttlogo.bmp");
+    QPixmap pix("ttlogo.bmp");
+
+
+    QPrinter prt;
+    if ( prt.setup(0) )
+    {
+        QPainter p;
+        p.begin( &prt );
+	//	p.setPen( red );
+	//	p.save();
+	p.drawRect( 10, 10, 100, 100 );
+	//	p.rotate( -90 );
+	p.drawPixmap( 10, 10, pix );
+	p.drawPixmap( 300, 10, pix );
+	//	p.restore();
+        p.end();
+    }
+    return 0;
+}
+
---
+++ tests/printer/printer.pro
@@ -0,0 +1,4 @@
+TEMPLATE	= app
+CONFIG		= qt warn_on release
+SOURCES		= printer.cpp
+TARGET		= printer
