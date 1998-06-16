df923f88b3a0010f32bcd411c2ab128d02da9937
Warwick Allison <warwick@trolltech.com>
1998-06-16 14:48:41 +0100

    Moved.
    [imported from CVS by cvs2p4 at 1999/10/31 19:30:27]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 7872]

---
+++ src/compat/qobjcoll.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/compat/qobjcoll.h#1 $
+**
+** Definition of QObject and QMetaObject collections
+**
+** Created : 940807
+**
+** Copyright (C) 1994-1997 by Troll Tech AS.  All rights reserved.
+**
+*****************************************************************************/
+
+#ifndef QOBJCOLL_H
+#define QOBJCOLL_H
+
+#include <qobjectlist.h>
+#include <qobjectdict.h>
+
+#endif // QOBJCOLL_H
+
---
+++ src/compat/qwidcoll.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/compat/qwidcoll.h#1 $
+**
+** Definition of QWidget collections
+**
+** Created : 950116
+**
+** Copyright (C) 1995-1997 by Troll Tech AS.  All rights reserved.
+**
+*****************************************************************************/
+
+#ifndef QWIDCOLL_H
+#define QWIDCOLL_H
+
+#include "qwidgetlist.h"
+#include "qwidgetintdict.h"
+
+#endif // QWIDCOLL_H
