b1192a9951015e6283cd2dbfd31135de1cc7fb5b
Warwick Allison <warwick@trolltech.com>
1998-08-10 15:11:48 +0100

    Test QWidget::setMask().
    [imported from CVS by cvs2p4 at 1999/10/31 19:40:05]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 8305]

---
+++ tests/widgetmask/widgetmask.cpp
@@ -0,0 +1,56 @@
+#include "widgetmask.h"
+#include <qpainter.h>
+#include <qapplication.h>
+#include <stdlib.h>
+
+Main::Main(QWidget* parent, const char* name, int f) :
+    QWidget(parent, name, f)
+{
+    QRegion rgn;
+    for (int i=0; i<100; i++) {
+	int x=rand()%width();
+	int y=rand()%height();
+	int w=rand()%(width()-x);
+	int h=rand()%(height()-y);
+	rgn = rgn.unite(QRegion(x,y,w,h));
+    }
+    setMask(rgn);
+}
+
+void Main::bang()
+{
+}
+
+void Main::resizeEvent(QResizeEvent*)
+{
+}
+
+void Main::keyPressEvent(QKeyEvent*)
+{
+}
+
+void Main::keyReleaseEvent(QKeyEvent*)
+{
+}
+
+void Main::paintEvent(QPaintEvent* e)
+{
+    QPainter p(this);
+    p.setClipRect(e->rect());
+    p.drawLine(0,0,width()-1,height()-1);
+    p.drawLine(width()-1,0,0,height()-1);
+}
+
+main(int argc, char** argv)
+{
+    QApplication::setColorSpec( QApplication::ManyColor );
+    QApplication app(argc, argv);
+    QApplication::setFont( QFont("Helvetica") );
+
+    Main m;
+    m.show();
+
+    QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
+
+    return app.exec();
+}
