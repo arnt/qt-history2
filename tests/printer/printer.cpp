99bd9971efc487b33a6a1c2c09d4b86bb07d902b
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1998-10-12 01:03:10 +0100

    more pixmap/image testing
    [imported from CVS by cvs2p4 at 1999/10/31 19:59:10]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 8965]

--- tests/printer/printer.cpp
+++ tests/printer/printer.cpp
@@ -8,24 +8,24 @@
 int main( int argc, char **argv )
 {
     QApplication a( argc, argv );
-    
+
     QImage  img("ttlogo.bmp");
     QPixmap pix("ttlogo.bmp");
-
 
     QPrinter prt;
     if ( prt.setup(0) )
     {
         QPainter p;
         p.begin( &prt );
-	p.setPen( red );
+	p.setPen( Qt::red );
 	p.save();
 	p.drawRect( 10, 10, 100, 100 );
-#if 0	
 	p.drawPixmap( 10, 10, pix );
-	p.rotate( -90 );
-	p.drawImage( 300, 10, img );
-#endif	
+	p.drawImage( 10, 300, img );
+	img = img.smoothScale( 112, 65 );
+	p.drawImage( 400,  10, img );
+	p.drawImage( 400,  80, img );
+	p.drawImage( 400, 150, img );
 	p.restore();
         p.end();
     }
