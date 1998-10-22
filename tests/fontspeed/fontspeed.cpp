77d9af38b19490e32fa588c125a3da370867aa9e
Haavard Nord <hanord@trolltech.com>
1998-10-22 12:28:37 +0100

    Tests speed of switching fonts
    [imported from CVS by cvs2p4 at 1999/10/31 20:00:22]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 9026]

---
+++ tests/fontspeed/fontspeed.cpp
@@ -0,0 +1,86 @@
+#include <qapplication.h>
+#include <qpainter.h>
+#include <qpixmap.h>
+#include <qdatetime.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//#define TEST_COLORS
+
+const int MAX_ITER  = 4000;
+const int MAX_FONTS = 100;
+typedef QFont *pQFont;
+QFont **fontArray;
+
+
+void createFontArray()
+{
+    fontArray = new pQFont[MAX_FONTS];
+    for ( int i=0; i<MAX_FONTS; i++ ) {
+	QString name  = "none";
+	int boldness  = QFont::Normal;
+	int pointSize = 10;
+	switch ( rand()%4 ) {
+	    case 0:
+		name = "Times Roman";
+		break;
+	    case 1:
+		name = "Helvetica";
+		break;
+	    case 2:
+		name = "Courier";
+		break;
+	    case 3:
+		name = "System";
+		break;
+	}
+	switch ( rand()%3 ) {
+	    case 0:
+		boldness = QFont::Light;
+		break;
+	    case 1:
+		boldness = QFont::Normal;
+		break;
+	    case 2:
+		boldness = QFont::Bold;
+		break;
+	}
+	pointSize = (rand()%50)+4;
+	fontArray[i] = new QFont( name, pointSize, boldness );
+    }
+}
+ 
+void deleteFontArray()
+{
+    for ( int i=0; i<MAX_FONTS; i++ )	
+	delete fontArray[i];
+    delete [] fontArray;
+}
+
+
+void drawFonts( QPainter *p )
+{
+    QRect vp = p->viewport();
+    for ( int i=0; i<MAX_ITER; i++ ) {
+	int x = rand()%vp.width();
+	int y = rand()%vp.height();
+	p->setFont( *fontArray[rand()%MAX_FONTS] );
+	p->drawText( x, y, 100, 40, AlignRight, "text to draw" );
+    }
+}
+
+
+int main( int argc, char **argv )
+{
+    QApplication a(argc,argv);
+    createFontArray();
+    QPixmap pm(256,256);
+    pm.fill( white );
+    QPainter p;
+    p.begin( &pm );
+    QTime t; t.start();
+    drawFonts( &p );
+    debug( "TIME = %d", t.elapsed() );
+    p.end();
+    deleteFontArray();
+}
---
+++ tests/fontspeed/fontspeed.pro
@@ -0,0 +1,5 @@
+TEMPLATE	= app
+CONFIG		= qt warn_on release console
+HEADERS		= 
+SOURCES		= fontspeed.cpp
+TARGET		= fontspeed
