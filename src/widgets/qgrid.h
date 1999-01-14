f322ee96bae4adc6b2e54b9b937b7232e1058f76
Warwick Allison <warwick@trolltech.com>
1999-01-14 15:40:09 +0100

    1999 copyright, QPL+Professional combined copyright notice.
    [imported from CVS by cvs2p4 at 1999/10/31 20:17:58]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 9804]

--- src/widgets/qgrid.h
+++ src/widgets/qgrid.h
@@ -1,7 +1,7 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/widgets/qgrid.h#8 $
+** $Id: //depot/qt/main/src/widgets/qgrid.h#9 $
 **
-** Copyright (C) 1992-1998 Troll Tech AS.  All rights reserved.
+** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
 **
 ** This file is part of an example program for Qt.  This example
 ** program may be used, distributed and modified without limitation.
--- src/widgets/qvbox.h
+++ src/widgets/qvbox.h
@@ -1,7 +1,7 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/widgets/qvbox.h#6 $
+** $Id: //depot/qt/main/src/widgets/qvbox.h#7 $
 **
-** Copyright (C) 1992-1998 Troll Tech AS.  All rights reserved.
+** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
 **
 ** This file is part of an example program for Qt.  This example
 ** program may be used, distributed and modified without limitation.
