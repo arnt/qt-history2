d1316dac5311e3c4b23fe4c1955b3b41debcb78e
Warwick Allison <warwick@trolltech.com>
1999-01-14 16:36:16 +0100

    Copyright headers.
    [imported from CVS by cvs2p4 at 1999/10/31 20:18:56]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 9808]

--- src/compat/qmenudta.h
+++ src/compat/qmenudta.h
@@ -1,3 +1,25 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/compat/qmenudta.h#2 $
+**
+** Compatibility file - should only be included by legacy code.
+** It #includes the file which obsoletes this one.
+**
+** Copyright (C) 1998-1999 Troll Tech AS.  All rights reserved.
+**  This file is part of the Qt GUI Toolkit.
+**
+** This file may be distributed under the terms of the Q Public License
+** as defined by Troll Tech AS of Norway and appearing in the file
+** LICENSE.QPL included in the packaging of this file.
+**
+** Licensees with valid Qt Professional Edition licenses may distribute and
+** use this file in accordance with the Qt Professional Edition License
+** provided at sale or upon request.
+**
+** See http://www.troll.no/pricing.html or email sales@troll.no for
+** information about the Professional Edition licensing, or see
+** http://www.troll.no/qpl/ for QPL licensing information.
+**
+*****************************************************************************/
 #ifndef QMENUDTA_H
 #define QMENUDTA_H
 #include "qmenudata.h"
--- src/compat/qwidcoll.h
+++ src/compat/qwidcoll.h
@@ -1,5 +1,27 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/compat/qwidcoll.h#1 $
+** $Id: //depot/qt/main/src/compat/qwidcoll.h#2 $
+**
+** Compatibility file - should only be included by legacy code.
+** It #includes the file which obsoletes this one.
+**
+** Copyright (C) 1998-1999 Troll Tech AS.  All rights reserved.
+**  This file is part of the Qt GUI Toolkit.
+**
+** This file may be distributed under the terms of the Q Public License
+** as defined by Troll Tech AS of Norway and appearing in the file
+** LICENSE.QPL included in the packaging of this file.
+**
+** Licensees with valid Qt Professional Edition licenses may distribute and
+** use this file in accordance with the Qt Professional Edition License
+** provided at sale or upon request.
+**
+** See http://www.troll.no/pricing.html or email sales@troll.no for
+** information about the Professional Edition licensing, or see
+** http://www.troll.no/qpl/ for QPL licensing information.
+**
+*****************************************************************************/
+/****************************************************************************
+** $Id: //depot/qt/main/src/compat/qwidcoll.h#2 $
 **
 ** Definition of QWidget collections
 **
