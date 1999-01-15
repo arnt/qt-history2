a7506a5f14f45b610a5cb4b7fb0061588135c88c
Paul Olav Tvete <paul@trolltech.com>
1999-01-15 20:59:17 +0100

    load does not work
    [imported from CVS by cvs2p4 at 1999/10/31 20:19:34]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 9848]

--- tests/messagefile/messagefile.cpp
+++ tests/messagefile/messagefile.cpp
@@ -5,62 +5,69 @@ const char * s2 = "vold";
 const char * s3 = "uff";
 const char * s4 = "hei";
 
+//#define MESSAGE_LOAD
+
 main(int, char** )
 {
     QMessageFile m( 0 );
 
+
+#ifdef MESSAGE_LOAD
+    m.load( "msg.tr" );
+#else
     m.insert( 1, s1 );
     m.insert( 123456, s2 );
     m.insert( 257, s3 );
     m.insert( 513, s4 );
-    if ( !m.contains( 1 ) )
+#endif
+    if ( !m.contains( 1, 0, 0 ) )
 	debug( "insertion failed(1)" );
-    if ( !m.contains( 123456 ) )
+    if ( !m.contains( 123456, 0, 0 ) )
 	debug( "insertion failed(123456)" );
-    if ( !m.contains( 257 ) )
+    if ( !m.contains( 257, 0, 0 ) )
 	debug( "insertion failed(257)" );
-    if ( !m.contains( 513 ) )
+    if ( !m.contains( 513, 0, 0 ) )
 	debug( "insertion failed(513)" );
 
     QString tmp;
-    tmp = m.find( 1 );
+    tmp = m.find( 1, 0, 0 );
     if ( tmp != s1 )
 	debug( "look-up failed(1) (<%s>)", tmp.ascii() );
-    tmp = m.find( 123456 );
+    tmp = m.find( 123456, 0, 0 );
     if ( tmp != s2 )
 	debug( "look-up failed(123456) (<%s>)", tmp.ascii() );
-    tmp = m.find( 257 );
+    tmp = m.find( 257, 0, 0 );
     if ( tmp != s3 )
 	debug( "look-up failed(257) (<%s>)", tmp.ascii() );
-    tmp = m.find( 513 );
+    tmp = m.find( 513, 0, 0 );
     if ( tmp != s4 )
 	debug( "look-up failed(513) (<%s>)", tmp.ascii() );
 
     m.squeeze();
-    tmp = m.find( 1 );
+    tmp = m.find( 1, 0, 0 );
     if ( tmp != s1 )
 	debug( "squeezed look-up failed(1) (<%s>)", tmp.ascii() );
-    tmp = m.find( 123456 );
+    tmp = m.find( 123456, 0, 0 );
     if ( tmp != s2 )
 	debug( "squeezed look-up failed(123456) (<%s>)", tmp.ascii() );
-    tmp = m.find( 257 );
+    tmp = m.find( 257, 0, 0 );
     if ( tmp != s3 )
 	debug( "squeezed look-up failed(257) (<%s>)", tmp.ascii() );
-    tmp = m.find( 513 );
+    tmp = m.find( 513, 0, 0 );
     if ( tmp != s4 )
 	debug( "squeezed look-up failed(513) (<%s>)", tmp.ascii() );
 
     m.unsqueeze();
-    tmp = m.find( 1 );
+    tmp = m.find( 1, 0, 0 );
     if ( tmp != s1 )
 	debug( "unsqueezed look-up failed(1) (<%s>)", tmp.ascii() );
-    tmp = m.find( 123456 );
+    tmp = m.find( 123456, 0, 0 );
     if ( tmp != s2 )
 	debug( "unsqueezed look-up failed(123456) (<%s>)", tmp.ascii() );
-    tmp = m.find( 257 );
+    tmp = m.find( 257, 0, 0 );
     if ( tmp != s3 )
 	debug( "unsqueezed look-up failed(257) (<%s>)", tmp.ascii() );
-    tmp = m.find( 513 );
+    tmp = m.find( 513, 0, 0 );
     if ( tmp != s4 )
 	debug( "unsqueezed look-up failed(513) (<%s>)", tmp.ascii() );
 
@@ -72,4 +79,7 @@ main(int, char** )
 	debug( "saw %d: <%s> using iterator", it.currentKey(), s->ascii() );
 	++it;
     }
+#ifndef MESSAGE_LOAD
+    m.save( "msg.tr" );
+#endif
 }
