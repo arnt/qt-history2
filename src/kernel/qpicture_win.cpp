6aa7db84da268abc07fdd92d2f73e01359230307
Warwick Allison <warwick@trolltech.com>
1999-01-25 11:21:15 +0100

    No QPL in *_win.cpp files.
    [imported from CVS by cvs2p4 at 1999/10/31 20:21:51]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 9943]

--- src/kernel/qpicture_win.cpp
+++ src/kernel/qpicture_win.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpicture_win.cpp#17 $
+** $Id: //depot/qt/main/src/kernel/qpicture_win.cpp#18 $
 **
 ** Implementation of QPicture class for Win32
 **
@@ -7,19 +7,14 @@
 **
 ** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
 **
-** This file is part of the Qt GUI Toolkit.
-**
-** This file may be distributed under the terms of the Q Public License
-** as defined by Troll Tech AS of Norway and appearing in the file
-** LICENSE.QPL included in the packaging of this file.
+** This file is part of the Qt GUI Toolkit Professional Edition.
 **
 ** Licensees with valid Qt Professional Edition licenses may distribute and
 ** use this file in accordance with the Qt Professional Edition License
 ** provided at sale or upon request.
 **
 ** See http://www.troll.no/pricing.html or email sales@troll.no for
-** information about the Professional Edition licensing, or see
-** http://www.troll.no/qpl/ for QPL licensing information.
+** information about the Professional Edition licensing.
 **
 *****************************************************************************/
 
