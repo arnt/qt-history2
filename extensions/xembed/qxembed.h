58cd3bae4154669819439e4edfa9658626e11e46
Matthias Ettrich <ettrich@trolltech.com>
1999-02-05 18:45:27 +0100

    qxembed, yet without documentation whatsoever
    [imported from CVS by cvs2p4 at 1999/10/31 20:25:16]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 10090]

---
+++ extensions/xembed/qxembed.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+** $Id: //depot/qt/main/extensions/xembed/qxembed.h#1 $
+**
+** Definition of something or other
+**
+** Created : 979899
+**
+** Copyright (C) 1997 by Troll Tech AS.  All rights reserved.
+**
+****************************************************************************/
+
+#ifndef QXEMBED_H
+#define QXEMBED_H
+
+class QXEmbed : public QWidget
+{
+  Q_OBJECT
+
+public:
+
+  QXEmbed(QWidget *parent=0, const char *name=0);
+
+  void embed(WId w);
+
+protected:
+  void keyPressEvent( QKeyEvent * );
+  void keyReleaseEvent( QKeyEvent * );
+  void focusInEvent( QFocusEvent * );
+  void focusOutEvent( QFocusEvent * );
+  void resizeEvent(QResizeEvent *);
+  void showEvent( QShowEvent * );
+
+  bool focusNextPrevChild( bool next );
+
+private:
+
+ void sendFocusIn();
+ void sendFocusOut();
+
+  WId  window;
+
+};
+
+
+#endif
