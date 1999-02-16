4143bfad82288d22ca2d3715f9c0d693df727e45
Warwick Allison <warwick@trolltech.com>
1999-02-16 19:07:00 +0100

    less spec.
    [imported from CVS by cvs2p4 at 1999/10/31 20:30:10]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 10282]

--- src/widgets/qlineedit.cpp
+++ src/widgets/qlineedit.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-** $Id: //depot/qt/main/src/widgets/qlineedit.cpp#181 $
+** $Id: //depot/qt/main/src/widgets/qlineedit.cpp#182 $
 **
 ** Implementation of QLineEdit widget class
 **
@@ -401,7 +401,7 @@ void QLineEdit::keyPressEvent( QKeyEvent *e )
 	    if ( v->validate( tbuf, cursorPos ) == QValidator::Acceptable )
 		emit returnPressed();
 	}
-#if defined(_OS_LINUX_)
+#if defined(_CC_GNU_)
 #warning "Arnt: must fix this" // fix what?
 #endif
 	// ### 2.0 must fix this
