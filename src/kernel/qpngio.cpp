44ef83e39e3c923b0743729cc7cc94036e850d69
Warwick Allison <warwick@trolltech.com>
1999-02-26 15:14:48 +0100

    compile on windows
    [imported from CVS by cvs2p4 at 1999/10/31 20:35:10]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 10568]

--- src/kernel/qpngio.cpp
+++ src/kernel/qpngio.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpngio.cpp#8 $
+** $Id: //depot/qt/main/src/kernel/qpngio.cpp#9 $
 **
 ** Implementation of PNG QImage IOHandler
 **
@@ -827,7 +827,8 @@ int QPNGFormat::user_chunk(png_structp png_ptr, png_infop,
     // debug("Got %ld-byte %s chunk", length, png_ptr->chunk_name);
     if ( 0==strcmp((char*)png_ptr->chunk_name, "gIFg") ) {
 
-	QPNGImageWriter::DisposalMethod disposal = data[0];
+	QPNGImageWriter::DisposalMethod disposal =
+	    (QPNGImageWriter::DisposalMethod)data[0];
 	// TODO: use the disposal method
 	int ms_delay = ((data[2] << 8) | data[3])*10;
 	consumer->setFramePeriod(ms_delay);
