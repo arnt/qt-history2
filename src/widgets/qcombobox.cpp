0260f9154c2b86c434bc70eb21d4865932188fcf
Reginald Stadlbauer <reggie@froglogic.com>
1999-04-26 11:34:43 +0100

    The listbox of a combobox did show max. 10 items. Fixed that.
    [imported from CVS by cvs2p4 at 1999/10/31 20:48:19]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 11305]

--- src/widgets/qcombobox.cpp
+++ src/widgets/qcombobox.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-** $Id: //depot/qt/main/src/widgets/qcombobox.cpp#195 $
+** $Id: //depot/qt/main/src/widgets/qcombobox.cpp#196 $
 **
 ** Implementation of QComboBox widget class
 **
@@ -1264,9 +1264,9 @@ void QComboBox::focusInEvent( QFocusEvent * )
    Calculates the listbox height needed to contain all items, or as
    many as the list box is supposed to contain.
 */
-static int listHeight( QListBox *l, int sl )
+static int listHeight( QListBox *l, int /*sl*/ )
 {
-    l->setFixedVisibleLines(QMIN(l->count(), (uint)sl));
+    //l->setFixedVisibleLines(QMIN(l->count(), (uint)sl));
     return l->sizeHint().height();
 }
 
