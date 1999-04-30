5300dc83ef4a34288de5b3e28caca1fd833ea34d
Matthias Ettrich <ettrich@trolltech.com>
1999-04-30 15:46:12 +0100

    one copyright header per file should be enough
    [imported from CVS by cvs2p4 at 1999/10/31 20:53:26]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 11392]

--- src/compat/qapp.h
+++ src/compat/qapp.h
@@ -1,27 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/compat/qapp.h#3 $
-**
-** Compatibility file - should only be included by legacy code.
-** It #includes the file which obsoletes this one.
-**
-** Copyright (C) 1998-1999 Troll Tech AS.  All rights reserved.
-**  This file is part of the Qt GUI Toolkit.
-**
-** This file may be distributed under the terms of the Q Public License
-** as defined by Troll Tech AS of Norway and appearing in the file
-** LICENSE.QPL included in the packaging of this file.
-**
-** Licensees holding valid Qt Professional Edition licenses may use this
-** file in accordance with the Qt Professional Edition License Agreement
-** provided with the Qt Professional Edition.
-**
-** See http://www.troll.no/pricing.html or email sales@troll.no for
-** information about the Professional Edition licensing, or see
-** http://www.troll.no/qpl/ for QPL licensing information.
-**
-*****************************************************************************/
-/****************************************************************************
-** $Id: //depot/qt/main/src/compat/qapp.h#3 $
+** $Id: //depot/qt/main/src/compat/qapp.h#4 $
 **
 ** Compatibility file - should only be included by legacy code.
 ** It #includes the file which obsoletes this one.
