a24791d27648c75b4be1bb653e48fe5543f88c23
Paul Olav Tvete <paul@trolltech.com>
1999-05-28 10:18:20 +0100

    Use heightForWidth() when calculating geometry.
    [imported from CVS by cvs2p4 at 1999/10/31 21:07:11]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 11848]

--- src/widgets/qmenubar.cpp
+++ src/widgets/qmenubar.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/widgets/qmenubar.cpp#141 $
+** $Id: //depot/qt/main/src/widgets/qmenubar.cpp#142 $
 **
 ** Implementation of QMenuBar class
 **
@@ -280,7 +280,9 @@ bool QMenuBar::eventFilter( QObject *object, QEvent *event )
 {
     if ( object == parent() && event->type() == QEvent::Resize ) {
 	QResizeEvent *e = (QResizeEvent *)event;
-	setGeometry( 0, y(), e->size().width(), height() );
+	int w = e->size().width(); 
+	qDebug( "QMenuBar::eventFilter %d", w );
+	setGeometry( 0, y(), w, heightForWidth(w) );
 	return FALSE;
     }
 
