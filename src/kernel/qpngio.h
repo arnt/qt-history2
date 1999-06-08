6f08bbc7c7289cee57ae994d77d53878c7ded2b3
Warwick Allison <warwick@trolltech.com>
1999-06-08 11:50:57 +0100

    Strip cr
    [imported from CVS by cvs2p4 at 1999/10/31 21:23:01]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 12235]

--- src/kernel/qpngio.h
+++ src/kernel/qpngio.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpngio.h#6 $
+** $Id: //depot/qt/main/src/kernel/qpngio.h#7 $
 **
 ** Definition of PNG QImage IOHandler
 **
