3fcef341720bf751a49ddb138a4e76aa3ea5fc3a
Eirik Aavitsland <aavit@trolltech.com>
1999-06-09 17:49:54 +0100

    Minimal test program showing a mocstuff segfault problem
    [imported from CVS by cvs2p4 at 1999/10/31 21:25:23]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 12299]

---
+++ tests/moc-bug/spin.h
@@ -0,0 +1,22 @@
+/****************************************************************************
+** $Id: //depot/qt/main/tests/moc-bug/spin.h#1 $
+**
+** Definition of 
+**
+** Copyright (C) 1999 by Troll Tech AS.  All rights reserved.
+**
+****************************************************************************/
+
+#ifndef SPIN_H
+#define SPIN_H
+
+#include <qwidget.h>
+
+class MainParent : public QWidget {
+    Q_OBJECT
+public:
+    MainParent( QWidget* parent = 0, const char* name = 0, int f = 0 );
+};
+
+
+#endif
---
+++ tests/moc-bug/spin.pro
@@ -0,0 +1,5 @@
+TEMPLATE	= app
+CONFIG		= qt warn_on release
+HEADERS		= spin.h
+SOURCES		= spin.cpp
+TARGET		= spin
