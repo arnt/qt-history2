721642b5f50102dc31ebf5e6023764024dbc28d9
Paul Olav Tvete <paul@trolltech.com>
1999-06-10 16:41:10 +0100

    Added PO files, changed .tr to .qm, made norwegian translation and made the example extensible.
    [imported from CVS by cvs2p4 at 1999/10/31 21:27:26]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 12344]

--- examples/i18n/main.cpp
+++ examples/i18n/main.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/examples/i18n/main.cpp#2 $
+** $Id: //depot/qt/main/examples/i18n/main.cpp#3 $
 **
 ** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
 **
@@ -10,40 +10,37 @@
 
 #include <qapplication.h>
 #include <qtranslator.h>
+#include <qfileinfo.h>
 
 #include "mywidget.h"
 
 main( int argc, char** argv )
 {
-	QString lfile;
-
-	if ( argc != 2 ) {
-		qWarning( "\n\n\nUsage: i18n <language> (where <language> can be 'de', 'en' and 'no')\n\n" );
+    if ( argc != 2 ) {
+	qWarning( "\n\nUsage: %s <language> (where <language> can be 'de', 'en', 'no'...)\n", argv[0] );
         return 0;
-    } else {
-		if ( QString( argv[1] ) == "de" )
-			lfile = "mywidget_de.tr";
-		else if ( QString( argv[1] ) == "no" ||
-				  QString( argv[1] ) == "en" )
-			lfile = "mywidget_en.tr";
-		else {
-			qWarning( "\n\n\nUsage: i18n <language> (where <language> can be 'de', 'en' and 'no')\n\n" );
-            return 0;
-		}
+    }
+    QString lang( argv[1] );
+
+    QString lfile = "mywidget_" + lang + ".qm";
+
+    QFileInfo fi( lfile );
+    if ( !fi.exists() ) {
+    	qWarning( "\n%s: cannot find translation for language '%s'" 
+		  " (try 'de', 'en' or 'no')\n", argv[0], argv[1] );
+	return 0;
     }
 
-	QApplication app( argc, argv );
+    QApplication app( argc, argv );
 
-	//QAppTranslator t;
+    QTranslator translator( 0 );
+    translator.load( lfile, "." );
+    app.installTranslator( &translator );
 
-	QTranslator translator( 0 );
-	translator.load( lfile, "." );
-	app.installTranslator( &translator );
+    MyWidget m( 0, QString( argv[1] ), "mainwindow" );
+    m.resize( 400, 300 );
+    app.setMainWidget( &m );
+    m.show();
 
-	MyWidget m( 0, QString( argv[1] ), "mainwindow" );
-	m.resize( 400, 300 );
-	app.setMainWidget( &m );
-	m.show();
-
-	return app.exec();
+    return app.exec();
 }
