8afd5d14180cf2a6253394a704544dc07140b9ab
Warwick Allison <warwick@trolltech.com>
1999-06-15 20:50:46 +0100

    pass-through hidden function
    [imported from CVS by cvs2p4 at 1999/10/31 21:33:55]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 12509]

--- src/tools/qfile.h
+++ src/tools/qfile.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qfile.h#36 $
+** $Id: //depot/qt/main/src/tools/qfile.h#37 $
 **
 ** Definition of QFile class
 **
@@ -71,6 +71,8 @@ public:
 
     int		readBlock( char *data, uint len );
     int		writeBlock( const char *data, uint len );
+    int		writeBlock( const QByteArray& data )
+		    { return QIODevice::writeBlock(data); }
     int		readLine( char *data, uint maxlen );
     int		readLine( QString &, uint maxlen );
 
