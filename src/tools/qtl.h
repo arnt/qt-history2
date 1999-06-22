96f255fe79170e6d38b9ea729bb6b64e43f547be
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
1999-06-22 16:13:13 +0100

    edit to be troll tech code style (mostly indentation), remove an unused function that hp-cc (bless its soul when it's finally dead) cannot compile
    [imported from CVS by cvs2p4 at 1999/10/31 21:42:26]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 12898]

--- src/tools/qtl.h
+++ src/tools/qtl.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/tools/qtl.h#9 $
+** $Id: //depot/qt/main/src/tools/qtl.h#10 $
 **
 ** Definition of Qt template library classes
 **
@@ -33,84 +33,77 @@ template <class T>
 class QTextOStreamIterator
 {
 protected:
-  QTextOStream& stream;
-  QString separator;
+    QTextOStream& stream;
+    QString separator;
 
 public:
-  QTextOStreamIterator( QTextOStream& s) : stream( s ) {}
-  QTextOStreamIterator( QTextOStream& s, const QString& sep ) : stream( s ), separator( sep )  {}
-  QTextOStreamIterator<T>& operator= ( const T& x ) { 
-    stream << x;
-    if ( !separator.isEmpty() )
-      stream << separator;
-    return *this;
-  }
-  QTextOStreamIterator<T>& operator*() { return *this; }
-  QTextOStreamIterator<T>& operator++() { return *this; } 
-  QTextOStreamIterator<T>& operator++(int) { return *this; } 
+    QTextOStreamIterator( QTextOStream& s) : stream( s ) {}
+    QTextOStreamIterator( QTextOStream& s, const QString& sep )
+	: stream( s ), separator( sep )  {}
+    QTextOStreamIterator<T>& operator= ( const T& x ) {
+	stream << x;
+	if ( !separator.isEmpty() )
+	    stream << separator;
+	return *this;
+    }
+    QTextOStreamIterator<T>& operator*() { return *this; }
+    QTextOStreamIterator<T>& operator++() { return *this; }
+    QTextOStreamIterator<T>& operator++(int) { return *this; }
 };
 
+
 template <class InputIterator, class OutputIterator>
-inline OutputIterator qCopy( InputIterator _begin, InputIterator _end, OutputIterator _dest )
+inline OutputIterator qCopy( InputIterator _begin, InputIterator _end,
+			     OutputIterator _dest )
 {
-  while( _begin != _end )
-     *_dest++ = *_begin++;
-  return _dest;
+    while( _begin != _end )
+	*_dest++ = *_begin++;
+    return _dest;
 }
 
-template <class Container, class OutputIterator>
-inline OutputIterator qCopy( Container& _container, OutputIterator _dest )
-{
-  typename Container::Iterator begin = _container.begin();
-  typename Container::Iterator end = _container.end();
-  while( begin != end )
-     *_dest++ = *begin++;
-  return _dest;
-}
 
 template <class T>
 inline void qSwap( T& _value1, T& _value2 )
 {
-  T tmp = _value1;
-  _value1 = _value2;
-  _value2 = tmp;
+    T tmp = _value1;
+    _value1 = _value2;
+    _value2 = tmp;
 }
+
 
 template <class InputIterator>
 inline void qBubbleSort( InputIterator b, InputIterator e )
 {
-  // Goto last element;
-  InputIterator last = e;
-  --last;
-  // only one element or no elements ?
-  if ( last == b )
-    return;
+    // Goto last element;
+    InputIterator last = e;
+    --last;
+    // only one element or no elements ?
+    if ( last == b )
+	return;
 
-  // So we have at least two elements in here
-  while( b != last )
-  {
-    bool swapped = FALSE;
-    InputIterator swap_pos = b;
-    InputIterator x = e;
-    InputIterator y = x;
-    y--;
-    do
-    {
-      --x;
-      --y;
-      if ( *x < *y )
-      {
-	swapped = TRUE;
-	qSwap( *x, *y );
-	swap_pos = y;
-      }
-    } while( y != b );
-    if ( !swapped )
-      return;
-    b = swap_pos;
-    b++;
-  }
+    // So we have at least two elements in here
+    while( b != last ) {
+	bool swapped = FALSE;
+	InputIterator swap_pos = b;
+	InputIterator x = e;
+	InputIterator y = x;
+	y--;
+	do {
+	    --x;
+	    --y;
+	    if ( *x < *y ) {
+		swapped = TRUE;
+		qSwap( *x, *y );
+		swap_pos = y;
+	    }
+	} while( y != b );
+	if ( !swapped )
+	    return;
+	b = swap_pos;
+	b++;
+    }
 }
+
 
 template <class Container>
 inline void qBubbleSort( Container &c )
@@ -118,76 +111,68 @@ inline void qBubbleSort( Container &c )
   qBubbleSort( c.begin(), c.end() );
 }
 
+
 template <class HeapPtr>
 void qHeapSortPushDown( HeapPtr heap, int first, int last )
 {
-  int r = first;
-  while( r <= last/2 )
-  {
-    // Node r has only one child ?
-    if ( last == 2*r )
-    {
-      // Need for swapping ?
-      if ( heap[r] > heap[ 2*r ] )
-	qSwap( heap[r], heap[ 2*r ] );
-      // That's it ...
-      r = last;
+    int r = first;
+    while( r <= last/2 ) {
+	// Node r has only one child ?
+	if ( last == 2*r ) {
+	    // Need for swapping ?
+	    if ( heap[r] > heap[ 2*r ] )
+		qSwap( heap[r], heap[ 2*r ] );
+	    // That's it ...
+	    r = last;
+	} else { // Node has two children
+	    if ( heap[r] > heap[ 2*r ] && heap[ 2*r ] <= heap[ 2*r+1 ] ) {
+		// Swap with left child
+		qSwap( heap[r], heap[ 2*r ] );
+		r *= 2;
+	    } else if ( heap[r] > heap[ 2*r+1 ] &&
+			heap[ 2*r+1 ] < heap[ 2*r ] ) {
+		// Swap with right child
+		qSwap( heap[r], heap[ 2*r+1 ] );
+		r = 2*r+1;
+	    } else {
+		// We are done
+		r = last;
+	    }
+	}
     }
-    // Node has two children
-    else
-    {
-      if ( heap[r] > heap[ 2*r ] && heap[ 2*r ] <= heap[ 2*r+1 ] )
-      {
-        // Swap with left child
-        qSwap( heap[r], heap[ 2*r ] );
-        r *= 2;
-      }
-      else if ( heap[r] > heap[ 2*r+1 ] && heap[ 2*r+1 ] < heap[ 2*r ] )
-      {
-        // Swap with right child
-        qSwap( heap[r], heap[ 2*r+1 ] );
-        r = 2*r+1;
-      }
-      else
-        // We are done
-        r = last;
-    }
-  }
 }
+
 
 template <class InputIterator, class Value>
 void qHeapSortHelper( InputIterator b, InputIterator e, Value, int n )
 {
-  // Create the heap
-  InputIterator insert = b;
-  Value* realheap = new Value[ n ];
-  // Wow, what a fake. But I want the heap to be indexed as 1...n
-  Value* heap = realheap - 1;
-  int size = 0;
-  for( ; insert != e; ++insert )
-  {
-    heap[++size] = *insert;
-    int i = size;
-    while( i > 1 && heap[i] < heap[ i / 2 ] )
-    {
-      qSwap( heap[i], heap[ i / 2 ] );
-      i /= 2;
+    // Create the heap
+    InputIterator insert = b;
+    Value* realheap = new Value[ n ];
+    // Wow, what a fake. But I want the heap to be indexed as 1...n
+    Value* heap = realheap - 1;
+    int size = 0;
+    for( ; insert != e; ++insert ) {
+	heap[++size] = *insert;
+	int i = size;
+	while( i > 1 && heap[i] < heap[ i / 2 ] ) {
+	    qSwap( heap[i], heap[ i / 2 ] );
+	    i /= 2;
+	}
     }
-  }
 
-  // Now do the sorting
-  for( int i = n; i > 0; i-- )
-  {
-    *b++ = heap[1];
-    if ( i > 1 )
-    {
-      heap[1] = heap[i];
-      qHeapSortPushDown( heap, 1, i - 1 );
+    // Now do the sorting
+    for( int i = n; i > 0; i-- ) {
+	*b++ = heap[1];
+	if ( i > 1 ) {
+	    heap[1] = heap[i];
+	    qHeapSortPushDown( heap, 1, i - 1 );
+	}
     }
-  }
 
-  delete[] realheap;
+    delete[] realheap;
 }
+
 
 template <class InputIterator>
 inline void qHeapSort( InputIterator b, InputIterator e )
@@ -199,12 +184,16 @@ inline void qHeapSort( InputIterator b, InputIterator e )
   // How many entries have to be sorted ?
   InputIterator it = b;
   int n = 0;
-  while ( it != e ) { ++n; ++it; }
+  while ( it != e ) {
+      ++n;
+      ++it;
+  }
 
   // The last parameter is a hack to retrieve the value type
   // Do the real sorting here
   qHeapSortHelper( b, e, *b );
 }
+
 
 template <class Container>
 inline void qHeapSort( Container &c )
