9d6d7bb19765579e181e4c920f0399b6bf637f2d
Paul Olav Tvete <paul@trolltech.com>
1999-06-24 18:54:52 +0100

    Do something by default.
    [imported from CVS by cvs2p4 at 1999/10/31 21:44:45]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 13018]

--- examples/tabdialog/main.cpp
+++ examples/tabdialog/main.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/examples/tabdialog/main.cpp#1 $
+** $Id: //depot/qt/main/examples/tabdialog/main.cpp#2 $
 **
 ** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
 **
@@ -14,18 +14,10 @@
 
 int main( int argc, char **argv )
 {
-    if (argc < 2) {
-        qWarning( "\n\n\n" );
-        qWarning( "--------------------------" );
-        qWarning( "Usage: tabdialog <filename>" );
-        qWarning( "--------------------------" );
-        qWarning( "\n\n\n" );
-        return 0;
-    }
 
     QApplication a( argc, argv );
 
-    TabDialog tabdialog( 0L, "tabdialog", QString( argv[1] ) );
+    TabDialog tabdialog( 0L, "tabdialog", QString( argc < 2 ? "." : argv[1] ) );
     tabdialog.resize( 450, 350 );
     tabdialog.setCaption( "Example for a Tabbed Dialog" );
     a.setMainWidget( &tabdialog );
