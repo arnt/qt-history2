44450e9841cd486c1588d869ec310cc6d8169267
Reginald Stadlbauer <reggie@froglogic.com>
1999-07-28 16:20:20 +0100

    use PaletteBase as background mode
    [imported from CVS by cvs2p4 at 1999/10/31 22:00:51]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 13817]

--- src/widgets/qiconview.cpp
+++ src/widgets/qiconview.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/widgets/qiconview.cpp#68 $
+** $Id: //depot/qt/main/src/widgets/qiconview.cpp#69 $
 **
 ** Definition of QIconView widget class
 **
@@ -1405,7 +1405,7 @@ QIconView::QIconView( QWidget *parent, const char *name )
     setMouseTracking( TRUE );
     viewport()->setMouseTracking( TRUE );
 
-    viewport()->setBackgroundMode( NoBackground );
+    viewport()->setBackgroundMode( PaletteBase );
     viewport()->setFocusProxy( this );
     viewport()->setFocusPolicy( QWidget::WheelFocus );
 }
