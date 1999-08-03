2f89da35a5ab5cfb442ec2bd178ba2dc2b45d305
Matthias Ettrich <ettrich@trolltech.com>
1999-08-03 13:47:05 +0100

    more testing
    [imported from CVS by cvs2p4 at 1999/10/31 22:02:38]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 13919]

--- tests/modal/modal.cpp
+++ tests/modal/modal.cpp
@@ -4,6 +4,9 @@
 int main( int argc, char* argv[]  )
 {
     QApplication a( argc, argv);
-    Dialog d;
-    return d.exec();
+    QLabel l( "Main Application", 0);
+    a.setMainWidget( &l );
+    l.show();
+    ( new Dialog( &l ) )->exec();
+    return a.exec();
 }
