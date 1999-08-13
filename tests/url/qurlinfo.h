4998a4c89c135e106cf984151627c4505ed24f6c
Reginald Stadlbauer <reggie@froglogic.com>
1999-08-13 14:25:37 +0100

    cleanups
    [imported from CVS by cvs2p4 at 1999/10/31 22:05:19]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 14043]

--- tests/url/qurlinfo.h
+++ tests/url/qurlinfo.h
@@ -1,3 +1,28 @@
+/****************************************************************************
+** $Id: //depot/qt/main/tests/url/qurlinfo.h#7 $
+**
+** Implementation of QFileDialog class
+**
+** Created : 950429
+**
+** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
+**
+** This file is part of the Qt GUI Toolkit.
+**
+** This file may be distributed under the terms of the Q Public License
+** as defined by Troll Tech AS of Norway and appearing in the file
+** LICENSE.QPL included in the packaging of this file.
+**
+** Licensees holding valid Qt Professional Edition licenses may use this
+** file in accordance with the Qt Professional Edition License Agreement
+** provided with the Qt Professional Edition.
+**
+** See http://www.troll.no/pricing.html or email sales@troll.no for
+** information about the Professional Edition licensing, or see
+** http://www.troll.no/qpl/ for QPL licensing information.
+**
+*****************************************************************************/
+
 #ifndef QURLINFO_H
 #define QURLINFO_H
 
@@ -30,7 +55,7 @@ public:
     void setOwner( const QString &s );
     void setGroup( const QString &s );
     void setSize( uint s );
-    
+
     QString name() const;
     int permissions() const;
     QString owner() const;
