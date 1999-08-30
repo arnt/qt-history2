a99a5f2bae5db6e8920b3b7c8666dfec584acbca
Haavard Nord <hanord@trolltech.com>
1999-08-30 18:22:38 +0100

    Reverted bad patch for qpngio.cpp
    [imported from CVS by cvs2p4 at 1999/10/31 22:10:55]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 14271]

--- src/kernel/qpngio.cpp
+++ src/kernel/qpngio.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qpngio.cpp#29 $
+** $Id: //depot/qt/main/src/kernel/qpngio.cpp#30 $
 **
 ** Implementation of PNG QImage IOHandler
 **
@@ -164,22 +164,22 @@ void setup_qt( QImage& image, png_structp png_ptr, png_infop info_ptr )
 	png_set_expand(png_ptr);
 
 	if (info_ptr->color_type == PNG_COLOR_TYPE_GRAY ||
-	    info_ptr->color_type == PNG_COLOR_TYPE_GRAY_ALPHA) {
+	    info_ptr->color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+	{
 	    png_set_gray_to_rgb(png_ptr);
 	}
 
 	// Only add filler if no alpha, or we can get 5 channel data.
 	if (!(info_ptr->color_type & PNG_COLOR_MASK_ALPHA)
-	   && !(info_ptr->valid & PNG_INFO_tRNS)) {
+	   && !(info_ptr->valid & PNG_INFO_tRNS))
+	{
 	    png_set_filler(png_ptr, 0xff,
 		QImage::systemByteOrder() == QImage::BigEndian ?
 		    PNG_FILLER_BEFORE : PNG_FILLER_AFTER);
 	    // We want 4 bytes, but it isn't an alpha channel
 	    noalpha = TRUE;
 	}
-	if (info_ptr->color_type == PNG_COLOR_TYPE_RGB_ALPHA) {
-	    png_set_swap_alpha(png_ptr);
-	}
+
 	png_read_update_info(png_ptr, info_ptr);
 	image.create(info_ptr->width,info_ptr->height,32);
     }
