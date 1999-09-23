2053b066ba8ba8338a723ed4929c50e02ff177bc
Paul Olav Tvete <paul@trolltech.com>
1999-09-23 08:36:27 +0100

    Test repeated use.
    [imported from CVS by cvs2p4 at 1999/10/31 22:16:03]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 14580]

--- tests/colorchooser/colorchooser.cpp
+++ tests/colorchooser/colorchooser.cpp
@@ -3,23 +3,32 @@
 #include "qlabel.h"
 #include <qapplication.h>
 
+
+class ColorLabel : public QLabel
+{
+public:
+    ColorLabel() : QLabel( "Click me", 0, "hello" ) {}
+protected:
+    void mouseReleaseEvent( QMouseEvent * )
+    {
+	QColor c = QColorDialog::getColor( backgroundColor() ); 
+	if ( c.isValid() ) 
+            setBackgroundColor( c );    
+    }
+};
+
 main(int argc, char** argv)
 {
     QApplication::setColorSpec( QApplication::ManyColor );
     QApplication app(argc, argv);
     QApplication::setFont( QFont("Helvetica") );
 
-    QColor c = QColorDialog::getColor( Qt::green );
-    if ( c.isValid() ) {
 
-	QLabel l( "\n\nHello, world\n\n\n\n", 0 ); 
-	l.setBackgroundColor( c );
+	ColorLabel l;
 	l.show();
 	   
-	QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
-
-	return app.exec();
-    }
+   QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
+   return app.exec();
 }
 
 
