c5c4302c8f236515721a6185602adf006ff51fcb
Reginald Stadlbauer <reggie@froglogic.com>
1999-09-29 15:16:34 +0100

    qbrowser example with network (HTTP) support
    [imported from CVS by cvs2p4 at 1999/10/31 22:18:19]
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 14706]

---
+++ tests/qbrowser/main.cpp
@@ -0,0 +1,59 @@
+/****************************************************************************
+** $Id: //depot/qt/main/tests/qbrowser/main.cpp#8 $
+**
+** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#include <qapplication.h>
+#include <qwindowsstyle.h>
+#include <qstylesheet.h>
+#include "helpwindow.h"
+#include <stdlib.h>
+#include <qnetwork.h>
+
+int main( int argc, char ** argv )
+{
+    QApplication::setColorSpec( QApplication::ManyColor );
+    QApplication::setDesktopSettingsAware( FALSE );
+    QApplication a(argc, argv);
+    
+    qInitNetworkProtocols();
+    
+    QStyleSheetItem* style;
+
+    // Modify the application-wide default style sheet to handle
+    // some extra HTML gracefully.
+    //
+    // Ignore any bodytext in <head>...</head>:
+    style = new QStyleSheetItem( QStyleSheet::defaultSheet(), "head" );
+    style->setDisplayMode(QStyleSheetItem::DisplayNone);
+    //
+    // Not in default style sheet, just fake it:
+    style = new QStyleSheetItem( QStyleSheet::defaultSheet(), "dl" );
+    style->setDisplayMode(QStyleSheetItem::DisplayBlock);
+    style = new QStyleSheetItem( QStyleSheet::defaultSheet(), "dt" );
+    style->setDisplayMode(QStyleSheetItem::DisplayBlock);
+    style->setContexts("dl");
+    //
+    // Many HTML files omit the </p> or </li>, so we add this for efficiency:
+    QStyleSheet::defaultSheet()->item("p")->setSelfNesting( FALSE );
+    QStyleSheet::defaultSheet()->item("li")->setSelfNesting( FALSE );
+
+    QString home;
+
+    if (argc > 1)
+        home = argv[1];
+    else
+        home = QString(getenv("QTDIR")) + "/doc/html/index.html";
+
+    ( new HelpWindow(home, ".", 0, "qbrowser") )->show();
+
+    QObject::connect( &a, SIGNAL(lastWindowClosed()),
+                      &a, SLOT(quit()) );
+
+    return a.exec();
+}
---
+++ tests/qbrowser/qbrowser.pro
@@ -0,0 +1,8 @@
+TEMPLATE	= app
+CONFIG		= qt warn_on release
+HEADERS		= helpwindow.h
+SOURCES		= helpwindow.cpp \
+		  main.cpp
+TARGET		= qbrowser
+LIBS		= -lqnetwork
+INCLUDEPATH	= $(QTDIR)/extensions/network/src
