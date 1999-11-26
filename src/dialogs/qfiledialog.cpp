3200a46873d669b2eb5eb58c9af3cc6ba5427396
Matthias Ettrich <ettrich@trolltech.com>
1999-11-26 19:19:37 +0100

    shouldn't have been gone in
    [git-p4: depot-paths = "//depot/qt/main/": change = 15582]

--- src/dialogs/qfiledialog.cpp
+++ src/dialogs/qfiledialog.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/dialogs/qfiledialog.cpp#377 $
+** $Id: //depot/qt/main/src/dialogs/qfiledialog.cpp#378 $
 **
 ** Implementation of QFileDialog class
 **
@@ -2371,7 +2371,6 @@ QString QFileDialog::getOpenFileName( const QString & startWith,
     QFileDialog *dlg = new QFileDialog( *workingDirectory, QString::null,
 					parent, name, TRUE );
     CHECK_PTR( dlg );
-    dlg->clearWState( WState_Modal );
     dlg->setCaption( QFileDialog::tr( "Open" ) );
     dlg->setFilters( filters );
     dlg->setMode( QFileDialog::ExistingFile );
