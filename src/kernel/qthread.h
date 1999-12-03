544545014942a0b77aee9fc4de883db671eb7a88
Jo Dillon <jo@trolltech.com>
1999-12-03 11:13:03 +0100

    Added license header, removed #warning from header file. Oopsie.
    [git-p4: depot-paths = "//depot/qt/main/": change = 15707]

--- src/kernel/qthread.h
+++ src/kernel/qthread.h
@@ -1,3 +1,29 @@
+/****************************************************************************
+** $Id: //depot/qt/main/src/kernel/qthread.h#16 $
+**
+** Definition of QThread class
+**
+** Created : 931107
+**
+** Copyright (C) 1992-1999 Troll Tech AS.  All rights reserved.
+**
+** This file is part of the Qt GUI Toolkit.
+**
+** This file may be distributed under the terms of the Q Public License
+** as defined by Troll Tech AS of Norway and appearing in the file
+** LICENSE.QPL included in the packaging of this file.
+**
+** Licensees holding valid Qt Professional Edition licenses may use this
+** file in accordance with the Qt Professional Edition License Agreement
+** provided with the Qt Professional Edition.
+**
+** See http://www.troll.no/pricing.html or email sales@troll.no for
+** information about the Professional Edition licensing, or see
+** http://www.troll.no/qpl/ for QPL licensing information.
+**
+*****************************************************************************/
+
+
 #ifndef QTHREAD_H
 #define QTHREAD_H
 
@@ -13,8 +39,6 @@ class QMutex {
 
 #ifdef UNIX
   pthread_mutex_t mymutex;
-#else
-#warning Eek,no mutex!
 #endif
 
 public:
