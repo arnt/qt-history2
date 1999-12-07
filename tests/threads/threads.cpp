1eb678f16a6d1ada9dc2b526633eef765b3089b2
Jo Dillon <jo@trolltech.com>
1999-12-07 16:46:29 +0100

    Extended test program. Everything seems to work.
    [git-p4: depot-paths = "//depot/qt/main/": change = 15831]

--- tests/threads/threads.cpp
+++ tests/threads/threads.cpp
@@ -13,8 +13,9 @@ public:
 protected:
 
     void customEvent(QCustomEvent * c) {
-      qDebug("Got customevent! %d %d",c->type(),QThread::currentThread());
-      setText(QString::number((int)c->data()));
+      QString * s=(QString *)c->data();
+      setText(*s);
+      delete s;
     }
 
 };
@@ -22,22 +23,84 @@ protected:
 MyWidget * mywidget;
 
 class MyThread : public QThread {
+
+    QString myname;
+    int pause;
     
 public:
-    
+
+    MyThread(QString s,int p) { myname=s; pause=p; }
     virtual void run();
     
 };
 
+class WaitThread : public QThread {
+    
+    QThread * thread1;
+    QThread * thread2;
+    QThreadEvent * done_event;
+    
+public:
+    
+    WaitThread(QThread *,QThread *,QThreadEvent *);
+    virtual void run();
+    
+};
+
+class FinalThread : public QThread {
+    
+    QThreadEvent * done_event;
+    
+public:
+    
+    FinalThread(QThreadEvent * t) { done_event=t; }
+    virtual void run();
+    
+};
+
+WaitThread::WaitThread(QThread * a,QThread * b,QThreadEvent * c)
+{
+  thread1=a;
+  thread2=b;
+  done_event=c;
+}
+
 void MyThread::run()
 {
     int n=0;
-    while(1) {
+    while(n<5) {
 	QCustomEvent * wibble=new QCustomEvent(6666);
-	wibble->setData((void *)n);
+	QString * message=new QString();
+	*message=myname+" ";
+	*message=*message+QString::number(n);
+	wibble->setData((void *)message);
 	QThread::postEvent(mywidget,wibble);
 	n++;
+	sleep(pause);
     }
+}
+
+void WaitThread::run()
+{
+    thread1->wait();
+    thread2->wait();
+    QString * message=new QString;
+    *message="First two threads done!";
+    QCustomEvent * wibble=new QCustomEvent(6666);
+    wibble->setData((void *)message);
+    QThread::postEvent(mywidget,wibble);
+    sleep(1);
+    done_event->wakeOne();
+}
+
+void FinalThread::run()
+{
+    done_event->wait();
+    QString * message=new QString;
+    *message="Final message!";
+    QCustomEvent * wibble=new QCustomEvent(6666);
+    wibble->setData((void *)message);
+    QThread::postEvent(mywidget,wibble);
 }
 
 int main(int argc,char ** argv)
@@ -45,8 +108,14 @@ int main(int argc,char ** argv)
   QApplication app(argc,argv);
   mywidget=new MyWidget();
   mywidget->show();
-  qDebug("Main thread is %d",QThread::currentThread());
-  MyThread foo;
+  MyThread foo("Thread one",2);
+  MyThread bar("Thread two",3);
+  QThreadEvent qte;
+  WaitThread frobnitz(&foo,&bar,&qte);
+  FinalThread fooble(&qte);
   foo.start();
+  bar.start();
+  frobnitz.start();
+  fooble.start();
   app.exec();
 }
