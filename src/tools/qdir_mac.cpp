62da5a9909009cd6d92fc7702e3a6c241a92658f
Jo Dillon <jo@trolltech.com>
1999-12-13 17:57:33 +0100

    Some Mac directory support
    [git-p4: depot-paths = "//depot/qt/main/": change = 15931]

--- src/tools/qdir_mac.cpp
+++ src/tools/qdir_mac.cpp
@@ -38,7 +38,12 @@ bool QDir::isReadable() const
 
 bool QDir::isRoot() const
 {
-    return false;
+    // Rather simple-minded but should work with an absolute path
+    // not containing the equivalent of '/../'
+    if(dPath.contains(':')) {
+	return false;
+    }
+    return true;
 }
 
 bool QDir::rename(const QString& name,const QString& newName,
@@ -75,5 +80,32 @@ bool QDir::readDirEntries(const QString& nameFilter,int filterSpec,
 
 const QFileInfoList * QDir::drives()
 {
+    QFileInfoList * qfl=new QFileInfoList();
+    QElemPtr drivep;
+    QHdrPtr headerp;
+    headerp=GetDrvQHdr();
+    drivep=headerp->qHead;
+    char somebuf[257];
+    int refnum;
+    int freebytes;
+    while(drivep) {
+	DrvQEl * el=(DrvQEl *)drivep;
+	drivep=el->qLink;
+	int drivenum=el->dQDrive;
+	int driveref=el->dQRefNum;
+	int driveid=el->dQFSID;
+	refnum=driveref;
+	OSErr ret=GetVInfo(drivenum,somebuf,&refnum,&freebytes);
+	if(ret!=noErr) {
+	    if(ret==nsvErr) {
+		qWarning("QDir::drives, no such volume");
+	    } else {
+		qWarning("QDir::drives unknown error");
+	    }
+	}
+	somebuf[somebuf[0]+1]=0;
+	QString * widg=new QString(&somebuf[1]);
+	qfl->append(widg);
+    }
     return 0;
 }
