94f96f5f633648076b2a13362157ef2b98b1eb28
Warwick Allison <warwick@trolltech.com>
2000-01-30 03:42:47 +0100

    Example server socket.
    [git-p4: depot-paths = "//depot/qt/main/": change = 17039]

---
+++ extensions/network/examples/httpd/httpd.cpp
@@ -0,0 +1,55 @@
+#include <qserversocket.h>
+#include <qapplication.h>
+#include <qmainwindow.h>
+#include <qtextstream.h>
+
+class HttpDaemon : public QServerSocket {
+    Q_OBJECT
+public:
+    HttpDaemon( QObject* parent=0 ) :
+	QServerSocket(8080,0,parent)
+    {
+	if ( !ok() ) {
+	    qWarning("Failed to bind to port 8080");
+	    exit(1);
+	}
+    }
+
+    void newConnection( int socket )
+    {
+	qDebug("new connection");
+	QSocket* s = new QSocket(this);
+	connect(s,SIGNAL(readyRead()),this,SLOT(readClient()));
+	connect(s,SIGNAL(delayedCloseFinished()),this,SLOT(discardClient()));
+	s->setSocket(socket);
+	s->setMode(QSocket::Ascii);
+    }
+
+private slots:
+    void readClient()
+    {
+	QSocket* socket = (QSocket*)sender();
+	if (socket->canReadLine()) {
+	    QStringList tokens = QStringList::split(QRegExp("[ \n\r][ \n\r]*"),socket->readLine());
+	    if ( tokens[0] == "GET" ) {
+		QTextStream os(socket);
+		os << "<h1>Nothing to see here</h1>";
+	    }
+	    socket->close();
+	}
+    }
+    void discardClient()
+    {
+	QSocket* socket = (QSocket*)sender();
+	delete socket;
+    }
+};
+
+main(int argc, char** argv)
+{
+    QApplication app(argc,argv);
+    HttpDaemon httpd;
+    return app.exec();
+}
+
+#include "httpd.moc"
---
+++ extensions/network/examples/httpd/httpd.pro
@@ -0,0 +1,7 @@
+TEMPLATE	= app
+CONFIG		= qt warn_on release
+TMAKE_CXXFLAGS  = -I../../src
+TMAKE_LIBS      = -lqnetwork
+HEADERS		= 
+SOURCES		= httpd.cpp
+TARGET		= httpd
