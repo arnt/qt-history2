3adafbd0fd17e6f177319aeb124ad4b209898e37
Matthias Ettrich <ettrich@trolltech.com>
2000-02-02 17:06:45 +0100

    more renaming
    [git-p4: depot-paths = "//depot/qt/main/": change = 17183]

---
+++ examples/fileiconview/mainwindow.h
@@ -0,0 +1,55 @@
+/****************************************************************************
+** $Id: //depot/qt/main/examples/qfileiconview/mainwindow.h#4 $
+**
+** Copyright (C) 1992-2000 Troll Tech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#ifndef MAINWIN_H
+#define MAINWIN_H
+
+#include <qmainwindow.h>
+
+class QtFileIconView;
+class DirectoryView;
+class QProgressBar;
+class QLabel;
+class QComboBox;
+
+class FileMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    FileMainWindow();
+
+    QtFileIconView *fileView() { return fileview; }
+    DirectoryView *dirList() { return dirlist; }
+
+    void show();
+
+protected:
+    void setup();
+    void setPathCombo();
+
+    QtFileIconView *fileview;
+    DirectoryView *dirlist;
+    QProgressBar *progress;
+    QLabel *label;
+    QComboBox *pathCombo;
+
+protected slots:
+    void directoryChanged( const QString & );
+    void slotStartReadDir( int dirs );
+    void slotReadNextDir();
+    void slotReadDirDone();
+    void cdUp();
+    void newFolder();
+    void changePath( const QString &path );
+
+};
+
+#endif
---
+++ examples/iconview/qiconview.pro
@@ -0,0 +1,5 @@
+TEMPLATE	= app
+CONFIG		= qt warn_on release
+SOURCES		= main.cpp
+TARGET		= qiconview
+DEPENDPATH=../../include
