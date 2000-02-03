4421529bb5aa5f10a6d3df3ad47297eb71006df5
Warwick Allison <warwick@trolltech.com>
2000-02-03 06:38:03 +0100

    Extend.
    [git-p4: depot-paths = "//depot/qt/main/": change = 17241]

--- extensions/network/examples/load/load.cpp
+++ extensions/network/examples/load/load.cpp
@@ -6,12 +6,22 @@
 
 #include "load.h"
 
+#define MAXCHUNKS 8
+
 class LoadData {
     QArray<int> ins,outs;
+    struct {
+	int start,end;
+	int total;
+    } chunk[MAXCHUNKS];
     Load* widget;
     QLabel* label;
     int current;
     int max;
+    int nchunk;
+    int chunk_start; // When did this chunk start?
+    int chunk_total; // Total bytes in this chunk
+    int chunk_baseline; // What is considered "silence"? (an average)
 
 private:
     void rescale(int need)
@@ -33,6 +43,7 @@ public:
 	current(0),
 	max(1)
     {
+	nchunk = chunk_baseline = chunk_start = chunk_total = 0;
 	label->setAutoMask(TRUE);
 	setWidth(1);
 	addDataPoint(0,0);
@@ -49,19 +60,65 @@ public:
 	setLabel();
     }
 
-    void setLabel()
+    QString numberAbbrev(int n) const
     {
 	QString s;
-	if ( max > 1024 ) {
-	    s.setNum(max/1024);
-	    s += "K";
+	int f=1;
+	QString suffix="";
+	if ( n > 1024*1024 ) {
+	    f=1024*1024;
+	    suffix="M";
+	} else if ( n > 1024 ) {
+	    f=1024;
+	    suffix="K";
 	} else {
-	    s.setNum(max);
+	    s.setNum(n);
+	    return s;
 	}
-	label->setText(s);
+	int n10 = n/(f/10);
+	s.setNum(n10/10);
+	s += ".";
+	s += QString::number(n10%10);
+	s += suffix;
+	return s;
+    }
+
+    void setLabel()
+    {
+	label->setText(numberAbbrev(max));
 	QSize sh = label->sizeHint();
 	label->setGeometry(widget->width()-sh.width()-10,
 		0, sh.width(), sh.height());
+    }
+
+    QRect chunkRect(int i)
+    {
+	int x = (ins.size()-chunk[i].start+current)%ins.size();
+	return QRect(x,widget->height()-30,
+	    (chunk[i].end-chunk[i].start+ins.size())%ins.size(),20);
+    }
+
+    void addChunk(int start, int end, int total)
+    {
+	int i=0;
+	QRegion r;
+	if ( nchunk == MAXCHUNKS ) {
+	    int smallest=0;
+	    for (int j=0; j<nchunk; j++) {
+		if ( chunk[j].total <= smallest ) {
+		    i = j;
+		    smallest = chunk[j].total;
+		}
+	    }
+	    r |= chunkRect(i);
+	} else {
+	    i = ++nchunk;
+	}
+	chunk[i].start = start;
+	chunk[i].end = end;
+	chunk[i].total = total;
+	r |= chunkRect(i);
+	widget->repaint(r);
     }
 
     void addDataPoint(int in, int out)
@@ -69,6 +126,19 @@ public:
 	ins[current]=in;
 	outs[current]=out;
 	current=(current+1)%ins.size();
+	/*
+	if ( in+out > chunk_baseline ) {
+	    if ( !chunk_total )
+		chunk_start = current;
+	    chunk_total += in+out;
+	} else {
+	    if ( chunk_total > chunk_baseline*4 ) {
+		addChunk(chunk_start,current,chunk_total);
+	    }
+	    chunk_total = 0;
+	}
+	chunk_baseline = (chunk_baseline+in+out)*15/16;
+	*/
 
 	if (in+out > max) {
 	    rescale(in+out);
@@ -93,6 +163,13 @@ public:
 	    p.lineTo(x,h-(in+out)*h/max);
 	    p.setPen(Qt::white);
 	    p.lineTo(x,0);
+	}
+	p.setPen(Qt::black);
+	for (int j=0; j<nchunk; j++) {
+	    QRect r = chunkRect(j);
+	    if ( r.intersects(QRect(QPoint(from,r.y()),QPoint(to,r.y()))) ) {
+		p.drawText(r,Qt::AlignCenter,numberAbbrev(chunk[j].total));
+	    }
 	}
     }
 };
