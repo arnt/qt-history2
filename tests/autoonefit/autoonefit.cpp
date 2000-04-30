237dbf4e751d46b304415e00ae17d5d8f32efef6
Dimitri Papadopoulos <dimitri@trolltech.com>
2000-04-30 21:33:30 +0100

    More debugging stuff in example for AutoOneFit.
    [git-p4: depot-paths = "//depot/qt/main/": change = 20151]

--- tests/autoonefit/autoonefit.cpp
+++ tests/autoonefit/autoonefit.cpp
@@ -3,10 +3,13 @@
 #include <qframe.h>
 #include <qlayout.h>
 #include <qlineedit.h>
+#include <qvbox.h>
+#include <qvbuttongroup.h>
+#include <qradiobutton.h>
 
-class Bazar : public QFrame {
+class Fit : public QFrame {
 public:
-	Bazar( QWidget* parent=0, const char* name=0 );
+	Fit( QWidget* parent=0, const char* name=0 );
 	QSize sizeHint() const;
 	QSize minimumSizeHint() const;
 	QSize minimumSize() const;
@@ -14,7 +17,7 @@ public:
 	void resize( int w, int h );
 };
 
-Bazar::Bazar( QWidget* parent, const char* name ) : QFrame( parent, name ) {
+Fit::Fit( QWidget* parent, const char* name ) : QFrame( parent, name ) {
 	QBoxLayout *layout = new QVBoxLayout( this );
 	for ( int i = 0; i < 100; ++i ) {
 		QString contents;
@@ -25,40 +28,156 @@ Bazar::Bazar( QWidget* parent, const char* name ) : QFrame( parent, name ) {
 	}
 }
 
-QSize Bazar::sizeHint() const {
-	qDebug( "Bazar::sizeHint()" );
-	return QFrame::sizeHint();
+QSize Fit::sizeHint() const {
+	QSize s = QFrame::sizeHint();
+	qDebug( "Fit::sizeHint() = %d x %d", s.width(), s.height() );
+	return s;
 }
 
-QSize Bazar::minimumSizeHint() const {
-	qDebug( "Bazar::minimumSizeHint()" );
-	return QFrame::minimumSizeHint();
+QSize Fit::minimumSizeHint() const {
+	QSize s = QFrame::minimumSizeHint();
+	qDebug( "Fit::minimumSizeHint() = %d x %d", s.width(), s.height() );
+	return s;
 }
 
-QSize Bazar::minimumSize() const {
-	qDebug( "Bazar::minimumSize()" );
-	return QFrame::minimumSize();
+QSize Fit::minimumSize() const {
+	QSize s = QFrame::minimumSize();
+	qDebug( "Fit::minimumSize() = %d x %d", s.width(), s.height() );
+	return s;
 }
 
-QSize Bazar::maximumSize() const {
-	qDebug( "Bazar::maximumSize()" );
-	return QFrame::maximumSize();
+QSize Fit::maximumSize() const {
+	QSize s = QFrame::maximumSize();
+	qDebug( "Fit::maximumSize() = %d x %d", s.width(), s.height() );
+	return s;
 }
 
-void Bazar::resize( int w, int h ) {
-	qDebug( "Bazar::resize( %d, %d )", w, h );
+void Fit::resize( int w, int h ) {
+	qDebug( "Fit::resize( %d, %d )", w, h );
 	return QFrame::resize( w, h );
 }
+
+
+class MyScrollView : public QScrollView {
+	Q_OBJECT
+public:
+	MyScrollView( QWidget* parent=0, const char* name=0, WFlags f=0 );
+	~MyScrollView();
+	void setResizePolicy( ResizePolicy r );
+	void addChild( QWidget* child, int x=0, int y=0 );
+	void moveChild( QWidget* child, int x=0, int y=0 );
+	void resizeEvent( QResizeEvent* e );
+	bool eventFilter( QObject *, QEvent *e );
+	void show();
+
+public slots:
+	void resizeContents( int w, int h );
+    	void setContentsPos( int x, int y );
+	void setResizePolicySlot( int id );
+};
+
+MyScrollView::MyScrollView( QWidget* parent, const char* name, WFlags f )
+	: QScrollView( parent, name, f ) {
+}
+
+MyScrollView::~MyScrollView() {
+}
+
+void MyScrollView::setResizePolicy( ResizePolicy r ) {
+	const char* s;
+	switch ( r ) {
+	case Default:
+		s = "Default";
+		break;
+	case Manual:
+		s = "Manual";
+		break;
+	case AutoOne:
+		s = "AutoOne";
+		break;
+	case AutoOneFit:
+		s = "AutoOneFit";
+		break;
+	};
+	qDebug( "QScrollView::setResizePolicy( %s )", s );
+	QScrollView::setResizePolicy( r );
+}
+
+void MyScrollView::addChild( QWidget* child, int x=0, int y=0 ) {
+	qDebug( "MyScrollView::addChild( %d, %d )", x, y );
+	QScrollView::addChild( child, x, y );
+}
+
+void MyScrollView::moveChild( QWidget* child, int x=0, int y=0 ) {
+	qDebug( "MyScrollView::moveChild( %d, %d )", x, y );
+	QScrollView::moveChild( child, x, y );
+}
+
+void MyScrollView::resizeEvent( QResizeEvent* e ) {
+	qDebug( "MyScrollView::resizeEvent()" );
+	QScrollView::resizeEvent( e );
+}
+
+bool MyScrollView::eventFilter( QObject* obj, QEvent* e ) {
+	if ( e->type() == QEvent::LayoutHint )
+		qDebug( "MyScrollView::eventFilter( QEvent::LayoutHint )" );
+	return QScrollView::eventFilter( obj, e );
+}
+
+void MyScrollView::show() {
+	qDebug( "MyScrollView::show()" );
+	QScrollView::show();
+}
+
+void MyScrollView::resizeContents( int w, int h ) {
+	qDebug( "MyScrollView::resizeContents( %d, %d )", w, h );
+	QScrollView::resizeContents( w, h );
+}
+
+void MyScrollView::setContentsPos( int x, int y ) {
+	qDebug( "MyScrollView::setContentsPos( %d, %d )", x, y );
+	QScrollView::setContentsPos( x, y );
+}
+
+void MyScrollView::setResizePolicySlot( int id ) {
+	switch ( id ) {
+	case 0:
+		setResizePolicy( AutoOne );
+		break;
+	case 1:
+		setResizePolicy( AutoOneFit );
+		break;
+	default:
+		qFatal( "internal error in %s:%d", __FILE__, __LINE__ );
+	};
+}
+
+
+#include "autoonefit.moc"
+
 
 int main( int argc, char* argv[] )
 {
 	QApplication application( argc, argv );
-	QScrollView* scrollview = new QScrollView( 0 );
-	Bazar* bazar = new Bazar( scrollview->viewport() );
-	// scrollview->setResizePolicy( QScrollView::AutoOne );
-	scrollview->setResizePolicy( QScrollView::AutoOneFit );
-	scrollview->addChild( bazar );
-	application.setMainWidget( scrollview );
-	scrollview->show();
+
+	MyScrollView* sv = new MyScrollView( 0, "sv" );
+	Fit* fit = new Fit( sv->viewport(), "fit" );
+	sv->setResizePolicy( QScrollView::AutoOne );
+	//sv->setResizePolicy( QScrollView::AutoOneFit );
+	sv->addChild( fit );
+	sv->show();
+
+	QButtonGroup* bg = new QVButtonGroup( "Resize policy:", 0, "bg" );
+	bg->resize( 200, 200 );
+	QRadioButton* b1 = new QRadioButton( "AutoOne", bg, "autoone" );
+	QRadioButton* b2 = new QRadioButton( "AutoOneFit", bg, "autoonefit" );
+	QObject::connect( bg, SIGNAL(clicked(int)),
+		sv, SLOT(setResizePolicySlot(int)) );
+//	sv->setResizePolicySlot(0);
+	b1->setChecked( TRUE );
+	bg->resize( bg->sizeHint() );
+	bg->show();
+
+	application.setMainWidget( sv );
 	return application.exec();
 }
