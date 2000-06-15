7076415bb5ec21c34b847ff1bc7d1c966bf6f588
Warwick Allison <warwick@trolltech.com>
2000-06-15 02:03:11 +0100

    update
    [git-p4: depot-paths = "//depot/qt/main/": change = 22135]

--- src/tools/qfeatures-minimal.h
+++ src/tools/qfeatures-minimal.h
@@ -43,99 +43,99 @@
 */
 
 // images
-#define QT_FEATURE_IMAGEIO_BMP
-#define QT_FEATURE_IMAGEIO_PPM
-#define QT_FEATURE_IMAGEIO_XBM
-#define QT_FEATURE_IMAGEIO_XPM
-#define QT_FEATURE_IMAGEIO_PNG
-#define QT_FEATURE_IMAGEIO_JPEG // currently also requires QT_JPEG_SUPPORT
+//#define QT_FEATURE_IMAGEIO_BMP
+//#define QT_FEATURE_IMAGEIO_PPM
+//#define QT_FEATURE_IMAGEIO_XBM
+//#define QT_FEATURE_IMAGEIO_XPM
+//#define QT_FEATURE_IMAGEIO_PNG
+//#define QT_FEATURE_IMAGEIO_JPEG // currently also requires QT_JPEG_SUPPORT
 
-#define QT_FEATURE_ASYNC_IO
-#define QT_FEATURE_ASYNC_IMAGE_IO
+//#define QT_FEATURE_ASYNC_IO
+//#define QT_FEATURE_ASYNC_IMAGE_IO
 #if defined(QT_FEATURE_ASYNC_IO) && defined(QT_FEATURE_ASYNC_IMAGE_IO)
     #define QT_FEATURE_MOVIE
 #endif
 
 // fonts
-#define QT_FEATURE_TRUETYPE
-#define QT_FEATURE_BDF
-#define QT_FEATURE_FONTDATABASE
+//#define QT_FEATURE_TRUETYPE
+//#define QT_FEATURE_BDF
+//#define QT_FEATURE_FONTDATABASE
 
 // i18n
 
 #if !defined(QT_NO_CODECS)
-    #define QT_FEATURE_I18N
+    //#define QT_FEATURE_I18N
 #endif
 
 #if !defined(QT_LITE_UNICODE)
-    #define QT_FEATURE_UNICODETABLES
+    //#define QT_FEATURE_UNICODETABLES
 #endif
 
 // misc
-#define QT_FEATURE_MIME
+//#define QT_FEATURE_MIME
 #if defined(QT_FEATURE_MIME)
     #define QT_FEATURE_RICHTEXT
-    #define QT_FEATURE_DRAGANDDROP
-    #define QT_FEATURE_CLIPBOARD
+    //#define QT_FEATURE_DRAGANDDROP
+    //#define QT_FEATURE_CLIPBOARD
 #endif
 
-#define QT_FEATURE_SOUND
+//#define QT_FEATURE_SOUND
 
-#define QT_FEATURE_PROPERTIES
+//#define QT_FEATURE_PROPERTIES
 
 #if defined(_WS_QWS_)
-    #define QT_FEATURE_QWS_CURSOR
+    //#define QT_FEATURE_QWS_CURSOR
     //#define QT_FEATURE_QWS_MACH64
-    #define QT_FEATURE_QWS_VFB
+    //#define QT_FEATURE_QWS_VFB
     //#define QT_FEATURE_QWS_DEPTH_8GRAYSCALE
     #define QT_FEATURE_QWS_DEPTH_8
     //#define QT_FEATURE_QWS_DEPTH_15
-    #define QT_FEATURE_QWS_DEPTH_16
-    #define QT_FEATURE_QWS_DEPTH_32
+    //#define QT_FEATURE_QWS_DEPTH_16
+    //#define QT_FEATURE_QWS_DEPTH_32
 #endif
 
 // network
-#define QT_FEATURE_NETWORKPROTOCOL
+//#define QT_FEATURE_NETWORKPROTOCOL
 #if defined(QT_FEATURE_NETWORKPROTOCOL)
-    #define QT_FEATURE_NETWORKPROTOCOL_FTP
-    #define QT_FEATURE_NETWORKPROTOCOL_HTTP
+    //#define QT_FEATURE_NETWORKPROTOCOL_FTP
+    //#define QT_FEATURE_NETWORKPROTOCOL_HTTP
 #endif
 
 // painting
-#define QT_FEATURE_COLORNAMES
-#define QT_FEATURE_TRANSFORMATIONS
+//#define QT_FEATURE_COLORNAMES
+//#define QT_FEATURE_TRANSFORMATIONS
 
-#define QT_FEATURE_PSPRINTER
+//#define QT_FEATURE_PSPRINTER
 #if defined(_WS_WIN_) || defined(QT_FEATURE_PSPRINTER)
     #define QT_FEATURE_PRINTER
 #endif
 
-#define QT_FEATURE_PICTURE
+//#define QT_FEATURE_PICTURE
 
 // widgets
-#define QT_FEATURE_WIDGETS
+//#define QT_FEATURE_WIDGETS
 
 #if defined(QT_FEATURE_WIDGETS)
     #if defined(QT_FEATURE_RICHTEXT)
-	#define QT_FEATURE_TEXTVIEW
+	//#define QT_FEATURE_TEXTVIEW
 	#if defined(QT_FEATURE_TEXTVIEW)
 	    #define QT_FEATURE_TEXTBROWSER
 	#endif
     #endif
     #if defined(QT_FEATURE_DRAGANDDROP)
-	#define QT_FEATURE_ICONVIEW
+	//#define QT_FEATURE_ICONVIEW
     #endif
     #define QT_FEATURE_LISTVIEW
-    #define QT_FEATURE_CANVAS
-    #define QT_FEATURE_DIAL
-    #define QT_FEATURE_WORKSPACE
-    #define QT_FEATURE_LCDNUMBER
-    #define QT_FEATURE_ACTION
+    //#define QT_FEATURE_CANVAS
+    //#define QT_FEATURE_DIAL
+    //#define QT_FEATURE_WORKSPACE
+    //#define QT_FEATURE_LCDNUMBER
+    //#define QT_FEATURE_ACTION
 
     // styles
     #define QT_FEATURE_STYLE_WINDOWS
-    #define QT_FEATURE_STYLE_MOTIF
-    #define QT_FEATURE_STYLE_PLATINUM
+    //#define QT_FEATURE_STYLE_MOTIF
+    //#define QT_FEATURE_STYLE_PLATINUM
     #if defined(QT_FEATURE_STYLE_MOTIF)
 	#define QT_FEATURE_STYLE_CDE
 	#define QT_FEATURE_STYLE_SGI
@@ -145,20 +145,20 @@
     #define QT_FEATURE_DIALOGS
     #if defined(QT_FEATURE_DIALOGS)
 	#if defined(QT_FEATURE_LISTVIEW) && defined(QT_FEATURE_NETWORKPROTOCOL)
-	    #define QT_FEATURE_FILEDIALOG
+	    //#define QT_FEATURE_FILEDIALOG
 	#endif
 	#if defined(QT_FEATURE_FONTDATABASE)
-	    #define QT_FEATURE_FONTDIALOG
+	    //#define QT_FEATURE_FONTDIALOG
 	#endif
-	#define QT_FEATURE_COLORDIALOG
+	//#define QT_FEATURE_COLORDIALOG
 	#if defined(QT_FEATURE_LISTVIEW) && defined(QT_FEATURE_PRINTER)
-	    #define QT_FEATURE_PRINTDIALOG
+	    //#define QT_FEATURE_PRINTDIALOG
 	#endif
 	#define QT_FEATURE_INPUTDIALOG
 	#define QT_FEATURE_MESSAGEBOX
-	#define QT_FEATURE_PROGRESSDIALOG
+	//#define QT_FEATURE_PROGRESSDIALOG
 	#define QT_FEATURE_TABDIALOG
-	#define QT_FEATURE_WIZARD
+	//#define QT_FEATURE_WIZARD
     #endif
 #endif
 
