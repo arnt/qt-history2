9acc77092d47393e42a518273554df395e4ba4ff
Warwick Allison <warwick@trolltech.com>
2000-06-26 12:08:54 +0100

    No MNG by default.
    [git-p4: depot-paths = "//depot/qt/main/": change = 22523]

--- src/tools/qconfig.h
+++ src/tools/qconfig.h
@@ -1,5 +1,8 @@
 // Empty leaves all features enabled.  See doc/html/features.html for choices.
-// Note that disabling features will produce a libqt that is not compatible
-// with other libqt builds. Such modifications are only supported on
-// Qt/Embedded where reducing the library size is important and where the
-// application-suite is often a fixed set.
+
+// Note that disabling some features will produce a libqt that is not
+// compatible with other libqt builds. Such modifications are only
+// supported on Qt/Embedded where reducing the library size is important
+// and where the application-suite is often a fixed set.
+
+#define QT_NO_IMAGEIO_MNG // not defined by default.
