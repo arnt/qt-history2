217a6d74391e31bd8cd15b6c8c77574f82ef6cb2
Volker Hilsheimer <vohi@trolltech.com>
2000-07-12 17:30:35 +0100

    Fixed placement of  The Indicator, formerly known as Radiobutton
    [git-p4: depot-paths = "//depot/qt/main/": change = 22895]

--- src/widgets/qsgistyle.cpp
+++ src/widgets/qsgistyle.cpp
@@ -519,39 +519,39 @@ QSGIStyle::exclusiveIndicatorSize() const
 */
 void QSGIStyle::drawExclusiveIndicator( QPainter* p,
 					int x, int y, int w, int h, const QColorGroup &g,
-					bool on, bool down, bool /* enabled */)
+					bool on, bool down, bool enabled )
 {
     p->eraseRect( x, y, w, h );
 
     p->setPen( g.button() );
     p->setBrush( g.button() );
     QPointArray a;
-    a.setPoints( 4, 5,0, 11,6, 6,11, 0,5);
+    a.setPoints( 4, 5,4, 11,10, 6,15, 0,9);
     p->drawPolygon( a );
 
     p->setPen( g.dark() );
-    p->drawLine( 0,5, 5,0 );
-    p->drawLine( 6,0, 11,5 );
+    p->drawLine( 0,9, 5,4 );
+    p->drawLine( 6,4, 11,9 );
     p->setPen( down ? g.light() : g.dark().dark(200) );
-    p->drawLine( 11,6, 6,11 );
-    p->drawLine( 5,11, 0,6 );
-    p->drawLine( 2,7, 5,10 );
-    p->drawLine( 6,10, 9,7 );
+    p->drawLine( 11,10, 6,15 );
+    p->drawLine( 5,15, 0,10 );
+    p->drawLine( 2,11, 5,14 );
+    p->drawLine( 6,14, 9,11 );
     p->setPen( g.light() );
-    p->drawLine( 2,5, 5,2 );
+    p->drawLine( 2,9, 5,6 );
 
     if (on) {
-	p->setPen( QColor(0,0,255) );
-	p->setBrush( QColor(0,0,255) );
+	p->setPen( enabled ? blue : darkGray );
+	p->setBrush( enabled ? blue : darkGray  );
 	a.setPoints(3, 6,2, 8,4, 6,6 );
 	a.translate(x,y);
 	p->drawPolygon( a );
 	p->setBrush( NoBrush );
 	
 	p->setPen( g.shadow() );
-	p->drawLine( 7,7, 9,5 );
+	p->drawLine( 7,11, 9,9 );
     } else {
-	p->drawLine( 6,2, 9,5 );
+	p->drawLine( 6,6, 9,9 );
     }
 }
 
