06410831a2bb3a59bd8a8c84c062637f7aad72fa
Martin Jones <mjones@trolltech.com>
2000-07-13 04:42:12 +0100

    qwsmouse_qws.h
    [git-p4: depot-paths = "//depot/qt/main/": change = 22907]

---
+++ src/kernel/qwsmouse_qws.h
@@ -0,0 +1,67 @@
+/****************************************************************************
+** $Id: //depot/qt/qws/util/qws/qws.h#4 $
+**
+** Definition of Qt/FB central server classes
+**
+** Created : 991025
+**
+** Copyright (C) 1992-2000 Troll Tech AS.  All rights reserved.
+**
+** This file is part of the Qt GUI Toolkit Professional Edition.
+**
+** Licensees holding valid Qt Professional Edition licenses may use this
+** file in accordance with the Qt Professional Edition License Agreement
+** provided with the Qt Professional Edition.
+**
+** See http://www.troll.no/pricing.html or email sales@troll.no for
+** information about the Professional Edition licensing.
+**
+*****************************************************************************/
+
+#ifndef QWSMOUSE_H
+#define QWSMOUSE_H
+
+#include <qobject.h>
+
+class QWSPointerCalibrationData
+{
+public:
+    enum Location { TopLeft = 0, BottomLeft = 1, BottomRight = 2, TopRight = 3,
+		    Center = 4, LastLocation = Center };
+    QPoint devPoints[5];
+    QPoint screenPoints[5];
+};
+
+class QMouseHandler : public QObject {
+    Q_OBJECT
+public:
+    QMouseHandler();
+    virtual ~QMouseHandler();
+
+    virtual void clearCalibration() {}
+    virtual void calibrate( QWSPointerCalibrationData * ) {}
+
+signals:
+    void mouseChanged(const QPoint& pos, int bstate);
+};
+
+class QCalibratedMouseHandler : public QMouseHandler
+{
+    Q_OBJECT
+public:
+    QCalibratedMouseHandler();
+
+    virtual void clearCalibration();
+    virtual void calibrate( QWSPointerCalibrationData * );
+
+protected:
+    QPoint transform( const QPoint & );
+
+private:
+    int a, b, c;
+    int d, e, f;
+    int s;
+};
+
+#endif
+
