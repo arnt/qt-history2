1e205b4fe50f369249b05f7d2963d4f1c1d010f9
Jo Dillon <jo@trolltech.com>
2000-07-13 15:04:15 +0100

    Oops, slight fix
    [git-p4: depot-paths = "//depot/qt/main/": change = 22916]

--- src/kernel/qpainter_x11.cpp
+++ src/kernel/qpainter_x11.cpp
@@ -2177,7 +2177,7 @@ void QPainter::drawPolyline( const QPointArray &a, int index, int npoints )
     if ( cpen.style() != NoPen ) {
 	while(npoints>65535) {
 	    XDrawLines( dpy, hd, gc, (XPoint*)(pa.shortPoints( index, 65535 )),
-			npoints, CoordModeOrigin );
+			65535, CoordModeOrigin );
 	    npoints-=65535;
 	    index+=65535;
 	}
