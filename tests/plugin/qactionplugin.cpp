800db24274ba3fd47bd36b6e13b4349f5e08059b
Volker Hilsheimer <vohi@trolltech.com>
2000-09-06 00:47:49 +0100

    QActions in a plugin
    [git-p4: depot-paths = "//depot/qt/main/": change = 25727]

---
+++ tests/plugin/qactionplugin.cpp
@@ -0,0 +1,121 @@
+#include "qactionplugin.h"
+#include <qaction.h>
+
+/*!
+  \class QActionPlugIn qdefaultplugin.h
+
+  \brief A plugin loader implementing the QActionInterface
+*/
+
+/*!
+  Constructs a default plugin with file \a file and policy \a pol.
+*/
+QActionPlugIn::QActionPlugIn( const QString& file, LibraryPolicy pol )
+    : QPlugIn( file, pol )
+{
+}
+
+/*! \reimp
+*/
+bool QActionPlugIn::addToManager( QPlugInDict& dict )
+{
+    if ( !use() )
+	return FALSE;
+    
+    bool useful = FALSE;
+
+    QStringList list = actions();
+    for ( QStringList::Iterator a = list.begin(); a != list.end(); a++ ) {
+	useful = TRUE;
+#ifdef CHECK_RANGE
+	if ( dict[*a] )
+	    qWarning("%s: Action %s already defined!", library().latin1(), (*a).latin1() );
+	else
+#endif
+	    dict.insert( *a, this );
+    }
+
+    return useful;
+}
+
+/*! \reimp
+*/
+bool QActionPlugIn::removeFromManager( QPlugInDict& dict )
+{
+    bool res = TRUE;
+
+    QStringList wl = this->actions();
+    for ( QStringList::Iterator w = wl.begin(); w != wl.end(); w++ )
+        res = res && dict.remove( *w );
+
+    return res;
+}
+
+/*! \reimp
+*/
+QAction* QActionPlugIn::create( const QString& classname, bool& self, QObject* parent )
+{
+    if ( !use() )
+	return 0;
+
+    QAction* w = ((QActionInterface*)iface())->create( classname, self, parent );
+    guard( w );
+    return w;
+}
+
+/*! \reimp
+*/
+QStringList QActionPlugIn::actions()
+{
+    if ( !use() )
+	return QStringList();
+    QStringList list = ((QActionInterface*)iface())->actions();
+
+    return list;
+}
+
+/*!
+  \class QDefaultPlugInManager qdefaultpluginmanager.h
+
+  \brief Implements a QPlugInManager that handles plugins for custom widgets
+
+  \sa QPlugInManager
+*/
+
+/*!
+  Creates a QDefaultPlugInManager.
+
+  \sa QPlugInManager
+*/
+QActionPlugInManager::QActionPlugInManager( const QString& path, QPlugIn::LibraryPolicy pol )
+: QPlugInManager<QActionPlugIn>( path, pol )
+{
+}
+
+/*! \reimp
+*/
+QAction* QActionPlugInManager::newAction( const QString& classname, bool& self, QObject* parent )
+{
+    QActionPlugIn* plugin = (QActionPlugIn*)plugDict[ classname ];
+    if ( plugin )
+	return plugin->create( classname, self, parent );
+    return 0;
+}
+
+/*!
+  Returns a list of all widget classes supported by registered plugins.
+*/
+QStringList QActionPlugInManager::actions()
+{
+    QStringList list;
+    QDictIterator<QPlugIn> it (libDict);
+
+    while( it.current() ) {
+	QStringList actions = ((QActionPlugIn*)it.current())->actions();
+	for ( QStringList::Iterator a = actions.begin(); a != actions.end(); a++ )
+	    list << *a;
+	++it;
+    }
+
+    return list;
+}
---
+++ tests/plugin/qactionplugin.h
@@ -0,0 +1,34 @@
+#ifndef QACTIONPLUGIN_H
+#define QACTIONPLUGIN_H
+
+#include "qplugin.h"
+#include "qactioninterface.h"
+#include "qactionfactory.h"
+
+class QActionPlugIn : public QActionInterface, public QPlugIn
+{
+public:
+    QString queryInterface() { return "QActionInterface"; }
+
+    QActionPlugIn( const QString& filename, LibraryPolicy = Default );
+
+    QAction* create( const QString& classname, bool& self, QObject* parent = 0 );
+    QStringList actions();
+    
+    bool addToManager( QPlugInDict& dict );
+    bool removeFromManager( QPlugInDict& dict );
+};
+
+class QActionPlugInManager : public QPlugInManager<QActionPlugIn>, public QActionFactory
+{
+public:
+    QActionPlugInManager( const QString& path = QString::null, QPlugIn::LibraryPolicy = QPlugIn::Default );
+
+    QString factoryName() const { return "QActionPlugInManager"; }
+
+private:
+    QAction* newAction( const QString& classname, bool& self, QObject* parent = 0 );
+    QStringList actions();
+};
+
+#endif // QACTIONPLUGIN_H
