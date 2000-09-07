b3c81593d07bc30b3089dbd8a51102e033e0d47a
Volker Hilsheimer <vohi@trolltech.com>
2000-09-08 00:16:24 +0100

    Got rid of the last bunch of unnecessary functions. Now all the hard work is done in the basic classes, and the plugin has nothing to do but to provide some useful data. Added a first concept for the opposite way, that is, the plugin calls functions the client application provides. This happens to be almost trivial because of the nice signal/slot mechanism. I wonder if this should be a part of the general plugin system, too...
    [git-p4: depot-paths = "//depot/qt/main/": change = 25884]

--- tests/plugin/plugin/src/actions/main.cpp
+++ tests/plugin/plugin/src/actions/main.cpp
@@ -6,6 +6,7 @@
 #include <qmenubar.h>
 #include <qtoolbar.h>
 #include <qdialog.h>
+#include <qapplication.h>
 
 #ifdef _WS_WIN_
 #undef LIBEXPORT
@@ -18,24 +19,38 @@ class TestInterface : public QObject, public QActionInterface
 {
     Q_OBJECT
 public:
+    TestInterface() 
+	: iface(0) {}
+
+    ~TestInterface()
+    {
+	delete iface;
+	iface = 0;
+    }
+
     QString name() { return "Test Actionplugin"; }
     QString description() { return "Test implementation of the QActionInterface"; }
     QString author() { return "vohi"; }
 
-    QStringList actions();
+    QStringList featureList();
     QAction* create( const QString &actionname, QObject* parent = 0 );
+    QApplicationInterface* appInterface() { return iface ? iface : ( iface = new QApplicationInterface() ); }
 
 public slots:
     void onOpenModalDialog();
     void onOpenNonmodalDialog();
+
+protected:
+    QApplicationInterface* iface;
 };
 
-QStringList TestInterface::actions()
+QStringList TestInterface::featureList()
 {
     QStringList list;
 
     list << "Open Modal Dialog";
     list << "Open non-Modal Dialog";
+    list << "Open file Dialog";
 
     return list;
 }
@@ -49,6 +64,10 @@ QAction* TestInterface::create( const QString& actionname, QObject* parent )
     } else if ( actionname == "Open non-Modal Dialog" ) {
 	QAction* a = new QAction( actionname, QIconSet(), "Open &non-modal dialog", Qt::CTRL + Qt::Key_N, parent, actionname, FALSE );
 	connect( a, SIGNAL(activated()), this, SLOT(onOpenNonmodalDialog()) );
+	return a;
+    } else if ( actionname == "Open file Dialog" ) {
+	QAction* a = new QAction( actionname, QIconSet(), "Open file-dialog", Qt::CTRL + Qt::Key_F, parent, actionname, FALSE );
+	connect( a, SIGNAL(activated()), appInterface(), SIGNAL( openFile() ) );
 	return a;
     } else 
 	return 0;
@@ -73,12 +92,12 @@ extern "C"
 
 LIBEXPORT QActionInterface* loadInterface()
 {
-    return new TestInterface();
+    return new TestInterface();;
 }
 
-LIBEXPORT bool onConnect()
+LIBEXPORT bool onConnect( QApplication* myapp )
 {
-    qDebug("I've been loaded!");
+    qDebug("I've been loaded by %p", myapp );
     return TRUE;
 }
 
