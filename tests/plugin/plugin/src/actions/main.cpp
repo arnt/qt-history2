822d95ce2cb555b19703ca6df09465d7772a8669
Volker Hilsheimer <vohi@trolltech.com>
2000-09-09 05:43:27 +0100

    Include necessary changes to example application
    [git-p4: depot-paths = "//depot/qt/main/": change = 25939]

--- tests/plugin/plugin/src/actions/main.cpp
+++ tests/plugin/plugin/src/actions/main.cpp
@@ -1,13 +1,12 @@
 #include "../../../qactioninterface.h"
 #include "../../../qcleanuphandler.h"
+#include "../../../qapplicationinterfaces.h"
 
 #include <qaction.h>
-#include <qmainwindow.h>
 #include <qpopupmenu.h>
-#include <qmenubar.h>
-#include <qtoolbar.h>
 #include <qdialog.h>
 #include <qapplication.h>
+#include <qvariant.h>
 
 #ifdef _WS_WIN_
 #undef LIBEXPORT
@@ -21,6 +20,7 @@ class TestInterface : public QObject, public QActionInterface
     Q_OBJECT
 public:
     TestInterface();
+    ~TestInterface();
     QString name() { return "Test Actionplugin"; }
     QString description() { return "Test implementation of the QActionInterface"; }
     QString author() { return "vohi"; }
@@ -29,68 +29,88 @@ public:
     QAction* create( const QString &actionname, QObject* parent = 0 );
 
     QCleanUpHandler<QAction>* actions;
+    QClientInterface* requestClientInterface( const QCString& );
 
 public slots:
     void onOpenModalDialog();
-    void onOpenNonmodalDialog();
-    void openFile();
-};
+    void turnOnText();
 
-TestInterface* that;
+protected:
+    QAction* actionTurnOnText;
+
+private:
+    QClientInterface* mwIface;
+};
 
 TestInterface::TestInterface()
 {
-    that = this;
-    actions = new QCleanUpHandler<QAction>();
+    mwIface = 0;
+    actions = new QCleanUpHandler<QAction>;
 }
 
+TestInterface::~TestInterface()
+{
+    delete mwIface;
+    delete actions;
+}
 
 QStringList TestInterface::featureList()
 {
     QStringList list;
 
-    list << "Open Modal Dialog";
-    list << "Open non-Modal Dialog";
-    list << "Open file Dialog";
+    list << "Open Dialog";
+    list << "Show Text";
 
     return list;
 }
 
 QAction* TestInterface::create( const QString& actionname, QObject* parent )
 {
-    if ( actionname == "Open Modal Dialog" ) {
-	QAction* a = new QAction( actionname, QIconSet(), "Open &modal dialog", Qt::CTRL + Qt::Key_M, parent, actionname, FALSE );
+    if ( actionname == "Open Dialog" ) {
+	QAction* a = new QAction( actionname, QIconSet(), "Open &dialog", Qt::CTRL + Qt::Key_D, parent, actionname, FALSE );
 	connect( a, SIGNAL(activated()), this, SLOT(onOpenModalDialog()) );
 	actions->addCleanUp( a );
 	return a;
-    } else if ( actionname == "Open non-Modal Dialog" ) {
-	QAction* a = new QAction( actionname, QIconSet(), "Open &non-modal dialog", Qt::CTRL + Qt::Key_N, parent, actionname, FALSE );
-	connect( a, SIGNAL(activated()), this, SLOT(onOpenNonmodalDialog()) );
-	actions->addCleanUp( a );
-	return a;
-    } else if ( actionname == "Open file Dialog" ) {
-	QAction* a = new QAction( actionname, QIconSet(), "Open file-dialog", Qt::CTRL + Qt::Key_F, parent, actionname, FALSE );
-	connect( a, SIGNAL(activated()), this, SLOT(openFile()) );
-	actions->addCleanUp( a );
-	return a;
+    } else if ( actionname == "Show Text" ) {
+	actionTurnOnText = new QAction( actionname, QIconSet(), "&Show Text", Qt::CTRL + Qt::Key_T, parent, actionname, TRUE );
+	connect( actionTurnOnText, SIGNAL(activated()), this, SLOT(turnOnText()) );
+	actions->addCleanUp( actionTurnOnText );
+	return actionTurnOnText;
     } else 
 	return 0;
 }
 
 void TestInterface::onOpenModalDialog()
 {
-    QDialog dialog( 0, 0, TRUE );
+    QVariant obj;
+    mwIface->requestProperty( "mainWindow", obj );
+    QDialog dialog( (QWidget*)obj.toUInt(), 0, TRUE );
     dialog.show();
 }
 
-void TestInterface::onOpenNonmodalDialog()
+void TestInterface::turnOnText()
 {
-    QDialog *dialog = new QDialog( 0, 0, FALSE );
-    dialog->show();
+    if ( mwIface ) {
+	QVariant onOff;
+	mwIface->requestProperty( "usesTextLabel", onOff );
+	bool on = onOff.toBool();
+	if ( !on ) {
+	    actionTurnOnText->setText( "Hide Text" );
+	    actionTurnOnText->setMenuText( "&Hide Text" );
+	} else {
+	    actionTurnOnText->setText( "Show Text" );
+	    actionTurnOnText->setMenuText( "&Show Text" );
+	}
+	mwIface->requestSetProperty( "usesTextLabel", !on );
+    }
 }
 
-void TestInterface::openFile()
+QClientInterface* TestInterface::requestClientInterface( const QCString& request )
 {
+    if ( QString(request) == QString(queryPlugInInterface()) )
+	return mwIface ? mwIface : ( mwIface = new QClientInterface );
+    else
+	return 0;
 }
 
 #if defined(__cplusplus )
@@ -105,17 +125,16 @@ LIBEXPORT QActionInterface* loadInterface()
 
 LIBEXPORT bool onConnect( QApplication* myapp )
 {
-    qDebug("I've been loaded by %p", myapp );
+    qDebug("I've been loaded by %p");
     return TRUE;
 }
 
 LIBEXPORT bool onDisconnect( QApplication* myapp )
 {
     if ( myapp ) {
-	delete that->actions;
         qDebug("I've been unloaded by %p", myapp);
     } else {
-	qDebug("I've been unloaded by operation system" );
+	qDebug("I've been unloaded on application destruction" );
     }
     return TRUE;
 }
