8951d9eee75f7864826623de7f0e1cb8a583d8c8
Volker Hilsheimer <vohi@trolltech.com>
2000-09-10 02:56:26 +0100

    Remove garbage
    [git-p4: depot-paths = "//depot/qt/main/": change = 25963]

--- tests/plugin/qapplicationinterfaces.h
+++ tests/plugin/qapplicationinterfaces.h
@@ -7,22 +7,11 @@ class QDualInterface : public QObject
 {
     Q_OBJECT
 signals:
-    // Plugin:	    requests the value of a property
     void readProperty( const QCString&, QVariant& );
-
-    // Plugin:	    demands to change a property
-    // Application: answers an request
     void writeProperty( const QCString&, const QVariant& );
 
 public slots:
-    // Would be cool to have a makro defined 
-
-    // Application: processes the readProperty request
-    // PlugIn:	    sends a readProperty request
     virtual void requestProperty( const QCString&, QVariant& ) {}
-
-    // Application: processes the writeProperty request
-    // PlugIn:	    sends a writeProperty request
     virtual void requestSetProperty( const QCString&, const QVariant& ) {}
 };
 
