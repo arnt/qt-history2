8558f1890038037e74aa39ba729e46aec4705b87
Volker Hilsheimer <vohi@trolltech.com>
2000-09-19 22:34:36 +0100

    One includefile to rule them all...
    [git-p4: depot-paths = "//depot/qt/main/": change = 26680]

---
+++ tools/designer/plugins/designerinterface.h
@@ -0,0 +1,14 @@
+#ifndef DESIGNERINTERFACE_H
+#define DESIGNERINTERFACE_H
+
+/* 
+  This include file declares all interfaces for the Qt Designer 
+  that can be used in the plugins.
+*/
+
+#include <qapplicationinterface.h>
+#include "../designer/actioniface.h"
+#include "../shared/widgetinterface.h"
+#include "../designer/filteriface.h"
+
+#endif //DESIGNERINTERFACE_H
