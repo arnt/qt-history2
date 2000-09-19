d8e40f15fdd690347ffa3086ea5320d1caa99f4e
Sam Magnuson <sam@trolltech.com>
2000-09-19 19:17:36 +0100

    ..
    [git-p4: depot-paths = "//depot/qt/main/": change = 26670]

--- qmake/generators/unix/unixmake.cpp
+++ qmake/generators/unix/unixmake.cpp
@@ -50,9 +50,9 @@ UnixMakefileGenerator::writeMakefile(QTextStream &t)
     writeHeader(t);
     if(!project->variables()["TMAKE_FAILED_REQUIREMENTS"].isEmpty()) {
 	t << "all clean:" << "\n\t"
-	  << "echo \"Some of the required modules (" 
+	  << "@echo \"Some of the required modules (" 
 	  << var("TMAKE_FAILED_REQUIREMENTS") << ") are not available.\"" << "\n\t"
-	  << "echo \"Skipped.\"" << endl << endl;
+	  << "@echo \"Skipped.\"" << endl << endl;
 	return TRUE;
     }
 	
