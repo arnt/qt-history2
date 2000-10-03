616b353225a6affa417d13fb387afef73742bc60
Volker Hilsheimer <vohi@trolltech.com>
2000-10-03 20:07:37 +0100

    A new abstract interface for the formlist
    [git-p4: depot-paths = "//depot/qt/main/": change = 27643]

--- tools/designer/plugins/designerinterface.h
+++ tools/designer/plugins/designerinterface.h
@@ -11,4 +11,17 @@
 #include "../shared/widgetinterface.h"
 #include "../designer/filteriface.h"
 
+class DesignerFormListInterface : public QComponentInterface
+{
+public:
+    DesignerFormListInterface( QObject *li ) : QComponentInterface( li ) {}
+
+    virtual const QPixmap* pixmap( QComponentInterface*, int col ) const = 0;
+    virtual void setPixmap( QComponentInterface*, int col, const QPixmap& ) = 0;
+    virtual QString text( QComponentInterface*, int col ) const = 0;
+    virtual void setText( QComponentInterface*, int col, const QString& ) = 0;
+
+    virtual QList<QComponentInterface> queryFormInterfaceList() = 0;
+};
+
 #endif //DESIGNERINTERFACE_H
