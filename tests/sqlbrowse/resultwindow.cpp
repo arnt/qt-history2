7beb1e214fbfd23d9b81bc854b22977464d4d581
Dave Berton <db@trolltech.com>
2000-10-04 13:16:06 +0100

    Update example
    [git-p4: depot-paths = "//depot/qt/main/": change = 27708]

--- tests/sqlbrowse/resultwindow.cpp
+++ tests/sqlbrowse/resultwindow.cpp
@@ -8,24 +8,7 @@
 #include <qsqltable.h>
 #include <qbuttongroup.h>
 #include <qpushbutton.h>
-
-// class MyDataGrid : public QSqlTable
-// {
-// public:
-//     MyDataGrid ( QWidget * parent = 0, const char * name = 0 )
-// 	: QSqlGrid( parent, name )
-//     {
-// 	QSqlConnection::addDatabase( "QMYSQL", "troll", "db", "", "lupinella", "mysql" );
-// 	QSql r( "select * from Customer order by 2;", "mysql" );
-// 	setQuery( r, FALSE );
-// 	addColumn( r.fields().field( 2 ) );
-// 	addColumn( r.fields().field( 4 ) );
-//     }
-//     ~MyDataGrid()
-//     {
-//     }
-// };
-
+#include <qtabwidget.h>
 
 ResultWindow::ResultWindow ( QWidget * parent, const char * name, WFlags f )
     : SqlBrowseWindowBase(parent, name, f)
@@ -47,30 +30,37 @@ ResultWindow::~ResultWindow()
 
 void ResultWindow::slotExec()
 {
+    QButton* b;
     if ( db->isOpen() ) {
-	QButton* b = browseType->selected();
-	if ( b ) {
-	    switch ( browseType->id( b ) ) {
-	    case 0: // SQL
-		dataGrid->setQuery( "select * from " + tableList->currentText() + ";" );
-		break;
-	    case 1: // Rowset
-		dataGrid->setRowset( tableList->currentText() );
-		break;
-	    case 2: // View
-		dataGrid->setView( tableList->currentText() );
-		break;
+	switch ( tabs->currentPageIndex() ) {
+	case 0:
+	    b = browseType->selected();
+	    if ( b ) {
+		switch ( browseType->id( b ) ) {
+		case 0: // SQL
+		    dataGrid->setQuery( "select * from " + tableList->currentText() + ";" );
+		    break;
+		case 1: // Rowset
+		    dataGrid->setRowset( tableList->currentText() );
+		    break;
+		case 2: // View
+		    dataGrid->setView( tableList->currentText() );
+		    break;
+		}
 	    }
+	    break;
+	case 1:
+	    dataGrid->setQuery( queryEdit->text().simplifyWhiteSpace() );
+	    break;
 	}
     }
 }
-
 
 void ResultWindow::newSelection( const QSqlFieldList* fields )
 {
     QString cap ("Selection ");
     for ( uint i = 0; i < fields->count(); ++i ) {
-	QSqlField f = fields->field(i);	
+	QSqlField f = fields->field(i);
 	cap += " - " + f.value().toString();
     }
     setCaption( cap );
