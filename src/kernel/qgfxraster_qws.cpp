ab6f6035a17f37ab7dcfacb11be58fb07fc13df3
John Ryland <jryland@trolltech.com>
2000-10-05 02:43:55 +0100

    Had a QT_NO_VGA16 instead of QT_NO_VGA_16 in one place
    [git-p4: depot-paths = "//depot/qt/main/": change = 27769]

--- src/kernel/qgfxraster_qws.cpp
+++ src/kernel/qgfxraster_qws.cpp
@@ -134,7 +134,7 @@ struct SWCursorData {
 
 // The GFX_8BPP_PIXEL macro screws up the VGA16 driver for alpha blended images
 // This might slow down alpha blending in 8-bit modes if VGA16 is compiled in
-#ifndef QT_NO_QWS_VGA16
+#ifndef QT_NO_QWS_VGA_16
 #undef GFX_8BPP_PIXEL
 #define GFX_8BPP_PIXEL(r,g,b)	QColor(r,g,b).alloc()
 #endif
