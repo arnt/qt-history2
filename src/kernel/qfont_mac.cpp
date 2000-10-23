01b51c4bbad4da3ad75fc03cd79093dd79cd5f3c
Sam Magnuson <sam@trolltech.com>
2000-10-24 00:47:37 +0100

    The debugging stuff is no longer necesary since don found fix.
    [git-p4: depot-paths = "//depot/qt/main/": change = 28925]

--- src/kernel/qfont_mac.cpp
+++ src/kernel/qfont_mac.cpp
@@ -30,12 +30,7 @@ int QFontMetrics::lineWidth() const
 }
 
 #undef FI
-#if 1
 #define FI (painter ? painter->cfont.d->fin->info : d->fin->info)
-#else
-#define FI foo()
-FontInfo foo() { FontInfo bar; GetFontInfo(&bar); return bar; }
-#endif
 
 int QFontMetrics::leading() const
 {
