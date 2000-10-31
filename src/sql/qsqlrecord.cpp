8459deafa788289613c143fcf019c77bfdf272ff
Dave Berton <db@trolltech.com>
2000-10-31 18:24:05 +0100

    don't generate select for calculated fields.
    [git-p4: depot-paths = "//depot/qt/main/": change = 29350]

--- src/sql/qsqlrecord.cpp
+++ src/sql/qsqlrecord.cpp
@@ -70,7 +70,7 @@ QVariant& QSqlRecord::operator[]( int i )
 
 QVariant& QSqlRecord::operator[]( const QString& name )
 {
-    findField( name )->setNull( FALSE );    
+    findField( name )->setNull( FALSE );
     return findField( name )->val;
 }
 
@@ -211,9 +211,11 @@ QString QSqlRecord::toString( const QString& prefix ) const
     QString pflist;
     QString pfix =  prefix.isNull() ? QString::null : prefix + ".";
     for ( uint i = 0; i < count(); ++i ){
-	pflist += pfix + field( i )->name();
-	if( i != (count() - 1) )
-	    pflist += ", ";
+	if ( !isCalculated( i ) ) {
+	    pflist += pfix + field( i )->name();
+	    if( i != (count() - 1) )
+		pflist += ", ";
+	}
     }
     return pflist;
 }
@@ -239,7 +241,7 @@ QSqlField* QSqlRecord::findField( int i )
 #ifdef QT_CHECK_RANGE
     static QSqlField dbg;
     if( (unsigned int) i > fieldList.count() ){
-	qWarning( "QSqlRecord warning: index out of range" );
+	qWarning( "QSqlRecord::findField: index out of range" );
 	return &dbg;
     }
 #endif // QT_CHECK_RANGE
@@ -256,7 +258,7 @@ QSqlField* QSqlRecord::findField( const QString& name )
 #ifdef QT_CHECK_RANGE
     static QSqlField dbg;
     if( (unsigned int) position( name ) > fieldList.count() ){
-	qWarning( "QSqlRecord warning: index out of range" );
+	qWarning( "QSqlRecord::findField : index out of range" );
 	return &dbg;
     }
 #endif // QT_CHECK_RANGE
