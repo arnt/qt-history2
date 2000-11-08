88bf0c6f5f040e56fd52ed053c67da3f7adc8d6e
Rainer Schmidt <rms@froglogic.com>
2000-11-08 11:50:08 +0100

    The mapping function-names -> functions is done by the context; this design should be open for this. Todo: custom functions and data types must be registered somewhere.
    [git-p4: depot-paths = "//depot/qt/main/": change = 29837]

--- tests/qxml/xpath/qxpath.h
+++ tests/qxml/xpath/qxpath.h
@@ -5,7 +5,10 @@
 #include "qdom.h"
 #endif // QT_H
 
+#if 0
 class QXPathStep;
+#endif
+class QXPathDataType;
 class QXPathPrivate;
 
 class Q_EXPORT QXPath
@@ -28,16 +31,49 @@ class Q_EXPORT QXPathFunction
 {
 public:
     QXPathFunction();
-    ~QXPathFunction();
+    virtual ~QXPathFunction();
+
+    virtual int minNumParameters() = 0;
+    virtual int maxNumParameters() = 0;
+    virtual int typeParameter( int i ) = 0;
+#if 0
+    // If I put the function-name -> function mapping in the context, then the
+    // whole recursion of the evaluation is hidden.
+    virtual void setParameter( int i, QXPathFunction *f );
+#else
+    virtual void setParameter( int i, QXPathDataType *value );
+#endif
+
+#if 0
+    // this approach has a nicer API, but it is not that easy to subclass since
+    // you need sharing mechanism... ???
+    virtual QXPathDataType evaluate() = 0;
+#else
+    virtual void evaluate( QXPathDataType* ret ) = 0;
+#endif
 };
 
 
+class Q_EXPORT QXPathDataType
+{
+public:
+    QXPathDataType();
+    virtual ~QXPathDataType();
+
+    virtual bool isType( const QString& ) = 0;
+    bool isType( int ) const;
+    int type() const;
+};
+
+
+#if 0
 class Q_EXPORT QXPathFunctionFactory
 {
 public:
     QXPathFunctionFactory();
     ~QXPathFunctionFactory();
 };
+#endif
 
 
 #if 0
