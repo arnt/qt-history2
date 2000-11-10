ec6a42ca9173b7479dfec51e5802bc549017111e
Rainer Schmidt <rms@froglogic.com>
2000-11-10 17:19:14 +0100

    yep, do some real parsing now!
    [git-p4: depot-paths = "//depot/qt/main/": change = 30023]

--- tests/qxml/xpath/qxpath.cpp
+++ tests/qxml/xpath/qxpath.cpp
@@ -73,6 +73,51 @@ public:
 	AxDescendantOrSelf,
 	AxAncestorOrSelf
     };
+
+    // Enumeration for the non-terminal productions. The sorting is important!
+    // (The numbers in brackets are the corresponding number of production of
+    // the standard.)
+    enum NonTerminal {
+	NtOrExpr, // [21]
+	NtAndExpr, // [22]
+	NtEqualityExpr, // [23]
+	NtRelationalExpr, // [24]
+	NtAdditiveExpr, // [25]
+	NtMultiplicativeExpr, // [26]
+	NtUnaryExpr, // [27]
+	NtUnionExpr, // [18]
+	NtPathExpr, // [19]
+	NtFilterExpr, // [20]
+	NtPrimaryExpr, // [15]
+	// until this point we have a real inclusion-order; i.e. a NtPathExpr
+	// is also a NtAndExpr, etc.
+	NtLocationPath, // [1]
+	NtRelativeLocationPath, // [3]
+	NtPredicate, // [8]
+	NtVariableReference,
+	NtLiteral,
+	NtNumber,
+	NtFunctionCall,
+	NtStep,
+	NtError
+    };
+    /*!
+      Returns true if the non terminal production \a isA can lead to the
+      non-terminal production \a nt (i.e. \a nt is more special than \a isA).
+    */
+    static bool nonTerminalIsA( NonTerminal nt, NonTerminal isA )
+    {
+	if ( isA <= NtPrimaryExpr ) {
+	    // we can use the more efficient test
+	    return isA <= nt;
+	} else {
+	    if ( nt==NtVariableReference || nt==NtLiteral ||
+		    nt==NtNumber || nt==NtFunctionCall ) {
+		return isA <= NtPrimaryExpr;
+	    }
+	}
+	return FALSE;
+    }
 };
 
 
@@ -108,11 +153,29 @@ public:
 	delete child;
     }
 
+    virtual int numberOfChildren()
+    {
+	return 0;
+    }
+
     virtual bool evaluate( QXPathDataType* ret ) = 0;
     virtual Type type() = 0;
+    virtual QXPathNS::NonTerminal nonTerminal() = 0;
+
+    virtual QXPathNS::NonTerminal rightmostDescendantNt()
+    {
+	return QXPathNS::NtError;
+    }
 
     QXPathAtom *parent;
+
+#if defined(QXPATH_DEBUG)
+    static QString indent;
+#endif
 };
+#if defined(QXPATH_DEBUG)
+QString QXPathAtom::indent = "";
+#endif
 
 class QXPathAtomWithChildren : public QXPathAtom
 {
@@ -132,6 +195,11 @@ public:
 	child->parent = this;
     }
 
+    int numberOfChildren()
+    {
+	return children.count();
+    }
+
 protected:
     QList<QXPathAtom> children; // QList is more complex than needed
 };
@@ -147,14 +215,16 @@ public:
     bool evaluate( QXPathDataType *ret )
     {
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: LocationPath {" );
+	qDebug( "QXPathAtom: " + indent + "LocationPath {" );
+	indent += "  ";
 #endif
 	QXPathAtom *it;
 	for ( it=children.first(); it!=0; it=children.next() ) {
 	    it->evaluate( ret );
 	}
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: }" );
+	indent.truncate( indent.length() - 2 );
+	qDebug( "QXPathAtom: " + indent + "}" );
 #endif
 	return TRUE;
     }
@@ -162,6 +232,11 @@ public:
     Type type()
     {
 	return LocationPath;
+    }
+
+    QXPathNS::NonTerminal nonTerminal()
+    {
+	return QXPathNS::NtLocationPath; // ### RelativeLocationPath???
     }
 };
 
@@ -182,7 +257,7 @@ public:
     bool evaluate( QXPathDataType* )
     {
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: LocationStep" );
+	qDebug( "QXPathAtom: " + indent + "LocationStep" );
 #endif
 	return TRUE;
     }
@@ -190,6 +265,11 @@ public:
     Type type()
     {
 	return LocationStep;
+    }
+
+    QXPathNS::NonTerminal nonTerminal()
+    {
+	return QXPathNS::NtStep;
     }
 
 private:
@@ -211,7 +291,7 @@ public:
     bool evaluate( QXPathDataType* )
     {
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: VariableReference: %s", value.latin1() );
+	qDebug( "QXPathAtom: " + indent + "VariableReference: %s", value.latin1() );
 #endif
 	return TRUE;
     }
@@ -219,6 +299,11 @@ public:
     Type type()
     {
 	return VariableReference;
+    }
+
+    QXPathNS::NonTerminal nonTerminal()
+    {
+	return QXPathNS::NtVariableReference;
     }
 
 private:
@@ -238,7 +323,7 @@ public:
     bool evaluate( QXPathDataType* )
     {
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: Literal: %s", value.latin1() );
+	qDebug( "QXPathAtom: " + indent + "Literal: %s", value.latin1() );
 #endif
 	return TRUE;
     }
@@ -246,6 +331,11 @@ public:
     Type type()
     {
 	return Literal;
+    }
+
+    QXPathNS::NonTerminal nonTerminal()
+    {
+	return QXPathNS::NtLiteral;
     }
 
 private:
@@ -265,7 +355,7 @@ public:
     bool evaluate( QXPathDataType* )
     {
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: Number: %f", value );
+	qDebug( "QXPathAtom: " + indent + "Number: %f", value );
 #endif
 	return TRUE;
     }
@@ -275,31 +365,66 @@ public:
 	return Number;
     }
 
+    QXPathNS::NonTerminal nonTerminal()
+    {
+	return QXPathNS::NtNumber;
+    }
+
 private:
     double value;
 };
 
-class QXPathAtomOperator : public QXPathAtomWithChildren
+class QXPathAtomOperator : public QXPathAtom
 {
 public:
-    QXPathAtomOperator( QXPathNS::Operator op ) : QXPathAtomWithChildren()
+    QXPathAtomOperator( QXPathNS::Operator op ) : QXPathAtom()
     {
 	value = op;
+	leftChild = 0;
+	rightChild = 0;
     }
     ~QXPathAtomOperator()
-    {}
+    {
+	delete leftChild;
+	delete rightChild;
+    }
+
+    void addChild( QXPathAtom* child )
+    {
+	if ( leftChild == 0 ) {
+	    leftChild = child;
+	    child->parent = this;
+	} else if ( rightChild == 0 ) {
+	    rightChild = child;
+	    child->parent = this;
+	} else {
+	    rightChild->addChild( child );
+	}
+    }
+
+    int numberOfChildren()
+    {
+	if ( leftChild == 0 )
+	    return 0;
+	else if ( rightChild == 0 )
+	    return 1;
+	else
+	    return 2;
+    }
 
     bool evaluate( QXPathDataType* ret )
     {
+	if ( leftChild == 0 || rightChild == 0 )
+	    return FALSE;
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: Operator %d [", value );
+	qDebug( "QXPathAtom: " + indent + "Operator %d [", value );
+	indent += "  ";
 #endif
-	QXPathAtom *it;
-	for ( it=children.first(); it!=0; it=children.next() ) {
-	    it->evaluate( ret );
-	}
+	leftChild->evaluate( ret );
+	rightChild->evaluate( ret );
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: ]" );
+	indent.truncate( indent.length() - 2 );
+	qDebug( "QXPathAtom: " + indent + "]" );
 #endif
 	return TRUE;
     }
@@ -309,8 +434,58 @@ public:
 	return Operator;
     }
 
+    QXPathNS::NonTerminal nonTerminal()
+    {
+	switch ( value ) {
+	    case QXPathNS::OpOr:
+		return QXPathNS::NtOrExpr;
+	    case QXPathNS::OpAnd:
+		return QXPathNS::NtAndExpr;
+	    case QXPathNS::OpEqual:
+	    case QXPathNS::OpNotEqual:
+		return QXPathNS::NtEqualityExpr;
+	    case QXPathNS::OpLt:
+	    case QXPathNS::OpLtEq:
+	    case QXPathNS::OpGt:
+	    case QXPathNS::OpGtEq:
+		return QXPathNS::NtRelationalExpr;
+	    case QXPathNS::OpPlus:
+	    case QXPathNS::OpMinus:
+		return QXPathNS::NtAdditiveExpr;
+	    case QXPathNS::OpMultiply:
+	    case QXPathNS::OpMod:
+	    case QXPathNS::OpDiv:
+		return QXPathNS::NtMultiplicativeExpr;
+	    case QXPathNS::OpPipe:
+		return QXPathNS::NtUnionExpr;
+
+	    // ### think about the stuff down this line
+	    case QXPathNS::OpSlash:
+	    case QXPathNS::OpDoubleSlash:
+	    default:
+		break; // this case should never happen
+	}
+	// "error"
+	return QXPathNS::NtError;
+    }
+
+    QXPathNS::NonTerminal rightmostDescendantNt()
+    {
+	if ( rightChild == 0 ) {
+	    QXPathNS::NonTerminal nt = nonTerminal();
+	    if ( nt == QXPathNS::NtError )
+		return nt;
+	    else
+		return (QXPathNS::NonTerminal)(nt + 1);
+	} else {
+	    return rightChild->rightmostDescendantNt();
+	}
+    }
+
 private:
     QXPathNS::Operator value;
+    QXPathAtom *leftChild;
+    QXPathAtom *rightChild;
 };
 
 class QXPathAtomFunctionCall : public QXPathAtomWithChildren
@@ -326,14 +501,16 @@ public:
     bool evaluate( QXPathDataType* ret )
     {
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: FunctionCall: %s (", value.latin1() );
+	qDebug( "QXPathAtom: " + indent + "FunctionCall: %s (", value.latin1() );
+	indent += "  ";
 #endif
 	QXPathAtom *it;
 	for ( it=children.first(); it!=0; it=children.next() ) {
 	    it->evaluate( ret );
 	}
 #if defined(QXPATH_DEBUG)
-	qDebug( "QXPathAtom: )" );
+	indent.truncate( indent.length() - 2 );
+	qDebug( "QXPathAtom: " + indent + ")" );
 #endif
 	return TRUE;
     }
@@ -341,6 +518,11 @@ public:
     Type type()
     {
 	return FunctionCall;
+    }
+
+    QXPathNS::NonTerminal nonTerminal()
+    {
+	return QXPathNS::NtFunctionCall;
     }
 
 private:
@@ -779,7 +961,10 @@ public:
     QXPathAtom* parse( const QString& expr );
 
 private:
+    bool reduce( QXPathAtom* &act );
+
     QXPathLexicalAnalyzer *lex;
+    QStack<QXPathAtom> *stack;
 };
 
 
@@ -800,6 +985,57 @@ QXPathParser::~QXPathParser()
 }
 
 /*!
+  Perfom a reduce step with \a act as the new node that should be inserted.
+*/
+bool QXPathParser::reduce( QXPathAtom* &act )
+{
+    QXPathAtom *tmp1, *tmp2;
+    if ( stack->isEmpty() )
+	return FALSE;
+    switch ( act->type() ) {
+	case QXPathAtom::VariableReference:
+	case QXPathAtom::Literal:
+	case QXPathAtom::Number:
+	    tmp1 = stack->pop();
+	    tmp1->addChild( act ); // ### error handling
+	    act = tmp1;
+	    break;
+	case QXPathAtom::Operator:
+	    switch ( act->numberOfChildren() ) {
+		case 0:
+		    tmp1 = stack->pop();
+		    if ( stack->isEmpty() ) {
+			act->addChild( tmp1 );
+		    } else {
+			tmp2 = stack->top();
+			NonTerminal r_act = act->rightmostDescendantNt();
+			NonTerminal r_tmp2 = tmp2->rightmostDescendantNt();
+			if ( nonTerminalIsA( tmp1->nonTerminal(), r_tmp2 ) &&
+				nonTerminalIsA( tmp2->nonTerminal(), r_act ) ) {
+			    tmp2->addChild( tmp1 ); // ### error handling
+			    act->addChild( stack->pop() );
+			} else {
+			    act->addChild( tmp1 );
+			}
+		    }
+		    break;
+		default:
+		    tmp1 = stack->pop();
+		    tmp1->addChild( act ); // ### error handling
+		    act = tmp1;
+		    break;
+	    }
+	    break;
+	case QXPathAtom::FunctionCall:
+	case QXPathAtom::LocationPath:
+	case QXPathAtom::LocationStep:
+	    // ### not implemented yet
+	    break;
+    }
+    return TRUE;
+}
+
+/*!
   Parses the expression and returns the root of the tree that belongs to the
   expression if no parsing error occurs. Otherwise this function returns 0.
 */
@@ -808,11 +1044,12 @@ QXPathAtom* QXPathParser::parse( const QString& expr )
     Token token;
     QXPathAtom *act_root = 0;
     QXPathAtom *tmp;
-    QStack<QXPathAtom> stack;
+    stack = new QStack<QXPathAtom>;
 
     // init lexical analyzer
     delete lex;
     lex = new QXPathLexicalAnalyzer( expr );
+
     // get token after token and parse the expression
     while ( !lex->atEnd() ) {
 	token = lex->nextToken();
@@ -848,11 +1085,10 @@ QXPathAtom* QXPathParser::parse( const QString& expr )
 	    case TkNodeType_Node:
 		break;
 	    case TkOperator:
-		if ( stack.isEmpty() )
-		    goto error;
 		tmp= new QXPathAtomOperator( lex->getOperator() );
-		tmp->addChild( stack.pop() );
-		stack.push( tmp );
+		if ( !reduce( tmp ) )
+		    goto error;
+		stack->push( tmp );
 		break;
 	    case TkFunctionName:
 		break;
@@ -861,7 +1097,7 @@ QXPathAtom* QXPathParser::parse( const QString& expr )
 	    case TkLiteral:
 		break;
 	    case TkNumber:
-		stack.push( new QXPathAtomNumber(lex->getNumber()) );
+		stack->push( new QXPathAtomNumber(lex->getNumber()) );
 		break;
 	    case TkVariableReference:
 		break;
@@ -871,23 +1107,30 @@ QXPathAtom* QXPathParser::parse( const QString& expr )
     lex = 0;
 
     // build the tree from the stack content
-    while ( !stack.isEmpty() ) {
+    while ( !stack->isEmpty() ) {
+qDebug( "Stack count: %d", stack->count() );
 	if ( act_root == 0 ) {
-	    act_root = stack.pop();
+	    act_root = stack->pop();
 	} else {
-	    if ( stack.top()->type() == QXPathAtom::Operator ) {
-		tmp = stack.pop();
+	    if ( !reduce( act_root ) )
+		goto error;
+#if 0
+	    if ( stack->top()->type() == QXPathAtom::Operator ) {
+		tmp = stack->pop();
 		tmp->addChild( act_root );
 		act_root = tmp;
 	    } else {
 		goto error;
 	    }
+#endif
 	}
     }
+    delete stack;
     return act_root;
 error:
     delete act_root;
-    stack.setAutoDelete( TRUE );
+    stack->setAutoDelete( TRUE );
+    delete stack;
     delete lex;
     lex = 0;
     return 0;
