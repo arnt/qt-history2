bb4c873e286ccd06fc38e16c66e60df0cb78ebdb
Rainer Schmidt <rms@froglogic.com>
2000-11-16 20:02:24 +0100

    more test functionality
    [git-p4: depot-paths = "//depot/qt/main/": change = 30273]

--- tests/qxml/domtest/domtree.cpp
+++ tests/qxml/domtest/domtree.cpp
@@ -1,36 +1,40 @@
-#include "domtree.h"
 #include <qfile.h>
 #include <qmessagebox.h>
 
+#include "domtree.h"
+
+//
+// DomTree
+//
+
 DomTree::DomTree( const QString fileName, QWidget *parent, const char *name )
-    : QListView( parent, name )
+    : QHBox( parent, name )
 {
     // div. configuration of the list view
-    addColumn( "DOM Nodes" );
-    setRootIsDecorated( TRUE );
-    setSorting( -1 );
+    tree = new QListView( this );
+    tree->addColumn( "Name" );
+    tree->addColumn( "Type" );
+    tree->setRootIsDecorated( TRUE );
+    tree->setSorting( -1 );
+    connect( tree, SIGNAL(selectionChanged(QListViewItem*)),
+	    this, SLOT(selectionChanged(QListViewItem*)) );
 
     // read the XML file and create DOM tree
     QFile file( fileName );
     if ( !file.open( IO_ReadOnly ) ) {
-#if 0
-	QMessageBox::critical( 0,
-		tr( "Critical Error" ),
-		tr( "Cannot open file %1" ).arg( fileName ) );
-#endif
 	return;
     }
     if ( !domTree.setContent( &file ) ) {
-#if 0
-	QMessageBox::critical( 0,
-		tr( "Critical Error" ),
-		tr( "Parsing error for file %1" ).arg( fileName ) );
-#endif
 	file.close();
 	return;
     }
     file.close();
-    buildTree( 0, domTree.documentElement() );
+    buildTree( 0, domTree );
+
+    // div. configuration of the list view
+    text = new QTextView( this );
+    text->setMinimumSize( 300, 400 );
+    text->setTextFormat( RichText );
 }
 
 DomTree::~DomTree()
@@ -39,16 +43,171 @@ DomTree::~DomTree()
 
 void DomTree::buildTree( QListViewItem *parentItem, const QDomNode &actNode )
 {
+    static int depth = -1;
     QListViewItem *thisItem = 0;
     QDomNode node = actNode;
+    depth++;
     while ( !node.isNull() ) {
-	if ( parentItem == 0 )
-	    thisItem = new QListViewItem( this, thisItem );
-	else
-	    thisItem = new QListViewItem( parentItem, thisItem );
-	thisItem->setText( 0, node.nodeName() );
-
+	if ( parentItem == 0 ) {
+	    thisItem = new DomTreeItem( node, tree, thisItem );
+	} else {
+	    thisItem = new DomTreeItem( node, parentItem, thisItem );
+	}
 	buildTree( thisItem, node.firstChild() );
+	if ( depth <= 1 ) {
+	    tree->setOpen( thisItem, TRUE );
+	}
 	node = node.nextSibling();
     }
+    depth--;
+}
+
+void DomTree::selectionChanged( QListViewItem *it )
+{
+    text->setText( ((DomTreeItem*)it)->contentString() );
+}
+
+//
+// DomTreeItem
+//
+
+DomTreeItem::DomTreeItem( const QDomNode &node, QListView *parent, QListViewItem *after )
+    : QListViewItem( parent, after )
+{
+    _node = node;
+    init();
+}
+
+DomTreeItem::DomTreeItem( const QDomNode &node, QListViewItem *parent, QListViewItem *after )
+    : QListViewItem( parent, after )
+{
+    _node = node;
+    init();
+}
+
+DomTreeItem::~DomTreeItem()
+{
+}
+
+void DomTreeItem::init()
+{
+    setText( 0, _node.nodeName() );
+
+    switch ( _node.nodeType() ) {
+	case QDomNode::ElementNode:
+	    setText( 1, "Element" );
+	    break;
+	case QDomNode::AttributeNode:
+	    setText( 1, "Attribute" );
+	    break;
+	case QDomNode::CDATASectionNode:
+	    setText( 1, "CDATA Section" );
+	    break;
+	case QDomNode::EntityReferenceNode:
+	    setText( 1, "Entity Reference" );
+	    break;
+	case QDomNode::EntityNode:
+	    setText( 1, "Entity" );
+	    break;
+	case QDomNode::ProcessingInstructionNode:
+	    setText( 1, "PI" );
+	    break;
+	case QDomNode::CommentNode:
+	    setText( 1, "Comment" );
+	    break;
+	case QDomNode::DocumentNode:
+	    setText( 1, "Document" );
+	    break;
+	case QDomNode::DocumentTypeNode:
+	    setText( 1, "Document Type" );
+	    break;
+	case QDomNode::DocumentFragmentNode:
+	    setText( 1, "Document Fragment" );
+	    break;
+	case QDomNode::NotationNode:
+	    setText( 1, "Notation" );
+	    break;
+	case QDomNode::TextNode:
+	    setText( 1, "Character Data" );
+	    break;
+	default:
+	    setText( 1, "" );
+	    break;
+    }
+}
+
+QString DomTreeItem::contentString()
+{
+    QString s;
+    switch ( _node.nodeType() ) {
+	case QDomNode::ElementNode:
+	    {
+		s += "<h3>Text</h3>";
+		s += _node.toElement().text();
+		s += "<hr/>";
+		s += "<h3>Attributes</h3>";
+		QDomNamedNodeMap attributes = _node.toElement().attributes();
+		for ( uint i=0; i< attributes.length(); i++ ) {
+		    s += attributes.item(i).toAttr().name();
+		    s += " = '";
+		    s += attributes.item(i).toAttr().value();
+		    s += "'<br/>";
+		}
+	    }
+	    break;
+	case QDomNode::CDATASectionNode:
+	    s += _node.toCDATASection().data();
+	    break;
+	case QDomNode::ProcessingInstructionNode:
+	    s += "<b>Target: </b>";
+	    s += _node.toProcessingInstruction().target();
+	    s += "<br/>";
+	    s += "<b>Value: </b>";
+	    s += _node.toProcessingInstruction().data();
+	    break;
+	case QDomNode::CommentNode:
+	    s += _node.toComment().data();
+	    break;
+	case QDomNode::DocumentNode:
+	    {
+		QDomDocumentType doctype = _node.toDocument().doctype();
+		s += "<h2>Document Type</h2>";
+		s += doctype.name();
+		s += "<hr/><h3>Entites</h3>";
+		QDomNamedNodeMap entities = doctype.entities();
+		if ( entities.length() > 0 ) {
+		    s += "<ul>";
+		    for ( uint i=0; i< entities.length(); i++ ) {
+			s += "<li>";
+			s += entities.item(i).toEntity().notationName();
+			s += "<b>Public:<b> ";
+			s += entities.item(i).toEntity().publicId();
+			s += "<b>System:<b> ";
+			s += entities.item(i).toEntity().systemId();
+		    }
+		    s += "</ul>";
+		}
+		s += "<hr/><h3>Notations</h3>";
+		QDomNamedNodeMap notations = doctype.notations();
+		if ( notations.length() > 0 ) {
+		    s += "<ul>";
+		    for ( uint i=0; i< notations.length(); i++ ) {
+			s += "<li>";
+			s += notations.item(i).nodeName();
+			s += "<b>Public:<b> ";
+			s += notations.item(i).toNotation().publicId();
+			s += "<b>System:<b> ";
+			s += notations.item(i).toNotation().systemId();
+		    }
+		    s += "</ul>";
+		}
+	    }
+	    break;
+	case QDomNode::TextNode:
+	    s += _node.toText().data();
+	    break;
+	default:
+	    break;
+    }
+    return s;
 }
--- tests/qxml/domtest/domtree.h
+++ tests/qxml/domtest/domtree.h
@@ -1,10 +1,12 @@
+#include <qhbox.h>
+#include <qlistview.h>
+#include <qtextview.h>
+#include <qdom.h>
+
 #ifndef DOMTREE_H
 #define DOMTREE_H
 
-#include <qlistview.h>
-#include <qdom.h>
-
-class DomTree : public QListView
+class DomTree : public QHBox
 {
     Q_OBJECT
 
@@ -12,9 +14,31 @@ public:
     DomTree( const QString fileName, QWidget *parent = 0, const char *name = 0 );
     ~DomTree();
 
+private slots:
+    void selectionChanged( QListViewItem *it );
+
 private:
     QDomDocument domTree;
+    QListView *tree;
+    QTextView *text;
+
     void buildTree( QListViewItem *parentItem, const QDomNode &actNode );
+};
+
+
+class DomTreeItem : public QListViewItem
+{
+public:
+    DomTreeItem( const QDomNode &node, QListView *parent, QListViewItem *after ) ;
+    DomTreeItem( const QDomNode &node, QListViewItem *parent, QListViewItem *after ) ;
+    ~DomTreeItem();
+
+    QString contentString();
+
+private:
+    QDomNode _node;
+
+    void init();
 };
 
 #endif // DOMTREE_H
