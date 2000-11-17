48ff17d2295459c87d20120a162c74f994f97c37
Volker Hilsheimer <vohi@trolltech.com>
2000-11-17 23:42:25 +0100

    Make stylefactory work
    [git-p4: depot-paths = "//depot/qt/main/": change = 30337]

--- src/styles/qstylefactory.cpp
+++ src/styles/qstylefactory.cpp
@@ -1,40 +1,80 @@
 #include "qstylefactory.h"
 
-#ifndef QT_H
-#include "qwindowsstyle.h"
-#include "qmotifstyle.h"
 #include "qinterfacemanager.h"
 #include "qstyleinterface.h"
-#endif // QT_H
+#include "qwindowsstyle.h"
+#include "qmotifstyle.h"
+#if defined(QT_STYLE_CDE)
+#include "qcdestyle.h"
+#endif
+#if defined(QT_STYLE_MOTIFPLUS)
+#include "qmotifplusstyle.h"
+#endif
+#if defined(QT_STYLE_PLATINUM)
+#include "qplatinumstyle.h"
+#endif
+#if defined(QT_STYLE_SGI)
+#include "qsgistyle.h"
+#endif
+#include <stdlib.h>
 
 static QInterfaceManager<QStyleInterface> *manager = 0;
 
 QStyle *QStyleFactory::create( const QString& style )
 {
+    if ( !manager )
+	manager = new QInterfaceManager<QStyleInterface>( "QStyleInterface", QString((char*)getenv( "QTDIR" )) + "/plugins" );
+
+    QStyleInterface *iface = manager->queryInterface( style );
+
+    if ( iface )
+	return iface->create( style );
+
     if ( style == "Windows" )
 	return new QWindowsStyle;
     else if ( style == "Motif" )
 	return new QMotifStyle;
-#if defined(Q_STYLE_CDE)
+#if defined(QT_STYLE_CDE)
     else if ( style == "CDE" )
 	return new QCDEStyle;
 #endif
-#if defined(Q_STYLE_MOTIFPLUS)
+#if defined(QT_STYLE_MOTIFPLUS)
     else if ( style == "MotifPlus" )
 	return new QMotifPlusStyle;
 #endif
-#if defined(Q_STYLE_PLATINUM)
+#if defined(QT_STYLE_PLATINUM)
     else if ( style == "Platinum" )
 	return new QPlatinumStyle;
 #endif
-#if defined(Q_STYLE_SGI)
+#if defined(QT_STYLE_SGI)
     else if ( style == "SGI")
 	return new QSGIStyle;
 #endif
 
-    QInterfaceManager<QStyleInterface> manager( "QStyleInterface" );
+    return 0;
+}
 
-    QStyleInterface *iface = manager.queryInterface( style );
-    
-    return iface ? iface->create( style ) : 0;
+QStringList QStyleFactory::styles()
+{
+    if ( !manager )
+	manager = new QInterfaceManager<QStyleInterface>( "QStyleInterface", QString((char*)getenv( "QTDIR" )) + "/plugins" );
+
+    QStringList list = manager->featureList();
+
+    list << "Windows";
+    list << "Motif";
+#if defined(QT_STYLE_CDE)
+    list << "CDE";
+#endif
+#if defined(QT_STYLE_MOTIFPLUS)
+    list << "MotifPlus";
+#endif
+#if defined(QT_STYLE_PLATINUM)
+    list << "Platinum";
+#endif
+#if defined(QT_STYLE_SGI)
+    list << "SGI";
+#endif
+
+    return list;
 }
--- src/styles/qstylefactory.h
+++ src/styles/qstylefactory.h
@@ -1,6 +1,10 @@
 #ifndef QSTYLEFACTORY_H
 #define QSTYLEFACTORY_H
 
+#ifndef QT_H
+#include "qstringlist.h"
+#endif // QT_H
+
 class QString;
 class QStyle;
 
@@ -8,6 +12,7 @@ class QStyleFactory
 {
 public:
     static QStyle *create( const QString& );
+    static QStringList styles();
 };
 
 #endif //QSTYLEFACTORY_H
