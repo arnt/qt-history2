8620de8eff1a6105f8bb2fe869e495f3cf4ee8d1
Dave Berton <db@trolltech.com>
2000-11-23 15:22:17 +0100

    Update to new API
    [git-p4: depot-paths = "//depot/qt/main/": change = 30650]

--- tests/sqloratest/sqloratest.cpp
+++ tests/sqloratest/sqloratest.cpp
@@ -1,6 +1,7 @@
 #include <qstring.h>
-#include <qsql.h>
+#include <qsqlquery.h>
 #include <qsqldatabase.h>
+#include <qsqldriver.h>
 #include <qapplication.h>
 #include <stdio.h>
 
@@ -12,16 +13,16 @@ QSqlDatabase* database;
 
 void TestInsert()
 {
-    qDebug("Inserting records...");
-    if ( database->hasTransactionSupport() )
+    if ( database->driver()->hasTransactionSupport() ) {
 	database->transaction();
-    for ( int i=0; i<TEST_RECS; ++i ) {
-	int r = database->exec( "insert into qsql_test values (" + QString::number(i) + ",'foo:" + QString::number(i) + "','blarg " + QString::number(i*10) + " blarg'," + QString::number(i*12.345) + ", '18-APR-1972');");
-	Q_ASSERT( r == 1 );
     }
-    int r = database->exec( "insert into qsql_test values (NULL,NULL,'should have nulls',NULL,NULL);");
-    Q_ASSERT( r == 1);
-    if ( database->hasTransactionSupport() )
+    for ( int i=0; i<TEST_RECS; ++i ) {
+	QSqlQuery r = database->exec( "insert into qsql_test values (" + QString::number(i) + ",'foo:" + QString::number(i) + "','blarg " + QString::number(i*10) + " blarg'," + QString::number(i*12.345) + ", '18-APR-1972');");
+	Q_ASSERT( r.numRowsAffected() == 1 );
+    }
+    QSqlQuery r = database->exec( "insert into qsql_test values (NULL,NULL,'should have nulls',NULL,NULL);");
+    Q_ASSERT( r.numRowsAffected() == 1);
+    if ( database->driver()->hasTransactionSupport() )
 	database->commit();
 }
 
@@ -37,14 +38,16 @@ void TestSelect()
 //    else
 //	qDebug("FAILED counting records: " + recCount.lastError().databaseText());
 
-    QSql selRecs = database->query( "select id, field1, field2, field3, field4 from qsql_test;");
+    QSqlQuery selRecs = database->exec( "select id, field1, field2, field3, field4 from qsql_test;");
     if ( !selRecs.isActive() ) {
 	qDebug(selRecs.lastError().databaseText());
     }
     int i = 0;
     while( selRecs.next() ) {
 	i++;
-	qDebug("data:" + selRecs[0].toString() + " " + selRecs[1].toString() + " " + selRecs[2].toString() + " " + selRecs[3].toString() + " " + selRecs[4].toString());
+	qDebug("data:" + selRecs.value(0).toString() + " " + selRecs.value(1).toString() + 
+	       " " + selRecs.value(2).toString() + " " + selRecs.value(3).toString() + 
+	       " " + selRecs.value(4).toString());
 	if ( selRecs.isNull(0) )
 	    qDebug("field1 IS NULL");
     }
@@ -76,29 +79,29 @@ void TestSelect()
 void TestDelete()
 {
     qDebug("Deleting records...");
-    if ( database->hasTransactionSupport() )
+    if ( database->driver()->hasTransactionSupport() )
 	database->transaction();
     for ( int i=0; i<TEST_RECS; ++i ) {
 	database->exec( "delete from qsql_test where id=" + QString::number(i) + ";" );
     }
-    if ( database->hasTransactionSupport() )
+    if ( database->driver()->hasTransactionSupport() )
 	database->commit();
 }
 
 void TestTrans()
 {
-    if ( database->hasTransactionSupport() ) {
+    if ( database->driver()->hasTransactionSupport() ) {
 	qDebug("Testing transaction...");
 	database->transaction();
 	for ( int i=0; i<TEST_RECS; ++i ) {
-	    database->exec( "insert into qsql_test values (" + QString::number(i) + ",'" + QString::number(i) + "');");
+	    database->exec( "insert into qsql_test values (" + QString::number(i) + ",'foo:" + QString::number(i) + "','blarg " + QString::number(i*10) + " blarg'," + QString::number(i*12.345) + ", '18-APR-1972');");	    
 	}
 	if ( database->rollback() )
 	    qDebug("Transaction successful.");
 	else {
-	    QSql recsRemaining = database->query("select count(1) from qsql_test;");
+	    QSqlQuery recsRemaining = database->exec("select count(1) from qsql_test;");
 	    recsRemaining.next();
-    	    qDebug("Transaction FAILURE.  Records remaining in table:" + recsRemaining[0].toString() );
+    	    qDebug("Transaction FAILURE.  Records remaining in table:" + recsRemaining.value(0).toString() );
     	}
     }
 }
@@ -108,22 +111,25 @@ int main()
     printf("Qt Oracle Test\n\n");
 
     printf("Opening database...\n");
-    database = new QSqlDatabase( "QOCI" );
-    database->reset("test", "db", "db", "iceblink");
+    database = QSqlDatabase::addDatabase( "QOCI" );
+    database->setDatabaseName( "trol" );
+    database->setUserName( "scott" );
+    database->setPassword( "tiger" );
+    database->setHostName( "anarki" );
     database->open();
+  
     if ( database->isOpen() )
 	qDebug("...success.");
     else
 	qDebug("...FAILED.");
 
     qDebug("Creating sample table...");
-    int count = database->exec( "create table qsql_test"
+    database->exec( "create table qsql_test"
 		    "(id number(10),"
 		    "field1 char(10),"
 		    "field2 varchar(200),"
 		    "field3 decimal(15,2),"
 		    "field4 date);");
-    qDebug("create table result:" + QString::number(count));
     qDebug("creating list of database tables...");
     QStringList tables = database->tables();
     for ( uint i = 0; i < tables.count(); ++i )
@@ -132,7 +138,8 @@ int main()
     TestInsert();
     TestSelect();
     TestDelete();
-//    TestTrans();
+    TestTrans();
+    
     qDebug("Dropping sample table...");
     database->exec( "drop table qsql_test;" );
 
