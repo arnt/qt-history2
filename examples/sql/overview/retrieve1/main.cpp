44af65e919c8483bb94a72ebfce98c44df16a9ad
Dave Berton <db@trolltech.com>
2000-12-07 10:43:36 +0100

    Use qDebug(), not debug(). Fix db connect information
    [git-p4: depot-paths = "//depot/qt/main/": change = 31481]

--- examples/sql/overview/retrieve1/main.cpp
+++ examples/sql/overview/retrieve1/main.cpp
@@ -21,7 +21,7 @@ int main( int argc, char *argv[] )
 	QSqlQuery q( "SELECT id, surname FROM staff;" );
 	if ( q.isActive() ) {
 	    while ( q.next() ) {
-		debug( q.value(0).toString() + ": " + 
+		qDebug( q.value(0).toString() + ": " +
 		       q.value(1).toString() );
 	    }
 	}
@@ -34,25 +34,25 @@ int main( int argc, char *argv[] )
 bool create_connections()
 {
 
-    QSqlDatabase *defaultDB = QSqlDatabase::addDatabase( "QPSQL6" );
-    defaultDB->setDatabaseName( "testdb" );
-    defaultDB->setUserName( "db" );
-    defaultDB->setPassword( "db" );
-    defaultDB->setHostName( "silverfish" );
-    if ( ! defaultDB->open() ) { 
-	qWarning( "Failed to open sales database: " + 
+    QSqlDatabase *defaultDB = QSqlDatabase::addDatabase( "QODBC" );
+    defaultDB->setDatabaseName( "sales" );
+    defaultDB->setUserName( "salesuser" );
+    defaultDB->setPassword( "salespw" );
+    defaultDB->setHostName( "saleshost" );
+    if ( ! defaultDB->open() ) {
+	qWarning( "Failed to open sales database: " +
 		  defaultDB->lastError().driverText() );
 	qWarning( defaultDB->lastError().databaseText() );
 	return false;
     }
 
-    QSqlDatabase *oracle = QSqlDatabase::addDatabase( "QPSQL6", "ORACLE" );
-    oracle->setDatabaseName( "pingpong" );
-    oracle->setUserName( "db" );
-    oracle->setPassword( "db" );
-    oracle->setHostName( "silverfish" );
+    QSqlDatabase *oracle = QSqlDatabase::addDatabase( "QOCI", "ORACLE" );
+    oracle->setDatabaseName( "orders" );
+    oracle->setUserName( "ordersuser" );
+    oracle->setPassword( "orderspw" );
+    oracle->setHostName( "ordershost" );
     if ( ! oracle->open() ) {
-	qWarning( "Failed to open orders database: " + 
+	qWarning( "Failed to open orders database: " +
 		  oracle->lastError().driverText() );
 	qWarning( oracle->lastError().databaseText() );
 	return false;
@@ -60,6 +60,3 @@ bool create_connections()
 
     return true;
 }
-
-
-
