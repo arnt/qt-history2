bdbd609481393f397b89e3053384a28c4d5fdc0e
Dave Berton <db@trolltech.com>
2000-12-07 10:28:57 +0100

    More comments
    [git-p4: depot-paths = "//depot/qt/main/": change = 31480]

--- examples/sql/overview/create_connections/main.cpp
+++ examples/sql/overview/create_connections/main.cpp
@@ -16,9 +16,10 @@ bool create_connections();
 int main( int argc, char *argv[] )
 {
     if ( create_connections() ) {
-        // Databases successfully opened; get pointers to them:
-        QSqlDatabase *oracledb = QSqlDatabase::database( "ORACLE" );
-        // Now we can now issue SQL commands.
+	// Databases successfully opened; get pointers to them:
+	QSqlDatabase *oracledb = QSqlDatabase::database( "ORACLE" );
+	// Now we can now issue SQL commands to the oracle connection
+	// or to the default connection
     }
 
     return 0;
@@ -26,30 +27,31 @@ int main( int argc, char *argv[] )
 
 bool create_connections()
 {
+    // create the default database connection
     QSqlDatabase *defaultDB = QSqlDatabase::addDatabase( "QODBC" );
     defaultDB->setDatabaseName( "sales" );
     defaultDB->setUserName( "salesuser" );
     defaultDB->setPassword( "salespw" );
     defaultDB->setHostName( "saleshost" );
-    if ( ! defaultDB->open() ) { 
-        qWarning( "Failed to open sales database: " + 
-                  defaultDB->lastError().driverText() );
-        qWarning( defaultDB->lastError().databaseText() );
-        return false;
+    if ( ! defaultDB->open() ) {
+	qWarning( "Failed to open sales database: " +
+		  defaultDB->lastError().driverText() );
+	qWarning( defaultDB->lastError().databaseText() );
+	return false;
     }
 
+    // create a named connection to oracle
     QSqlDatabase *oracle = QSqlDatabase::addDatabase( "QOCI", "ORACLE" );
     oracle->setDatabaseName( "orders" );
     oracle->setUserName( "ordersuser" );
     oracle->setPassword( "orderspw" );
     oracle->setHostName( "ordershost" );
     if ( ! oracle->open() ) {
-        qWarning( "Failed to open orders database: " + 
-                  oracle->lastError().driverText() );
-        qWarning( oracle->lastError().databaseText() );
-        return false;
+	qWarning( "Failed to open orders database: " +
+		  oracle->lastError().driverText() );
+	qWarning( oracle->lastError().databaseText() );
+	return false;
     }
 
     return true;
 }
-
