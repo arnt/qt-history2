85036570eb72e74dc85c262079441c2a0d9742f4
Mark Summerfield <mark@trolltech.com>
2000-12-08 18:47:01 +0100

    More updates to sql.doc.    Added subclass5 example.
    [git-p4: depot-paths = "//depot/qt/main/": change = 31664]

---
+++ examples/sql/overview/subclass5/main.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+** $Id$
+**
+** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#include <qapplication.h>
+#include <qdatetime.h>
+#include <qsqldatabase.h>
+#include <qsqlcursor.h>
+#include <qsqltable.h>
+
+class QSqlRecord;
+
+bool create_connections();
+
+
+class InvoiceItemCursor : public QSqlCursor
+{
+    public:
+	InvoiceItemCursor();
+	QSqlRecord *primeInsert();
+    protected:
+	QVariant calculateField( const QString & name );
+};
+
+
