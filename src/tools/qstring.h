be57fce21d665b6979c2cd83b246410dbed71ed7
Harri Porten <porten@froglogic.com>
2001-01-05 14:44:45 +0100

    integrated from 2.2
    [git-p4: depot-paths = "//depot/qt/main/": change = 32682]

--- src/tools/qstring.h
+++ src/tools/qstring.h
@@ -671,7 +671,7 @@ inline QString::QString() :
 //
 inline QString::~QString()
 {
-    if ( d != 0 && d->deref() ) {
+    if ( d->deref() ) {
         if ( d == shared_null )
             shared_null = 0;
         d->deleteSelf();
