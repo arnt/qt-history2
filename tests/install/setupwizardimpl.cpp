94859006802b1aa27c2ae2c382b36688b19bc950
ebakke <ebakke@trolltech.com>
2001-01-08 04:07:15 +0100

    The installer GUI is ready. Now we need to decide how we want to package the distribution files
    [git-p4: depot-paths = "//depot/qt/main/": change = 32773]

--- tests/install/setupwizardimpl.cpp
+++ tests/install/setupwizardimpl.cpp
@@ -1,10 +1,34 @@
 #include "setupwizardimpl.h"
+#include <qfiledialog.h>
+#include <qlineedit.h>
 
 SetupWizardImpl::SetupWizardImpl( QWidget* pParent, const char* pName, bool modal, WFlags f ) : SetupWizard( pParent, pName, modal, f )
 {
+    connect( this, SIGNAL( selected( const QString& ) ), this, SLOT( changedPage( const QString& ) ) );
+    setFinishEnabled( finishPage, true );
+    setBackEnabled( progressPage, false );
+    setBackEnabled( finishPage, false );
 }
 
 void SetupWizardImpl::clickedPath()
 {
-    qDebug( "Local function");
+    QFileDialog dlg( installPath->text(), QString::null, this, NULL, true );
+
+    dlg.setMode( QFileDialog::DirectoryOnly );
+    if( dlg.exec() )
+    {
+	installPath->setText( dlg.dir()->absPath() );
+    }
+}
+
+void SetupWizardImpl::clickedSystem( int sys )
+{
+    sysID = sys;
+}
+
+void SetupWizardImpl::changedPage( const QString& pageName )
+{
+    if( pageName == "Installing" ) {
+	
+    }
 }
--- tests/install/setupwizardimpl.h
+++ tests/install/setupwizardimpl.h
@@ -7,4 +7,11 @@ public:
     SetupWizardImpl( QWidget* pParent = NULL, const char* pName = NULL, bool modal = FALSE, WFlags f = 0 );
 
     virtual void clickedPath();
+    virtual void clickedSystem( int );
+
+public slots:
+    virtual void changedPage( const QString& );
+
+private:
+    int sysID;
 };
