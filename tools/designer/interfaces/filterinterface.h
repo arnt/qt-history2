0c930641c467216d9cfab93118b31c8be36b1190
Volker Hilsheimer <vohi@trolltech.com>
2001-01-11 08:51:59 +0100

    Adjust designer(plugin) interfaces to new QUuid stuff
    [git-p4: depot-paths = "//depot/qt/main/": change = 33062]

--- tools/designer/interfaces/filterinterface.h
+++ tools/designer/interfaces/filterinterface.h
@@ -2,10 +2,12 @@
 #define FILTERINTERFACE_H
 
 #include <qcomponentinterface.h>
+#include <qstringlist.h>
 
 // {EA8CB381-59B5-44a8-BAE5-9BEA8295762A}
-Q_UUID( IID_ImportFilterInterface,
-	0xea8cb381, 0x59b5, 0x44a8, 0xba, 0xe5, 0x9b, 0xea, 0x82, 0x95, 0x76, 0x2a);
+#ifndef IID_ImportFilterInterface
+#define IID_ImportFilterInterface QUuid( 0xea8cb381, 0x59b5, 0x44a8, 0xba, 0xe5, 0x9b, 0xea, 0x82, 0x95, 0x76, 0x2a )
+#endif
 
 struct ImportFilterInterface : public QUnknownInterface
 {
@@ -14,8 +16,9 @@ struct ImportFilterInterface : public QUnknownInterface
 };
 
 // {C32A07E0-B006-471e-AFCA-D227457A1280}
-Q_UUID( IID_ExportFilterInterface,
-	0xc32a07e0, 0xb006, 0x471e, 0xaf, 0xca, 0xd2, 0x27, 0x45, 0x7a, 0x12, 0x80);
+#ifndef IID_ExportFilterInterface
+#define IID_ExportFilterInterface QUuid( 0xc32a07e0, 0xb006, 0x471e, 0xaf, 0xca, 0xd2, 0x27, 0x45, 0x7a, 0x12, 0x80 )
+#endif
 
 struct ExportFilterInterface : public QUnknownInterface
 {
--- tools/designer/interfaces/templatewizardiface.h
+++ tools/designer/interfaces/templatewizardiface.h
@@ -5,11 +5,12 @@
 #include <qstringlist.h>
 
 class QWidget;
-class DesignerFormWindow;
+struct DesignerFormWindow;
 
 // {983d3eab-fea3-49cc-97ad-d8cc89b7c17b}
-Q_UUID( IID_TemplateWizardInterface,
-	0x983d3eab, 0xfea3, 0x49cc, 0x97, 0xad, 0xd8, 0xcc, 0x89, 0xb7, 0xc1, 0x7b );
+#ifndef IID_TemplateWizardInterface
+#define IID_TemplateWizardInterface QUuid( 0x983d3eab, 0xfea3, 0x49cc, 0x97, 0xad, 0xd8, 0xcc, 0x89, 0xb7, 0xc1, 0x7b )
+#endif
 
 class TemplateWizardInterface : public QUnknownInterface
 {
