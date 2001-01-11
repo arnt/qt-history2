34d9513381cee4e88de905c2c7b5ca1a9da100d5
Rainer Schmidt <rms@froglogic.com>
2001-01-11 16:16:10 +0100

    add copyright header
    [git-p4: depot-paths = "//depot/qt/main/": change = 33126]

--- examples/process/process.cpp
+++ examples/process/process.cpp
@@ -1,3 +1,13 @@
+/****************************************************************************
+** $Id: //depot/qt/main/examples/process/process.cpp#5 $
+**
+** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
 #include <qobject.h>
 #include <qprocess.h>
 #include <qvbox.h>
