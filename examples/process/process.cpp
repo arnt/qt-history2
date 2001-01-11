2e46c911e9de9fbfe5a5f2e4654e65eaae27d8eb
Rainer Schmidt <rms@froglogic.com>
2001-01-11 16:04:24 +0100

    this is nicer when I want to use the example from the class documentation
    [git-p4: depot-paths = "//depot/qt/main/": change = 33121]

--- examples/process/process.cpp
+++ examples/process/process.cpp
@@ -11,54 +11,12 @@ class UicManager : public QVBox
     Q_OBJECT
 
 public:
-    UicManager()
-    {
-	// Layout
-	output = new QTextView( this );
-	quitButton = new QPushButton( tr("Quit"), this );
-	connect( quitButton, SIGNAL(clicked()),
-		qApp, SLOT(quit()) );
-	resize( 500, 500 );
-
-	// QProcess related code
-	proc = new QProcess( this );
-
-	// Set up the command and arguments.
-	// On the command line you would do:
-	//   uic -tr i18n "small dialog.ui"
-	proc->addArgument( "uic" );
-	proc->addArgument( "-tr" );
-	proc->addArgument( "i18n" );
-	proc->addArgument( "small dialog.ui" );
-
-	connect( proc, SIGNAL(readyReadStdout()),
-		this, SLOT(readFromStdout()) );
-	connect( proc, SIGNAL(processExited()),
-		this, SLOT(scrollToTop()) );
-
-	if ( !proc->start() ) {
-	    // error handling
-	    QMessageBox::critical( 0,
-		    tr("Fatal error"),
-		    tr("Could not start the uic command."),
-		    tr("Quit") );
-	    exit( -1 );
-	}
-    }
+    UicManager();
     ~UicManager() {}
 
 public slots:
-    void readFromStdout()
-    {
-	// Read and process the data.
-	// Keep in mind that the data might be reported in chunks.
-	output->append( proc->readStdout() );
-    }
-
-    void scrollToTop()
-    {
-	output->setContentsPos( 0, 0 );
-    }
+    void readFromStdout();
+    void scrollToTop();
 
 private:
     QProcess *proc;
@@ -66,6 +24,52 @@ private:
     QPushButton *quitButton;
 };
 
+UicManager::UicManager()
+{
+    // Layout
+    output = new QTextView( this );
+    quitButton = new QPushButton( tr("Quit"), this );
+    connect( quitButton, SIGNAL(clicked()),
+	    qApp, SLOT(quit()) );
+    resize( 500, 500 );
+
+    // QProcess related code
+    proc = new QProcess( this );
+
+    // Set up the command and arguments.
+    // On the command line you would do:
+    //   uic -tr i18n "small dialog.ui"
+    proc->addArgument( "uic" );
+    proc->addArgument( "-tr" );
+    proc->addArgument( "i18n" );
+    proc->addArgument( "small dialog.ui" );
+
+    connect( proc, SIGNAL(readyReadStdout()),
+	    this, SLOT(readFromStdout()) );
+    connect( proc, SIGNAL(processExited()),
+	    this, SLOT(scrollToTop()) );
+
+    if ( !proc->start() ) {
+	// error handling
+	QMessageBox::critical( 0,
+		tr("Fatal error"),
+		tr("Could not start the uic command."),
+		tr("Quit") );
+	exit( -1 );
+    }
+}
+
+void UicManager::readFromStdout()
+{
+    // Read and process the data.
+    // Keep in mind that the data might be reported in chunks.
+    output->append( proc->readStdout() );
+}
+
+void UicManager::scrollToTop()
+{
+    output->setContentsPos( 0, 0 );
+}
 
 int main( int argc, char **argv )
 {
