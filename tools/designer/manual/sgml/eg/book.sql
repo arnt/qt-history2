43b6a767169ec1b93bd5e8b5e9ff81b0b54df232
Mark Summerfield <mark@trolltech.com>
2001-01-15 09:26:15 +0100

    Minor change.
    [git-p4: depot-paths = "//depot/qt/main/": change = 33305]

--- tools/designer/manual/sgml/eg/book.sql
+++ tools/designer/manual/sgml/eg/book.sql
@@ -1,48 +1,48 @@
-drop table author;
-drop table book;
-drop table format;
-drop table author_seq;
-drop table book_seq;
-drop table format_seq;
-drop view book_view;
+DROP TABLE author;
+DROP TABLE book;
+DROP TABLE format;
+DROP TABLE author_seq;
+DROP TABLE book_seq;
+DROP TABLE format_seq;
+DROP VIEW book_view;
 
-create sequence author_seq;
-create sequence book_seq;
-create sequence format_seq;
+CREATE SEQUENCE author_seq;
+CREATE SEQUENCE book_seq;
+CREATE SEQUENCE format_seq;
 
-create table author ( id int primary key, forename varchar(40), surname varchar(40) );
-create table book ( id int primary key, title varchar(40), price float, authorid int, formatid int );
-create table format ( id int primary key, name varchar(20) );
-create view book_view as ( select book.id as id, book.title as title,
-book.price as price, authorid, format.name as format from book, author,
-format where book.authorid=author.id and book.formatid=format.id );
-create view author_view as ( select author.id as id, author.forename || ' ' || author.surname as name);
+CREATE TABLE author ( id int primary key, forename varchar(40), surname varchar(40) );
+CREATE TABLE book ( id int primary key, title varchar(40), price float, authorid int, formatid int );
+CREATE TABLE format ( id int primary key, name varchar(20) );
+CREATE VIEW book_view AS ( SELECT book.id AS id, book.title AS title,
+book.price AS price, authorid, format.name AS format FROM book, author,
+format WHERE book.authorid=author.id AND book.formatid=format.id );
+CREATE VIEW author_view AS ( SELECT author.id AS id, author.forename || ' ' || author.surname AS name);
 
-select nextval( 'author_seq' );
-select nextval( 'author_seq' );
-select nextval( 'author_seq' );
-insert into author values ( 1, 'Philip K', 'Dick' );
-insert into author values ( 2, 'Robert', 'Heinlein' );
-insert into author values ( 3, 'Sarah', 'Paretsky' );
+SELECT nextval( 'author_seq' );
+SELECT nextval( 'author_seq' );
+SELECT nextval( 'author_seq' );
+INSERT INTO author VALUES ( 1, 'Philip K', 'Dick' );
+INSERT INTO author VALUES ( 2, 'Robert', 'Heinlein' );
+INSERT INTO author VALUES ( 3, 'Sarah', 'Paretsky' );
 
-select nextval( 'book_seq' );
-select nextval( 'book_seq' );
-select nextval( 'book_seq' );
-select nextval( 'book_seq' );
-select nextval( 'book_seq' );
-select nextval( 'book_seq' );
-insert into book values ( 1, 'The Man Who Japed', 6.99, 1, 2 );
-insert into book values ( 2, 'The Man in the High Castle', 9.99, 1, 1 );
-insert into book values ( 3, 'The Number of the Beast', 8.99, 2, 3 );
-insert into book values ( 4, 'Indemnity Only', 9.99, 3, 4 );
-insert into book values ( 5, 'Burn Marks', 9.99, 3, 1 );
-insert into book values ( 6, 'Deadlock', 9.99, 3, 1 );
+SELECT nextval( 'book_seq' );
+SELECT nextval( 'book_seq' );
+SELECT nextval( 'book_seq' );
+SELECT nextval( 'book_seq' );
+SELECT nextval( 'book_seq' );
+SELECT nextval( 'book_seq' );
+INSERT INTO book VALUES ( 1, 'The Man Who Japed', 6.99, 1, 2 );
+INSERT INTO book VALUES ( 2, 'The Man in the High Castle', 9.99, 1, 1 );
+INSERT INTO book VALUES ( 3, 'The Number of the Beast', 8.99, 2, 3 );
+INSERT INTO book VALUES ( 4, 'Indemnity Only', 9.99, 3, 4 );
+INSERT INTO book VALUES ( 5, 'Burn Marks', 9.99, 3, 1 );
+INSERT INTO book VALUES ( 6, 'Deadlock', 9.99, 3, 1 );
 
-select nextval( 'format_seq' );
-select nextval( 'format_seq' );
-select nextval( 'format_seq' );
-select nextval( 'format_seq' );
-insert into format values ( 1, 'Paperback' );
-insert into format values ( 2, 'Hardback' );
-insert into format values ( 3, 'Softback' );
-insert into format values ( 4, 'Ringbound' );
+SELECT nextval( 'format_seq' );
+SELECT nextval( 'format_seq' );
+SELECT nextval( 'format_seq' );
+SELECT nextval( 'format_seq' );
+INSERT INTO format VALUES ( 1, 'Paperback' );
+INSERT INTO format VALUES ( 2, 'Hardback' );
+INSERT INTO format VALUES ( 3, 'Softback' );
+INSERT INTO format VALUES ( 4, 'Ringbound' );
