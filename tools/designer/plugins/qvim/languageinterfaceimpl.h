fd7d79945247106c510e94510b567588617968a8
Dave Berton <db@trolltech.com>
2001-01-15 14:52:35 +0100

    Port vim to Qt.
    This is intended as a Qt/Designer editor plugin example.
    See INSTALL.qvim for build instructions.
    Non-functional currently (although designer will load it, and you can
    edit some text (sort of))
    This plugin will not go out in snapshots.
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 33333]

---
+++ tools/designer/plugins/qvim/languageinterfaceimpl.h
@@ -0,0 +1,24 @@
+#ifndef LANGUAGEINTERFACEIMPL_H
+#define LANGUAGEINTERFACEIMPL_H
+
+#include <languageinterface.h>
+
+class LanguageInterfaceImpl : public LanguageInterface
+{
+public:
+    LanguageInterfaceImpl();
+
+    QUnknownInterface *queryInterface( const QUuid& );
+    unsigned long addRef();
+    unsigned long release();
+
+    QStringList featureList() const;
+    void functions( const QString &code, QMap<QString, QString> *funcs ) const;
+    QString createFunctionStart( const QString &className, const QString &func );
+
+private:
+    ulong ref;
+
+};
+
+#endif
