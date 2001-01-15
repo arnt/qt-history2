a21aa5e86c10d8b8d35ab8e7d1f94324e2bd3568
Reginald Stadlbauer <reggie@froglogic.com>
2001-01-15 01:08:21 +0100

    Even cooler completion
    [git-p4: depot-paths = "//depot/qt/main/": change = 33290]

--- tools/designer/plugins/cppeditor/cppcompletion.cpp
+++ tools/designer/plugins/cppeditor/cppcompletion.cpp
@@ -13,7 +13,7 @@ bool CppEditorCompletion::doObjectCompletion( const QString &object )
 {
     if ( !ths )
 	return FALSE;
-    QObject *obj;
+    QObject *obj = 0;
     if ( ths->name() == object ) {
 	obj = ths;
     } else {
@@ -28,7 +28,7 @@ bool CppEditorCompletion::doObjectCompletion( const QString &object )
     for ( QListIterator<char> sit( slts ); sit.current(); ++sit ) {
 	QString f( sit.current() );
 	f = f.left( f.find( "(" ) ) + "()";
-	if ( lst.find( f ) != lst.end() )
+	if ( lst.find( f ) == lst.end() )
 	    lst << f;
     }
     QStrList props = obj->metaObject()->propertyNames( TRUE );
@@ -39,7 +39,7 @@ bool CppEditorCompletion::doObjectCompletion( const QString &object )
 	f.prepend( c.upper() );
 	f.prepend( "set" );
 	f += "()";
-	if ( lst.find( f ) != lst.end() )
+	if ( lst.find( f ) == lst.end() )
 	    lst << f;
     }
 
@@ -53,8 +53,89 @@ bool CppEditorCompletion::doObjectCompletion( const QString &object )
     return TRUE;
 }
 
-QStringList CppEditorCompletion::functionParameters( const QString &, QChar & )
+QStringList CppEditorCompletion::functionParameters( const QString &expr, QChar &separator )
 {
+    separator = ',';
+    if ( !ths )
+	return QStringList();
+    QString func;
+    QString objName;
+    int i = -1;
+
+    i = expr.findRev( "->" );
+    if ( i == -1 )
+	i = expr.findRev( "." );
+    else
+	++i;
+    if ( i == -1 ) {
+	i = expr.findRev( " " );
+
+	if ( i == -1 )
+	    i = expr.findRev( "\t" );
+	else
+	    objName = ths->name();
+	
+	if ( i == -1 && expr[ 0 ] != ' ' && expr[ 0 ] != '\t' )
+	    objName = ths->name();
+    }
+
+    qDebug( objName );
+
+    if ( !objName.isEmpty() ) {
+	func = expr.mid( i + 1 );
+	func = func.simplifyWhiteSpace();
+    } else {
+	func = expr.mid( i + 1 );
+	func = func.simplifyWhiteSpace();
+	QString ex( expr );
+	ex.remove( i, 0xFFFFFF );
+	if ( ex[ (int)ex.length() - 1 ] == '-' )
+	    ex.remove( ex.length() - 1, 1 );
+	int j = -1;
+	j = ex.findRev( "->" );
+	if ( j == -1 )
+	    j = ex.findRev( "." );
+	else
+	    ++j;
+	if ( j == -1 ) {
+	    j = ex.findRev( " " );
+
+	    if ( j == -1 )
+		j = ex.findRev( "\t" );
+	    else
+		objName = ths->name();
+
+	    if ( j == -1 )
+		objName = ths->name();
+	}
+	objName = ex.mid( j + 1 );
+	objName = objName.simplifyWhiteSpace();
+    }
+
+    QObject *obj = 0;
+    if ( ths->name() == objName ) {
+	obj = ths;
+    } else {
+	obj = ths->child( objName );
+    }
+
+    if ( !obj )
+	return QStringList();
+
+    QStrList slts = obj->metaObject()->slotNames( TRUE );
+    for ( QListIterator<char> sit( slts ); sit.current(); ++sit ) {
+	QString f( sit.current() );
+	f = f.left( f.find( "(" ) );
+	if ( f == func ) {
+	    f = QString( sit.current() );
+	    f.remove( 0, f.find( "(" ) + 1 );
+	    f = f.left( f.find( ")" ) );
+	    QStringList lst = QStringList::split( ',', f );
+	    if ( !lst.isEmpty() )
+		return lst;
+	}
+    }
+
     return QStringList();
 }
 
