9fff49aa9b0b9c444aaddc7711ac04a068534dc9
Martin Jones <mjones@trolltech.com>
2001-01-16 03:23:14 +0100

    Add new files
    [git-p4: depot-paths = "//depot/qt/main/": change = 33420]

--- src/kernel/qt.h
+++ src/kernel/qt.h
@@ -197,9 +197,11 @@
 #include "qeditorfactory.h"
 #include "qsqlindex.h"
 #include <qsqldriver.h>
+#include <qsqldataview.h>
 #include <qsqlform.h>
 #include "qsqlcursor.h"
 #include "qsqlnavigator.h"
+#include <qsqldataform.h>
 #include <qsqlpropertymap.h>
 #include <qsqldatabase.h>
 #include <qsqlresult.h>
