3b948cbc7b6bc0ba2730d7235c1c45cf9c915f8c
Jasmin Blanchette <jasmin@trolltech.com>
2001-01-16 16:32:31 +0100

    Fix operator= bug. Now, things are starting to look good.
    [git-p4: depot-paths = "//depot/qt/main/": change = 33479]

--- util/qdoc/doc.cpp
+++ util/qdoc/doc.cpp
@@ -1198,6 +1198,7 @@ ExampleLocation& ExampleLocation::operator=( const ExampleLocation& el )
     ininc = el.ininc;
     exfile = el.exfile;
     ln = el.ln;
+    uniq = el.uniq;
     return *this;
 }
 
