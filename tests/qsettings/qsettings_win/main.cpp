4016a8f4fbed57d54a8f442d01f96545be724bd5
Volker Hilsheimer <vohi@trolltech.com>
2001-01-17 19:44:41 +0100

    Test changed
    [git-p4: depot-paths = "//depot/qt/main/": change = 33599]

--- tests/qsettings/qsettings_win/main.cpp
+++ tests/qsettings/qsettings_win/main.cpp
@@ -3,15 +3,15 @@
 int main ( int argc, char **argv )
 {
     QSettings settings;
-    settings.writeEntry( "/Software/Trolltech/Qt/QSettings/string", "String" );
-	settings.writeEntry( "/Software/Trolltech/Qt/QSettings/int", 0xffff );
-	settings.writeEntry( "/Software/Trolltech/Qt/QSettings/bool", true );
-	settings.writeEntry( "/Software/Trolltech/Qt/QSettings/double", 3.1415927 );
+    settings.writeEntry( "/Trolltech/Qt/QSettings/string", "String" );
+    settings.writeEntry( "/Trolltech/Qt/QSettings/int", 0xffff );
+    settings.writeEntry( "/Trolltech/Qt/QSettings/bool", true );
+    settings.writeEntry( "/Trolltech/Qt/QSettings/double", 3.1415927 );
 
-	qDebug( settings.readEntry( "/Software/Trolltech/Qt/QSettings/string" ) );
-	qDebug( "%d", settings.readNumEntry( "/Software/Trolltech/Qt/QSettings/int" ) );
-	qDebug( "%d", settings.readBoolEntry( "/Software/Trolltech/Qt/QSettings/bool" ) );
-	qDebug( "%g", settings.readDoubleEntry( "/Software/Trolltech/Qt/QSettings/double" ) );
+    qDebug( settings.readEntry( "/Trolltech/Qt/QSettings/string" ) );
+    qDebug( "%d", settings.readNumEntry( "/Trolltech/Qt/QSettings/int" ) );
+    qDebug( "%d", settings.readBoolEntry( "/Trolltech/Qt/QSettings/bool" ) );
+    qDebug( "%g", settings.readDoubleEntry( "/Trolltech/Qt/QSettings/double" ) );
 
-	return 0;
+    return 0;
 }
