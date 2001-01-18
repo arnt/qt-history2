fe5ea070e1750b3ec79773ad566bcd3da4a8acb0
Dave Berton <db@trolltech.com>
2001-01-18 17:53:44 +0100

    Update to new API
    [git-p4: depot-paths = "//depot/qt/main/": change = 33669]

--- tests/sqlbrowse/resultwindow.cpp
+++ tests/sqlbrowse/resultwindow.cpp
@@ -35,7 +35,7 @@ void ResultWindow::slotExec()
     sql.setName( tableList->currentText() );
     sql.setSort( sql.primaryIndex() );
     dataGrid->setCursor( &sql, TRUE );
-    dataGrid->refresh( TRUE, TRUE );
+    dataGrid->refresh( QDataTable::RefreshAll );
 }
 
 void ResultWindow::newSelection( const QSqlRecord* fields )
