ab13e075c4cacc72bad9fca9796d9c18cd55607d
Volker Hilsheimer <vohi@trolltech.com>
2001-01-19 02:05:55 +0100

    Implement QLibraryInterface::cleanup() to delete style.
    [git-p4: depot-paths = "//depot/qt/main/": change = 33719]

--- src/styles/src/cde/main.cpp
+++ src/styles/src/cde/main.cpp
@@ -15,6 +15,7 @@ public:
     QStyle *create( const QString& );
 
     bool init();
+    void cleanup();
     bool canUnload() const;
 
 private:
@@ -75,6 +76,11 @@ QStyle* CDEStyle::create( const QString& s )
 bool CDEStyle::init()
 {
     return TRUE;
+}
+
+void CDEStyle::cleanup() 
+{
+    delete style;
 }
 
 bool CDEStyle::canUnload() const
