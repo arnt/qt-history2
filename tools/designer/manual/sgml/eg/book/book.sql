4a0cd102c1dd156f24909500cfdf757d264f7798
Mark Summerfield <mark@trolltech.com>
2001-01-23 12:27:52 +0100

    Updated database generation script for egs.
    [git-p4: depot-paths = "//depot/qt/main/": change = 34057]

--- tools/designer/manual/sgml/eg/book/book.sql
+++ tools/designer/manual/sgml/eg/book/book.sql
@@ -5,6 +5,7 @@ DROP TABLE author_seq;
 DROP TABLE book_seq;
 DROP TABLE format_seq;
 DROP VIEW book_view;
+DROP VIEW author_view;
 
 CREATE SEQUENCE author_seq;
 CREATE SEQUENCE book_seq;
@@ -21,9 +22,9 @@ CREATE VIEW author_view AS ( SELECT author.id AS id, author.forename || ' ' || a
 SELECT nextval( 'author_seq' );
 SELECT nextval( 'author_seq' );
 SELECT nextval( 'author_seq' );
-INSERT INTO author VALUES ( 1, 'Philip K', 'Dick' );
-INSERT INTO author VALUES ( 2, 'Robert', 'Heinlein' );
-INSERT INTO author VALUES ( 3, 'Sarah', 'Paretsky' );
+INSERT INTO author VALUES ( 0, 'Philip K', 'Dick' );
+INSERT INTO author VALUES ( 1, 'Robert', 'Heinlein' );
+INSERT INTO author VALUES ( 2, 'Sarah', 'Paretsky' );
 
 SELECT nextval( 'book_seq' );
 SELECT nextval( 'book_seq' );
@@ -31,16 +32,16 @@ SELECT nextval( 'book_seq' );
 SELECT nextval( 'book_seq' );
 SELECT nextval( 'book_seq' );
 SELECT nextval( 'book_seq' );
-INSERT INTO book VALUES ( 1, 'The Man Who Japed', 6.99, 1, 2 );
-INSERT INTO book VALUES ( 2, 'The Man in the High Castle', 9.99, 1, 1 );
-INSERT INTO book VALUES ( 3, 'The Number of the Beast', 8.99, 2, 3 );
-INSERT INTO book VALUES ( 4, 'Indemnity Only', 9.99, 3, 4 );
-INSERT INTO book VALUES ( 5, 'Burn Marks', 9.99, 3, 1 );
-INSERT INTO book VALUES ( 6, 'Deadlock', 9.99, 3, 1 );
+INSERT INTO book VALUES ( 0, 'The Man Who Japed', 6.99, 0, 1 );
+INSERT INTO book VALUES ( 1, 'The Man in the High Castle', 9.99, 0, 0 );
+INSERT INTO book VALUES ( 2, 'The Number of the Beast', 8.99, 1, 2 );
+INSERT INTO book VALUES ( 3, 'Indemnity Only', 9.99, 2, 3 );
+INSERT INTO book VALUES ( 4, 'Burn Marks', 9.99, 2, 0 );
+INSERT INTO book VALUES ( 5, 'Deadlock', 9.99, 2, 0 );
 
 SELECT nextval( 'format_seq' );
 SELECT nextval( 'format_seq' );
 SELECT nextval( 'format_seq' );
-INSERT INTO format VALUES ( 1, 'Paperback' );
-INSERT INTO format VALUES ( 2, 'Hardback' );
-INSERT INTO format VALUES ( 3, 'Softback' );
+INSERT INTO format VALUES ( 0, 'Paperback' );
+INSERT INTO format VALUES ( 1, 'Hardback' );
+INSERT INTO format VALUES ( 2, 'Softback' );
