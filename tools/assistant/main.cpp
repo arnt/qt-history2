823e3823f02226871345dd1ceee971f6fad24bc3
Reginald Stadlbauer <reggie@froglogic.com>
2001-01-25 21:10:12 +0100

    Made the assistant a unique app. This means, if you start it it tries to connect to a certain port at which it expects an already running assistant listening. If this succeeds, it writes the app-arguments (the file to open) to the socket, so that it gets shown in the running assistant, and this window also gets raised then. If connecting to that server fails, it starts the app window, shows the page and starts the server, so that the next assistant can use this one.
    [git-p4: depot-paths = "//depot/qt/main/": change = 34309]

--- tools/assistant/main.cpp
+++ tools/assistant/main.cpp
@@ -1,11 +1,129 @@
 #include <qapplication.h>
 #include "mainwindow.h"
+#include <qserversocket.h>
+#include <qsocket.h>
+#include <qlist.h>
+
+const int server_port = 9999;
+
+class Socket : public QSocket
+{
+    Q_OBJECT
+
+public:
+    Socket( QObject *parent );
+    ~Socket();
+
+    void start();
+
+private slots:
+    void startOwnWindow();
+    void sendFile();
+
+};
+
+
+class ServerSocket : public QServerSocket
+{
+    Q_OBJECT
+public:
+    ServerSocket( MainWindow *mw );
+    ~ServerSocket();
+    void newConnection( int );
+
+private slots:
+    void dataReceived();
+
+private:
+    QList<QSocket> connections;
+    MainWindow *mainWindow;
+
+};
+
+
+
+Socket::Socket( QObject *parent )
+    : QSocket( parent )
+{
+    connect( this, SIGNAL( error( int ) ), this, SLOT( startOwnWindow() ) );
+    connect( this, SIGNAL( connected() ), this, SLOT( sendFile() ) );
+    connect( this, SIGNAL( bytesWritten( int ) ), qApp, SLOT( quit() ) );
+}
+
+Socket::~Socket()
+{
+}
+
+void Socket::start()
+{
+    connectToHost( "127.0.0.1", server_port );
+}
+
+void Socket::startOwnWindow()
+{
+    MainWindow * mw = new MainWindow;
+    mw->show();
+    QString s = qApp->argv()[ 1 ];
+    mw->showLink( s, "" );
+    (void)new ServerSocket( mw );
+}
+
+void Socket::sendFile()
+{
+    QString s = qApp->argv()[ 1 ];
+    s += "\n";
+    writeBlock( s.latin1(), s.length() );
+}
+
+
+
+
+ServerSocket::ServerSocket( MainWindow *mw )
+    : QServerSocket( server_port, 0, mw ), mainWindow( mw )
+{
+    connections.setAutoDelete( TRUE );
+}
+
+ServerSocket::~ServerSocket()
+{
+    for ( QSocket *s = connections.first(); s; s = connections.next() )
+	s->close();
+}
+
+void ServerSocket::newConnection( int socket )
+{
+    QSocket *s = new QSocket( this );
+    s->setSocket( socket );
+    connect( s, SIGNAL( readyRead() ),
+	     this, SLOT( dataReceived() ) );
+    connections.append( s );
+}
+
+void ServerSocket::dataReceived()
+{
+    QSocket *s = (QSocket*)sender();
+    if ( !s || !s->inherits( "QSocket" ) )
+	return;
+    if ( !s->canReadLine() )
+	return;
+    QString line = s->readLine();
+    line = line.simplifyWhiteSpace();
+    mainWindow->showLink( line, "" );
+    mainWindow->show();
+    mainWindow->raise();
+}
+
+
+
 
 int main( int argc, char ** argv )
 {
     QApplication a( argc, argv );
-    MainWindow * mw = new MainWindow;
-    mw->show();
+
+    Socket *s = new Socket( 0 );
+    s->start();
     a.connect( &a, SIGNAL( lastWindowClosed() ), &a, SLOT( quit() ) );
     return a.exec();
 }
+
+#include "main.moc"
