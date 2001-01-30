3e6dfe89e07ae7d4814985e3c483b018a77c0074
Patricia Jung <trish@trish.de>
2001-01-30 14:00:34 +0100

    new walkthrough (small-table-demo)
    [git-p4: depot-paths = "//depot/qt/main/": change = 34578]

--- examples/table/small-table-demo/main.cpp
+++ examples/table/small-table-demo/main.cpp
@@ -8,98 +8,48 @@
 **
 *****************************************************************************/
 
-#include <qtable.h>
 #include <qapplication.h>
+#include <qtable.h>
+#include <qpixmap.h>
+#include <qstringlist.h>
 #include <qwmatrix.h>
 
-// Qt logo
+// Qt logo: static const char *qtlogo_xpm[]
+#include "qtlogo.xpm"
 
-static const char *qtlogo_xpm[] = {
-    "45 36 13 1",
-    "  c #000000",
-    ". c #999999",
-    "X c #333366",
-    "o c #6666CC",
-    "O c #333333",
-    "@ c #666699",
-    "# c #000066",
-    "$ c #666666",
-    "% c #3333CC",
-    "& c #000033",
-    "* c #9999CC",
-    "= c #333399",
-    "+ c None",
-    "+++++++++++++++++++++++++++++++++++++++++++++",
-    "+++++++++++++++.$OOO$.+++++++++++++++++++++++",
-    "+++++++++++++$         O.++++++++++++++++++++",
-    "+++++++++++.O            $+++++++++++++++++++",
-    "++++++++++.    $.++.$     O++++++++++++++++++",
-    "+++++++++.   O.+++++++$    O+++++++++++++++++",
-    "+++++++++O   ++++++++++$    $++++++++++++++++",
-    "++++++++$   .+++++++++++O    .+++++++++++++++",
-    "+++++++.   O+++++++++++++    O++++++.++++++++",
-    "+++++++$   .+++++++++++++$    .+++.O ++++++++",
-    "+++++++    +++++++++++++++    O+++.  ++++++++",
-    "++++++.  &Xoooo*++++++++++$    +++.  ++++++++",
-    "++++++@=%%%%%%%%%%*+++++++.    .++.  ++++++++",
-    "+++**oooooo**++*o%%%%o+++++    $++O  ++++++++",
-    "+*****$OOX@oooo*++*%%%%%*++O   $+.   OOO$++++",
-    "+.++....$O$+*ooooo*+*o%%%%%O   O+$   $$O.++++",
-    "*+++++$$....+++*oooo**+*o%%#   O++O  ++++++**",
-    "++++++O  $.....++**oooo**+*X   &o*O  ++++*ooo",
-    "++++++$   O++.....++**oooo*X   &%%&  ..*o%%*+",
-    "++++++$    ++++.....+++**ooO   $*o&  @oo*++++",
-    "++++++.    .++++++.....+++*O   Xo*O  .+++++++",
-    "+++++++    O+++++++++......    .++O  ++++++++",
-    "+++++++O    +++.$$$.++++++.   O+++O  ++++++++",
-    "+++++++.    $$OO    O.++++O   .+++O  ++++++++",
-    "++++++++O    .+++.O   $++.   O++++O  ++++++++",
-    "++++++++.    O+++++O   $+O   +++++O  ++++++++",
-    "+++++++++.    O+++++O   O   .+++++O  .+++++++",
-    "++++++++++$     .++++O     .++++.+$  O+.$.+++",
-    "+++++++++++.      O$$O    .+++++...      ++++",
-    "+++++++++++++$            O+++++$$+.O O$.++++",
-    "+++++++++++++++$OO  O$.O   O.++. .+++++++++++",
-    "+++++++++++++++++++++++.     OO  .+++++++++++",
-    "++++++++++++++++++++++++.       O++++++++++++",
-    "+++++++++++++++++++++++++.      .++++++++++++",
-    "++++++++++++++++++++++++++.O  O.+++++++++++++",
-    "+++++++++++++++++++++++++++++++++++++++++++++"
-};
+// Table size
 
-
-// Constants
-
-const int numRows = 100;				// Tablesize: number of rows
-const int numCols = 100;				// Tablesize: number of columns
+const int numRows = 100;
+const int numCols = 100;
 
 // The program starts here.
 
 int main( int argc, char **argv )
 {
-    QApplication a(argc,argv);			
+    QApplication app( argc, argv );			
 
-    QTable v( numRows, numCols );
+    QTable table( numRows, numCols );
     QPixmap pix( qtlogo_xpm );
 
-    float factor = (float) v.rowHeight( 3 ) / pix.height();
+    double factor = (double) table.rowHeight( 3 ) / pix.height();
     
     QWMatrix wm;
     wm.scale( factor, factor );
     pix = pix.xForm( wm );
 
-    v.setPixmap( 3, 3, pix );
-    v.setText( 3, 3, "A Pixmap" );
+    table.setPixmap( 3, 2, pix );
+    table.setText( 3, 2, "A Pixmap" );
 
-    QStringList l;
-    l << "one" << "two" << "three" << "four";
+    QStringList comboEntries;
+    comboEntries << "one" << "two" << "three" << "four";
 
     for ( int i = 0; i < numRows; ++i )
-	v.setItem( i, 5, new QComboTableItem( &v, l, TRUE ) );
+	table.setItem( i, 5, new QComboTableItem( &table, comboEntries, 
+	               TRUE ) );
     for ( int j = 0; j < numRows; ++j )
-	v.setItem( j, 1, new QCheckTableItem( &v, "Check me" ) );
+	table.setItem( j, 1, new QCheckTableItem( &table, "Check me" ) );
 
-    a.setMainWidget( &v );
-    v.show();
-    return a.exec();
+    app.setMainWidget( &table );
+    table.show();
+    return app.exec();
 }
