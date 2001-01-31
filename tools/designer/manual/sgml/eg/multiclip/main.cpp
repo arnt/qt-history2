f9a734d3dca3ca14cf4199c27dbc202f45e56597
Mark Summerfield <mark@trolltech.com>
2001-01-31 09:51:46 +0100

    If there's time to rewrite chap 1 of the designer manual this will be the new eg.
    [git-p4: depot-paths = "//depot/qt/main/": change = 34654]

---
+++ tools/designer/manual/sgml/eg/multiclip/main.cpp
@@ -0,0 +1,27 @@
+/****************************************************************************
+** $Id$
+**
+** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#include <qapplication.h>
+#include <qclipboard.h>
+#include "multiclip.h"
+
+
+int main( int argc, char *argv[] ) 
+{
+    QApplication app( argc, argv );
+
+    FormClipping clippingForm;
+    app.setMainWidget( &clippingForm );
+    clippingForm.show();
+
+    return app.exec();
+}
+
+
---
+++ tools/designer/manual/sgml/eg/multiclip/multiclip.pro
@@ -0,0 +1,7 @@
+TEMPLATE	= app
+CONFIG	= qt warn_on release
+TARGET	= multiclip 
+SOURCES = main.cpp
+INTERFACES	= multiclip.ui 
+PROJECTNAME	= multiclip
+LANGUAGE	= C++
