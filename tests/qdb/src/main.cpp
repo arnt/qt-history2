14c90e5e278b46d8da023dfaa7db23ceddb9bf27
Dave Berton <db@trolltech.com>
2001-02-15 11:30:34 +0100

    update test code
    [git-p4: depot-paths = "//depot/qt/main/": change = 35724]

--- tests/qdb/src/main.cpp
+++ tests/qdb/src/main.cpp
@@ -77,7 +77,6 @@ int main( int /*argc*/, char** /*argv*/ )
 
 #if 0
     /* update a record */
-    /* update a record */
     env.program()->append( new Open( 0, FILENAME ) );
     env.program()->append( new Next( 0, 6 ) );
     env.program()->append( new Push( QString("name") ) );
@@ -85,36 +84,6 @@ int main( int /*argc*/, char** /*argv*/ )
     env.program()->append( new PushList( 2 ) );
     env.program()->append( new Update( 0 ) );
     env.program()->append( new Close( 0 ) );
-#endif
-
-#if 0
-    /* select some records */
-    QValueList<QVariant> field;
-    QVariant name = "id";
-    QVariant value;
-    value.cast( QVariant::Int );
-    field.append( name );
-    field.append( value );
-    env.program().append( new Push( field ) );
-    field.clear();
-    name = "name";
-    value.cast( QVariant::String );
-    field.append( name );
-    field.append( value );
-    env.program().append( new Push( field ) );
-    env.program().append( new PushList( 2 ) );
-    env.program().append( new CreateResult( 0 ) );
-    env.program().append( new Open( 0, FILENAME ) );
-    env.program().append( new Next( 0, 14 ) );
-    env.program().append( new PushFieldValue( 0, 0 ) );
-    env.program().append( new Push( 2 ) );
-    env.program().append( new Ne( 5 ) );
-    env.program().append( new PushFieldValue( 0, 0 ) );
-    env.program().append( new PushFieldValue( 0, 1 ) );
-    env.program().append( new PushList( 2 ) );
-    env.program().append( new SaveResult( 0 ) );
-    env.program().append( new Goto( 5 ) );
-    env.program().append( new Close( 0 ) );
 #endif
 
 #if 0
@@ -127,27 +96,27 @@ int main( int /*argc*/, char** /*argv*/ )
 #endif
 
     /* select some records using a range scan */
-    env.program().append( new Open( 0, FILENAME ) );
-    env.program().append( new PushFieldDesc( 0, "id" ) );
-    env.program().append( new PushFieldDesc( 0, "name" ) );
-    env.program().append( new PushList( 2 ) );
-    env.program().append( new CreateResult( 0 ) );
-    env.program().append( new PushFieldDesc( 0, "name" ) );
-    env.program().append( new Push( "trolltech" ) );
-    env.program().append( new PushList( 2 ) );
-    env.program().append( new RangeScan( 0 ) );
-    env.program().append( new RewindMarked( 0 ) );
-    env.program().append( new NextMarked( 0 , 16 ) );
-    env.program().append( new PushFieldValue( 0, 0 ) );
-    env.program().append( new PushFieldValue( 0, 1 ) );
-    env.program().append( new PushList( 2 ) );
-    env.program().append( new SaveResult( 0 ) );
-    env.program().append( new Goto( 10 ) );
-    env.program().append( new PushFieldDesc( 0, "id" ) );
-    env.program().append( new Push( QVariant( TRUE, 0 )  ) );
-    env.program().append( new PushList( 2 ) );
-    env.program().append( new Sort( 0 ) );
-    env.program().append( new Close( 0 ) );
+    env.program()->append( new Open( 0, FILENAME ) );
+    env.program()->append( new PushFieldDesc( 0, "id" ) );
+    env.program()->append( new PushFieldDesc( 0, "name" ) );
+    env.program()->append( new PushList( 2 ) );
+    env.program()->append( new CreateResult( 0 ) );
+    env.program()->append( new PushFieldDesc( 0, "name" ) );
+    env.program()->append( new Push( "blarg" ) );
+    env.program()->append( new PushList( 2 ) );
+    env.program()->append( new RangeScan( 0 ) );
+    env.program()->append( new RewindMarked( 0 ) );
+    env.program()->append( new NextMarked( 0 , 16 ) );
+    env.program()->append( new PushFieldValue( 0, 0 ) );
+    env.program()->append( new PushFieldValue( 0, 1 ) );
+    env.program()->append( new PushList( 2 ) );
+    env.program()->append( new SaveResult( 0 ) );
+    env.program()->append( new Goto( 10 ) );
+    env.program()->append( new PushFieldDesc( 0, "id" ) );
+    env.program()->append( new Push( QVariant( TRUE, 0 )  ) );
+    env.program()->append( new PushList( 2 ) );
+    env.program()->append( new Noop( ) );
+    env.program()->append( new Close( 0 ) );
 
 #if 0
     /* select all records and sort */
