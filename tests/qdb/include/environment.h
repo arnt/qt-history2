d75af522d56e12d2e964718a10bbf2731994d721
Dave Berton <db@trolltech.com>
2001-02-16 10:22:55 +0100

    Docs
    [git-p4: depot-paths = "//depot/qt/main/": change = 35815]

--- tests/qdb/include/environment.h
+++ tests/qdb/include/environment.h
@@ -19,10 +19,15 @@ class QIODevice;
 
 namespace qdb {
 
-    typedef QValueList<QVariant> List; /*! list which may contain other lists */
-    typedef QValueList<QVariant> Record; /*! list which only contains non-list datatypes */
-    typedef QValueList<Record> Data; /*! list of records */
+    /*! List which may contain other lists */
+    typedef QValueList<QVariant> List;
+    /*! List which only contains non-list datatypes */
+    typedef QValueList<QVariant> Record;
+    /*! List of records */
+    typedef QValueList<Record> Data;
+    /*! For simulating multimap behavior with QMap */
     typedef QMap< QVariant, QValueList<int> > ColumnKey;
+    /*! The stack. */
     typedef QValueStack<QVariant> Stack;
 
     class Environment;
@@ -33,6 +38,7 @@ namespace qdb {
      */
     struct Parser
     {
+	/*! Parses \a commands inthe environment \a env, and creates a program. */
 	virtual bool parse( const QString& commands, Environment* env ) = 0;
     };
 
@@ -44,11 +50,17 @@ namespace qdb {
 
     struct DataSet
     {
+	/*! Moves the data set to the next record */
 	virtual bool next() = 0;
+	/*! Returns the number of fields in the data set */
 	virtual uint count() const = 0;
+	/*! Returns the number of records in the data set */
 	virtual uint size() const = 0;
+	/*! Returns the names of all columns (in order) in the data set */
 	virtual QStringList columnNames() const = 0;
+	/*! Returns the types of all columns (in order) in the data set */
 	virtual QValueList<QVariant::Type> columnTypes() const = 0;
+	/*! Returns the value of field \a i (zero-based) */
 	virtual bool field( uint i, QVariant& v ) = 0;
     };
 
@@ -59,15 +71,21 @@ namespace qdb {
 
     struct ResultSet : public DataSet
     {
-	/*! Clears all records in the result
-	 */
+	/*! Clears all records in the result */
 	virtual void clear() = 0;
+	/*! Sets the header (and thus, the entire structure) of the result */
 	virtual bool setHeader( const List& data ) = 0;
+	/*! Appends the record \a buf, which must match the structure set by setHeader() */
 	virtual bool append( const Record& buf ) = 0;
+	/*! Sorts the result */
 	virtual bool sort( const List& index ) = 0;
+	/*! Moves result to the first record */
 	virtual bool first() = 0;
+	/*! Moves result to the last record */
 	virtual bool last() = 0;
+	/*! Moves result to the previous record */
 	virtual bool prev() = 0;
+	/*! Returns the current record buffer, which matched the structer set by setHeader() */
 	virtual Record& currentRecord() = 0;
     };
 
@@ -107,8 +125,11 @@ namespace qdb {
 
     struct Op
     {
+	/*! Returns the operand \a i */
 	virtual QVariant& P( int i ) = 0;
+	/*! Executes the instruction in the environment \a env */
 	virtual int exec( Environment* env ) = 0;
+	/*! Returns the name of the instruction */
 	virtual QString name() const = 0;
     };
 
@@ -119,15 +140,25 @@ namespace qdb {
 
     struct Program
     {
+	/*! Appends the operand \a op to the program.  The program takes ownership of the pointer */
 	virtual void append( Op* op ) = 0;
+	/*! Removes the \a i-th operand from the program  */
 	virtual void remove( uint i ) = 0;
+	/*! Clears the program  */
 	virtual void clear() = 0;
+	/*! Sets the internal program counter so that the \a i-th instruction executes next  */
 	virtual void setCounter( int i ) = 0;
+	/*! Resets the internal program counter to the beginning */
 	virtual void resetCounter() = 0;
+	/*! Returns the value of the internal program counter */
 	virtual int counter() = 0;
+	/*! ### */
 	virtual int getLabel() = 0;
+	/*! ### */
 	virtual void setLabel( int lab, int counter ) = 0;
+	/*! Returns the next instruction,or 0 if there are no more */
 	virtual Op* next() = 0;
+	/*! Returns a human-readable program listing */
 	virtual QStringList listing() const = 0;
     };
 
