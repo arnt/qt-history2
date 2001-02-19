2eaad9e518ef4aea6327330f6565ad622ec924f8
ebakke <ebakke@trolltech.com>
2001-02-19 15:25:33 +0100

    We no longer use the confirmation dialog
    [git-p4: depot-paths = "//depot/qt/main/": change = 35978]

--- tests/install/uninstaller.cpp
+++ tests/install/uninstaller.cpp
@@ -3,7 +3,6 @@
 #include <qdir.h>
 #include <qmultilineedit.h>
 #include <qpushbutton.h>
-#include "confirmdlg.h"
 #include "uninstall.h"
 #include "environment.h"
 
@@ -37,17 +36,12 @@ int main( int argc, char** argv )
 {
     app = new QApplication( argc, argv );
     progress = new UninstallDlg;
-    ConfirmDlg dlg( progress );
     
     if( argc != 4 )
 	qFatal( "Incorrect parameters" );
 
     progress->setCaption( QString( "Uninstalling Qt " ) + argv[ 3 ] );
-    dlg.setCaption( QString( "Uninstalling Qt " ) + argv[ 3 ] );
     progress->show();
-    dlg.confirmText->setText( "Do you really want to remove this version of Qt from your computer?" );
-    if( !dlg.exec() )
-	return 0;
     
     app->setMainWidget( progress );
 
