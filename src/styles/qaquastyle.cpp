7f029b754c163aaeba7628c40910e089198250eb
Volker Hilsheimer <vohi@trolltech.com>
2001-02-23 03:37:46 +0100

    Now I know why I didn't use qt_winver!
    [git-p4: depot-paths = "//depot/qt/main/": change = 36351]

--- src/styles/qaquastyle.cpp
+++ src/styles/qaquastyle.cpp
@@ -8561,7 +8561,6 @@ static char * aqua_tab_usel_dis_right_xpm[] = {
 "========"};
 
 #include "qapplication.h"
-#include "../kernel/qapplication_p.h"
 #include "qpainter.h"
 #include "qdrawutil.h"
 #include "qpixmap.h"
@@ -10328,9 +10327,9 @@ void QAquaStyle::drawPopupMenuItem( QPainter* p, bool checkable,
 
     if ( checkable ) {
 #if defined(Q_WS_WIN)
-        if ( qt_winver == Qt::WV_2000 ||
-             qt_winver == Qt::WV_98 || 
-	     qt_winver == Qt::WV_XP )
+        if ( qWinVersion() == Qt::WV_2000 ||
+             qWinVersion() == Qt::WV_98 || 
+	     qWinVersion() == Qt::WV_XP )
             maxpmw = QMAX( maxpmw, 16 );
 #endif
         maxpmw = QMAX( maxpmw, 12 ); // space for the checkmarks
