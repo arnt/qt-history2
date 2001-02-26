d161abf74c517f0c9d90e55e4c2556d527ee5c34
Finn Arild Aasheim <finn@trolltech.com>
2001-02-26 14:08:20 +0100

    Fixed icons - some new ...
    [git-p4: depot-paths = "//depot/qt/main/": change = 36611]

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/examples/demo/main.cpp#19 $
+** $Id: //depot/qt/main/examples/demo/main.cpp#20 $
 **
 ** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
 **
@@ -38,37 +38,53 @@ int main( int argc, char **argv )
     QPixmap pix;
     pix.convertFromImage( img.smoothScale( 48, 48 ) );
 
-    QPixmap dbpix(dbicon);
-    QPixmap textpix(texticon);
-    QPixmap widgetpix(widgeticon);
-    QPixmap twodpix(twodicon);
-    QPixmap threedpix(threedicon);
+    QPixmap widgetpix( widgeticon );
+    QPixmap widgetpix_sel( widgeticon_sel );
+    QPixmap dbpix( dbicon );
+    QPixmap dbpix_sel( dbicon_sel );
+    QPixmap textpix( texticon );
+    QPixmap textpix_sel( texticon_sel );
+    QPixmap twodpix( twodicon );
+    QPixmap twodpix_sel( twodicon_sel );
+    QPixmap threedpix( threedicon );
+    QPixmap threedpix_sel( threedicon_sel );
 
     // example 1
     QTabWidget *tab = new QTabWidget();
     QWidget *w = new QWidget( tab );
     tab->addTab( w, "Widget" );
-    frame.addCategory( tab, widgetpix, "Widgets" );
+    frame.addCategory( tab, widgetpix, widgetpix_sel, "Widgets" );
 
     // example 2
     tab = new QTabWidget();
     w = new QWidget( tab );
     tab->addTab( w, "Database" );
-    frame.addCategory( tab, dbpix, "Database" );
+    frame.addCategory( tab, dbpix, dbpix_sel, "Database" );
 
     // 2D Graphics
     tab = new QTabWidget();
     w = new GraphWidget( tab );
     tab->addTab( w, "Graph Drawing" );
     tab->addTab( new DisplayWidget(), "Display" );
-    frame.addCategory( tab, twodpix, "2D Graphics" );
+    frame.addCategory( tab, twodpix, twodpix_sel, "2D Graphics" );
 
 #if defined(QT_MODULE_OPENGL)
     // 3D Graphics
     tab = new QTabWidget();
+>>>> ORIGINAL 
+    w = new GLObjectWindow( tab );
+    tab->addTab( w, "3d Demo" );
+    frame.addCategory( tab, threedpix, "3D Graphics" );
+==== THEIRS 
     GLWorkspace *ws = new GLWorkspace( tab );
     tab->addTab( ws, "3D Demo" );
     frame.addCategory( tab, threedpix, "3D Graphics" );
+==== YOURS 
+    w = new GLObjectWindow( tab );
+    tab->addTab( w, "3d Demo" );
+    frame.addCategory( tab, threedpix, threedpix_sel, "3D Graphics" );
+
+<<<<
 #endif
     // example 4
     tab = new QTabWidget();
@@ -77,7 +93,7 @@ int main( int argc, char **argv )
     QString home = QString(getenv("QTDIR")) + "/doc/html/index.html";
     w = new HelpWindow( home, ".", 0, "helpviewer" );
     tab->addTab( w, "Help Browser" );
-    frame.addCategory( tab, textpix, "Text Drawing/Editing" );
+    frame.addCategory( tab, textpix, textpix_sel, "Text Drawing/Editing" );
 
     a.setMainWidget( &frame );
     frame.show();
