77d6a24d006d850f7c2e77ded86476dc58de3fdb
Brad Hughes <bhughes@trolltech.com>
2001-02-27 13:51:50 +0100

    it should draw something on the button
    [git-p4: depot-paths = "//depot/qt/main/": change = 36703]

--- examples/fonts/simple-qfont-demo/simple-qfont-demo.cpp
+++ examples/fonts/simple-qfont-demo/simple-qfont-demo.cpp
@@ -3,20 +3,20 @@
 #include <qapplication.h>
 #include <qpushbutton.h>
 #include <qfont.h>
- 
+
 int main( int argc, char **argv )
 {
     QApplication app( argc, argv );
-    QPushButton  push( 0, "Push me" );
- 
-    QFont font( "Bavaria", 18 );        // preferred family is Bavaria
+    QPushButton  push( "Push me", 0, "Push me" );
+
+    QFont font( "Bavaria", 18 );          // preferred family is Bavaria
     font.setStyleHint( QFont::Serif );    // can also use any serif font
 
     font.setWeight( QFont::Bold );
- 
+
     push.setFont( font );
 
     app.setMainWidget( &push );
     push.show();
     return app.exec();
-}                  
+}
