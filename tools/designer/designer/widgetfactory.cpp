481ef5a7ec0ee83686d4640f25281092d7ede25b
Reginald Stadlbauer <reggie@froglogic.com>
2001-03-01 20:49:44 +0100

    Do not hold pointers to tempoaray data of objects which got deleted
    [git-p4: depot-paths = "//depot/qt/main/": change = 36955]

--- tools/designer/designer/widgetfactory.cpp
+++ tools/designer/designer/widgetfactory.cpp
@@ -282,11 +282,12 @@ QWidget *WidgetFactory::create( int id, QWidget *parent, const char *name, bool 
     }
 
     QWidget *w = 0;
+    QString str = WidgetDatabase::createWidgetName( id );
+    const char *s = str.latin1();
     if ( !WidgetDatabase::isCustomWidget( id ) )
-	w = createWidget( n, parent, name ? name : (const char*)WidgetDatabase::createWidgetName( id ).data(), init, r, orient );
+	w = createWidget( n, parent, name ? name : s, init, r, orient );
     else
-	w = createCustomWidget( parent, name ? name : (const char*)WidgetDatabase::createWidgetName( id ).data(),
-				MetaDataBase::customWidget( id ) );
+	w = createCustomWidget( parent, name ? name : s, MetaDataBase::customWidget( id ) );
     if ( !w )
 	return 0;
     MetaDataBase::addEntry( w );
