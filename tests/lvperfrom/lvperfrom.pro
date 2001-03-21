f49117ad1bb1e4c6410972c61a85f6467b437e45
Reginald Stadlbauer <reggie@froglogic.com>
2001-03-21 13:50:30 +0100

    Performance test for inserting items in listviews
    [git-p4: depot-paths = "//depot/qt/main/": change = 38039]

---
+++ tests/lvperfrom/lvperfrom.pro
@@ -0,0 +1,7 @@
+TEMPLATE	= app
+CONFIG		+= qt warn_on release
+HEADERS		= 
+SOURCES		= main.cpp
+TARGET		= lvperf 
+INTERFACES 	= form.ui
+TMAKE_UIC 	=  $(QTDIR)/bin/uic
---
+++ tests/lvperfrom/main.cpp
@@ -0,0 +1,30 @@
+/**********************************************************************
+** Copyright (C) 2000 Trolltech AS.  All rights reserved.
+**
+** This file is part of Qt GUI Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
+#include <qapplication.h>
+#include "form.h"
+
+int main( int argc, char ** argv ) {
+    QApplication a( argc, argv );
+    Form1 * mw = new Form1;
+    mw->show();
+    a.connect( &a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()) );
+    return a.exec();
+}
