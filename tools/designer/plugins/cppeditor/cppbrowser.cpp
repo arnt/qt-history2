589c7aa6e39d974945f5bc1386c61c29352139d8
Reginald Stadlbauer <reggie@froglogic.com>
2001-03-30 17:33:43 +0100

    More functionality for the browser stuff
    [git-p4: depot-paths = "//depot/qt/main/": change = 38434]

--- tools/designer/plugins/cppeditor/cppbrowser.cpp
+++ tools/designer/plugins/cppeditor/cppbrowser.cpp
@@ -22,9 +22,23 @@ void CppEditorBrowser::showHelp( const QString &w )
 	lst << "assistant" << word;
 	QProcess proc( lst );
 	proc.start();
-    } else {
-	QWidget *wid = curEditor->topLevelWidget();
-	if ( wid->inherits( "QMainWindow" ) )
-	    ( (QMainWindow*)wid )->statusBar()->message( tr( "Nothing available for '%1'" ).arg( w ), 1500 );
+	return;
     }
+
+    if ( word.find( '(' ) != -1 ) {
+	QString txt = "::" + word.left( word.find( '(' ) );
+	QTextDocument *doc = curEditor->document();
+	QTextParag *p = doc->firstParag();
+	while ( p ) {
+	    if ( p->string()->toString().find( txt ) != -1 ) {
+		curEditor->setCursorPosition( p->paragId(), 0 );
+		return;
+	    }
+	    p = p->next();
+	}
+    }
+
+    QWidget *wid = curEditor->topLevelWidget();
+    if ( wid->inherits( "QMainWindow" ) )
+	( (QMainWindow*)wid )->statusBar()->message( tr( "Nothing available for '%1'" ).arg( w ), 1500 );
 }
