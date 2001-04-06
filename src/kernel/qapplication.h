db05429eb0da177521717addc5d79c4606407903
Sam Magnuson <sam@trolltech.com>
2001-04-06 11:05:24 +0100

    compiles on mac again
    [git-p4: depot-paths = "//depot/qt/main/": change = 38826]

--- src/kernel/qapplication.h
+++ src/kernel/qapplication.h
@@ -285,6 +285,7 @@ private:
     static QMAC_PASCAL OSStatus globalEventProcessor(EventHandlerCallRef,  EventRef, void *);
     static QMAC_PASCAL void qt_trap_context_mouse(EventLoopTimerRef, void *);
     static QMAC_PASCAL void qt_select_timer_callbk(EventLoopTimerRef, void *);
+    friend class QMacInputMethod;
     friend void qt_mac_destroy_widget(QWidget *);
     friend void qt_init(int *, char **, QApplication::Type);
 #endif
