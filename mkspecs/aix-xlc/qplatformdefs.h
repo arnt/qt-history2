6a9e54797edf42b4cc3f25e8d31e49ccc6a28ff9
Dimitri Papadopoulos <dimitri@trolltech.com>
2001-04-08 10:37:27 +0100

    AIX mkspecs: a first try. I don't know if this works, I hope someone will test the beta on AIX.
    [git-p4: depot-paths = "//depot/qt/main/": change = 38910]

---
+++ mkspecs/aix-xlc/qplatformdefs.h
@@ -0,0 +1,100 @@
+#ifndef QPLATFORMDEFS_H
+#define QPLATFORMDEFS_H
+
+// Get Qt defines/settings
+
+#include "qglobal.h"
+
+
+// Set any POSIX/XOPEN defines at the top of this file to turn on specific APIs
+
+#include <unistd.h>
+#include <sys/types.h>
+
+
+// We are hot - unistd.h should have turned on the specific APIs we requested
+
+#ifdef QT_THREAD_SUPPORT
+#include <pthread.h>
+#endif // QT_THREAD_SUPPORT
+
+
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <limits.h>
+#include <locale.h>
+#include <pwd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <netinet/in.h>
+
+#include <sys/ioctl.h>
+#include <sys/ipc.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/shm.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+
+#define QT_STATBUF		struct stat
+#define QT_STATBUF4TSTAT	struct stat
+#define QT_STAT			::stat
+#define QT_FSTAT		::fstat
+#define QT_STAT_REG		S_IFREG
+#define QT_STAT_DIR		S_IFDIR
+#define QT_STAT_MASK		S_IFMT
+#define QT_STAT_LNK		S_IFLNK
+#define QT_FILENO		fileno
+#define QT_OPEN			::open
+#define QT_CLOSE		::close
+#define QT_LSEEK		::lseek
+#define QT_READ			::read
+#define QT_WRITE		::write
+#define QT_ACCESS		::access
+#define QT_GETCWD		::getcwd
+#define QT_CHDIR		::chdir
+#define QT_MKDIR		::mkdir
+#define QT_RMDIR		::rmdir
+#define QT_OPEN_RDONLY		O_RDONLY
+#define QT_OPEN_WRONLY		O_WRONLY
+#define QT_OPEN_RDWR		O_RDWR
+#define QT_OPEN_CREAT		O_CREAT
+#define QT_OPEN_TRUNC		O_TRUNC
+#define QT_OPEN_APPEND		O_APPEND
+
+#define QT_SIGNAL_RETTYPE	void
+#define QT_SIGNAL_ARGS		int
+#define QT_SIGNAL_IGNORE	SIG_IGN
+
+#ifdef _AIX43
+// AIX 4.3
+// The AIX 4.3 online documentation says 'size_t' but a user asked IBM
+// and they told him the documentation is wrong. And anyway 'socklen_t'
+// is needed by most AIX 4.3 users.
+#define QT_SOCKLEN_T socklen_t
+#elif _AIX42
+// AIX 4.2
+#define QT_SOCKLEN_T size_t
+#else
+// AIX 4.1
+#define QT_SOCKLEN_T int
+#endif
+
+#define QT_NREAD	FIONREAD
+
+inline int qt_socket_accept(int s, struct sockaddr *addr, QT_SOCKLEN_T *addrlen)
+{ return ::accept(s, addr, addrlen); }
+
+inline int qt_socket_listen(int s, int backlog)
+{ return ::listen(s, backlog); }
+
+
+#endif // QPLATFORMDEFS_H
