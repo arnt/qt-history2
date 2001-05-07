0efdeb04421282c02d52a34153cf224acc9d512e
Harri Porten <porten@froglogic.com>
2001-05-07 15:11:58 +0100

    include qplatformdefs.h *after* qsocketdevice.h as the included system headers may ruin the class definition. E.g. on UnixWare7        with #define listen _listen.
    [git-p4: depot-paths = "//depot/qt/main/": change = 40376]

--- src/network/qsocketdevice_unix.cpp
+++ src/network/qsocketdevice_unix.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/network/qsocketdevice_unix.cpp#53 $
+** $Id: //depot/qt/main/src/network/qsocketdevice_unix.cpp#54 $
 **
 ** Implementation of QSocketDevice class.
 **
@@ -41,6 +41,8 @@
 
 #ifndef QT_NO_NETWORK
 
+#include "qsocketdevice.h"
+#include "qwindowdefs.h"
 #include "qplatformdefs.h"
 
 // Tru64 redefines accept() to _accept() when _XOPEN_SOURCE_EXTENDED is
@@ -57,9 +59,6 @@ inline int qt_socket_listen(int s, int backlog)
 #if defined(listen)
 # undef listen
 #endif
-
-#include "qsocketdevice.h"
-#include "qwindowdefs.h"
 
 //#define QSOCKETDEVICE_DEBUG
 
