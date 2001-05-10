2c21314b3f307b73a5e6968133ffcd5ff2911a9c
Jasmin Blanchette <jasmin@trolltech.com>
2001-05-10 17:46:48 +0100

    OK, this change was good. The problems I had were not related to it. MOC's treatment of #ifdef's got me on the defensive.
    [git-p4: depot-paths = "//depot/qt/main/": change = 40661]

--- src/kernel/qobject.h
+++ src/kernel/qobject.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: //depot/qt/main/src/kernel/qobject.h#134 $
+** $Id: //depot/qt/main/src/kernel/qobject.h#135 $
 **
 ** Definition of QObject class
 **
@@ -69,12 +69,10 @@ public:
     virtual ~QObject();
 
 #ifdef Q_QDOC
-/*
     const char *className() const;
     QString tr( const char *, const char * ) const;
     QString trUtf8( const char *, const char * ) const;
     QMetaObject *metaObject() const;
-*/
 #endif
 
     virtual bool event( QEvent * );
