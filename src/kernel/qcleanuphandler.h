61a8167a3c571a71df6153abf701503d3383be59
Matthias Ettrich <ettrich@trolltech.com>
2001-05-11 19:00:16 +0100

    simplified QCleanupHandler template ( and removed the unused QGuardedCleanupHandler)
    [git-p4: depot-paths = "//depot/qt/main/": change = 40736]

--- src/kernel/qcleanuphandler.h
+++ src/kernel/qcleanuphandler.h
@@ -3,107 +3,36 @@
 
 #ifndef QT_H
 #include <qptrlist.h>
-#include <qguardedptr.h>
 #endif // QT_H
-
-template<class Type>
-class Q_EXPORT QGuardedCleanupHandler
-{
-public:
-    ~QGuardedCleanupHandler() { clear(); }
-
-    Type* add( Type* object )
-    {
-	cleanupObjects.insert( 0, new QGuardedPtr<Type>(object) );
-	return object;
-    }
-
-    void remove( Type *object )
-    {
-	QPtrListIterator<QGuardedPtr<Type> > it( cleanupObjects );
-	while ( it.current() ) {
-	    QGuardedPtr<Type>* guard = it.current();
-	    ++it;
-	    if ( (Type *)guard == object ) {
-		cleanupObjects.removeRef( guard );
-		delete guard;
-		break;
-	    }
-	}
-    }
-
-    bool isEmpty() const
-    {
-	QPtrListIterator<QGuardedPtr<Type> > it( cleanupObjects );
-	while ( it.current() ) {
-	    QGuardedPtr<Type>* guard = it.current();
-	    ++it;
-	    if ( (Type*)*guard )
-		return FALSE;
-	}
-	return TRUE;
-    }
-
-    void clear() {
-	QPtrListIterator<QGuardedPtr<Type> > it( cleanupObjects );
-	it.toLast();
-	while ( it.current() ) {
-	    QGuardedPtr<Type>* guard = it.current();
-	    --it;
-	    cleanupObjects.removeRef( guard );
-	    delete (Type*)*guard;
-	    delete guard;
-	}
-    }
-
-private:
-    QPtrList<QGuardedPtr<Type> > cleanupObjects;
-};
 
 template<class Type>
 class Q_EXPORT QCleanupHandler
 {
 public:
-    QCleanupHandler() : cleanupObjects( 0 )
-    {}
+    QCleanupHandler() : cleanupObjects( 0 ) {}
     ~QCleanupHandler() { clear(); }
 
-    Type* add( Type* object )
-    {
+    Type* add( Type* object ) {
 	if ( !cleanupObjects ) {
 	    cleanupObjects = new QPtrList<Type>;
+ 	    cleanupObjects->setAutoDelete( TRUE );
 	}
 	cleanupObjects->insert( 0, object );
 	return object;
     }
 
-    void remove( Type *object )
-    {
+    void remove( Type *object ) {
 	if ( !cleanupObjects )
 	    return;
 	if ( object )
 	    cleanupObjects->removeRef( object );
     }
 
-    bool isEmpty() const
-    {
+    bool isEmpty() const {
 	return cleanupObjects ? cleanupObjects->isEmpty() : TRUE;
     }
 
-    void clear()
-    {
-	if ( !cleanupObjects )
-	    return;
-
-	QPtrListIterator<Type> it( *cleanupObjects );
-	it.toLast();
-	while ( it.current() ) {
-	    Type* object = it.current();
-	    --it;
-	    cleanupObjects->removeRef( object );
-	    delete object;
-	}
-
+    void clear() {
 	delete cleanupObjects;
 	cleanupObjects = 0;
     }
