073ef99f45ee54bf73d3038574bfc94a6dc78a61
Reginald Stadlbauer <reggie@froglogic.com>
2001-06-20 19:49:33 +0100

    Arguments _and_ functionname have to match
    [git-p4: depot-paths = "//depot/qt/main/": change = 43046]

--- tools/designer/designer/metadatabase.cpp
+++ tools/designer/designer/metadatabase.cpp
@@ -1170,7 +1170,8 @@ bool MetaDataBase::setEventFunctions( QObject *o, QObject *form, const QString &
 	for ( QValueList<Slot>::Iterator it = r2->slotList.begin(); it != r2->slotList.end(); ++it ) {
 	    Slot s = *it;
 	    QString sName = normalizeSlot( s.slot );
-	    if ( liface->canConnect( sName, fName ) ) {
+	    if ( sName.left( sName.find( '(' ) ) == fName.left( fName.find( '(' ) ) &&
+		 liface->canConnect( sName, fName ) ) {
 		needAddSlot = FALSE;
 		break;
 	    }
