2e9f33b6423ed6e25ca39656602c4457f35b77d3
Dimitri Papadopoulos <dimitri@trolltech.com>
2001-07-04 22:23:04 +0100

    Ooops.
    [git-p4: depot-paths = "//depot/qt/main/": change = 44026]

---
+++ tools/linguist/linguist/splashloader.cpp
@@ -0,0 +1,23 @@
+/**********************************************************************
+**   Copyright (C) 2001 Trolltech AS.  All rights reserved.
+**
+**   splashloader.cpp
+**
+**   This file is part of Qt Linguist.
+**
+**   See the file LICENSE included in the distribution for the usage
+**   and distribution terms.
+**
+**   The file is provided AS IS with NO WARRANTY OF ANY KIND,
+**   INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR
+**   A PARTICULAR PURPOSE.
+**
+**********************************************************************/
+
+#include "splashloader.h"
+#include "trwindow.h"
+
+QPixmap splashScreen()
+{
+    return TrWindow::splash();
+}
---
+++ tools/linguist/linguist/splashloader.h
@@ -0,0 +1,24 @@
+/**********************************************************************
+**   Copyright (C) 2001 Trolltech AS.  All rights reserved.
+**
+**   splashloader.h
+**
+**   This file is part of Qt Linguist.
+**
+**   See the file LICENSE included in the distribution for the usage
+**   and distribution terms.
+**
+**   The file is provided AS IS with NO WARRANTY OF ANY KIND,
+**   INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR
+**   A PARTICULAR PURPOSE.
+**
+**********************************************************************/
+
+#ifndef SPLASHLOADER_H
+#define SPLASHLOADER_H
+
+#include <qpixmap.h>
+
+QPixmap splashScreen();
+
+#endif
