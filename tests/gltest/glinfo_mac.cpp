fc4077952e908a84c571888a3fa42abfbb4d3fa7
karl <karl@trolltech.com>
2001-07-04 10:53:20 +0100

    Initial mac glinfo test.
    [git-p4: depot-paths = "//depot/qt/main/": change = 43935]

--- tests/gltest/glinfo_mac.cpp
+++ tests/gltest/glinfo_mac.cpp
@@ -8,15 +8,11 @@
 **
 *****************************************************************************/
 
-#if defined(Q_CC_MSVC)
-#pragma warning(disable:4305) // init: truncation from const double to float
-#endif
-
 #include "glinfo.h"
 
 #include "qstring.h"
+#include <agl.h>
 #include <gl.h>
-#include <glu.h>
 
 
 GLInfo::GLInfo(QWidget* parent, const char* name)
@@ -29,7 +25,52 @@ GLInfo::GLInfo(QWidget* parent, const char* name)
 
 QString GLInfo::getText()
 {
-  return *infotext;
+    int   i;
+    char* s;
+    char  t[80];
+    char* p;
+    
+    makeCurrent();
+    return *infotext;
+
+    infotext->sprintf("%sdisplay: N/A\n"
+		      "server agl vendor string: N/A\n"
+		      "server agl version string: N/A\n"
+		      "server agl extensions (AGL_): N/A\n"
+		      "client agl version: N/A\n"
+		      "client agl extensions (AGL_): none\n"
+		      "OpenGL vendor string: %s\n", (const char*)*infotext, 
+		      ((const char*)glGetString(GL_VENDOR)));
+    infotext->sprintf("%sOpenGL renderer string: %s\n", (const char*)*infotext, 
+		      glGetString(GL_RENDERER));
+    infotext->sprintf("%sOpenGL version string: %s\n", (const char*)*infotext, 
+		      glGetString(GL_VERSION));
+    infotext->sprintf("%sOpenGL extensions (GL_): \n", (const char*)*infotext);
+    i = 0;
+    s = (char*)glGetString(GL_EXTENSIONS);
+	t[79] = '\0';
+    while(*s) {
+		t[i++] = *s;
+		if(*s == ' ') {
+			if (*(s+1) != '\0') {
+				t[i-1] = ',';
+				t[i] = ' ';
+				p = &t[i++];
+			} else {       // zoinks! last one terminated in a space! //
+				t[i-1] = '\0';
+			}
+		}
+		if(i > 80 - 5) {
+			*p = t[i] = '\0';
+			infotext->sprintf("%s    %s\n", (const char*)*infotext, t);
+			p++;
+			i = strlen(p);
+			strcpy(t, p);
+		}
+		s++;
+	}
+    t[i] = '\0';
+    infotext->sprintf("%s    %s.", (const char*)*infotext, t);
 }
 
 QStringList GLInfo::getViewList()
