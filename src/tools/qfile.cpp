da565f5c4664d33cc932260fef4b7db47bf2b1a4
Dimitri Papadopoulos <dimitri@trolltech.com>
2001-07-07 16:35:38 +0100

    This seems to be useless, it's already in win32-*/qplatformdefs.h?
    [git-p4: depot-paths = "//depot/qt/main/": change = 44146]

--- src/tools/qfile.cpp
+++ src/tools/qfile.cpp
@@ -36,13 +36,7 @@
 **********************************************************************/
 
 #include "qplatformdefs.h"
-#if defined(Q_OS_WIN32) // ### this seems useless, what is it for?
-#  ifdef UNICODE        // ### shouldn't this go to qplatformdefs.h anyway?
-#   ifndef _UNICODE
-#     define _UNICODE
-#   endif
-#  endif
-#endif
+
 #include "qfile.h"
 
 
