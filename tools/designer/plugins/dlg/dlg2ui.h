019dfcd797a3af4f394975e74e4242f7459a7108
Dimitri Papadopoulos <dimitri@trolltech.com>
2001-07-15 15:07:37 +0100

    - Copyright notice was missing from the dlg plugin too.   This was not on purpose, was it? I've added a GPL one. - Move a few #include from header to implementation file.
    [git-p4: depot-paths = "//depot/qt/main/": change = 44524]

--- tools/designer/plugins/dlg/dlg2ui.h
+++ tools/designer/plugins/dlg/dlg2ui.h
@@ -1,26 +1,30 @@
-/*
-  dlg2ui.h
-*/
+/**********************************************************************
+**
+** Copyright (C) 2000-2001 Trolltech AS.  All rights reserved.
+**
+** This file is part of Qt Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
 
 #ifndef DLG2UI_H
 #define DLG2UI_H
 
-#include <qcombobox.h>
 #include <qdom.h>
-#include <qfile.h>
-#include <qiconview.h>
-#include <qlayout.h>
-#include <qlcdnumber.h>
-#include <qlineedit.h>
-#include <qlistbox.h>
-#include <qmessagebox.h>
-#include <qregexp.h>
-#include <qscrollview.h>
-#include <qslider.h>
-#include <qspinbox.h>
+#include <qmap.h>
 #include <qstring.h>
-#include <qtabbar.h>
-#include <qtextstream.h>
 #include <qvariant.h>
 
 typedef QMap<QString, QString> AttributeMap;
