c1b085762bb5f37c216d6c1e5dc9f66eb609342d
Reginald Stadlbauer <reggie@froglogic.com>
2001-08-14 17:33:04 +0100

    Window Icon
    [git-p4: depot-paths = "//depot/qt/main/": change = 46426]

--- tools/designer/designer/sourceeditor.cpp
+++ tools/designer/designer/sourceeditor.cpp
@@ -40,6 +40,7 @@ SourceEditor::SourceEditor( QWidget *parent, EditorInterface *iface, LanguageInt
     editor = iFace->editor( this, MainWindow::self->designerInterface() );
     iFace->onBreakPointChange( MainWindow::self, SLOT( breakPointsChanged() ) );
     resize( 600, 400 );
+    setIcon( PixmapChooser::loadPixmap( "filenew.xpm", PixmapChooser::Small ) );
 }
 
 SourceEditor::~SourceEditor()
