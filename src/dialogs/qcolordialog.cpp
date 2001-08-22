f4c88abc26a07a1dc8051143703a692cd0e38b43
Jasmin Blanchette <jasmin@trolltech.com>
2001-08-22 09:46:09 +0100

    Remove sad, inaccurate warning.
    [git-p4: depot-paths = "//depot/qt/main/": change = 46867]

--- src/dialogs/qcolordialog.cpp
+++ src/dialogs/qcolordialog.cpp
@@ -55,18 +55,6 @@
 
 //////////// QWellArray BEGIN
 
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists for the convenience
-// of qwellarray.cpp and qcolordialog.cpp.
-// This header file may change from version to version without notice,
-// or even be removed.
-//
-//
-
-
 struct QWellArrayData;
 
 class QWellArray : public QGridView
