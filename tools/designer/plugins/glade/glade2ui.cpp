5269a1cb176d667613590177ce15231bf4e3d1bb
Jasmin Blanchette <jasmin@trolltech.com>
2001-09-05 00:01:07 +0100

    Fixed Key_F3...F12, that were wrong thanks to copy-paste. I like to look at good old code I wrote, on lonely nights like this one.
    [git-p4: depot-paths = "//depot/qt/main/": change = 47578]

--- tools/designer/plugins/glade/glade2ui.cpp
+++ tools/designer/plugins/glade/glade2ui.cpp
@@ -34,10 +34,6 @@
 
 #include <ctype.h>
 
-/*
-  TODO: Provide stock icons.
-*/
-
 static const struct {
     const char *gtkName;
     const char *qtName;
@@ -141,17 +137,17 @@ static const struct {
     { "End", Qt::Key_End },
     { "Escape", Qt::Key_Escape },
     { "F1", Qt::Key_F1 },
-    { "F10", Qt::Key_F2 },
-    { "F11", Qt::Key_F1 },
-    { "F12", Qt::Key_F2 },
+    { "F10", Qt::Key_F10 },
+    { "F11", Qt::Key_F11 },
+    { "F12", Qt::Key_F12 },
     { "F2", Qt::Key_F2 },
-    { "F3", Qt::Key_F1 },
-    { "F4", Qt::Key_F2 },
-    { "F5", Qt::Key_F1 },
-    { "F6", Qt::Key_F2 },
-    { "F7", Qt::Key_F1 },
-    { "F8", Qt::Key_F2 },
-    { "F9", Qt::Key_F1 },
+    { "F3", Qt::Key_F3 },
+    { "F4", Qt::Key_F4 },
+    { "F5", Qt::Key_F5 },
+    { "F6", Qt::Key_F6 },
+    { "F7", Qt::Key_F7 },
+    { "F8", Qt::Key_F8 },
+    { "F9", Qt::Key_F9 },
     { "Home", Qt::Key_Home },
     { "Insert", Qt::Key_Insert },
     { "KP_Enter", Qt::Key_Enter },
@@ -170,7 +166,7 @@ static QString nonMenuText( const QString& menuText )
 {
     QString t;
     int len = (int) menuText.length();
-    if ( menuText.right(3) == QString("...") )
+    if ( menuText.endsWith(QString("...")) )
 	len -= 3;
     for ( int i = 0; i < len; i++ ) {
 	if ( menuText[i] != QChar('&') )
@@ -533,7 +529,6 @@ QString Glade2Ui::gtk2qtClass( const QString& gtkClass,
 	if ( qtClass.isEmpty() )
 	    qtClass = QString( "Unknown" );
     }
-
     return qtClass;
 }
 
@@ -2185,8 +2180,8 @@ QStringList Glade2Ui::convertGladeFile( const QString& fileName )
 	    widgetNo++;
 
 	    qApp->processEvents();
-            if ( fremskritt.wasCancelled() )
-        	break;
+	    if ( fremskritt.wasCancelled() )
+		break;
 	    fremskritt.setProgress( widgetNo );
 	}
 	n = n.nextSibling();
