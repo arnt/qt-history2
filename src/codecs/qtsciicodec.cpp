462b917df89277f1cde09c6de561cc46500075d7
Dimitri Papadopoulos <dimitri@trolltech.com>
2001-09-06 20:57:56 +0100

    Ooops.
    [git-p4: depot-paths = "//depot/qt/main/": change = 47718]

--- src/codecs/qtsciicodec.cpp
+++ src/codecs/qtsciicodec.cpp
@@ -1,11 +1,11 @@
 /****************************************************************************
-** $Id: $
+** $Id$
 **
 ** Implementation of QTsciiCodec class
 **
 ** Copyright (C) 2000-2001 Trolltech AS.  All rights reserved.
 **
-** This file is part of the kernel module of the Qt GUI Toolkit.
+** This file is part of the codecs module of the Qt GUI Toolkit.
 **
 ** This file may be distributed under the terms of the Q Public License
 ** as defined by Trolltech AS of Norway and appearing in the file
