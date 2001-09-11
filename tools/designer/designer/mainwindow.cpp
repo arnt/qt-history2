c166daf0b6236ad3024a594e8b0854a675534955
Reginald Stadlbauer <reggie@froglogic.com>
2001-09-11 08:31:28 +0100

    WDestructiveClose sucks, so I have to remember the previewed form and do a deferredDelete() on it in the close event to make previewing crash-free.
    Maybe WDescructiveClose should do a deferred delete?
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 47886]

--- tools/designer/designer/mainwindow.cpp
+++ tools/designer/designer/mainwindow.cpp
@@ -689,7 +689,6 @@ QWidget* MainWindow::previewFormInternal( QStyle* style, QPalette* palet )
 	delete l;
 
 	w->move( fw->mapToGlobal( QPoint(0,0) ) );
-	((MainWindow*)w )->setWFlags( WDestructiveClose );
 	previewing = TRUE;
 	w->show();
 	previewing = FALSE;
@@ -1165,6 +1164,7 @@ bool MainWindow::eventFilter( QObject *o, QEvent *e )
 		    MetaDataBase::fakeProperty( lastActiveFormWindow, "database" ).toStringList();
 		lastActiveFormWindow->project()->closeDatabase( lst[ 0 ] );
 	    }
+	    previewedForm->deferredDelete();
 	}
 	break;
     case QEvent::DragEnter:
