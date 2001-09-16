efc9d59a6d1e91781971111b2e8ebf4b56c476ed
Volker Hilsheimer <vohi@trolltech.com>
2001-09-16 18:44:45 +0100

    Show how to use versioning.
    [git-p4: depot-paths = "//depot/qt/main/": change = 48211]

--- examples/component/client/main.cpp
+++ examples/component/client/main.cpp
@@ -6,12 +6,12 @@ int main()
 {
     QInterfacePtr<PrintInterface> iface;
 
-    if ( QComponentFactory::createInstance( "Qt.Example1", IID_Print, (QUnknownInterface**)&iface ) == QS_OK )
+    if ( QComponentFactory::createInstance( "Qt.Example", IID_Print, (QUnknownInterface**)&iface ) == QS_OK )
 	iface->sayHello();
 
     iface = 0;
 
-    if ( QComponentFactory::createInstance( "Qt.Example2", IID_Print, (QUnknownInterface**)&iface ) == QS_OK )
+    if ( QComponentFactory::createInstance( "Qt.AnotherExample.1", IID_Print, (QUnknownInterface**)&iface ) == QS_OK )
 	iface->sayHello();
 
     return 0;
