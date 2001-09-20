2f655849aa57b383367ef1bb51065f9edad17492
Volker Hilsheimer <vohi@trolltech.com>
2001-09-20 18:31:28 +0100

    Reimplement new version of codec plugin.
    [git-p4: depot-paths = "//depot/qt/main/": change = 48621]

--- plugins/src/codecs/kr/main.cpp
+++ plugins/src/codecs/kr/main.cpp
@@ -9,87 +9,38 @@
 class KRTextCodecs : public QTextCodecPlugin
 {
 public:
-    KRTextCodecs();
+    KRTextCodecs() {}
 
-    QStringList keys() const;
+    QStringList names() const { return QStringList() << "eucKR" << "ksc5601.1987-0"; }
+    QValueList<int> mibEnums() const { return QValueList<int>() << 38 << 36; }
     QTextCodec *createForMib( int );
     QTextCodec *createForName( const QString & );
-
-private:
-    QPtrList<QTextCodec> codecs;
 };
 
 
-KRTextCodecs::KRTextCodecs()
-{
-}
-
-
-QStringList KRTextCodecs::keys() const
-{
-    QStringList list;
-    list << "eucKR" << "ksc5601.1987-0";
-    list << "MIB-38" << "MIB-36";
-    return list;
-}
-
 QTextCodec *KRTextCodecs::createForMib( int mib )
 {
-    QTextCodec *codec = 0;
-
-    QPtrListIterator<QTextCodec> it(codecs);
-    while ((codec = it.current())) {
-	++it;
-
-	if (codec->mibEnum() == mib)
-	    break;
+    switch (mib) {
+    case 36:
+	return new QFontKsc5601Codec;
+    case 38:
+	return new QEucKrCodec;
+    default:
+	;
     }
 
-    if (! codec ) {
-	switch (mib) {
-	case 36:
-	    codec = new QFontKsc5601Codec;
-	    break;
-
-	case 38:
-	    codec = new QEucKrCodec;
-	    break;
-
-	default:
-	    ;
-	}
-
-	if (codec)
-	    codecs.append(codec);
-    }
-
-    return codec;
+    return 0;
 }
 
 
 QTextCodec *KRTextCodecs::createForName( const QString &name )
 {
-    QTextCodec *codec = 0;
+    if (name == "eucKR")
+	return new QEucKrCodec;
+    if (name == "ksc5601.1987-0")
+	return new QFontKsc5601Codec;
 
-    QPtrListIterator<QTextCodec> it(codecs);
-    while ((codec = it.current())) {
-	++it;
-
-	if (codec->name() == name)
-	    break;
-    }
-
-    if (! codec) {
-	if (name == "eucKR")
-	    codec = new QEucKrCodec;
-	else if (name == "ksc5601.1987-0")
-	    codec = new QFontKsc5601Codec;
-
-	if (codec)
-	    codecs.append(codec);
-    }
-
-    return codec;
+    return 0;
 }
 
 
