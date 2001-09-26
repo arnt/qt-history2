3a75eac1ea276bcba74c26304113758e2ed66590
Jasmin Blanchette <jasmin@trolltech.com>
2001-09-26 19:42:17 +0100

    Removed resize( 140, 60 );
    [git-p4: depot-paths = "//depot/qt/main/": change = 49116]

--- tests/whitepaper/examples/clock/main.cpp
+++ tests/whitepaper/examples/clock/main.cpp
@@ -11,7 +11,6 @@ int main( int argc, char **argv )
     QApplication app( argc, argv );
     Clock *clock = new Clock;
     app.setMainWidget( clock );
-    clock->resize( 140, 60 );
     clock->show();
     return app.exec();
 }
