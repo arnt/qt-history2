3720257298ab7b26b3afdfbc783b8375b7422f5c
Trond Kjernaasen <trond@trolltech.com>
2001-10-08 13:02:43 +0100

    Make these examples compile and work again.
    [git-p4: depot-paths = "//depot/qt/main/": change = 49890]

---
+++ tools/designer/examples/book/login.h
@@ -0,0 +1,16 @@
+/****************************************************************************
+** $Id$
+**
+** Copyright (C) 1992-2001 Trolltech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#define DB_BOOKS_DRIVER "QMYSQL3"
+#define DB_BOOKS 	"testdb"
+#define DB_BOOKS_USER 	"troll"
+#define DB_BOOKS_PASSWD "trond"
+#define DB_BOOKS_HOST 	"iceblink.troll.no"
+
