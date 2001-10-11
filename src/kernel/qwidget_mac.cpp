fad46d0369c9bd675bc86d2a348c91dd575a56f5
Sam Magnuson <sam@trolltech.com>
2001-10-11 03:36:10 +0100

    window activation - hide docks when their owner are no longer active.
    [git-p4: depot-paths = "//depot/qt/main/": change = 50152]

--- src/kernel/qwidget_mac.cpp
+++ src/kernel/qwidget_mac.cpp
@@ -585,9 +585,7 @@ void QWidget::create( WId window, bool initializeWindow, bool destroyOldWindow  
 	    }
 	}
 
-	if(wclass == kFloatingWindowClass || 
-	   (!testWFlags(WShowModal) && dialog && parentWidget() && 
-	    !parentWidget()->topLevelWidget()->isDesktop())) //these dialogs don't hide
+	if(wclass == kFloatingWindowClass) //these dialogs don't hide
 	    ChangeWindowAttributes((WindowRef)id, 0, kWindowHideOnSuspendAttribute);
 #ifdef Q_WS_MACX
 	if(testWFlags(WStyle_StaysOnTop)) {
