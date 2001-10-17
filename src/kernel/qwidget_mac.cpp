15edb880e3e7d361e4c4218d4116d3748b5f3f79
Sam Magnuson <sam@trolltech.com>
2001-10-17 08:26:56 +0100

    oops again
    [git-p4: depot-paths = "//depot/qt/main/": change = 50580]

--- src/kernel/qwidget_mac.cpp
+++ src/kernel/qwidget_mac.cpp
@@ -518,7 +518,7 @@ void QWidget::create( WId window, bool initializeWindow, bool destroyOldWindow  
 	id = (WId)hd;
 	own_id = 0;
 	setWinId( id );
-    } else if( !parentWidget() || (popup || dialog) ) {
+    } else if( !parentWidget() || (popup || dialog || topLevel) ) {
 	own_id = 1; //I created it, I own it
 
 	Rect r;
