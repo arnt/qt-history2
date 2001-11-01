e362ac1de42cf5dd11ad875b2c4e3cea0e3216c7
Andy Shaw <andy@trolltech.com>
2001-11-01 12:11:51 +0100

    Integrate from 3.0
    [git-p4: depot-paths = "//depot/qt/main/": change = 51577]

--- qmake/generators/win32/msvc_nmake.cpp
+++ qmake/generators/win32/msvc_nmake.cpp
@@ -181,11 +181,8 @@ NmakeMakefileGenerator::writeNmakeParts(QTextStream &t)
       << varGlue("OBJMOC" ,"\n\t-del ","\n\t-del ","")
       << varGlue("UICDECLS" ,"\n\t-del ","\n\t-del ","")
       << varGlue("UICIMPLS" ,"\n\t-del ","\n\t-del ","")
-      << "\n\t-del $(TARGET)"
       << varGlue("QMAKE_CLEAN","\n\t-del ","\n\t-del ","")
       << varGlue("CLEAN_FILES","\n\t-del ","\n\t-del ","");
-    if(project->isActiveConfig("dll") && !project->variables()["DLLDESTDIR"].isEmpty()) 
-	t << "\n\t-del " << var("DLLDESTDIR") << "\\" << project->variables()[ "TARGET" ].first() << project->variables()[ "TARGET_EXT" ].first();
     if(!project->isEmpty("IMAGES"))
 	t << varGlue("QMAKE_IMAGE_COLLECTION", "\n\t-del ", "\n\t-del ", "");
     t << endl << endl;
