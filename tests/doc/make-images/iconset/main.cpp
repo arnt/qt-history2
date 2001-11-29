27f0b01e90f6a4540703d9dc1ee32d9bad19dc07
Mark Summerfield <mark@trolltech.com>
2001-11-29 23:57:56 +0100

    doc integrate
    [git-p4: depot-paths = "//depot/qt/main/": change = 53438]

--- tests/doc/make-images/iconset/main.cpp
+++ tests/doc/make-images/iconset/main.cpp
@@ -20,41 +20,25 @@ public:
 	painter.setPen( Qt::black );
 	painter.setFont( QFont( "Helvetica", 10, QFont::Bold ) );
 
-	painter.drawPixmap( 0, 0, ico.pixmap( QIconSet::Small, QIconSet::Normal, QIconSet::On ) );
-	painter.drawText( 25, 0, 150, 30, Qt::AlignCenter, "Small, Normal, On" );
+	painter.drawPixmap( 0, 0, ico.pixmap( QIconSet::Small, QIconSet::Normal ) );
+	painter.drawText( 30, 0, 150, 30, Qt::AlignVCenter, "Small, Normal" );
 
-	painter.drawPixmap( 0, 30, ico.pixmap( QIconSet::Small, QIconSet::Normal, QIconSet::Off ) );
-	painter.drawText( 25, 30, 150, 30, Qt::AlignCenter, "Small, Normal, Off" );
+	painter.drawPixmap( 0, 30, ico.pixmap( QIconSet::Small, QIconSet::Active ) );
+	painter.drawText( 30, 30, 150, 30, Qt::AlignVCenter, "Small, Active" );
 
-	painter.drawPixmap( 0, 60, ico.pixmap( QIconSet::Small, QIconSet::Active, QIconSet::On ) );
-	painter.drawText( 25, 60, 150, 30, Qt::AlignCenter, "Small, Active, On" );
+	painter.drawPixmap( 0, 60, ico.pixmap( QIconSet::Small, QIconSet::Disabled ) );
+	painter.drawText( 30, 60, 150, 30, Qt::AlignVCenter, "Small, Disabled" );
 
-	painter.drawPixmap( 0, 90, ico.pixmap( QIconSet::Small, QIconSet::Active, QIconSet::Off ) );
-	painter.drawText( 25, 90, 150, 30, Qt::AlignCenter, "Small, Active, Off" );
+	painter.drawPixmap( 200, 0, ico.pixmap( QIconSet::Large, QIconSet::Normal ) );
+	painter.drawText( 240, 0, 150, 30, Qt::AlignVCenter, "Large, Normal" );
 
-	painter.drawPixmap( 0, 120, ico.pixmap( QIconSet::Small, QIconSet::Disabled, QIconSet::On ) );
-	painter.drawText( 25, 120, 150, 30, Qt::AlignCenter, "Small, Disabled, On" );
+	painter.drawPixmap( 200, 30, ico.pixmap( QIconSet::Large, QIconSet::Active ) );
+	painter.drawText( 240, 30, 150, 30, Qt::AlignVCenter, "Large, Active" );
 
-	painter.drawPixmap( 0, 150, ico.pixmap( QIconSet::Small, QIconSet::Disabled, QIconSet::Off ) );
-	painter.drawText( 25, 150, 150, 30, Qt::AlignCenter, "Small, Disabled, Off" );
+	painter.drawPixmap( 200, 60, ico.pixmap( QIconSet::Large, QIconSet::Disabled ) );
+	painter.drawText( 240, 60, 150, 30, Qt::AlignVCenter, "Large, Disabled" );
 
-	painter.drawPixmap( 200, 0, ico.pixmap( QIconSet::Large, QIconSet::Normal, QIconSet::On ) );
-	painter.drawText( 225, 0, 150, 30, Qt::AlignCenter, "Large, Normal, On" );
-
-	painter.drawPixmap( 200, 30, ico.pixmap( QIconSet::Large, QIconSet::Normal, QIconSet::Off ) );
-	painter.drawText( 225, 30, 150, 30, Qt::AlignCenter, "Large, Normal, Off" );
-
-	painter.drawPixmap( 200, 60, ico.pixmap( QIconSet::Large, QIconSet::Active, QIconSet::On ) );
-	painter.drawText( 225, 60, 150, 30, Qt::AlignCenter, "Large, Active, On" );
-
-	painter.drawPixmap( 200, 90, ico.pixmap( QIconSet::Large, QIconSet::Active, QIconSet::Off ) );
-	painter.drawText( 225, 90, 150, 30, Qt::AlignCenter, "Large, Active, Off" );
-
-	painter.drawPixmap( 200, 120, ico.pixmap( QIconSet::Large, QIconSet::Disabled, QIconSet::On ) );
-	painter.drawText( 225, 120, 150, 30, Qt::AlignCenter, "Large, Disabled, On" );
-
-	painter.drawPixmap( 200, 150, ico.pixmap( QIconSet::Large, QIconSet::Disabled, QIconSet::Off ) );
-	painter.drawText( 225, 150, 150, 30, Qt::AlignCenter, "Large, Disabled, Off" );
+	painter.drawText( 0, 100, 330, 30, Qt::AlignCenter, "Normal == Active unless explicitly set" );
     }
 };
 
@@ -63,9 +47,9 @@ int main(int argc, char **argv)
     QApplication app( argc, argv );
     Frame *mw = new Frame( 0 );
     app.setMainWidget( mw );
-    mw->setCaption( "IconSet" );
+    mw->setCaption( "QIconSet" );
     mw->setBackgroundColor( Qt::white );
-//    mw->resize( 300, 320 );
+    mw->resize( 360, 130 );
     mw->show();
     return app.exec();
 }
