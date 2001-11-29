94a2a06ee998be6a2884e007d8ccf02fe016f70c
Andy Shaw <andy@trolltech.com>
2001-11-29 12:05:48 +0100

    Integrate from 3.0
    [git-p4: depot-paths = "//depot/qt/main/": change = 53375]

--- src/dialogs/qcolordialog.cpp
+++ src/dialogs/qcolordialog.cpp
@@ -1411,9 +1411,9 @@ void QColorDialogPrivate::addCustom()
 */
 
 QColorDialog::QColorDialog(QWidget* parent, const char* name, bool modal) :
-    QDialog(parent, name, modal )
+    QDialog(parent, name, modal, WStyle_Customize | WStyle_Title | WStyle_DialogBorder | WStyle_SysMenu )
 {
-    setSizeGripEnabled( TRUE );
+    setSizeGripEnabled( FALSE );
     d = new QColorDialogPrivate( this );
 }
 
