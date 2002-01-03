bef9c6daec8363c8ec605fe5707128ee6e6a5644
Ian Walters <ianw@trolltech.com>
2002-01-03 16:08:39 +0100

    removing inappropriate comment.
    [git-p4: depot-paths = "//depot/qt/main/": change = 54673]

--- src/iconview/qiconview.cpp
+++ src/iconview/qiconview.cpp
@@ -36,8 +36,6 @@
 **********************************************************************/
 
 // needed for qsort (Borland)
-// ### PLEASE EXPLAIN #@зг~!!! what are we supposed to understand from this???
-// ### qsort() should be in <stdlib.h>. Is it not in there? Where is it then?
 #include "qplatformdefs.h"
 
 // Solaris redefines connect -> __xnet_connect with _XOPEN_SOURCE_EXTENDED.
