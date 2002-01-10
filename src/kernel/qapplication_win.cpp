fb9f50c360007399a58f48a34f6786b794630c80
Volker Hilsheimer <vohi@trolltech.com>
2002-01-10 15:25:09 +0100

    Start some preliminary support for multi-media-keys.
    [git-p4: depot-paths = "//depot/qt/main/": change = 54971]

--- src/kernel/qapplication_win.cpp
+++ src/kernel/qapplication_win.cpp
@@ -118,8 +118,47 @@ static void initWinTabFunctions();	// resolve the WINTAB api functions
 extern IAccessible *qt_createWindowsAccessible( QAccessibleInterface *object );
 #endif // QT_ACCESSIBILITY_SUPPORT
 
+// support for on-the-fly changes of the XP theme engine
 #ifndef WM_THEMECHANGED
 #define WM_THEMECHANGED                 0x031A
+#endif
+// support for multi-media-keys on ME/2000/XP
+#ifndef WM_APPCOMMAND
+#define WM_APPCOMMAND                   0x0319
+
+#define FAPPCOMMAND_MOUSE 0x8000
+#define FAPPCOMMAND_KEY   0
+#define FAPPCOMMAND_OEM   0x1000
+#define FAPPCOMMAND_MASK  0xF000
+#define GET_APPCOMMAND_LPARAM(lParam) ((short)(HIWORD(lParam) & ~FAPPCOMMAND_MASK))
+#define GET_DEVICE_LPARAM(lParam)     ((WORD)(HIWORD(lParam) & FAPPCOMMAND_MASK))
+#define GET_MOUSEORKEY_LPARAM         GET_DEVICE_LPARAM
+#define GET_FLAGS_LPARAM(lParam)      (LOWORD(lParam))
+#define GET_KEYSTATE_LPARAM(lParam)   GET_FLAGS_LPARAM(lParam)
+
+#define APPCOMMAND_BROWSER_BACKWARD       1
+#define APPCOMMAND_BROWSER_FORWARD        2
+#define APPCOMMAND_BROWSER_REFRESH        3
+#define APPCOMMAND_BROWSER_STOP           4
+#define APPCOMMAND_BROWSER_SEARCH         5
+#define APPCOMMAND_BROWSER_FAVORITES      6
+#define APPCOMMAND_BROWSER_HOME           7
+#define APPCOMMAND_VOLUME_MUTE            8
+#define APPCOMMAND_VOLUME_DOWN            9
+#define APPCOMMAND_VOLUME_UP              10
+#define APPCOMMAND_MEDIA_NEXTTRACK        11
+#define APPCOMMAND_MEDIA_PREVIOUSTRACK    12
+#define APPCOMMAND_MEDIA_STOP             13
+#define APPCOMMAND_MEDIA_PLAY_PAUSE       14
+#define APPCOMMAND_LAUNCH_MAIL            15
+#define APPCOMMAND_LAUNCH_MEDIA_SELECT    16
+#define APPCOMMAND_LAUNCH_APP1            17
+#define APPCOMMAND_LAUNCH_APP2            18
+#define APPCOMMAND_BASS_DOWN              19
+#define APPCOMMAND_BASS_BOOST             20
+#define APPCOMMAND_BASS_UP                21
+#define APPCOMMAND_TREBLE_DOWN            22
+#define APPCOMMAND_TREBLE_UP              23
 #endif
 
 static UINT WM95_MOUSEWHEEL = 0;
@@ -2006,6 +2045,17 @@ LRESULT CALLBACK QtWndProc( HWND hwnd, UINT message, WPARAM wParam,
 		result = widget->translateWheelEvent( msg );
 		break;
 
+	    case WM_APPCOMMAND:
+		{
+		    uint cmd = GET_APPCOMMAND_LPARAM(lParam);
+		    uint uDevice = GET_DEVICE_LPARAM(lParam);
+		    uint dwKeys = GET_KEYSTATE_LPARAM(lParam);
+
+		    if ( uDevice != FAPPCOMMAND_KEY )
+			break;
+		}
+		break;
+
 #ifndef Q_OS_TEMP
 	    case WM_NCMOUSEMOVE:
 		{
@@ -3365,11 +3415,6 @@ bool QETWidget::translateKeyEvent( const MSG &msg, bool grab )
     if ( GetKeyState(VK_MENU) < 0 )
 	state |= Qt::AltButton;
     //TODO: if it is a pure shift/ctrl/alt keydown, invert state logic, like X
-
-    /*
-    if ( msg.lParam & 0xc0000000 ) // multi media keys on MS keyboards
-	state |= Qt::Keypad;
-    */
 
     if ( msg.message == WM_CHAR ) {
 	// a multi-character key not found by our look-ahead
