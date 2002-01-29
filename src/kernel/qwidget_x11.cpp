3824c4dc0f4fc36c270d6867ed6c0a44e705dede
Matthias Ettrich <ettrich@trolltech.com>
2002-01-29 16:12:28 +0100

    3.0 stuff
    [git-p4: depot-paths = "//depot/qt/main/": change = 56265]

--- src/kernel/qwidget_x11.cpp
+++ src/kernel/qwidget_x11.cpp
@@ -435,12 +435,12 @@ void QWidget::create( WId window, bool initializeWindow, bool destroyOldWindow)
 	}
 
 	XClassHint class_hint;
-	class_hint.res_class = (char *)title;	// app name and widget name
-	class_hint.res_name = name() ? (char *)name() : (char *)title;
+	class_hint.res_class = (char*) title;	// app name and widget name
+	class_hint.res_name = name(0) ? (char*) name(0) : (char*) title;
 	XSetWMProperties( dpy, id, 0, 0, 0, 0, &size_hints, &wm_hints,
 			  &class_hint );
 	XResizeWindow( dpy, id, crect.width(), crect.height() );
-	XStoreName( dpy, id, (char *)title );
+	XStoreName( dpy, id, (char*) title );
 	Atom protocols[3];
 	int n = 0;
 	protocols[n++] = qt_wm_delete_window;	// support del window protocol
