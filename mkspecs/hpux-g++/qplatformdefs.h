99b1ececab15d212ca44c262e8eb0109d6ca60df
Harri Porten <porten@froglogic.com>
2002-02-04 17:01:22 +0100

    p4i integration Integrate change 56695 from 3.0 to main. Description for change 56695:
    : Change 56695 by porten@porten-hp71280 on 2002/02/04 16:57:23
    : 
    : 	those headers need to be included explicitly on HP-UX 9
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/mkspecs/hpux-g++/qplatformdefs.h#3 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 56697]

--- mkspecs/hpux-g++/qplatformdefs.h
+++ mkspecs/hpux-g++/qplatformdefs.h
@@ -34,8 +34,13 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+// explicitly for HP-UX 9
+#include <limits.h>
+
 // DNS header files are not fully covered by X/Open specifications.
 // In particular nothing is said about res_* :/
+#include <netinet/in.h>		// HP-UX 9
+#include <arpa/nameser.h>	// HP-UX 9
 #include <resolv.h>
 // Undeclared - at least on HP-UX 10.20.
 extern "C" int res_init();
