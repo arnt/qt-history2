b0c15d1963a14d0bb9b1bedff13e432f077f4a46
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-03-15 23:22:33 +0100

    More `using' C++ keyword.
    [git-p4: depot-paths = "//depot/qt/main/": change = 59976]

--- tools/designer/designer/widgetaction.h
+++ tools/designer/designer/widgetaction.h
@@ -1,3 +1,23 @@
+/**********************************************************************
+** Copyright (C) 2000-2002 Trolltech AS.  All rights reserved.
+**
+** This file is part of Qt Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
 #ifndef WIDGETACTION_H
 #define WIDGETACTION_H
 
@@ -17,6 +37,9 @@ public:
 		  const char* name = 0, bool toggle = FALSE )
 	: QAction( text, menuText, accel, parent, name, toggle ) {}
 
+#if defined(Q_USING)
+    using QAction::addedTo;
+#endif
     void addedTo( QWidget *, QWidget * );
 
 };
