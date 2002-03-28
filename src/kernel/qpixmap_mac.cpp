5fc008940a2403fd96702dfd29e3c06918f0e0e7
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-03-28 01:17:16 +0100

    p4i integration Integrate change 60689 from 3.0 to main. Description for change 60689:
    : Change 60689 by dimitri@dimitri-paris on 2002/03/28 01:12:20
    : 
    : 	Add copyright.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/src/kernel/qpixmap_mac.cpp#7 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 60694]

--- src/kernel/qpixmap_mac.cpp
+++ src/kernel/qpixmap_mac.cpp
@@ -1,3 +1,30 @@
+/****************************************************************************
+** $Id$
+**
+** Implementation of QFileInfo class
+**
+** Copyright (C) 1992-2002 Trolltech AS.  All rights reserved.
+**
+** This file is part of the kernel module of the Qt GUI Toolkit.
+**
+** Licensees holding valid Qt Enterprise Edition or Qt Professional Edition
+** licenses for Macintosh may use this file in accordance with the Qt Commercial
+** License Agreement provided with the Software.
+**
+** This file is not available for use under any other license without
+** express written permission from the copyright holder.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+**   information about Qt Commercial License Agreements.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
 #include "qpixmap.h"
 #include "qimage.h"
 #include "qpaintdevicemetrics.h"
@@ -678,4 +705,3 @@ bool QPixmap::hasAlpha() const
 {
     return data->mask;
 }
-
