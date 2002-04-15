932dbb9ea8cf7b8a04dc44d8c8cf401ef5263260
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-04-15 23:36:15 +0100

    Consistency.
    [git-p4: depot-paths = "//depot/qt/main/": change = 61780]

--- mkspecs/qws/qnx6-generic-g++/qplatformdefs.h
+++ mkspecs/qws/qnx6-generic-g++/qplatformdefs.h
@@ -2,9 +2,16 @@
 #define QPLATFORMDEFS_H
 
 // Get Qt defines/settings
+
 #include "qglobal.h"
 
+// Set any POSIX/XOPEN defines at the top of this file to turn on specific APIs
+
 #include <unistd.h>
+
+
+// We are hot - unistd.h should have turned on the specific APIs we requested
+
 
 #ifdef QT_THREAD_SUPPORT
 #include <pthread.h>
