8eab5866b215e4f91cfbedf83d2117016550568c
luke <luke@trolltech.com>
2002-04-15 09:24:15 +0100

    Better build options
    [git-p4: depot-paths = "//depot/qt/main/": change = 61688]

--- mkspecs/qws/qnx6-generic-g++/qplatformdefs.h
+++ mkspecs/qws/qnx6-generic-g++/qplatformdefs.h
@@ -4,7 +4,7 @@
 // Get Qt defines/settings
 #include "qglobal.h"
 
-#include <unix.h>
+#include <unistd.h>
 
 #ifdef QT_THREAD_SUPPORT
 #include <pthread.h>
@@ -66,9 +66,7 @@
 
 #define QT_SOCKLEN_T		size_t
 
-#if defined(_XOPEN_SOURCE) && (_XOPEN_SOURCE >= 500)
 #define QT_SNPRINTF		::snprintf
 #define QT_VSNPRINTF		::vsnprintf
-#endif
 
 #endif // QPLATFORMDEFS_H
