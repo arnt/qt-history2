5914cdfc6c64319b1cd1d5aa9c98e8b233a3ba52
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-04-21 20:02:21 +0100

    p4i integration Integrate change 62229 from 3.0 to main. Description for change 62229:
    : Change 62229 by dimitri@dimitri-paris on 2002/04/21 21:00:48
    : 
    : 	Copyright.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/src/tools/qcleanuphandler.h#6 edit
    : ... //depot/qt/3.0/src/tools/qcom_p.h#5 edit
    : ... //depot/qt/3.0/src/tools/qcomlibrary.cpp#2 edit
    : ... //depot/qt/3.0/src/tools/qcomlibrary_p.h#2 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 62230]

--- src/tools/qcomlibrary_p.h
+++ src/tools/qcomlibrary_p.h
@@ -3,22 +3,30 @@
 **
 ** Definition of QComLibrary class
 **
-** Copyright (C) 1992-2002 Trolltech AS.  All rights reserved.
+** Copyright (C) 2001-2002 Trolltech AS.  All rights reserved.
 **
 ** This file is part of the tools module of the Qt GUI Toolkit.
 **
-** Licensees holding valid Qt Enterprise Edition or Qt Professional Edition
-** licenses for Windows may use this file in accordance with the Qt Commercial
-** License Agreement provided with the Software.
+** This file may be distributed under the terms of the Q Public License
+** as defined by Trolltech AS of Norway and appearing in the file
+** LICENSE.QPL included in the packaging of this file.
 **
-** This file is not available for use under any other license without
-** express written permission from the copyright holder.
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** Licensees holding valid Qt Enterprise Edition or Qt Professional Edition
+** licenses may use this file in accordance with the Qt Commercial License
+** Agreement provided with the Software.
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
 ** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
 **   information about Qt Commercial License Agreements.
+** See http://www.trolltech.com/qpl/ for QPL licensing information.
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
 **
 ** Contact info@trolltech.com if any conditions of this licensing are
 ** not clear to you.
@@ -46,6 +54,7 @@
 #endif // QT_H
 
 #ifndef QT_NO_COMPONENT
+
 class Q_EXPORT QComLibrary : public QLibrary
 {
 public:
@@ -62,5 +71,7 @@ private:
     QLibraryInterface *libiface;
 
 };
+
 #endif //QT_NO_COMPONENT
+
 #endif // QUCOMLIBRARY_H
