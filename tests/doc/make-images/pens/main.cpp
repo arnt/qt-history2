93b7c989b6144bfd00f6b6620d9e16ecccb452eb
Mark Summerfield <mark@trolltech.com>
2002-04-23 20:02:44 +0100

    integrate
    [git-p4: depot-paths = "//depot/qt/main/": change = 62483]

--- tests/doc/make-images/pens/main.cpp
+++ tests/doc/make-images/pens/main.cpp
@@ -15,32 +15,112 @@ public:
     void paintEvent( QPaintEvent * )
     {
 	QPainter painter( this );
-	painter.setPen( Qt::black );
 	painter.setFont( QFont( "Helvetica", 10, QFont::Bold ) );
 
-	painter.drawText( 150,  0, 150, 20, Qt::AlignLeft|Qt::AlignBottom, "NoPen" );
-	painter.setPen( Qt::NoPen );
-	painter.drawLine( 10,  10, 140, 10 );
+	painter.setPen( Qt::darkGreen );
+	painter.drawText( 150,  10, 160,  20, Qt::AlignLeft|Qt::AlignBottom, "setWidth(0)" );
+	painter.drawText( 250,  10, 160,  20, Qt::AlignLeft|Qt::AlignBottom, "setWidth(2)" );
+	painter.drawText( 350,  10, 160,  20, Qt::AlignLeft|Qt::AlignBottom, "setWidth(3)" );
+	painter.drawText( 450,  10, 160,  20, Qt::AlignLeft|Qt::AlignBottom, "setWidth(4)" );
 
-	painter.drawText( 150, 40, 150, 20, Qt::AlignLeft|Qt::AlignBottom, "SolidLine" );
-	painter.setPen( Qt::SolidLine );
-	painter.drawLine( 10,  50, 140, 50 );
+	painter.setPen( Qt::darkBlue );
+	QPen pen = painter.pen();
 
-	painter.drawText( 150, 80, 150, 20, Qt::AlignLeft|Qt::AlignBottom, "DashLine" );
-	painter.setPen( Qt::DashLine );
-	painter.drawLine( 10,  90, 140, 90 );
+	painter.drawText(  10,  40, 150,  20, Qt::AlignLeft|Qt::AlignBottom, "NoPen" );
+	pen.setColor( Qt::black );
+	pen.setStyle( Qt::NoPen );
+	painter.setPen( pen );
+	painter.drawLine( 150,  50, 240,  50 );
 
-	painter.drawText( 150, 120, 150, 20, Qt::AlignLeft|Qt::AlignBottom, "DotLine" );
-	painter.setPen( Qt::DotLine );
-	painter.drawLine( 10, 130, 140, 130 );
+	pen.setWidth( 0 );
+	pen.setColor( Qt::darkBlue );
+	painter.setPen( pen );
+	painter.drawText(  10,  70, 150,  20, Qt::AlignLeft|Qt::AlignBottom, "SolidLine" );
+	pen.setColor( Qt::black );
+	pen.setStyle( Qt::SolidLine );
+	painter.setPen( pen );
+	painter.drawLine( 150,  80, 240,  80 );
+	pen.setWidth( 2 );
+	painter.setPen( pen );
+	painter.drawLine( 250,  80, 340,  80 );
+	pen.setWidth( 3 );
+	painter.setPen( pen );
+	painter.drawLine( 350,  80, 440,  80 );
+	pen.setWidth( 4 );
+	painter.setPen( pen );
+	painter.drawLine( 450,  80, 540,  80 );
 
-	painter.drawText( 150, 160, 150, 20, Qt::AlignLeft|Qt::AlignBottom, "DashDotLine" );
-	painter.setPen( Qt::DashDotLine );
-	painter.drawLine( 10, 170, 140, 170 );
+	pen.setWidth( 0 );
+	pen.setColor( Qt::darkBlue );
+	painter.setPen( pen );
+	painter.drawText(  10, 100, 150,  20, Qt::AlignLeft|Qt::AlignBottom, "DashLine" );
+	pen.setColor( Qt::black );
+	pen.setStyle( Qt::DashLine );
+	painter.setPen( pen );
+	painter.drawLine( 150, 110, 240, 110 );
+	pen.setWidth( 2 );
+	painter.setPen( pen );
+	painter.drawLine( 250, 110, 340, 110 );
+	pen.setWidth( 3 );
+	painter.setPen( pen );
+	painter.drawLine( 350, 110, 440, 110 );
+	pen.setWidth( 4 );
+	painter.setPen( pen );
+	painter.drawLine( 450, 110, 540, 110 );
 
-	painter.drawText( 150, 200, 150, 20, Qt::AlignLeft|Qt::AlignBottom, "DashDotDotLine" );
-	painter.setPen( Qt::DashDotDotLine );
-	painter.drawLine( 10, 210, 140, 210 );
+	pen.setWidth( 0 );
+	pen.setColor( Qt::darkBlue );
+	painter.setPen( pen );
+	painter.drawText(  10, 130, 150,  20, Qt::AlignLeft|Qt::AlignBottom, "DotLine" );
+	pen.setColor( Qt::black );
+	pen.setStyle( Qt::DotLine );
+	painter.setPen( pen );
+	painter.drawLine( 150, 140, 240, 140 );
+	pen.setWidth( 2 );
+	painter.setPen( pen );
+	painter.drawLine( 250, 140, 340, 140 );
+	pen.setWidth( 3 );
+	painter.setPen( pen );
+	painter.drawLine( 350, 140, 440, 140 );
+	pen.setWidth( 4 );
+	painter.setPen( pen );
+	painter.drawLine( 450, 140, 540, 140 );
+
+	pen.setWidth( 0 );
+	pen.setColor( Qt::darkBlue );
+	painter.setPen( pen );
+	painter.drawText(  10, 160, 150,  20, Qt::AlignLeft|Qt::AlignBottom, "DashDotLine" );
+	pen.setColor( Qt::black );
+	pen.setStyle( Qt::DashDotLine );
+	painter.setPen( pen );
+	painter.drawLine( 150, 170, 240, 170 );
+	pen.setWidth( 2 );
+	painter.setPen( pen );
+	painter.drawLine( 250, 170, 340, 170 );
+	pen.setWidth( 3 );
+	painter.setPen( pen );
+	painter.drawLine( 350, 170, 440, 170 );
+	pen.setWidth( 4 );
+	painter.setPen( pen );
+	painter.drawLine( 450, 170, 540, 170 );
+
+	pen.setWidth( 0 );
+	pen.setColor( Qt::darkBlue );
+	painter.setPen( pen );
+	painter.drawText(  10, 190, 150,  20, Qt::AlignLeft|Qt::AlignBottom, "DashDotDotLine" );
+	pen.setColor( Qt::black );
+	pen.setStyle( Qt::DashDotDotLine );
+	painter.setPen( pen );
+	painter.drawLine( 150, 200, 240, 200 );
+	pen.setWidth( 2 );
+	painter.setPen( pen );
+	painter.drawLine( 250, 200, 340, 200 );
+	pen.setWidth( 3 );
+	painter.setPen( pen );
+	painter.drawLine( 350, 200, 440, 200 );
+	pen.setWidth( 4 );
+	painter.setPen( pen );
+	painter.drawLine( 450, 200, 540, 200 );
     }
 };
 
@@ -51,7 +131,7 @@ int main(int argc, char **argv)
     app.setMainWidget( mw );
     mw->setCaption( "Pen Styles" );
     mw->setBackgroundColor( Qt::white );
-    mw->resize( 270, 225 );
+    mw->resize( 550, 220 );
     mw->show();
     return app.exec();
 }
