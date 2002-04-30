584f7299ae229f65728bd85d6eca7c93b9a086ff
Mark Summerfield <mark@trolltech.com>
2002-04-30 19:56:45 +0100

    p4i integration Integrate change 62922 from 3.0 to main. Description for change 62922:
    : Change 62922 by mark@ptath on 2002/04/30 20:53:26
    : 
    : 	Doc fixes
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/doc/plugins-howto.doc#7 edit
    : ... //depot/qt/3.0/src/kernel/qtimer.cpp#4 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 62924]

--- src/kernel/qtimer.cpp
+++ src/kernel/qtimer.cpp
@@ -40,63 +40,65 @@
 #include "qobjectlist.h"
 
 /*!
-  \class QTimer qtimer.h
-  \brief The QTimer class provides timer signals and single-shot timers.
+    \class QTimer qtimer.h
+    \brief The QTimer class provides timer signals and single-shot timers.
 
-  \ingroup time
-  \ingroup events
-  \mainclass
+    \ingroup time
+    \ingroup events
+    \mainclass
 
-  It uses \link QTimerEvent timer events\endlink internally to provide a
-  more versatile timer.	 QTimer is very easy to use: create a QTimer, call
-  start() to start it and connect its timeout() to the appropriate slots.
-  When the time is up it will emit the timeout() signal.
+    It uses \link QTimerEvent timer events\endlink internally to
+    provide a more versatile timer. QTimer is very easy to use:
+    create a QTimer, call start() to start it and connect its
+    timeout() to the appropriate slots. When the time is up it will
+    emit the timeout() signal.
 
-  Note that a QTimer object is destroyed automatically when its parent
-  object is destroyed.
+    Note that a QTimer object is destroyed automatically when its
+    parent object is destroyed.
 
-  Example:
-  \code
-    QTimer *timer = new QTimer( myObject );
-    connect( timer, SIGNAL(timeout()), myObject, SLOT(timerDone()) );
-    timer->start( 2000, TRUE ); // 2 seconds single-shot timer
-  \endcode
+    Example:
+    \code
+	QTimer *timer = new QTimer( myObject );
+	connect( timer, SIGNAL(timeout()), myObject, SLOT(timerDone()) );
+	timer->start( 2000, TRUE ); // 2 seconds single-shot timer
+    \endcode
 
-  You can also use the static singleShot() function to create a single
-  shot timer.
+    You can also use the static singleShot() function to create a
+    single shot timer.
 
-  As a special case, a QTimer with timeout 0 times out as soon as all
-  the events in the window system's event queue have been processed.
+    As a special case, a QTimer with timeout 0 times out as soon as
+    all the events in the window system's event queue have been
+    processed.
 
-  This can be used to do heavy work while providing a snappy
-  user interface:
-  \code
-    QTimer *t = new QTimer( myObject );
-    connect( t, SIGNAL(timeout()), SLOT(processOneThing()) );
-    t->start( 0, FALSE );
-  \endcode
+    This can be used to do heavy work while providing a snappy
+    user interface:
+    \code
+	QTimer *t = new QTimer( myObject );
+	connect( t, SIGNAL(timeout()), SLOT(processOneThing()) );
+	t->start( 0, FALSE );
+    \endcode
 
-  myObject->processOneThing() will be called repeatedly and should
-  return quickly (typically after processing one data item) so that Qt
-  can deliver events to widgets and stop the timer as soon as it has
-  done all its work.  This is the traditional way of implementing heavy
-  work in GUI applications; multi-threading is now becoming available
-  on more and more platforms, and we expect that null events will
-  eventually be replaced by threading.
+    myObject->processOneThing() will be called repeatedly and should
+    return quickly (typically after processing one data item) so that
+    Qt can deliver events to widgets and stop the timer as soon as it
+    has done all its work. This is the traditional way of
+    implementing heavy work in GUI applications; multi-threading is
+    now becoming available on more and more platforms, and we expect
+    that null events will eventually be replaced by threading.
 
-  Note that QTimer's accuracy depends on the underlying operating
-  system and hardware.  Most platforms support an accuracy of 20ms;
-  some provide more.  If Qt is unable to deliver the requested number
-  of timer clicks, it will silently discard some.
+    Note that QTimer's accuracy depends on the underlying operating
+    system and hardware. Most platforms support an accuracy of 20ms;
+    some provide more. If Qt is unable to deliver the requested
+    number of timer clicks, it will silently discard some.
 
-  An alternative to using QTimer is to call QObject::startTimer() for
-  your object and reimplement the QObject::timerEvent() event handler
-  in your class (which must, of course, inherit QObject).  The
-  disadvantage is that timerEvent() does not support such high-level
-  features as single-shot timers or signals.
+    An alternative to using QTimer is to call QObject::startTimer()
+    for your object and reimplement the QObject::timerEvent() event
+    handler in your class (which must, of course, inherit QObject).
+    The disadvantage is that timerEvent() does not support such
+    high-level features as single-shot timers or signals.
 
-  Some operating systems limit the number of timers that may be used;
-  Qt tries to work around these limitations.
+    Some operating systems limit the number of timers that may be
+    used; Qt tries to work around these limitations.
 */
 
 
@@ -104,10 +106,10 @@ static const int INV_TIMER = -1;		// invalid timer id
 
 
 /*!
-  Constructs a timer with a \a parent and a \a name.
+    Constructs a timer called \a name, with the parent \a parent.
 
-  Note that the destructor of the parent object will destroy this timer
-  object.
+    Note that the parent object's destructor will destroy this timer
+    object.
 */
 
 QTimer::QTimer( QObject *parent, const char *name )
@@ -116,7 +118,7 @@ QTimer::QTimer( QObject *parent, const char *name )
 }
 
 /*!
-  Destroys the timer.
+    Destroys the timer.
 */
 
 QTimer::~QTimer()
@@ -127,26 +129,28 @@ QTimer::~QTimer()
 
 
 /*!
-  \fn void QTimer::timeout()
-  This signal is emitted when the timer is activated.
+    \fn void QTimer::timeout()
+
+    This signal is emitted when the timer is activated.
 */
 
 /*!
-  \fn bool QTimer::isActive() const
-  Returns TRUE if the timer is running (pending) or FALSE if the timer is
-  idle.
+    \fn bool QTimer::isActive() const
+
+    Returns TRUE if the timer is running (pending); otherwise returns
+    FALSE.
 */
 
 
 /*!
-  Starts the timer with an \a msec milliseconds timeout.
+    Starts the timer with a \a msec milliseconds timeout.
 
-  If \a sshot is TRUE, the timer will be activated only once;
-  otherwise it will continue until it is stopped.
+    If \a sshot is TRUE, the timer will be activated only once;
+    otherwise it will continue until it is stopped.
 
-  Any pending timer will be stopped.
+    Any pending timer will be stopped.
 
-  \sa singleShot() stop(), changeInterval(), isActive()
+    \sa singleShot() stop(), changeInterval(), isActive()
 */
 
 int QTimer::start( int msec, bool sshot )
@@ -162,12 +166,12 @@ int QTimer::start( int msec, bool sshot )
 
 
 /*!
-  Changes the timeout interval to \a msec milliseconds.
+    Changes the timeout interval to \a msec milliseconds.
 
-  If the timer signal is pending, it will be stopped and restarted;
-  otherwise it will be started.
+    If the timer signal is pending, it will be stopped and restarted;
+    otherwise it will be started.
 
-  \sa start(), isActive()
+    \sa start(), isActive()
 */
 
 void QTimer::changeInterval( int msec )
@@ -181,8 +185,9 @@ void QTimer::changeInterval( int msec )
 }
 
 /*!
-  Stops the timer.
-  \sa start()
+    Stops the timer.
+
+    \sa start()
 */
 
 void QTimer::stop()
@@ -194,7 +199,8 @@ void QTimer::stop()
 }
 
 
-/*!\reimp
+/*!
+    \reimp
 */
 bool QTimer::event( QEvent *e )
 {
@@ -268,31 +274,31 @@ bool QSingleShotTimer::event( QEvent * )
 
 
 /*!
-  This static function calls a slot after a given time interval.
+    This static function calls a slot after a given time interval.
 
-  It is very convenient to use this function because you do not need to
-  bother with a \link QObject::timerEvent() timerEvent\endlink or to
-  create a local QTimer object.
+    It is very convenient to use this function because you do not need
+    to bother with a \link QObject::timerEvent() timerEvent\endlink or
+    to create a local QTimer object.
 
-  Example:
-  \code
-    #include <qapplication.h>
-    #include <qtimer.h>
+    Example:
+    \code
+	#include <qapplication.h>
+	#include <qtimer.h>
 
-    int main( int argc, char **argv )
-    {
-	QApplication a( argc, argv );
-	QTimer::singleShot( 10*60*1000, &a, SLOT(quit()) );
-	    ... // create and show your widgets
-	return a.exec();
-    }
-  \endcode
+	int main( int argc, char **argv )
+	{
+	    QApplication a( argc, argv );
+	    QTimer::singleShot( 10*60*1000, &a, SLOT(quit()) );
+		... // create and show your widgets
+	    return a.exec();
+	}
+    \endcode
 
-  This sample program automatically terminates after 10 minutes (i.e.
-  600000 milliseconds).
+    This sample program automatically terminates after 10 minutes (i.e.
+    600000 milliseconds).
 
-  The \a receiver is the receiving object and the \a member is the
-  slot. The time interval is \a msec.
+    The \a receiver is the receiving object and the \a member is the
+    slot. The time interval is \a msec.
 */
 
 void QTimer::singleShot( int msec, QObject *receiver, const char *member )
