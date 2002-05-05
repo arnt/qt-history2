f0f64c412397955a495c15fc1d6bc53dbe1665f1
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-05-05 15:51:51 +0100

    Programs that succeed return 0. Don't go against standards.
    [git-p4: depot-paths = "//depot/qt/main/": change = 63149]

--- config.tests/unix/stl/stltest.cpp
+++ config.tests/unix/stl/stltest.cpp
@@ -11,7 +11,7 @@ templates for common STL container classes.
 
 int main()
 {
-    int rval = 1;
+    int rval = 0;
 
     std::vector<int> v1;
     v1.push_back( 0 );
@@ -21,27 +21,27 @@ int main()
     v1.push_back( 4 );
     if ( v1.size() != 5 ) {
 	std::cout << "stltest: vector failed size check, expected 5, got " << v1.size();
-	rval = 0;
+	++rval;
     }
     if ( v1.capacity() < 5 ) {
 	std::cout << "stltest: vector failed capacity check, expected >= 5, got " << v1.capacity();
-	rval = 0;
+	++rval;
     }
     std::vector<int>::iterator v1it = std::find( v1.begin(), v1.end(), 99 );
     if ( v1it != v1.end() ) {
 	std::cout << "stltest: find failed, expected end(), got " << *v1it;
-	rval = 0;
+	++rval;
     }
     v1it = std::find( v1.begin(), v1.end(), 3 );
     if ( v1it == v1.end() ) {
 	std::cout << "stltest: find failed, expected to find element, got end()";
-	rval = 0;
+	++rval;
     }
     std::vector<int> v2;
     std::copy( v1.begin(), v1it, std::back_inserter( v2 ) );
     if ( v2.size() != 3 ) {
 	std::cout << "stltest: copy failed, expected 3 elements, got " << v2.size();
-	rval = 0;
+	++rval;
     }
 
     std::map<int, double> m1;
@@ -51,26 +51,21 @@ int main()
     m1.insert( std::make_pair( 7, 2.0 ) );
     if ( m1.size() != 4 ) {
 	std::cout << "stltest: map failed size check, expected 4, got " << m1.size();
-	rval = 0;
+	++rval;
     }
     std::map<int,double>::iterator m1it = m1.begin();
     for ( ; m1it != m1.end(); ++m1it ) {
 	if ( (*m1it).second != 2 ) {
 	    std::cout << "stltest: iterator failed, expected 2, got " << (*m1it).second;
-	    rval = 0;
+	    ++rval;
 	}
     }
     std::map< int, double > m2( m1 );
     if ( m2.size() != m1.size() ) {
 	std::cout << "stltest: map copy failed, expected " << m1.size() << " elements, got " << m2.size();
-	rval = 0;
+	++rval;
     }
 
-    if ( !rval ) {
-	std::cout << std::endl << "stltest: Failure:" << rval << std::endl;
-    } else {
-	std::cout << std::endl << "stltest: Success" << std::endl;
-    }
     return rval;
 }
 
