b34f82ad2a96ca3fdad18c72527e693ecdf90c62
Matthias Ettrich <ettrich@trolltech.com>
2002-05-06 19:28:22 +0100

    p4i integration Integrate change 63212 from 3.0 to main. Description for change 63212:
    : Change 63212 by ettrich@matthias-divan on 2002/05/06 20:27:52
    : 
    : 	Improved QStyleSheet::convertFromPlainText 
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/src/kernel/qstylesheet.cpp#7 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 63213]

--- src/kernel/qstylesheet.cpp
+++ src/kernel/qstylesheet.cpp
@@ -1315,10 +1315,9 @@ QTextCustomItem* QStyleSheet::tag(  const QString& name,
 #endif
 
 
-/*!
-    Auxiliary function. Converts the plain text string \a plain to a
-    rich text formatted paragraph while preserving its look.
-
+/*!  Auxiliary function. Converts the plain text string \a plain to a
+    rich text formatted paragraph while preserving most of its look.
+    
     \a mode defines the whitespace mode. Possible values are \c
     QStyleSheetItem::WhiteSpacePre (no wrapping, all whitespaces
     preserved) and \c QStyleSheetItem::WhiteSpaceNormal (wrapping,
@@ -1333,32 +1332,43 @@ QString QStyleSheet::convertFromPlainText( const QString& plain, QStyleSheetItem
     rich += "<p>";
     for ( int i = 0; i < int(plain.length()); ++i ) {
 	if ( plain[i] == '\n' ){
-	    if ( col == 1 )
-		rich += "<p></p>";
-	    else
-		rich += "<br>";
-	    col = 0;
-	}
-	else if ( mode == QStyleSheetItem::WhiteSpacePre && plain[i] == '\t' ){
-	    rich += 0x00a0U;
-	    while ( col % 4 ) {
-		rich += 0x00a0U;
-		++col;
+	    int c = 1;
+	    while ( i+1 < int(plain.length()) && plain[i+1] == '\n' ) {
+		i++;
+		c++;
 	    }
+	    if ( c == 1)
+		rich += "<br>\n";
+	    else {
+		rich += "</p>\n";
+		while ( --c > 1 )
+		    rich += "<br>\n";
+		rich += "<p>";
+	    }
+	    col = 0;
+	} else {
+	    if ( mode == QStyleSheetItem::WhiteSpacePre && plain[i] == '\t' ){
+		rich += 0x00a0U;
+		while ( col % 4 ) {
+		    rich += 0x00a0U;
+		    ++col;
+		}
+	    }
+	    else if ( mode == QStyleSheetItem::WhiteSpacePre && plain[i].isSpace() )
+		rich += 0x00a0U;
+	    else if ( plain[i] == '<' )
+		rich +="&lt;";
+	    else if ( plain[i] == '>' )
+		rich +="&gt;";
+	    else if ( plain[i] == '&' )
+		rich +="&amp;";
+	    else
+		rich += plain[i];
+	    ++col;
 	}
-	else if ( mode == QStyleSheetItem::WhiteSpacePre && plain[i].isSpace() )
-	    rich += 0x00a0U;
-	else if ( plain[i] == '<' )
-	    rich +="&lt;";
-	else if ( plain[i] == '>' )
-	    rich +="&gt;";
-	else if ( plain[i] == '&' )
-	    rich +="&amp;";
-	else
-	    rich += plain[i];
-	++col;
     }
-    rich += "</p>";
+    if ( col != 0 )
+	rich += "</p>";
     return rich;
 }
 
