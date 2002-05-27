1cf8bd0648531c07a2ee709b9f3b76b043453aab
Trond Kjernaasen <trond@trolltech.com>
2002-05-27 09:42:50 +0100

    Make append() scroll the contents properly in LogText mode (ie. like in PlainText or RichText mode).
    [git-p4: depot-paths = "//depot/qt/main/": change = 64326]

--- src/widgets/qtextedit.cpp
+++ src/widgets/qtextedit.cpp
@@ -5539,8 +5539,15 @@ void QTextEdit::optimAppend( const QString &str )
 	if ( lWidth > d->od->maxLineWidth )
 	    d->od->maxLineWidth = lWidth;
     }
+    bool scrollToEnd = contentsY() >= QABS( contentsHeight() - visibleHeight() -
+		       ( horizontalScrollBar()->isVisible() ?
+			 horizontalScrollBar()->height() : 0 ) );
     resizeContents( d->od->maxLineWidth + 4, d->od->numLines * fm.lineSpacing() +
  		    fm.descent() + 1 );
+    if ( scrollToEnd ) {
+	updateScrollBars();
+	ensureVisible( 0, contentsHeight(), 1, 1 );
+    }
     emit textChanged();
 }
 
