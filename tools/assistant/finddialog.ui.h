e51168ea15872c1cb41429697f06cf813295f7ca
Thomas Strehl <thomas@trolltech.com>
2002-05-28 13:37:32 +0100

    Fixed backward search. Introduced messagebox if nothing was found.
    [git-p4: depot-paths = "//depot/qt/main/": change = 64461]

--- tools/assistant/finddialog.ui.h
+++ tools/assistant/finddialog.ui.h
@@ -2,6 +2,7 @@ void FindDialog::init()
 {
     browser = 0;
     fromBegin = TRUE;
+    firstRun = TRUE;
 }
 
 void FindDialog::destroy()
@@ -12,14 +13,33 @@ void FindDialog::doFind()
 {
     if ( !browser )
 	return;
+   
+    int para = 0;
+    int index = 0;
     
-    int dummy = 0;
-    if ( !fromBegin )
+    if ( !radioForward->isChecked() ) {
+	para = browser->paragraphs() - 1;
+	index = browser->paragraphLength( para );
+	if ( index == -1 )
+	    index = 0;
+    }
+    
+    if ( !fromBegin ) 
 	fromBegin = !browser->find( comboFind->currentText(), checkCase->isChecked(),
 			checkWords->isChecked(), radioForward->isChecked() );    
-    else
+    else 
 	fromBegin = !browser->find( comboFind->currentText(), checkCase->isChecked(), 
-			checkWords->isChecked(), radioForward->isChecked(), &dummy, &dummy );    
+			checkWords->isChecked(), radioForward->isChecked(), &para, &index );    
+    
+    if ( fromBegin && firstRun ) {
+	fromBegin = !browser->find( comboFind->currentText(), checkCase->isChecked(),
+			checkWords->isChecked(), radioForward->isChecked(), &para, &index ); 
+	if ( fromBegin )
+	    QMessageBox::information( this, tr( "Find Text" ), 
+				      tr( "Can not find requested text in this document." ) );	
+    }
+    if ( !fromBegin ) 
+	firstRun = FALSE;
 }
 
 void FindDialog::setBrowser( QTextBrowser * b )
@@ -28,3 +48,7 @@ void FindDialog::setBrowser( QTextBrowser * b )
     fromBegin = TRUE;
 }
 
+void FindDialog::sthChanged()
+{
+    firstRun = TRUE;
+}
