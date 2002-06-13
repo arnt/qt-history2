4fc2b1e88c70636072daca97bd9f1e4c0d634464
Thomas Strehl <thomas@trolltech.com>
2002-06-13 13:53:36 +0100

    changed design of finddialog. added statusbar and messages.
    [git-p4: depot-paths = "//depot/qt/main/": change = 65970]

--- tools/assistant/finddialog.ui.h
+++ tools/assistant/finddialog.ui.h
@@ -5,6 +5,11 @@ void FindDialog::init()
     browser = 0;
     fromBegin = TRUE;
     firstRun = TRUE;
+    onceFound = FALSE;
+    findExpr = "";
+    sb = new QStatusBar( this );
+    FindDialogLayout->addWidget( sb );
+    sb->message( tr( "Enter the text you are looking for." ) );
 }
 
 void FindDialog::destroy()
@@ -15,26 +20,33 @@ void FindDialog::doFind()
 {
     if ( !browser )
 	return;
-
+    sb->clear();
+    if ( comboFind->currentText() != findExpr ) 
+	onceFound = FALSE;
+    findExpr = comboFind->currentText();
+        
     int dummy = radioForward->isChecked() ? 0 : INT_MAX;
     if ( !fromBegin )
-	fromBegin = !browser->find( comboFind->currentText(), checkCase->isChecked(),
+	fromBegin = !browser->find( findExpr, checkCase->isChecked(),
 			checkWords->isChecked(), radioForward->isChecked() );
     else
-	fromBegin = !browser->find( comboFind->currentText(), checkCase->isChecked(),
+	fromBegin = !browser->find( findExpr, checkCase->isChecked(),
 			checkWords->isChecked(), radioForward->isChecked(), &dummy, &dummy );
-    if ( fromBegin )
-	QMessageBox::information( this, tr( "Find Text" ),
-				  tr( "Can not find requested text in this document." ) );	
+    if ( fromBegin ) {
+	QApplication::beep();
+	if ( onceFound ) {
+	    if ( radioForward->isChecked() )
+		sb->message( tr( "Search reached end of the document" ) );
+	    else
+		sb->message( tr( "Search reached start of the document" ) );
+	} else
+	    sb->message( tr( "Text not found" ) );  
+    } else
+	onceFound = TRUE;
 }
 
 void FindDialog::setBrowser( QTextBrowser * b )
 {
     browser = b;
     fromBegin = TRUE;
-}
-
-void FindDialog::sthChanged()
-{
-    firstRun = TRUE;
 }
