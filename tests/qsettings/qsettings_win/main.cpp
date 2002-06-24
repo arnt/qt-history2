cf2f27977e073dbf381a9b9729bc3d684d56e06c
Volker Hilsheimer <vohi@trolltech.com>
2002-06-24 08:53:48 +0100

    Use different QSettings test objects for read after write.
    [git-p4: depot-paths = "//depot/qt/main/": change = 66728]

--- tests/qsettings/qsettings_win/main.cpp
+++ tests/qsettings/qsettings_win/main.cpp
@@ -2,34 +2,39 @@
 
 int main ( int argc, char **argv )
 {
-    QSettings settings( TRUE );
-    settings.writeEntry( "/Trolltech/QSettings/string", "String" );
-    settings.writeEntry( "/Trolltech/QSettings/int", 0xffff );
-    settings.writeEntry( "/Trolltech/QSettings/bool", true );
-    settings.writeEntry( "/Trolltech/QSettings/double", 3.1415927 );
+    {
+	QSettings settings( QSettings::Ini );
+	settings.writeEntry( "/Trolltech/QSettings/string", "String" );
+	settings.writeEntry( "/Trolltech/QSettings/int", 0xffff );
+	settings.writeEntry( "/Trolltech/QSettings/bool", true );
+	settings.writeEntry( "/Trolltech/QSettings/double", 3.1415927 );
 
-    QStringList subkeys = settings.subkeyList( "/Trolltech" );
-    for ( QStringList::Iterator it = subkeys.begin(); it != subkeys.end(); ++it ) {
-	qDebug( "/Trolltech/%s", (*it).latin1() );
-	QStringList entries = settings.entryList( "/Trolltech/" + *it );
-	for ( QStringList::Iterator it2 = entries.begin(); it2 != entries.end(); ++it2 ) {
-	    qDebug( "/Trolltech/%s/%s", (*it).latin1(), (*it2).latin1() );
+	QStringList subkeys = settings.subkeyList( "/Trolltech" );
+	for ( QStringList::Iterator it = subkeys.begin(); it != subkeys.end(); ++it ) {
+	    qDebug( "/Trolltech/%s", (*it).latin1() );
+	    QStringList entries = settings.entryList( "/Trolltech/" + *it );
+	    for ( QStringList::Iterator it2 = entries.begin(); it2 != entries.end(); ++it2 ) {
+		qDebug( "/Trolltech/%s/%s", (*it).latin1(), (*it2).latin1() );
+	    }
 	}
     }
 
-    qDebug( settings.readEntry( "/Trolltech/QSettings/string" ) );
-    qDebug( "%d", settings.readNumEntry( "/Trolltech/QSettings/int" ) );
-    qDebug( "%d", settings.readBoolEntry( "/Trolltech/QSettings/bool" ) );
-    qDebug( "%g", settings.readDoubleEntry( "/Trolltech/QSettings/double" ) );
-    bool ok;
-    QString str = settings.readEntry( "/Trolltech/QSettings/foo", QString::null, &ok );
-    if ( ok )
-	qDebug( "%s", str );
+    {
+	QSettings settings( QSettings::Ini );
+	qDebug( settings.readEntry( "/Trolltech/QSettings/string" ) );
+	qDebug( "%d", settings.readNumEntry( "/Trolltech/QSettings/int" ) );
+	qDebug( "%d", settings.readBoolEntry( "/Trolltech/QSettings/bool" ) );
+	qDebug( "%g", settings.readDoubleEntry( "/Trolltech/QSettings/double" ) );
+	bool ok;
+	QString str = settings.readEntry( "/Trolltech/QSettings/foo", QString::null, &ok );
+	if ( ok )
+	    qDebug( "%s", str );
 
-    settings.removeEntry( "/Trolltech/QSettings/string" );
-    settings.removeEntry( "/Trolltech/QSettings/bool" );
-    settings.removeEntry( "/Trolltech/QSettings/double" );
-    settings.removeEntry( "/Trolltech/QSettings/int" );
+	settings.removeEntry( "/Trolltech/QSettings/string" );
+	settings.removeEntry( "/Trolltech/QSettings/bool" );
+	settings.removeEntry( "/Trolltech/QSettings/double" );
+	settings.removeEntry( "/Trolltech/QSettings/int" );
+    }
 
     return 0;
 }
