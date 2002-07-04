0ecd16a4ec48cf61a0c81eaf7de91dea3e09d538
Rainer Schmidt <rms@froglogic.com>
2002-07-04 14:59:17 +0100

    Start working on a new ftpclient example that uses QFtp instead of QUrlOperator. This is basically only the GUI of it.
    [git-p4: depot-paths = "//depot/qt/main/": change = 67457]

--- examples/network/ftpclient/ftpclient.pro
+++ examples/network/ftpclient/ftpclient.pro
@@ -1,9 +1,7 @@
-REQUIRES        = network full-config
-TEMPLATE	= app
+REQUIRES         = network full-config
+TEMPLATE	 = app
 CONFIG		+= qt warn_on release
-HEADERS	= ftpmainwindow.h \
-		ftpview.h
-SOURCES	= ftpmainwindow.cpp \
-		ftpview.cpp \
-		  main.cpp
-TARGET		= ftpclient
+HEADERS		 =
+SOURCES		 = main.cpp
+INTERFACES	 = ftpmainwindow.ui
+TARGET		 = ftpclient
--- examples/network/ftpclient/main.cpp
+++ examples/network/ftpclient/main.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: $
+** $Id$
 **
 ** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
 **
@@ -9,25 +9,14 @@
 *****************************************************************************/
 
 #include <qapplication.h>
-#include <qnetwork.h>
-#include <qsplitter.h>
-
 #include "ftpmainwindow.h"
 
 int main( int argc, char **argv )
 {
     QApplication a( argc, argv );
 
-    // call this to register the built-in network protocols, e.g. FTP
-    // and HTTP.
-    qInitNetworkProtocols();
-
     FtpMainWindow m;
     a.setMainWidget( &m );
-    QValueList<int> sizes;
-    sizes << 300 << 70 << 300;
-    m.mainSplitter()->setSizes( sizes );
-    m.resize( 800, 600 );
     m.show();
     return a.exec();
 }
