1c2f4fbd18778f011a6e13eedfc7c5daa4d99a5a
Mark Summerfield <mark@trolltech.com>
2002-07-08 18:37:41 +0100

    p4i integration Integrate change 67784 from 3.0 to main. Description for change 67784:
    : Change 67784 by mark@ptath on 2002/07/08 19:36:51
    : 
    : 	doc fixes
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/doc/overviews-list.doc#8 edit
    : ... //depot/qt/3.0/doc/sql.doc#5 edit
    : ... //depot/qt/3.0/examples/sql/overview/connect1/main.cpp#4 edit
    : ... //depot/qt/3.0/examples/sql/overview/connection.cpp#2 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 67785]

--- examples/sql/overview/connect1/main.cpp
+++ examples/sql/overview/connect1/main.cpp
@@ -18,6 +18,10 @@ int main( int argc, char *argv[] )
     QApplication app( argc, argv, FALSE );
 
     QSqlDatabase *defaultDB = QSqlDatabase::addDatabase( DB_SALES_DRIVER );
+    if ( ! defaultDB ) {
+	qWarning( "Failed to connect to the database driver" );
+	return 1;
+    }
     defaultDB->setDatabaseName( DB_SALES_DBNAME );
     defaultDB->setUserName( DB_SALES_USER );
     defaultDB->setPassword( DB_SALES_PASSWD );
--- examples/sql/overview/connection.cpp
+++ examples/sql/overview/connection.cpp
@@ -14,24 +14,32 @@ bool createConnections()
 {
 
     QSqlDatabase *defaultDB = QSqlDatabase::addDatabase( DB_SALES_DRIVER );
+    if ( ! defaultDB ) {
+        qWarning( "Failed to connect to " + DB_SALES_DRIVER + " driver" );
+        return FALSE;
+    }
     defaultDB->setDatabaseName( DB_SALES_DBNAME );
     defaultDB->setUserName( DB_SALES_USER );
     defaultDB->setPassword( DB_SALES_PASSWD );
     defaultDB->setHostName( DB_SALES_HOST );
-    if ( ! defaultDB->open() ) { 
-	qWarning( "Failed to open sales database: " + 
+    if ( ! defaultDB->open() ) {
+	qWarning( "Failed to open sales database: " +
 		  defaultDB->lastError().driverText() );
 	qWarning( defaultDB->lastError().databaseText() );
 	return FALSE;
     }
 
     QSqlDatabase *oracle = QSqlDatabase::addDatabase( DB_ORDERS_DRIVER, "ORACLE" );
+    if ( ! oracle ) {
+        qWarning( "Failed to connect to " + DB_ORDERS_DRIVER + " driver" );
+        return FALSE;
+    }
     oracle->setDatabaseName( DB_ORDERS_DBNAME );
     oracle->setUserName( DB_ORDERS_USER );
     oracle->setPassword( DB_ORDERS_PASSWD );
     oracle->setHostName( DB_ORDERS_HOST );
     if ( ! oracle->open() ) {
-	qWarning( "Failed to open orders database: " + 
+	qWarning( "Failed to open orders database: " +
 		  oracle->lastError().driverText() );
 	qWarning( oracle->lastError().databaseText() );
 	return FALSE;
