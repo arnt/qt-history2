99ec10f2d50505b824a4e91c5e9ca39f92443caa
Jasmin Blanchette <jasmin@trolltech.com>
2002-07-12 16:55:47 +0100

    Put some order in the tests.
    [git-p4: depot-paths = "//depot/qt/main/": change = 68181]

---
+++ tests/qdoc3/test/extensions/test.cpp
@@ -0,0 +1,21 @@
+/*! \fn void QListView::selectionChanged()
+
+    This is a Qt signal.
+*/
+
+/*! \fn QListViewItem *QListView::currentItem() const
+
+    This is a Qt plain function. This documentation should be
+    overridden.
+*/
+
+/*! \fn QListViewItem *QListView::lastItem() const
+
+    This is a Qt plain function. This documentation should not
+    be overridden.
+*/
+
+/*! \fn QListViewItem *QuickListViewInterface::currentItem() const
+
+    This is a Quick slot for a documented Qt function.
+*/
