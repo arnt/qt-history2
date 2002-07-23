00b9589184a3b2ba7b3df26dd65d5e9250af09c1
Lars Knoll <lars@trolltech.com>
2002-07-23 11:32:24 +0100

    * added euro countries to 8859-15 locale list. * added a separate implementation for the 8859-15 codec to speed   up this (now) much used case.
    [git-p4: depot-paths = "//depot/qt/main/": change = 68847]

--- src/codecs/qtextcodec.cpp
+++ src/codecs/qtextcodec.cpp
@@ -665,7 +665,13 @@ static const char * const iso8859_13locales[] = {
     "lt", "lt_LT", 0 };
 
 static const char * const iso8859_15locales[] = {
-    "fr", "fi", "french", "finnish", "et", "et_EE", 0 };
+    "et", "et_EE", 
+    // Euro countries
+    "ca_ES", "de", "de_DE", "de_AT", "en_IE", "es", "es_ES", "eu_ES", 
+    "fi", "fi_FI", "finnish", "fr", "fr_FR", "fr_BE", "fr_LU", "french",
+    "ga_IE", "gl_ES", "it", "it_IT", "nl", "nl_BE", "nl_NL", 
+    "pt", "pt_PT", "sv_FI",
+    0 };
 
 static const char * const koi8_ulocales[] = {
     "uk", "uk_UA", "ru_UA", "ukrainian", 0 };
@@ -822,7 +828,9 @@ QTextCodec* QTextCodec::codecForLocale()
 
 	// Now do the quessing.
 	if ( lang && *lang && !localeMapper && try_by_name && *try_by_name ) {
-	    if ( try_locale_list( iso8859_2locales, lang ) )
+	    if ( try_locale_list( iso8859_15locales, lang ) )
+		localeMapper = codecForName( "ISO 8859-15" );
+	    else if ( try_locale_list( iso8859_2locales, lang ) )
 		localeMapper = codecForName( "ISO 8859-2" );
 	    else if ( try_locale_list( iso8859_3locales, lang ) )
 		localeMapper = codecForName( "ISO 8859-3" );
@@ -840,8 +848,6 @@ QTextCodec* QTextCodec::codecForLocale()
 		localeMapper = codecForName( "ISO 8859-9" );
 	    else if ( try_locale_list( iso8859_13locales, lang ) )
 		localeMapper = codecForName( "ISO 8859-13" );
-	    else if ( try_locale_list( iso8859_15locales, lang ) )
-		localeMapper = codecForName( "ISO 8859-15" );
 	    else if ( try_locale_list( tis_620locales, lang ) )
 		localeMapper = codecForName( "ISO 8859-11" );
 	    else if ( try_locale_list( koi8_ulocales, lang ) )
@@ -1987,23 +1993,6 @@ static const struct {
 	      0x00E8, 0x00E9, 0x00EA, 0x00EB, 0x00EC, 0x00ED, 0x00EE, 0x00EF,
 	      0x0175, 0x00F1, 0x00F2, 0x00F3, 0x00F4, 0x00F5, 0x00F6, 0x1E6B,
 	      0x00F8, 0x00F9, 0x00FA, 0x00FB, 0x00FC, 0x00FD, 0x0177, 0x00FF} },
-    { "ISO-8859-15", "ISO 8859-15", 111,
-	{ 0x0080, 0x0081, 0x0082, 0x0083, 0x0084, 0x0085, 0x0086, 0x0087,
-	      0x0088, 0x0089, 0x008A, 0x008B, 0x008C, 0x008D, 0x008E, 0x008F,
-	      0x0090, 0x0091, 0x0092, 0x0093, 0x0094, 0x0095, 0x0096, 0x0097,
-	      0x0098, 0x0099, 0x009A, 0x009B, 0x009C, 0x009D, 0x009E, 0x009F,
-	      0x00A0, 0x00A1, 0x00A2, 0x00A3, 0x20AC, 0x00A5, 0x0160, 0x00A7,
-	      0x0161, 0x00A9, 0x00AA, 0x00AB, 0x00AC, 0x00AD, 0x00AE, 0x00AF,
-	      0x00B0, 0x00B1, 0x00B2, 0x00B3, 0x017D, 0x00B5, 0x00B6, 0x00B7,
-	      0x017E, 0x00B9, 0x00BA, 0x00BB, 0x0152, 0x0153, 0x0178, 0x00BF,
-	      0x00C0, 0x00C1, 0x00C2, 0x00C3, 0x00C4, 0x00C5, 0x00C6, 0x00C7,
-	      0x00C8, 0x00C9, 0x00CA, 0x00CB, 0x00CC, 0x00CD, 0x00CE, 0x00CF,
-	      0x00D0, 0x00D1, 0x00D2, 0x00D3, 0x00D4, 0x00D5, 0x00D6, 0x00D7,
-	      0x00D8, 0x00D9, 0x00DA, 0x00DB, 0x00DC, 0x00DD, 0x00DE, 0x00DF,
-	      0x00E0, 0x00E1, 0x00E2, 0x00E3, 0x00E4, 0x00E5, 0x00E6, 0x00E7,
-	      0x00E8, 0x00E9, 0x00EA, 0x00EB, 0x00EC, 0x00ED, 0x00EE, 0x00EF,
-	      0x00F0, 0x00F1, 0x00F2, 0x00F3, 0x00F4, 0x00F5, 0x00F6, 0x00F7,
-	      0x00F8, 0x00F9, 0x00FA, 0x00FB, 0x00FC, 0x00FD, 0x00FE, 0x00FF} },
 
     // next bits generated again from tables on the Unicode 3.0 CD.
 
@@ -2471,12 +2460,9 @@ int QSimpleTextCodec::heuristicContentMatch(const char* chars, int len) const
 
 #endif
 
-class QLatin1Codec: public QTextCodec
+class QLatin1Codec : public QTextCodec
 {
 public:
-    QLatin1Codec();
-    ~QLatin1Codec();
-
     QString toUnicode(const char* chars, int len) const;
 #if !defined(Q_NO_USING_KEYWORD)
     using QTextCodec::fromUnicode;
@@ -2490,22 +2476,10 @@ public:
 
     int heuristicContentMatch(const char* chars, int len) const;
 
-    int heuristicNameMatch(const char* hint) const;
-
 private:
     int forwardIndex;
 };
 
-
-QLatin1Codec::QLatin1Codec()
-    : QTextCodec()
-{
-}
-
-
-QLatin1Codec::~QLatin1Codec()
-{
-}
 
 QString QLatin1Codec::toUnicode(const char* chars, int len) const
 {
@@ -2559,11 +2533,6 @@ int QLatin1Codec::mibEnum() const
     return 4;
 }
 
-int QLatin1Codec::heuristicNameMatch(const char* hint) const
-{
-    return QTextCodec::heuristicNameMatch(hint);
-}
-
 int QLatin1Codec::heuristicContentMatch(const char* chars, int len) const
 {
     if ( len<1 || !chars )
@@ -2580,13 +2549,175 @@ int QLatin1Codec::heuristicContentMatch(const char* chars, int len) const
 	i++;
 	c++;
     }
+    if ( this == (const QTextCodec *)codecForLocale() )
+	r += 5;
     return r;
 }
+
+class QLatin15Codec: public QLatin1Codec
+{
+public:
+    QString toUnicode(const char* chars, int len) const;
+#if !defined(Q_NO_USING_KEYWORD)
+    using QTextCodec::fromUnicode;
+#endif
+    QCString fromUnicode(const QString& uc, int& lenInOut ) const;
+    unsigned short characterFromUnicode(const QString &str, int pos) const;
+
+    const char* name() const;
+    const char* mimeName() const;
+    int mibEnum() const;
+
+private:
+    int forwardIndex;
+};
+
+
+QString QLatin15Codec::toUnicode(const char* chars, int len) const
+{
+    if ( chars == 0 )
+	return QString::null;
+
+    QString str = QString::fromLatin1(chars, len);
+    QChar *uc = (QChar *)str.unicode();
+    while( len-- ) {
+	switch( uc->unicode() ) {
+	    case 0xa4:
+		*uc = 0x20ac;
+		break;
+	    case 0xa6:
+		*uc = 0x0160;
+		break;
+	    case 0xa8:
+		*uc = 0x0161;
+		break;
+	    case 0xb4:
+		*uc = 0x017d;
+		break;
+	    case 0xb8:
+		*uc = 0x017e;
+		break;
+	    case 0xbc:
+		*uc = 0x0152;
+		break;
+	    case 0xbd:
+		*uc = 0x0153;
+		break;
+	    case 0xbe:
+		*uc = 0x0178;
+		break;
+	    default:
+		break;
+	}
+	uc++;
+    }
+    return str;
+}
+
+static inline unsigned char latin15CharFromUnicode( unsigned short uc ) 
+{
+    uchar c;
+    if ( uc < 0x0100 ) {
+	if ( uc > 0xa3 && uc < 0xbf ) {
+	    switch( uc ) {
+		case 0xa4:
+		case 0xa6:
+		case 0xa8:
+		case 0xb4:
+		case 0xb8:
+		case 0xbc:
+		case 0xbd:
+		case 0xbe:
+		    c = '?';
+		    break;
+		default:
+		    c = (unsigned char) uc;
+		    break;
+	    }
+	} else {
+	    c = (unsigned char) uc;
+	}
+    } else {
+	if ( uc == 0x20ac )
+	    c = 0xa4;
+	else if ( uc & 0xff00 == 0x0100 ) {
+	    switch( uc ) {
+		case 0x0160:
+		    c = 0xa6;
+		    break;
+		case 0x0161:
+		    c = 0xa8;
+		    break;
+		case 0x017d:
+		    c = 0xb4;
+		    break;
+		case 0x017e:
+		    c = 0xb8;
+		    break;
+		case 0x0152:
+		    c = 0xbc;
+		    break;
+		case 0x0153:
+		    c = 0xbd;
+		    break;
+		case 0x0178:
+		    c = 0xbe;
+		    break;
+		default:
+		    c = '?';
+	    }
+	} else {
+	    c = '?';
+	}
+    }
+    return c;
+}
+QCString QLatin15Codec::fromUnicode(const QString& uc, int& len ) const
+{
+    if ( len <0 || len > (int)uc.length() )
+	len = uc.length();
+    QCString r( len+1 );
+    char *d = r.data();
+    int i = 0;
+    const QChar *ch = uc.unicode();
+    while ( i < len ) {
+	d[i] = latin15CharFromUnicode( ch->unicode() );
+	i++;
+	ch++;
+    }
+    r[len] = 0;
+    return r;
+}
+
+unsigned short QLatin15Codec::characterFromUnicode(const QString &str, int pos) const
+{
+    return latin15CharFromUnicode( str.unicode()[pos].unicode() );
+}
+
+
+const char* QLatin15Codec::name() const
+{
+    return "ISO 8859-15";
+}
+
+const char* QLatin15Codec::mimeName() const
+{
+    return "ISO-8859-15";
+}
+
+
+int QLatin15Codec::mibEnum() const
+{
+    return 111;
+}
+
+
 
 
 static void setupBuiltinCodecs()
 {
     (void)new QLatin1Codec;
+    (void)new QLatin15Codec;
     (void)new QUtf8Codec;
     (void)new QUtf16Codec;
 
