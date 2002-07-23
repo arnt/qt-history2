a4a07a2bc6ef234d7362d463283acdebda0945b0
John Ryland <jryland@trolltech.com>
2002-07-23 02:18:34 +0100

    p4i integration Integrate change 68814 from 3.0 to main. Description for change 68814:
    : Change 68814 by jryland@jryland-igloo on 2002/07/23 03:17:05
    : 
    : 	Update copyright headers so they are correct
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/src/kernel/qfunctions_wce.cpp#2 edit
    : ... //depot/qt/3.0/src/kernel/qfunctions_wce.h#2 edit
    : ... //depot/qt/3.0/src/kernel/qwinfunctions_wce.h#2 edit
    : ... //depot/qt/3.0/src/styles/qpocketpcstyle_wce.cpp#5 edit
    : ... //depot/qt/3.0/src/styles/qpocketpcstyle_wce.h#2 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 68815]

--- src/kernel/qfunctions_wce.h
+++ src/kernel/qfunctions_wce.h
@@ -1,3 +1,31 @@
+/****************************************************************************
+** $Id: $
+**
+** Compatibility functionality for WindowsCE platforms 
+**
+** Created : 2001/06/13
+**
+** Copyright (C) 2001-2002 Trolltech AS.  All rights reserved.
+**
+** This file is part of the styles module of the Qt GUI Toolkit.
+**
+** Licensees holding valid Qt Enterprise Edition or Qt Professional Edition
+** licenses for Windows may use this file in accordance with the Qt Commercial
+** License Agreement provided with the Software.
+**
+** This file is not available for use under any other license without
+** express written permission from the copyright holder.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+**   information about Qt Commercial License Agreements.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
 #ifndef QFUNCTIONS_WCE_H
 #define QFUNCTIONS_WCE_H
 
