0cd0bf7ae63f45a7de9db99b00e447a846d8abef
Jasmin Blanchette <jasmin@trolltech.com>
2002-07-23 14:39:42 +0100

    Modify the tests a bit.
    [git-p4: depot-paths = "//depot/qt/main/": change = 68859]

--- tests/qdoc3/test/extensions/test.cpp
+++ tests/qdoc3/test/extensions/test.cpp
@@ -38,8 +38,8 @@
 
   \target 5
   \code
-    QString a("abcd");
     QString b( a.unicode(), b.length() );
+    QString a("abcd");
   \endcode
 
   \target 6
@@ -54,6 +54,95 @@
     QString s = "truncate me";
     s.truncate( 5 );            // s == "trunc"
   \endcode
+
+    \code
+    QLabel *splashScreen = new QLabel( 0, "mySplashScreen",
+				WStyle_Customize | WStyle_NoBorder |
+				WStyle_Tool );
+    \endcode
+
+    \code
+	width = baseSize().width() + i * sizeIncrement().width();
+	height = baseSize().height() + j * sizeIncrement().height();
+    \endcode
+
+    \code
+	aWidget->topLevelWidget()->setCaption( "New Caption" );
+    \endcode
+
+    \code
+    setBackgroundMode( PaletteBase );
+    \endcode
+
+    \code
+    QFont f( "Helvetica", 12, QFont::Bold );
+    setFont( f );
+    \endcode
+
+    \code
+	setCursor( IbeamCursor );
+    \endcode
+
+    \code
+	setTabOrder( a, b ); // a to b
+	setTabOrder( b, c ); // a to b to c
+	setTabOrder( c, d ); // a to b to c to d
+    \endcode
+
+    \code
+	setUpdatesEnabled( FALSE );
+	bigVisualChanges();
+	setUpdatesEnabled( TRUE );
+	repaint();
+    \endcode
+
+    \code
+	repaint( w->visibleRect(), FALSE );
+    \endcode
+
+    \code
+	if ( autoMask() )
+	    updateMask();
+    \endcode
+
+    \code
+    QSqlQuery query( "SELECT name FROM customer" );
+    while ( query.next() ) {
+	QString name = query.value(0).toString();
+	doSomething( name );
+    }
+    \endcode
+
+    \code
+    // Binding values using named placeholders (e.g. in Oracle)
+    QSqlQuery q;
+    q.prepare( "insert into mytable (id, name, lastname) values (:id, :name, :lname)" );
+    q.bindValue( ":id", 0 );
+    q.bindValue( ":name", "Testname" );
+    q.bindValue( ":lname", "Lastname" );
+    q.exec();
+
+    // Binding values using positional placeholders (e.g. in ODBC)
+    QSqlQuery q;
+    q.prepare( "insert into mytable (id, name, lastname) values (?, ?, ?)" );
+    q.bindValue( 0, 0 );
+    q.bindValue( 1, "Testname" );
+    q.bindValue( 2, "Lastname" );
+    q.exec();
+    
+    // or alternatively
+    q.prepare( "insert into mytable (id, name, lastname) values (?, ?, ?)" );
+    q.addBindValue( 0 );
+    q.addBindValue( "Testname" );
+    q.addBindValue( "Lastname" );
+    q.exec();
+    
+    // and then for repeated runs:
+    q.bindValue( 0, 1 );
+    q.bindValue( 1, "John" );
+    q.bindValue( 2, "Doe" );    
+    q.exec();
+    \endcode
 */
 
 /*! \fn QListViewItem *QListView::lastItem() const
--- tests/qdoc3/test/qsa/test.cpp
+++ tests/qdoc3/test/qsa/test.cpp
@@ -5,6 +5,7 @@
     \replace /qApp/Application/
     \replace /(filter)\(\)/\1/
     \replace /setEnabled\(true\)/enabled = true/
+    \replace /(WStyle_[A-Za-z0-9]+)/"\1"/
 */
 
 /*! \quickfn ListView.setContentsPos( Number alpha, Number beta )
