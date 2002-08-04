450cd8a42788b9bcb71ae2690ed367303b4a1a68
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-08-04 16:46:19 +0100

    p4i integration Integrate change 69851 from 3.0 to main. Description for change 69851:
    : Change 69851 by dimitri@dimitri-paris on 2002/08/04 17:45:41
    : 
    : 	Have Assistant say it's licensed under GPL in the "About" dialog.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/tools/assistant/mainwindow.ui.h#18 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 69852]

--- tools/assistant/mainwindow.ui.h
+++ tools/assistant/mainwindow.ui.h
@@ -260,7 +260,16 @@ void MainWindow::about()
     "<p><b><font size=+2>Qt Assistant</font></b></p>"
     "<p>The Qt documentation browser</p>"
     "<p>Version 2.0</p>"
-    "<p>Copyright (C) 2001-2002 Trolltech AS. All rights reserved.</p>";
+    "<p>Copyright (C) 2001-2002 Trolltech AS. All rights reserved.</p>"
+    "<p></p>"
+    "<p>This program is licensed to you under the terms of the GNU General "
+    "Public License Version 2 as published by the Free Software Foundation. This "
+    "gives you legal permission to copy, distribute and/or modify this software "
+    "under certain conditions. For details, see the file 'COPYING' that came with "
+    "this software distribution. If you did not get the file, send email to "
+    "info@trolltech.com.</p>\n\n<p>The program is provided AS IS with NO WARRANTY "
+    "OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS "
+    "FOR A PARTICULAR PURPOSE.</p>";
     QMessageBox::about( this, tr("Qt Assistant"), tr( about_text ) );
 }
 
