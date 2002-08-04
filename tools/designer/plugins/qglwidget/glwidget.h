879aac95f5215a1f17ef277ffdfc2804613ca811
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-08-04 13:16:22 +0100

    p4i integration Integrate change 69827 from 3.0 to main. Description for change 69827:
    : Change 69827 by dimitri@dimitri-paris on 2002/08/04 14:15:36
    : 
    : 	Copyright.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/tools/designer/plugins/qglwidget/glwidget.cpp#3 edit
    : ... //depot/qt/3.0/tools/designer/plugins/qglwidget/glwidget.h#3 edit
    : ... //depot/qt/3.0/tools/designer/plugins/qglwidget/main.cpp#3 edit
    : ... //depot/qt/3.0/tools/designer/plugins/rc/main.cpp#2 edit
    : ... //depot/qt/3.0/tools/designer/plugins/rc/rc2ui.cpp#2 edit
    : ... //depot/qt/3.0/tools/designer/plugins/rc/rc2ui.h#2 edit
    : ... //depot/qt/3.0/tools/designer/plugins/wizards/main.cpp#3 edit
    : ... //depot/qt/3.0/tools/designer/plugins/wizards/mainwindowwizard.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/plugins/wizards/mainwindowwizard.ui.h#2 edit
    : ... //depot/qt/3.0/tools/designer/plugins/wizards/sqlformwizard.ui#4 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 69828]

--- tools/designer/plugins/qglwidget/glwidget.h
+++ tools/designer/plugins/qglwidget/glwidget.h
@@ -1,3 +1,24 @@
+/**********************************************************************
+**
+** Copyright (C) 2000-2001 Trolltech AS.  All rights reserved.
+**
+** This file is part of Qt Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
 
@@ -38,4 +59,4 @@ private:
     GLfloat xrot, yrot, zrot, scale_;
 };
 
-#endif //GLWIDGET_H
+#endif
