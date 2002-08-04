b0d84d48bfebc3250ef04cda1cbe350acf24d70d
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-08-04 13:49:35 +0100

    p4i integration Integrate change 69829 from 3.0 to main. Description for change 69829:
    : Change 69829 by dimitri@dimitri-paris on 2002/08/04 14:44:25
    : 
    : 	Copyright.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/tools/designer/designer/about.ui#5 edit
    : ... //depot/qt/3.0/tools/designer/designer/actioneditor.ui#3 edit
    : ... //depot/qt/3.0/tools/designer/designer/dbconnection.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/dbconnectioneditor.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/dbconnections.ui#3 edit
    : ... //depot/qt/3.0/tools/designer/designer/filechooser.cpp#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/filechooser.h#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/finddialog.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/finddialog.ui.h#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/gotolinedialog.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/gotolinedialog.ui.h#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/listeditor.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/outputwindow.cpp#4 edit
    : ... //depot/qt/3.0/tools/designer/designer/outputwindow.h#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/pixmapcollectioneditor.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/pixmapcollectioneditor.ui.h#3 edit
    : ... //depot/qt/3.0/tools/designer/designer/previewwidget.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/projectsettings.ui#3 edit
    : ... //depot/qt/3.0/tools/designer/designer/qcompletionedit.cpp#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/qcompletionedit.h#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/replacedialog.ui#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/replacedialog.ui.h#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/tableeditor.ui#3 edit
    : ... //depot/qt/3.0/tools/designer/designer/tableeditorimpl.cpp#4 edit
    : ... //depot/qt/3.0/tools/designer/designer/tableeditorimpl.h#2 edit
    : ... //depot/qt/3.0/tools/designer/designer/wizardeditor.ui#3 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 69830]

--- tools/designer/designer/pixmapcollectioneditor.ui.h
+++ tools/designer/designer/pixmapcollectioneditor.ui.h
@@ -1,3 +1,23 @@
+/**********************************************************************
+** Copyright (C) 2001-2002 Trolltech AS.  All rights reserved.
+**
+** This file is part of Qt Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
 void PixmapCollectionEditor::init()
 {
     project = 0;
@@ -121,4 +141,3 @@ QPixmap PixmapCollectionEditor::scaledPixmap( const QPixmap & p )
     pix.convertFromImage( img );
     return pix;
 }
-
