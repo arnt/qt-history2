4c3c91a36e679533313ae7f3f6db2f1cfa3fb56f
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-08-08 20:43:33 +0100

    Integrate manually.
    [git-p4: depot-paths = "//depot/qt/main/": change = 70413]

--- tools/qvfb/qvfb.cpp
+++ tools/qvfb/qvfb.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+** Copyright (C) 1992-2002 Trolltech AS.  All rights reserved.
 **
 ** This file is part of Qt/Embedded virtual framebuffer.
 **
@@ -301,13 +301,14 @@ void QVFb::about()
 	"<p>Version 1.0</p>"
 	"<p>Copyright (C) 2001-2002 Trolltech AS. All rights reserved.</p>"
 	"<p></p>"
-	"<p>This application runs under Qt/" + qt + ", emulating a simple framebuffer, "
-	"which the Qt/Embedded server and clients can attach to just as if "
-	"it was a realframebuffer. "
-	"<p>With the aid of this development tool, you can develop Qt/Embedded "
-	"applications under " + platform + " without having to switch to a virtual console. "
-	"This means you can comfortably use your other development tools such "
-	"as GUI profilers and debuggers."
+	"<p>This program is licensed to you under the terms of the GNU General "
+	"Public License Version 2 as published by the Free Software Foundation. This "
+	"gives you legal permission to copy, distribute and/or modify this software "
+	"under certain conditions. For details, see the file 'COPYING' that came with "
+	"this software distribution. If you did not get the file, send email to "
+	"info@trolltech.com.</p>\n\n<p>The program is provided AS IS with NO WARRANTY "
+	"OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS "
+	"FOR A PARTICULAR PURPOSE.</p>"
     );
 }
 
