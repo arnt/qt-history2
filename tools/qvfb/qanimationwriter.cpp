1596a308bf6127f214bb2597bfe2cd0ca2935e54
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-08-08 20:34:40 +0100

    p4i integration Integrate change 70408 from 3.0 to main. Description for change 70408:
    : Change 70408 by dimitri@dimitri-paris on 2002/08/08 21:32:31
    : 
    : 	QVFB is distributed under GPL.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/tools/qvfb/config.ui#3 edit
    : ... //depot/qt/3.0/tools/qvfb/gammaview.h#2 edit
    : ... //depot/qt/3.0/tools/qvfb/main.cpp#2 edit
    : ... //depot/qt/3.0/tools/qvfb/qanimationwriter.cpp#2 edit
    : ... //depot/qt/3.0/tools/qvfb/qanimationwriter.h#2 edit
    : ... //depot/qt/3.0/tools/qvfb/qvfb.cpp#6 edit
    : ... //depot/qt/3.0/tools/qvfb/qvfb.h#3 edit
    : ... //depot/qt/3.0/tools/qvfb/qvfbratedlg.cpp#2 edit
    : ... //depot/qt/3.0/tools/qvfb/qvfbratedlg.h#2 edit
    : ... //depot/qt/3.0/tools/qvfb/qvfbview.cpp#4 edit
    : ... //depot/qt/3.0/tools/qvfb/qvfbview.h#2 edit
    : ... //depot/qt/3.0/tools/qvfb/skin.cpp#3 edit
    : ... //depot/qt/3.0/tools/qvfb/skin.h#3 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 70409]

--- tools/qvfb/qanimationwriter.cpp
+++ tools/qvfb/qanimationwriter.cpp
@@ -1,21 +1,22 @@
-/****************************************************************************
-**
-** Qt/Embedded virtual framebuffer
-**
-** Created : 20000605
-**
+/**********************************************************************
 ** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
 **
-** This file is part of the Qt GUI Toolkit.
+** This file is part of Qt/Embedded virtual framebuffer.
 **
-** Licensees holding valid Qt Professional Edition licenses may use this
-** file in accordance with the Qt Professional Edition License Agreement
-** provided with the Qt Professional Edition.
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
 **
-** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
-** information about the Professional Edition licensing.
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
-*****************************************************************************/
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
 
 #define QT_CLEAN_NAMESPACE
 #include "qanimationwriter.h"
