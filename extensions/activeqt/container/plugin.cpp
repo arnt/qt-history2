1de14ca919e382b9c80dfc5244d04466d72b1752
Volker Hilsheimer <vohi@trolltech.com>
2002-08-15 21:25:16 +0100

    Add a fancy Designer Action that shows the API reference of the current QAxWidget objects in the form.
    [git-p4: depot-paths = "//depot/qt/main/": change = 71160]

--- extensions/activeqt/container/plugin.cpp
+++ extensions/activeqt/container/plugin.cpp
@@ -28,18 +28,25 @@
 #ifdef QT_PLUGIN
 
 #include <qwidgetplugin.h>
+#include <qdialog.h>
+#include <qlayout.h>
 #include <qlistbox.h>
 #include <qapplication.h>
 #include <qtimer.h>
+#include <qaction.h>
+#include <qtextbrowser.h>
+#include <qwidgetlist.h>
+#include <qobjectcleanuphandler.h>
 #include <private/qwidgetinterface_p.h>
 #include <actioninterface.h>
 #include <designerinterface.h>
+#include <qdict.h>
 
 #include "qaxwidget.h"
 #include "qactivexselect.h"
 
 /* XPM */
-static const char *icon[]={
+static const char *widgetIcon[]={
 "15 13 4 1",
 "a c #000000",
 "# c #808080",
@@ -59,9 +66,32 @@ static const char *icon[]={
 "....a.########a",
 "....aaaaaaaaaaa"};
 
-class QActiveXPlugin : public QWidgetFactoryInterface, 
+static const char *helpIcon[]={
+"15 13 4 1",
+"a c #000000",
+"# c #808080",
+". c #d4d0c8",
+"b c #ffffff",
+"......#a#.#a#..",
+"......aba.aba..",
+"......#a#.#a#..",
+".......a...a...",
+"....aaaaaaaaaaa",
+"....a.........a",
+"....a........#a",
+"#a#.a.a.aaaa.#a",
+"abaaaaaa.a.a.#a",
+"#a#.aa.a.a.aa#a",
+"....a........#a",
+"....a.########a",
+"....aaaaaaaaaaa"};
+
+class QActiveXPlugin : public QObject,
+		       public QLibraryInterface,
+		       public QWidgetFactoryInterface, 
 		       public ActionInterface
 {
+    Q_OBJECT
 public:
     QActiveXPlugin();
     ~QActiveXPlugin();
@@ -83,14 +113,33 @@ public:
     QString whatsThis( const QString& ) const;
     bool isContainer( const QString& ) const;
 
+
+    bool init() { return TRUE; }
+    void cleanup() {}
+    bool canUnload() const { return objects.isEmpty(); }
+
+protected slots:
+    void onFormChange();
+    void onSelectionChanged();
+    void onShowDocumentation();
+
 private:
+    QAction *actionDocu;
+
+    QDict<QDialog> browserDict;
+    QObjectCleanupHandler objects;
+
     static DesignerInterface *designer;
 };
+
+#include "plugin.moc"
 
 DesignerInterface *QActiveXPlugin::designer = 0;
 
 QActiveXPlugin::QActiveXPlugin()
+: actionDocu(0)
 {
+    browserDict.setAutoDelete( TRUE );
     if ( designer )
 	designer->addRef();
 }
@@ -101,11 +150,75 @@ QActiveXPlugin::~QActiveXPlugin()
 	designer->release();
 }
 
+void QActiveXPlugin::onFormChange()
+{
+    DesignerFormWindow *currentForm = designer->currentForm();
+    if ( !currentForm || !currentForm->form() )
+	return;
+
+    currentForm->form()->disconnect( SIGNAL(selectionChanged()), this );
+    connect( currentForm->form(), SIGNAL(selectionChanged()), this, SLOT(onSelectionChanged()) );
+}
+
+void QActiveXPlugin::onSelectionChanged()
+{
+    if ( !designer || !designer->currentForm() || !actionDocu )
+	return;
+    
+    QWidgetList widgets = designer->currentForm()->selectedWidgets();
+    QWidgetListIt it( widgets );
+    bool enabled = FALSE;
+    while ( it.current() ) {
+	QWidget *widget = it.current();
+	++it;
+	QAxWidget *axwidget = (QAxWidget*)widget->qt_cast( "QAxWidget" );
+	if ( !axwidget ) {
+	    actionDocu->setEnabled( FALSE );
+	    return;
+	} else {
+	    enabled = TRUE;
+	}
+    }
+    actionDocu->setEnabled( enabled );
+}
+
+void QActiveXPlugin::onShowDocumentation()
+{
+    if ( !designer || !designer->currentForm() )
+	return;
+
+    QWidgetList widgets = designer->currentForm()->selectedWidgets();
+    QWidgetListIt it( widgets );
+    while ( it.current() ) {
+	QWidget *widget = it.current();
+	++it;
+	if ( widget->inherits( "QAxWidget" ) ) {
+	    QAxWidget *axwidget = (QAxWidget*)widget;
+	    if ( !axwidget->isNull() ) {
+		QDialog *dialog = browserDict[ axwidget->control() ];
+		if ( !dialog ) {
+		    dialog = new QDialog( axwidget->topLevelWidget(), 0, FALSE, 0 );
+		    QVBoxLayout *layout = new QVBoxLayout( dialog );
+		    QTextBrowser *docuView = new QTextBrowser( dialog );
+		    layout->addWidget( docuView );
+
+		    dialog->setCaption( tr("API of %1").arg( axwidget->control() ) );
+		    browserDict.insert( axwidget->control(), dialog );
+		    docuView->setText( axwidget->generateDocumentation() );
+		}
+		dialog->show();
+	    }
+	}
+    }
+}
+
 QRESULT QActiveXPlugin::queryInterface( const QUuid &iid, QUnknownInterface **iface )
 {
     *iface = 0;
     if ( iid == IID_QUnknown )
 	*iface = (QUnknownInterface*)(ActionInterface*) this;
+    else if ( iid == IID_QLibrary )
+	*iface = (QLibraryInterface*)this;
     else if ( iid == IID_QFeatureList )
 	*iface = (QFeatureListInterface*)(ActionInterface*) this;
     else if ( iid == IID_Action )
@@ -128,14 +241,22 @@ QStringList QActiveXPlugin::featureList() const
     return list;
 }
 
-QAction *QActiveXPlugin::create( const QString &/*key*/, QObject * /*parent*/ )
+QAction *QActiveXPlugin::create( const QString &key, QObject *parent )
 {
+    if ( key == "QAxWidget" ) {
+	actionDocu = new QAction( "QAxWidget Documentation", QIconSet( helpIcon ),
+	                          "QA&xWidget Documentation", 0, parent, "qaxdocu" );
+	actionDocu->setEnabled( FALSE );
+	objects.add( actionDocu );
+	connect( actionDocu, SIGNAL(activated()), this, SLOT(onShowDocumentation()) );
+	return actionDocu;
+    }
     return 0;
 }
 
 bool QActiveXPlugin::location( const QString & /*name*/, Location /*l*/ ) const
 {
-    return FALSE;
+    return TRUE;
 }
 
 void QActiveXPlugin::connectTo( QUnknownInterface *app )
@@ -143,6 +264,11 @@ void QActiveXPlugin::connectTo( QUnknownInterface *app )
     if ( !app )
 	return;
     app->queryInterface( IID_Designer, (QUnknownInterface**)&designer );
+    if ( !designer )
+	return;
+
+    designer->addRef();
+    designer->onFormChange( this, SLOT(onFormChange()) );
 }
 
 QWidget* QActiveXPlugin::create( const QString &key, QWidget* parent, const char* name )
@@ -172,7 +298,7 @@ QString QActiveXPlugin::group( const QString& key ) const
 QIconSet QActiveXPlugin::iconSet( const QString &key ) const
 {
     if ( key == "QAxWidget" )
-	return QIconSet( icon );
+	return QIconSet( widgetIcon );
     return QIconSet();
 }
 
