68c517e1ff1dfd459494e4d962c4006ac4503af6
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-08-24 18:43:21 +0100

    p4i integration Integrate change 71762 from 3.0 to main. Description for change 71762:
    : Change 71762 by dimitri@dimitri-paris on 2002/08/24 19:42:17
    : 
    : 	Fixes for the BSD/OS platform (task 5116).
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/configure#142 edit
    : ... //depot/qt/3.0/mkspecs/bsdi-g++/qplatformdefs.h#4 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 71763]

--- mkspecs/bsdi-g++/qplatformdefs.h
+++ mkspecs/bsdi-g++/qplatformdefs.h
@@ -76,15 +76,10 @@
 #define QT_SIGNAL_ARGS		int
 #define QT_SIGNAL_IGNORE	SIG_IGN
 
-#define QT_SOCKLEN_T		size_t
+#define QT_SOCKLEN_T		socklen_t
+#define QT_SOCKOPTLEN_T		size_t
 
 #define QT_SNPRINTF		::snprintf
 #define QT_VSNPRINTF		::vsnprintf
-
-// Older BSD/OS versions may still use the a.out format instead of ELF.
-#ifndef __ELF__
-#define QT_AOUT_UNDERSCORE
-#endif
-
 
 #endif // QPLATFORMDEFS_H
