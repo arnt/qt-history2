963af27520c1dd17978d2752e25715b009cca482
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-08-25 12:27:45 +0100

    p4i integration Integrate change 71781 from 3.0 to main. Description for change 71781:
    : Change 71781 by dimitri@dimitri-paris on 2002/08/25 13:27:34
    : 
    : 	Ooops.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/src/kernel/qaccel.cpp#14 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 71782]

--- src/kernel/qaccel.cpp
+++ src/kernel/qaccel.cpp
@@ -5,7 +5,7 @@
 **
 ** Created : 950419
 **
-** Copyright (C) 1992-2002Trolltech AS.  All rights reserved.
+** Copyright (C) 1992-2002 Trolltech AS.  All rights reserved.
 **
 ** This file is part of the kernel module of the Qt GUI Toolkit.
 **
