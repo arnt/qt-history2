09fe4f53083fee85ad3e3cfc6389d6779b680f33
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-09-12 22:53:37 +0100

    Copyright.
    [git-p4: depot-paths = "//depot/qt/main/": change = 73663]

--- tools/designer/designer/connectionitems.cpp
+++ tools/designer/designer/connectionitems.cpp
@@ -1,12 +1,32 @@
+/**********************************************************************
+** Copyright (C) 2002 Trolltech AS.  All rights reserved.
+**
+** This file is part of Qt Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
 #include "connectionitems.h"
 #include "formwindow.h"
 #include "mainwindow.h"
-#include <qpainter.h>
 #include "metadatabase.h"
-#include <qcombobox.h>
-#include <qmetaobject.h>
 #include "widgetfactory.h"
 #include "project.h"
+#include <qpainter.h>
+#include <qcombobox.h>
+#include <qmetaobject.h>
 #include <qcombobox.h>
 #include <qlistbox.h>
 #include <qaction.h>
