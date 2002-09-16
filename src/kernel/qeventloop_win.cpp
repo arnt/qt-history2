f895195e7a14f0dcc93154d526cd4e84f7484ff0
Volker Hilsheimer <vohi@trolltech.com>
2002-09-16 14:49:35 +0100

    Activate simple timers for the next beta. This solves numerous issues, e.g. with timers not being fired during drag'n'drop, or when the user fiddles around in the non-client area. The only reason I can see that this might not be a good idea is that optimizations for zero-timers will not kick in, but that's probably not really going to hit anybody.
    #6607 and others
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 73892]

--- src/kernel/qeventloop_win.cpp
+++ src/kernel/qeventloop_win.cpp
@@ -41,7 +41,7 @@ extern bool qt_winEventFilter( MSG* msg );
 static DWORD qt_gui_thread = 0;
 // Simpler timers are needed when Qt does not have the event loop,
 // such as for plugins.
-Q_EXPORT bool	qt_win_use_simple_timers = FALSE;
+Q_EXPORT bool	qt_win_use_simple_timers = TRUE;
 void CALLBACK   qt_simple_timer_func( HWND, UINT, UINT, DWORD );
 
 static void	initTimers();
