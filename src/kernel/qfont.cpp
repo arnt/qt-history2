32eadbea333a342328f7f3c6932ea27ebf507c62
Lars Knoll <lars@trolltech.com>
2002-09-17 13:56:32 +0100

    if we change the datastream version to 5, I can aswell add the StyleStrategy to QFont streaming. See task #3726
    [git-p4: depot-paths = "//depot/qt/main/": change = 74038]

--- src/kernel/qfont.cpp
+++ src/kernel/qfont.cpp
@@ -1543,8 +1543,10 @@ QDataStream &operator<<( QDataStream &s, const QFont &font )
 	s << (Q_INT16) font.d->request.pixelSize;
     }
 
-    return s << (Q_UINT8) font.d->request.styleHint
-	     << (Q_UINT8) 0
+    s << (Q_UINT8) font.d->request.styleHint;
+    if ( s.version() >= 5 )
+	s << (Q_UINT8 ) font.d->request.styleStrategy;
+    return s << (Q_UINT8) 0
 	     << (Q_UINT8) font.d->request.weight
 	     << get_font_bits(font.d->request);
 }
@@ -1565,7 +1567,7 @@ QDataStream &operator>>( QDataStream &s, QFont &font )
     font.d = new QFontPrivate;
 
     Q_INT16 pointSize, pixelSize = -1;
-    Q_UINT8 styleHint, charSet, weight, bits;
+    Q_UINT8 styleHint, styleStrategy = QFont::PreferDefault, charSet, weight, bits;
 
     if ( s.version() == 1 ) {
 	QCString fam;
@@ -1579,6 +1581,8 @@ QDataStream &operator>>( QDataStream &s, QFont &font )
     if ( s.version() >= 4 )
 	s >> pixelSize;
     s >> styleHint;
+    if ( s.version() >= 5 )
+	s >> styleStrategy;
     s >> charSet;
     s >> weight;
     s >> bits;
@@ -1586,6 +1590,7 @@ QDataStream &operator>>( QDataStream &s, QFont &font )
     font.d->request.pointSize = pointSize;
     font.d->request.pixelSize = pixelSize;
     font.d->request.styleHint = styleHint;
+    font.d->request.styleStrategy = styleStrategy;
     font.d->request.weight = weight;
     font.d->request.dirty = TRUE;
 
@@ -2793,7 +2798,7 @@ static const unsigned char otherScripts [112] = {
 #define SCRIPTS_13 26
     0x7f, QFont::Ethiopic, 0x9f, QFont::Unicode, 0xff, QFont::Cherokee,	// row 0x13, index 24
 #define SCRIPTS_16 32
-    0x7f, QFont::CanadianAboriginal, 0x9f, QFont::Ogham, 
+    0x7f, QFont::CanadianAboriginal, 0x9f, QFont::Ogham,
     0xff, QFont::Runic, 						// row 0x16 index 30
 #define SCRIPTS_17 38
 //     0x1f, QFont::Tagalog, 0x3f, QFont::Hanunoo, 0x5f, QFont::Buhid,
@@ -2803,11 +2808,11 @@ static const unsigned char otherScripts [112] = {
 #define SCRIPTS_18 48
     0xaf, QFont::Mongolian, 0xff, QFont::Unicode,		       	// row 0x18, index 46
 #define SCRIPTS_20 52
-    0x6f, QFont::Unicode, 0x9f, QFont::NumberForms, 
+    0x6f, QFont::Unicode, 0x9f, QFont::NumberForms,
     0xab, QFont::CurrencySymbols, 0xac, QFont::LatinExtendedA_15,
     0xcf, QFont::CurrencySymbols, 0xff, QFont::CombiningMarks,		// row 0x20, index 50
 #define SCRIPTS_21 64
-    0x4f, QFont::LetterlikeSymbols, 0x8f, QFont::NumberForms, 
+    0x4f, QFont::LetterlikeSymbols, 0x8f, QFont::NumberForms,
     0xff, QFont::MathematicalOperators,					// row 0x21, index 62
 #define SCRIPTS_24 70
     0x5f, QFont::TechnicalSymbols, 0xff, QFont::EnclosedAndSquare,	// row 0x24, index 68
@@ -2816,7 +2821,7 @@ static const unsigned char otherScripts [112] = {
 #define SCRIPTS_30 78
     0x3f, QFont::Han, 0x9f, QFont::Hiragana, 0xff, QFont::Katakana,	// row 0x30, index 76
 #define SCRIPTS_31 84
-    0x2f, QFont::Bopomofo, 0x8f, QFont::Hangul, 0x9f, QFont::Han, 
+    0x2f, QFont::Bopomofo, 0x8f, QFont::Hangul, 0x9f, QFont::Han,
     0xff, QFont::Unicode,						// row 0x31, index 82
 #define SCRIPTS_fb 92
     0x06, QFont::Latin, 0x1c, QFont::Unicode, 0x4f, QFont::Hebrew,
@@ -2845,61 +2850,61 @@ static const unsigned char indicScripts [] =
 // 0x80 + x: x is the offset into the otherScripts table
 static const unsigned char scriptTable[256] =
 {
-    QFont::LatinBasic, SCRIPTS_LATINEXT, 0x80+SCRIPTS_02, 0x80+SCRIPTS_03, 
+    QFont::LatinBasic, SCRIPTS_LATINEXT, 0x80+SCRIPTS_02, 0x80+SCRIPTS_03,
     QFont::Cyrillic, 0x80+SCRIPTS_05, QFont::Arabic, 0x80+SCRIPTS_07,
-    QFont::Unicode, SCRIPTS_INDIC, SCRIPTS_INDIC, SCRIPTS_INDIC, 
+    QFont::Unicode, SCRIPTS_INDIC, SCRIPTS_INDIC, SCRIPTS_INDIC,
     SCRIPTS_INDIC, SCRIPTS_INDIC, SCRIPTS_INDIC, QFont::Tibetan,
 
-    0x80+SCRIPTS_10, QFont::Hangul, QFont::Ethiopic, 0x80+SCRIPTS_13, 
+    0x80+SCRIPTS_10, QFont::Hangul, QFont::Ethiopic, 0x80+SCRIPTS_13,
     QFont::CanadianAboriginal, QFont::CanadianAboriginal, 0x80+SCRIPTS_16, 0x80+SCRIPTS_17,
-    0x80+SCRIPTS_18, QFont::Unicode, QFont::Unicode, QFont::Unicode, 
+    0x80+SCRIPTS_18, QFont::Unicode, QFont::Unicode, QFont::Unicode,
     QFont::Unicode, QFont::Unicode, QFont::Latin, QFont::Greek,
-    
-    0x80+SCRIPTS_20, 0x80+SCRIPTS_21, QFont::MathematicalOperators, QFont::TechnicalSymbols, 
+
+    0x80+SCRIPTS_20, 0x80+SCRIPTS_21, QFont::MathematicalOperators, QFont::TechnicalSymbols,
     0x80+SCRIPTS_24, QFont::GeometricSymbols, QFont::MiscellaneousSymbols, QFont::MiscellaneousSymbols,
-    QFont::Braille, QFont::Unicode, QFont::Unicode, QFont::Unicode, 
+    QFont::Braille, QFont::Unicode, QFont::Unicode, QFont::Unicode,
     QFont::Unicode, QFont::Unicode, 0x80+SCRIPTS_2e, QFont::Han,
-    
-    0x80+SCRIPTS_30, 0x80+SCRIPTS_31, QFont::EnclosedAndSquare, QFont::EnclosedAndSquare, 
+
+    0x80+SCRIPTS_30, 0x80+SCRIPTS_31, QFont::EnclosedAndSquare, QFont::EnclosedAndSquare,
     QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    QFont::Han, QFont::Han, QFont::Han, QFont::Han, 
     QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    
-    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    
-    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    
-    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    
+    QFont::Han, QFont::Han, QFont::Han, QFont::Han,
+
     QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
     QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
 
-    
     QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
     QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    
+
     QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
     QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
-    
+
+    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
+    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
+
+
+    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
+    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
+
+    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
+    QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han, QFont::Han,
+
     QFont::Yi, QFont::Yi, QFont::Yi, QFont::Yi, QFont::Yi, QFont::Unicode, QFont::Unicode, QFont::Unicode,
-    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, 
-    
-    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, 
-    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, 
-    
-    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, 
-    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, 
-    
-    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, 
-    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, 
-    
-    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, 
-    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, 
-    
-    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, 
+    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul,
+
+    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul,
+    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul,
+
+    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul,
+    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul,
+
+    QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul, QFont::Hangul,
+    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode,
+
+    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode,
+    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode,
+
+    QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode, QFont::Unicode,
     QFont::Unicode, QFont::Han, QFont::Han, 0x80+SCRIPTS_fb, QFont::Arabic, QFont::Arabic, 0x80+SCRIPTS_fe, 0x80+SCRIPTS_ff
 };
 
