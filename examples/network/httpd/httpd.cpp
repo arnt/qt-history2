a35835e00484d023866e24866e37f509b30fae7b
Rainer Schmidt <rms@froglogic.com>
2002-09-20 13:29:18 +0100

    p4i integration Integrate change 74512 from 3.0 to main. Description for change 74512:
    : Change 74512 by rms@rms-nibbler-qt30 on 2002/09/20 14:29:00
    : 
    : 	HTTP uses CRLF and not LFCR combinations as the protocol separtors.
    : 	See also task 7070.
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/examples/network/httpd/httpd.cpp#3 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 74513]

--- examples/network/httpd/httpd.cpp
+++ examples/network/httpd/httpd.cpp
@@ -59,13 +59,13 @@ private slots:
 	// document back.
 	QSocket* socket = (QSocket*)sender();
 	if ( socket->canReadLine() ) {
-	    QStringList tokens = QStringList::split( QRegExp("[ \n\r][ \n\r]*"), socket->readLine() );
+	    QStringList tokens = QStringList::split( QRegExp("[ \r\n][ \r\n]*"), socket->readLine() );
 	    if ( tokens[0] == "GET" ) {
 		QTextStream os( socket );
 		os.setEncoding( QTextStream::UnicodeUTF8 );
-		os << "HTTP/1.0 200 Ok\n\r"
-		    "Content-Type: text/html; charset=\"utf-8\"\n\r"
-		    "\n\r"
+		os << "HTTP/1.0 200 Ok\r\n"
+		    "Content-Type: text/html; charset=\"utf-8\"\r\n"
+		    "\r\n"
 		    "<h1>Nothing to see here</h1>\n";
 		socket->close();
 		emit wroteToClient();
