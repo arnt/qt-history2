3c9dc3924a8461b9363b2e3096d51161cf011baa
Reginald Stadlbauer <reggie@froglogic.com>
2002-09-25 07:34:21 +0100

    client doesn't need a X connection
    [git-p4: depot-paths = "//depot/qt/main/": change = 74987]

--- util/packageclient/main.cpp
+++ util/packageclient/main.cpp
@@ -3,7 +3,7 @@
 
 int main( int argc, char **argv )
 {
-    QApplication a( argc, argv );
+    QApplication a( argc, argv, FALSE );
 
     if ( a.argc() != 4 ) {
 	qFatal( "Three arguments expected" );
