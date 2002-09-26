6109b9a9b0077cd8f568a163b4dde5008be79ef5
Martin Jones <mjones@trolltech.com>
2002-09-26 09:48:10 +0100

    Make transformed driver compile.
    [git-p4: depot-paths = "//depot/qt/main/": change = 75206]

--- src/embedded/qgfxtransformed_qws.h
+++ src/embedded/qgfxtransformed_qws.h
@@ -39,19 +39,15 @@
 
 #ifndef Q_OS_QNX6
 #define QT_TRANS_SCREEN_BASE    QLinuxFbScreen
+#define QT_TRANS_CURSOR_BASE	QScreenCursor
+#define QT_TRANS_GFX_BASE	QGfxRaster
 //#define QT_TRANS_SCREEN_BASE  QVFbScreen
+//#define QT_TRANS_CURSOR_BASE   QVFbScreenCursor
+//#define QT_TRANS_GFX_BASE      QGfxVFb
 #include "qgfxlinuxfb_qws.h"
 #else
 #define QT_TRANS_SCREEN_BASE    QQnxScreen
 #include "qwsgfx_qnx.h"
-#endif
-
-#if QT_TRANS_SCREEN_BASE == QVFbScreen
-# define QT_TRANS_CURSOR_BASE   QVFbScreenCursor
-# define QT_TRANS_GFX_BASE      QGfxVFb
-#else
-# define QT_TRANS_CURSOR_BASE   QScreenCursor
-# define QT_TRANS_GFX_BASE      QGfxRaster
 #endif
 
 class QTransformedScreen : public QT_TRANS_SCREEN_BASE
