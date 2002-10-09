43f645b095aa8204cd5b64ec0e3cc80de2b0cdd3
Brad Hughes <bhughes@trolltech.com>
2002-10-09 13:36:24 +0100

    resolve task 1380.
    some changes made in 3.1 after i fixed the task cause QDesktopWidget to
    delete all widgets on other screens (something i tried to fixed in change
    68035, but apparently broke again).
    
    also, i noticed that the workarea is per screen, so i redid change 73384 to
    enable this.
    
    change 73384 also used a QWidget subclass that ignored all insertChild requests
    for widget types.  this doesn't work because insertChild is called on
           construction of QObject, at which point isWidgetType returns false.  instead
    we remove all children from the QSingleDesktopWidget when it it destructed
    (so that the QWidget destructor thinks there are no children).
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 76483]

--- src/kernel/qdesktopwidget_x11.cpp
+++ src/kernel/qdesktopwidget_x11.cpp
@@ -36,6 +36,7 @@
 **********************************************************************/
 
 #include "qdesktopwidget.h"
+#include "qobjectlist.h"
 #include "qt_x11.h"
 
 // defined in qwidget_x11.cpp
@@ -56,18 +57,21 @@ void qt_desktopwidget_update_workarea()
 class QSingleDesktopWidget : public QWidget
 {
 public:
-    QSingleDesktopWidget()
-	: QWidget( 0, "desktop", WType_Desktop )
-    {
-    }
-
-    void insertChild( QObject *child )
-    {
-	if ( child->isWidgetType() )
-	    return;
-	QWidget::insertChild( child );
-    }
+    QSingleDesktopWidget();
+    ~QSingleDesktopWidget();
 };
+
+QSingleDesktopWidget::QSingleDesktopWidget()
+    : QWidget( 0, "desktop", WType_Desktop )
+{
+}
+
+QSingleDesktopWidget::~QSingleDesktopWidget()
+{
+    while ( children() )
+        removeChild( children()->getFirst() );
+}
+
 
 class QDesktopWidgetPrivate
 {
@@ -75,19 +79,43 @@ public:
     QDesktopWidgetPrivate();
     ~QDesktopWidgetPrivate();
 
+    void init();
+
     bool use_xinerama;
     int defaultScreen;
     int screenCount;
 
     QWidget **screens;
-    QMemArray<QRect> rects;
-    QRect workarea;
+    QRect *rects;
+    QRect *workareas;
 };
 
 QDesktopWidgetPrivate::QDesktopWidgetPrivate()
-    : use_xinerama(FALSE)
+    : use_xinerama(FALSE), defaultScreen(0), screenCount(1),
+      screens( 0 ), rects( 0 ), workareas( 0 )
 {
+    init();
+}
 
+QDesktopWidgetPrivate::~QDesktopWidgetPrivate()
+{
+    if ( screens ) {
+	for ( int i = 0; i < screenCount; ++i ) {
+	    if (i == defaultScreen) continue;
+	    delete screens[i];
+	    screens[i] = 0;
+	}
+
+	delete [] screens;
+    }
+
+    if ( rects )     delete [] rects;
+    if ( workareas ) delete [] workareas;
+}
+
+void QDesktopWidgetPrivate::init()
+{
+    // get the screen count
 #ifndef QT_NO_XINERAMA
     XineramaScreenInfo *xinerama_screeninfo = 0;
     int unused;
@@ -106,10 +134,12 @@ QDesktopWidgetPrivate::QDesktopWidgetPrivate()
 	screenCount = ScreenCount(QPaintDevice::x11AppDisplay());
     }
 
+    rects     = new QRect[ screenCount ];
+    workareas = new QRect[ screenCount ];
+
     // get the geometry of each screen
-    rects.resize( screenCount );
     int i, x, y, w, h;
-    for (i = 0; i < screenCount; i++) {
+    for ( i = 0; i < screenCount; i++ ) {
 
 #ifndef QT_NO_XINERAMA
 	if (use_xinerama) {
@@ -119,7 +149,6 @@ QDesktopWidgetPrivate::QDesktopWidgetPrivate()
 	    h = xinerama_screeninfo[i].height;
 	} else
 #endif // QT_NO_XINERAMA
-
 	    {
 		x = 0;
 		y = 0;
@@ -128,30 +157,14 @@ QDesktopWidgetPrivate::QDesktopWidgetPrivate()
 	    }
 
 	rects[i].setRect(x, y, w, h);
+	workareas[i] = QRect();
     }
-
-    screens = 0;
 
 #ifndef QT_NO_XINERAMA
     if (xinerama_screeninfo)
 	XFree(xinerama_screeninfo);
 #endif // QT_NO_XINERAMA
 
-}
-
-QDesktopWidgetPrivate::~QDesktopWidgetPrivate()
-{
-    if (! screens)
-	return;
-
-    for ( int i = 0; i < screenCount; ++i ) {
-	if (i == defaultScreen) continue;
-
-	delete screens[ i ];
-	screens[i] = 0;
-    }
-
-    delete [] screens;
 }
 
 // the QDesktopWidget itself will be created on the default screen
@@ -191,9 +204,9 @@ QWidget *QDesktopWidget::screen( int screen )
 	screen = d->defaultScreen;
 
     if ( ! d->screens ) {
-	memset( (d->screens = new QWidget*[d->screenCount] ), 0,
-		d->screenCount * sizeof( QWidget*) );
-	d->screens[d->defaultScreen] = this;
+	d->screens = new QWidget*[ d->screenCount ];
+	memset( d->screens, 0, d->screenCount * sizeof( QWidget * ) );
+	d->screens[ d->defaultScreen ] = this;
     }
 
     if ( ! d->screens[screen] ||               // not created yet
@@ -209,33 +222,39 @@ QWidget *QDesktopWidget::screen( int screen )
 const QRect& QDesktopWidget::availableGeometry( int screen ) const
 {
     if ( qt_desktopwidget_workarea_dirty ) {
-	if ( ! isVirtualDesktop() && qt_net_supports( qt_net_workarea ) ) {
-	    Atom ret;
-	    int format, e;
-	    unsigned char *data = 0;
-	    unsigned long nitems, after;
-
-	    e = XGetWindowProperty( QPaintDevice::x11AppDisplay(),
-				    QPaintDevice::x11AppRootWindow( screen ),
-				    qt_net_workarea, 0, 4, False, XA_CARDINAL,
-				    &ret, &format, &nitems, &after, &data );
-
-	    if (e == Success && ret == XA_CARDINAL &&
-		format == 32 && nitems == 4) {
-		long *workarea = (long *) data;
-		d->workarea.setRect( workarea[0],
-				     workarea[1],
-				     workarea[2],
-				     workarea[3] );
-	    } else {
-		d->workarea = screenGeometry(screen);
-	    }
-	} else {
-	    d->workarea = screenGeometry(screen);
-	}
+	// the workareas are dirty, invalidate them
+	for ( int i = 0; i < d->screenCount; ++i )
+	    d->workareas[i] = QRect();
 	qt_desktopwidget_workarea_dirty = FALSE;
     }
-    return d->workarea;
+
+    if ( d->workareas[screen].isValid() )
+	return d->workareas[screen];
+
+    if ( ! isVirtualDesktop() && qt_net_supports( qt_net_workarea ) ) {
+	Atom ret;
+	int format, e;
+	unsigned char *data = 0;
+	unsigned long nitems, after;
+
+	e = XGetWindowProperty( QPaintDevice::x11AppDisplay(),
+				QPaintDevice::x11AppRootWindow( screen ),
+				qt_net_workarea, 0, 4, False, XA_CARDINAL,
+				&ret, &format, &nitems, &after, &data );
+
+	if (e == Success && ret == XA_CARDINAL &&
+	    format == 32 && nitems == 4) {
+	    long *workarea = (long *) data;
+	    d->workareas[screen].setRect( workarea[0], workarea[1],
+					  workarea[2], workarea[3] );
+	} else {
+	    d->workareas[screen] = screenGeometry(screen);
+	}
+    } else {
+	d->workareas[screen] = screenGeometry(screen);
+    }
+
+    return d->workareas[screen];
 }
 
 const QRect& QDesktopWidget::screenGeometry( int screen ) const
@@ -285,9 +304,8 @@ int QDesktopWidget::screenNumber( const QPoint &point ) const
     return -1;
 }
 
-void QDesktopWidget::resizeEvent( QResizeEvent * )
+void QDesktopWidget::resizeEvent( QResizeEvent *event )
 {
-    delete d;
-    d = new QDesktopWidgetPrivate;
     qt_desktopwidget_workarea_dirty = TRUE;
+    QWidget::resizeEvent( event );
 }
