c185998f1e17965409ba26c981df63b438875ec9
Matthias Ettrich <ettrich@trolltech.com>
2002-10-11 16:56:46 +0100

    p4i integration Integrate change 76729 from 3.0 to main. Description for change 76729:
    : Change 76729 by ettrich@matthias-jeunehomme on 2002/10/11 17:54:48
    : 
    : 	ugly QT_REQUIRE_VERSION macro to allow checking
    : 	for incompatibilities like 3.0.3 vs. 3.0.5
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/src/dialogs/qmessagebox.h#2 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 76730]

--- src/dialogs/qmessagebox.h
+++ src/dialogs/qmessagebox.h
@@ -182,6 +182,27 @@ private:	// Disabled copy constructor and operator=
 #endif
 };
 
+/*
+  Macro to be used at the beginning of main(), e.g.
+
+   #include <qapplication.h>
+   #include <qmessagebox.h>
+   int main( int argc, char**argv )
+   {
+     QT_REQUIRE_VERSION( argc, argv, "3.0.5" )
+     ...
+   }
+ */
+#define QT_REQUIRE_VERSION( argc, argv, str ) { QString s=QString::fromLatin1(str);\
+QString sq=QString::fromLatin1(qVersion()); if ( (sq.section('.',0,0).toInt()<<16)+\
+(sq.section('.',1,1).toInt()<<8)+sq.section('.',2,2).toInt()<(s.section('.',0,0).toInt()<<16)+\
+(s.section('.',1,1).toInt()<<8)+s.section('.',2,2).toInt() ){if ( !qApp){ int c=0; new \
+QApplication(argc,argv);} QMessageBox::critical( 0, QString::fromLatin1(\
+"Incompatible Qt Library Error" ), QApplication::tr("Executable '%1' requires Qt "\
+ "%2, found Qt %3").arg(QString::fromLatin1(qAppName())).arg(QString::fromLatin1(\
+str)).arg(QString::fromLatin1(qVersion()) ), QMessageBox::Abort,0 ); exit(1); }}
+
+
 #endif // QT_NO_MESSAGEBOX
 
 #endif // QMESSAGEBOX_H
