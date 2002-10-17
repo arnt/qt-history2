cbf6c684fe071ab02aef0ddea1fedb3cb47c0fa6
harald <harald@harald-morpork>
2002-10-17 15:02:36 +0100

    p4i integration Integrate change 77156 from 3.0 to main. Description for change 77156:
    : Change 77156 by harald@harald-morpork on 2002/10/17 15:59:31
    : 
    : 	Don't distribute our passwords to the outside world
    : 
    : Affected files ...
    : 
    : ... //depot/qt/3.0/examples/sql/overview/connection.h#2 edit
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 77157]

--- examples/sql/overview/connection.h
+++ examples/sql/overview/connection.h
@@ -7,16 +7,19 @@
 ** program may be used, distributed and modified without limitation.
 **
 *****************************************************************************/
+
+// Enter your connection info here
+
 #define DB_SALES_DRIVER	    "QPSQL7"
-#define DB_SALES_DBNAME	    "testdb"
-#define DB_SALES_USER	    "troll"
-#define DB_SALES_PASSWD	    "trond"
-#define DB_SALES_HOST	    "iceblink.troll.no"
+#define DB_SALES_DBNAME	    ""
+#define DB_SALES_USER	    ""
+#define DB_SALES_PASSWD	    ""
+#define DB_SALES_HOST	    ""
 
 #define DB_ORDERS_DRIVER    "QPSQL7"
-#define DB_ORDERS_DBNAME    "testdb"
-#define DB_ORDERS_USER	    "troll"
-#define DB_ORDERS_PASSWD    "trond"
-#define DB_ORDERS_HOST	    "iceblink.troll.no"
+#define DB_ORDERS_DBNAME    ""
+#define DB_ORDERS_USER	    ""
+#define DB_ORDERS_PASSWD    ""
+#define DB_ORDERS_HOST	    ""
 
 bool createConnections();
