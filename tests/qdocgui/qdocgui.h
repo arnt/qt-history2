736c77258238d0e7627b1891223046b1f6cd037c
Mark Summerfield <mark@trolltech.com>
2002-10-29 19:44:03 +0100

    Fixes I did ages ago but forgot to submit.
    [git-p4: depot-paths = "//depot/qt/main/": change = 78239]

--- tests/qdocgui/qdocgui.h
+++ tests/qdocgui/qdocgui.h
@@ -1,35 +1,41 @@
-#include <qprocess.h>
-#include <qmainwindow.h>
+#include <qdialog.h>
+#include <qlabel.h>
+#include <qlayout.h>
 #include <qlistview.h>
 #include <qmultilineedit.h>
-#include <qlabel.h>
+#include <qprocess.h>
 
 class QPushButton;
 
-class QDocMainWindow : public QMainWindow
+class QDocMainWindow : public QDialog
 {
     Q_OBJECT
 public:
     QDocMainWindow( QWidget* parent=0, const char* name=0 );
     ~QDocMainWindow();
-    void init();
+
 public slots:
     void readOutput();
     void activateEditor( QListViewItem* );
     void populateListView();
     void editorFinished();
     void finished();
+
 private:
-    QVBoxLayout* vb;
-    QListView* classList;
-    QProcess* proc;
-    QProcess* procper;
-    QProcess* procedit;
+    void setEditor();
+
+    QVBoxLayout *vb;
+    QListView *classList;
+    QProcess *proc;
+    QProcess *procper;
+    QProcess *procedit;
     QString outputText;
     QString qtdirenv;
-    QLabel* waitText;
-    QString editText;
-    QPushButton* redo;
+    QLabel *statusBar;
+    QPushButton *redo;
+    int msgCount;
+    QString editor;
+    int warnings;
 };
 
 class QDocListItem : public QListViewItem
@@ -37,7 +43,9 @@ class QDocListItem : public QListViewItem
 public:
     QDocListItem( QListViewItem* after, QString text, QString lineNumber );
     ~QDocListItem();
+
     QString key( int column, bool ascending ) const;
+
 private:
     QString line;
 };
