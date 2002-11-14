972e5405d753a580f491814372b5eb02feb1ddee
Harri Porten <porten@froglogic.com>
2002-11-14 13:06:54 +0100

    added a comment suggesting to move this operator definition out of the QValueList class definition. See task N10679. Not quite sure whether this is a compiler bug or not.
    [git-p4: depot-paths = "//depot/qt/main/": change = 79921]

--- src/tools/qvaluelist.h
+++ src/tools/qvaluelist.h
@@ -132,7 +132,7 @@ class QValueListIterator
 	node = node->prev;
 	return tmp;
     }
-    
+
     QValueListIterator<T>& operator+=( int j ) {
 	while ( j-- )
 	    node = node->next;
@@ -472,7 +472,7 @@ public:
 	return TRUE;
     }
 #  ifdef Q_CC_HPACC    // undo the HP-UX aCC hackery done above
-#    undef Q_TYPENAME 
+#    undef Q_TYPENAME
 #    define Q_TYPENAME typename
 #  endif
 #endif
@@ -486,6 +486,7 @@ public:
     uint remove( const T& x ) { detach(); return sh->remove( x ); }
     void clear();
 
+    // ### 4.0: move out of class
     QValueList<T>& operator<< ( const T& x )
     {
 	append( x );
