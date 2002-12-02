803edac4c052117963799561b2b4cc992cb6848e
Dimitri Papadopoulos <dimitri@trolltech.com>
2002-12-03 00:34:14 +0100

    p4i integration Integrate 81866 from 3.1 to main: More Cygwin oddities: sys/ipc.h: No such file or directory sys/shm.h: No such file or directory Are customers really interested in this platform?!?
    [git-p4: depot-paths = "//depot/qt/main/": change = 81868]

--- mkspecs/cygwin-g++/qplatformdefs.h
+++ mkspecs/cygwin-g++/qplatformdefs.h
@@ -42,9 +42,7 @@
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
-#include <sys/ipc.h>
 #include <sys/time.h>
-#include <sys/shm.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
