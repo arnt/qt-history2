920160324a2d0087d48dd2c3e7b4e886ceb8b54c
Sam Magnuson <sam@trolltech.com>
2002-12-19 09:06:08 +0100

    p4i integration Integrate 83961 from 3.1 to main: 	I cannot cache the available rects, so I must query it each time. Unfortunatly because of the API I must still hold it in memory because QDesktopWidget returns a QRect&, this is bad, but too late to change.
    [git-p4: depot-paths = "//depot/qt/main/": change = 83962]

--- src/kernel/qdesktopwidget_mac.cpp
+++ src/kernel/qdesktopwidget_mac.cpp
@@ -37,34 +37,32 @@ public:
     int appScreen;
     int screenCount;
 
-    QMemArray<QRect> rects;
+    QMemArray<GDHandle> devs;
     QMemArray<QRect> avail_rects;
-    QPtrList<GDPtr> devs;
+    QMemArray<QRect> rects;
 };
 
 QDesktopWidgetPrivate::QDesktopWidgetPrivate()
 {
-    int i = 0;
-    appScreen = 0;
+    screenCount = 0;
     for(GDHandle g = GetMainDevice(); g; g = GetNextDevice(g)) 
-	devs.append(g);
-    screenCount = devs.count();
-    rects.resize( screenCount);
-    avail_rects.resize( screenCount );
-    for(GDHandle g = devs.first(); g; g = devs.next(), i++) {
-	Rect r;
-	GetAvailableWindowPositioningBounds(g, &r);
-	avail_rects[i] = QRect(r.left, r.top, r.right - r.left, r.bottom - r.top);
-	r = (*g)->gdRect;
+	screenCount++;
+    devs.resize(screenCount);
+    rects.resize(screenCount);
+    avail_rects.resize(screenCount);
+    int i = 0;
+    for(GDHandle g = GetMainDevice(); i < screenCount && g; g = GetNextDevice(g), i++) {
+	devs[i] = g;
+	Rect r = (*g)->gdRect;
 	rects[i] = QRect(r.left, r.top, r.right - r.left, r.bottom - r.top);
     }
 }
 
 QDesktopWidget::QDesktopWidget()
-: QWidget( 0, "desktop", WType_Desktop )
+: QWidget(0, "desktop", WType_Desktop)
 {
     d = new QDesktopWidgetPrivate;
-    setWState( WState_Visible );
+    setWState(WState_Visible);
 }
 
 QDesktopWidget::~QDesktopWidget()
@@ -87,39 +85,39 @@ int QDesktopWidget::numScreens() const
     return d->screenCount;
 }
 
-QWidget *QDesktopWidget::screen( int )
+QWidget *QDesktopWidget::screen(int)
 {
     return this;
 }
 
-const QRect& QDesktopWidget::availableGeometry( int screen ) const
+const QRect& QDesktopWidget::availableGeometry(int screen) const
 {
-    if ( screen < 0 || screen >= d->screenCount )
+    if(screen < 0 || screen >= d->screenCount)
 	screen = d->appScreen;
-    return d->avail_rects[ screen ];
+    Rect r;
+    GetAvailableWindowPositioningBounds(d->devs[screen], &r);
+    return d->avail_rects[screen] = QRect(r.left, r.top, r.right - r.left, r.bottom - r.top);
 }
 
-const QRect& QDesktopWidget::screenGeometry( int screen ) const
+const QRect& QDesktopWidget::screenGeometry(int screen) const
 {
-    if ( screen < 0 || screen >= d->screenCount )
+    if(screen < 0 || screen >= d->screenCount)
 	screen = d->appScreen;
-    return d->rects[ screen ];
+    return d->rects[screen];
 }
 
-int QDesktopWidget::screenNumber( QWidget *widget ) const
+int QDesktopWidget::screenNumber(QWidget *widget) const
 {
-    if ( !widget )
+    if(!widget)
 	return d->appScreen;
     QRect frame = widget->frameGeometry();
-    if ( !widget->isTopLevel() )
-	frame.moveTopLeft( widget->mapToGlobal( frame.topLeft() ) );
-
-    int maxSize = -1;
-    int maxScreen = -1;
-    for ( int i = 0; i < d->screenCount; ++i ) {
-	QRect sect = d->rects[i].intersect( frame );
+    if(!widget->isTopLevel())
+	frame.moveTopLeft(widget->mapToGlobal(frame.topLeft()));
+    int maxSize = -1, maxScreen = -1;
+    for(int i = 0; i < d->screenCount; ++i) {
+	QRect sect = d->rects[i].intersect(frame);
 	int size = sect.width() * sect.height();
-	if ( size > maxSize && sect.width() > 0 && sect.height() > 0 ) {
+	if(size > maxSize && sect.width() > 0 && sect.height() > 0) {
 	    maxSize = size;
 	    maxScreen = i;
 	}
@@ -127,16 +125,16 @@ int QDesktopWidget::screenNumber( QWidget *widget ) const
     return maxScreen;
 }
 
-int QDesktopWidget::screenNumber( const QPoint &point ) const
+int QDesktopWidget::screenNumber(const QPoint &point) const
 {
-    for ( int i = 0; i < d->screenCount; ++i ) {
-	if ( d->rects[i].contains( point ) )
+    for(int i = 0; i < d->screenCount; ++i) {
+	if(d->rects[i].contains(point))
 	    return i;
     }
     return -1;
 }
 
-void QDesktopWidget::resizeEvent( QResizeEvent * )
+void QDesktopWidget::resizeEvent(QResizeEvent *)
 {
     delete d;
     d = new QDesktopWidgetPrivate;
