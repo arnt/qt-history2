890c7b17385bf2088df9de93c5a61060b22ad7ff
Dimitri Papadopoulos <dimitri@trolltech.com>
2003-01-03 00:40:12 +0100

    p4i integration Integrate 84534 from 3.1 to main: Ooops.
    [git-p4: depot-paths = "//depot/qt/main/": change = 84535]

--- src/widgets/qlistview.cpp
+++ src/widgets/qlistview.cpp
@@ -5,7 +5,7 @@
 **
 ** Created : 970809
 **
-** Copyright (C) 1992-2002 Trolltech AS.  All rights reserved.
+** Copyright (C) 1992-2003 Trolltech AS.  All rights reserved.
 **
 ** This file is part of the widgets module of the Qt GUI Toolkit.
 **
