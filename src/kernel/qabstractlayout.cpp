aa956e34ae1eb5438466be78956f4aa490c0d3d1
Andy Shaw <andy@trolltech.com>
2003-01-02 14:44:30 +0100

    p4i integration Integrate 84488 from 3.1 to main: Integrate 84487 from 3.0 to 3.1: Docu fix
    [git-p4: depot-paths = "//depot/qt/main/": change = 84489]

--- src/kernel/qabstractlayout.cpp
+++ src/kernel/qabstractlayout.cpp
@@ -1573,7 +1573,7 @@ QGLayoutIterator::~QGLayoutIterator()
 	QLayoutItem *child;
 	while ( (child = it.current()) != 0 ) {
 	    paintLayout( p, child );
-	    it.next();
+	    it++;
 	}
 	p->drawRect( lay->geometry() );
     }
