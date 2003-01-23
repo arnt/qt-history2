063cb26f2cac49bb61f8a214c0363a3f698343b6
Marius Storm-Olsen <marius@trolltech.com>
2003-01-23 11:45:35 +0100

    p4i integration Integrate 86594 from 3.1 to main: : Change 86594 by marius@marius-dingo on 2003/01/23 11:45:17 : : 3.0->3.1 : QListView items doesn't like transform
    [git-p4: depot-paths = "//depot/qt/main/": change = 86595]

--- src/styles/qwindowsxpstyle.cpp
+++ src/styles/qwindowsxpstyle.cpp
@@ -343,6 +343,8 @@ struct XPThemeData
 	    painter->setPen( tbBorderColor );
 	    if ( !flipped )
 		painter->drawLine( rec.left(), rec.bottom(), rec.right()+1, rec.bottom() );
+	} else if ( name && name == "TREEVIEW" ) {
+	    ulong res = DrawThemeBackground( handle(), painter->handle(), partId, stateId, &rect(), 0 );
 	} else {
 	    QRect rt = rec;
 	    rec = painter->xForm( rec );
