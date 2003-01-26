36af6c459af9496b7ed12234b6a7b7203a5089e4
Dimitri Papadopoulos <dimitri@trolltech.com>
2003-01-26 21:01:53 +0100

    p4i integration Integrate 86870 from 3.1 to main: Not all thread-safe -r functions are available in FreeBSD 5.0, use only those that are available - task  11547.
    The long story is that we assume _POSIX_THREAD_SAFE_FUNCTIONS
    means that all _r thread-safe ufnctions are available. This may
    be true according to Single UNIX Specification version 2:
    
    	The following symbolic constants are always defined to
    	unspecified values to indicate that this functionality
    	from the POSIX Threads Extension is always present on
    	XSI-conformant systems:
    
    	_POSIX_THREAD_SAFE_FUNCTIONS
    		The implementation supports the thread-safe
    		functions option.
    
    However this is not true anymore in Single UNIX Specification
    version 3:
    
    	If a symbolic constant is defined with the value -1, the
    	option is not supported. Headers, data types, and function
    	interfaces required only for the option need not be supplied.
    	An application that attempts to use anything associated only
    	with the option is considered to be requiring an extension.
    
    	If a symbolic constant is defined with a value greater
    	than zero, the option shall always be supported when the
    	application is executed. All headers, data types, and
    	functions shall be present and shall operate as specified.
    
    	If a symbolic constant is defined with the value zero, all
    	headers, data types, and functions shall be present. The
    	application can check at runtime to see whether the option
    	is supported by calling fpathconf(), pathconf(), or
    	sysconf() with the indicated name parameter.
    
    	_POSIX_THREAD_SAFE_FUNCTIONS
    		The implementation supports the Thread-Safe Functions
    		option. If this symbol has a value other than -1 or 0,
    		it shall have the value 200112L.
    
    Now we could of course stick to Single UNIX Specification
    version 3. I don't think it would cause compilation problems.
    In that case we wouldn't use any _r thread-safe function
    whenever the following test fails:
    	#id defined(_POSIX_THREAD_SAFE_FUNCTIONS) \
    		&& _POSIX_THREAD_SAFE_FUNCTIONS-0 > 0
    
    However some systems implement some _r thread-safe functions
    and lack some other. The value of _POSIX_THREAD_SAFE_FUNCTIONS
    is fuzzy on such systems. For example:
    * FreeBSD 5.0 only lacks getpwnam_r() therefore <unistd.h>:
      	#define _POSIX_THREAD_SAFE_FUNCTIONS    -1
    * Cygwin lacks readdir_r() although <unistd.h>:
    	#define _POSIX_THREAD_SAFE_FUNCTIONS     1
    
    In such cases I think it's better to use additional system-specific
    tests rather than relying exclusively against a pure POSIX test.
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 86871]

--- src/dialogs/qfiledialog.cpp
+++ src/dialogs/qfiledialog.cpp
@@ -3187,7 +3187,7 @@ void QFileDialog::setDir( const QString & pathstr )
 	    user = dr.mid( 1, i-1 ).local8Bit();
 	dr = dr.mid( i, dr.length() );
 	struct passwd *pw;
-#if defined(QT_THREAD_SUPPORT) && defined(_POSIX_THREAD_SAFE_FUNCTIONS)
+#if defined(QT_THREAD_SUPPORT) && defined(_POSIX_THREAD_SAFE_FUNCTIONS) && !defined(Q_OS_FREEBSD)
 	struct passwd mt_pw;
 	char buffer[2048];
 	if ( ::getpwnam_r( user, &mt_pw, buffer, 2048, &pw ) == 0 && pw == &mt_pw )
