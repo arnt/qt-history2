2b1922d7a5e8c58acfeb873318e1d2bdb3f1f215
Lars Knoll <lars@trolltech.com>
2003-02-13 13:34:19 +0100

    fix compilation.
    [git-p4: depot-paths = "//depot/qt/main/": change = 89185]

--- src/kernel/qfont.cpp
+++ src/kernel/qfont.cpp
@@ -1826,7 +1826,7 @@ QRect QFontMetrics::boundingRect( int x, int y, int w, int h, int flgs,
 
     QRect rb;
     QRect r(x, y, w, h);
-    qt_format_text( QFont( d, (bool)FALSE ), r, flgs|Qt::DontPrint, str, len, &rb,
+    qt_format_text( QFont( d, d->paintdevice ), r, flgs|Qt::DontPrint, str, len, &rb,
 		    tabstops, tabarray, tabarraylen, intern, 0 );
 
     return rb;
