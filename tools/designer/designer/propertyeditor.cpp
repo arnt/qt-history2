6e57756839815c26b2985846d6049c7e7c06aaa2
mmonsen <mmonsen@trolltech.com>
2003-02-17 18:07:52 +0100

    p4i integration Integrate 89550 from 3.1 to main: Enable richtext editing of QAction whatsThis text. Change "QDesignerMenubar" to "MenuBarEditor".
    [git-p4: depot-paths = "//depot/qt/main/": change = 89556]

--- tools/designer/designer/propertyeditor.cpp
+++ tools/designer/designer/propertyeditor.cpp
@@ -2879,7 +2879,7 @@ void PropertyList::setupProperties()
 	}
     }
     
-    if ( !w->inherits( "QSplitter" ) && !w->inherits( "QDesignerMenuBar" ) && !w->inherits( "QDesignerToolBar" ) &&
+    if ( !w->inherits( "QSplitter" ) && !w->inherits( "MenuBarEditor" ) && !w->inherits( "QDesignerToolBar" ) &&
 	 w->isWidgetType() && WidgetFactory::layoutType( (QWidget*)w ) != WidgetFactory::NoLayout ) {
 	item = new PropertyLayoutItem( this, item, 0, "layoutSpacing" );
 	setPropertyValue( item );
@@ -2897,8 +2897,8 @@ void PropertyList::setupProperties()
 	    layoutInitValue( item );
     }
 
-    if ( !w->inherits( "Spacer" ) && !w->inherits( "QLayoutWidget" ) && !w->inherits( "QAction" ) &&
-	 !w->inherits( "QDesignerMenuBar" ) && !w->inherits( "QDesignerToolBar" ) ) {
+    if ( !w->inherits( "Spacer" ) && !w->inherits( "QLayoutWidget" ) &&
+	 !w->inherits( "MenuBarEditor" ) && !w->inherits( "QDesignerToolBar" ) ) {
 	item = new PropertyTextItem( this, item, 0, "toolTip", TRUE, FALSE );
 	setPropertyValue( item );
 	if ( MetaDataBase::isPropertyChanged( editor->widget(), "toolTip" ) )
