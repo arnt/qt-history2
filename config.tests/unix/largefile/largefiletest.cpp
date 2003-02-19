de0981b45e96089b64f649279dc4ae37d18733bd
Harri Porten <porten@froglogic.com>
2003-02-19 16:11:21 +0100

    p4i integration Integrate 89884 from 3.1 to main: stat() produces nonsense values (as far as I see it) when Large File Support is enabled on SCO OpenServer. So we'll do a simple test.
    [git-p4: depot-paths = "//depot/qt/main/": change = 89888]

--- config.tests/unix/largefile/largefiletest.cpp
+++ config.tests/unix/largefile/largefiletest.cpp
@@ -6,6 +6,7 @@ platforms.
 #define _FILE_OFFSET_BITS 64
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <assert.h>
 #include <stdio.h>
 
@@ -14,6 +15,11 @@ int main( int, char **argv )
 // check that off_t can hold 2^63 - 1 and perform basic operations...
 #define OFF_T_64 (((off_t) 1 << 62) - 1 + ((off_t) 1 << 62))
     assert( OFF_T_64 % 2147483647 == 1 );
+
+    // stat breaks on SCO OpenServer
+    struct stat buf;
+    stat( argv[0], &buf );
+    assert( S_ISREG(buf.st_mode) );
 
     FILE *file = fopen( argv[0], "r" );
     off_t offset = ftello( file );
