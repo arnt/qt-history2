beecc9c6784a862f1453b50b4bfb94ec39be2253
Trond Kjernaasen <trond@trolltech.com>
2003-02-20 15:34:07 +0100

    Start with -i to only get the info dialog.
    [git-p4: depot-paths = "//depot/qt/main/": change = 90058]

--- tests/gltest/main.cpp
+++ tests/gltest/main.cpp
@@ -10,13 +10,22 @@
 
 #include <qapplication.h>
 #include "application.h"
+#include "glinfo.h"
+#include <qstring.h>
 
 int main( int argc, char ** argv ) {
     QApplication a( argc, argv );
-    ApplicationWindow * mw = new ApplicationWindow();
-    mw->setCaption( "Qt Example - Open GL (MDI)" );
-    mw->show();
-    a.connect( &a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()) );
-    int res = a.exec();
+    int res;
+    if ( argc > 1 && QString(argv[1]) == "-i" ) {
+	GLInfo info(0);
+	info.resize( 640, 480 );
+	res = info.exec();
+    } else {
+	ApplicationWindow * mw = new ApplicationWindow();
+	mw->setCaption( "Qt Example - Open GL (MDI)" );
+	mw->show();
+	a.connect( &a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()) );
+	res = a.exec();
+    }
     return res;
 }
