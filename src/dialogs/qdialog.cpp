845588ceae6d2ce7e6d598bcf4867405bf697c28
Sam Magnuson <sam@trolltech.com>
2003-03-03 03:44:13 +0100

    p4i integration Integrate 91538 from 3.1 to main: Cmd+. == Escape on Mac.
    [git-p4: depot-paths = "//depot/qt/main/": change = 91539]

--- src/dialogs/qdialog.cpp
+++ src/dialogs/qdialog.cpp
@@ -450,6 +450,14 @@ void QDialog::contextMenuEvent( QContextMenuEvent *e )
 /*! \reimp */
 void QDialog::keyPressEvent( QKeyEvent *e )
 {
+#ifdef Q_OS_MAC
+    qDebug("%d %d :: %d %d", e->state(), ControlButton, e->key(), Key_Period);
+    if(e->state() == ControlButton && e->key() == Key_Period) {
+	reject();
+	return;
+    }
+#endif
+
     //   Calls reject() if Escape is pressed. Simulates a button
     //   click for the default button if Enter is pressed. Move focus
     //   for the arrow keys. Ignore the rest.
