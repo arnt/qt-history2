a1300b77d75f70d2ea5ade97f64ad72e7eafdd8f
Marius Storm-Olsen <marius@trolltech.com>
2003-03-03 15:14:02 +0100

    p4i integration Integrate 91639 from 3.1 to main: : Change 91639 by marius@marius-dingo on 2003/03/03 15:10:18 : : Add the missing operator=( std::string ) : The declaration was there, but it was never implemented. The implementation is inlined, so it shouldn't create binary incompatability.
    [git-p4: depot-paths = "//depot/qt/main/": change = 91641]

--- src/tools/qstring.h
+++ src/tools/qstring.h
@@ -925,6 +925,8 @@ inline QString &QString::operator+=( const QByteArray &s )
 #endif
 
 #ifndef QT_NO_STL
+inline QString &operator=( const std::string& str ) 
+{ return operator=(str.c_str()); }
 inline QString &QString::operator+=( const std::string& s )
 { return operator+=(s.c_str()); }
 inline QString &QString::append( const std::string& s )
