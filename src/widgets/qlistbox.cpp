91abc3ad444aff723b87765fc861cae09dd3c1f7
Lars Knoll <lars@trolltech.com>
2003-03-03 09:15:12 +0100

    compile
    [git-p4: depot-paths = "//depot/qt/main/": change = 91566]

--- src/widgets/qlistbox.cpp
+++ src/widgets/qlistbox.cpp
@@ -2364,8 +2364,6 @@ void QListBox::updateSelection()
 			emit selectionChanged();
 #if defined(QT_ACCESSIBILITY_SUPPORT)
 		    QAccessible::updateAccessibility( viewport(), 0, QAccessible::Selection );
-#else
-		    Q_UNUSED( ind );
 #endif
 		    triggerUpdate( FALSE );
 		}
