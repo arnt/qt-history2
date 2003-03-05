4688ceeff52faf9a29117cd941584c1259a73f36
Trond Kjernaasen <trond@trolltech.com>
2003-03-05 17:28:53 +0100

    p4i integration Integrate 92006 from 3.1 to main: FIXED: QDataTable Emit currentChanged() whenever refresh() is called.
    #17633
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 92010]

--- src/sql/qdatatable.cpp
+++ src/sql/qdatatable.cpp
@@ -2089,6 +2089,8 @@ void QDataTable::refresh( QDataTable::Refresh mode )
  	setCurrentSelection( -1, -1 );
     else if ( d->lastAt != currentRow() )
 	setCurrentSelection( currentRow(), currentColumn() );
+    }
+    emit currentChanged( sqlCursor() );
 }
 
 /*!
