efe6f3fd554fd62b7eb7cde22655a9f72440b2fa
Jasmin Blanchette <jasmin@trolltech.com>
2003-03-05 14:45:25 +0100

    Accept / in PRODUCT string.
    [git-p4: depot-paths = "//depot/qt/main/": change = 91960]

--- util/qdoc/emitter.cpp
+++ util/qdoc/emitter.cpp
@@ -634,7 +634,8 @@ void DocEmitter::emitHtml() const
     rootSection.title = config->product() + " Reference Documentation";
     rootSection.ref = "index.html";
     generateXmlSections( rootSection,
-			 config->product().lower().replace(" ", "-") + ".xml",
+			 config->product().lower()
+			 .replace(QRegExp("[\\s/]"), "-") + ".xml",
 			 config->product().lower() + "/reference" );
 }
 
