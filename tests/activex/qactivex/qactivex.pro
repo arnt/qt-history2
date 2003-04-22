3f8348791db417c8a325b02705a19c4d977535d1
Dimitri Papadopoulos <dimitri@trolltech.com>
2003-04-23 00:53:37 +0100

    p4i integration Integrate 97148 from 3 to main: Change 95905 was not really an "empty integrate".
    It restored files that had been deleted on purpose
    in changes 90239 and 90383.
    
    Espen, could you review this?
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 97149]

--- tests/activex/qactivex/qactivex.pro
+++
@@ -1,6 +0,0 @@
-TEMPLATE	= lib
-CONFIG		+= qt warn_on debug
-DEFINES		= QT_DLL
-HEADERS		= qactivex.h
-SOURCES		= qactivex.cpp
-TARGET		= qactivex
--- tests/activex/testcontrol_atl3/TestControl.cpp
+++
@@ -1,9 +0,0 @@
-// TestControl.cpp : Implementation of CTestControl
-
-#include "stdafx.h"
-#include "AtlTest.h"
-#include "TestControl.h"
-
-/////////////////////////////////////////////////////////////////////////////
-// CTestControl
-
