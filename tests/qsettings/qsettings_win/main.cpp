5ae3e4a938cd8a9686fa126dc669a2f2af4bc798
Volker Hilsheimer <vohi@trolltech.com>
2003-05-08 18:38:26 +0100

    p4i integration Integrate 99221 from 3 to main: Test a bit locally.
    [git-p4: depot-paths = "//depot/qt/main/": change = 99233]

--- tests/qsettings/qsettings_win/main.cpp
+++ tests/qsettings/qsettings_win/main.cpp
@@ -1,51 +1,73 @@
 #include <qsettings.h>
 
+static void writeTest( QSettings &settings )
+{
+    settings.writeEntry( "/values/string", "String" );
+    settings.writeEntry( "/values/int", 0xffff );
+    settings.writeEntry( "/values/bool", true );
+    settings.writeEntry( "/values/double", 3.1415927 );
+    QStringList list;
+    list << "a" << "b" << "c";
+    settings.writeEntry( "/values/list", list );
+    
+    QStringList subkeys = settings.subkeyList( "/values" );
+    for ( QStringList::Iterator it = subkeys.begin(); it != subkeys.end(); ++it ) {
+	qDebug( "/values/%s", (*it).latin1() );
+	QStringList entries = settings.entryList( "/values/" + *it );
+	for ( QStringList::Iterator it2 = entries.begin(); it2 != entries.end(); ++it2 ) {
+	    qDebug( "/values/%s/%s", (*it).latin1(), (*it2).latin1() );
+	}
+    }
+}
+
+static void readTest( QSettings &settings )
+{
+    qDebug( "%s", settings.readEntry( "/values/string" ).latin1() );
+    qDebug( "%d", settings.readNumEntry( "/values/int" ) );
+    qDebug( "%d", settings.readBoolEntry( "/values/bool" ) );
+    qDebug( "%g", settings.readDoubleEntry( "/values/double" ) );
+    QStringList list = settings.readListEntry( "/values/list" );
+    qDebug( "%s", list.join(" ").latin1() );
+    bool ok;
+    QString str = settings.readEntry( "/values/foo", QString::null, &ok );
+    if ( ok )
+	qDebug( "%s", str );
+    
+    settings.removeEntry( "/values/string" );
+    settings.removeEntry( "/values/bool" );
+    settings.removeEntry( "/values/double" );
+    settings.removeEntry( "/values/int" );
+    settings.removeEntry( "/values/list" );
+    settings.removeEntry( "/." );
+}
+
 int main ( int argc, char **argv )
 {
     {
+	QSettings settings( QSettings::Ini );
+	settings.setPath( "trolltech.com", "QSettings", QSettings::User );
+	
+	writeTest( settings );
+    }
+    {
+	QSettings settings( QSettings::Ini );
+	settings.setPath( "trolltech.com", "QSettings", QSettings::User );
+	
+	readTest( settings );
+    }
+    {
 	QSettings settings;
 	settings.setPath( "trolltech.com", "QSettings" );
-
-	settings.writeEntry( "/string", "String" );
-	settings.writeEntry( "/int", 0xffff );
-	settings.writeEntry( "/bool", true );
-	settings.writeEntry( "/double", 3.1415927 );
-	QStringList list;
-	list << "a" << "b" << "c";
-	settings.writeEntry( "/list", list );
-
-	settings.resetGroup();
-
-	QStringList subkeys = settings.subkeyList( "/Trolltech" );
-	for ( QStringList::Iterator it = subkeys.begin(); it != subkeys.end(); ++it ) {
-	    qDebug( "/Trolltech/%s", (*it).latin1() );
-	    QStringList entries = settings.entryList( "/Trolltech/" + *it );
-	    for ( QStringList::Iterator it2 = entries.begin(); it2 != entries.end(); ++it2 ) {
-		qDebug( "/Trolltech/%s/%s", (*it).latin1(), (*it2).latin1() );
-	    }
-	}
+	
+	writeTest( settings );
     }
-
+    
     {
 	QSettings settings;
 	settings.insertSearchPath( QSettings::Windows, "/Trolltech/QSettings" );
-	qDebug( "%s", settings.readEntry( "/string" ).latin1() );
-	qDebug( "%d", settings.readNumEntry( "/int" ) );
-	qDebug( "%d", settings.readBoolEntry( "/bool" ) );
-	qDebug( "%g", settings.readDoubleEntry( "/double" ) );
-	QStringList list = settings.readListEntry( "/list" );
-	qDebug( "%s", list.join(" ").latin1() );
-	bool ok;
-	QString str = settings.readEntry( "/Trolltech/QSettings/foo", QString::null, &ok );
-	if ( ok )
-	    qDebug( "%s", str );
-
-	settings.removeEntry( "/Trolltech/QSettings/string" );
-	settings.removeEntry( "/Trolltech/QSettings/bool" );
-	settings.removeEntry( "/Trolltech/QSettings/double" );
-	settings.removeEntry( "/Trolltech/QSettings/int" );
-	settings.removeEntry( "/Trolltech/QSettings/list" );
+	
+	readTest( settings );
     }
-
+    
     return 0;
 }
