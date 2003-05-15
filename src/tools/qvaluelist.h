852d79049d28353add5d59d611bba8bd390a8445
Brad Hughes <bhughes@trolltech.com>
2003-05-15 08:45:33 +0100

    typename cleanups to shut up g++ 3.2
    use more proper C++ in template declarations (template <typename T>
    instead of template <class T>)
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 100183]

--- src/tools/qvaluelist.h
+++ src/tools/qvaluelist.h
@@ -50,37 +50,35 @@
 #endif
 
 #ifndef QT_NO_COMPAT
-template <class T> class QValueListIterator : public QList<T>::Iterator
+template <typename T> class QValueListIterator : public QList<T>::Iterator
 {
 public:
     inline QValueListIterator<T>() :
 	QList<T>::Iterator() {}
     inline QValueListIterator<T>(const QValueListIterator &o) :
 	QList<T>::Iterator(o) {}
-    inline QValueListIterator<T>(const QList<T>::Iterator &o) :
+    inline QValueListIterator<T>(const typename QList<T>::Iterator &o) :
 	QList<T>::Iterator(o) {}
 };
 
-template <class T> class QValueListConstIterator : public QList<T>::ConstIterator
+template <typename T> class QValueListConstIterator : public QList<T>::ConstIterator
 {
 public:
     inline QValueListConstIterator<T>() :
 	QList<T>::ConstIterator() {}
     inline QValueListConstIterator<T>(const QValueListConstIterator &o) :
 	QList<T>::ConstIterator(o) {}
-    inline QValueListConstIterator<T>(const QList<T>::ConstIterator &o) :
+    inline QValueListConstIterator<T>(const typename QList<T>::ConstIterator &o) :
 	QList<T>::ConstIterator(o) {}
-    inline QValueListConstIterator<T>(const QList<T>::Iterator &o) :
+    inline QValueListConstIterator<T>(const typename QList<T>::Iterator &o) :
 	QList<T>::ConstIterator(o) {}
 };
 #endif
 
-template <class T>
+template <typename T>
 class QValueList : public QList<T>
 {
 public:
-    typedef Iterator iterator;
-    typedef ConstIterator const_iterator;
     typedef T value_type;
     typedef value_type* pointer;
     typedef const value_type* const_pointer;
@@ -96,92 +94,110 @@ public:
      * API
      */
     QValueList() {}
-    QValueList( const QValueList<T>& l ) : QList<T>( l ) {}
-    QValueList( const QList<T>& l ) : QList<T>( l ) {}
+    QValueList(const QValueList<T>& l) : QList<T>(l) {}
+    QValueList(const QList<T>& l) : QList<T>(l) {}
 #ifndef QT_NO_STL
-    QValueList( const std::list<T>& l )
+    QValueList(const std::list<T>& l)
     {
-	qCopy( l.begin(), l.end(), std::back_inserter( *this ) );
+	qCopy(l.begin(), l.end(), std::back_inserter(*this));
     }
 #endif
     ~QValueList() {}
 
-    QValueList<T>& operator= ( const QValueList<T>& l )
+    QValueList<T>& operator= (const QValueList<T>& l)
     {
 	QList<T>::operator=(l);
 	return *this;
     }
 #ifndef QT_NO_STL
-    QValueList<T>& operator= ( const std::list<T>& l )
+    QValueList<T>& operator= (const std::list<T>& l)
     {
 	detach();
-	qCopy( l.begin(), l.end(), std::back_inserter( *this ) );
+	qCopy(l.begin(), l.end(), std::back_inserter(*this));
 	return *this;
     }
-    bool operator== ( const std::list<T>& l ) const
+    bool operator== (const std::list<T>& l) const
     {
-	if ( size() != l.size() )
+	if (size() != l.size())
 	    return FALSE;
-	const_iterator it2 = begin();
+	typename QValueList<T>::const_iterator it2 = begin();
 #if !defined(Q_CC_MIPS)
 	typename
 #endif
-	std::list<T>::const_iterator it = l.begin();
-	for ( ; it2 != end(); ++it2, ++it )
-	if ( !((*it2) == (*it)) )
+	    std::list<T>::const_iterator it = l.begin();
+	for (; it2 != end(); ++it2, ++it)
+	if (!((*it2) == (*it)))
 	    return FALSE;
 	return TRUE;
     }
 #endif
-    bool operator== ( const QValueList<T>& l ) const { return QList<T>::operator==(l); }
-    bool operator!= ( const QValueList<T>& l ) const { return QList<T>::operator!=(l); }
+    bool operator== (const QValueList<T>& l) const { return QList<T>::operator==(l); }
+    bool operator!= (const QValueList<T>& l) const { return QList<T>::operator!=(l); }
 
-    inline QValueList<T>& operator<< ( const T& x ) { append( x ); return *this; }
+    inline QValueList<T>& operator<< (const T& x) { append(x); return *this; }
 
-    void insert( Iterator pos, size_type n, const T& x );
-    Iterator insert( Iterator pos, const T& x ) { return QList<T>::insert(pos, x); }
+    void insert(typename QValueList<T>::Iterator pos,
+		 typename QValueList<T>::size_type n,
+		 const T& x);
 
-    inline QValueList<T> operator+ ( const QValueList<T>& l ) const
+    typename QValueList<T>::Iterator insert(typename QValueList<T>::Iterator pos,
+					     const T& x)
+        { return QList<T>::insert(pos, x); }
+
+    inline QValueList<T> operator+ (const QValueList<T>& l) const
 	{ return static_cast<QValueList<T> >(QList<T>::operator+(l)); }
-    inline QValueList<T>& operator+= ( const QValueList<T>& l ) { QList<T>::operator+=(l); return *this; }
+    inline QValueList<T>& operator+= (const QValueList<T>& l)
+        { QList<T>::operator+=(l); return *this; }
 
-    Iterator fromLast() { return (isEmpty() ? end() : --end()); }
-    ConstIterator fromLast() const { return (isEmpty() ? end() : --end()); }
+    typename QValueList<T>::Iterator fromLast()
+        { return (isEmpty() ? end() : --end()); }
+    typename QValueList<T>::ConstIterator fromLast() const
+        { return (isEmpty() ? end() : --end()); }
 
-    Iterator append( const T& x ) { QList<T>::append(x); return begin(); }
-    Iterator prepend( const T& x ) { QList<T>::prepend(x); return --end(); }
+    typename QValueList<T>::Iterator append(const T& x)
+        { QList<T>::append(x); return begin(); }
+    typename QValueList<T>::Iterator prepend(const T& x)
+        { QList<T>::prepend(x); return --end(); }
 
-    Iterator at( size_type i  ){ Q_ASSERT( i < size() ); detach(); return begin()+i; }
-    ConstIterator at( size_type i ) const { Q_ASSERT( i < size() ); return begin()+i; }
-    Iterator find ( const T& x ) { int i = QList<T>::find(x); return (i == -1 ? end() : (begin()+i)); }
-    ConstIterator find ( const T& x ) const { int i = QList<T>::find(x); return (i == -1 ? end() : (begin()+i)); }
-    Iterator find ( Iterator it, const T& x )
+    typename QValueList<T>::Iterator at(typename QValueList<T>::size_type i )
+        { Q_ASSERT(i < size()); detach(); return begin()+i; }
+    typename QValueList<T>::ConstIterator at(typename QValueList<T>::size_type i) const
+        { Q_ASSERT(i < size()); return begin()+i; }
+    typename QValueList<T>::Iterator find (const T& x)
+        { int i = QList<T>::find(x); return (i == -1 ? end() : (begin()+i)); }
+    typename QValueList<T>::ConstIterator find (const T& x) const
+        { int i = QList<T>::find(x); return (i == -1 ? end() : (begin()+i)); }
+    typename QValueList<T>::Iterator find (typename QValueList<T>::Iterator it,
+					   const T& x)
 	{ int i = QList<T>::find(x, it-begin()); return i == -1 ? end() : begin()+i; }
-    ConstIterator find ( ConstIterator it, const T& x ) const
+    typename QValueList<T>::ConstIterator find(typename QValueList<T>::ConstIterator it,
+					       const T& x) const
 	{ int i = QList<T>::find(x, it-begin()); return i == -1 ? end() : begin()+i; }
-    size_type contains( const T& x ) const { return QList<T>::count(x); }
+    typename QValueList<T>::size_type contains(const T& x) const
+        { return QList<T>::count(x); }
 
-    size_type count() const { return size(); }
+    typename QValueList<T>::size_type count() const { return size(); }
 
-    QValueList<T>& operator+= ( const T& x ) { append( x ); return *this; }
+    QValueList<T>& operator+= (const T& x) { append(x); return *this; }
 };
 
-template <class T>
-void QValueList<T>::insert( Q_TYPENAME QValueList<T>::Iterator pos, size_type n, const T& x )
+template <typename T>
+void QValueList<T>::insert(typename QValueList<T>::Iterator pos,
+			   typename QValueList<T>::size_type n, const T& x)
 {
-    for ( ; n > 0; --n )
-	insert( pos, x );
+    for (; n > 0; --n)
+	insert(pos, x);
 }
 
 #ifndef QT_NO_DATASTREAM
-template <class T>
-QDataStream& operator>>( QDataStream& s, QValueList<T>& l )
+template <typename T>
+QDataStream& operator>>(QDataStream& s, QValueList<T>& l)
 {
     return operator>>(s, (QList<T>&)l);
 }
 
-template <class T>
-QDataStream& operator<<( QDataStream& s, const QValueList<T>& l )
+template <typename T>
+QDataStream& operator<<(QDataStream& s, const QValueList<T>& l)
 {
     return operator<<(s, (QList<T>&)l);
 }
