687aef8f571f4469dc2ec1147a2dd9f88d023378
Lars Knoll <lars@trolltech.com>
2003-05-16 12:06:10 +0100

    some additions
    [git-p4: depot-paths = "//depot/qt/main/": change = 100440]

--- src/tools/qtl.h
+++ src/tools/qtl.h
@@ -40,75 +40,74 @@
 
 #ifndef QT_H
 #include "qglobal.h"
-#include "qtextstream.h"
 #include "qstring.h"
 #endif // QT_H
 
 #ifndef QT_NO_TEXTSTREAM
+#include "qtextstream.h"
 template <class T>
 class QTextOStreamIterator
 {
 protected:
-    QTextOStream& stream;
+    QTextOStream &stream;
     QString separator;
 
 public:
-    QTextOStreamIterator( QTextOStream& s) : stream( s ) {}
-    QTextOStreamIterator( QTextOStream& s, const QString& sep )
-	: stream( s ), separator( sep )  {}
-    QTextOStreamIterator<T>& operator= ( const T& x ) {
+    QTextOStreamIterator(QTextOStream &s) : stream(s) {}
+    QTextOStreamIterator(QTextOStream &s, const QString &sep)
+	: stream(s), separator(sep)  {}
+    QTextOStreamIterator<T> &operator= (const T &x) {
 	stream << x;
-	if ( !separator.isEmpty() )
+	if (!separator.isEmpty())
 	    stream << separator;
 	return *this;
     }
-    QTextOStreamIterator<T>& operator*() { return *this; }
-    QTextOStreamIterator<T>& operator++() { return *this; }
-    QTextOStreamIterator<T>& operator++(int) { return *this; }
+    QTextOStreamIterator<T> &operator*() { return *this; }
+    QTextOStreamIterator<T> &operator++() { return *this; }
+    QTextOStreamIterator<T> &operator++(int) { return *this; }
 };
 #endif //QT_NO_TEXTSTREAM
 
 template <class InputIterator, class OutputIterator>
-inline OutputIterator qCopy( InputIterator _begin, InputIterator _end,
-			     OutputIterator _dest )
+inline OutputIterator qCopy(InputIterator _begin, InputIterator _end,
+			     OutputIterator _dest)
 {
-    while( _begin != _end )
+    while(_begin != _end)
 	*_dest++ = *_begin++;
     return _dest;
 }
 
 template <class BiIterator, class BiOutputIterator>
-inline BiOutputIterator qCopyBackward( BiIterator _begin, BiIterator _end,
-				       BiOutputIterator _dest )
+inline BiOutputIterator qCopyBackward(BiIterator _begin, BiIterator _end,
+				       BiOutputIterator _dest)
 {
-    while ( _begin != _end )
+    while (_begin != _end)
 	*--_dest = *--_end;
     return _dest;
 }
 
 template <class InputIterator1, class InputIterator2>
-inline bool qEqual( InputIterator1 first1, InputIterator1 last1, InputIterator2 first2 )
+inline bool qEqual(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
 {
-    // ### compare using !(*first1 == *first2) in Qt 4.0
-    for ( ; first1 != last1; ++first1, ++first2 )
-	if ( *first1 != *first2 )
-	    return FALSE;
-    return TRUE;
+    for (; first1 != last1; ++first1, ++first2)
+	if (! (*first1 == *first2))
+	    return false;
+    return true;
 }
 
 template <class ForwardIterator, class T>
-inline void qFill( ForwardIterator first, ForwardIterator last, const T& val )
+inline void qFill(ForwardIterator first, ForwardIterator last, const T &val)
 {
-    for ( ; first != last; ++first )
+    for (; first != last; ++first)
 	*first = val;
 }
 
 #if 0
 template <class BiIterator, class OutputIterator>
-inline OutputIterator qReverseCopy( BiIterator _begin, BiIterator _end,
-				    OutputIterator _dest )
+inline OutputIterator qReverseCopy(BiIterator _begin, BiIterator _end,
+				    OutputIterator _dest)
 {
-    while ( _begin != _end ) {
+    while (_begin != _end) {
 	--_end;
 	*_dest = *_end;
 	++_dest;
@@ -119,25 +118,25 @@ inline OutputIterator qReverseCopy( BiIterator _begin, BiIterator _end,
 
 
 template <class InputIterator, class T>
-inline InputIterator qFind( InputIterator first, InputIterator last,
-			    const T& val )
+inline InputIterator qFind(InputIterator first, InputIterator last,
+			    const T &val)
 {
-    while ( first != last && *first != val )
+    while (first != last && *first != val)
 	++first;
     return first;
 }
 
 template <class InputIterator, class T, class Size>
-inline void qCount( InputIterator first, InputIterator last, const T& value,
-		    Size& n )
+inline void qCount(InputIterator first, InputIterator last, const T &value,
+		    Size &n)
 {
-    for ( ; first != last; ++first )
-	if ( *first == value )
+    for (; first != last; ++first)
+	if (*first == value)
 	    ++n;
 }
 
 template <class T>
-inline void qSwap( T& _value1, T& _value2 )
+inline void qSwap(T &_value1, T &_value2)
 {
     T tmp = _value1;
     _value1 = _value2;
@@ -146,18 +145,18 @@ inline void qSwap( T& _value1, T& _value2 )
 
 
 template <class InputIterator>
-Q_INLINE_TEMPLATES void qBubbleSort( InputIterator b, InputIterator e )
+ void qBubbleSort(InputIterator b, InputIterator e)
 {
     // Goto last element;
     InputIterator last = e;
     --last;
     // only one element or no elements ?
-    if ( last == b )
+    if (last == b)
 	return;
 
     // So we have at least two elements in here
-    while( b != last ) {
-	bool swapped = FALSE;
+    while(b != last) {
+	bool swapped = false;
 	InputIterator swap_pos = b;
 	InputIterator x = e;
 	InputIterator y = x;
@@ -165,13 +164,13 @@ Q_INLINE_TEMPLATES void qBubbleSort( InputIterator b, InputIterator e )
 	do {
 	    --x;
 	    --y;
-	    if ( *x < *y ) {
-		swapped = TRUE;
-		qSwap( *x, *y );
+	    if (*x < *y) {
+		swapped = true;
+		qSwap(*x, *y);
 		swap_pos = y;
 	    }
-	} while( y != b );
-	if ( !swapped )
+	} while(y != b);
+	if (!swapped)
 	    return;
 	b = swap_pos;
 	b++;
@@ -180,32 +179,32 @@ Q_INLINE_TEMPLATES void qBubbleSort( InputIterator b, InputIterator e )
 
 
 template <class Container>
-inline void qBubbleSort( Container &c )
+inline void qBubbleSort(Container &c)
 {
-  qBubbleSort( c.begin(), c.end() );
+  qBubbleSort(c.begin(), c.end());
 }
 
 
 template <class Value>
-Q_INLINE_TEMPLATES void qHeapSortPushDown( Value* heap, int first, int last )
+ void qHeapSortPushDown(Value *heap, int first, int last)
 {
     int r = first;
-    while ( r <= last / 2 ) {
-	if ( last == 2 * r ) {
+    while (r <= last / 2) {
+	if (last == 2 * r) {
 	    // node r has only one child
-	    if ( heap[2 * r] < heap[r] )
-		qSwap( heap[r], heap[2 * r] );
+	    if (heap[2 * r] < heap[r])
+		qSwap(heap[r], heap[2 * r]);
 	    r = last;
 	} else {
 	    // node r has two children
-	    if ( heap[2 * r] < heap[r] && !(heap[2 * r + 1] < heap[2 * r]) ) {
+	    if (heap[2 * r] < heap[r] && !(heap[2 * r + 1] < heap[2 * r])) {
 		// swap with left child
-		qSwap( heap[r], heap[2 * r] );
+		qSwap(heap[r], heap[2 * r]);
 		r *= 2;
-	    } else if ( heap[2 * r + 1] < heap[r]
-			&& heap[2 * r + 1] < heap[2 * r] ) {
+	    } else if (heap[2 * r + 1] < heap[r]
+			&& heap[2 * r + 1] < heap[2 * r]) {
 		// swap with right child
-		qSwap( heap[r], heap[2 * r + 1] );
+		qSwap(heap[r], heap[2 * r + 1]);
 		r = 2 * r + 1;
 	    } else {
 		r = last;
@@ -216,29 +215,29 @@ Q_INLINE_TEMPLATES void qHeapSortPushDown( Value* heap, int first, int last )
 
 
 template <class InputIterator, class Value>
-Q_INLINE_TEMPLATES void qHeapSortHelper( InputIterator b, InputIterator e, Value, uint n )
+ void qHeapSortHelper(InputIterator b, InputIterator e, Value, uint n)
 {
     // Create the heap
     InputIterator insert = b;
-    Value* realheap = new Value[n];
+    Value *realheap = new Value[n];
     // Wow, what a fake. But I want the heap to be indexed as 1...n
-    Value* heap = realheap - 1;
+    Value *heap = realheap - 1;
     int size = 0;
-    for( ; insert != e; ++insert ) {
+    for(; insert != e; ++insert) {
 	heap[++size] = *insert;
 	int i = size;
-	while( i > 1 && heap[i] < heap[i / 2] ) {
-	    qSwap( heap[i], heap[i / 2] );
+	while(i > 1 && heap[i] < heap[i / 2]) {
+	    qSwap(heap[i], heap[i / 2]);
 	    i /= 2;
 	}
     }
 
     // Now do the sorting
-    for( uint i = n; i > 0; i-- ) {
+    for(uint i = n; i > 0; i--) {
 	*b++ = heap[1];
-	if ( i > 1 ) {
+	if (i > 1) {
 	    heap[1] = heap[i];
-	    qHeapSortPushDown( heap, 1, (int)i - 1 );
+	    qHeapSortPushDown(heap, 1, (int)i - 1);
 	}
     }
 
@@ -247,76 +246,78 @@ Q_INLINE_TEMPLATES void qHeapSortHelper( InputIterator b, InputIterator e, Value
 
 
 template <class InputIterator>
-Q_INLINE_TEMPLATES void qHeapSort( InputIterator b, InputIterator e )
+ void qHeapSort(InputIterator b, InputIterator e)
 {
     // Empty ?
-    if ( b == e )
+    if (b == e)
 	return;
 
     // How many entries have to be sorted ?
     InputIterator it = b;
     uint n = 0;
-    while ( it != e ) {
+    while (it != e) {
 	++n;
 	++it;
     }
 
     // The second last parameter is a hack to retrieve the value type
     // Do the real sorting here
-    qHeapSortHelper( b, e, *b, n );
+    qHeapSortHelper(b, e, *b, n);
 }
 
 
 template <class Container>
-Q_INLINE_TEMPLATES void qHeapSort( Container &c )
+void qHeapSort(Container &c)
 {
-    if ( c.begin() == c.end() )
+    if (c.begin() == c.end())
 	return;
 
     // The second last parameter is a hack to retrieve the value type
     // Do the real sorting here
-    qHeapSortHelper( c.begin(), c.end(), *(c.begin()), (uint)c.count() );
+    qHeapSortHelper(c.begin(), c.end(), *c.begin(), (uint)c.count());
 }
+
+template <class InputIterator, class T>
+bool qBinarySearch(InputIterator b, InputIterator e, const T &value)
+{
+    int l = 0;
+    int r = e - b - 1;
+    if (r <= 0)
+	return false;
+    int i = ((l+r+1) / 2);
+    while (r - l) {
+	if (b[i] > value)
+	    r = i -1;
+	else
+	    l = i;
+	i = ((l+r+1) / 2);
+    }
+    return b[i] == value;
+}
+
+template <class Container, class T>
+bool qBinarySearch(const Container &c, const T &value)
+{
+    return qBinarySearch(c.begin(), c.end(), value);
+}
+
 
 template <class Container>
 class QBackInsertIterator
 {
 public:
-    Q_EXPLICIT QBackInsertIterator( Container &c )
-	: container( &c )
-    {
-    }
-
-    QBackInsertIterator<Container>&
-    operator=( const Q_TYPENAME Container::value_type &value )
-    {
-	container->push_back( value );
-	return *this;
-    }
-
-    QBackInsertIterator<Container>& operator*()
-    {
-	return *this;
-    }
-
-    QBackInsertIterator<Container>& operator++()
-    {
-	return *this;
-    }
-
-    QBackInsertIterator<Container>& operator++(int)
-    {
-	return *this;
-    }
-
+    inline explicit QBackInsertIterator(Container &c): container(&c) {}
+    inline QBackInsertIterator<Container>&operator=(const typename Container::value_type &value)
+    { container->push_back(value); return *this; }
+    inline QBackInsertIterator<Container> &operator*() { return *this; }
+    inline QBackInsertIterator<Container> &operator++() { return *this; }
+    inline QBackInsertIterator<Container> &operator++(int) {	return *this; }
 protected:
     Container *container;
 };
 
 template <class Container>
-inline QBackInsertIterator<Container> qBackInserter( Container &c )
-{
-    return QBackInsertIterator<Container>( c );
-}
+inline QBackInsertIterator<Container> qBackInserter(Container &c)
+{ return QBackInsertIterator<Container>(c); }
 
 #endif
