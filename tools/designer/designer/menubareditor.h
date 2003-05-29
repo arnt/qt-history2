4827d2954224777ca053df3876e7041356723a76
Dimitri Papadopoulos <dimitri@trolltech.com>
2003-05-29 15:01:52 +0100

    p4i integration Integrate 102351 from 3 to main: Integrate 102350 from 3.2 to 3: Bit field signedness must be specified. Copyright.
    [git-p4: depot-paths = "//depot/qt/main/": change = 102352]

--- tools/designer/designer/menubareditor.h
+++ tools/designer/designer/menubareditor.h
@@ -1,3 +1,29 @@
+/**********************************************************************
+** Copyright (C) 2003 Trolltech AS.  All rights reserved.
+**
+** This file is part of Qt Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** Licensees holding valid Qt Enterprise Edition or Qt Professional Edition
+** licenses may use this file in accordance with the Qt Commercial License
+** Agreement provided with the Software.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+**   information about Qt Commercial License Agreements.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
 #ifndef MENUBAREDITOR_H
 #define MENUBAREDITOR_H
 
@@ -44,9 +70,9 @@ private:
     MenuBarEditor * menuBar;
     PopupMenuEditor * popupMenu;
     QString text;
-    int visible : 1;
-    int separator : 1;
-    int removable : 1;
+    uint visible : 1;
+    uint separator : 1;
+    uint removable : 1;
 };
 
 class QLineEdit;
