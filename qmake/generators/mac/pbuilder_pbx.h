d8e203f4208efdcaaf82cdc3563a17fb22123214
Dimitri Papadopoulos <dimitri@trolltech.com>
2003-05-30 09:01:24 +0100

    p4i integration Integrate 102438 from 3 to main: Integrate 102437 from 3.2 to 3: Copyright.
    [git-p4: depot-paths = "//depot/qt/main/": change = 102439]

--- qmake/generators/mac/pbuilder_pbx.h
+++ qmake/generators/mac/pbuilder_pbx.h
@@ -1,13 +1,11 @@
 /****************************************************************************
 ** $Id$
 **
-** Definition of ________ class.
+** Definition of ProjectBuilderMakefileGenerator class.
 **
-** Created : 970521
+** Copyright (C) 1992-2003 Trolltech AS.  All rights reserved.
 **
-** Copyright (C) 1992-2002 Trolltech AS.  All rights reserved.
-**
-** This file is part of the network module of the Qt GUI Toolkit.
+** This file is part of qmake.
 **
 ** This file may be distributed under the terms of the Q Public License
 ** as defined by Trolltech AS of Norway and appearing in the file
@@ -34,6 +32,7 @@
 ** not clear to you.
 **
 **********************************************************************/
+
 #ifndef __PBUILDER_PBX_H__
 #define __PBUILDER_PBX_H__
 
