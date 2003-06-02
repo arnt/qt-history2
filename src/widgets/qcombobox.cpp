3d8b6a632b6c179370a944c937a63895e9aaa8ff
Brad Hughes <bhughes@trolltech.com>
2003-06-02 11:56:16 +0100

    p4i integration Integrate 102650 from 3 to main: Integrate 102649 from 3.2 to 3: Integrate 102648 from 3.1 to 3.2: don't use a pointer to a temporary pixmap
    [git-p4: depot-paths = "//depot/qt/main/": change = 102651]

--- src/widgets/qcombobox.cpp
+++ src/widgets/qcombobox.cpp
@@ -1224,19 +1224,16 @@ void QComboBox::paintEvent( QPaintEvent * )
 	QString str = d->popup()->text( this->d->current );
 	if ( !str.isNull() ) {
 	    p.drawText( clip, AlignCenter | SingleLine, str );
-	} 
-	QPixmap *pix = d->popup()->pixmap( this->d->current );
-	if ( !pix ) {                                              // try for icon instead 
-	    QIconSet * iconSet = d->popup()->iconSet( this->d->current );
-	    if ( iconSet )
-		pix = &(iconSet->pixmap());
 	}
-	if ( pix ) {
+
+	QPixmap *pix = d->popup()->pixmap( this->d->current );
+	QIconSet *iconSet = d->popup()->iconSet( this->d->current );
+	if (pix || iconSet) {
+	    QPixmap pm = ( pix ? *pix : iconSet->pixmap() );
 	    p.setClipRect( clip );
-	    p.drawPixmap( 4, (height()-pix->height())/2, *pix );
+	    p.drawPixmap( 4, (height()-pm.height())/2, pm );
 	    p.setClipping( FALSE );
 	}
-   
 
 	if ( hasFocus() )
 	    p.drawRect( xPos - 5, 4, width() - xPos + 1 , height() - 8 );
