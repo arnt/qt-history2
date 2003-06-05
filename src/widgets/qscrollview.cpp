f91450d0ebca97180d06e7bbeee459130053a1b5
Anders Bakken <anders@honshu>
2003-06-05 10:37:48 +0100

    p4i integration Integrate 103153 from 3 to main: Integrate 103152 from 3.2 to 3: Account for alwayson scrollbars in QScrollView::sizeHint(). Approved by Paul. Task: 23624.
    [git-p4: depot-paths = "//depot/qt/main/": change = 103154]

--- src/widgets/qscrollview.cpp
+++ src/widgets/qscrollview.cpp
@@ -2665,6 +2665,10 @@ QSize QScrollView::sizeHint() const
     } else {
 	sz += QSize( d->contentsWidth(), contentsHeight() );
     }
+    if (d->hMode == AlwaysOn)
+	sz.setWidth(sz.width() + d->hbar->sizeHint().width());
+    if (d->vMode == AlwaysOn)
+	sz.setHeight(sz.height() + d->hbar->sizeHint().height());
     return sz.expandedTo( QSize(12 * h, 8 * h) )
 	     .boundedTo( QSize(36 * h, 24 * h) );
 }
