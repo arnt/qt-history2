45d807a082be42adff2f6fdff193ede5d882179a
Sam Magnuson <sam@trolltech.com>
2003-06-06 01:16:58 +0100

    p4i integration Integrate 103123 from 3 to main: parent implemented..
    [git-p4: depot-paths = "//depot/qt/main/": change = 103278]

--- src/kernel/qaccessible_mac.cpp
+++ src/kernel/qaccessible_mac.cpp
@@ -159,8 +159,19 @@ void QAccessible::updateAccessibility(QObject *object, int control, Event reason
 	//need to look up the proper object.. FIXME
     }
     CFStringRef notification = 0;
-    //turn reason into notification! FIXME
-
+    if(object->isWidgetType()) {
+	if(reason == ObjectCreated)
+	    notification = kAXWindowCreatedNotification;
+    } else if(reason == ValueChanged) {
+	notification = kAXValueChangedNotification;
+    } else if(reason == MenuStart) {
+	notification = kAXMenuOpenedNotification;
+    } else if(reason == MenuEnd) {
+	notification = kAXMenuClosedNotification;
+    } else if(reason == LocationChanged) {
+	notification = kAXWindowMovedNotification;
+    }
+    
     if(notification) {
 	AXUIElementRef access = qt_mac_find_uielement(object); 
 	UInt64 id;
@@ -190,30 +201,22 @@ QAccessible::globalEventProcessor(EventHandlerCallRef next_ref, EventRef event, 
 		GetEventParameter(event, kEventParamMouseLocation, typeQDPoint, NULL,
 				  sizeof(where), NULL, &where);
 		if(object) {
-#if 0
 		    QAccessibleInterface *iface;
-		    if(queryAccessibleInterface(widget, &iface) == QS_OK) {
-			QPoint p = widget->mapFromGlobal(QPoint(where.h, where.v));
+		    if(queryAccessibleInterface(object, &iface) == QS_OK) {
+			QPoint p(where.h, where.v);
+			if(object->isWidgetType())
+			    ((QWidget*)object)->mapFromGlobal(p);
+			object = NULL;
 			int child = iface->controlAt(p.x(), p.y());
 			if(child > 0) {
 			    QAccessibleInterface *child_iface;
 			    if(iface->queryChild(child, &child_iface) == QS_OK) {
-				QObject *o = queryAccessibleObject(child_iface);
-				if(o && o->isWidgetType())
-				    widget = (QWidget*)o;
+				object = queryAccessibleObject(child_iface);
 				child_iface->release();
 			    }
 			}
 			iface->release();
 		    }
-#else
-		    if(object->isWidgetType()) {
-			QWidget *widget = (QWidget*)object;
-			object = widget->childAt(widget->mapFromGlobal(QPoint(where.h, where.v)));
-		    } else {
-			qDebug("cannot handle this..");
-		    }
-#endif
 		} else {
 		    object = QApplication::widgetAt(where.h, where.v);
 		}
@@ -251,93 +254,46 @@ QAccessible::globalEventProcessor(EventHandlerCallRef next_ref, EventRef event, 
 		    CFArrayAppendValue(attrs, kAXGrowAreaAttribute);
 		}
 	    } else if(ekind == kEventAccessibleGetNamedAttribute) {
-		CFStringRef str;
-		GetEventParameter(event, kEventParamAccessibleAttributeName, typeCFStringRef, NULL,
-				  sizeof(str), NULL, &str);
-		if(CFStringCompare(str, kAXChildrenAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXParentAttribute, 0) == kCFCompareEqualTo) {
-		    
-		} else if(CFStringCompare(str, kAXPositionAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSizeAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXRoleDescriptionAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXValueAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXHelpAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXRoleAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXEnabledAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXExpandedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSelectedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSelectedChildrenAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMainAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXCloseButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXZoomButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXToolbarButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXGrowAreaAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizedAttribute, 0) == kCFCompareEqualTo) {
-		} else {
-		    qDebug("Unknown [kEventAccessibleGetNamedAttribute]: %s", cfstring2qstring(str).latin1());
+		QAccessibleInterface *iface;
+		if(queryAccessibleInterface(object, &iface) == QS_OK) {
+		    CFStringRef str;
+		    GetEventParameter(event, kEventParamAccessibleAttributeName, typeCFStringRef, NULL,
+				      sizeof(str), NULL, &str);
+		    if(CFStringCompare(str, kAXChildrenAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXParentAttribute, 0) == kCFCompareEqualTo) {
+			QAccessibleInterface *parent_iface;
+			if(iface->queryParent(&parent_iface) == QS_OK) {
+			    QObject *parent = queryAccessibleObject(parent_iface);
+			    AXUIElementRef element = qt_mac_find_uielement(parent);
+			    SetEventParameter(event, kEventParamAccessibleAttributeValue, typeCFTypeRef, sizeof(element), &element);
+			}
+		    } else if(CFStringCompare(str, kAXPositionAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXSizeAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXRoleDescriptionAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXValueAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXHelpAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXRoleAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXEnabledAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXExpandedAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXSelectedAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXSelectedChildrenAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXMainAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXCloseButtonAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXZoomButtonAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXToolbarButtonAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXGrowAreaAttribute, 0) == kCFCompareEqualTo) {
+		    } else if(CFStringCompare(str, kAXMinimizedAttribute, 0) == kCFCompareEqualTo) {
+		    } else {
+			qDebug("Unknown [kEventAccessibleGetNamedAttribute]: %s", cfstring2qstring(str).latin1());
+		    }
+		    iface->release();
 		}
 	    } else if(ekind == kEventAccessibleSetNamedAttribute) {
-		CFStringRef str;
-		GetEventParameter(event, kEventParamAccessibleAttributeName, typeCFStringRef, NULL,
-				  sizeof(str), NULL, &str);
-		if(CFStringCompare(str, kAXChildrenAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXParentAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXPositionAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSizeAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXRoleDescriptionAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXValueAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXHelpAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXRoleAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXEnabledAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXExpandedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSelectedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSelectedChildrenAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMainAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXCloseButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXZoomButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXToolbarButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXGrowAreaAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizedAttribute, 0) == kCFCompareEqualTo) {
-		} else {
-		    qDebug("Unknown [kEventAccessibleSetNamedAttribute]: %s", cfstring2qstring(str).latin1());
-		}
 	    } else if(ekind == kEventAccessibleIsNamedAttributeSettable) {
-		CFStringRef str;
-		GetEventParameter(event, kEventParamAccessibleAttributeName, typeCFStringRef, NULL,
-				  sizeof(str), NULL, &str);
-		if(CFStringCompare(str, kAXChildrenAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXParentAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXPositionAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSizeAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXRoleDescriptionAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXValueAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXHelpAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXRoleAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXEnabledAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXExpandedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSelectedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXSelectedChildrenAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMainAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXFocusedAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXCloseButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXZoomButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizeButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXToolbarButtonAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXGrowAreaAttribute, 0) == kCFCompareEqualTo) {
-		} else if(CFStringCompare(str, kAXMinimizedAttribute, 0) == kCFCompareEqualTo) {
-		} else {
-		    qDebug("Unknown [kEventAccessibleIsNamedAttributeSettable]: %s", cfstring2qstring(str).latin1());
-		}
 	    } else if(ekind == kEventAccessibleGetAllActionNames) {
 	    } else if(ekind == kEventAccessiblePerformNamedAction) {
 	    } else if(ekind == kEventAccessibleGetNamedActionDescription) {
