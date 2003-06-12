db281cf41f30336ff766ce482e9752be189299d2
Sam Magnuson <sam@trolltech.com>
2003-06-12 06:27:36 +0100

    p4i integration Integrate 103880 from 3 to main: Integrate 103878 from 3.2 to 3: Integrate 103876 from 3.1 to 3.2: further problems found in free package testing
    [git-p4: depot-paths = "//depot/qt/main/": change = 103883]

--- src/kernel/qeventloop_mac.cpp
+++ src/kernel/qeventloop_mac.cpp
@@ -1,11 +1,10 @@
 /****************************************************************************
 **
-** Implementation of Mac startup routines and event handling.
+** Implementation of timers, socket notifiers, and event handling
 **
 ** Copyright (C) 1992-2003 Trolltech AS. All rights reserved.
 **
-** This file is part of the kernel module of the Qt GUI Toolkit.
-** EDITIONS: PROFESSIONAL, ENTERPRISE
+** EDITIONS: FREE, PROFESSIONAL, ENTERPRISE
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
