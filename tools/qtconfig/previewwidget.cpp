a8713d2aa8f527349ca432891181826a604bd581
Sam Magnuson <sam@trolltech.com>
2003-06-13 05:06:14 +0100

    qtconfig gets packaged into commercial packages, so the license will be there
    [git-p4: depot-paths = "//depot/qt/main/": change = 104060]

--- tools/qtconfig/previewwidget.cpp
+++ tools/qtconfig/previewwidget.cpp
@@ -3,7 +3,7 @@
 ** Copyright (C) 1992-2003 Trolltech AS. All rights reserved.
 **
 ** This file is part of Qt Configuration.
-** EDITIONS: FREE
+** EDITIONS: FREE, PROFESSIONAL, ENTERPRISE
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
