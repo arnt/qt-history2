a68773be49b73a89470aaa5d682e0bc742583ad6
Brad Hughes <bhughes@trolltech.com>
2003-08-04 16:24:33 +0100

    q_cas_* implementations for 32-bit PowerPC
    [git-p4: depot-paths = "//depot/qt/main/": change = 109224]

---
+++ arch/powerpc/arch.pri
@@ -0,0 +1,11 @@
+#
+# PowerPC arch files
+#
+
+ARCH_CPP=$$QT_SOURCE_TREE/arch/powerpc
+ARCH_H=$$ARCH_CPP/arch
+
+DEPENDPATH += $$ARCH_CPP;$$ARCH_H
+
+HEADERS += $$ARCH_H/qatomic.h
+
---
+++ arch/powerpc/arch/qatomic.h
@@ -0,0 +1,71 @@
+/****************************************************************************
+**
+** Definition of q_cas_* functions.
+**
+** Copyright (C) 1992-2003 Trolltech AS. All rights reserved.
+**
+** This file is part of the tools module of the Qt GUI Toolkit.
+** EDITIONS: FREE, PROFESSIONAL, ENTERPRISE
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QATOMIC_P_H
+#define QATOMIC_P_H
+
+#ifndef QT_H
+#  include <qglobal.h>
+#endif // QT_H
+
+#if defined(Q_CC_GNU)
+
+inline int q_cas_32(int *volatile ptr, int expected, int newval)
+{
+    int ret;
+    asm ("1:\tlwarx %0, 0, %2\n\t"
+         "cmplw %3, %0\n\t"
+         "bne 2f\n\t"
+         "stwcx. %4, 0, %2\n\t"
+         "bne- 1b\n\t"
+         "b 3f\n\t"
+         "2:\n\t"
+         "stwcx. %0, 0, %2\n\t"
+         "3:\n\t"
+         : "=&r" (ret), "+m" (*ptr)
+         : "r" (ptr), "r" (expected), "r" (newval)
+         : "cc", "memory");
+    return ret;
+}
+
+inline void *q_cas_ptr(void *volatile *ptr, void *expected, void *newval)
+{
+    Q_ASSERT_X(sizeof(newval) == 4, "QAtomic", "64-bit PowerPC not yet supported");
+    void *ret;
+    asm ("1:\tlwarx %0, 0, %2\n\t"
+         "cmplw %3, %0\n\t"
+         "bne 2f\n\t"
+         "stwcx. %4, 0, %2\n\t"
+         "bne- 1b\n\t"
+         "b 3f\n\t"
+         "2:\n\t"
+         "stwcx. %0, 0, %2\n\t"          
+         "3:\n\t"
+         : "=&r" (ret), "+m" (*ptr)
+         : "r" (ptr), "r" (expected), "r" (newval)
+         : "cc", "memory");
+    return ret;  
+}
+
+#else
+
+// compiler doesn't support inline assembly
+extern "C" {
+    int q_cas_32(int * volatile ptr, int expected, int newval);
+    void *q_cas_ptr(void * volatile *ptr, void *expected, void *newval);
+}
+
+#endif
+
+#endif // QATOMIC_P_H
