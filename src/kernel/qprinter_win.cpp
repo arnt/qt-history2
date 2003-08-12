bbd70640265f7fb2d5730245960d75281e29f60d
gunnar <gunnar@THAUMASIA>
2003-08-12 17:25:47 +0100

    p4i integration Integrate 109921 from 3 to main: Integrate 109920 from 3.2 to 3: Regression: QPrinter crashes on windows 95, 98 and ME.
    Reason: We use the function DeviceCapabilities to query the
    printer for its bins. The bins are represented as an array of WORD's
    We first call the function to determine the size of the array. Docs first state
    that function return number of bytes required. Later that the number of
    entries in the WORD array is returned. Wine docs confirm the latter. Since 
    WORD is 16 bit and the array is only half the size when allocating bytes.
    
    Fix: Allocate based on number of entries, not number of bytes.
    
    Reviewed by Jazek
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 109922]

--- src/kernel/qprinter_win.cpp
+++ src/kernel/qprinter_win.cpp
@@ -988,7 +988,7 @@ void QPrinter::writeDevmode( HANDLE hdm )
 	   See task: 19052 and 23626 */
 	DWORD caps = DeviceCapabilities( (TCHAR*)printer_name.ucs2(), 0, DC_BINS, 0, 0 );
 	if( caps == DWORD( -1 ) ) caps = 0;
-	LPTSTR bins = new TCHAR[caps];
+	LPTSTR bins = new WORD[caps];
 	if( !DeviceCapabilities( (TCHAR*)printer_name.ucs2(), 0, DC_BINS, bins, 0 ) ) {
 	    WRITE_DM_VAR( dm->dmDefaultSource, DMBIN_AUTO )
 	} else {
@@ -1031,7 +1031,7 @@ void QPrinter::writeDevmodeA( HANDLE hdm )
 
 	DWORD caps = DeviceCapabilitiesA( printer_name.latin1(), 0, DC_BINS, 0, 0 );
 	if( caps == DWORD( -1 ) ) caps = 0;
-	LPSTR bins = new char[caps];
+	LPSTR bins = (LPSTR)(new WORD[caps]);
 	if( !DeviceCapabilitiesA( printer_name.latin1(), 0, DC_BINS, bins, 0 ) ) {
 	    WRITE_DM_VAR( dm->dmDefaultSource, DMBIN_AUTO )
 	} else {
