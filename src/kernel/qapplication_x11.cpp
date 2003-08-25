154922e7767b0e4cd054559368a053083d8099a8
espenr <espenr@deimos>
2003-08-25 16:06:08 +0100

    Make Delete work again (in QLineEdit for example). We want to use the KeyTbl to convert Delete not the unicode text we get back. Spotted by an autotest, fixed with Paul.
    [git-p4: depot-paths = "//depot/qt/main/": change = 110902]

--- src/kernel/qapplication_x11.cpp
+++ src/kernel/qapplication_x11.cpp
@@ -4736,7 +4736,7 @@ bool QETWidget::translateKeyEventInternal( const XEvent *event, int& count, QStr
     //
     if ( key < 128 || (key < 256 && (!input_mapper || input_mapper->mibEnum()==4)) ) {
 	code = isprint((int)key) ? toupper((int)key) : 0; // upper-case key, if known
-    } else if (text.length() == 1 && text.unicode()->unicode() > 0x1f) {
+    } else if (text.length() == 1 && text.unicode()->unicode() > 0x1f && text.unicode()->unicode() != 0x7f) {
 	code = text.unicode()->upper().unicode();
     } else if ( key >= XK_F1 && key <= XK_F35 ) {
 	code = Key_F1 + ((int)key - XK_F1);	// function keys
