f58121c9f890326d21701ab6662288e70414245c
gunnar <gunnar@THAUMASIA>
2003-09-17 15:47:53 +0100

    Removed debug painting...
    [git-p4: depot-paths = "//depot/qt/main/": change = 113284]

--- src/styles/qwindowsstyle.cpp
+++ src/styles/qwindowsstyle.cpp
@@ -424,7 +424,6 @@ void QWindowsStyle::drawPrimitive( PrimitiveElement pe,
 		      QBrush(pal.light(), Dense4Pattern));
 		p->setBrush(br);
 	    }
-	    p->setBrush(Qt::red);
 	    p->drawRect(r);
 	    p->setBackgroundColor(c);
 	    break;
