6c8bf60a7171336518f5c346cff1c3c2ec81ea27
Anders Bakken <anders@honshu>
2003-10-03 12:26:33 +0100

    p4i integration Integrate 114979 from 3 to main: Integrate 114978 from 3.2 to 3: more generic
    [git-p4: depot-paths = "//depot/qt/main/": change = 114980]

--- tools/designer/integration/designer.el
+++ tools/designer/integration/designer.el
@@ -1,9 +1,9 @@
-(defvar designer-exec "/home/joe/qt/bin/designer")
+(defvar designer-exec (concat (getenv "QTDIR") "/bin/designer"))
 
 (defun designer-open-ui-file ()
   (interactive)
   (defvar procname "designer")
-  (start-process procname nil designer-exec "-style" "windows" "-client" buffer-file-name)
+  (start-process procname nil designer-exec "-client" buffer-file-name)
 )
 
 (defun designer-create-ui-file (&optional arg)
@@ -14,6 +14,6 @@
   (designer-open-ui-file)
 )
 
-(global-set-key [f7] 'designer-open-ui-file)
-(global-set-key [f8] 'designer-create-ui-file)
+;(global-set-key [f7] 'designer-open-ui-file)
+;(global-set-key [f8] 'designer-create-ui-file)
 
