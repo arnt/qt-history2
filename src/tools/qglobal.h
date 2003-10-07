267f561d52b29727a11bdb3dcb2e5c342cf2de2d
Matthias Ettrich <ettrich@trolltech.com>
2003-10-07 15:26:16 +0100

    When using operator & with explicit unsigned values like 0xffff0000, gcc-3.3 tells us that the iso standard claims this was ambigious. Resolve this by adding uint overloads.
    [git-p4: depot-paths = "//depot/qt/main/": change = 115229]

--- src/tools/qglobal.h
+++ src/tools/qglobal.h
@@ -1348,6 +1348,7 @@ public:
 
     inline QFlags &operator=(const QFlags &f) { i = f.i; return *this; }
     inline QFlags &operator&=(int mask) {  i &= mask; return *this; }
+    inline QFlags &operator&=(uint mask) {  i &= mask; return *this; }
     inline QFlags &operator|=(QFlags f) {  i |= f.i; return *this; }
     inline QFlags &operator|=(Enum f) {  i |= f; return *this; }
 
@@ -1357,6 +1358,7 @@ public:
     inline QFlags operator|(Enum f) const { QFlags g; g.i = i | f; return g; }
     inline int operator|(int f) const { return i | f; }
     inline QFlags operator&(int mask) const { QFlags g; g.i = i & mask; return g; }
+    inline QFlags operator&(uint mask) const { QFlags g; g.i = i & mask; return g; }
     inline QFlags operator&(Enum f) const { QFlags g; g.i = i & f; return g; }
     inline QFlags operator~() const { QFlags g; g.i = ~i; return g; }
 
