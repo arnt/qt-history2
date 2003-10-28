b0cf0384abcfa1481edfff741ddd1988f0ef87a1
Trenton Schulz <twschulz@twschulz-rain>
2003-10-28 12:11:08 +0100

    p4i integration Integrate 117906 from 3 to main: Make the ipv6 test work.
    [git-p4: depot-paths = "//depot/qt/main/": change = 117907]

--- config.tests/unix/ipv6/ipv6test.cpp
+++ config.tests/unix/ipv6/ipv6test.cpp
@@ -1,11 +1,13 @@
 /* Sample program for configure to test IPv6 support on target
 platforms. We check for the required IPv6 data structures. */
 
+#include <sys/types.h>
 #include <netinet/in.h>
 
 int main()
 {
     sockaddr_in6 tmp;
+    (void)tmp;
 
     return 0;
 }
