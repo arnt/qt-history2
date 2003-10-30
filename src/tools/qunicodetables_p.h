ba2cecdfeb3ee7eb4d6e9ae753dfcf2caf41b3f0
Brad Hughes <bhughes@trolltech.com>
2003-10-30 14:20:51 +0100

    p4i integration Integrate 118448 from 3 to main: this file declares private API, document it
    [git-p4: depot-paths = "//depot/qt/main/": change = 118449]

--- src/tools/qunicodetables_p.h
+++ src/tools/qunicodetables_p.h
@@ -13,6 +13,18 @@
 #ifndef QUNICODETABLES_P_H
 #define QUNICODETABLES_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of internal files.  This header file may change from version to version
+// without notice, or even be removed.
+//
+// We mean it.
+//
+//
+
 #ifndef QT_H
 #include "qstring.h"
 #endif // QT_H
