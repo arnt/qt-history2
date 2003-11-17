686e81321ac60874fd176a4773232841f2193e7f
Volker Hilsheimer <vohi@trolltech.com>
2003-11-17 13:36:48 +0100

    p4i integration Integrate 120745 from 3 to main: Update some comments.
    [git-p4: depot-paths = "//depot/qt/main/": change = 120746]

--- extensions/activeqt/plugin/plugin.cpp
+++ extensions/activeqt/plugin/plugin.cpp
@@ -3,7 +3,7 @@
 **
 ** Implementation of the Qt Designer integration plugin
 **
-** Copyright (C) 2001-2002 Trolltech AS.  All rights reserved.
+** Copyright (C) 2001-2003 Trolltech AS.  All rights reserved.
 **
 ** This file is part of the Active Qt integration.
 **
