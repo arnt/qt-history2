d0a0563389a35e648400ee9bc1a99d888796e5ef
harald <harald@harald-morpork>
2003-12-01 17:33:51 +0100

    integrate sqlite 3rdparty into main
    [git-p4: depot-paths = "//depot/qt/main/": change = 122449]

---
+++ src/3rdparty/sqlite/config.h
@@ -0,0 +1,15 @@
+#include <qglobal.h>
+#include <qconfig.h>
+
+#ifndef QT_POINTER_SIZE
+# ifdef Q_OS_WIN32
+#  define QT_POINTER_SIZE 4
+# elif Q_OS_WIN64
+#  define QT_POINTER_SIZE 8
+# else
+#  error This platform is unsupported
+# endif
+#endif /* QT_POINTER_SIZE */
+
+#define SQLITE_PTR_SZ QT_POINTER_SIZE
+
