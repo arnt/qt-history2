c60304af0b998ebbc5e9950c9a91201984b4f652
Simon Hausmann <shausman@trolltech.com>
2003-12-09 17:38:20 +0100

    First attempt to make it work. More to come.
    [git-p4: depot-paths = "//depot/qt/main/": change = 123517]

--- src/tools/qsharedpointer.h
+++ src/tools/qsharedpointer.h
@@ -6,60 +6,91 @@
 #include <qglobal.h>
 #endif
 
-template class<T>class QSharedPointer;
+template <class T> class QSharedPointer;
 
 class QSharedObject
 {
-    friend template class<T>class QSharedPtr;
+public:
     QAtomic ref;
 
-public:
-    QSharedObject() : ref(0) {}
-    QSharedObject(const QSharedObject &o) : ref(0) {}
+    QSharedObject() { ref = 0; }
+    QSharedObject(const QSharedObject &o) { ref = 0; }
 private:
     // using the assignment operator would lead to corruption in the refcounting.
     QSharedObject &operator=(const QSharedObject &);
 };
 
-template class QSharedPointer
+template <class T> class QSharedPointer
 {
 protected:
-    T *d;
+    QSharedObject *d;
 public:
-    T * operator->() { if (d && d->ref != 1) detach(); return d; }
-    const T * operator->() const { return d; }
+    T * operator->() { if (d && d->ref != 1) detach(); return static_cast<T *>(d); }
+    const T * operator->() const { return static_cast<T *>(d); }
+    operator T *() { if (d && d->ref != 1) detach(); return static_cast<T *>(d); }
+    operator const T *() const { return static_cast<T *>(d); }
+
     QSharedPointer() { d = 0; }
     ~QSharedPointer() { if (d && --d->ref) delete d; }
 
-    Q_EXPLICIT QSharedPointer(Data *data) { d = data; ++d->ref; }
+    Q_EXPLICIT QSharedPointer(T *data) { d = data; ++d->ref; }
     QSharedPointer(const QSharedPointer &o) { d = o.d; ++d->ref; }
     QSharedPointer & operator=(const QSharedPointer &o) {
 	if (o.d != d) {
-	    T *x = o.d;
+	    QSharedObject *x = o.d;
 	    if (x) ++x->ref;
-	    x = qAtomicSetPointer(&d, x);
+	    x = qAtomicSetPtr(&d, x);
 	    if (x && !--x->ref)
 		delete x;
 	}
 	return *this;
     }
-    void detach() {
-	if (!d) return;
-	T *x = new T(*d);
-	x = qAtomicSetPointer(&d, x);
-	if (!--x->ref)
-	    delete x;
+    QSharedPointer &operator=(T *o) {
+	if (o != d) {
+	    QSharedObject *x = o;
+	    if (x) ++x->ref;
+	    x = qAtomicSetPtr(&d, x);
+	    if (x && !--x->ref)
+		delete x;
+	}
+	return *this;
+
     }
+    void detach();
 };
 
+template <class T>
+Q_OUTOFLINE_TEMPLATE void QSharedPointer<T>::detach()
+{
+    if (!d) return;
+    T *x = new T(*static_cast<T *>(d));
+    x = qAtomicSetPointer(&d, x);
+    if (!--x->ref)
+	delete x;
+}
+ 
 template<class T> class QExplicitSharedPointer : public QSharedPointer<T>
 {
-    T * operator->() { return d; }
+public:
+    T * operator->() { return static_cast<T *>(d); }
+    const T * operator->() const { return static_cast<T *>(d); }
+    operator T *() { return static_cast<T *>(d); }
+    operator const T *() const { return static_cast<T *>(d); }
     QExplicitSharedPointer() {}
 
-    Q_EXPLICIT QSharedPointer(Data *data) : QSharedPointer(data) {}
-    QSharedPointer(const QSharedPointer &o) : QSharedPointer(o) {}
-}
+    Q_EXPLICIT QExplicitSharedPointer(T *data) : QSharedPointer<T>(data) {}
+    QExplicitSharedPointer(const QSharedPointer<T> &o) : QSharedPointer<T>(o) {}
+
+    QExplicitSharedPointer & operator=(const QSharedPointer<T> &o) {
+	QSharedPointer<T>::operator=(o);
+	return *this;
+    }
+    QExplicitSharedPointer &operator=(T *o) {
+	QSharedPointer<T>::operator=(o);
+	return *this;
+    }
+
+};
 
 
 #endif
