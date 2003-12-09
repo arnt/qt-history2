5d84514d763231efc3ae7708d09090512da26a57
harald <harald@harald-morpork>
2003-12-09 14:35:18 +0100

    no_compat
    [git-p4: depot-paths = "//depot/qt/main/": change = 123468]

--- examples/iconview/main.cpp
+++ examples/iconview/main.cpp
@@ -62,7 +62,7 @@ int main( int argc, char **argv )
 	item->setRenameEnabled( TRUE );
     }
 
-    qiconview.setCaption( "Qt Example - Iconview" );
+    qiconview.setWindowTitle( "Qt Example - Iconview" );
 
     ListenDND listen_dnd( &qiconview );
     QObject::connect( &qiconview, SIGNAL( dropped( QDropEvent *, const QValueList<QIconDragItem> & ) ),
