11ec6180b44fdbe51df594847b1c2852d461d97c
harald <harald@harald-morpork>
2003-12-17 17:16:52 +0100

    Compile
    [git-p4: depot-paths = "//depot/qt/main/": change = 124429]

--- examples/lineedits/main.cpp
+++ examples/lineedits/main.cpp
@@ -18,7 +18,7 @@ int main( int argc, char **argv )
     QApplication a( argc, argv );
 
     LineEdits lineedits;
-    lineedits.setCaption( "Qt Example - Lineedits" );
+    lineedits.setWindowTitle( "Qt Example - Lineedits" );
     a.setMainWidget( &lineedits );
     lineedits.show();
 
