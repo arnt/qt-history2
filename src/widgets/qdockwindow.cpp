e33d7f23f7b13cc9360421c6f8e8bcfbb3ca63b9
Mark Summerfield <mark@trolltech.com>
2003-12-23 15:57:14 +0100

    p4i integration Integrate 124783 from 3 to main: tiny doc fix
    [git-p4: depot-paths = "//depot/qt/main/": change = 124784]

--- src/widgets/qdockwindow.cpp
+++ src/widgets/qdockwindow.cpp
@@ -717,7 +717,8 @@ void QDockWindowTitleBar::mouseDoubleClickEvent( QMouseEvent * )
     QDockArea documentation if you want to create your own dock
     areas.) In QMainWindow a right-click popup menu (the dock window
     menu) is available which lists dock windows and can be used to
-    show or hide them.
+    show or hide them. (The popup menu only lists dock windows that
+    have a \link setCaption() caption\endlink.)
 
     When you construct a dock window you \e must pass it a QDockArea
     or a QMainWindow as its parent if you want it docked. Pass 0 for
