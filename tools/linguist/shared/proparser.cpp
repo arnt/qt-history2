1170431db5b72045b6729f10cdc5ef7fc85682fa
Andy Shaw <andy@trolltech.com>
2004-01-19 14:36:47 +0100

    Integrate 126196 manually
    [git-p4: depot-paths = "//depot/qt/main/": change = 126198]

--- tools/linguist/shared/proparser.cpp
+++ tools/linguist/shared/proparser.cpp
@@ -47,122 +47,142 @@ QMap<QString, QString> proFileTagMap( const QString& text )
 {
     QString t = text;
 
-    /*
-      Process include() commands.
-      $$PWD is a special case so we have to change it while 
-      we know where the included file is.
-    */
-    QRegExp callToInclude("include\\s*\\(\\s*([^()\\s]+)\\s*\\)");
-    int i = 0;
-    while ( (i = callToInclude.search(t, i)) != -1 ) {
-	QString after = loadFile( callToInclude.cap(1) );
-	QFileInfo fi(callToInclude.cap(1));
-	after.replace("$$PWD", fi.dirPath());
-	t.replace( i, callToInclude.matchedLength(), after );
-	i += after.length();
-    }
-
-    /*
-      Strip comments, merge lines ending with backslash, add spaces
-      around '=', '+=', and '*=', replace '\n' with ';', and simplify
-      white spaces.
-    */
-    t.replace( QRegExp(QString("#[^\n]*\n")), QString(" ") );
-    t.replace( QRegExp(QString("\\\\[^\n\\S]*\n")), QString(" ") );
-    t.replace( "=", QString(" = ") );
-    t.replace( "+ =", QString(" += ") );
-    t.replace( "* =", QString(" *= ") );
-    t.replace( "\n", QString(";") );
-    t = t.simplifyWhiteSpace();
-
-    /*
-      Populate tagMap with 'key = value' entries.
-    */
     QMap<QString, QString> tagMap;
-    QStringList lines = QStringList::split( QChar(';'), t );
-    QStringList::Iterator line;
-    for ( line = lines.begin(); line != lines.end(); ++line ) {
-	QStringList toks = QStringList::split( QChar(' '), *line );
-
-	if ( toks.count() >= 3 &&
-	     (toks[1] == QString("=") || toks[1] == QString("+=") ||
-	      toks[1] == QString("*=")) ) {
-	    QString tag = toks.first();
-	    int k = tag.findRev( QChar(':') ); // as in 'unix:'
-	    if ( k != -1 )
-		tag = tag.mid( k + 1 );
-	    toks.remove( toks.begin() );
-
-	    QString action = toks.first();
-	    toks.remove( toks.begin() );
-
-	    if ( tagMap.contains(tag) ) {
-		if ( action == QString("=") )
-		    tagMap.replace( tag, toks.join(" ") );
-		else
-		    tagMap[tag] += QChar( ' ' ) + toks.join( " " );
-	    } else {
-		tagMap[tag] = toks.join( " " );
-	    }
-	}
-    }
-
-    /*
-      Expand $$variables within the 'value' part of a 'key = value'
-      pair.
-    */
-    QRegExp var( "\\$\\$[({]?([a-zA-Z0-9_]+)[)}]?" );
-    QMap<QString, QString>::Iterator it;
-    for ( it = tagMap.begin(); it != tagMap.end(); ++it ) {
+    bool stillProcess = true; // If include() has a $$tag then we need to reprocess
+    
+    while(stillProcess) {
+	/*
+	    Process include() commands.
+	    $$PWD is a special case so we have to change it while 
+	    we know where the included file is.
+        */
+	QRegExp callToInclude("include\\s*\\(\\s*([^()\\s]+)\\s*\\)");
 	int i = 0;
-	while ( (i = var.search((*it), i)) != -1 ) {
-	    int len = var.matchedLength();
-	    QString invocation = var.cap(1);
-	    QString after;
-
-	    if ( invocation == "system" ) {
-		// skip system(); it will be handled in the next pass
-		++i;
-	    } else {
-		if ( tagMap.contains(invocation) )
-		    after = tagMap[invocation];
-		else if (invocation.lower() == "pwd")
-		    after = QDir::currentDirPath();
-		(*it).replace( i, len, after );
-	    }
-	}
-    }
-
-    /*
-      Execute system() calls.
-    */
-    QRegExp callToSystem( "\\$\\$system\\s*\\(([^()]*)\\)" );
-    for ( it = tagMap.begin(); it != tagMap.end(); ++it ) {
-	int i = 0;
-	while ( (i = callToSystem.search((*it), i)) != -1 ) {
-	    /*
-	      This code is stolen from qmake's project.cpp file.
-	      Ideally we would use the same parser, so we wouldn't
-	      have this code duplication.
-	    */
-	    QString after;
-	    char buff[256];
-	    FILE *proc = QT_POPEN( callToSystem.cap(1).latin1(), "r" );
-	    while ( proc && !feof(proc) ) {
-		int read_in = fread( buff, 1, 255, proc );
-		if ( !read_in )
-		    break;
-		for ( int i = 0; i < read_in; i++ ) {
-		    if ( buff[i] == '\n' || buff[i] == '\t' )
-			buff[i] = ' ';
+	while ( (i = callToInclude.search(t, i)) != -1 ) {
+	    bool doneWithVar = false;
+	    QString fileName = callToInclude.cap(1);
+	    QString after = fileName.replace("$$PWD", QDir::currentDirPath());
+	    if (!tagMap.isEmpty() && after.contains("$$")) {
+		QRegExp var( "\\$\\$[({]?([a-zA-Z0-9_]+)[)}]?" );
+		int ii = 0;
+		while ((ii = after.find(var, ii)) != -1) {
+		    if (tagMap.contains(var.cap(1))) {
+			after.replace(ii, var.cap(0).length(), tagMap[var.cap(1)]);
+		    } else { // Couldn't find it
+			doneWithVar = true;
+			break;
+		    }
 		}
-		buff[read_in] = '\0';
-		after += buff;
+		
 	    }
-	    (*it).replace( i, callToSystem.matchedLength(), after );
+	    if (doneWithVar || !after.contains("$$")) {
+		after = loadFile(after);
+		QFileInfo fi(callToInclude.cap(1));
+		after.replace("$$PWD", fi.dirPath());
+		t.replace( i, callToInclude.matchedLength(), after );
+	    }
 	    i += after.length();
 	}
-    }
 
+	/*
+	    Strip comments, merge lines ending with backslash, add
+	    spaces around '=' and '+=', replace '\n' with ';', and
+	    simplify white spaces.
+	*/
+	t.replace( QRegExp(QString("#[^\n]*\n")), QString(" ") );
+	t.replace( QRegExp(QString("\\\\[^\n\\S]*\n")), QString(" ") );
+	t.replace( "=", QString(" = ") );
+	t.replace( "+ =", QString(" += ") );
+	t.replace( "\n", QString(";") );
+	t = t.simplifyWhiteSpace();
+
+	/*
+	    Populate tagMap with 'key = value' entries.
+	*/
+	QStringList lines = QStringList::split( QChar(';'), t );
+	QStringList::Iterator line;
+	for ( line = lines.begin(); line != lines.end(); ++line ) {
+	    QStringList toks = QStringList::split( QChar(' '), *line );
+
+	    if ( toks.count() >= 3 &&
+		(toks[1] == QString("=") || toks[1] == QString("+=") ||
+		toks[1] == QString("*=")) ) {
+		QString tag = toks.first();
+		int k = tag.findRev( QChar(':') ); // as in 'unix:'
+		if ( k != -1 )
+		    tag = tag.mid( k + 1 );
+		toks.remove( toks.begin() );
+		
+		QString action = toks.first();
+		toks.remove( toks.begin() );
+		
+		if ( tagMap.contains(tag) ) {
+		    if ( action == QString("=") )
+			tagMap.replace( tag, toks.join(" ") );
+		    else
+			tagMap[tag] += QChar( ' ' ) + toks.join( " " );
+		} else {
+		    tagMap[tag] = toks.join( " " );
+		}
+	    }
+	}
+	/*
+	    Expand $$variables within the 'value' part of a 'key = value'
+	    pair.
+	*/
+	QRegExp var( "\\$\\$[({]?([a-zA-Z0-9_]+)[)}]?" );
+	QMap<QString, QString>::Iterator it;
+	for ( it = tagMap.begin(); it != tagMap.end(); ++it ) {
+	    int i = 0;
+	    while ( (i = var.search((*it), i)) != -1 ) {
+		int len = var.matchedLength();
+		QString invocation = var.cap(1);
+		QString after;
+		
+		if ( invocation == "system" ) {
+		    // skip system(); it will be handled in the next pass
+		    ++i;
+		} else {
+		    if ( tagMap.contains(invocation) )
+			after = tagMap[invocation];
+		    else if (invocation.lower() == "pwd")
+			after = QDir::currentDirPath();
+		    (*it).replace( i, len, after );
+		}
+	    }
+	}
+    
+	/*
+	  Execute system() calls.
+	*/
+	QRegExp callToSystem( "\\$\\$system\\s*\\(([^()]*)\\)" );
+	for ( it = tagMap.begin(); it != tagMap.end(); ++it ) {
+	    int i = 0;
+	    while ( (i = callToSystem.search((*it), i)) != -1 ) {
+		/*
+		  This code is stolen from qmake's project.cpp file.
+		  Ideally we would use the same parser, so we wouldn't
+		  have this code duplication.
+		*/
+		QString after;
+		char buff[256];
+		FILE *proc = QT_POPEN( callToSystem.cap(1).latin1(), "r" );
+		while ( proc && !feof(proc) ) {
+		    int read_in = fread( buff, 1, 255, proc );
+		    if ( !read_in )
+			break;
+		    for ( int i = 0; i < read_in; i++ ) {
+			if ( buff[i] == '\n' || buff[i] == '\t' )
+			    buff[i] = ' ';
+		    }
+		    buff[read_in] = '\0';
+		    after += buff;
+		}
+		(*it).replace( i, callToSystem.matchedLength(), after );
+		i += after.length();
+	    }
+	}
+	stillProcess = callToInclude.search(t) != -1;
+    }
     return tagMap;
 }
