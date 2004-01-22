603223089fdeec9e163ca62e84467dfc7fb9f408
Dimitri Papadopoulos <dimitri@trolltech.com>
2004-01-22 22:26:03 +0100

    manual integration Integrate 126839 from 3 to main: link to new SAX website
    [git-p4: depot-paths = "//depot/qt/main/": change = 126840]

--- src/xml/qxml.cpp
+++ src/xml/qxml.cpp
@@ -2335,7 +2335,7 @@ private:
     (e.g. a validating parser).
 
     The design of the XML classes follows the \link
-    http://www.megginson.com/SAX/ SAX2 Java interface\endlink, with
+    http://www.saxproject.org/ SAX2 Java interface\endlink, with
     the names adapted to fit Qt naming conventions. It should be very
     easy for anybody who has worked with SAX2 to get started with the
     Qt XML classes.
