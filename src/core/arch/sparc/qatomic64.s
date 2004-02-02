88bef1ab5b721c8aa3f2787332d58f742facfd07
Brad Hughes <bhughes@trolltech.com>
2004-02-02 13:51:15 +0100

    correct the q_atomic_test_and_set_* implementation for SPARC-v9 (64 bit)...
    [git-p4: depot-paths = "//depot/qt/main/": change = 127844]

--- src/core/arch/sparc/qatomic64.s
+++ src/core/arch/sparc/qatomic64.s
@@ -4,23 +4,20 @@
 	.global q_atomic_test_and_set_int
 q_atomic_test_and_set_int:
 	cas [%o0],%o1,%o2
-        ld [%o0],%o1
 	cmp %o1,%o2
-	or %g0,0,%o2
-	movne %xcc,1,%o2
+        clr %o0
 	retl
-	mov %o2,%o0
+	move %icc,1,%o0
 	.size q_atomic_test_and_set_int,.-q_atomic_test_and_set_int
 	.align 4
 	.type q_atomic_test_and_set_ptr,#function
 	.global q_atomic_test_and_set_ptr
 q_atomic_test_and_set_ptr:
 	casx [%o0],%o1,%o2
-        ld [%o0],%o1
 	cmp %o1,%o2
-	or %g0,0,%o2
-	movne %xcc,1,%o2
+	clr %o0
 	retl
-	mov %o2,%o0
+	move %icc,1,%o0
 	.size q_atomic_test_and_set_ptr,.-q_atomic_test_and_set_ptr
 	.align 4
+
