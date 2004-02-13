8cd85b483daff91a638cd6604a8223b06ac1f3fe
Sam Magnuson <sam@trolltech.com>
2004-02-13 23:37:34 +0100

    docs
    [git-p4: depot-paths = "//depot/qt/main/": change = 129254]

--- src/gui/widgets/qrubberband.cpp
+++ src/gui/widgets/qrubberband.cpp
@@ -9,6 +9,37 @@
 #define d d_func()
 #define q q_func()
 
+/*!
+   \class QRubberBand qrubberband.h
+   \brief The QRubberBand widget provides a widget that can shown to represent a rubber band
+    around selection (or temporary boundries).
+
+    \ingroup misc
+    \mainclass
+
+    A rubber band is often displayed to show a new bounding area (as
+    in a QSplitter or a QDockWindow that is undocking). Traditionally
+    this has been implemented using a QPainter and XOr, this can be
+    dangerous as rendering can happen in the window below the rubber
+    band, but before the rubber band has been "erased". 
+
+    You can create a QRubberBand class whenever you need to render a
+    rubber band around a given area (or to represent a single line),
+    then setGeometry() on it to move to a new rectangle, finally
+    hiding the widget will make the rubber band disappear. 
+
+*/
+
+
+/*!
+    Construct a rubber band of type \a t.
+  
+    By default a \e Rectangle QRubberBand will be set to auto mask, so
+    that the boundry of the rectangle is all that is visible, some
+    styles (for example native Mac OS X) will change this and call
+    QWidget::setWindowOpacity() to make the window only partially
+    opaque.
+*/
 QRubberBand::QRubberBand(QRubberBand::Type t, QWidget *p) : 
     QWidget(*new QRubberBandPrivate, p, WType_TopLevel | WStyle_Tool | WStyle_Customize | WStyle_NoBorder)
 {
@@ -17,10 +48,19 @@ QRubberBand::QRubberBand(QRubberBand::Type t, QWidget *p) :
 	setAutoMask(true);
 }
 
+/*!
+  Destructor.
+*/
 QRubberBand::~QRubberBand()
 {
 }
 
+/*!
+    Virtual function that draws the mask of a QRubberBand.
+
+   This is normally themed (via QStyle) however you can overload this
+   for special renderings in a given widget.
+*/
 void
 QRubberBand::drawRubberBandMask(QPainter *p)
 {
@@ -30,6 +70,12 @@ QRubberBand::drawRubberBandMask(QPainter *p)
     style().drawPrimitive(QStyle::PE_RubberBandMask, p, rect(), palette(), flags);
 }
 
+/*!
+    Virtual function that draws the contents of a QRubberBand.
+
+   This is normally themed (via QStyle) however you can overload this
+   for special renderings in a given widget.
+*/
 void
 QRubberBand::drawRubberBand(QPainter *p)
 {
@@ -39,6 +85,9 @@ QRubberBand::drawRubberBand(QPainter *p)
     style().drawPrimitive(QStyle::PE_RubberBand, p, rect(), palette(), flags);
 }
 
+/*!
+    \reimp
+*/
 void 
 QRubberBand::updateMask()
 {
@@ -49,6 +98,9 @@ QRubberBand::updateMask()
     setMask(QRegion(bm));
 }
 
+/*!
+    \reimp
+*/
 void
 QRubberBand::paintEvent(QPaintEvent *)
 {
@@ -56,6 +108,9 @@ QRubberBand::paintEvent(QPaintEvent *)
     drawRubberBand(&p);
 }
 
+/*!
+    \reimp
+*/
 void 
 QRubberBand::changeEvent(QEvent *ev)
 {
