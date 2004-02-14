b072b3869f8eed4338a5aa762a004c58aad28032
Andy Shaw <andy@trolltech.com>
2004-02-14 13:35:24 +0100

    Build dirview example
    [git-p4: depot-paths = "//depot/qt/main/": change = 129275]

--- examples/dirview/main.cpp
+++ examples/dirview/main.cpp
@@ -25,13 +25,10 @@ int main( int argc, char ** argv )
     mw.addColumn( "Type" );
     mw.setTreeStepSize( 20 );
 
-    const QFileInfoList* roots = QDir::drives();
-    QPtrListIterator<QFileInfo> i(*roots);
-    QFileInfo* fi;
-    while ( (fi = *i) ) {
-	++i;
-	Directory * root = new Directory( &mw, fi->filePath() );
-	if ( roots->count() <= 1 )
+    const QFileInfoList roots = QDir::drives();
+    for (int i=0;i<roots.size();++i) {
+	Directory * root = new Directory( &mw, roots.at(i).filePath() );
+	if ( roots.size() <= 1 )
 	    root->setOpen( TRUE ); // be interesting
     }
 
