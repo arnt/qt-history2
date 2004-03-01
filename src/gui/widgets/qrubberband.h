6e3108aceb20b3e59e33c042c3d2650cf2147ee0
Brad Hughes <bhughes@trolltech.com>
2004-03-01 17:37:57 +0100

    follow style guidelines
    [git-p4: depot-paths = "//depot/qt/main/": change = 131325]

--- src/gui/widgets/qrubberband.h
+++ src/gui/widgets/qrubberband.h
@@ -25,6 +25,7 @@ class Q_GUI_EXPORT QRubberBand : public QWidget
 {
     Q_OBJECT
     Q_DECLARE_PRIVATE(QRubberBand);
+
 public:
     enum Shape { Line, Rectangle };
     QRubberBand(Shape, QWidget * =0);
@@ -32,12 +33,20 @@ public:
 
     Shape shape() const;
 
-    void move(const QPoint &);
-    void move(int, int);
-    void resize(const QSize &);
-    void resize(int, int);
-    void setGeometry(const QRect &);
     void setGeometry(int x, int y, int w, int h);
+
+    inline void move(const QPoint &p)
+    { move(p.x(), p.y()); }
+    inline void move(int x, int y)
+    { setGeometry(x + geometry().x() - QWidget::x(),
+		  y - geometry().y() - QWidget::y(),
+		  width(), height()); }
+    inline void resize(const QSize &s)
+    { resize(s.width(), s.height()); }
+    inline void resize(int w, int h)
+    { setGeometry(geometry().x(), geometry().y(), w, h); }
+    inline void setGeometry(const QRect &r)
+    { setGeometry(r.left(), r.top(), r.width(), r.height()); }
 
 protected:
     virtual void drawRubberBandMask(QPainter *);
@@ -45,24 +54,7 @@ protected:
 
     void paintEvent(QPaintEvent *);
     void changeEvent(QEvent *);
-private:
     void updateMask();
 };
-
-inline void QRubberBand::move(const QPoint &p)
-{ move(p.x(), p.y()); }
-
-inline void QRubberBand::move(int x, int y)
-{ setGeometry(x + geometry().x() - QWidget::x(), y - geometry().y() - QWidget::y(), width(), height()); }
-
-inline void QRubberBand::resize(const QSize &s)
-{ resize(s.width(), s.height()); }
-
-inline void QRubberBand::resize(int w, int h)
-{ setGeometry(geometry().x(), geometry().y(), w, h); }
-
-inline void QRubberBand::setGeometry(const QRect &r)
-{ setGeometry(r.left(), r.top(), r.width(), r.height()); }
-
 
 #endif /* __QRUBBERBAND_H__ */
