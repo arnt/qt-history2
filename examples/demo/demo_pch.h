8772edb7fbea3a5b132ce41b19fe5011b55d7111
gunnar <gunnar@THAUMASIA>
2004-03-08 14:41:58 +0100

    Locate the qt_pch file in the right directory.
    [git-p4: depot-paths = "//depot/qt/main/": change = 132100]

--- examples/demo/demo_pch.h
+++ examples/demo/demo_pch.h
@@ -1,13 +1,13 @@
 /*
- * This is a precompiled header file for use in Xcode / Mac GCC / 
+ * This is a precompiled header file for use in Xcode / Mac GCC /
  * GCC >= 3.4 / VC to greatly speed the building of the Demo example.
- * It may also be of use to people developing their own project, but 
- * it is probably better to define your own header.  Use of this 
+ * It may also be of use to people developing their own project, but
+ * it is probably better to define your own header.  Use of this
  * header is currently UNSUPPORTED.
  */
 
 #if defined __cplusplus
 
-#include "../../src/core/base/qt_pch.h"
+#include "../../src/core/global/qt_pch.h"
 
 #endif
