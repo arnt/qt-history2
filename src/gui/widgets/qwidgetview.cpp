0ea6d7293b627b9db16b167fbc0851f9bbee21bc
Matthias Ettrich <ettrich@trolltech.com>
2004-03-10 14:42:37 +0100

    no need to initialize QPointers (and added copyright header)
    [git-p4: depot-paths = "//depot/qt/main/": change = 132530]

--- src/gui/widgets/qwidgetview.cpp
+++ src/gui/widgets/qwidgetview.cpp
@@ -1,3 +1,17 @@
+/****************************************************************************
+**
+** Implementation of QWidgetView widget class.
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the widgets module of the Qt GUI Toolkit.
+** EDITIONS: FREE, PROFESSIONAL, ENTERPRISE
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #include "qwidgetview.h"
 #include "qscrollbar.h"
 #include "private/qviewport_p.h"
@@ -8,7 +22,7 @@ class QWidgetViewPrivate: public QViewportPrivate
     Q_DECLARE_PUBLIC(QWidgetView);
 
 public:
-    QWidgetViewPrivate():widget(0), resizable(false){}
+    QWidgetViewPrivate(): resizable(false){}
     void updateScrollBars();
     QPointer<QWidget> widget;
     mutable QSize widgetSize;
