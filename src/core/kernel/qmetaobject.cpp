bee133e5eede4190c2e9ec8c8ee90051a18fe56d
Volker Hilsheimer <vohi@trolltech.com>
2004-03-18 15:27:04 +0100

    QVariant::data() might construct, so test for QVariant-type first.
    [git-p4: depot-paths = "//depot/qt/main/": change = 133507]

--- src/core/kernel/qmetaobject.cpp
+++ src/core/kernel/qmetaobject.cpp
@@ -1225,7 +1225,7 @@ QCoreVariant QMetaProperty::read(const QObject *obj) const
     const_cast<QObject*>(obj)->qt_metacall(QMetaObject::ReadProperty,
                      idx[QMetaObject::ReadProperty] + mobj[QMetaObject::ReadProperty]->propertyOffset(),
                      argv);
-    if (argv[0] != value.data() && t != 0xffffffff)
+    if (t != 0xffffffff && argv[0] != value.data())
         return QCoreVariant(t, argv[0]);
     return value;
 }
