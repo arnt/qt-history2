565ceb23ffa0c266e8129cff1f75cb1246693d82
ahanssen <ahanssen@:w>
2004-03-22 15:53:44 +0100

    Use qInvokeSlot to invoke the receiver/member when the argument to gethostbyname is an IP address. Use a better way to register the QResolverHostInfo type.
    [git-p4: depot-paths = "//depot/qt/main/": change = 133838]

--- src/network/qresolver.cpp
+++ src/network/qresolver.cpp
@@ -1,34 +1,58 @@
 #include "qresolver.h"
 #include "qresolver_p.h"
 #include <qmetaobject.h>
-
-void *qt_copy_QResolverHostInfo(const void *voidCopy)
-{
-    QResolverHostInfo *copy = (QResolverHostInfo *)voidCopy;
-    QResolverHostInfo *info = new QResolverHostInfo(*copy);
-    return info;
-}
-
-void qt_destroy_QResolverHostInfo(void *)
-{
-}
+#include <qtimer.h>
+#include <qsignal.h>
+#include <qsocketdevice.h>
 
 /*!
     Looks up the hostname \a name. When the results of the lookup are
     ready, the slot or singal \a resultsReady in \a receiver is invoked.
 */
-void QResolver::getHostByName(const QString &name, const QObject *receiver,
-                              const char *resultsReady )
+void QResolver::getHostByName(const QString &name, QObject *receiver,
+                              const char *member)
 {
-    QResolverAgent *agent = new QResolverAgent(name);
+#if defined Q_OS_WIN32
+    QSocketDevice bust; // ### makes sure WSAStartup was callled
+#endif
 
-    QMetaType::registerType("QResolverHostInfo",
-                            qt_destroy_QResolverHostInfo,
-                            qt_copy_QResolverHostInfo);
+    // Don't start a thread if we don't have to do any lookup.    
+    QHostAddress addr;
+    if (addr.setAddress(name)) {
+        if (!member || !member[0]) {
+            qWarning("QResolver::getHostByName() called with invalid slot [%s]", member);
+            return;
+        }
+
+        QByteArray arr(member + 1);
+        if (!arr.contains('(')) {
+            qWarning("QResolver::getHostByName() called with invalid slot [%s]", member);
+            return;
+        }
+
+        QResolverHostInfo info;
+        info.addresses << addr;
+        arr.resize(arr.indexOf('('));
+
+        // To mimic the same behavior that the lookup would have if it was not
+        // an IP, we need to choose a QueuedConnection if there is thread support;
+        // otherwise DirectConnection.
+        if (!qInvokeSlot(receiver, arr,
+#if !defined QT_NO_THREAD
+                         Qt::QueuedConnection,
+#else
+                         Qt::DirectConnection,
+#endif
+                         QGenericArgument("QResolverHostInfo", &info))) {
+            qWarning("QResolver::getHostByName() called with invalid slot (qInvokeSlot failed)");
+        }
+        return;
+    }
     
-    QObject::connect(agent, SIGNAL(resultsReady(QResolverHostInfo)),
-                     receiver, resultsReady);
+    QResolverAgent *agent = new QResolverAgent(name);
+    qRegisterMetaType<QResolverHostInfo>("QResolverHostInfo");
 
+    QObject::connect(agent, SIGNAL(resultsReady(QResolverHostInfo)), receiver, member);
 
 #if !defined QT_NO_THREAD
     agent->start();
@@ -46,4 +70,3 @@ QResolverHostInfo::QResolverHostInfo(const QResolverHostInfo &d)
     : error( d.error ), errorString( d.errorString ), addresses( d.addresses )
 {
 }
-
