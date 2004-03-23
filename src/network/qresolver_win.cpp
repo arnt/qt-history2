04280694fd962ec5b4ba03956fc5d2eb712b8d01
ahanssen <ahanssen@:w>
2004-03-23 11:29:26 +0100

    Added debug data.
    [git-p4: depot-paths = "//depot/qt/main/": change = 133959]

--- src/network/qresolver_win.cpp
+++ src/network/qresolver_win.cpp
@@ -5,24 +5,31 @@
 #include <qsignal.h>
 #include <qtimer.h>
 
-//#define QRESOLVER_DEBUG
+// #define QRESOLVER_DEBUG
 
+// Older SDKs do not include the addrinfo struct declaration, so we
+// include a copy of it here.
 struct qt_addrinfo
 {
-    int                 ai_flags;       // AI_PASSIVE, AI_CANONNAME, AI_NUMERICHOST
-    int                 ai_family;      // PF_xxx
-    int                 ai_socktype;    // SOCK_xxx
-    int                 ai_protocol;    // 0 or IPPROTO_xxx for IPv4 and IPv6
-    size_t              ai_addrlen;     // Length of ai_addr
-    char *              ai_canonname;   // Canonical name for nodename
-    struct sockaddr    *ai_addr;        // Binary address
-    struct qt_addrinfo *ai_next;        // Next structure in linked list
+    int ai_flags;
+    int ai_family;
+    int ai_socktype;
+    int ai_protocol;
+    size_t ai_addrlen;
+    char *ai_canonname;
+    sockaddr *ai_addr;
+    qt_addrinfo *ai_next;
 };
 
+/*
+    Performs a blocking call to gethostbyname or getaddrinfo, stores
+    the results in a QResolverHostInfo structure and emits the
+    resultsReady() signal.
+*/
 void QResolverAgent::run()
 {
 #if defined(QRESOLVER_DEBUG)
-    qDebug("QResolverAgent::run(%p): start DNS lookup", this);
+    qDebug("QResolverAgent::run(%p): looking up \"%s\"", this, hostName.latin1());
 #endif
 
     // Attempt to resolve getaddrinfo(); without it we'll have to fall
@@ -69,12 +76,21 @@ void QResolverAgent::run()
             results.errorString = tr("Host not found");
         } else {
             results.error = QResolver::UnknownError;
+            results.errorString = tr("Unknown error");
             // Get the error messages returned by getaddrinfo's gai_strerror
             QT_WA( {
-                results.errorString = QString::fromUtf16((ushort *) gai_strerrorW(err));
+                typedef char *(*gai_strerrorWProto)(int);
+                gai_strerrorWProto local_gai_strerrorW;
+                local_gai_strerrorW = (gai_strerrorWProto) QLibrary::resolve("ws2_32.dll", "gai_strerrorW");
+                if (local_gai_strerrorW)
+                    results.errorString = QString::fromUtf16((ushort *) local_gai_strerrorW(err));
             } , {
-                results.errorString = QString::fromLocal8Bit(gai_strerrorA(err));
-            } );            
+                typedef char *(*gai_strerrorAProto)(int);
+                gai_strerrorAProto local_gai_strerrorA;
+                local_gai_strerrorA = (gai_strerrorAProto) QLibrary::resolve("ws2_32.dll", "gai_strerrorA");
+                if (local_gai_strerrorA)
+                    results.errorString = QString::fromLocal8Bit(local_gai_strerrorA(err));
+            } );
         }
     } else {
         // Fall back to gethostbyname, which only supports IPv4.
@@ -101,6 +117,21 @@ void QResolverAgent::run()
             results.error = QResolver::UnknownError;
         }
     }
+
+#if defined(QRESOLVER_DEBUG)
+    if (results.error != QResolver::NoError) {
+        qDebug("QResolverAgent::run(%p): error (%s)",
+               this, results.errorString.latin1());
+    } else {
+        QString tmp;
+        for (int i = 0; i < results.addresses.count(); ++i) {
+            if (i != 0) tmp += ", ";
+            tmp += results.addresses.at(i).toString();
+        }
+        qDebug("QResolverAgent::run(%p): found %i entries: {%s}",
+               this, results.addresses.count(), tmp.latin1());
+    }
+#endif
     
     emit resultsReady(results);
 
