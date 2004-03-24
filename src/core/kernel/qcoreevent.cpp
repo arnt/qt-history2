6d37dade6d7b440b41c220a1ba9222c44f630f49
Marius Storm-Olsen <marius@trolltech.com>
2004-03-24 16:55:21 +0100

    Document Shortcut & ShortcutOverride events
    [git-p4: depot-paths = "//depot/qt/main/": change = 134207]

--- src/core/kernel/qcoreevent.cpp
+++ src/core/kernel/qcoreevent.cpp
@@ -132,6 +132,8 @@
     \value Quit  Reserved.
     \value Reparent  Reserved.
     \value Resize  Widget's size changed, \l{QResizeEvent}.
+    \value Shortcut Key press in child for shortcut key handling, \l{QKeyEvent}.
+    \value ShortcutOverride  Key press in child, for overriding shortcut key handling, \l{QKeyEvent}.
     \value Show  Widget was shown on screen, \l{QShowEvent}.
     \value ShowFullScreen  Widget should be shown full-screen (obsolete).
     \value ShowMaximized  Widget should be shown maximized (obsolete).
