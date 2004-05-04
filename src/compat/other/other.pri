533aabbf24a5754bd3a740e83cb0ab3514571b02
ahanssen <ahanssen@:w>
2004-05-04 11:01:34 +0100

    Moving old QUrl into Q3Url.
    [git-p4: depot-paths = "//depot/qt/main/": change = 138238]

--- src/compat/other/other.pri
+++ src/compat/other/other.pri
@@ -6,11 +6,14 @@ HEADERS +=      other/qdropsite.h \
                 other/qnetworkprotocol.h \
                 other/q3dns.h \
                 other/qurloperator.h \
-                other/qaccel.h
+                other/qaccel.h \
+		other/q3url.h
 
 SOURCES +=      other/qdropsite.cpp \
                 other/qlocalfs.cpp \
                 other/qnetworkprotocol.cpp \
                 other/q3dns.cpp \
                 other/qurloperator.cpp \
-                other/qaccel.cpp
+                other/qaccel.cpp \
+		other/q3url.cpp
+
---
+++ src/compat/other/q3url.h
@@ -0,0 +1,109 @@
+/****************************************************************************
+**
+** Definition of QUrl class.
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the kernel module of the Qt GUI Toolkit.
+** EDITIONS: FREE, PROFESSIONAL, ENTERPRISE
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QURL_H
+#define QURL_H
+
+#ifndef QT_H
+#include "qstring.h"
+#endif // QT_H
+
+#ifndef QT_NO_URL
+
+class QUrlPrivate;
+
+class Q_CORE_EXPORT QUrl
+{
+public:
+    QUrl();
+    QUrl(const QString& url);
+    QUrl(const QUrl& url);
+    QUrl(const QUrl& url, const QString& relUrl, bool checkSlash = false);
+    virtual ~QUrl();
+
+    QString protocol() const;
+    virtual void setProtocol(const QString& protocol);
+
+    QString user() const;
+    virtual void setUser(const QString& user);
+    bool hasUser() const;
+
+    QString password() const;
+    virtual void setPassword(const QString& pass);
+    bool hasPassword() const;
+
+    QString host() const;
+    virtual void setHost(const QString& user);
+    bool hasHost() const;
+
+    int port() const;
+    virtual void setPort(int port);
+    bool hasPort() const;
+
+    QString path(bool correct = true) const;
+    virtual void setPath(const QString& path);
+    bool hasPath() const;
+
+    virtual void setEncodedPathAndQuery(const QString& enc);
+    QString encodedPathAndQuery();
+
+    virtual void setQuery(const QString& txt);
+    QString query() const;
+
+    QString ref() const;
+    virtual void setRef(const QString& txt);
+    bool hasRef() const;
+
+    bool isValid() const;
+    bool isLocalFile() const;
+
+    virtual void addPath(const QString& path);
+    virtual void setFileName(const QString& txt);
+
+    QString fileName() const;
+    QString dirPath() const;
+
+    QUrl& operator=(const QUrl& url);
+    QUrl& operator=(const QString& url);
+
+    bool operator==(const QUrl& url) const;
+    bool operator==(const QString& url) const;
+
+    inline bool operator!=(const QUrl &url) const { return !(operator==(url)); }
+    inline bool operator!=(const QString &url) const { return !(operator==(url)); }
+
+    static void decode(QString& url);
+    static void encode(QString& url);
+
+    operator QString() const;
+
+
+    virtual QString toString(bool encodedPath = false, bool forcePrependProtocol = true) const;
+
+    virtual bool cdUp();
+
+    static bool isRelativeUrl(const QString &url);
+
+protected:
+    virtual void reset();
+    virtual bool parse(const QString& url);
+
+private:
+    QUrlPrivate *d;
+
+};
+
+#endif //QT_NO_URL
+
+#endif
