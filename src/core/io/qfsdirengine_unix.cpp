700c68abd7d01399079eab98b4cde59d2685f133
Sam Magnuson <sam@trolltech.com>
2004-05-06 03:13:22 +0100

    all dirs is definetlely right (this is how the old one worked as well). Without this fix qmake was broken and qtestlib didn't work :(
    [git-p4: depot-paths = "//depot/qt/main/": change = 138525]

--- src/core/io/qfsdirengine_unix.cpp
+++ src/core/io/qfsdirengine_unix.cpp
@@ -88,13 +88,12 @@ QFSDirEngine::entryList(int filterSpec, const QStringList &filters) const
         QString fn = QFile::decodeName(QByteArray(file->d_name));
         fi.setFile(d->path + '/' + fn);
 #ifndef QT_NO_REGEXP
-        if(!(filterSpec & QDir::Dirs && fi.isDir())) {
+        if(!((filterSpec & QDir::AllDirs) && fi.isDir())) {
             bool matched = false;
             for(QStringList::ConstIterator sit = filters.begin(); sit != filters.end(); ++sit) {
                 QRegExp rx(*sit, QString::CaseSensitive, QRegExp::Wildcard);
-                if (rx.exactMatch(fn)) {
+                if (rx.exactMatch(fn)) 
                     matched = true;
-                }
             }
             if(!matched)
                 continue;
