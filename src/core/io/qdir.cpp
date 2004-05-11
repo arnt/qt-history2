347c0bb2f25d0527c0f7a6eb6a0e3d39855ce533
Sam Magnuson <sam@trolltech.com>
2004-05-11 23:57:37 +0100

    another bug.
    [git-p4: depot-paths = "//depot/qt/main/": change = 139269]

--- src/core/io/qdir.cpp
+++ src/core/io/qdir.cpp
@@ -1632,7 +1632,7 @@ QDir::cleanDirPath(const QString &in)
                 eaten = true;
 #endif
                 last = -1;
-            } else if(i == len-1) {
+            } else if(i && i == len-1) {
                 eaten = true;
             } else {
                 levels++;
