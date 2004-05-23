68008672c68fabd9cf11a8197560717dc6ec0586
Dimitri Papadopoulos <dimitri@trolltech.com>
2004-05-23 10:14:53 +0100

    Copyright.
    [git-p4: depot-paths = "//depot/qt/main/": change = 140579]

--- src/network/qdns_p.h
+++ src/network/qdns_p.h
@@ -1,3 +1,17 @@
+/****************************************************************************
+**
+** Definition of QDnsHostInfo class.
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the network module of the Qt GUI Toolkit.
+** EDITIONS: FREE, ENTERPRISE
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #ifndef QDNS_P_H
 #define QDNS_P_H
 
