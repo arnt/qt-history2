23b52e5ad40c415861cdb9d2746b0b2c160a3847
Marius Storm-Olsen <marius@trolltech.com>
2004-05-28 08:39:48 +0100

    No, keep WinCE code, and don't break desktop
    [git-p4: depot-paths = "//depot/qt/main/": change = 141300]

--- src/gui/styles/qwindowsstyle.cpp
+++ src/gui/styles/qwindowsstyle.cpp
@@ -303,7 +303,11 @@ int QWindowsStyle::pixelMetric(PixelMetric metric, const QWidget *widget) const
 #if defined(Q_WS_WIN)
     case PM_TitleBarHeight:
         {
+#if defined(Q_OS_TEMP)
             ret = GetSystemMetrics(SM_CYCAPTION) - 1;
+#else
+            ret = GetSystemMetrics(SM_CYMCAPTION) - 1;
+#endif
         }
 
         break;
