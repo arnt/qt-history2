129b7d6fbc20c3b1ff5ac80485d893f457953f85
ahanssen <ahanssen@:w>
2004-05-28 09:44:08 +0100

    Added tests for blocking DNS lookups.
    [git-p4: depot-paths = "//depot/qt/main/": change = 141315]

--- src/network/qdns_unix.cpp
+++ src/network/qdns_unix.cpp
@@ -88,16 +88,16 @@ QDnsHostInfo QDnsAgent::getHostByName(const QString &hostName)
 
 #if defined(QDNS_DEBUG)
     if (results.d->err != QDnsHostInfo::NoError) {
-        qDebug("QDnsAgent::run(%p): error #%d %s",
-               this, h_errno, results.d->errorStr.latin1() );
+        qDebug("QDnsAgent::getHostByName(): error #%d %s",
+               h_errno, results.d->errorStr.latin1());
     } else {
         QString tmp;
         for (int i = 0; i < results.d->addrs.count(); ++i) {
             if (i != 0) tmp += ", ";
             tmp += results.d->addrs.at(i).toString();
         }
-        qDebug("QDnsAgent::run(%p): found %i entries for \"%s\": {%s}",
-               this, results.d->addrs.count(), hostName.latin1(), tmp.latin1());
+        qDebug("QDnsAgent::getHostByName(): found %i entries for \"%s\": {%s}",
+               results.d->addrs.count(), hostName.latin1(), tmp.latin1());
     }
 #endif
     return results;
