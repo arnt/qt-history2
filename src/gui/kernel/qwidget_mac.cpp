d5dd1f35a357862b1ab5f69d8b4f2d9009a42a80
Sam Magnuson <sam@trolltech.com>
2004-06-02 01:35:51 +0100

    emacs failed to save before submit
    [git-p4: depot-paths = "//depot/qt/main/": change = 141604]

--- src/gui/kernel/qwidget_mac.cpp
+++ src/gui/kernel/qwidget_mac.cpp
@@ -248,7 +248,7 @@ QMAC_PASCAL OSStatus QWidgetPrivate::qt_window_event(EventHandlerCallRef er, Eve
         }
         break;
     case kEventClassMouse: {
-        QMacMouseEvent macmouse(event);
+        QMacMouseEvent macmous(event);
         handled_event = (SendEventToApplication(event) == noErr);
         break; }
     default:
