13127953c91367278867cc320ec4845676515910
Brad Hughes <bhughes@trolltech.com>
2004-06-08 10:28:41 +0100

    compile with gcc 3.4.1
    [git-p4: depot-paths = "//depot/qt/main/": change = 142417]

--- src/core/tools/qqueue.h
+++ src/core/tools/qqueue.h
@@ -11,10 +11,10 @@ class QQueue : public QList<T>
 public:
     inline QQueue() {}
     inline ~QQueue() {}
-    inline void enqueue(const T &t) { append(t); }
-    inline T dequeue() { return takeFirst(); }
-    inline T &head() { return first(); }
-    inline const T &head() const { return first(); }
+    inline void enqueue(const T &t) { QList<T>::append(t); }
+    inline T dequeue() { return QList<T>::takeFirst(); }
+    inline T &head() { return QList<T>::first(); }
+    inline const T &head() const { return QList<T>::first(); }
 };
 
 #endif
