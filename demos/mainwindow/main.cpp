6ef3c0179c52f1b9bec2d703aac4f55275fbe228
Brad Hughes <bhughes@trolltech.com>
2004-06-16 14:29:06 +0100

    use a more appropriate caption^Wwindow title
    [git-p4: depot-paths = "//depot/qt/main/": change = 143477]

--- demos/mainwindow/main.cpp
+++ demos/mainwindow/main.cpp
@@ -1,6 +1,5 @@
 #include <qapplication.h>
 
-#include "colorswatch.h"
 #include "mainwindow.h"
 
 int main(int argc, char **argv)
@@ -8,64 +7,9 @@ int main(int argc, char **argv)
     QApplication app(argc, argv);
     app.connect(&app, SIGNAL(lastWindowClosed()), SLOT(quit()));
 
-    MainWindow *mainwindow;
-    ColorSwatch *colorswatch;
-
-#if 0
-    mainwindow = new MainWindow;
-    mainwindow->setWindowTitle("Main Window");
-    colorswatch = new ColorSwatch("White", mainwindow);
-    colorswatch->setArea(Qt::DockWindowAreaLeft);
-    colorswatch->setArea(Qt::DockWindowAreaLeft);
-    colorswatch->setArea(Qt::DockWindowAreaLeft);
-    colorswatch->setArea(Qt::DockWindowAreaLeft);
+    MainWindow *mainwindow = new MainWindow(0, Qt::WDestructiveClose);
+    mainwindow->setWindowTitle("Qt Main Window Demo");
     mainwindow->show();
-#endif
-
-#if 0
-    mainwindow = new MainWindow;
-    mainwindow->setWindowTitle("Not Closable");
-    colorswatch = new ColorSwatch("Red", mainwindow);
-    colorswatch->setClosable(false);
-    colorswatch->setArea(Qt::DockWindowAreaLeft);
-    mainwindow->show();
-#endif
-
-#if 0
-    mainwindow = new MainWindow;
-    mainwindow->setWindowTitle("Not Movable");
-    colorswatch = new ColorSwatch("Green", mainwindow);
-    colorswatch->setMovable(false);
-    colorswatch->setArea(Qt::DockWindowAreaLeft);
-    mainwindow->show();
-#endif
-
-#if 0
-    mainwindow = new MainWindow;
-    mainwindow->setWindowTitle("Not Floatable");
-    colorswatch = new ColorSwatch("Blue", mainwindow);
-    colorswatch->setFloatable(false);
-    colorswatch->setArea(Qt::DockWindowAreaLeft);
-    mainwindow->show();
-#endif
-
-#if 1
-    mainwindow = new MainWindow(0, Qt::WDestructiveClose);
-    mainwindow->setWindowTitle("Allowed on Left and Right");
-//     colorswatch = new ColorSwatch("Yellow", mainwindow);
-//     colorswatch->setArea(Qt::DockWindowAreaLeft);
-//     colorswatch->setAllowedAreas(Qt::DockWindowAreaLeft | Qt::DockWindowAreaRight);
-    mainwindow->show();
-#endif
-
-#if 0
-    mainwindow = new MainWindow;
-    mainwindow->setWindowTitle("Allowed on Top and Bottom");
-    colorswatch = new ColorSwatch("Magenta", mainwindow);
-    colorswatch->setArea(Qt::DockWindowAreaTop);
-    colorswatch->setAllowedAreas(Qt::DockWindowAreaTop | Qt::DockWindowAreaBottom);
-    mainwindow->show();
-#endif
 
     return app.exec();
 }
