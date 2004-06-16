b37c8579ae62fa813de3c98b7b14f1073ab9f4cf
Andy Shaw <andy@trolltech.com>
2004-06-16 12:10:26 +0100

    Build on Windows
    [git-p4: depot-paths = "//depot/qt/main/": change = 143428]

--- src/compat/widgets/q3frame.h
+++ src/compat/widgets/q3frame.h
@@ -19,7 +19,7 @@
 #include "qframe.h"
 #endif
 
-class Q_GUI_EXPORT Q3Frame : public QFrame
+class Q_COMPAT_EXPORT Q3Frame : public QFrame
 {
     Q_OBJECT
     Q_PROPERTY(int margin READ margin WRITE setMargin)
