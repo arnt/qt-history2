6e61b216332615d8f46531aa0b48f4f8b81c47d6
Volker Hilsheimer <vohi@trolltech.com>
2004-06-19 19:15:42 +0100

    Bad code, but at least compile.
    [git-p4: depot-paths = "//depot/qt/main/": change = 143816]

--- src/gui/widgets/qdatetimeedit.cpp
+++ src/gui/widgets/qdatetimeedit.cpp
@@ -1117,8 +1117,8 @@ QCoreVariant QDateTimeEditPrivate::valueForPosition(int pos) const
     }
 
     double diff = (totalDays * percentage);
-    QDate date = minimum.toDate().addDays((long long)diff); // ### hack. There must be a nicer way
-    QTime time = QTime().addMSecs((int)(24 * 3600 * 1000 * (diff - ((double)(long long)diff))));
+    QDate date = minimum.toDate().addDays((Q_LLONG)diff); // ### hack. There must be a nicer way
+    QTime time = QTime().addMSecs((int)(24 * 3600 * 1000 * (diff - ((double)(Q_LLONG)diff))));
     return QCoreVariant(QDateTime(date, time));
 }
 
