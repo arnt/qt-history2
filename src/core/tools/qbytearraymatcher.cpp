0e193054c406cd777c0c7c825a59e409b9e1995d
harald <harald@harald-morpork>
2004-06-21 17:11:49 +0100

    add our new header to all files without a header
    [git-p4: depot-paths = "//depot/qt/main/": change = 143958]

--- src/core/tools/qbytearraymatcher.cpp
+++ src/core/tools/qbytearraymatcher.cpp
@@ -1,3 +1,16 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #include "qbytearraymatcher.h"
 
 static inline void bm_init_skiptable(const uchar *cc, int l, uint *skiptable)
