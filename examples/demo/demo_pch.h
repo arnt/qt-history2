0a98b9216742811381273acbc9498946af9829ab
harald <harald@harald-morpork>
2004-06-22 20:29:20 +0100

    added copyright headers for files without headers. Spared 3rd-party stuff such as qasteroids, kiosk/* and fbm.h
    [git-p4: depot-paths = "//depot/qt/main/": change = 144106]

--- examples/demo/demo_pch.h
+++ examples/demo/demo_pch.h
@@ -1,3 +1,16 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 /*
  * This is a precompiled header file for use in Xcode / Mac GCC /
  * GCC >= 3.4 / VC to greatly speed the building of the Demo example.
