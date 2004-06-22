b219092c4f87d0a2400dc0773f61cbb27201dbb6
harald <harald@harald-morpork>
2004-06-22 21:35:48 +0100

    new license headers
    [git-p4: depot-paths = "//depot/qt/main/": change = 144117]

--- tools/qembed/qembed.cpp
+++ tools/qembed/qembed.cpp
@@ -1,10 +1,10 @@
 /****************************************************************************
 **
-** documentation for any purpose and without fee is hereby granted, provided.
-**
 ** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
 **
-** EDITIONS: UNKNOWN
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
