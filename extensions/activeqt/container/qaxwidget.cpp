0b7cf2f110c12eee06fdfaf30bf0c76bfff44706
harald <harald@harald-morpork>
2004-06-22 21:20:18 +0100

    new license headers
    [git-p4: depot-paths = "//depot/qt/main/": change = 144115]

--- extensions/activeqt/container/qaxwidget.cpp
+++ extensions/activeqt/container/qaxwidget.cpp
@@ -1,11 +1,10 @@
 /****************************************************************************
 **
-** Implementation of the QAxWidget class.
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
 **
-** Copyright (C) 2001-$THISYEAR$ Trolltech AS. All rights reserved.
+** This file is part of the $MODULE$ of the Qt Toolkit.
 **
-** This file is part of the Active Qt integration.
-** EDITIONS: ENTERPRISE
+** $LICENSE$
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
--- extensions/motif/examples/walkthrough/Exm/CommandBP.h
+++ extensions/motif/examples/walkthrough/Exm/CommandBP.h
@@ -1,3 +1,16 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 /* $XConsortium: CommandBP.h /main/4 1995/07/15 20:40:31 drk $ */
 /*
  *  @OPENGROUP_COPYRIGHT@
