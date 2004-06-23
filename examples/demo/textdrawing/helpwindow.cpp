d6227dd37b3409519eb8694d12f6f1ee60f0ecf5
harald <harald@harald-morpork>
2004-06-23 14:04:30 +0100

    license headers
    [git-p4: depot-paths = "//depot/qt/main/": change = 144217]

--- examples/demo/textdrawing/helpwindow.cpp
+++ examples/demo/textdrawing/helpwindow.cpp
@@ -1,12 +1,15 @@
 /****************************************************************************
-** $Id: $
 **
-** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
 **
-** This file is part of an example program for Qt.  This example
-** program may be used, distributed and modified without limitation.
+** This file is part of the $MODULE$ of the Qt Toolkit.
 **
-*****************************************************************************/
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
 
 #include "helpwindow.h"
 #include <qurl.h>
