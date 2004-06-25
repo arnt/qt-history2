c01db0fbc1cdd8a419250011c42ec45b74792c36
Trond Kjernaasen <trond@trolltech.com>
2004-06-25 16:04:13 +0100

    Kill warning under Windows.
    [git-p4: depot-paths = "//depot/qt/main/": change = 144584]

--- demos/arthur/glpainter.cpp
+++ demos/arthur/glpainter.cpp
@@ -168,7 +168,7 @@ void GLWidget::paintEvent(QPaintEvent *)
     glPushMatrix();
     glLoadIdentity();
     glOrtho(-1.1, 1.1, -1.1, 1.1, 0.1, 10);
-    glTranslatef(0, 0, -1.2);
+    glTranslatef(0, 0, -1.2f);
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
     glLoadIdentity();
