10c5703dcf378af4e316dbbd6e1c2dbb18edce91
Brad Hughes <bhughes@trolltech.com>
2004-06-25 09:28:12 +0100

    add a comment about private API
    [git-p4: depot-paths = "//depot/qt/main/": change = 144504]

--- src/gui/widgets/qtoolbar_p.h
+++ src/gui/widgets/qtoolbar_p.h
@@ -11,6 +11,17 @@
 **
 ****************************************************************************/
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #ifndef QTOOLBAR_P_H
 #define QTOOLBAR_P_H
 
