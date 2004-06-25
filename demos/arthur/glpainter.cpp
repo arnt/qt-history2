3c436f00219522dab6f18f454cca5c3026f32d6b
Trond Kjernaasen <trond@trolltech.com>
2004-06-25 15:53:22 +0100

    Mix in some native GL code for fun.
    [git-p4: depot-paths = "//depot/qt/main/": change = 144578]

--- demos/arthur/glpainter.cpp
+++ demos/arthur/glpainter.cpp
@@ -23,12 +23,16 @@ public:
     GLWidget(QWidget *parent);
 
 protected:
+    void initializeGL();
     void paintEvent(QPaintEvent *);
     void timerEvent(QTimerEvent *);
 
 private:
     DemoWidget *dw;
     int step;
+    int rot;
+    GLuint cubeList;
+    GLuint cubeTextureId;
 };
 
 extern void drawPrimitives(DemoWidget *dw, QPainter *p, int count, double distance, int step);
@@ -39,31 +43,147 @@ GLWidget::GLWidget(QWidget *parent)
 {
     dw = qt_cast<DemoWidget *>(parent);
     step = 0;
+    rot = 0;
     Q_ASSERT(dw);
+}
+
+void GLWidget::initializeGL()
+{
+    // cubeList
+    QImage tex;
+    tex.load("cubelogo.png");
+
+    tex = QGLWidget::convertToGLFormat(tex);  // flipped 32bit RGBA
+
+    glGenTextures(1, &cubeTextureId);
+    glBindTexture(GL_TEXTURE_2D, cubeTextureId);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0,
+		 GL_RGBA, GL_UNSIGNED_BYTE, tex.bits());
+
+    cubeList = glGenLists(1);
+    glNewList(cubeList, GL_COMPILE);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+
+    glColor4ub(255, 255, 255, 127);
+    glBegin( GL_QUADS );
+    {
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 0.0, 0.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 1.0, 0.0, 0.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 1.0, 0.0, 0.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 1.0, 0.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 0.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 0.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 0.0, 0.0 );
+
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 0.0, 1.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 0.0, 1.0, 1.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 0.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 1.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 1.0, 0.0, 1.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 0.0, 1.0 );
+
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 0.0, 0.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 0.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 0.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 0.0, 1.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 1.0, 0.0, 0.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 1.0, 0.0, 0.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 0.0, 0.0 );
+
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 0.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 1.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 1.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 1.0, 1.0, 1.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 1.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 0.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 0.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 0.0, 1.0, 0.0 );
+
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 1.0, 0.0, 0.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 1.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 1.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 1.0, 1.0, 1.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 1.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 1.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 1.0, 0.0, 0.0 );
+
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 0.0, 0.0, 0.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 0.0 ); glVertex3f( 0.0, 1.0, 0.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 0.0, 1.0, 1.0 );
+	glTexCoord2f( 0.0, 1.0 ); glVertex3f( 0.0, 1.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 0.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 1.0 ); glVertex3f( 0.0, 0.0, 1.0 );
+	glTexCoord2f( 1.0, 0.0 ); glVertex3f( 0.0, 0.0, 0.0 );
+    }
+    glEnd();
+
+    glEndList();
 }
 
 void GLWidget::timerEvent(QTimerEvent *)
 {
     update();
     ++step;
+    ++rot;
 }
 
 void GLWidget::paintEvent(QPaintEvent *)
 {
-    static int i = 0;
     QPainter p(this);
 
     p.setBrush(QBrush(QPoint(0,0), Qt::white,
  		      QPoint(width(), height()), Qt::black));
     p.drawRect(0, 0, width(), height());
     p.translate(width()/2, height()/2);
-    p.rotate(++i % 360);
-    p.shear(dw->xfunc(i*0.8), dw->yfunc(i*0.8));
+    p.rotate(++rot % 360);
+    p.shear(dw->xfunc(rot*0.5), dw->yfunc(rot*0.5));
     p.translate(-width()/2, -height()/2);
     dw->fillBackground(&p);
     drawShadedCube(dw, &p, 2, 5, step);
     p.resetXForm();
+
     drawPrimitives(dw, &p, 150, 0.3, step);
+    p.setFont(QFont("helvetica", 14, QFont::Bold, true));
+    p.setPen(Qt::white);
+    p.drawText(75, height() - 75/3, "Arthur & OpenGL - together in harmony");
+
+
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+    glViewport(0, 0, 75, 75);
+    glMatrixMode( GL_PROJECTION );
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(-1.1, 1.1, -1.1, 1.1, 0.1, 10);
+    glTranslatef(0, 0, -1.2);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+    glRotatef(rot % 360, 1, 0, 0);
+    glRotatef(rot % 360, 0, 1, 0);
+    glRotatef(rot % 360, 0, 0, 1);
+    glTranslatef(-0.5, -0.5, -0.5);
+
+    glBindTexture(GL_TEXTURE_2D, cubeTextureId);
+    glCallList(cubeList);
+
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glPopAttrib();
 }
 
 GLPainter::GLPainter(QWidget *parent)
