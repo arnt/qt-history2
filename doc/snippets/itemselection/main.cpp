09aedcc8e1590ad430ba4ec07863fcd3de400f5c
dboddie <dboddie@mikaeld-lurven>
2004-07-02 20:33:59 +0100

    Created two more snippet examples, based on a simple, experimental, string-based table model. These just allowed me to make lots of selection-oriented pictures.
    [git-p4: depot-paths = "//depot/qt/main/": change = 145116]

---
+++ doc/snippets/itemselection/main.cpp
@@ -0,0 +1,55 @@
+/****************************************************************************
+**
+** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of an example program for Qt.
+** EDITIONS: NOLIMITS
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+/*
+  main.cpp
+
+  A simple example that shows how selections can be used directly on a model.
+  It shows the result of some selections made using a table view.
+*/
+
+#include <QApplication>
+#include <QItemSelection>
+#include <QItemSelectionModel>
+#include <QTableView>
+
+#include "model.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    TableModel *model = new TableModel(8, 8, &app);
+
+    QTableView *table = new QTableView(0);
+    table->setModel(model);
+
+    QItemSelectionModel *selectionModel = table->selectionModel();
+
+    for (int row = 0; row < 8; row += 2) {
+        for (int column = 0; column < 8; column += 2) {
+            QModelIndex topLeft = model->index(row, column, QModelIndex(),
+                QModelIndex::View);
+            QModelIndex bottomRight = model->index(row+1, column+1,
+                QModelIndex(), QModelIndex::View);
+            QItemSelection selection(topLeft, bottomRight, model);
+            if (((row % 4) ^ (column % 4)) != 0)
+                selectionModel->select(selection, QItemSelectionModel::Select);
+        }
+    }
+
+    table->setWindowTitle("Selected items in a table model");
+    table->show();
+    app.setMainWidget(table);
+
+    return app.exec();
+}
---
+++ doc/snippets/sharedtablemodel/main.cpp
@@ -0,0 +1,59 @@
+/****************************************************************************
+**
+** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of an example program for Qt.
+** EDITIONS: NOLIMITS
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+/*
+  main.cpp
+
+  A simple example that shows how a single model can be shared between
+  multiple views.
+*/
+
+#include <QApplication>
+#include <QHeaderView>
+#include <QItemSelectionModel>
+#include <QTableView>
+
+#include "model.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    TableModel *model = new TableModel(4, 2, &app);
+
+    QTableView *firstTableView = new QTableView(0);
+    QTableView *secondTableView = new QTableView(0);
+
+    firstTableView->setModel(model);
+    secondTableView->setModel(model);
+
+    firstTableView->horizontalHeader()->setModel(model);
+
+    for (int row = 0; row < 4; ++row) {
+        for (int column = 0; column < 2; ++column) {
+            QModelIndex index = model->index(row, column, QModelIndex(),
+                QModelIndex::View);
+            model->setData(index, QAbstractItemModel::EditRole,
+                QVariant(QString("(%1, %2)").arg(row).arg(column)));
+        }
+    }
+
+    secondTableView->setSelectionModel(firstTableView->selectionModel());
+
+    firstTableView->setWindowTitle("First table view");
+    secondTableView->setWindowTitle("Second table view");
+    firstTableView->show();
+    secondTableView->show();
+    app.setMainWidget(firstTableView);
+
+    return app.exec();
+}
