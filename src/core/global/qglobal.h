93c59abc5f2e900a8ef69ce0ab699640c661a81e
Brad Hughes <bhughes@trolltech.com>
2004-07-22 15:15:10 +0100

    enable compat warnings on the intel compiler
    [git-p4: depot-paths = "//depot/qt/main/": change = 146745]

--- src/core/global/qglobal.h
+++ src/core/global/qglobal.h
@@ -646,7 +646,9 @@ typedef int QNoImplicitBoolCast;
 #  endif
 #  if defined(Q_MOC_RUN)
 #    define QT_COMPAT QT_COMPAT
-#  elif defined(Q_CC_GNU) && (__GNUC__ - 0 > 3 || (__GNUC__ - 0 == 3 && __GNUC_MINOR__ - 0 >= 2))
+#  elif (defined(Q_CC_GNU) \
+         && (__GNUC__ - 0 > 3 || (__GNUC__ - 0 == 3 && __GNUC_MINOR__ - 0 >= 2))) \
+    || (defined(Q_CC_INTEL) && (__INTEL_COMPILER - 0 == 800))
 #    define QT_COMPAT __attribute__ ((__deprecated__))
 #  elif defined(Q_CC_MSVC) && (_MSC_VER >= 1300)
 #    define QT_COMPAT __declspec(deprecated)
@@ -680,7 +682,9 @@ typedef int QNoImplicitBoolCast;
 #if defined(QT_DEPRECATED_WARNINGS)
 #  if defined(Q_MOC_RUN)
 #    define QT_DEPRECATED QT_DEPRECATED
-#  elif defined(Q_CC_GNU) && (__GNUC__ - 0 > 3 || (__GNUC__ - 0 == 3 && __GNUC_MINOR__ - 0 >= 2))
+#  elif (defined(Q_CC_GNU) \
+         && (__GNUC__ - 0 > 3 || (__GNUC__ - 0 == 3 && __GNUC_MINOR__ - 0 >= 2))) \
+    || (defined(Q_CC_INTEL) && (__INTEL_COMPILER - 0 == 800))
 #    define QT_DEPRECATED __attribute__ ((__deprecated__))
 #  elif defined(Q_CC_MSVC) && (_MSC_VER >= 1300)
 #    define QT_DEPRECATED __declspec(deprecated)
