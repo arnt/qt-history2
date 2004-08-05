5629097d77368ddfb952b2683d85fd608d9fdfe6
dboddie <dboddie@mikaeld-lurven>
2004-08-05 13:41:06 +0100

    Use non-compat signals with QActions.
    [git-p4: depot-paths = "//depot/qt/main/": change = 147953]

--- examples/gui/application/mainwindow.cpp
+++ examples/gui/application/mainwindow.cpp
@@ -110,52 +110,52 @@ void MainWindow::createActions()
     newAct = new QAction(QIconSet(QPixmap("images/new.png")), tr("&New"), this);
     newAct->setShortcut(tr("Ctrl+N"));
     newAct->setStatusTip(tr("Create a new file"));
-    connect(newAct, SIGNAL(activated()), this, SLOT(newFile()));
+    connect(newAct, SIGNAL(triggered()), this, SLOT(newFile()));
 
     openAct = new QAction(QIconSet(QPixmap("images/open.png")), tr("&Open..."), this);
     openAct->setShortcut(tr("Ctrl+O"));
     openAct->setStatusTip(tr("Open an existing file"));
-    connect(openAct, SIGNAL(activated()), this, SLOT(open()));
+    connect(openAct, SIGNAL(triggered()), this, SLOT(open()));
 
     saveAct = new QAction(QIconSet(QPixmap("images/save.png")), tr("&Save"), this);
     saveAct->setShortcut(tr("Ctrl+S"));
     saveAct->setStatusTip(tr("Save the spreadsheet to disk"));
-    connect(saveAct, SIGNAL(activated()), this, SLOT(save()));
+    connect(saveAct, SIGNAL(triggered()), this, SLOT(save()));
 
     saveAsAct = new QAction(tr("Save &As..."), this);
     saveAsAct->setStatusTip(tr("Save the spreadsheet under a new name"));
-    connect(saveAsAct, SIGNAL(activated()), this, SLOT(saveAs()));
+    connect(saveAsAct, SIGNAL(triggered()), this, SLOT(saveAs()));
 
     exitAct = new QAction(tr("E&xit"), this);
     exitAct->setShortcut(tr("Ctrl+Q"));
     exitAct->setStatusTip(tr("Exit the application"));
-    connect(exitAct, SIGNAL(activated()), qApp, SLOT(quit()));
+    connect(exitAct, SIGNAL(triggered()), qApp, SLOT(quit()));
 
     cutAct = new QAction(QIconSet(QPixmap("images/cut.png")), tr("Cu&t"), this);
     cutAct->setShortcut(tr("Ctrl+X"));
     cutAct->setStatusTip(tr("Cut the current selection's contents to the "
                             "clipboard"));
-    connect(cutAct, SIGNAL(activated()), textEdit, SLOT(cut()));
+    connect(cutAct, SIGNAL(triggered()), textEdit, SLOT(cut()));
 
     copyAct = new QAction(QIconSet(QPixmap("images/copy.png")), tr("&Copy"), this);
     copyAct->setShortcut(tr("Ctrl+C"));
     copyAct->setStatusTip(tr("Copy the current selection's contents to the "
                              "clipboard"));
-    connect(copyAct, SIGNAL(activated()), textEdit, SLOT(copy()));
+    connect(copyAct, SIGNAL(triggered()), textEdit, SLOT(copy()));
 
     pasteAct = new QAction(QIconSet(QPixmap("images/paste.png")), tr("&Paste"), this);
     pasteAct->setShortcut(tr("Ctrl+V"));
     pasteAct->setStatusTip(tr("Paste the clipboard's contents into the current "
                               "selection"));
-    connect(pasteAct, SIGNAL(activated()), textEdit, SLOT(paste()));
+    connect(pasteAct, SIGNAL(triggered()), textEdit, SLOT(paste()));
 
     aboutAct = new QAction(tr("&About"), this);
     aboutAct->setStatusTip(tr("Show the application's About box"));
-    connect(aboutAct, SIGNAL(activated()), this, SLOT(about()));
+    connect(aboutAct, SIGNAL(triggered()), this, SLOT(about()));
 
     aboutQtAct = new QAction(tr("About &Qt"), this);
     aboutQtAct->setStatusTip(tr("Show the Qt library's About box"));
-    connect(aboutQtAct, SIGNAL(activated()), qApp, SLOT(aboutQt()));
+    connect(aboutQtAct, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
 }
 
 void MainWindow::createMenus()
