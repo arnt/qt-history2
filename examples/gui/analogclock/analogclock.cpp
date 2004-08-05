29697dc7833c96af9ccea09fd0d9158496760d95
Jasmin Blanchette <jasmin@trolltech.com>
2004-08-05 16:42:35 +0100

    Minor changes to AnalogClock example.
    [git-p4: depot-paths = "//depot/qt/main/": change = 147992]

--- examples/gui/analogclock/analogclock.cpp
+++ examples/gui/analogclock/analogclock.cpp
@@ -6,12 +6,11 @@
 AnalogClock::AnalogClock(QWidget *parent)
     : QWidget(parent)
 {
-    setWindowTitle(tr("Analog Clock"));
-
     QTimer *timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(update()));
     timer->start(5000);
 
+    setWindowTitle(tr("Analog Clock"));
     resize(100, 100);
 }
 
@@ -20,14 +19,13 @@ void AnalogClock::paintEvent(QPaintEvent *)
     static QCOORD hourHand[8] = { 2, 0, 0, 2, -2, 0, 0, -25 };
     static QCOORD minuteHand[8] = { 1, 0, 0, 1, -1, 0, 0, -40 };
 
+    int side = qMin(width(), height());
+
     QPainter painter(this);
     painter.setWindow(-50, -50, 100, 100);
-    painter.setBrush(palette().foreground());
-
-    for (int i = 0; i < 12; i++) {
-        painter.drawLine(44, 0, 46, 0);
-        painter.rotate(30);
-    }
+    painter.setViewport((width() - side) / 2, (height() - side) / 2,
+                        side, side);
+    painter.setBrush(painter.pen().color());
 
     QTime time = QTime::currentTime();
 
@@ -40,4 +38,9 @@ void AnalogClock::paintEvent(QPaintEvent *)
     painter.rotate(6 * time.minute());
     painter.drawConvexPolygon(QPointArray(4, minuteHand));
     painter.restore();
+
+    for (int i = 0; i < 12; i++) {
+        painter.drawLine(44, 0, 46, 0);
+        painter.rotate(30);
+    }
 }
