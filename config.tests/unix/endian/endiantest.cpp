21bd15ae79b1ba41dfea75c9e28d6a59f2883b67
Dimitri Papadopoulos <dimitri@trolltech.com>
2004-08-10 22:01:56 +0100

    p4i integration Integrate 141203 from 3 to main: If test fail, choose LittleEndian instead of BigEndian. (This normally only happens when cross-compiling, and most of our embedded customers use little-endian hardware.) Discussed with Brad, reviewed by Marius 1.
    [git-p4: depot-paths = "//depot/qt/main/": change = 148433]

--- config.tests/unix/endian/endiantest.cpp
+++ config.tests/unix/endian/endiantest.cpp
@@ -3,6 +3,6 @@ int main(int, char **)
     unsigned char bytes[] = { 0x01, 0x02, 0x03, 0x04 };
     unsigned int *integer = (unsigned int *) bytes;
     return (*integer == 0x01020304 ?
-	    1 : // big endian
-	    0);  // little endian
+	    0 : // big endian
+	    1);  // little endian
 }
