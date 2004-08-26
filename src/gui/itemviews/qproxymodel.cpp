b95ffff841e417d3a8f4a60cda2c9719e1554855
mmonsen <mmonsen@trolltech.com>
2004-08-26 21:17:16 +0100

    Add QProxyModel.    The idea is that you can subclass this model and use it for filtering and sorting.
    [git-p4: depot-paths = "//depot/qt/main/": change = 150332]

---
+++ src/gui/itemviews/qproxymodel.cpp
@@ -0,0 +1,236 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "qproxymodel.h"
+#include <private/qabstractitemmodel_p.h>
+#include <qsize.h>
+
+#define d d_func()
+#define q q_func()
+
+class QEmptyModel : public QAbstractItemModel
+{
+public:
+    QEmptyModel(QObject *parent = 0) : QAbstractItemModel(parent) {}
+
+    QModelIndex index(int, int, const QModelIndex &) const { return QModelIndex::Null; }
+    QModelIndex parent(const QModelIndex &) const { return QModelIndex::Null; }
+    int rowCount(const QModelIndex &) const { return 0; }
+    int columnCount(const QModelIndex &) const { return 0; }
+    QVariant data(const QModelIndex &, int) const { return QVariant(); }
+};
+
+class QProxyModelPrivate : private QAbstractItemModelPrivate
+{
+    Q_DECLARE_PUBLIC(QProxyModel)
+
+public:
+    QProxyModelPrivate() : QAbstractItemModelPrivate(), model(0) {}
+    QAbstractItemModel *model;
+    QEmptyModel empty;
+};
+
+QProxyModel::QProxyModel(QObject *parent)
+    : QAbstractItemModel(*new QProxyModelPrivate, parent)
+{
+    setModel(&d->empty);
+}
+
+QProxyModel::QProxyModel(QProxyModelPrivate &dd, QObject *parent)
+    : QAbstractItemModel(dd, parent)
+{
+    setModel(&d->empty);
+}
+
+QProxyModel::~QProxyModel()
+{
+}
+
+void QProxyModel::setModel(QAbstractItemModel *model)
+{
+    if (d->model) {
+        disconnect(d->model,
+                   SIGNAL(dataChanged(const QModelIndex&, const QModelIndex&)),
+                   this,
+                   SIGNAL(dataChanged(const QModelIndex&, const QModelIndex&)));
+        disconnect(d->model,
+                   SIGNAL(headerDataChanged(Qt::Orientation, int, int)),
+                   this,
+                   SIGNAL(headerDataChanged(Qt::Orientation, int, int)));
+        disconnect(d->model,
+                   SIGNAL(rowsInserted(const QModelIndex&, int, int)),
+                   this,
+                   SIGNAL(rowsInserted(const QModelIndex&, int, int)));
+        disconnect(d->model,
+                   SIGNAL(rowsRemoved(const QModelIndex&, int, int)),
+                   this,
+                   SIGNAL(rowsRemoved(const QModelIndex&, int, int)));
+        disconnect(d->model,
+                   SIGNAL(columnsInserted(const QModelIndex&, int, int)),
+                   this,
+                   SIGNAL(columnsInserted(const QModelIndex&, int, int)));
+        disconnect(d->model,
+                   SIGNAL(columnsRemoved(const QModelIndex&, int, int)),
+                   this,
+                   SIGNAL(columnsRemoved(const QModelIndex&, int, int)));
+        disconnect(d->model, SIGNAL(reset()), this, SIGNAL(reset()));
+    }
+
+    if (model)
+        d->model = model;
+    else
+        d->model = &d->empty;
+
+    connect(d->model,
+            SIGNAL(dataChanged(const QModelIndex&, const QModelIndex&)),
+            this,
+            SIGNAL(dataChanged(const QModelIndex&, const QModelIndex &)));
+    connect(d->model,
+            SIGNAL(headerDataChanged(Qt::Orientation, int, int)),
+            this,
+            SIGNAL(headerDataChanged(Qt::Orientation, int, int)));
+    connect(d->model,
+            SIGNAL(rowsInserted(const QModelIndex&, int, int)),
+            this,
+            SIGNAL(rowsInserted(const QModelIndex&, int, int)));
+    connect(d->model,
+            SIGNAL(rowsRemoved(const QModelIndex&, int, int)),
+            this,
+            SIGNAL(rowsRemoved(const QModelIndex&, int, int)));
+    connect(d->model,
+            SIGNAL(columnsInserted(const QModelIndex&, int, int)),
+            this,
+            SIGNAL(columnsInserted(const QModelIndex&, int, int)));
+    connect(d->model,
+            SIGNAL(columnsRemoved(const QModelIndex&, int, int)),
+            this,
+            SIGNAL(columnsRemoved(const QModelIndex&, int, int)));
+    connect(d->model, SIGNAL(reset()), this, SIGNAL(reset()));
+}
+
+QAbstractItemModel *QProxyModel::model()
+{
+    return d->model;
+}
+
+QModelIndex QProxyModel::index(int row, int column, const QModelIndex &parent) const
+{
+    return d->model->index(row, column, parent);    
+}
+
+QModelIndex QProxyModel::parent(const QModelIndex &child) const
+{
+    return d->model->parent(child);
+}
+
+int QProxyModel::rowCount(const QModelIndex &parent) const
+{
+    return d->model->rowCount(parent);
+}
+
+int QProxyModel::columnCount(const QModelIndex &parent) const
+{
+    return d->model->columnCount(parent);
+}
+
+bool QProxyModel::hasChildren(const QModelIndex &parent) const
+{
+    return d->model->hasChildren(parent);
+}
+
+bool QProxyModel::canDecode(QMimeSource *src) const
+{
+    return d->model->canDecode(src);
+}
+
+bool QProxyModel::decode(QDropEvent *e, const QModelIndex &parent)
+{
+    return d->model->decode(e, parent);
+}
+
+QDragObject *QProxyModel::dragObject(const QModelIndexList &indexes, QWidget *dragSource)
+{
+    return d->model->dragObject(indexes, dragSource);
+}
+
+QVariant QProxyModel::data(const QModelIndex &index, int role) const
+{
+    return d->model->data(index, role);
+}
+
+bool QProxyModel::setData(const QModelIndex &index, int role, const QVariant &value)
+{
+    return d->model->setData(index, role, value);
+}
+
+QVariant QProxyModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    return d->model->headerData(section, orientation, role);
+}
+
+bool QProxyModel::setHeaderData(int section, Qt::Orientation orientation, int role, const QVariant &value)
+{
+    return d->model->setHeaderData(section, orientation, role, value);
+}
+
+bool QProxyModel::insertRows(int row, const QModelIndex &parent, int count)
+{
+    return d->model->insertRows(row, parent, count);
+}
+
+bool QProxyModel::insertColumns(int column, const QModelIndex &parent, int count)
+{
+    return d->model->insertColumns(column, parent, count);
+}
+
+void QProxyModel::fetchMore(const QModelIndex &parent)
+{
+    d->model->fetchMore(parent);
+}
+
+QAbstractItemModel::ItemFlags QProxyModel::flags(const QModelIndex &index) const
+{
+    return d->model->flags(index);
+}
+
+bool QProxyModel::isSortable() const
+{
+    return true;
+}
+
+void QProxyModel::sort(int column, const QModelIndex &parent, Qt::SortOrder order)
+{
+    d->model->sort(column, parent, order);
+}
+
+bool QProxyModel::equal(const QModelIndex &left, const QModelIndex &right) const
+{
+    return d->model->equal(left, right);
+}
+
+bool QProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
+{
+    return d->model->lessThan(left, right);
+}
+
+QModelIndexList QProxyModel::match(const QModelIndex &start, int role,
+                                   const QVariant &value,
+                                   int hits, QAbstractItemModel::MatchFlags flags) const
+{
+    return d->model->match(start, role, value, hits, flags);
+}
+
+QSize QProxyModel::span(const QModelIndex &index) const
+{
+    return d->model->span(index);
+}
---
+++ src/gui/itemviews/qproxymodel.h
@@ -0,0 +1,73 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QPROXYMODEL_H
+#define QPROXYMODEL_H
+
+#include <qabstractitemmodel.h>
+
+class QProxyModelPrivate;
+
+class Q_GUI_EXPORT QProxyModel : public QAbstractItemModel
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(QProxyModel);
+
+public:
+    QProxyModel(QObject *parent = 0);
+    ~QProxyModel();
+
+    void setModel(QAbstractItemModel *model);
+    QAbstractItemModel *model();
+
+    // implementing model interface
+
+    QModelIndex index(int row, int column, const QModelIndex &parent) const;
+    QModelIndex parent(const QModelIndex &child) const;
+
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    bool hasChildren(const QModelIndex &parent) const;
+
+    bool canDecode(QMimeSource *src) const;
+    bool decode(QDropEvent *e, const QModelIndex &parent);
+    QDragObject *dragObject(const QModelIndexList &indexes, QWidget *dragSource);
+
+    QVariant data(const QModelIndex &index, int role) const;
+    bool setData(const QModelIndex &index, int role, const QVariant &value);
+
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    bool setHeaderData(int section, Qt::Orientation orientation, int role, const QVariant &value);
+
+    bool insertRows(int row, const QModelIndex &parent, int count);
+    bool insertColumns(int column, const QModelIndex &parent, int count);
+
+    void fetchMore(const QModelIndex &parent);
+    QAbstractItemModel::ItemFlags flags(const QModelIndex &index) const;
+
+    bool isSortable() const;
+    void sort(int column, const QModelIndex &parent, Qt::SortOrder order);
+
+    bool equal(const QModelIndex &left, const QModelIndex &right) const;
+    bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
+
+    QModelIndexList match(const QModelIndex &start, int role, const QVariant &value,
+                          int hits, QAbstractItemModel::MatchFlags flags) const;
+
+    QSize span(const QModelIndex &index) const;
+
+protected:
+    QProxyModel(QProxyModelPrivate &, QObject *parent = 0);
+};
+
+#endif
