c1d4e6c4651fb04ade2f1d519e208694030a320b
Jasmin Blanchette <jasmin@trolltech.com>
2004-09-08 14:45:12 +0100

    Forgot to add these two example files.
    [git-p4: depot-paths = "//depot/qt/main/": change = 152467]

---
+++ examples/network/ftp/ftpwindow.cpp
@@ -0,0 +1,213 @@
+#include <QtGui>
+#include <QtNetwork>
+
+#include "ftpwindow.h"
+
+FtpWindow::FtpWindow(QWidget *parent)
+    : QDialog(parent)
+{
+    ftpServerLabel = new QLabel(tr("Ftp &server:"), this);
+    ftpServerLineEdit = new QLineEdit("ftp.trolltech.com", this);
+    ftpServerLabel->setBuddy(ftpServerLineEdit);
+
+    statusLabel = new QLabel(tr("Please enter the name of an FTP server."),
+                             this);
+
+    fileList = new QListWidget(this);
+
+    connectButton = new QPushButton(tr("Connect"), this);
+    connectButton->setDefault(true);
+
+    downloadButton = new QPushButton(tr("Download"), this);
+    downloadButton->setEnabled(false);
+
+    cdToParentButton = new QPushButton(this);
+    cdToParentButton->setIcon(QPixmap(":/images/cdtoparent.png"));
+    cdToParentButton->setEnabled(false);
+
+    quitButton = new QPushButton(tr("Quit"), this);
+
+    ftp = new QFtp(this);
+
+    progressDialog = new QProgressDialog(this);
+
+    connect(ftpServerLineEdit, SIGNAL(textChanged(const QString &)),
+            this, SLOT(enableConnectButton()));
+    connect(fileList, SIGNAL(doubleClicked(QListWidgetItem *, int)),
+            this, SLOT(processItem(QListWidgetItem *)));
+    connect(fileList, SIGNAL(returnPressed(QListWidgetItem *)),
+            this, SLOT(processItem(QListWidgetItem *)));
+    connect(fileList, SIGNAL(selectionChanged()),
+            this, SLOT(enableDownloadButton()));
+    connect(ftp, SIGNAL(commandFinished(int, bool)),
+            this, SLOT(ftpCommandFinished(int, bool)));
+    connect(ftp, SIGNAL(listInfo(const QUrlInfo &)),
+            this, SLOT(addToList(const QUrlInfo &)));
+    connect(ftp, SIGNAL(dataTransferProgress(Q_LONGLONG, Q_LONGLONG)),
+            this, SLOT(updateDataTransferProgress(Q_LONGLONG, Q_LONGLONG)));
+    connect(progressDialog, SIGNAL(canceled()), this, SLOT(cancelDownload()));
+    connect(connectButton, SIGNAL(clicked()), this, SLOT(connectToFtpServer()));
+    connect(cdToParentButton, SIGNAL(clicked()), this, SLOT(cdToParent()));
+    connect(downloadButton, SIGNAL(clicked()), this, SLOT(downloadFile()));
+    connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
+
+    QHBoxLayout *topLayout = new QHBoxLayout;
+    topLayout->addWidget(ftpServerLabel);
+    topLayout->addWidget(ftpServerLineEdit);
+    topLayout->addStretch(1);
+    topLayout->addWidget(cdToParentButton);
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+    buttonLayout->addStretch(1);
+    buttonLayout->addWidget(downloadButton);
+    buttonLayout->addWidget(connectButton);
+    buttonLayout->addWidget(quitButton);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addLayout(topLayout);
+    mainLayout->addWidget(fileList);
+    mainLayout->addWidget(statusLabel);
+    mainLayout->addLayout(buttonLayout);
+
+    setWindowTitle(tr("FTP"));
+}
+
+void FtpWindow::connectToFtpServer()
+{
+    ftp->connectToHost(ftpServerLineEdit->text());
+    ftp->list();
+    statusLabel->setText(tr("Connecting to FTP server %1.")
+                         .arg(ftpServerLineEdit->text()));
+}
+
+void FtpWindow::downloadFile()
+{
+    QString fileName = fileList->currentItem()->text();
+
+    if (QFile::exists(fileName)) {
+        QMessageBox::information(this, tr("Ftp"),
+                                 tr("You already have a file called %1.")
+                                 .arg(fileName));
+        return;
+    }
+
+    file = new QFile(fileName);
+    if (!file->open(QIODevice::WriteOnly)) {
+        QMessageBox::information(this, tr("Ftp"),
+                                 tr("Unable to save the file %1: %2.")
+                                 .arg(fileName).arg(file->errorString()));
+        delete file;
+        return;
+    }
+
+    ftp->get(fileList->currentItem()->text(), file);
+
+    progressDialog->setLabel(new QLabel(tr("Downloading %1").arg(fileName), this));
+    progressDialog->show();
+    downloadButton->setEnabled(false);
+}
+
+void FtpWindow::cancelDownload()
+{
+    ftp->abort();
+}
+
+void FtpWindow::ftpCommandFinished(int /* command */, bool error)
+{
+    if (ftp->currentCommand() == QFtp::ConnectToHost) {
+        if (error) {
+            QMessageBox::information(this, tr("Ftp"),
+                                     tr("Unable to connect to the FTP server "
+                                        "at %1. Please check that the host "
+                                        "name is correct.")
+                                     .arg(ftpServerLineEdit->text()));
+            return;
+        }
+
+        statusLabel->setText(tr("Connected to %1.").arg(ftpServerLineEdit->text()));
+        fileList->setFocus();
+        connectButton->setEnabled(false);
+        downloadButton->setDefault(true);
+        return;
+    }
+
+    if (ftp->currentCommand() == QFtp::Get) {
+        if (error) {
+            statusLabel->setText(tr("Canceled download of %1.").arg(file->fileName()));
+            file->close();
+            file->remove();
+            delete file;
+            enableDownloadButton();
+            return;
+        }
+
+        statusLabel->setText(tr("Downloaded %1.").arg(file->fileName()));
+        file->close();
+        delete file;
+    }
+
+    if (ftp->currentCommand() == QFtp::List) {
+        if (isDirectory.isEmpty()) {
+            fileList->appendItem(tr("<empty>"));
+            fileList->setEnabled(false);
+        }
+    }
+}
+
+void FtpWindow::addToList(const QUrlInfo &urlInfo)
+{
+    QListWidgetItem *item = new QListWidgetItem;
+    item->setText(urlInfo.name());
+    QPixmap pixmap(urlInfo.isDir() ? ":/images/dir.png" : ":/images/file.png");
+    item->setIcon(pixmap);
+
+    isDirectory[urlInfo.name()] = urlInfo.isDir();
+    fileList->appendItem(item);
+    if (!fileList->currentItem()) {
+        fileList->setCurrentItem(fileList->item(0));
+        fileList->setEnabled(true);
+    }
+}
+
+void FtpWindow::processItem(QListWidgetItem *item)
+{
+    QString name = item->text();
+    if (isDirectory.value(name)) {
+        fileList->clear();
+        isDirectory.clear();
+        currentPath += "/" + name;
+        ftp->cd(name);
+        ftp->list();
+        cdToParentButton->setEnabled(true);
+        return;
+    }
+}
+
+void FtpWindow::cdToParent()
+{
+    fileList->clear();
+    isDirectory.clear();
+    currentPath = currentPath.left(currentPath.lastIndexOf('/'));
+    ftp->cd(currentPath);
+    ftp->list();
+
+    if (currentPath.isEmpty())
+        cdToParentButton->setEnabled(false);
+}
+
+void FtpWindow::updateDataTransferProgress(Q_LONGLONG readBytes,
+                                           Q_LONGLONG totalBytes)
+{
+    progressDialog->setTotalSteps(totalBytes);
+    progressDialog->setProgress(readBytes);
+}
+
+void FtpWindow::enableConnectButton()
+{
+    connectButton->setEnabled(!ftpServerLineEdit->text().isEmpty());
+}
+
+void FtpWindow::enableDownloadButton()
+{
+    downloadButton->setEnabled(!isDirectory.value(fileList->currentItem()->text()));
+}
---
+++ examples/network/ftp/ftpwindow.h
@@ -0,0 +1,55 @@
+#ifndef FTPWINDOW_H
+#define FTPWINDOW_H
+
+#include <QDialog>
+#include <QMap>
+
+class QFile;
+class QFtp;
+class QLabel;
+class QLineEdit;
+class QListWidget;
+class QListWidgetItem;
+class QProgressDialog;
+class QPushButton;
+class QUrlInfo;
+
+class FtpWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    FtpWindow(QWidget *parent = 0);
+
+private slots:
+    void connectToFtpServer();
+    void downloadFile();
+    void cancelDownload();
+
+    void ftpCommandFinished(int id, bool error);
+    void addToList(const QUrlInfo &urlInfo);
+    void processItem(QListWidgetItem *item);
+    void cdToParent();
+    void updateDataTransferProgress(Q_LONGLONG readBytes, Q_LONGLONG totalBytes);
+    void enableConnectButton();
+    void enableDownloadButton();
+
+private:
+    QLabel *ftpServerLabel;
+    QLineEdit *ftpServerLineEdit;
+    QLabel *statusLabel;
+    QListWidget *fileList;
+    QPushButton *quitButton;
+    QPushButton *connectButton;
+    QPushButton *downloadButton;
+    QPushButton *cdToParentButton;
+    QProgressDialog *progressDialog;
+
+    QMap<QString, bool> isDirectory;
+    QString currentPath;
+    QFtp *ftp;
+    QFile *file;
+};
+
+#endif
+
