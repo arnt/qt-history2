4fc1213e7438aed1ddfcf9111a3bcb666e110be1
espenr <espenr@deimos>
2004-09-09 17:10:26 +0100

    add context menu with sum and clear
    [git-p4: depot-paths = "//depot/qt/main/": change = 152757]

--- demos/spreadsheet/main.cpp
+++ demos/spreadsheet/main.cpp
@@ -2,6 +2,26 @@
 #include <qtablewidget.h>
 #include <qstatusbar.h>
 #include <qmainwindow.h>
+#include <qmenu.h>
+
+class SpreadSheetTable : public QTableWidget
+{
+
+    Q_OBJECT
+
+public:
+    SpreadSheetTable(int rows, int columns, QWidget *parent) :
+        QTableWidget(rows, columns, parent) {}
+
+    QItemSelectionModel::SelectionFlags selectionCommand(Qt::ButtonState state,
+                                                         const QModelIndex &index,
+                                                         QEvent::Type type,
+                                                         Qt::Key key) const {
+        if (state & Qt::RightButton || state & Qt::MidButton)
+            return QItemSelectionModel::NoUpdate;
+        return QTableWidget::selectionCommand(state, index, type, key);
+    }
+};
 
 class SpreadSheetItem : public QTableWidgetItem
 {
@@ -48,7 +68,7 @@ public:
     QVariant data(int role) const {
         if (role == QAbstractItemModel::DisplayRole)
             return display();
-        if (role == QAbstractItemModel::EditRole)
+        if (role == QAbstractItemModel::EditRole || role == QAbstractItemModel::StatusTipRole)
             return formula;
 
         QString t = text();
@@ -77,6 +97,11 @@ public:
             return QFont("Arial", 14, QFont::Normal);
         }
 
+        // background color
+        if (role == QAbstractItemModel::BackgroundColorRole && formula.startsWith("sum")) {
+                return QColor(Qt::lightGray);
+        }
+
         return QTableWidgetItem::data(role);
     }
 
@@ -88,6 +113,7 @@ public:
         }
         QTableWidgetItem::setData(role, value);
     }
+
 
 private:
     QString formula;
@@ -105,29 +131,58 @@ public:
     static const int numColumns = 9;
 
     SpreadSheet(QWidget *parent = 0) : QMainWindow(parent) {
-        table = new QTableWidget(numRows, numColumns, this);
+        table = new SpreadSheetTable(numRows, numColumns, this);
         for (int r=0; r<numRows; ++r)
             for (int c=0; c<numColumns; ++c)
                 table->setItem(r, c, new SpreadSheetItem(table));
         setCentralWidget(table);
+        statusBar();
         connect(table, SIGNAL(currentChanged(QTableWidgetItem*, QTableWidgetItem*)),
                 this, SLOT(updateStatus(QTableWidgetItem*)));
         connect(table, SIGNAL(itemChanged(QTableWidgetItem*)),
                 this, SLOT(updateStatus(QTableWidgetItem*)));
-
-        statusBar();
+        connect(table, SIGNAL(aboutToShowContextMenu(QMenu*, QTableWidgetItem*)),
+                this, SLOT(contextActions(QMenu*, QTableWidgetItem*)));
     }
 
 public slots:
     void updateStatus(QTableWidgetItem *item) {
         if (item && item == table->currentItem())
-            statusBar()->message("Content: " + item->data(QAbstractItemModel::EditRole).toString(), 1000);
+            statusBar()->message(item->data(QAbstractItemModel::StatusTipRole).toString(), 1000);
         else
             qDebug("no item!");
     }
 
+    void contextActions(QMenu *menu, QTableWidgetItem *item) {
+        if (table->selectedItems().count() > 0) {
+            connect(menu->addAction("Sum"), SIGNAL(triggered()), this, SLOT(sum()));
+            menu->addSeparator();
+            connect(menu->addAction("Clear"), SIGNAL(triggered()), this, SLOT(clear()));
+        }
+        contextItem = item;
+    }
+
+    void sum() {
+        QList<QTableWidgetItem*> selected = table->selectedItems();
+        QTableWidgetItem *first = selected.first();
+        QTableWidgetItem *last = selected.last();
+        if (first && last && contextItem)
+            contextItem->setText(QString("sum %1%2 %3%4").
+                                 arg((table->row(first) + 1)).
+                                 arg((table->column(first) + 1)).
+                                 arg((table->row(last) + 1)).
+                                 arg((table->column(last) +1)));
+    }
+
+    void clear() {
+        QList<QTableWidgetItem*> selected = table->selectedItems();
+        foreach (QTableWidgetItem *i, selected)
+            i->setText(QString::null);
+    }
+
 private:
     QTableWidget *table;
+    QTableWidgetItem *contextItem;
 };
 
 
