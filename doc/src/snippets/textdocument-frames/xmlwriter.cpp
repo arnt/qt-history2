cc3a40db0ecd2e3fe223dfb1c6c59c2316ea47a8
dboddie <dboddie@mikaeld-lurven>
2004-09-13 14:24:52 +0100

    A snippet to show frame usage.
    [git-p4: depot-paths = "//depot/qt/main/": change = 153084]

---
+++ doc/src/snippets/textdocument-frames/xmlwriter.cpp
@@ -0,0 +1,71 @@
+#include <QtGui>
+
+#include "xmlwriter.h"
+
+QDomDocument *XmlWriter::toXml()
+{
+    QDomImplementation implementation;
+    QDomDocumentType docType = implementation.createDocumentType(
+        "scribe-document", "scribe", "www.trolltech.com/scribe");
+
+    document = new QDomDocument(docType);
+
+    // ### This processing instruction is required to ensure that any kind
+    // of encoding is given when the document is written.
+    QDomProcessingInstruction process = document->createProcessingInstruction(
+        "xml", "version=\"1.0\" encoding=\"utf-8\"");
+    document->appendChild(process);
+
+    QDomElement documentElement = document->createElement("document");
+    document->appendChild(documentElement);
+
+    QTextFrame *root = textDocument->rootFrame();
+    
+    if (root)
+        createItems(documentElement, root);
+
+    return document;
+}
+
+void XmlWriter::createItems(QDomElement &parent, const QTextBlock &block)
+{
+    QDomElement blockElement = document->createElement("block");
+    blockElement.setAttribute("position", block.position());
+    blockElement.setAttribute("length", block.length());
+    parent.appendChild(blockElement);
+
+    QTextBlock::iterator it;
+    for (it = block.begin(); !(it.atEnd()); ++it) {
+        QTextFragment fragment = it.fragment();
+
+        if (fragment.isValid()) {
+            QDomElement fragmentElement = document->createElement("fragment");
+            blockElement.appendChild(fragmentElement);
+
+            fragmentElement.setAttribute("length", fragment.length());
+            QDomText fragmentText = document->createTextNode(fragment.text());
+
+            fragmentElement.appendChild(fragmentText);
+        }
+    }
+}
+
+void XmlWriter::createItems(QDomElement &parent, QTextFrame *frame)
+{
+    QDomElement frameElement = document->createElement("frame");
+    frameElement.setAttribute("begin", frame->firstPosition());
+    frameElement.setAttribute("end", frame->lastPosition());
+    parent.appendChild(frameElement);
+
+    QTextFrame::iterator it;
+    for (it = frame->begin(); !(it.atEnd()); ++it) {
+
+        QTextFrame *childFrame = it.currentFrame();
+        QTextBlock childBlock = it.currentBlock();
+
+        if (childFrame)
+            createItems(frameElement, childFrame);
+        else if (childBlock.isValid())
+            createItems(frameElement, childBlock);
+    }
+}
