92571758bc37f4313fb7e6c35d9049f390408eea
espenr <espenr@deimos>
2004-09-13 16:35:24 +0100

    added content for the cells added tooltips set col/row resize modes remove qdebug make a2 and A2 both work
    [git-p4: depot-paths = "//depot/qt/main/": change = 153098]

--- demos/spreadsheet/main.cpp
+++ demos/spreadsheet/main.cpp
@@ -13,6 +13,7 @@
 
 #include <qapplication.h>
 #include <qtablewidget.h>
+#include <qheaderview.h>
 #include <qstatusbar.h>
 #include <qmainwindow.h>
 #include <qmenu.h>
@@ -45,9 +46,9 @@ public:
             return formula;
 
         QString op = list.at(0).toLower();
-        int c1 = list.at(1).at(0).ascii() - 'A';
+        int c1 = list.at(1).at(0).toUpper().ascii() - 'A';
         int r1 = list.at(1).at(1).digitValue() - 1;
-        int c2 = list.at(2).at(0).ascii() - 'A';
+        int c2 = list.at(2).at(0).toUpper().ascii() - 'A';
         int r2 = list.at(2).at(1).digitValue() - 1;
         QTableWidgetItem *start = table->item(r1, c1);
         QTableWidgetItem *end = table->item(r2, c2);
@@ -81,6 +82,27 @@ public:
             return display();
         if (role == QAbstractItemModel::EditRole || role == QAbstractItemModel::StatusTipRole)
             return formula;
+        if (role == QAbstractItemModel::ToolTipRole) {
+            switch (table->column(this)) {
+            case 0:
+                return "This column shows the purchased item/service";
+                break;
+            case 1:
+                return "This column shows the price of the purchase";
+                break;
+            case 2:
+                return "This column shows the currency";
+                break;
+            case 3:
+                return "This column shows the exchange rate to NOK";
+                break;
+            case 4:
+                return "This column shows the expenses in NOK";
+                break;
+            default:
+                return "Demo of a travel expense spreadsheet application";
+            }
+        }
 
         QString t = text();
         bool numberOk;
@@ -125,7 +147,6 @@ public:
         QTableWidgetItem::setData(role, value);
     }
 
-
 private:
     QString formula;
     QTableWidget *table;
@@ -150,6 +171,62 @@ public:
                 table->setItem(r, c, new SpreadSheetItem(table));
             }
         }
+        table->verticalHeader()->setResizeMode(QHeaderView::Stretch);
+        table->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
+        // column 0
+        table->horizontalHeader()->setResizeMode(QHeaderView::Custom, 0);
+        table->item(0,0)->setText("Item");
+        table->item(1,0)->setText("AirportBus");
+        table->item(2,0)->setText("Flight (Munich)");
+        table->item(3,0)->setText("Lunch");
+        table->item(4,0)->setText("Flight (LA)");
+        table->item(5,0)->setText("Taxi");
+        table->item(6,0)->setText("Dinner");
+        table->item(7,0)->setText("Hotel");
+        table->item(8,0)->setText("Flight (Oslo)");
+        table->item(9,0)->setText("Total:");
+        // column 1
+        table->item(0,1)->setText("Price");
+        table->item(1,1)->setText("150");
+        table->item(2,1)->setText("2350");
+        table->item(3,1)->setText("-14");
+        table->item(4,1)->setText("980");
+        table->item(5,1)->setText("5");
+        table->item(6,1)->setText("120");
+        table->item(7,1)->setText("300");
+        table->item(8,1)->setText("1240");
+        // column 2
+        table->item(0,2)->setText("Currency");
+        table->item(1,2)->setText("NOK");
+        table->item(2,2)->setText("NOK");
+        table->item(3,2)->setText("EUR");
+        table->item(4,2)->setText("EUR");
+        table->item(5,2)->setText("USD");
+        table->item(6,2)->setText("USD");
+        table->item(7,2)->setText("USD");
+        table->item(8,2)->setText("USD");
+        // column 3
+        table->item(0,3)->setText("Ex.Rate");
+        table->item(1,3)->setText("1");
+        table->item(2,3)->setText("1");
+        table->item(3,3)->setText("8");
+        table->item(4,3)->setText("8");
+        table->item(5,3)->setText("7");
+        table->item(6,3)->setText("7");
+        table->item(7,3)->setText("7");
+        table->item(8,3)->setText("7");
+        // column 4
+        table->item(0,4)->setText("NOK");
+        table->item(1,4)->setText("* B2 D2");
+        table->item(2,4)->setText("* B3 D3");
+        table->item(3,4)->setText("* B4 D4");
+        table->item(4,4)->setText("* B5 D5");
+        table->item(5,4)->setText("* B6 D6");
+        table->item(6,4)->setText("* B7 D7");
+        table->item(7,4)->setText("* B8 D8");
+        table->item(8,4)->setText("* B9 D9");
+        table->item(9,4)->setText("sum E2 E9");
+
         setCentralWidget(table);
         statusBar();
         connect(table, SIGNAL(currentChanged(QTableWidgetItem*, QTableWidgetItem*)),
@@ -164,8 +241,6 @@ public slots:
     void updateStatus(QTableWidgetItem *item) {
         if (item && item == table->currentItem())
             statusBar()->message(item->data(QAbstractItemModel::StatusTipRole).toString(), 1000);
-        else
-            qDebug("no item!");
     }
 
     void contextActions(QMenu *menu, QTableWidgetItem *item) {
@@ -206,6 +281,7 @@ int main(int argc, char** argv) {
     SpreadSheet sheet;
     app.setMainWidget(&sheet);
     sheet.show();
+    sheet.resize(600, 350);
     return app.exec();
 }
 
