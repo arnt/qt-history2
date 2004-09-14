5598ed50a583a913ba020a4ab734ee7ac564fc75
Brad Hughes <bhughes@trolltech.com>
2004-09-14 16:51:40 +0100

    q_atomic_test_and_set_* should restart if the store fails
    [git-p4: depot-paths = "//depot/qt/main/": change = 153299]

--- src/core/arch/mips/qatomic64.s
+++ src/core/arch/mips/qatomic64.s
@@ -5,25 +5,27 @@
 	.ent	q_atomic_test_and_set_int
 q_atomic_test_and_set_int:
 	.frame	$sp,0,$31
-	ll   $3,0($4)
+1:	ll   $3,0($4)
 	.set noreorder
-	bne  $3,$5,1f
+	bne  $3,$5,2f
 	move $2,$0
 	.set reorder
 	move $2,$6
 	sc   $2,0($4)
-1:	j    $31
+	beqz $2,1b
+2:	j    $31
 	.end	q_atomic_test_and_set_int
 	.globl	q_atomic_test_and_set_ptr
 	.ent	q_atomic_test_and_set_ptr
 q_atomic_test_and_set_ptr:
 	.frame	$sp,0,$31
-	lld  $3,0($4)
+1:	lld  $3,0($4)
 	.set noreoder
-	bne  $3,$5,1f
+	bne  $3,$5,2f
 	move $2,$0
 	.set reorder
 	move $2,$6
 	scd  $2,0($4)
-1:	j    $31
+	beqz $2,1b
+2:	j    $31
 	.end	q_atomic_test_and_set_ptr
