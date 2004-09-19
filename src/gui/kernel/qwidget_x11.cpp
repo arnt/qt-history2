eb476528e49374bad00ad7704bfe2ba983c896ad
Matthias Ettrich <ettrich@trolltech.com>
2004-09-19 23:01:29 +0100

    transients for transients (dialogs of dialogs) should probably not be group transient, but transient to the window. At least on my machine they might show up _behind_ the parent dialog otherwise. An example is the standarddialogs example.
    [git-p4: depot-paths = "//depot/qt/main/": change = 153853]

--- src/gui/kernel/qwidget_x11.cpp
+++ src/gui/kernel/qwidget_x11.cpp
@@ -569,7 +569,7 @@ void QWidget::create(WId window, bool initializeWindow, bool destroyOldWindow)
             p = p->topLevelWidget();
 
         if (dialog || testWFlags(Qt::WStyle_DialogBorder) || testWFlags(Qt::WStyle_Tool)) {
-            if (p && p->testWFlags(Qt::WGroupLeader)) {
+            if (p && (p->testWFlags(Qt::WGroupLeader) || p->testWFlags(Qt::WType_Dialog))) {
                 // transient for window
                 XSetTransientForHint(dpy, id, p->winId());
             } else {
