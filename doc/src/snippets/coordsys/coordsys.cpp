770f9100ca8c6085767e2cb92e1fc9499ee94569
Jasmin Blanchette <jasmin@trolltech.com>
2004-09-20 18:22:39 +0100

    Fix the coordsys.html page. Other minor doc fixes.
    [git-p4: depot-paths = "//depot/qt/main/": change = 154009]

---
+++ doc/src/snippets/coordsys/coordsys.cpp
@@ -0,0 +1,35 @@
+#include <QtGui>
+
+class MyWidget : public QWidget
+{
+public:
+    MyWidget();
+
+protected:
+    void paintEvent(QPaintEvent *);
+};
+
+MyWidget::MyWidget()
+{
+    QPalette palette(MyWidget::palette());
+    palette.setColor(backgroundRole(), Qt::white);
+    setPalette(palette);
+}
+
+void MyWidget::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    painter.setPen(Qt::darkGray);
+    painter.drawRect(1, 2, 5, 4);
+    painter.setPen(Qt::lightGray);
+    painter.drawLine(9, 2, 7, 7);
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    MyWidget widget;
+    app.setMainWidget(&widget);
+    widget.show();
+    return app.exec();
+}
