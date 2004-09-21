6fe3e3275027810f5b8ecda538ef4ba572c69016
Trenton Schulz <twschulz@twschulz-rain>
2004-09-21 13:30:39 +0100

    Since there aren't a lot of fancy window managers out on the Mac, we        probably should move the windows just a little. To keep things simple. Just use (100,        100) as the starting point.
    [git-p4: depot-paths = "//depot/qt/main/": change = 154149]

--- examples/sql/querymodel/main.cpp
+++ examples/sql/querymodel/main.cpp
@@ -17,9 +17,12 @@ void initializeModel(QSqlQueryModel *model)
 
 void createView(const QString &title, QSqlQueryModel *model)
 {
+    static int delta = 0;
     QTableView *view = new QTableView;
     view->setModel(model);
     view->setWindowTitle(title);
+    view->move(100 + delta, 100 + delta);
+    delta += 20;
     view->show();
 }
 
