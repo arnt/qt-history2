cf3e43de4c2971d6671618d1f918186a1b538309
Matthias Ettrich <ettrich@trolltech.com>
2004-10-18 13:24:11 +0100

    new event debug utility qev
    [git-p4: depot-paths = "//depot/qt/main/": change = 156617]

---
+++ tools/qev/qev.cpp
@@ -0,0 +1,23 @@
+#include <QtGui>
+
+class Widget : public QWidget
+{
+public:
+    QSize sizeHint() const { return QSize(20, 20); }
+    bool event(QEvent *e) {
+        if (e->type() == QEvent::ContextMenu)
+            return false;
+        qDebug() << e;
+        return QWidget::event(e);
+    }
+};
+
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    Widget w;
+    app.setMainWidget(&w);
+    w.show();
+    return app.exec();
+}
