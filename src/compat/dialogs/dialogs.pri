64091d0a7ddb34a245db0697d6a3d55e30ea3cd5
Lars Knoll <lars@trolltech.com>
2004-10-18 20:36:25 +0100

    re-ported QWizard form Qt 3.
    [git-p4: depot-paths = "//depot/qt/main/": change = 156674]

--- src/compat/dialogs/dialogs.pri
+++ src/compat/dialogs/dialogs.pri
@@ -4,11 +4,12 @@ HEADERS += \
 	dialogs/q3filedialog.h \
 	dialogs/q3tabdialog.h \
 	dialogs/qtabdialog.h \
+	dialogs/q3wizard.h \
 	dialogs/qwizard.h
 SOURCES += \
 	dialogs/q3filedialog.cpp \
 	dialogs/q3tabdialog.cpp \
-	dialogs/qwizard.cpp
+	dialogs/q3wizard.cpp
 
 win32:SOURCES += dialogs/q3filedialog_win.cpp
 !embedded:mac:SOURCES  += dialogs/q3filedialog_mac.cpp
---
+++ src/compat/dialogs/q3wizard.h
@@ -0,0 +1,103 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef Q3WIZARDDIALOG_H
+#define Q3WIZARDDIALOG_H
+
+#include "qdialog.h"
+
+class QHBoxLayout;
+class Q3WizardPrivate;
+
+class Q_COMPAT_EXPORT Q3Wizard : public QDialog
+{
+    Q_OBJECT
+    Q_PROPERTY( QFont titleFont READ titleFont WRITE setTitleFont )
+
+public:
+    Q3Wizard( QWidget* parent=0, const char* name=0, bool modal=false, Qt::WFlags f=0 );
+    ~Q3Wizard();
+
+    void show();
+
+    void setFont( const QFont & font );
+
+    virtual void addPage( QWidget *, const QString & );
+    virtual void insertPage( QWidget*, const QString&, int );
+    virtual void removePage( QWidget * );
+
+    QString title( QWidget * ) const;
+    void setTitle( QWidget *, const QString & );
+    QFont titleFont() const;
+    void setTitleFont( const QFont & );
+
+    virtual void showPage( QWidget * );
+
+    QWidget * currentPage() const;
+
+    QWidget* page( int ) const;
+    int pageCount() const;
+    int indexOf( QWidget* ) const;
+
+    virtual bool appropriate( QWidget * ) const;
+    virtual void setAppropriate( QWidget *, bool );
+
+    QPushButton * backButton() const;
+    QPushButton * nextButton() const;
+    QPushButton * finishButton() const;
+    QPushButton * cancelButton() const;
+    QPushButton * helpButton() const;
+
+    bool eventFilter( QObject *, QEvent * );
+
+public slots:
+    virtual void setBackEnabled( QWidget *, bool );
+    virtual void setNextEnabled( QWidget *, bool );
+    virtual void setFinishEnabled( QWidget *, bool );
+
+    virtual void setHelpEnabled( QWidget *, bool );
+
+    // obsolete
+    virtual void setFinish(  QWidget *, bool ) {}
+
+protected slots:
+    virtual void back();
+    virtual void next();
+    virtual void help();
+
+signals:
+    void helpClicked();
+    void selected( const QString& );
+
+protected:
+    virtual void layOutButtonRow( QHBoxLayout * );
+    virtual void layOutTitleRow( QHBoxLayout *, const QString & );
+
+private:
+    void setBackEnabled( bool );
+    void setNextEnabled( bool );
+
+    void setHelpEnabled( bool );
+
+    void setNextPage( QWidget * );
+
+    void updateButtons();
+
+    void layOut();
+
+    Q3WizardPrivate *d;
+
+    Q_DISABLE_COPY(Q3Wizard)
+};
+
+#endif // Q3WIZARD_H
