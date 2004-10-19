d1634eae3cadfce6ad461e3323a39c0373bed074
harald <harald@harald-morpork>
2004-10-19 14:09:43 +0100

    introduce QT_SOCKET_CONNECT that maps to ::connect (or whatever the operating systems defines ::connect to)
    [git-p4: depot-paths = "//depot/qt/main/": change = 156792]

--- mkspecs/aix-g++-64/qplatformdefs.h
+++ mkspecs/aix-g++-64/qplatformdefs.h
@@ -53,6 +53,7 @@ static inline int qt_truncate(const char *pathname, off_t length)
 #define QT_STAT_DIR		S_IFDIR
 #define QT_STAT_MASK		S_IFMT
 #define QT_STAT_LNK		S_IFLNK
+#define QT_SOCKET_CONNECT	::connect
 #define QT_FILENO		fileno
 #define QT_OPEN			qt_open
 #define QT_CLOSE		::close
