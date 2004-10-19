6f2a28516a7be4c39f2661b3bf8871ef8563f229
Lars Knoll <lars@trolltech.com>
2004-10-19 10:11:49 +0100

    Qt 4 coding style.
    [git-p4: depot-paths = "//depot/qt/main/": change = 156741]

--- src/compat/tools/q3memarray.h
+++ src/compat/tools/q3memarray.h
@@ -26,13 +26,13 @@ public:
     typedef type ValueType;
 
 protected:
-    Q3MemArray( int, int ) : Q3GArray( 0, 0 ) {}
+    Q3MemArray(int, int) : Q3GArray(0, 0) {}
 
 public:
     Q3MemArray() {}
-    Q3MemArray( int size ) : Q3GArray(size*sizeof(type)) {}
-    Q3MemArray( const Q3MemArray<type> &a ) : Q3GArray(a) {}
-    Q3MemArray( const QVector<type> &vector);
+    Q3MemArray(int size) : Q3GArray(size*sizeof(type)) {}
+    Q3MemArray(const Q3MemArray<type> &a) : Q3GArray(a) {}
+    Q3MemArray(const QVector<type> &vector);
    ~Q3MemArray() {}
     Q3MemArray<type> &operator=(const Q3MemArray<type> &a)
 				{ return (Q3MemArray<type>&)Q3GArray::assign(a); }
@@ -42,42 +42,42 @@ public:
     uint  count()   const	{ return size(); }
     bool  isEmpty() const	{ return Q3GArray::size() == 0; }
     bool  isNull()  const	{ return Q3GArray::data() == 0; }
-    bool  resize( uint size )	{ return Q3GArray::resize(size*sizeof(type)); }
-    bool  resize( uint size, Optimization optim ) { return Q3GArray::resize(size*sizeof(type), optim); }
-    bool  truncate( uint pos )	{ return Q3GArray::resize(pos*sizeof(type)); }
-    bool  fill( const type &d, int size = -1 )
-	{ return Q3GArray::fill((char*)&d,size,sizeof(type) ); }
+    bool  resize(uint size)	{ return Q3GArray::resize(size*sizeof(type)); }
+    bool  resize(uint size, Optimization optim) { return Q3GArray::resize(size*sizeof(type), optim); }
+    bool  truncate(uint pos)	{ return Q3GArray::resize(pos*sizeof(type)); }
+    bool  fill(const type &d, int size = -1)
+	{ return Q3GArray::fill((char*)&d,size,sizeof(type)); }
     void  detach()		{ Q3GArray::detach(); }
     Q3MemArray<type>   copy() const
 	{ Q3MemArray<type> tmp; return tmp.duplicate(*this); }
-    Q3MemArray<type>& assign( const Q3MemArray<type>& a )
+    Q3MemArray<type>& assign(const Q3MemArray<type>& a)
 	{ return (Q3MemArray<type>&)Q3GArray::assign(a); }
-    Q3MemArray<type>& assign( const type *a, uint n )
+    Q3MemArray<type>& assign(const type *a, uint n)
 	{ return (Q3MemArray<type>&)Q3GArray::assign((char*)a,n*sizeof(type)); }
-    Q3MemArray<type>& duplicate( const Q3MemArray<type>& a )
+    Q3MemArray<type>& duplicate(const Q3MemArray<type>& a)
 	{ return (Q3MemArray<type>&)Q3GArray::duplicate(a); }
-    Q3MemArray<type>& duplicate( const type *a, uint n )
+    Q3MemArray<type>& duplicate(const type *a, uint n)
 	{ return (Q3MemArray<type>&)Q3GArray::duplicate((char*)a,n*sizeof(type)); }
-    Q3MemArray<type>& setRawData( const type *a, uint n )
+    Q3MemArray<type>& setRawData(const type *a, uint n)
 	{ return (Q3MemArray<type>&)Q3GArray::setRawData((char*)a,
 						     n*sizeof(type)); }
-    void resetRawData( const type *a, uint n )
+    void resetRawData(const type *a, uint n)
 	{ Q3GArray::resetRawData((char*)a,n*sizeof(type)); }
-    int	 find( const type &d, uint i=0 ) const
+    int	 find(const type &d, uint i=0) const
 	{ return Q3GArray::find((char*)&d,i,sizeof(type)); }
-    int	 contains( const type &d ) const
+    int	 contains(const type &d) const
 	{ return Q3GArray::contains((char*)&d,sizeof(type)); }
     void sort() { Q3GArray::sort(sizeof(type)); }
-    int  bsearch( const type &d ) const
+    int  bsearch(const type &d) const
 	{ return Q3GArray::bsearch((const char*)&d,sizeof(type)); }
     // ### Qt 4.0: maybe provide uint overload as work-around for MSVC bug
-    type& operator[]( int i ) const
+    type& operator[](int i) const
 	{ return (type &)(*(type *)Q3GArray::at(i*sizeof(type))); }
-    type& at( uint i ) const
+    type& at(uint i) const
 	{ return (type &)(*(type *)Q3GArray::at(i*sizeof(type))); }
 	 operator const type*() const { return (const type *)Q3GArray::data(); }
-    bool operator==( const Q3MemArray<type> &a ) const { return isEqual(a); }
-    bool operator!=( const Q3MemArray<type> &a ) const { return !isEqual(a); }
+    bool operator==(const Q3MemArray<type> &a) const { return isEqual(a); }
+    bool operator!=(const Q3MemArray<type> &a) const { return !isEqual(a); }
     Iterator begin() { return data(); }
     Iterator end() { return data() + size(); }
     ConstIterator begin() const { return data(); }
@@ -87,7 +87,7 @@ public:
 };
 
 template<class type>
-Q_OUTOFLINE_TEMPLATE Q3MemArray<type>::Q3MemArray( const QVector<type> &vector)
+Q_OUTOFLINE_TEMPLATE Q3MemArray<type>::Q3MemArray(const QVector<type> &vector)
     : Q3GArray(vector.size())
 {
     for (int i = 0; i < vector.size(); ++i)
