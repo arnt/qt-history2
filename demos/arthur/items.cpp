e9f4b878f3b99aefc0bd234e8e1d4adefa31248e
Trond Kjernaasen <trond@trolltech.com>
2004-10-20 14:50:38 +0100

    Better.
    [git-p4: depot-paths = "//depot/qt/main/": change = 156918]

--- demos/arthur/items.cpp
+++ demos/arthur/items.cpp
@@ -15,39 +15,57 @@
 #include <QPainter>
 #include <QMouseEvent>
 #include <QDateTime>
+#include <QPainterPath>
+#include <QMatrix>
 
 #include "items.h"
 
 class Item
 {
 public:
-    enum Shape {Circle, Rectangle, Text};
+    enum Shape {Circle, Rectangle, Path};
 
     Item(QPoint topLeft, Shape _shape, const QColor &c)
         : shape(_shape), sel(false), color(c) {
         translate(topLeft);
+        if (shape == Path) {
+            path = new QPainterPath;
+
+            path->addRect(20,20,60,60);
+            path->moveTo(0,0);
+            path->curveTo(99,0,50,50,99,99);
+            path->moveTo(99,99);
+            path->curveTo(0,99,50,50,0,0);
+            QFont fnt("times", 75);
+            QRect r = QFontMetrics(fnt).boundingRect("Trolltech");
+            path->addText(-r.center()+QPoint(50,140), fnt, "Trolltech");
+
+        }
     }
 
-    void draw(QPainter *p) {
-        p->save();
+    ~Item() {
+        if (shape == Path)
+            delete path;
+    }
+
+    void draw(QPainter *p, bool useOwnColor = true) {
         if (sel)
-            p->setPen(Qt::white);
+            p->setPen(Qt::red);
         else
             p->setPen(Qt::black);
+        if (useOwnColor)
+            p->setBrush(color);
         switch (shape) {
         case Circle:
-            p->setBrush(color);
             p->drawEllipse(trans.x(), trans.y(), 100, 100);
             break;
         case Rectangle:
-            p->setBrush(color);
             p->drawRect(trans.x(), trans.y(), 100, 100);
             break;
-        case Text:
-            p->drawText(trans.x(), trans.y(), "Trolltech");
+        case Path:
+            p->drawPath(*path * QMatrix(1, 0, 0, 1, trans.x(), trans.y()));
             break;
         }
-        p->restore();
     }
 
     QRect boundingRect() const {
@@ -57,11 +75,9 @@ public:
         case Rectangle:
             r = QRect(trans.x(), trans.y(), 100, 100);
             break;
-        case Text:
-            QFontMetrics fm(qApp->font());
-            r = fm.boundingRect("Trolltech");
-            r.setX(trans.x());
-            r.setY(trans.y());
+        case Path:
+            r = path->boundingRect().toRect();
+            r.translate(trans);
             break;
         }
         return r;
@@ -89,18 +105,24 @@ private:
     Shape shape;
     bool sel;
     QColor color;
+    QPainterPath *path;
 };
 
 Items::Items(QWidget *parent)
     : DemoWidget(parent)
 {
     srand(QTime::currentTime().msec());
-    for (int i = 0; i < 100; ++i) {
-        items.append(new Item(QPoint(rand()%512, rand()%512), Item::Rectangle,
-                              QColor(rand()%255,rand()%256,rand()%256,rand()%256)));
-        items.append(new Item(QPoint(rand()%512, rand()%512), Item::Circle,
-                              QColor(rand()%255,rand()%256,rand()%256,rand()%256)));
+    for (int i = 0; i < 500; ++i) {
+        items.append(new Item(QPoint(50+rand()%380, 50+rand()%380), Item::Rectangle,
+                              QColor(120+rand()%136,120+rand()%136,120+rand()%136)));
+        items.append(new Item(QPoint(50+rand()%380, 50+rand()%380), Item::Circle,
+                              QColor(120+rand()%136,120+rand()%136,120+rand()%136)));
     }
+
+    Item *item = new Item(QPoint(), Item::Path, QColor(120,255,120,200));
+    QRect r = item->boundingRect();
+    item->translate(QPoint(591/2,600/2)-r.center());
+    items.append(item);
 }
 
 Items::~Items()
@@ -114,9 +136,10 @@ void Items::paintEvent(QPaintEvent *)
     QPainter p(this);
     p.drawPixmap(0, 0, buffer);
 
-    p.setBrush(QColor(255,255,255,80));
-    if (!anchor.isNull())
-        p.drawRect(itemBr);
+    if (!anchor.isNull()) {
+        p.setBrush(QColor(120,120,255,100));
+        items[items.size()-1]->draw(&p, false);
+    }
 }
 
 void Items::mousePressEvent(QMouseEvent *event)
@@ -130,6 +153,8 @@ void Items::mousePressEvent(QMouseEvent *event)
             items[i]->setOffset(anchor);
             itemBr = items.at(i)->boundingRect();
             selected = true;
+            items.move(i, items.size()-1);
+            break;
         } else {
             items[i]->setSelected(false);
         }
@@ -167,28 +192,26 @@ void Items::mouseReleaseEvent(QMouseEvent *)
 void Items::resizeEvent(QResizeEvent *)
 {
     buffer.resize(size());
-    drawItems(QRect(QPoint(0,0),size()));
+    drawItems(QRect());
 }
 
 void Items::drawItems(const QRect &rect)
 {
-    QRect result = rect;
     QPainter px(&buffer);
-
     int drawn = 0;
-    for (int i = 0; i < items.size(); ++i) {
-        if (rect.isEmpty() || items.at(i)->boundingRect().intersects(rect)) {
-            result |= items.at(i)->boundingRect();
-            ++drawn;
-        }
+
+    QRect clip = rect;
+    if (!clip.isEmpty()) {
+        clip.addCoords(-1,-1,1,1);
+        px.setClipRect(rect);
     }
-    px.setClipRect(result);
     fillBackground(&px);
+//     if (attribs()->antialias)
+//         px.setRenderHint(QPainter::LineAntialiasing);
     for (int i = 0; i < items.size(); ++i) {
-        if (items[i]->boundingRect().intersects(result))
+        if (clip.isEmpty() || items[i]->boundingRect().intersects(clip))
             items[i]->draw(&px);
     }
-//    qDebug() << drawn;
 }
 
 void Items::resetState()
