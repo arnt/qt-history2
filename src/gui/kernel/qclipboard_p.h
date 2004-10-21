e67ce3e7b2a336f395e1e7eb871262f45a83b793
Lars Knoll <lars@trolltech.com>
2004-10-21 14:09:08 +0100

    forgot that one.
    [git-p4: depot-paths = "//depot/qt/main/": change = 157027]

---
+++ src/gui/kernel/qclipboard_p.h
@@ -0,0 +1,80 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QCLIPBOARD_P_H
+#define QCLIPBOARD_P_H
+
+#include <private/qobject_p.h>
+#include "qmime.h"
+#include "qclipboard.h"
+
+class QClipboardPrivate;
+
+class QMimeDataWrapper : public QMimeSource
+{
+public:
+    QMimeDataWrapper() {}
+
+    const char* format(int n) const;
+    QByteArray encodedData(const char*) const;
+
+    const QMimeData *data;
+};
+
+class QMimeSourceWrapper : public QMimeData
+{
+public:
+    QMimeSourceWrapper(QClipboardPrivate *priv, QClipboard::Mode m);
+    ~QMimeSourceWrapper();
+
+    bool hasFormat(const QString &mimetype) const;
+    QStringList formats() const;
+
+protected:
+    QVariant retrieveData(const QString &mimetype, QVariant::Type) const;
+private:
+    QClipboardPrivate *d;
+    QClipboard::Mode mode;
+    QMimeSource *source;
+};
+
+
+class QClipboardPrivate : public QObjectPrivate
+{
+public:
+    QClipboardPrivate() : QObjectPrivate() {
+        compat_data[0] = compat_data[1] = 0;
+        wrapper[0] = new QMimeDataWrapper();
+        wrapper[1] = new QMimeDataWrapper();
+    }
+
+    mutable QMimeDataWrapper *wrapper[2];
+    mutable QMimeSource *compat_data[2];
+};
+
+inline QMimeSourceWrapper::QMimeSourceWrapper(QClipboardPrivate *priv, QClipboard::Mode m)
+    : QMimeData(0)
+{
+    d = priv;
+    mode = m;
+    source = d->compat_data[mode];
+}
+
+inline QMimeSourceWrapper::~QMimeSourceWrapper()
+{
+    if (d->compat_data[mode] == source)
+        d->compat_data[mode] = 0;
+    delete source;
+}
+
+#endif
