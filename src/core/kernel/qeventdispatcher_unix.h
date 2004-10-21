5ab58c84e5e48bc0f5277cee319fcdbcc612188e
Brad Hughes <bhughes@trolltech.com>
2004-10-21 11:04:52 +0100

    introduce QEventDispatcherUNIX, the event dispatcher for UNIX machines
    [git-p4: depot-paths = "//depot/qt/main/": change = 157005]

---
+++ src/core/kernel/qeventdispatcher_unix.h
@@ -0,0 +1,49 @@
+#ifndef QEVENTDISPATCHER_UNIX_H
+#define QEVENTDISPATCHER_UNIX_H
+
+#include "qabstracteventdispatcher.h"
+
+class QEventDispatcherUNIXPrivate;
+
+class QEventDispatcherUNIX : public QAbstractEventDispatcher
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(QEventDispatcherUNIX)
+
+public:
+    QEventDispatcherUNIX(QObject *parent = 0);
+    ~QEventDispatcherUNIX();
+
+    bool processEvents(QEventLoop::ProcessEventsFlags flags);
+    bool hasPendingEvents();
+
+    void registerSocketNotifier(QSocketNotifier *notifier);
+    void unregisterSocketNotifier(QSocketNotifier *notifier);
+
+    int registerTimer(int timerInterval, QObject *object);
+    bool unregisterTimer(int timerId);
+    bool unregisterTimers(QObject *object);
+
+    void wakeUp();
+    void flush();
+
+#if 0
+    void watchUnixSignal(int signal, bool);
+signals:
+    void unixSignal(int);
+#endif
+
+protected:
+    QEventDispatcherUNIX(QEventDispatcherUNIXPrivate &, QObject *parent);
+
+    int timeToWait();
+    void setSocketNotifierPending(QSocketNotifier *notifier);
+
+    int activateTimers();
+    int activateSocketNotifiers();
+
+    virtual int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+                       timeval *timeout);
+};
+
+#endif // QEVENTDISPATCHER_UNIX_H
