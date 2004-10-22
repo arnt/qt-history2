d9476b3c89787b8588829ded3b81eb750f3cb8c4
Volker Hilsheimer <vohi@trolltech.com>
2004-10-22 13:58:52 +0100

    Brad: for now, use a private reimplementation of QEventDispatcherWin32 to handle config requests... i will see if i can find a better way to do this
    [git-p4: depot-paths = "//depot/qt/main/": change = 157185]

--- src/gui/kernel/qapplication_win.cpp
+++ src/gui/kernel/qapplication_win.cpp
@@ -860,8 +860,127 @@ static void unregWinClasses()
     hash->clear();
 }
 
+
+/*****************************************************************************
+  Safe configuration (move,resize,setGeometry) mechanism to avoid
+  recursion when processing messages.
+ *****************************************************************************/
+
+struct QWinConfigRequest {
+    WId         id;                                        // widget to be configured
+    int         req;                                        // 0=move, 1=resize, 2=setGeo
+    int         x, y, w, h;                                // request parameters
+};
+
+static QList<QWinConfigRequest*> *configRequests = 0;
+
+void qWinRequestConfig(WId id, int req, int x, int y, int w, int h)
+{
+    if (!configRequests)                        // create queue
+        configRequests = new QList<QWinConfigRequest*>;
+    QWinConfigRequest *r = new QWinConfigRequest;
+    r->id = id;                                        // create new request
+    r->req = req;
+    r->x = x;
+    r->y = y;
+    r->w = w;
+    r->h = h;
+    configRequests->append(r);                // store request in queue
+}
+
+Q_GUI_EXPORT void qWinProcessConfigRequests()                // perform requests in queue
+{
+    if (!configRequests)
+        return;
+    QWinConfigRequest *r;
+    for (;;) {
+        if (configRequests->isEmpty())
+            break;
+        r = configRequests->takeLast();
+        QWidget *w = QWidget::find(r->id);
+        if (w) {                                // widget exists
+            if (w->testWState(Qt::WState_ConfigPending))
+                return;                                // biting our tail
+            if (r->req == 0)
+                w->move(r->x, r->y);
+            else if (r->req == 1)
+                w->resize(r->w, r->h);
+            else
+                w->setGeometry(r->x, r->y, r->w, r->h);
+        }
+        delete r;
+    }
+    delete configRequests;
+    configRequests = 0;
+}
+
+
+/*****************************************************************************
+    GUI event dispatcher
+ *****************************************************************************/
+
+class QGuiEventDispatcherWin32 : public QEventDispatcherWin32
+{
+    Q_DECLARE_PRIVATE(QEventDispatcherWin32)
+public:
+    QGuiEventDispatcherWin32(QObject *parent = 0);
+    bool processEvents(QEventLoop::ProcessEventsFlags flags);
+    void winProcessEvent(void *message);
+};
+
+QGuiEventDispatcherWin32::QGuiEventDispatcherWin32(QObject *parent)
+    : QEventDispatcherWin32(parent)
+{ }
+
+bool QGuiEventDispatcherWin32::processEvents(QEventLoop::ProcessEventsFlags flags)
+{
+    if (!QEventDispatcherWin32::processEvents(flags))
+        return false;
+
+    if (configRequests)                        // any pending configs?
+        qWinProcessConfigRequests();
+    QCoreApplication::sendPostedEvents();
+
+    return true;
+}
+
+void QGuiEventDispatcherWin32::winProcessEvent(void *message)
+{
+#if 0
+    if (d->process_event_handler && d->process_event_handler(message))
+        return;
+#endif
+
+    MSG *msg = (MSG*)message;
+
+    bool handled = false;
+    if (msg->message == WM_TIMER) {
+        if (qt_dispatch_timer(msg->wParam, msg))
+            return;
+    } else if (msg->message && (!msg->hwnd || !QWidget::find(msg->hwnd))) {
+        // broadcast, or message for a non-Qt widget
+	long res = 0;
+	handled = winEventFilter(msg, &res);
+    }
+
+    if (!handled) {
+	QInputContext::TranslateMessage(msg); // translate to WM_CHAR
+
+        QT_WA({
+            DispatchMessage(msg);
+        } , {
+            DispatchMessageA(msg);
+        });
+    }
+}
+
 void QApplicationPrivate::createEventDispatcher()
-{ QCoreApplicationPrivate::createEventDispatcher(); }
+{
+    if (q->type() != QApplication::Tty)
+        eventDispatcher = new QGuiEventDispatcherWin32(q);
+    else
+        eventDispatcher = new QEventDispatcherWin32(q); 
+}
 
 /*****************************************************************************
   Platform specific QApplication members
