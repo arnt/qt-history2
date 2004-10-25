f597051a18eeef989b69aeae81e6b36567f35114
Matthias Ettrich <ettrich@trolltech.com>
2004-10-25 11:12:42 +0100

    Q_FLAGS takes the flags, the the single flag
    [git-p4: depot-paths = "//depot/qt/main/": change = 157382]

--- src/gui/itemviews/qitemselectionmodel.h
+++ src/gui/itemviews/qitemselectionmodel.h
@@ -70,7 +70,7 @@ class Q_GUI_EXPORT QItemSelectionModel : public QObject
 {
     Q_OBJECT
     Q_DECLARE_PRIVATE(QItemSelectionModel)
-    Q_FLAGS(SelectionFlag)
+    Q_FLAGS(SelectionFlags)
 
 public:
 
