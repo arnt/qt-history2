66be1e1956f8b398b10502807b930a6cd393b24c
dboddie <dboddie@mikaeld-lurven>
2004-10-26 11:35:54 +0100

    Initial documentation for QDrag.
    [git-p4: depot-paths = "//depot/qt/main/": change = 157527]

--- src/gui/kernel/qdrag.cpp
+++ src/gui/kernel/qdrag.cpp
@@ -18,6 +18,26 @@
 #include "qdnd_p.h"
 
 
+/*!
+    \class QDrag
+    \brief The QDrag class provides support for MIME-based drag and drop data
+    transfer.
+*/
+
+/*!
+    \enum QDrag::DropAction
+
+    \value CopyAction       Copy the data to the target.
+    \value MoveAction       Move the date from the source to the target.
+    \value LinkAction       Create a link from the source to the target.
+    \value ActionMask       ###
+    \value TargetMoveAction ###
+    \value IgnoreAction     Ignore the action (do nothing with the data).
+*/
+
+/*!
+    Constructs a new drag object for the widget specified by \a dragSource.
+*/
 QDrag::QDrag(QWidget *dragSource)
     : QObject(*new QDragPrivate, dragSource)
 {
@@ -30,6 +50,9 @@ QDrag::QDrag(QWidget *dragSource)
     d->executed_action = QDrag::IgnoreAction;
 }
 
+/*!
+    Destroys the drag object.
+*/
 QDrag::~QDrag()
 {
     QDragManager *manager = QDragManager::self();
@@ -37,53 +60,87 @@ QDrag::~QDrag()
         manager->cancel(false);
 }
 
+/*!
+    Sets the data to be sent to the given MIME \a data.
+*/
 void QDrag::setMimeData(QMimeData *data)
 {
     Q_D(QDrag);
     d->data = data;
 }
 
+/*!
+    Returns the MIME data that is encapsulated by the drag object.
+*/
 QMimeData *QDrag::mimeData() const
 {
     Q_D(const QDrag);
     return d->data;
 }
 
+/*!
+    Sets \a pixmap as the pixmap used to represent the data in a drag
+    and drop operation.
+*/
 void QDrag::setPixmap(const QPixmap &pixmap)
 {
     Q_D(QDrag);
     d->pixmap = pixmap;
 }
 
+/*!
+    Returns the pixmap used to represent the data in a drag and drop operation.
+*/
 QPixmap QDrag::pixmap() const
 {
     Q_D(const QDrag);
     return d->pixmap;
 }
 
+/*!
+    Sets the position of the hot spot relative to the top-left corner of the
+    cursor to the point specified by \a hotspot.
+*/
 void QDrag::setHotSpot(const QPoint& hotspot)
 {
     Q_D(QDrag);
     d->hotspot = hotspot;
 }
 
+/*!
+    Returns the position of the hot spot relative to the top-left corner of the
+    cursor.
+*/
 QPoint QDrag::hotSpot() const
 {
     Q_D(const QDrag);
     return d->hotspot;
 };
 
+/*!
+    Returns the source of the drag object. This is the widget where the drag
+    and drop operation originated.
+*/
 QWidget *QDrag::source() const
 {
     Q_D(const QDrag);
     return d->source;
 }
 
+/*!
+    Returns the target of the drag and drop operation. This is the widget where
+    the drag object was dropped.
+*/
 QWidget *QDrag::target() const
 {
     return 0; // ########### d->target;
 }
 
+/*!
+    Starts the drag and drop operation. The actions that are available to the
+    user when the drag and drop operation is completed are specified in
+    \a request.
+*/
 QDrag::DropAction QDrag::start(QDrag::DropActions request)
 {
     Q_D(QDrag);
