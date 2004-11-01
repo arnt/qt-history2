a515e8ce4b45f3c78ea4f6f6c37c2d721b6b8c8f
Brad Hughes <bhughes@trolltech.com>
2004-11-01 16:46:54 +0100

    don't dereference a null pointer
    [git-p4: depot-paths = "//depot/qt/main/": change = 158134]

--- src/gui/kernel/qapplication_x11.cpp
+++ src/gui/kernel/qapplication_x11.cpp
@@ -822,7 +822,7 @@ static void qt_set_input_encoding()
         if(!qt_input_mapper)
             qt_input_mapper = QTextCodec::codecForName("ISO 8859-1");
     }
-    if (qt_input_mapper->mibEnum() == 11) // 8859-8
+    if (qt_input_mapper && qt_input_mapper->mibEnum() == 11) // 8859-8
         qt_input_mapper = QTextCodec::codecForName("ISO 8859-8-I");
     if(data)
         XFree((char *)data);
