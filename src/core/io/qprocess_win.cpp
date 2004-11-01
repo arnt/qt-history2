fde97bf7fb45a383ebb5cbadd4c122643148e609
pbuhler <pbuhler@arsia>
2004-11-01 15:10:30 +0100

    qprocess now pass the test on windows (if you blink at the right time)
    [git-p4: depot-paths = "//depot/qt/main/": change = 158113]

--- src/core/io/qprocess_win.cpp
+++ src/core/io/qprocess_win.cpp
@@ -28,30 +28,28 @@ void QProcessPrivate::createPipe(Q_PIPE pipe[2])
     // Open the pipes.  Make non-inheritable copies of input write and output
     // read handles to avoid non-closable handles (this is done by the
     // DuplicateHandle() call).
-
-      SECURITY_ATTRIBUTES secAtt = { sizeof( SECURITY_ATTRIBUTES ), NULL, TRUE };
+    
+    SECURITY_ATTRIBUTES secAtt = { sizeof( SECURITY_ATTRIBUTES ), NULL, TRUE };
     
     HANDLE tmpStdin, tmpStdout, tmpStderr;
-        if ( !CreatePipe( &writePipe[0], &tmpStdin, &secAtt, 0 ) ) {
-           
-        }
-        if ( !DuplicateHandle( GetCurrentProcess(), tmpStdin, GetCurrentProcess(), &writePipe[1], 0, FALSE, DUPLICATE_SAME_ACCESS ) ) {
-        }
-        if ( !CloseHandle( tmpStdin ) ) {
-        }
-    
-        if ( !CreatePipe( &tmpStdout, &standardReadPipe[1], &secAtt, 0 ) ) {
-        }
-        if ( !DuplicateHandle( GetCurrentProcess(), tmpStdout, GetCurrentProcess(), &standardReadPipe[0], 0, FALSE, DUPLICATE_SAME_ACCESS ) ) {
-        }
-        if ( !CloseHandle( tmpStdout ) ) {
-        }
-        if ( !CreatePipe( &tmpStderr, &errorReadPipe[1], &secAtt, 0 ) ) {
-        }
-        if ( !DuplicateHandle( GetCurrentProcess(), tmpStderr, GetCurrentProcess(), &errorReadPipe[0], 0, FALSE, DUPLICATE_SAME_ACCESS ) ) {
-        }
-        if ( !CloseHandle( tmpStderr ) ) {
-        }
+    if (!CreatePipe(&writePipe[0], &tmpStdin, &secAtt, 0))
+        return;
+    if (!DuplicateHandle(GetCurrentProcess(), tmpStdin, GetCurrentProcess(), &writePipe[1], 0, FALSE, DUPLICATE_SAME_ACCESS))
+        return;
+    if (!CloseHandle(tmpStdin))
+        return;
+    if (!CreatePipe(&tmpStdout, &standardReadPipe[1], &secAtt, 0))
+        return;
+    if (!DuplicateHandle(GetCurrentProcess(), tmpStdout, GetCurrentProcess(), &standardReadPipe[0], 0, FALSE, DUPLICATE_SAME_ACCESS))
+        return;
+    if (!CloseHandle(tmpStdout))
+        return;
+    if (!CreatePipe(&tmpStderr, &errorReadPipe[1], &secAtt, 0))
+        return;
+    if (!DuplicateHandle(GetCurrentProcess(), tmpStderr, GetCurrentProcess(), &errorReadPipe[0], 0, FALSE, DUPLICATE_SAME_ACCESS))
+        return;
+    if (!CloseHandle(tmpStderr))
+        return;
 }
 
 void QProcessPrivate::destroyPipe(Q_PIPE pipe[2])
@@ -82,26 +80,26 @@ void QProcessPrivate::startProcess()
     // add the prgram as the first arrg ... it works better
     args = program + " ";
     for (int i=0; i<arguments.size(); ++i) {
-        ///### handle .bat files
-   	    QString tmp = arguments.at(i);
-	    // escape a single " because the arguments will be parsed
-	    tmp.replace( "\"", "\\\"" );
-	    if (tmp.isEmpty() || tmp.contains(' ') || tmp.contains('\t')) {
-	        // The argument must not end with a \ since this would be interpreted
-	        // as escaping the quote -- rather put the \ behind the quote: e.g.
-	        // rather use "foo"\ than "foo\"
-	        QString endQuote("\"");
-	        int i = tmp.length();
-	        while (i>=0 && tmp.at(i-1) == '\\') {
+    ///### handle .bat files
+   	QString tmp = arguments.at(i);
+        // escape a single " because the arguments will be parsed
+        tmp.replace( "\"", "\\\"" );
+        if (tmp.isEmpty() || tmp.contains(' ') || tmp.contains('\t')) {
+            // The argument must not end with a \ since this would be interpreted
+            // as escaping the quote -- rather put the \ behind the quote: e.g.
+            // rather use "foo"\ than "foo\"
+            QString endQuote("\"");
+            int i = tmp.length();
+            while (i>=0 && tmp.at(i-1) == '\\') {
                 --i;
-		        endQuote += "\\";
-	        }
+                endQuote += "\\";
+            }
             args += QString(" \"") + tmp.left(i) + endQuote;
-	    } else {
-	        args += ' ' + tmp;
-	    }
+        } else {
+            args += ' ' + tmp;
+        }
     }
-
+    
     bool success;
     
     if (pid) {
@@ -116,47 +114,47 @@ void QProcessPrivate::startProcess()
 
 #ifdef UNICODE
     if (!(QSysInfo::WindowsVersion & QSysInfo::WV_DOS_based)) {
-	    STARTUPINFOW startupInfo = {
-	        sizeof( STARTUPINFO ), 0, 0, 0,
-	        (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT,
-	        0, 0, 0,
-	        STARTF_USESTDHANDLES,
-	        0, 0, 0,
-	        writePipe[0], standardReadPipe[1], errorReadPipe[1]
-	    };
+	STARTUPINFOW startupInfo = {
+	    sizeof( STARTUPINFO ), 0, 0, 0,
+	    (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT,
+	    0, 0, 0,
+	    STARTF_USESTDHANDLES,
+	    0, 0, 0,
+	    writePipe[0], standardReadPipe[1], errorReadPipe[1]
+	};
         QByteArray envlist;
-	    if (environment.isEmpty()) {
-	        int pos = 0;
-	        // add PATH if necessary (for DLL loading)
-	        char *path = qgetenv("PATH");
-	        if (environment.grep(QRegExp("^PATH=",FALSE)).empty() && path) {
-		        QString tmp = QString("PATH=%1").arg(qgetenv("PATH"));
-		        uint tmpSize = sizeof(TCHAR) * (tmp.length()+1);
-		        envlist.resize(envlist.size() + tmpSize );
-		        memcpy(envlist.data()+pos, tmp.ucs2(), tmpSize);
-		        pos += tmpSize;
-	        }
-	        // add the user environment
-	        for (QStringList::Iterator it = environment.begin(); it != environment.end(); it++ ) {
-		        QString tmp = *it;
-		        uint tmpSize = sizeof(TCHAR) * (tmp.length()+1);
-		        envlist.resize(envlist.size() + tmpSize);
-		        memcpy(envlist.data()+pos, tmp.ucs2(), tmpSize);
-		        pos += tmpSize;
-	        }
-	        // add the 2 terminating 0 (actually 4, just to be on the safe side)
-	        envlist.resize( envlist.size()+4 );
-	        envlist[pos++] = 0;
-	        envlist[pos++] = 0;
-	        envlist[pos++] = 0;
-	        envlist[pos++] = 0;
+	if (environment.isEmpty()) {
+	    int pos = 0;
+	    // add PATH if necessary (for DLL loading)
+	    char *path = qgetenv("PATH");
+	    if (environment.grep(QRegExp("^PATH=",FALSE)).empty() && path) {
+		    QString tmp = QString("PATH=%1").arg(qgetenv("PATH"));
+		    uint tmpSize = sizeof(TCHAR) * (tmp.length()+1);
+		    envlist.resize(envlist.size() + tmpSize );
+		    memcpy(envlist.data()+pos, tmp.ucs2(), tmpSize);
+		    pos += tmpSize;
 	    }
-
+	    // add the user environment
+	    for (QStringList::Iterator it = environment.begin(); it != environment.end(); it++ ) {
+		    QString tmp = *it;
+		    uint tmpSize = sizeof(TCHAR) * (tmp.length()+1);
+		    envlist.resize(envlist.size() + tmpSize);
+		    memcpy(envlist.data()+pos, tmp.ucs2(), tmpSize);
+		    pos += tmpSize;
+	    }
+	    // add the 2 terminating 0 (actually 4, just to be on the safe side)
+	    envlist.resize( envlist.size()+4 );
+	    envlist[pos++] = 0;
+	    envlist[pos++] = 0;
+	    envlist[pos++] = 0;
+	    envlist[pos++] = 0;
+	}
+#if defined QPROCESS_DEBUG
         qDebug("Creating process");
         qDebug("   program : [%s]", program.latin1());
         qDebug("   args : %s", args.latin1());
         qDebug("   pass enviroment : %s", environment.isEmpty() ? "no" : "yes");
-        
+#endif
         success = CreateProcessW(0, (WCHAR*)args.utf16(),
 		                         0, 0, TRUE, CREATE_UNICODE_ENVIRONMENT | CREATE_NO_WINDOW, 
                                  environment.isEmpty() ? 0 : envlist.data(),
@@ -166,15 +164,15 @@ void QProcessPrivate::startProcess()
 #endif // UNICODE
     {
 #ifndef Q_OS_TEMP
-	    STARTUPINFOA startupInfo = { sizeof( STARTUPINFOA ), 0, 0, 0,
-	        (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT,
-	        0, 0, 0,
-	        STARTF_USESTDHANDLES,
-	        0, 0, 0,
-	        writePipe[0], standardReadPipe[1], errorReadPipe[1]
-	    };
+	STARTUPINFOA startupInfo = { sizeof( STARTUPINFOA ), 0, 0, 0,
+	    (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT, (ulong)CW_USEDEFAULT,
+	    0, 0, 0,
+	    STARTF_USESTDHANDLES,
+	    0, 0, 0,
+	    writePipe[0], standardReadPipe[1], errorReadPipe[1]
+	};
         QByteArray envlist;
-	    if (environment.isEmpty()) {
+	if (environment.isEmpty()) {
             int pos = 0;
 	        // add PATH if necessary (for DLL loading)
 	        char *path = qgetenv("PATH");
@@ -198,27 +196,27 @@ void QProcessPrivate::startProcess()
 	        envlist[pos++] = 0;
 	        envlist[pos++] = 0;
 	    }
+#if defined QPROCESS_DEBUG
         qDebug("Creating process");
-        qDebug("   program : %s", program.latin1());
+        qDebug("   program : [%s]", program.latin1());
         qDebug("   args : %s", args.latin1());
-
-	    success = CreateProcessA(program.toLocal8Bit().data(), args.toLocal8Bit().data(),
-		0, 0, TRUE, 0, environment.isEmpty() ? 0 : envlist.data(),
-		workingDirectory.local8Bit(), &startupInfo, (PROCESS_INFORMATION*)pid);
+        qDebug("   pass enviroment : %s", environment.isEmpty() ? "no" : "yes");
+#endif
+	success = CreateProcessA(0, args.toLocal8Bit().data(),
+	    0, 0, TRUE, 0, environment.isEmpty() ? 0 : envlist.data(),
+	    workingDirectory.local8Bit(), &startupInfo, (PROCESS_INFORMATION*)pid);
 #endif // Q_OS_TEMP
     }
     
 #ifndef Q_OS_TEMP
     CloseHandle(writePipe[0]);
-    standardReadPipe[1] = INVALID_Q_PIPE;
+    writePipe[0] = INVALID_Q_PIPE;
     CloseHandle(standardReadPipe[1]);
     standardReadPipe[1] = INVALID_Q_PIPE;
     CloseHandle(errorReadPipe[1]);
     errorReadPipe[1] = INVALID_Q_PIPE;
 #endif
     
-    qDebug("call startupNotification()");
-
     processState = QProcess::Running;
     startupNotification();
 
@@ -231,7 +229,6 @@ void QProcessPrivate::startProcess()
     QObject::connect(notifier, SIGNAL(timeout()), q, SLOT(notified()));
     notifier->start(NOTIFYTIMEOUT);
     
-    qDebug("started");
 }
 
 void QProcessPrivate::execChild()
@@ -246,10 +243,7 @@ bool QProcessPrivate::processStarted()
 Q_LONGLONG QProcessPrivate::bytesAvailableFromStdout() const
 {
     DWORD bytesAvail = 0;
-    DWORD r;
-    char dummy;    
-    PeekNamedPipe(standardReadPipe[0], &dummy, 1, &r, &bytesAvail, 0);
-    qDebug("bytesAvailableFromStdout() = %d", bytesAvail); 
+    PeekNamedPipe(standardReadPipe[0], 0, 0, 0, &bytesAvail, 0);
     return bytesAvail;
 }
 
@@ -257,7 +251,6 @@ Q_LONGLONG QProcessPrivate::bytesAvailableFromStderr() const
 {
     DWORD bytesAvail = 0; 
     PeekNamedPipe(errorReadPipe[0], 0, 0, 0, &bytesAvail, 0);
-    qDebug("bytesAvailableFromStderr() = %d", bytesAvail);
     return bytesAvail;
 }
 
@@ -268,7 +261,6 @@ Q_LONGLONG QProcessPrivate::readFromStdout(char *data, Q_LONGLONG maxlen)
 
     if (read > 0 && !ReadFile(standardReadPipe[0], data, read, &bytesRead, 0))
         bytesRead = -1;
-    qDebug("readFromStdout() = %d", bytesRead);
     return bytesRead;
 }
 
@@ -279,15 +271,13 @@ Q_LONGLONG QProcessPrivate::readFromStderr(char *data, Q_LONGLONG maxlen)
 
     if (read > 0 && !ReadFile(errorReadPipe[0], data, read, &bytesRead, 0))
         bytesRead = -1;
-
-    qDebug("readFromStdout() = %d", bytesRead);
     return bytesRead;
 }
 
 void QProcessPrivate::killProcess()
 {
     if (pid)
-	    TerminateProcess(((PROCESS_INFORMATION*)pid)->hProcess, 0xf291);
+        TerminateProcess(((PROCESS_INFORMATION*)pid)->hProcess, 0xf291);
 }
 
 bool QProcessPrivate::waitForStarted(int)
@@ -316,10 +306,11 @@ bool QProcessPrivate::waitForReadyRead(int msecs)
     int nextSleep = SLEEPMIN; 
     for (;;) {
         DWORD bytesAvail = 0; 
-        if (PeekNamedPipe(pipe, 0, 0, 0, &bytesAvail, 0) == 0 || bytesAvail != 0)
+        if (PeekNamedPipe(pipe, 0, 0, 0, &bytesAvail, 0) == 0 || bytesAvail != 0) {
             return true;
+        }
         nextSleep = qMin(qMin(nextSleep, SLEEPMAX), msecs - start.elapsed());
-        if (nextSleep >= 0)
+        if (nextSleep <= 0)
             break;
         Sleep(nextSleep);
         nextSleep *= 2;
@@ -333,8 +324,6 @@ bool QProcessPrivate::waitForReadyRead(int msecs)
 
 bool isProcessStopped(QProcessPrivate * that, int msecs)
 {
-    qDebug("isProcessStopped");
-
     if (!that->pid)
         return true;
 
@@ -344,10 +333,10 @@ bool isProcessStopped(QProcessPrivate * that, int msecs)
 
     DWORD theExitCode;
     if (GetExitCodeProcess(((PROCESS_INFORMATION*)that->pid)->hProcess, &theExitCode)) {
-        if (theExitCode != STILL_ACTIVE) { // this should ever be true?
-            that->exitCode = theExitCode;
-            that->crashed = that->exitCode != 0xf291;
-        }
+        that->exitCode = theExitCode;
+        //### for now we asume a crash if exit code is less than -1 or the maig number
+        if (that->exitCode == 0xf291 || (int)that->exitCode < 0) 
+            that->crashed = true;
     }
     return true;
 }
@@ -356,15 +345,14 @@ bool isProcessStopped(QProcessPrivate * that, int msecs)
 bool QProcessPrivate::waitForFinished(int msecs)
 {
     Q_Q(QProcess);
-
+#if defined QPROCESS_DEBUG
     qDebug("QProcessPrivate::waitForFinished(%d)", msecs);
+#endif
     if (!isProcessStopped(this, msecs)) {
-        qDebug("timeout");
         processError = QProcess::Timedout;
         q->setErrorString(QT_TRANSLATE_NOOP(QProcess, "Process opeation timed out"));
         return false;
     }
-    qDebug("finished");
     processDied();
     return true;
 }
@@ -383,7 +371,6 @@ Q_LONGLONG QProcessPrivate::writeToStdin(const char *data, Q_LONGLONG maxlen)
         }
 		totalWritten += written;
     }
-    qDebug("writeToStdin() = %d", totalWritten); 
     return totalWritten;
 }
 
@@ -404,7 +391,6 @@ void QProcessPrivate::notified()
     notifier->stop();
 
     if (pid && isProcessStopped(this, 0)) {
-        qDebug("processs is stopped");
         processDied();
         return;
     }
