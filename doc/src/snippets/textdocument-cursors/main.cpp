0d39966103fe2003c888ca6b5a3569721b9a6998
dboddie <dboddie@mikaeld-lurven>
2004-11-04 16:47:32 +0100

    Cosmetic change.
    [git-p4: depot-paths = "//depot/qt/main/": change = 158422]

--- doc/src/snippets/textdocument-cursors/main.cpp
+++ doc/src/snippets/textdocument-cursors/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QTextEdit *editor = new QTextEdit();
+    QTextEdit *editor = new QTextEdit;
 
     QTextDocument *document = editor->document();
     QTextCursor redCursor(document);
@@ -20,9 +20,8 @@ int main(int argc, char *argv[])
     for (int i = 0; i < 20; ++i) {
         if (i % 2 == 0)
             redCursor.insertText(QObject::tr("%1 ").arg(i), redFormat);
-        if (i % 5 == 0) {
+        if (i % 5 == 0)
             blueCursor.insertText(QObject::tr("%1 ").arg(i), blueFormat);
-        }
     }
 
     editor->setWindowTitle(QObject::tr("Text Document Cursors"));
