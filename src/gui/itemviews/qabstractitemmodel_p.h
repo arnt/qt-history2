ca1f509f556ffb58b31261f7c67b0daedd25d654
harald <harald@harald-morpork>
2004-11-09 13:22:00 +0100

    export the abstract item model private class and add the usual warning.
    [git-p4: depot-paths = "//depot/qt/main/": change = 158818]

--- src/gui/itemviews/qabstractitemmodel_p.h
+++ src/gui/itemviews/qabstractitemmodel_p.h
@@ -11,6 +11,18 @@
 **
 ****************************************************************************/
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of QAbstractItemModel*.  This header file may change from version
+// to version without notice, or even be removed.
+//
+// We mean it.
+//
+//
+
 #ifndef QABSTRACTITEMMODEL_P_H
 #define QABSTRACTITEMMODEL_P_H
 
@@ -28,7 +40,7 @@ public:
     static void destroy(QPersistentModelIndexData *data);
 };
 
-class QAbstractItemModelPrivate : public QObjectPrivate
+class Q_GUI_EXPORT QAbstractItemModelPrivate : public QObjectPrivate
 {
     Q_DECLARE_PUBLIC(QAbstractItemModel)
 
