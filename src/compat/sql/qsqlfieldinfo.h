50bbd543c75d28bd42fbd49c53802c04d6dcc733
harald <harald@harald-morpork>
2004-11-10 13:45:03 +0100

    add compat headers
    [git-p4: depot-paths = "//depot/qt/main/": change = 159067]

---
+++ src/compat/sql/qsqlfieldinfo.h
@@ -0,0 +1,26 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QSQLFIELDINFO_H
+#define QSQLFIELDINFO_H
+
+#ifndef QT_NO_SQL
+
+#include "q3sqlfieldinfo.h"
+
+typedef Q3SqlFieldInfo QSqlFieldInfo;
+
+#endif
+
+#endif
+
