c77842fb0e35034e3477220bb7e2e7d12104185a
dboddie <dboddie@mikaeld-lurven>
2004-11-10 17:30:50 +0100

    No dragging.
    [git-p4: depot-paths = "//depot/qt/main/": change = 159142]

--- examples/dialogs/tabdialog/tabdialog.cpp
+++ examples/dialogs/tabdialog/tabdialog.cpp
@@ -119,6 +119,7 @@ ApplicationsTab::ApplicationsTab(const QFileInfo &fileInfo, QWidget *parent)
     QLabel *topLabel = new QLabel(tr("Open with:"), this);
 
     QListWidget *applicationsListBox = new QListWidget(this);
+    applicationsListBox->setDraggableItems(false);
     QStringList applications;
 
     for (int i = 1; i <= 30; ++i)
