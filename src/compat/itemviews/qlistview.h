55eaaf328a9c08d9c6553193d9291d46c162405d
ahanssen <ahanssen@:w>
2004-11-11 08:52:21 +0100

    Compat typedefs.
    [git-p4: depot-paths = "//depot/qt/main/": change = 159193]

---
+++ src/compat/itemviews/qlistview.h
@@ -0,0 +1,22 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QLISTVIEW_H
+#define QLISTVIEW_H
+
+#include <q3listview.h>
+typedef Q3ListView QListView;
+typedef Q3ListViewItem QListViewItem;
+typedef Q3ListViewItemIterator QListViewItemIterator;
+
+#endif
