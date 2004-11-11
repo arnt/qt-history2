8da0d677a81fcf07c795be78d7ff92600c0b7f68
ahanssen <ahanssen@:w>
2004-11-11 08:33:01 +0100

    Add typedefs for compat classes whose names don't clash.
    [git-p4: depot-paths = "//depot/qt/main/": change = 159192]

---
+++ src/compat/network/qnetwork.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QNETWORK_H
+#define QNETWORK_H
+
+#include <q3network.h>
+typedef q3InitNetworkProtocols qInitNetworkProtocols;
+
+#endif
