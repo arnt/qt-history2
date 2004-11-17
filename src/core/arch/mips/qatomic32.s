59ce6f12fbd0d719f0ebe6d26c636537206a968c
Brad Hughes <bhughes@trolltech.com>
2004-11-17 10:31:01 +0100

    correct MIPS implementation of atomic test and set
    [git-p4: depot-paths = "//depot/qt/main/": change = 159827]

--- src/core/arch/mips/qatomic32.s
+++ src/core/arch/mips/qatomic32.s
@@ -1,31 +1,34 @@
 	.set nobopt
+	.set noreorder
 	.option pic2
 	.text
+
 	.globl	q_atomic_test_and_set_int
 	.ent	q_atomic_test_and_set_int
 q_atomic_test_and_set_int:
-	.frame	$sp,0,$31
-1:	ll   $3,0($4)
-	.set noreorder
-	bne  $3,$5,2f
-	move $2,$0
-	.set reorder
+1:	ll   $8,0($4)
+	bne  $8,$5,2f
 	move $2,$6
 	sc   $2,0($4)
 	beqz $2,1b
-2:	j    $31
+	nop
+	jr   $31
+	nop
+2:	jr   $31
+	move $2,$0
 	.end	q_atomic_test_and_set_int
+
 	.globl	q_atomic_test_and_set_ptr
 	.ent	q_atomic_test_and_set_ptr
 q_atomic_test_and_set_ptr:
-	.frame	$sp,0,$31
-1:	ll   $3,0($4)
-	.set noreoder
-	bne  $3,$5,2f
-	move $2,$0
-	.set reorder
+1:	ll   $8,0($4)
+	bne  $8,$5,2f
 	move $2,$6
 	sc   $2,0($4)
 	beqz $2,1b
-2:	j    $31
+	nop
+	jr   $31
+	nop
+2:	jr   $31
+	move $2,$0
 	.end	q_atomic_test_and_set_ptr
