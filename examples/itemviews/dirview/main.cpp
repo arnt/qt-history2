1173c882d2bfb5860de39c00a81aae8d8b55a33c
dboddie <dboddie@mikaeld-lurven>
2004-11-24 16:04:04 +0100

    Start from the root directory with a larger window.
    [git-p4: depot-paths = "//depot/qt/main/": change = 160585]

--- examples/itemviews/dirview/main.cpp
+++ examples/itemviews/dirview/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QDirModel *model = new QDirModel(QDir());
+    QDirModel *model = new QDirModel(QDir::root());
 
     QTreeView *tree = new QTreeView;
     tree->setModel(model);
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 
     tree->setWindowTitle(QObject::tr("Dir View"));
     app.setMainWidget(tree);
+    tree->resize(640, 480);
     tree->show();
 
     return app.exec();
