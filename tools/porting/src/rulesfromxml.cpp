5bea064e397659f1f99a750f94de26a9a0ff010b
msorvig <msorvig@dole>
2004-11-25 15:41:55 +0100

    optimize xml parsing
    [git-p4: depot-paths = "//depot/qt/main/": change = 160767]

--- tools/porting/src/rulesfromxml.cpp
+++ tools/porting/src/rulesfromxml.cpp
@@ -1,8 +1,9 @@
 #include "rulesfromxml.h"
 #include <QFile>
-#include <QStringList>
+
 
 RulesFromXml::RulesFromXml(QString xmlFilePath)
+:isParsed(false)
 {
     QFile f(xmlFilePath);
     if(!f.open(QIODevice::ReadOnly)) {
@@ -12,79 +13,76 @@ RulesFromXml::RulesFromXml(QString xmlFilePath)
         qFatal("Xml parsing failed! Error: %s", xml.errorString().latin1());
 }
 
-QList<TokenReplacement*> RulesFromXml::getTokenReplacementRules()
-{
-    int ruleCount = xml["Rules"]["Count"].text().toInt();
-
-    QList<TokenReplacement*> tokenReplacements;
-
-    int rule;
-    for(rule=0; rule<ruleCount; ++rule) {
-        if(xml["Rules"][rule].attribute("Type")=="RenamedHeader") {
-            TokenReplacement* testReplacement = new IncludeTokenReplacement(xml["Rules"][rule]["Qt3"].text().latin1(),
-                                                                            xml["Rules"][rule]["Qt4"].text().latin1());
-            tokenReplacements.append(testReplacement);
-        }
-    }
-
-    return tokenReplacements;
-}
-
 QList<TokenReplacement*> RulesFromXml::getNoPreprocessPortingTokenRules()
 {
-    QList<TokenReplacement*> masterReplaceList;
-    masterReplaceList += getTokenReplacementRules();
-    //TODO: add further rules here.
-
-    int ruleCount = xml["Rules"]["Count"].text().toInt();
-    int rule;
-    for(rule=0; rule<ruleCount; ++rule) {
-        if(xml["Rules"][rule].attribute("Type")=="RenamedClass" ||
-           xml["Rules"][rule].attribute("Type")=="RenamedToken"   ) {
-            TokenReplacement* tokenReplacement = new GenericTokenReplacement(xml["Rules"][rule]["Qt3"].text().latin1(),
-                                                                             xml["Rules"][rule]["Qt4"].text().latin1());
-            masterReplaceList.append(tokenReplacement);
-        }
-
-        if(xml["Rules"][rule].attribute("Type")=="RenamedEnumvalue" ||
-           xml["Rules"][rule].attribute("Type")=="RenamedType"      ||
-           xml["Rules"][rule].attribute("Type")=="RenamedQtSymbol" )
-         {
-            TokenReplacement* tokenReplacement = new ScopedTokenReplacement(xml["Rules"][rule]["Qt3"].text().latin1(),
-                                                                            xml["Rules"][rule]["Qt4"].text().latin1());
-            masterReplaceList.append(tokenReplacement);
-        }
+    if(!isParsed) {
+        parseXml();
+        isParsed=true;
     }
-    return masterReplaceList;
+    return tokenRules;
 }
 
 QStringList RulesFromXml::getHeaderList(QtVersion qtVersion)
 {
-    QString ruleText;
-    if (qtVersion == Qt3)
-        ruleText = "qt3Header";
-    else
-        ruleText = "qt4Header";
-    QStringList headers;
-    int ruleCount = xml["Rules"]["Count"].text().toInt();
-    for(int rule=0; rule<ruleCount; ++rule) {
-         if(xml["Rules"][rule].attribute("Type")==ruleText)
-             headers+=xml["Rules"][rule].text();
-   }
-   return headers;
+    if(!isParsed) {
+       parseXml();
+       isParsed=true;
+    }
+    
+    if (qtVersion==Qt3)
+        return qt3Headers;
+    else //Qt4
+        return qt4Headers;
 }
 
 QStringList RulesFromXml::getNeededHeaderList()
-{
-    QStringList headers;
-    int ruleCount = xml["Rules"]["Count"].text().toInt();
-    for(int rule=0; rule<ruleCount; ++rule) {
-         if(xml["Rules"][rule].attribute("Type")=="NeedHeader")
-             headers+=xml["Rules"][rule]["Header"].text();
+{    
+    if(!isParsed) {
+       parseXml();
+       isParsed=true;
     }
-    return headers;
+    
+    return neededHeaders;
 }
 
+
+void RulesFromXml::parseXml()
+{
+    int ruleCount = xml["Rules"]["Count"].text().toInt();
+        
+    for(int rule=0; rule<ruleCount; ++rule) {
+        QtSimpleXml &currentRule = xml["Rules"][rule];
+        
+        if(currentRule.attribute("Type")=="RenamedHeader") {
+             tokenRules.append(new IncludeTokenReplacement(
+                     currentRule["Qt3"].text().latin1(),
+                     currentRule["Qt4"].text().latin1()));
+        } 
+        else if(currentRule.attribute("Type")=="RenamedClass" ||
+                currentRule.attribute("Type")=="RenamedToken" ) {
+            tokenRules.append(new GenericTokenReplacement(
+                    currentRule["Qt3"].text().latin1(),
+                    currentRule["Qt4"].text().latin1()));
+        } 
+        else if(currentRule.attribute("Type")=="RenamedEnumvalue" ||
+                currentRule.attribute("Type")=="RenamedType" ||
+                currentRule.attribute("Type")=="RenamedQtSymbol" ) {
+            tokenRules.append(new ScopedTokenReplacement(
+                    currentRule["Qt3"].text().latin1(),
+                    currentRule["Qt4"].text().latin1()));
+        }
+        else if(currentRule.attribute("Type")=="NeedHeader") {
+            neededHeaders += currentRule["Header"].text();
+        }
+        else if(currentRule.attribute("Type")=="qt3Header") {
+            qt3Headers += currentRule.text();
+        }
+        else if(currentRule.attribute("Type")=="qt4Header") {
+            qt4Headers += currentRule.text();
+        }
+    
+    }    
+}
 
 
 
--- tools/porting/src/rulesfromxml.h
+++ tools/porting/src/rulesfromxml.h
@@ -2,6 +2,7 @@
 #define RULESFROMXML_H
 
 #include <QList>
+#include <QStringList>
 #include "qtsimplexml.h"
 #include "tokenreplacements.h"
 
@@ -11,12 +12,18 @@ class RulesFromXml
 public:
     enum QtVersion{Qt3, Qt4};
     RulesFromXml(QString xmlFilePath);
-    QList<TokenReplacement*> getTokenReplacementRules();
     QList<TokenReplacement*> getNoPreprocessPortingTokenRules();
     QStringList getHeaderList(QtVersion qtVersion);
     QStringList getNeededHeaderList();
 private:
     QtSimpleXml xml;
+        
+    bool isParsed;
+    QList<TokenReplacement*> tokenRules;
+    QStringList qt3Headers;
+    QStringList qt4Headers;
+    QStringList neededHeaders;
+    void parseXml();    
 };
 
 
