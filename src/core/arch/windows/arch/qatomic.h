67b551466b1c778b9f08d5be86aa74d7345b758e
gunnar <gunnar@THAUMASIA>
2004-11-25 13:22:38 +0100

    MSVC++ 6.0 doesn't generate correct code when optimization are turned on for the interlocked intrinsics.
    Done with Lars and Brad
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 160716]

--- src/core/arch/windows/arch/qatomic.h
+++ src/core/arch/windows/arch/qatomic.h
@@ -18,33 +18,10 @@
 
 #ifndef Q_CC_GNU
 
-// use compiler intrinsics for all atomic functions
-extern "C" {
-    long _InterlockedIncrement(volatile long *);
-    long _InterlockedDecrement(volatile long *);
-    long _InterlockedExchange(volatile long *, long);
-}
-#pragma intrinsic (_InterlockedIncrement)
-#pragma intrinsic (_InterlockedDecrement)
-#pragma intrinsic (_InterlockedExchange)
-
-#ifndef _M_IX86
-extern "C" {
-    long _InterlockedCompareExchange(volatile long *, long, long);
-    void *_InterlockedCompareExchangePointer(void * volatile *, void *, void *);
-    void *_InterlockedExchangePointer(void * volatile *, void *);
-}
-#  pragma intrinsic (_InterlockedCompareExchange)
-#  pragma intrinsic (_InterlockedCompareExchangePointer)
-#  pragma intrinsic (_InterlockedExchangePointer)
-#else
-#  if _MSC_VER >= 1300
-// Let's hope MSVC++.NET gets it right
-extern "C" long _InterlockedCompareExchange(volatile long *, long, long);
-#    pragma intrinsic (_InterlockedCompareExchange)
-#  else
 // MSVC++ 6.0 doesn't generate correct code when optimization are turned on!
-inline long _InterlockedCompareExchange(volatile long *pointer, long newval, long expected)
+#if _MSC_VER < 1300 && defined (_M_IX86)
+
+inline int q_atomic_test_and_set_int(volatile int *pointer, int expected, int newval)
 {
     __asm {
         mov EDX,pointer
@@ -53,14 +30,91 @@ inline long _InterlockedCompareExchange(volatile long *pointer, long newval, lon
         lock cmpxchg dword ptr[EDX],ECX
         mov newval,EAX
     }
+    return newval == expected;
+}
+
+inline int q_atomic_test_and_set_ptr(volatile void *pointer, void *expected, void *newval)
+{
+    __asm {
+        mov EDX,pointer
+        mov EAX,expected
+        mov ECX,newval
+        lock cmpxchg dword ptr[EDX],ECX
+        mov newval,EAX
+    }
+    return newval == expected;
+}
+
+inline int q_atomic_increment(volatile int *pointer)
+{
+    unsigned char retVal;
+    __asm {
+        mov ECX,pointer
+        lock inc DWORD ptr[ECX]
+        setne retVal
+    }
+    return static_cast<int>(retVal);
+}
+
+inline int q_atomic_decrement(volatile int *pointer)
+{
+    unsigned char retVal;
+    __asm {
+        mov ECX,pointer
+        lock dec DWORD ptr[ECX]
+        setne retVal
+    }
+    return static_cast<int>(retVal);
+}
+
+inline int q_atomic_set_int(volatile int *pointer, int newval)
+{
+    __asm {
+        mov EDX,pointer
+        mov ECX,newval
+        lock xchg dword ptr[EDX],ECX
+        mov newval,ECX
+    }
     return newval;
 }
-#  endif // _MSC_VER
-#  define _InterlockedCompareExchangePointer(a,b,c) \
+
+inline void *q_atomic_set_ptr(volatile void *pointer, void *newval)
+{
+    __asm {
+        mov EDX,pointer
+        mov ECX,newval
+        lock xchg dword ptr[EDX],ECX
+        mov newval,ECX
+    }
+    return newval;
+}
+
+#else
+// use compiler intrinsics for all atomic functions
+extern "C" {
+    long _InterlockedIncrement(volatile long *);
+    long _InterlockedDecrement(volatile long *);
+    long _InterlockedExchange(volatile long *, long);
+    long _InterlockedCompareExchange(volatile long *, long, long);
+}
+#  pragma intrinsic (_InterlockedIncrement)
+#  pragma intrinsic (_InterlockedDecrement)
+#  pragma intrinsic (_InterlockedExchange)
+#  pragma intrinsic (_InterlockedCompareExchange)
+
+#  ifndef _M_IX86
+extern "C" {
+    void *_InterlockedCompareExchangePointer(void * volatile *, void *, void *);
+    void *_InterlockedExchangePointer(void * volatile *, void *);
+}
+#    pragma intrinsic (_InterlockedCompareExchangePointer)
+#    pragma intrinsic (_InterlockedExchangePointer)
+#  else
+#    define _InterlockedCompareExchangePointer(a,b,c) \
         reinterpret_cast<void *>(_InterlockedCompareExchange(reinterpret_cast<volatile long *>(a), reinterpret_cast<long>(b), reinterpret_cast<long>(c)))
-#  define _InterlockedExchangePointer(a, b) \
+#    define _InterlockedExchangePointer(a, b) \
         reinterpret_cast<void *>(_InterlockedExchange(reinterpret_cast<volatile long *>(a), reinterpret_cast<long>(b)))
-#endif
+#  endif
 
 inline int q_atomic_test_and_set_int(volatile int *ptr, int expected, int newval)
 { return _InterlockedCompareExchange(reinterpret_cast<volatile long *>(ptr), newval, expected) == expected; }
@@ -79,6 +133,8 @@ inline int q_atomic_set_int(volatile int *ptr, int newval)
 
 inline void *q_atomic_set_ptr(volatile void *ptr, void *newval)
 { return _InterlockedExchangePointer(reinterpret_cast<void * volatile *>(ptr), newval); }
+
+#endif // _MSC_VER ...
 
 #else
 
