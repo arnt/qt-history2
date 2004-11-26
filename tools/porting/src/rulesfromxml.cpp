ff8d3a7588335e10e7c48158d2ee9ed6bc73a4e7
msorvig <msorvig@dole>
2004-11-26 14:15:49 +0100

    temp fix for error in qtsimplexml
    [git-p4: depot-paths = "//depot/qt/main/": change = 160888]

--- tools/porting/src/rulesfromxml.cpp
+++ tools/porting/src/rulesfromxml.cpp
@@ -1,6 +1,9 @@
 #include "rulesfromxml.h"
+#include <iostream>
 #include <QFile>
 
+using std::cout;
+using std::endl;
 
 RulesFromXml::RulesFromXml(QString xmlFilePath)
 :isParsed(false)
@@ -19,6 +22,10 @@ QList<TokenReplacement*> RulesFromXml::getNoPreprocessPortingTokenRules()
         parseXml();
         isParsed=true;
     }
+    
+    if(tokenRules.isEmpty()) {
+         cout << "Warning: token rules list is empty" << endl;
+    }
     return tokenRules;
 }
 
@@ -27,6 +34,9 @@ QStringList RulesFromXml::getHeaderList(QtVersion qtVersion)
     if(!isParsed) {
        parseXml();
        isParsed=true;
+    }
+    if(qt3Headers.isEmpty() || qt4Headers.isEmpty()) {
+         cout << "Warning: headers list is empty" << endl;
     }
     
     if (qtVersion==Qt3)
@@ -42,6 +52,10 @@ QStringList RulesFromXml::getNeededHeaderList()
        isParsed=true;
     }
     
+    if(tokenRules.isEmpty()) {
+         cout << "Warning: needed Headers list is empty" << endl;
+    }
+    
     return neededHeaders;
 }
 
@@ -50,10 +64,13 @@ void RulesFromXml::parseXml()
 {
     int ruleCount = xml["Rules"]["Count"].text().toInt();
         
-    for(int rule=0; rule<ruleCount; ++rule) {
+                       
+    for(int rule=0; rule<=ruleCount; ++rule) {
+   //                    ^ Hack! compensate for off-by-one error somewhere in QtSimpleXml
         QtSimpleXml &currentRule = xml["Rules"][rule];
         
         if(currentRule.attribute("Type")=="RenamedHeader") {
+             cout <<" rename header "<<  currentRule["Qt3"].text().latin1()<<endl;
              tokenRules.append(new IncludeTokenReplacement(
                      currentRule["Qt3"].text().latin1(),
                      currentRule["Qt4"].text().latin1()));
