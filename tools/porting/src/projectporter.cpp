666fdad6999f1bab28f2a92f422c513c84e43279
Lars Knoll <lars@trolltech.com>
2004-11-29 13:57:15 +0100

    if we have CONFIG += opengl, translate this to QT += opengl. port .ui.h files as well.
    [git-p4: depot-paths = "//depot/qt/main/": change = 161066]

--- tools/porting/src/projectporter.cpp
+++ tools/porting/src/projectporter.cpp
@@ -28,8 +28,8 @@ using std::endl;
 
 
 ProjectPorter::ProjectPorter(QString rulesFileName)
-:rulesFileName(rulesFileName)
-,filePorter(rulesFileName)
+    :rulesFileName(rulesFileName),
+     filePorter(rulesFileName)
 {
 
 }
@@ -41,8 +41,8 @@ void ProjectPorter::portProject(QString basePath, QString proFileName)
     if(!infileInfo.exists()) {
         cout<<"Could not open file: " << QDir::convertSeparators(fullInFileName).latin1() << endl;
         return;
-    }     
-   
+    }
+
     QString proFileContents = loadFile(fullInFileName);
     QMap<QString, QString> proFileMap = proFileTagMap(proFileContents, QDir(basePath).absolutePath());
 #if 0
@@ -52,7 +52,7 @@ void ProjectPorter::portProject(QString basePath, QString proFileName)
     puts(proFileMap["INCLUDEPATH"].latin1());
     puts(proFileMap["DEPENDPATH"].latin1());
 #endif
-    
+
     /*
         Check if this is a TEMPLATE = subdirs .pro file, in that case we
         process each subdir (recursively)
@@ -72,11 +72,20 @@ void ProjectPorter::portProject(QString basePath, QString proFileName)
     */
     QStringList sources = proFileMap["SOURCES"].split(" ", QString::SkipEmptyParts);
     QStringList headers = proFileMap["HEADERS"].split(" ", QString::SkipEmptyParts);
-    
+    QStringList forms = proFileMap["FORMS"].split(" ", QString::SkipEmptyParts);
+    QStringList uidoth;
+    for (int i = 0; i < forms.size(); ++i) {
+        QString ui_h = forms.at(i) + ".h";
+        if (QFile::exists(basePath + "/" + ui_h))
+            uidoth += ui_h;
+    }
+
     portFiles(basePath, sources, FilePorter::Source);
     portFiles(basePath, headers, FilePorter::Header);
-    
-    Logger::instance()->setFileState(fullInFileName); 
+    if (!uidoth.isEmpty())
+        portFiles(basePath, uidoth, FilePorter::Source);
+
+    Logger::instance()->setFileState(fullInFileName);
     QString portedProFile = portProFile(proFileContents, proFileMap);
     FileWriter::instance()->writeFileVerbously(fullInFileName , portedProFile.latin1());
 }
@@ -84,21 +93,21 @@ void ProjectPorter::portProject(QString basePath, QString proFileName)
 void ProjectPorter::portFiles(QString basePath, QStringList fileNames, FilePorter::FileType fileType)
 {
     foreach(QString fileName, fileNames) {
-        QString fullFilePath; 
+        QString fullFilePath;
         QFileInfo fileInfo(fileName);
         if (fileInfo.isAbsolute()) {
             fullFilePath = QDir::cleanPath(fileName);
         } else {
             fullFilePath = QDir::cleanPath(basePath + "/" + fileName);
         }
-        
+
         QFileInfo fullFilePathInfo(fullFilePath);
         if(!fullFilePathInfo.exists()) {
-            cout << "Could not find file:" << 
-                QDir::convertSeparators(fullFilePath).latin1() <<endl;        
+            cout << "Could not find file:" <<
+                QDir::convertSeparators(fullFilePath).latin1() <<endl;
             continue;
         }
-       
+
         if(!processedFilesSet.contains(fullFilePath)){
             filePorter.port(QString(), fullFilePath, QString() , fullFilePath, fileType);
             processedFilesSet.insert(fullFilePath, 0);
@@ -108,14 +117,23 @@ void ProjectPorter::portFiles(QString basePath, QStringList fileNames, FilePorte
 
 QString ProjectPorter::portProFile(QString contents, QMap<QString, QString> tagMap)
 {
-   
+
     Logger *logger = Logger::instance();
-    
+
     //add compat to the Qt tag
     QStringList QTTagAdd;
-    QStringList config = tagMap["QT"].split(" ", QString::SkipEmptyParts);
-    if (!config.contains("compat"))
+    QStringList qt = tagMap["QT"].split(" ", QString::SkipEmptyParts);
+    if (!qt.contains("compat"))
         QTTagAdd.append("compat");
+    QStringList config = tagMap["CONFIG"].split(" ", QString::SkipEmptyParts);
+    if (config.contains("opengl"))
+        QTTagAdd.append("opengl");
+    if (config.contains("xml"))
+        QTTagAdd.append("xml");
+    if (config.contains("sql"))
+        QTTagAdd.append("sql");
+    if (config.contains("network"))
+        QTTagAdd.append("network");
 
     if (!QTTagAdd.isEmpty()) {
         contents += "\n#The following line was inserted by the Qt porting tool\n";
@@ -124,9 +142,16 @@ QString ProjectPorter::portProFile(QString contents, QMap<QString, QString> tagM
         QString logText = "Added entry to .pro file: " + insertText;
         logger->addEntry("profile", logText,  QString() , -2, -1); //TODO get line/column here
     }
- 
+    if (!tagMap["FORMS"].isEmpty()) {
+        contents += "\n#The following line was inserted by the Qt porting tool\n";
+        QString insertText = "CONFIG += uic3\n";
+        contents += insertText;
+        QString logText = "Added entry to .pro file: " + insertText;
+        logger->addEntry("profile", logText,  QString() , -2, -1); //TODO get line/column here
+    }
+
     //comment out any REQUIRES tag
-    //TODO: make this more intelligent by checking if REQUIRES really is a tag and 
+    //TODO: make this more intelligent by checking if REQUIRES really is a tag and
     //not the name of a source file for example
     if(!tagMap["REQUIRES"].isEmpty()) {
         int j=0;
@@ -137,6 +162,6 @@ QString ProjectPorter::portProFile(QString contents, QMap<QString, QString> tagM
             j+=insertText.size() + 1;
         }
     }
-         
+
     return contents;
 }
