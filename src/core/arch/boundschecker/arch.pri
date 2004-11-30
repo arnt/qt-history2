3f8e36f6d1a1ce25ef4f8b3dbf0435875b18c10e
Marius Storm-Olsen <marius@trolltech.com>
2004-11-30 15:16:08 +0100

    Add architecture for BoundsChecker. Since BC instruments the code on the IL level it cannot instrument our QAtomic assembly code. It seems that it doens't understand the compiler intrinsic functions either, though that is still bug-report feedback pending. Therefore, add a BoundsChecker QAtomic architecture which uses spinlocks to guard normal C-code pointer-swaps. If not, BoundsChecker it totally useless..
    [git-p4: depot-paths = "//depot/qt/main/": change = 161247]

---
+++ src/core/arch/boundschecker/arch.pri
@@ -0,0 +1,13 @@
+#
+# BoundsChecker architecture
+# Since BoundsChecker doesn't work correctly with assembly and intrinsic
+# functions we need to have an own architecture, which uses spinlocks to
+# be fairly fast and swaps pointers with normal C code.
+#
+
+ARCH_CPP=$$QT_SOURCE_TREE/src/core/arch/boundschecker
+ARCH_H=$$ARCH_CPP/arch
+
+DEPENDPATH += $$ARCH_CPP;$$ARCH_H
+
+HEADERS += $$ARCH_H/qatomic.h
---
+++ src/core/arch/boundschecker/arch/qatomic.h
@@ -0,0 +1,103 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef BOUNDSCHECKER_QATOMIC_H
+#define BOUNDSCHECKER_QATOMIC_H
+
+#include <qglobal.h>
+
+__forceinline int q_atomic_xchg(int newval)
+{
+    static int lockvar = 0;
+    int *pointer = &lockvar;
+
+    __asm {
+        mov EDX,pointer
+        mov ECX,newval
+        xchg dword ptr[EDX],ECX
+        mov newval,ECX
+    }
+    return newval;
+}
+
+inline void q_atomic_lock()
+{
+    while (q_atomic_xchg(~0) != 0)
+        ;
+}
+
+inline void q_atomic_unlock()
+{
+    q_atomic_xchg(0);
+}
+
+inline int q_atomic_test_and_set_int(volatile int *pointer, int expected, int newval)
+{
+    q_atomic_lock();
+    if (*pointer == expected) {
+        *pointer = newval;
+        q_atomic_unlock();
+        return 1;
+    }
+    q_atomic_unlock();
+    return 0;
+}
+
+inline int q_atomic_test_and_set_ptr(volatile void *pointer, void *expected, void *newval)
+{
+    q_atomic_lock();
+    if (*reinterpret_cast<void * volatile *>(pointer) == expected) {
+        *reinterpret_cast<void * volatile *>(pointer) = newval;
+        q_atomic_unlock();
+        return 1;
+    }
+    q_atomic_unlock();
+    return 0;
+}
+
+inline int q_atomic_increment(volatile int *pointer)
+{
+    q_atomic_lock();
+    int ret = ++(*pointer);
+    q_atomic_unlock();
+    return ret;
+}
+
+inline int q_atomic_decrement(volatile int *pointer)
+{
+    q_atomic_lock();
+    int ret = --(*pointer);
+    q_atomic_unlock();
+    return ret;
+}
+
+inline int q_atomic_set_int(volatile int *pointer, int newval)
+{
+    q_atomic_lock();
+    int ret = *pointer;
+    *pointer = newval;
+    q_atomic_unlock();
+    return ret;
+}
+
+inline void *q_atomic_set_ptr(volatile void *pointer, void *newval)
+{
+    q_atomic_lock();
+    void *ret = *reinterpret_cast<void * volatile *>(pointer);
+    *reinterpret_cast<void * volatile *>(pointer) = newval;
+    q_atomic_unlock();
+    return ret;
+
+}
+
+#endif // BOUNDSCHECKER_QATOMIC_H
