d377afcc98ccf4a452d391d6b81af9b114c3dbb2
dboddie <dboddie@mikaeld-lurven>
2004-12-03 16:06:03 +0100

    A snippet for QDrag.
    [git-p4: depot-paths = "//depot/qt/main/": change = 161657]

---
+++ doc/src/snippets/dropevents/window.cpp
@@ -0,0 +1,54 @@
+#include <QtGui>
+
+#include "window.h"
+
+Window::Window(QWidget *parent)
+    : QWidget(parent)
+{
+    QLabel *textLabel = new QLabel(tr("Data:"), this);
+    textInfo = new QTextBrowser(this);
+
+    QLabel *mimeTypeLabel = new QLabel(tr("MIME types:"), this);
+    mimeTypeInfo = new QComboBox(this);
+
+    dropFrame = new QFrame(this);
+    dropFrame->setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
+    QLabel *dropLabel = new QLabel(tr("Drop items here"), dropFrame);
+    dropLabel->setAlignment(Qt::AlignHCenter);
+
+    QVBoxLayout *dropFrameLayout = new QVBoxLayout(dropFrame);
+    dropFrameLayout->addWidget(dropLabel);
+
+    QHBoxLayout *dropLayout = new QHBoxLayout;
+    dropLayout->addStretch(0);
+    dropLayout->addWidget(dropFrame);
+    dropLayout->addStretch(0);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(textLabel);
+    mainLayout->addWidget(textInfo);
+    mainLayout->addWidget(mimeTypeLabel);
+    mainLayout->addWidget(mimeTypeInfo);
+    mainLayout->addSpacing(32);
+    mainLayout->addLayout(dropLayout);
+
+    setAcceptDrops(true);
+    setWindowTitle(tr("Drop Events"));
+}
+
+void Window::dragMoveEvent(QDragMoveEvent *event)
+{
+    if (event->mimeData()->hasFormat("text/plain")
+        && event->answerRect().intersects(dropFrame->geometry()))
+
+        event->acceptProposedAction();
+}
+
+void Window::dropEvent(QDropEvent *event)
+{
+    textInfo->setPlainText(event->mimeData()->text());
+    mimeTypeInfo->clear();
+    mimeTypeInfo->insertStringList(event->mimeData()->formats());
+
+    event->acceptProposedAction();
+}
