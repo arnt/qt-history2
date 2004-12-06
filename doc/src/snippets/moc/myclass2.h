a7574748d97fa52f9a1aefb01dbd23d57fe4279a
Jasmin Blanchette <jasmin@trolltech.com>
2004-12-06 21:19:10 +0100

    Doc: Minor changes to moc docs.
    [git-p4: depot-paths = "//depot/qt/main/": change = 161901]

---
+++ doc/src/snippets/moc/myclass2.h
@@ -0,0 +1,24 @@
+#ifndef MYCLASS2_H
+#define MYCLASS2_H
+
+#include <QObject>
+
+#define MyClass MyClass2
+
+class MyClass : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(Priority priority READ priority WRITE setPriority)
+    Q_ENUMS(Priority)
+
+public:
+    enum Priority { High, Low, VeryHigh, VeryLow };
+
+    MyClass(QObject *parent = 0);
+    ~MyClass();
+
+    void setPriority(Priority priority);
+    Priority priority() const;
+};
+
+#endif
