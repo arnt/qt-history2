fc1a746256250a0ccc497c782de5d8e3ab3f2c66
Roberto Raggi <rraggi@trolltech.com>
2004-12-07 13:58:30 +0100

    ops!
    [git-p4: depot-paths = "//depot/qt/main/": change = 161974]

---
+++ src/tools/uic3/qt3to4.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QT3TO4_H
+#define QT3TO4_H
+
+#include <QString>
+#include <qlist.h>
+#include <qpair.h>
+
+class Porting
+{
+public:
+    typedef QPair<QString, QString> Rule;
+    typedef QList<Rule> RuleList;
+
+public:
+    Porting();
+
+    inline RuleList renamedHeaders() const
+    { return m_renamedHeaders; }
+
+    inline RuleList renamedClasses() const
+    { return m_renamedClasses; }
+
+    QString renameHeader(const QString &headerName) const;
+    QString renameClass(const QString &className) const;
+
+protected:
+    static void readXML(QString fileName = QString(), RuleList *renamedHeaders = 0, RuleList *renamedClasses = 0);
+    static int findRule(const RuleList &rules, const QString &q3);
+
+private:
+    RuleList m_renamedHeaders;
+    RuleList m_renamedClasses;
+};
+
+#endif // QT3TO4_H
