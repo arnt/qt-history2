447e8c7839daa85a4c76cf3c1e25c6e6c9237263
Trond Kjernaasen <trond@trolltech.com>
2004-12-14 16:30:01 +0100

    Remove debug output.
    [git-p4: depot-paths = "//depot/qt/main/": change = 162773]

--- tools/designer/src/components/formeditor/pixmapcache.cpp
+++ tools/designer/src/components/formeditor/pixmapcache.cpp
@@ -1,6 +1,4 @@
 #include <QImage>
-#include <qdebug.h>
-
 #include "pixmapcache.h"
 
 PixmapCache::PixmapCache(QObject *parent)
@@ -10,8 +8,6 @@ PixmapCache::PixmapCache(QObject *parent)
 
 QPixmap PixmapCache::nameToPixmap(const QString &name)
 {
-    qDebug() << "PixmapCache::nameToPixmap()" << name;
-
     NameToPixmapMap::const_iterator it = m_name_to_pixmap.find(name);
     if (it != m_name_to_pixmap.end())
         return *it;
@@ -24,21 +20,14 @@ QPixmap PixmapCache::nameToPixmap(const QString &name)
         return QPixmap();
     m_name_to_pixmap.insert(name, pm);
     m_serial_to_name.insert(pm.serialNumber(), name);
-    
-    qDebug() << "PixmapCache::nameToPixmap() returning" << pm.serialNumber();
-    
     return pm;
 }
 
 QString PixmapCache::pixmapToName(const QPixmap &pm)
 {
-    qDebug() << "PixmapCache::pixmapToName()" << pm.serialNumber();
-    
     SerialNumberToNameMap::const_iterator it = m_serial_to_name.find(pm.serialNumber());
-    if (it != m_serial_to_name.end()) {
-        qDebug() << "PixmapCache::pixmapToName()" << *it;
+    if (it != m_serial_to_name.end())
         return *it;
-    }
     return QString();
 }
 
