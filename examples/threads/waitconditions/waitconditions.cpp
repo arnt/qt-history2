7ed3e6eb66ca7c03ebc6d156b8156367d8d0e98b
Jasmin Blanchette <jasmin@trolltech.com>
2005-01-07 15:21:46 +0100

    Added a Wait Conditions example, similar to the one in the Qt book.
    [git-p4: depot-paths = "//depot/qt/main/": change = 164193]

---
+++ examples/threads/waitconditions/waitconditions.cpp
@@ -0,0 +1,75 @@
+#include <QCoreApplication>
+#include <QMutex>
+#include <QThread>
+#include <QWaitCondition>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const int DataSize = 100000;
+const int BufferSize = 8192;
+char buffer[BufferSize];
+
+QWaitCondition bufferNotFull;
+QWaitCondition bufferNotEmpty;
+QMutex mutex;
+int numUsedBytes = 0;
+
+class Producer : public QThread
+{
+public:
+    void run();
+};
+
+void Producer::run()
+{
+    for (int i = 0; i < DataSize; ++i) {
+        mutex.lock();
+        if (numUsedBytes == BufferSize)
+            bufferNotFull.wait(&mutex);
+        mutex.unlock();
+
+        buffer[i % BufferSize] = "ACGT"[(int)rand() % 4];
+
+        mutex.lock();
+        ++numUsedBytes;
+        bufferNotEmpty.wakeAll();
+        mutex.unlock();
+    }
+}
+
+class Consumer : public QThread
+{
+public:
+    void run();
+};
+
+void Consumer::run()
+{
+    for (int i = 0; i < DataSize; ++i) {
+        mutex.lock();
+        if (numUsedBytes == 0)
+            bufferNotEmpty.wait(&mutex);
+        mutex.unlock();
+
+        fprintf(stderr, "%c", buffer[i % BufferSize]);
+
+        mutex.lock();
+        --numUsedBytes;
+        bufferNotFull.wakeAll();
+        mutex.unlock();
+    }
+    fprintf(stderr, "\n");
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    Producer producer;
+    Consumer consumer;
+    producer.start();
+    consumer.start();
+    producer.wait();
+    consumer.wait();
+    return 0;
+}
