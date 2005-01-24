6771bc9d1b58838f9bc2089d63c9f56e52741477
dboddie <dboddie@mikaeld-lurven>
2005-01-24 14:58:02 +0100

    API fixes.
    [git-p4: depot-paths = "//depot/qt/main/": change = 165577]

--- examples/itemviews/pixelator/mainwindow.cpp
+++ examples/itemviews/pixelator/mainwindow.cpp
@@ -85,7 +85,7 @@ void MainWindow::printImage()
             return;
     }
 
-    QPrinter printer;
+    QPrinter printer(QPrinter::PrinterResolution);
 
     QPrintDialog *dlg = new QPrintDialog(&printer, this);
     dlg->setWindowTitle(tr("Print Image"));
