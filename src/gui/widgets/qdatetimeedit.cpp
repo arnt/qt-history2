875e623446e4b1a4251d7eea48936810d9d71973
Matthias Ettrich <ettrich@trolltech.com>
2005-01-27 20:06:32 +0100

    compile, at least. Not sure if this works. How was this supposed to work?
    [git-p4: depot-paths = "//depot/qt/main/": change = 165961]

--- src/gui/widgets/qdatetimeedit.cpp
+++ src/gui/widgets/qdatetimeedit.cpp
@@ -2110,7 +2110,7 @@ int QDateTimeEditPrivate::sectionValue(Section s, QString *text, QValidator::Sta
                 state = (num == -1 ? QValidator::Invalid : QValidator::Acceptable);
             } else {
                 const QChar ch = (st.at(0) == space ? st.at(1) : st.at(0));
-                switch (ch.latin1()) {
+                switch (ch.toLatin1()) {
                 case 'p': num = 1;
                 case 'a':
                 case 'm': state = QValidator::Intermediate; break;
@@ -2419,7 +2419,7 @@ QValidator::State QDateTimeEditPrivate::checkIntermediate(const QDateTime &dt, c
                 int tmp = dt.date().month(); // I know the first possible month makes the date too small
                 while ((tmp = findMonth(t, tmp + 1)) != -1) {
                     const QCoreVariant copy(dt.addMonths(tmp - dt.date().month()));
-                    if (copy >= minimum && copy <= maximum)
+                    if (copy.toInt() >= minimum.toInt() && copy.toInt() <= maximum.toInt())
                         break;
                 }
                 if (tmp == -1)
@@ -2429,7 +2429,7 @@ QValidator::State QDateTimeEditPrivate::checkIntermediate(const QDateTime &dt, c
             case AmPmLowerCaseSection:
                 if (tooSmall && (t.count(space) == 2 || t.contains('m'))) {
                     const QCoreVariant copy(dt.addSecs(12 * 60 * 60));
-                    if (copy >= minimum && copy <= maximum)
+                    if (copy.toInt() >= minimum.toInt() && copy.toInt() <= maximum.toInt())
                         break;
                 }
                 return QValidator::Invalid;
@@ -2467,7 +2467,7 @@ QValidator::State QDateTimeEditPrivate::checkIntermediate(const QDateTime &dt, c
 
                     QCoreVariant var(dt);
                     setDigit(&var, sn.section, tmp);
-                    Q_ASSERT(var >= minimum);
+                    Q_ASSERT(var.toInt() >= minimum.toInt());
                     if (var > maximum) {
                         QDTEDEBUG("invalid because %s > %s",
                                   var.toString().toLatin1().constData(),
