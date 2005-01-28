08d7161717b9b503a472b60b83faa7aa513ece7e
Thomas Strehl <thomas@trolltech.com>
2005-01-28 11:11:57 +0100

    Since _vsnprintf is declared as deprecated in VS 2005 and the secure version of this function has a different signature use the Qt implementation of this function.
    [git-p4: depot-paths = "//depot/qt/main/": change = 166006]

--- mkspecs/win32-msvc.net/qplatformdefs.h
+++ mkspecs/win32-msvc.net/qplatformdefs.h
@@ -73,9 +73,8 @@
 
 #define QT_SIGNAL_ARGS		int
 
-#if defined(_MSC_VER) && _MSC_VER >= 1400
-#define QT_VSNPRINTF(buffer, count, format, arg) \
-	vsnprintf_s(buffer, count, count-1, format, arg)
+#if defined(_MSC_VER) && _MSC_VER >= 1400 && defined(QT_VSNPRINTF)
+# undef QT_VSNPRINTF // use the Qt implementation of vsnprintf
 #else
 #define QT_VSNPRINTF ::_vsnprintf
 #endif
