6c469943b0011e32f7171457c72f41bfd694f919
Roberto Raggi <rraggi@trolltech.com>
2005-02-03 16:35:38 +0100

    the delegate
    [git-p4: depot-paths = "//depot/qt/main/": change = 166585]

---
+++ tools/designer/src/shared/sheet_delegate.cpp
@@ -0,0 +1,62 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "sheet_delegate.h"
+
+#include <QAbstractItemModel>
+#include <QTreeView>
+
+#include <QStyle>
+#include <QPainter>
+
+SheetDelegate::SheetDelegate(QTreeView *view, QWidget *parent)
+    : QItemDelegate(parent),
+      m_view(view)
+{
+}
+
+void SheetDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    const QAbstractItemModel *model = index.model();
+    Q_ASSERT(model);
+
+    if (!model->parent(index).isValid()) {
+        // this is a top-level item.
+        QStyleOptionButton buttonOption;
+
+        buttonOption.state = option.state;
+        // buttonOption.state |= QStyle::State_Raised;
+        buttonOption.state &= ~QStyle::State_HasFocus;
+
+        buttonOption.rect = option.rect;
+        buttonOption.palette = option.palette;
+        buttonOption.features = QStyleOptionButton::None;
+        buttonOption.text = model->data(index, QAbstractItemModel::DisplayRole).toString();
+        m_view->style()->drawControl(QStyle::CE_PushButton, &buttonOption, painter, m_view);
+
+        QStyleOption branchOption;
+        static const int i = 9; // ### hardcoded in qcommonstyle.cpp
+        QRect r = option.rect;
+        branchOption.rect = QRect(r.left() + i/2, r.top() + (r.height() - i)/2, i, i);
+        branchOption.palette = option.palette;
+        branchOption.state = QStyle::State_Children;
+
+        if (m_view->isOpen(index))
+            branchOption.state |= QStyle::State_Open;
+
+        m_view->style()->drawPrimitive(QStyle::PE_IndicatorBranch, &branchOption, painter, m_view);
+    } else {
+        QItemDelegate::paint(painter, option, index);
+    }
+}
+
