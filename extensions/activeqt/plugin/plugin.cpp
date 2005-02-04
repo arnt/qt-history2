c354686a47a6001a8cc7268471602c5402a7816d
Sverre <sdjonne@trolltech.com>
2005-02-04 10:25:31 +0100

    Initial submit of new qt4 QAxWidget plugin for designer.
    [git-p4: depot-paths = "//depot/qt/main/": change = 166646]

---
+++ extensions/activeqt/plugin/plugin.cpp
@@ -0,0 +1,353 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <customwidget.h>
+#include <abstractformeditor.h>
+#include <abstractformwindowmanager.h>
+#include <abstractformwindowcursor.h>
+#include <qextensionmanager.h>
+#include <default_propertysheet.h>
+#include <taskmenu.h>
+
+#include <qplugin.h>
+
+#include <QObject>
+#include <QTimer>
+#include <QIcon>
+#include <QMessageBox>
+#include <QPainter>
+
+#include <QUuid>
+#include <QAxWidget>
+#include <olectl.h>
+#include "../container/qaxselect.h"
+
+/* XPM */
+static const char *widgetIcon[]={
+"22 22 6 1",
+"a c #000000",
+"# c #808080",
+"+ c #aaa5a0",
+"b c #dddddd",
+"* c #d4d0c8",
+". c none",
+".........#aa#...#aa#..",
+".........abba...abba..",
+".........abba...abba..",
+".........#aa#...#aa#..",
+"..........aa.....aa...",
+"..........aa.....aa...",
+"..........aa.....aa...",
+".......aaaaaaaaaaaaaaa",
+".......a*************a",
+".......a************#a",
+".......a***********+#a",
+".......a***********+#a",
+".......a***********+#a",
+"#aa#...a***********+#a",
+"abbaaaaa***********+#a",
+"abbaaaaa***********+#a",
+"#aa#...a***********+#a",
+".......a***********+#a",
+".......a***********+#a",
+".......a**++++++++++#a",
+".......a*############a",
+".......aaaaaaaaaaaaaaa"};
+
+class QActiveXPlugin;
+
+class QActiveXPluginObject : public QAxWidget
+{
+public:
+    QActiveXPluginObject(QWidget *parent)
+        : QAxWidget(parent), m_hasControl(false)
+    { 
+        m_axImage = QPixmap(widgetIcon);
+    }
+    
+    ~QActiveXPluginObject()
+    { 
+    }
+
+    bool setControl(const QString &clsid)
+    {
+        m_hasControl = QAxWidget::setControl(clsid);
+        return m_hasControl;
+    }
+
+    QSize sizeHint() const
+    {
+        return QSize(80,70);
+    }
+
+protected:
+    void paintEvent (QPaintEvent *event)
+    {
+        if (!m_hasControl)
+        {
+            QPainter p(this);
+            QRect r = contentsRect();
+            p.drawRect(r.adjusted(0,0,-1,-1));
+            p.drawPixmap((r.width()-m_axImage.width())/2,
+                (r.height()-m_axImage.height())/2,m_axImage);
+        }
+        else
+        {
+            QAxWidget::paintEvent(event);
+        }
+    }
+
+private:
+    QPixmap m_axImage;
+    bool m_hasControl;
+};
+
+class QActiveXPropertySheet: public QDesignerPropertySheet
+{
+    Q_OBJECT
+    Q_INTERFACES(IPropertySheet)
+public:
+    QActiveXPropertySheet(QAxWidget *object, QObject *parent = 0)
+        : QDesignerPropertySheet(object, parent)
+    { 
+        createFakeProperty("control", QString());
+        int index = indexOf("control");
+        setVisible(index, false);
+    }
+
+    void setProperty(int index, const QVariant &value)
+    {
+        QDesignerPropertySheet::setProperty(index, value);
+
+        if (isAdditionalProperty(index) 
+            && (propertyName(index) == "control"))
+        {
+            if (!value.toString().isEmpty())
+                static_cast<QActiveXPluginObject*>(m_object)->setControl(value.toString());
+        }
+    }
+
+    virtual ~QActiveXPropertySheet() { }
+};
+
+class QActiveXTaskMenu: public QObject, public ITaskMenu
+{
+    Q_OBJECT
+    Q_INTERFACES(ITaskMenu)
+public:
+    QActiveXTaskMenu(QAxWidget *object, QObject *parent = 0)
+        : QObject(parent), m_axwidget(object) { }
+    virtual ~QActiveXTaskMenu() { }
+
+    virtual QList<QAction*> taskActions() const
+    {
+        if (!m_taskActions.isEmpty())
+            return m_taskActions;
+
+        QAction *action = 0;
+        QActiveXTaskMenu *that = const_cast<QActiveXTaskMenu*>(this);
+
+        action = new QAction(that);
+        action->setText(tr("Set Control"));
+        connect(action, SIGNAL(triggered()), this, SLOT(setActiveXControl()));
+        m_taskActions.append(action);
+        
+        return m_taskActions;
+    }
+
+    static void setActiveXControl(QActiveXPluginObject *widget)
+    {
+       QAxSelect *dialog = new QAxSelect(widget->topLevelWidget());
+
+        if (dialog->exec())
+        {
+	    QUuid clsid = dialog->clsid();
+	    QString key;
+
+	    IClassFactory2 *cf2 = 0;
+	    CoGetClassObject(clsid, CLSCTX_SERVER, 0, IID_IClassFactory2, (void**)&cf2);
+	    if (cf2)
+            {
+	        BSTR bKey;
+	        HRESULT hres = cf2->RequestLicKey(0, &bKey);
+	        if (hres == CLASS_E_NOTLICENSED) {
+		    QMessageBox::warning(widget->topLevelWidget(), tr("Licensed Control"),
+		        tr("The control requires a design-time license"));
+		    clsid = QUuid();
+	        }
+                else
+                {
+		    key = QString::fromUtf16(bKey);
+	        }
+                cf2->Release();
+            }
+
+	    if (!clsid.isNull())
+            {
+                AbstractFormWindow *formWin = AbstractFormWindow::findFormWindow(widget);
+                formWin->selectWidget(widget, true);
+
+	        if (key.isEmpty())
+                    formWin->cursor()->setProperty(QLatin1String("control"), clsid.toString());
+	        else
+		    formWin->cursor()->setProperty(QLatin1String("control"), clsid.toString() + ":" + key);
+            }
+        }
+
+        delete dialog;
+    }
+
+private slots:
+    void setActiveXControl()
+    { QTimer::singleShot(100, this, SLOT(openLater())); }
+
+    void openLater()
+    { setActiveXControl(static_cast<QActiveXPluginObject*>(m_axwidget)); }
+
+private:
+    QAxWidget *m_axwidget;
+    mutable QList<QAction*> m_taskActions;
+};
+
+class QActiveXExtensionFactory: public DefaultExtensionFactory
+{
+    Q_OBJECT
+    Q_INTERFACES(ExtensionFactory)
+public:
+    QActiveXExtensionFactory(QExtensionManager *parent = 0)
+        : DefaultExtensionFactory(parent) { }
+
+protected:
+    virtual QObject *createExtension(QObject *object, const QString &iid, QObject *parent) const
+    {
+        QAxWidget *w = qt_cast<QAxWidget*>(object);
+
+        if (!w)
+            return 0;
+
+        if (iid == Q_TYPEID(IPropertySheet))
+            return new QActiveXPropertySheet(w, parent);
+
+        if (iid == Q_TYPEID(ITaskMenu))
+            return new QActiveXTaskMenu(w, parent);
+        
+        return 0;
+    }
+};
+
+class QActiveXPlugin : public QObject, public ICustomWidget
+{
+    Q_OBJECT
+    Q_INTERFACES(ICustomWidget)
+public:
+    inline QActiveXPlugin(QObject *parent = 0)
+        : QObject(parent), m_core(0), m_activex(0) {}
+        
+    virtual QString name() const
+    { return QLatin1String("QAxWidget"); }
+    
+    virtual QString group() const
+    { return QLatin1String("Containers"); }
+    
+    virtual QString toolTip() const
+    { return tr("ActiveX control"); }
+    
+    virtual QString whatsThis() const
+    { return tr("ActiveX control widget"); }
+    
+    virtual QString includeFile() const
+    { return QLatin1String("qaxwidget.h"); }
+    
+    virtual QIcon icon() const
+    { return QIcon(widgetIcon); }
+
+    virtual bool isContainer() const
+    { return false; }
+    
+    virtual bool isForm() const
+    { return false; }
+
+    virtual QWidget *createWidget(QWidget *parent)
+    {
+        m_activex = new QActiveXPluginObject(parent);
+        return m_activex;
+    }
+    
+    virtual bool isInitialized() const 
+    { return (m_core != 0); }
+    
+    virtual void initialize(AbstractFormEditor *core) 
+    { 
+        if (m_core != 0)
+            return;
+
+        m_core = core;
+
+#ifndef NO_INITIAL_POPUP
+        connect(m_core->formWindowManager(), SIGNAL(formWindowAdded(AbstractFormWindow *)),
+            this, SLOT(formWindowAdded(AbstractFormWindow *)));
+
+        connect(m_core->formWindowManager(), SIGNAL(formWindowRemoved(AbstractFormWindow *)),
+            this, SLOT(formWindowRemoved(AbstractFormWindow *)));
+#endif //NO_INITIAL_POPUP
+
+        QExtensionManager *mgr = core->extensionManager();
+        QActiveXExtensionFactory *axf = new QActiveXExtensionFactory(mgr);
+        mgr->registerExtensions(axf, Q_TYPEID(IPropertySheet));
+        mgr->registerExtensions(axf, Q_TYPEID(ITaskMenu));
+    }
+    
+    virtual QString codeTemplate() const
+    { return QString::null; }
+
+#ifndef NO_INITIAL_POPUP
+private slots:
+    void initActiveXControl(QWidget *widget)
+    {
+        if (widget == m_activex)
+            QTimer::singleShot(100, this, SLOT(openLater()));
+    }
+
+    void openLater()
+    {
+        // no need to set a control that is already set
+        if (m_activex->control().isEmpty())
+            QActiveXTaskMenu::setActiveXControl(m_activex);
+        m_activex = 0;
+    }
+
+    void formWindowAdded(AbstractFormWindow *formWindow)
+    {
+        connect(formWindow, SIGNAL(widgetManaged(QWidget *)),
+            this, SLOT(initActiveXControl(QWidget *)));
+        connect(formWindow, SIGNAL(widgetUnmanaged(QWidget *)),
+            this, SLOT(initActiveXControl(QWidget *)));
+    }
+
+    void formWindowRemoved(AbstractFormWindow *formWindow)
+    {
+        disconnect(formWindow, SIGNAL(widgetManaged(QWidget *)),
+            this, SLOT(setActiveXControl(QWidget *)));
+        disconnect(formWindow, SIGNAL(widgetUnmanaged(QWidget *)),
+            this, SLOT(setActiveXControl(QWidget *)));
+    }
+#endif //NO_INITIAL_POPUP
+    
+private:
+    AbstractFormEditor *m_core;
+    QActiveXPluginObject *m_activex;
+};
+
+Q_EXPORT_PLUGIN(QActiveXPlugin)
+
+#include "plugin.moc"
---
+++ extensions/activeqt/plugin/qaxwidget.pro
@@ -0,0 +1,23 @@
+TEMPLATE = lib
+
+IDEDIR = $$QT_BUILD_TREE/tools/designer
+DESTDIR = $$QT_BUILD_TREE/plugins/designer
+
+DEFINES += NO_INITIAL_POPUP
+
+INCLUDEPATH += \
+    $$IDEDIR/src/lib/sdk \
+    $$IDEDIR/src/lib/extension \
+    $$IDEDIR/src/uilib \
+    $$IDEDIR/src/shared
+
+# Input
+SOURCES += plugin.cpp
+CONFIG += qt warn_on qaxcontainer
+
+
+CONFIG(debug, debug|release) {
+    LIBS += $$IDEDIR/lib/sharedd.lib
+} else {
+    LIBS += $$IDEDIR/lib/shared.lib
+}
