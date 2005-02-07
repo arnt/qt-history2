c7b2ef976e63768a1e0bc366772e153f2a3b4b4d
Sam Magnuson <sam@trolltech.com>
2005-02-07 23:02:45 +0100

    plug memory leak as asked by customer in #65659
    [git-p4: depot-paths = "//depot/qt/main/": change = 166781]

--- src/gui/dialogs/qfiledialog_mac.cpp
+++ src/gui/dialogs/qfiledialog_mac.cpp
@@ -138,6 +138,7 @@ static Boolean qt_mac_nav_filter(AEDesc *theItem, void *info,
             tmp[str[0]] = '\0';
             file = tmp;
         }
+        DisposeHandle((Handle)alias);
     } else if (theItem->descriptorType == typeFSRef) {
         FSRef ref;
         AEGetDescData(theItem, &ref, sizeof(ref));
