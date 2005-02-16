eef69be69ee1ff89c4f6dac1fa5ec619f354a061
Brad Hughes <bhughes@trolltech.com>
2005-02-16 14:30:29 +0100

    don't know why he removed the .h
    [git-p4: depot-paths = "//depot/qt/main/": change = 167897]

--- src/core/tools/qmap.h
+++ src/core/tools/qmap.h
@@ -822,7 +822,7 @@ private:
 };
 
 #ifndef QT_NO_DEBUG_OUTPUT
-#include <QtCore/qdebug>
+#include <QtCore/qdebug.h>
 template <class aKey, class aT>
 Q_CORE_EXPORT Q_OUTOFLINE_TEMPLATE QDebug operator<<(QDebug dbg, const QMap<aKey, aT> &map)
 {
