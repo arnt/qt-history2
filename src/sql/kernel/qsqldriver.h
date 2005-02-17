2abaf37b07b2cee9482eaf4c243565c80a713f29
harald <harald@harald-morpork>
2005-02-17 16:51:10 +0100

    no need for a virtual hook since it is a QObject
    [git-p4: depot-paths = "//depot/qt/main/": change = 168122]

--- src/sql/kernel/qsqldriver.h
+++ src/sql/kernel/qsqldriver.h
@@ -83,8 +83,6 @@ protected:
     virtual void setOpenError(bool e);
     virtual void setLastError(const QSqlError& e);
 
-    virtual void virtual_hook(int id, void *data);
-
 private:
     Q_DISABLE_COPY(QSqlDriver)
 };
