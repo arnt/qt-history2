faf35b406922cd50d4555e06674a6157dd1acf05
ahanssen <ahanssen@:w>
2005-02-21 14:22:55 +0100

    Renaming the QDns class to QHostInfo.
    [git-p4: depot-paths = "//depot/qt/main/": change = 168467]

---
+++ src/network/qhostinfo_p.h
@@ -0,0 +1,113 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QDNS_P_H
+#define QDNS_P_H
+
+#include <qcoreapplication.h>
+#include <private/qcoreapplication_p.h>
+#include "qdns.h"
+#include <qmutex.h>
+#include <qwaitcondition.h>
+#include <qobject.h>
+#include <qpointer.h>
+
+#if !defined QT_NO_THREAD
+#include <qthread.h>
+#    define QDnsAgentBase QThread
+#else
+#    define QDnsAgentBase QObject
+#endif
+
+class QDnsResult : public QObject
+{
+    Q_OBJECT
+public:
+    inline void emitResultsReady(const QDnsHostInfo &info)
+    {
+        emit resultsReady(info);
+    }
+signals:
+    void resultsReady(const QDnsHostInfo &info);
+};
+
+struct QDnsQuery
+{
+    inline QDnsQuery() : object(0) {}
+    inline ~QDnsQuery() { delete object; }
+    inline QDnsQuery(const QString &name, QDnsResult *result)
+        : hostName(name), object(result) {}
+
+    QString hostName;
+    QDnsResult *object;
+};
+
+class QDnsAgent : public QDnsAgentBase
+{
+    Q_OBJECT
+public:
+    inline QDnsAgent()
+    {
+        connect(QCoreApplication::instance(), SIGNAL(aboutToQuit()), this, SLOT(cleanup()));
+        connect(QCoreApplication::instance(), SIGNAL(destroyed(QObject *)), this, SLOT(cleanup()));
+        QCoreApplicationPrivate::moveToMainThread(this);
+        quit = false;
+    }
+    inline ~QDnsAgent()
+    { cleanup(); }
+
+    void run();
+    static QDnsHostInfo getHostByName(const QString &hostName);
+
+    inline void addHostName(const QString &name, QDnsResult *result)
+    {
+        QMutexLocker locker(&mutex);
+        queries << new QDnsQuery(name, result);
+        cond.wakeOne();
+    }
+
+public slots:
+    inline void cleanup()
+    {
+        {
+            QMutexLocker locker(&mutex);
+            queries.clear();
+            quit = true;
+            cond.wakeOne();
+        }
+        wait();
+    }
+
+private:
+    QList<QDnsQuery *> queries;
+    QMutex mutex;
+    QWaitCondition cond;
+    bool quit;
+};
+
+class QDnsHostInfoPrivate
+{
+public:
+    inline QDnsHostInfoPrivate()
+        : err(QDnsHostInfo::NoError),
+          errorStr(QT_TRANSLATE_NOOP("QDnsHostInfo", "Unknown error"))
+    {
+    }
+
+    QDnsHostInfo::Error err;
+    QString errorStr;
+    QList<QHostAddress> addrs;
+    QString hostName;
+};
+
+#endif // QDNS_P_H
---
+++ src/network/qhostinfo_unix.cpp
@@ -0,0 +1,132 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "qplatformdefs.h"
+
+#include "qdns_p.h"
+#include "qiodevice.h"
+#include <qbytearray.h>
+
+extern "C" {
+#include <netdb.h>
+}
+
+//#define QDNS_DEBUG
+
+QDnsHostInfo QDnsAgent::getHostByName(const QString &hostName)
+{
+    QDnsHostInfo results;
+    results.d->hostName = hostName;
+
+#if defined(QDNS_DEBUG)
+    qDebug("QDnsAgent::getHostByName(%s) looking up...", hostName.latin1());
+#endif
+
+#if !defined (QT_NO_GETADDRINFO)
+    // Call getaddrinfo, and place all IPv4 addresses at the start and
+    // the IPv6 addresses at the end of the address list in results.
+    addrinfo *res = 0;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;
+
+    int result = getaddrinfo(hostName.toLatin1().constData(), 0, &hints, &res);
+    if (result == 0) {
+        addrinfo *node = res;
+        while (node) {
+            if (node->ai_family == AF_INET) {
+                QHostAddress addr;
+                addr.setAddress(ntohl(((sockaddr_in *) node->ai_addr)->sin_addr.s_addr));
+                if (!results.d->addrs.contains(addr))
+                    results.d->addrs.prepend(addr);
+            } else if (node->ai_family == AF_INET6) {
+                QHostAddress addr;
+                addr.setAddress(((sockaddr_in6 *) node->ai_addr)->sin6_addr.s6_addr);
+                if (!results.d->addrs.contains(addr))
+                    results.d->addrs.append(addr);
+            } else {
+                results.d->err = QDnsHostInfo::UnknownError;
+                results.d->errorStr = tr("Unknown address type");
+                break;
+            }
+            node = node->ai_next;
+        }
+
+        freeaddrinfo(res);
+    } else if (result == EAI_NONAME
+#ifdef EAI_NODATA
+	       // EAI_NODATA is deprecated in RFC 3493
+	       || result == EAI_NODATA
+#endif
+	       ) {
+        results.d->err = QDnsHostInfo::HostNotFound;
+        results.d->errorStr = tr("Host not found");
+    } else {
+        results.d->err = QDnsHostInfo::UnknownError;
+        results.d->errorStr = QString::fromLocal8Bit(gai_strerror(result));
+    }
+
+#else
+    // Fall back to gethostbyname for platforms that don't define
+    // getaddrinfo. gethostbyname does not support IPv6, and it's not
+    // reentrant on all platforms. For now this is okay since we only
+    // use one QDnsAgent, but if more agents are introduced, locking
+    // must be provided.
+    hostent *result = gethostbyname(hostName.latin1());
+    if (result) {
+        if (result->h_addrtype == AF_INET) {
+            for (char **p = result->h_addr_list; *p != 0; p++) {
+                QHostAddress addr;
+                addr.setAddress(ntohl(*((quint32 *)*p)));
+                if (!results.d->addrs.contains(addr))
+                    results.d->addrs.prepend(addr);
+            }
+        } else {
+            results.d->err = QDnsHostInfo::UnknownError;
+            results.d->errorStr = tr("Unknown address type");
+        }
+    } else if (h_errno == HOST_NOT_FOUND || h_errno == NO_DATA
+               || h_errno == NO_ADDRESS) {
+        results.d->err = QDnsHostInfo::HostNotFound;
+        results.d->errorStr = tr("Host not found");
+    } else {
+        results.d->err = QDnsHostInfo::UnknownError;
+        results.d->errorStr = tr("Unknown error");
+    }
+#endif //  !defined (QT_NO_GETADDRINFO)
+
+#if defined(QDNS_DEBUG)
+    if (results.d->err != QDnsHostInfo::NoError) {
+        qDebug("QDnsAgent::getHostByName(): error #%d %s",
+               h_errno, results.d->errorStr.latin1());
+    } else {
+        QString tmp;
+        for (int i = 0; i < results.d->addrs.count(); ++i) {
+            if (i != 0) tmp += ", ";
+            tmp += results.d->addrs.at(i).toString();
+        }
+        qDebug("QDnsAgent::getHostByName(): found %i entries for \"%s\": {%s}",
+               results.d->addrs.count(), hostName.latin1(), tmp.latin1());
+    }
+#endif
+    return results;
+}
+
+QString QDns::getHostName()
+{
+    char hostName[512];
+    if (gethostname(hostName, sizeof(hostName)) == -1)
+        return QString::null;
+    hostName[sizeof(hostName) - 1] = '\0';
+    return QString::fromLocal8Bit(hostName);
+}
