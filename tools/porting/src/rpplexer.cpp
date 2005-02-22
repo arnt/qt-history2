9b4b4d41d4bb5010fb70e9613deaa7c3bb7bd371
msorvig <msorvig@dole>
2005-02-22 13:10:12 +0100

    Legal requested that the order of the copyright notices should be changed,    now Trolltech's notice is listed first.
    [git-p4: depot-paths = "//depot/qt/main/": change = 168692]

--- tools/porting/src/rpplexer.cpp
+++ tools/porting/src/rpplexer.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 **
-** Copyright (C) 2001-2004 Roberto Raggi
 ** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+** Copyright (C) 2001-2004 Roberto Raggi
 **
 ** This file is part of the $MODULE$ of the Qt Toolkit.
 **
