89e851aceb7653d8d81bcfa8b4007bb9c9bb41de
dboddie <dboddie@mikaeld-lurven>
2005-02-22 11:45:22 +0100

    Added copyright header.
    [git-p4: depot-paths = "//depot/qt/main/": change = 168678]

--- examples/itemviews/chart/main.cpp
+++ examples/itemviews/chart/main.cpp
@@ -1,3 +1,15 @@
+/****************************************************************************
+**
+** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of an example program for Qt.
+** EDITIONS: NOLIMITS
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #include <QApplication>
 
 #include "mainwindow.h"
