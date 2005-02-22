91fb5ead09773d1567704ddc11485df777e056b6
dboddie <dboddie@mikaeld-lurven>
2005-02-22 13:30:37 +0100

    Combo box API fixes.
    [git-p4: depot-paths = "//depot/qt/main/": change = 168698]

--- doc/src/snippets/designer/multipleinheritance/imagedialog.cpp
+++ doc/src/snippets/designer/multipleinheritance/imagedialog.cpp
@@ -7,12 +7,12 @@ ImageDialog::ImageDialog(QWidget *parent)
 {
     setupUi(this);
 
-    colorDepthCombo->insertItem(tr("2 colors (1 bit per pixel)"));
-    colorDepthCombo->insertItem(tr("4 colors (2 bits per pixel)"));
-    colorDepthCombo->insertItem(tr("16 colors (4 bits per pixel)"));
-    colorDepthCombo->insertItem(tr("256 colors (8 bits per pixel)"));
-    colorDepthCombo->insertItem(tr("65536 colors (16 bits per pixel)"));
-    colorDepthCombo->insertItem(tr("16 million colors (24 bits per pixel)"));
+    colorDepthCombo->addItem(tr("2 colors (1 bit per pixel)"));
+    colorDepthCombo->addItem(tr("4 colors (2 bits per pixel)"));
+    colorDepthCombo->addItem(tr("16 colors (4 bits per pixel)"));
+    colorDepthCombo->addItem(tr("256 colors (8 bits per pixel)"));
+    colorDepthCombo->addItem(tr("65536 colors (16 bits per pixel)"));
+    colorDepthCombo->addItem(tr("16 million colors (24 bits per pixel)"));
 
     connect(okButton, SIGNAL(clicked()), this, SLOT(accept()));
     connect(cancelButton, SIGNAL(clicked()), this, SLOT(reject()));
--- doc/src/snippets/designer/singleinheritance/imagedialog.cpp
+++ doc/src/snippets/designer/singleinheritance/imagedialog.cpp
@@ -7,12 +7,12 @@ ImageDialog::ImageDialog(QWidget *parent)
 {
     ui.setupUi(this);
 
-    ui.colorDepthCombo->insertItem(tr("2 colors (1 bit per pixel)"));
-    ui.colorDepthCombo->insertItem(tr("4 colors (2 bits per pixel)"));
-    ui.colorDepthCombo->insertItem(tr("16 colors (4 bits per pixel)"));
-    ui.colorDepthCombo->insertItem(tr("256 colors (8 bits per pixel)"));
-    ui.colorDepthCombo->insertItem(tr("65536 colors (16 bits per pixel)"));
-    ui.colorDepthCombo->insertItem(tr("16 million colors (24 bits per pixel)"));
+    ui.colorDepthCombo->addItem(tr("2 colors (1 bit per pixel)"));
+    ui.colorDepthCombo->addItem(tr("4 colors (2 bits per pixel)"));
+    ui.colorDepthCombo->addItem(tr("16 colors (4 bits per pixel)"));
+    ui.colorDepthCombo->addItem(tr("256 colors (8 bits per pixel)"));
+    ui.colorDepthCombo->addItem(tr("65536 colors (16 bits per pixel)"));
+    ui.colorDepthCombo->addItem(tr("16 million colors (24 bits per pixel)"));
 
     connect(ui.okButton, SIGNAL(clicked()), this, SLOT(accept()));
     connect(ui.cancelButton, SIGNAL(clicked()), this, SLOT(reject()));
