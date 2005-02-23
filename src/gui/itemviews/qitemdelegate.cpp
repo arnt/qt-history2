ce36d1cca16715cc5866b5f97988003c85ecceb5
mmonsen <mmonsen@trolltech.com>
2005-02-23 17:57:00 +0100

    Not all items have decoration.
    [git-p4: depot-paths = "//depot/qt/main/": change = 168935]

--- src/gui/itemviews/qitemdelegate.cpp
+++ src/gui/itemviews/qitemdelegate.cpp
@@ -29,6 +29,7 @@
 #include <qitemeditorfactory.h>
 #include <private/qobject_p.h>
 #include <private/qdnd_p.h>
+#include <qdebug.h>
 
 static const int border = 1;
 static const int textMargin = 2;
@@ -135,8 +136,13 @@ void QItemDelegate::paint(QPainter *painter,
         opt.palette.setColor(QPalette::Text, qvariant_cast<QColor>(value));
 
     // do layout
-    QRect pixmapRect = QRect(0, 0, opt.decorationSize.width(), opt.decorationSize.height());
-
+    value = model->data(index, QAbstractItemModel::DecorationRole);
+    QPixmap pixmap;
+    QRect pixmapRect;
+    if (value.isValid()) {
+        pixmap = decoration(opt, value);
+        pixmapRect = QRect(0, 0, opt.decorationSize.width(), opt.decorationSize.height());
+    }
     QFontMetrics fontMetrics(opt.font);
     QString text = model->data(index, QAbstractItemModel::DisplayRole).toString();
     QRect textRect(0, 0, fontMetrics.width(text), fontMetrics.height());
@@ -153,9 +159,6 @@ void QItemDelegate::paint(QPainter *painter,
         painter->fillRect(option.rect, qvariant_cast<QColor>(value));
 
     // draw the item
-    value = model->data(index, QAbstractItemModel::DecorationRole);
-    QPixmap pixmap = decoration(opt, value);
-
     drawCheck(painter, opt, checkRect, checkState);
     drawDecoration(painter, opt, pixmapRect, pixmap);
     drawDisplay(painter, opt, textRect, text);
@@ -178,9 +181,11 @@ QSize QItemDelegate::sizeHint(const QStyleOptionViewItem &option,
     QVariant value = model->data(index, QAbstractItemModel::FontRole);
     QFont fnt = value.isValid() ? qvariant_cast<QFont>(value) : option.font;
     QString text = model->data(index, QAbstractItemModel::DisplayRole).toString();
+    QRect pixmapRect;
+    if (model->data(index, QAbstractItemModel::DecorationRole).isValid())
+        pixmapRect = QRect(0, 0, option.decorationSize.width(),
+                           option.decorationSize.height());
 
-    QRect pixmapRect = QRect(0, 0, option.decorationSize.width(),
-                             option.decorationSize.height());
     QFontMetrics fontMetrics(fnt);
     QRect textRect(0, 0, fontMetrics.width(text), fontMetrics.height());
     QRect checkRect = check(option, model->data(index, QAbstractItemModel::CheckStateRole));
