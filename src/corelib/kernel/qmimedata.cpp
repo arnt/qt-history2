6893b86c389de4fdbdd3fb653fe85d230a0e5a3b
dboddie <dboddie@mikaeld-lurven>
2005-03-16 18:11:32 +0100

    p4i integration Integrate 172130 from 4.0 to main: Doc: It would be useful to link to QDropEvent here.
    [git-p4: depot-paths = "//depot/qt/main/": change = 172131]

--- src/corelib/kernel/qmimedata.cpp
+++ src/corelib/kernel/qmimedata.cpp
@@ -103,7 +103,7 @@ QVariant QMimeDataPrivate::getData(const QString &format) const
     Usually, it is easier to rely on QMimeData's support for QImage and QPixmap when
     handling images.
 
-    \sa QClipboard QDrag \link Drag and Drop\endlink
+    \sa QClipboard QMimeData QDrag \link Drag and Drop\endlink
 */
 
 /*!
