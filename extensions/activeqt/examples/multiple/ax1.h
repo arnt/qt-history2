05f74e0743a8700bdfed9e25eb5ebd2bd06f92dd
Volker Hilsheimer <vohi@trolltech.com>
2005-03-16 14:58:29 +0100

    p4i integration Integrate 172068 from 4.0 to main: Do it right in the example.
    [git-p4: depot-paths = "//depot/qt/main/": change = 172069]

--- extensions/activeqt/examples/multiple/ax1.h
+++ extensions/activeqt/examples/multiple/ax1.h
@@ -20,7 +20,6 @@ class QAxWidget1 : public QWidget
     Q_CLASSINFO("ClassID", "{1D9928BD-4453-4bdd-903D-E525ED17FDE5}")
     Q_CLASSINFO("InterfaceID", "{99F6860E-2C5A-42ec-87F2-43396F4BE389}")
     Q_CLASSINFO("EventsID", "{0A3E9F27-E4F1-45bb-9E47-63099BCCD0E3}")
-    Q_CLASSINFO("ToSuperClass", "QAxWidget1")
 
     Q_PROPERTY(QColor fillColor READ fillColor WRITE setFillColor)
 public:
