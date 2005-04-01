3d38d45bbfacc71534f7a577b7fe390320d09ee2
Roberto Raggi <rraggi@trolltech.com>
2005-04-01 16:22:51 +0100

    p4i integration Integrate 173711 from 4.0 to main: Details: added the StyledButton
    [git-p4: depot-paths = "//depot/qt/main/": change = 173712]

---
+++ tools/designer/src/components/propertyeditor/styledbutton.h
@@ -0,0 +1,55 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef STYLEBUTTON_H
+#define STYLEBUTTON_H
+
+#include <QtGui/QPushButton>
+
+class StyledButton : public QPushButton
+{
+    Q_OBJECT
+    Q_PROPERTY(QBrush brush READ brush WRITE setBrush)
+    Q_ENUMS(ButtonType)
+
+public:
+    enum ButtonType {ColorButton, PixmapButton};
+
+    StyledButton (QWidget *parent = 0, ButtonType type = ColorButton);
+    virtual ~StyledButton () {}
+
+    void setButtonType(ButtonType type);
+    const QBrush &brush();
+    void setBrush(const QBrush &b);
+
+    QString pixmapFileName() const;
+
+signals:
+    void changed();
+
+public slots:
+    virtual void onEditor();
+
+protected:
+    void paintEvent (QPaintEvent *event);
+
+private:
+    QString buildImageFormatList() const;
+    bool openPixmap();
+
+    ButtonType btype;
+    QString pixFile;
+    QBrush mBrush;
+};
+
+#endif // STYLEBUTTON_H
