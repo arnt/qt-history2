42b7e8cbbd9f62090854f01719c3aea43314a18e
Sam Magnuson <sam@trolltech.com>
2005-04-06 01:48:35 +0100

    p4i integration Integrate 174044 from 4.0 to main: Fixes:    compile after integrate
    [git-p4: depot-paths = "//depot/qt/main/": change = 174046]

--- qmake/generators/mac/pbuilder_pbx.cpp
+++ qmake/generators/mac/pbuilder_pbx.cpp
@@ -886,7 +886,7 @@ ProjectBuilderMakefileGenerator::writeMakeParts(QTextStream &t)
         QString destDir = Option::output_dir;
         if (!project->isEmpty("QMAKE_ORIG_DESTDIR"))
             destDir = project->first("QMAKE_ORIG_DESTDIR");
-        fixEnvs(destDir);
+        destDir = fixForOutput(destDir);
         destDir = fileInfo(Option::fixPathToLocalOS(destDir)).absoluteFilePath();
         project->variables()["QMAKE_PBX_PRESCRIPT_BUILDPHASES"].append(phase_key);
         t << "\t\t" << phase_key << " = {\n"
