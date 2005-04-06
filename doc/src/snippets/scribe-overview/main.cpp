f03c70bcd1b7076a356aac3462b832b51abc6732
dboddie <dboddie@mikaeld-lurven>
2005-04-06 17:51:03 +0100

    p4i integration Integrate 174198 from 4.0 to main: Doc: Added a missing snippet.
    [git-p4: depot-paths = "//depot/qt/main/": change = 174199]

---
+++ doc/src/snippets/scribe-overview/main.cpp
@@ -0,0 +1,29 @@
+#include <QtGui>
+
+void mergeFormat(QTextEdit *edit)
+{
+    QTextDocument *document = edit->document(); 
+    QTextCursor cursor(document);
+
+    cursor.movePosition(QTextCursor::Start); 
+    cursor.movePosition(QTextCursor::EndOfLine, QTextCursor::KeepAnchor);
+
+    QTextCharFormat format;
+    format.setFontWeight(QFont::Bold);
+
+    cursor.mergeCharFormat(format);
+}
+
+int main(int argc, char *argv[])
+{
+    QWidget *parent = 0;
+    QString aStringContainingHTMLtext("<h1>Scribe Overview</h1>");
+
+    QApplication app(argc, argv);
+
+    QTextEdit *edit = new QTextEdit(parent);
+    edit->setHtml(aStringContainingHTMLtext);
+    edit->show();
+
+    return app.exec();
+}
