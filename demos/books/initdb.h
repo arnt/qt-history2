0bd883735618f529d7def81735d0d67f918f8346
harald <harald@harald-morpork>
2005-04-07 15:35:07 +0100

    p4i integration Integrate 174335 from 4.0 to main:        add more books and beautify it a bit more
    [git-p4: depot-paths = "//depot/qt/main/": change = 174336]

--- demos/books/initdb.h
+++ demos/books/initdb.h
@@ -3,14 +3,29 @@
 
 #include <QtSql>
 
-bool addBook(QSqlQuery &q, const QString &title, int year, const QVariant &authorId,
+void addBook(QSqlQuery &q, const QString &title, int year, const QVariant &authorId,
              const QVariant &genreId)
 {
     q.addBindValue(title);
     q.addBindValue(year);
     q.addBindValue(authorId);
     q.addBindValue(genreId);
-    return q.exec();
+    q.exec();
+}
+
+QVariant addGenre(QSqlQuery &q, const QString &name)
+{
+    q.addBindValue(name);
+    q.exec();
+    return q.lastInsertId();
+}
+
+QVariant addAuthor(QSqlQuery &q, const QString &name, const QDate &birthdate)
+{
+    q.addBindValue(name);
+    q.addBindValue(birthdate);
+    q.exec();
+    return q.lastInsertId();
 }
 
 QSqlError initDb()
@@ -36,50 +51,31 @@ QSqlError initDb()
 
     if (!q.prepare(QLatin1String("insert into authors(name, birthdate) values(?, ?)")))
         return q.lastError();
-    q.addBindValue(QLatin1String("Isaac Asimov"));
-    q.addBindValue(QDate(1920, 2, 1));
-    if (!q.exec())
-        return q.lastError();
-    QVariant asimovId = q.lastInsertId();
-    q.addBindValue(QLatin1String("Graham Greene"));
-    q.addBindValue(QDate(1904, 10, 2));
-    if (!q.exec())
-        return q.lastError();
-    QVariant greeneId = q.lastInsertId();
+    QVariant asimovId = addAuthor(q, QLatin1String("Isaac Asimov"), QDate(1920, 2, 1));
+    QVariant greeneId = addAuthor(q, QLatin1String("Graham Greene"), QDate(1904, 10, 2));
+    QVariant pratchettId = addAuthor(q, QLatin1String("Terry Pratchett"), QDate(1948, 4, 28));
 
     if (!q.prepare(QLatin1String("insert into genres(name) values(?)")))
         return q.lastError();
-    q.addBindValue(QLatin1String("Science Fiction"));
-    if (!q.exec())
-        return q.lastError();
-    QVariant sfId = q.lastInsertId();
-    q.addBindValue(QLatin1String("Fiction"));
-    if (!q.exec())
-        return q.lastError();
-    QVariant fId = q.lastInsertId();
+    QVariant sfiction = addGenre(q, QLatin1String("Science Fiction"));
+    QVariant fiction = addGenre(q, QLatin1String("Fiction"));
+    QVariant fantasy = addGenre(q, QLatin1String("Fantasy"));
 
     if (!q.prepare(QLatin1String("insert into books(title, year, author, genre) values(?, ?, ?, ?)")))
         return q.lastError();
-    if (!addBook(q, QLatin1String("Foundation"), 1951, asimovId, sfId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("Foundation and Empire"), 1952, asimovId, sfId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("Second Foundation"), 1953, asimovId, sfId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("Foundation's Edge"), 1982, asimovId, sfId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("Foundation and Earth"), 1986, asimovId, sfId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("Prelude to Foundation"), 1988, asimovId, sfId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("Forward the Foundation"), 1993, asimovId, sfId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("The Power and the Glory"), 1940, greeneId, fId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("The Third Man"), 1950, greeneId, fId))
-        return q.lastError();
-    if (!addBook(q, QLatin1String("Our Man in Havana"), 1958, greeneId, fId))
-        return q.lastError();
+    addBook(q, QLatin1String("Foundation"), 1951, asimovId, sfiction);
+    addBook(q, QLatin1String("Foundation and Empire"), 1952, asimovId, sfiction);
+    addBook(q, QLatin1String("Second Foundation"), 1953, asimovId, sfiction);
+    addBook(q, QLatin1String("Foundation's Edge"), 1982, asimovId, sfiction);
+    addBook(q, QLatin1String("Foundation and Earth"), 1986, asimovId, sfiction);
+    addBook(q, QLatin1String("Prelude to Foundation"), 1988, asimovId, sfiction);
+    addBook(q, QLatin1String("Forward the Foundation"), 1993, asimovId, sfiction);
+    addBook(q, QLatin1String("The Power and the Glory"), 1940, greeneId, fiction);
+    addBook(q, QLatin1String("The Third Man"), 1950, greeneId, fiction);
+    addBook(q, QLatin1String("Our Man in Havana"), 1958, greeneId, fiction);
+    addBook(q, QLatin1String("Guards! Guards!"), 1989, pratchettId, fantasy);
+    addBook(q, QLatin1String("Night Watch"), 2002, pratchettId, fantasy);
+    addBook(q, QLatin1String("Going Postal"), 2004, pratchettId, fantasy);
 
     return QSqlError();
 }
