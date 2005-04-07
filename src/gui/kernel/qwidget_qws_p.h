13bf73d56d6d2e81086a2600aafaf55da644e164
Paul Olav Tvete <paul@trolltech.com>
2005-04-07 09:09:40 +0100

    p4i integration Integrate 174248 from 4.0 to main: Fixes:    Add mising file from change 173730 Task:     69073
    [git-p4: depot-paths = "//depot/qt/main/": change = 174250]

---
+++ src/gui/kernel/qwidget_qws_p.h
@@ -0,0 +1,42 @@
+/****************************************************************************
+** $Id: .emacs,v 1.3 1998/02/20 15:06:53 agulbra Exp $
+**
+** Definition of something or other
+**
+** Created : 979899
+**
+** Copyright (C) 1997-2005 by Trolltech AS.  All rights reserved.
+**
+****************************************************************************/
+
+#ifndef QWIDGET_QWS_P_H
+#define QWIDGET_QWS_P_H
+
+#include <qbytearray.h>
+
+class QWSBackingStore
+{
+public:
+    QWSBackingStore();
+    ~QWSBackingStore();
+
+    void create(QSize size);
+    void attach(int shmid, QSize size);
+    void detach();
+
+    void lock(bool write);
+    void release();
+
+    QPixmap *pixmap();
+
+
+
+    int memoryId() const { return shmid; }
+    QSize size() const;
+
+private:
+    QPixmap *pix;
+    int shmid;
+    void *shmaddr;
+};
+#endif
