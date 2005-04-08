dbc8acf3d648f97b5d736ca8c3295c012e47c2a3
Paul Olav Tvete <paul@trolltech.com>
2005-04-08 16:07:42 +0100

    p4i integration Integrate 174544 from 4.0 to main: Fixes:    The Qt 4 way of doing things (tm) RevBy:    Jasmin Details:  After change 174541, this is now possible.
    [git-p4: depot-paths = "//depot/qt/main/": change = 174545]

--- examples/layouts/basiclayouts/window.cpp
+++ examples/layouts/basiclayouts/window.cpp
@@ -4,46 +4,46 @@
 
 Window::Window()
 {
-    QMenuBar *menuBar = new QMenuBar(this);
+    QMenuBar *menuBar = new QMenuBar;
 
-    QMenu* fileMenu = new QMenu(tr("&File"), this);
+    QMenu* fileMenu = new QMenu(tr("&File"));
     QAction *quitAction = fileMenu->addAction(tr("&Quit"));
     menuBar->addMenu(fileMenu);
 
     connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
 
-    QGroupBox *horizontalGroup = new QGroupBox(tr("Horizontal layout"), this);
+    QGroupBox *horizontalGroup = new QGroupBox(tr("Horizontal layout"));
 
     QBoxLayout *horizontalLayout = new QHBoxLayout(horizontalGroup);
     for (int i = 1; i <= 4; ++i) {
-	QPushButton *button = new QPushButton(horizontalGroup);
+	QPushButton *button = new QPushButton;
 	button->setText(tr("Button %1").arg(i));
 	horizontalLayout->addWidget(button, 10);
     }
 
-    QGroupBox *gridGroup = new QGroupBox(tr("Grid layout"), this);
+    QGroupBox *gridGroup = new QGroupBox(tr("Grid layout"));
     QGridLayout *gridLayout = new QGridLayout(gridGroup);
 
     for (int row = 0; row < 3; ++row) {
-	QLabel *label = new QLabel(tr("Line %1").arg(row+1), gridGroup);
-	QLineEdit *lineEdit = new QLineEdit(gridGroup);
+	QLabel *label = new QLabel(tr("Line %1").arg(row+1));
+	QLineEdit *lineEdit = new QLineEdit;
 	gridLayout->addWidget(label, row, 0);
-	gridLayout->addWidget(lineEdit, row, 1);	
+	gridLayout->addWidget(lineEdit, row, 1);
     }
 
-    QTextEdit *gridEditor = new QTextEdit(gridGroup);
+    QTextEdit *gridEditor = new QTextEdit;
     gridEditor->setPlainText(tr("This widget will take up three rows in "
                                 "the grid layout."));
     gridLayout->addWidget(gridEditor, 0, 2, 3, 1);
     gridLayout->setColumnStretch(1, 10);
     gridLayout->setColumnStretch(2, 20);
 
-    QTextEdit *bigEditor = new QTextEdit(this);
+    QTextEdit *bigEditor = new QTextEdit;
     bigEditor->setPlainText(tr("This widget will take up all the remaining "
                                "space in the top-level layout."));
 
-    QPushButton *okButton = new QPushButton(tr("OK"), this);
-    QPushButton *cancelButton = new QPushButton(tr("Cancel"), this );
+    QPushButton *okButton = new QPushButton(tr("OK"));
+    QPushButton *cancelButton = new QPushButton(tr("Cancel"));
     okButton->setDefault(true);
 
     connect(okButton, SIGNAL(clicked()), qApp, SLOT(quit()));
