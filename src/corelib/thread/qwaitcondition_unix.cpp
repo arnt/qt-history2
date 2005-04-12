b2f109fd4e7b4f4c2d7513848af106fa3288a14d
Brad Hughes <bhughes@trolltech.com>
2005-04-12 12:04:42 +0100

    p4i integration Integrate 174926 from 4.0 to main: Fixes:    spurious wakeups in pthread_cond_wait() Task:     68717 RevBy:    harald, trenton AutoTest: 174916, QWaitCondition, QProcess Details:
    many UNIX machines spuriosly wakeup from pthread_cond_wait(),
    especially after signal delivery, even though POSIX doesn't allow for
    it... work around it
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 174927]

--- src/corelib/thread/qwaitcondition_unix.cpp
+++ src/corelib/thread/qwaitcondition_unix.cpp
@@ -31,6 +31,8 @@ static void report_error(int code, const char *where, const char *what)
 struct QWaitConditionPrivate {
     pthread_mutex_t mutex;
     pthread_cond_t cond;
+    int waiters;
+    int wakeups;
 };
 
 
@@ -138,6 +140,7 @@ QWaitCondition::QWaitCondition()
     d = new QWaitConditionPrivate;
     report_error(pthread_mutex_init(&d->mutex, NULL), "QWaitCondition", "mutex init");
     report_error(pthread_cond_init(&d->cond, NULL), "QWaitCondition", "cv init");
+    d->waiters = d->wakeups = 0;
 }
 
 
@@ -165,6 +168,7 @@ QWaitCondition::~QWaitCondition()
 void QWaitCondition::wakeOne()
 {
     report_error(pthread_mutex_lock(&d->mutex), "QWaitCondition::wakeOne()", "mutex lock");
+    d->wakeups = qMin(d->wakeups + 1, d->waiters);
     report_error(pthread_cond_signal(&d->cond), "QWaitCondition::wakeOne()", "cv signal");
     report_error(pthread_mutex_unlock(&d->mutex), "QWaitCondition::wakeOne()", "mutex unlock");
 }
@@ -179,6 +183,7 @@ void QWaitCondition::wakeOne()
 void QWaitCondition::wakeAll()
 {
     report_error(pthread_mutex_lock(&d->mutex), "QWaitCondition::wakeAll()", "mutex lock");
+    d->wakeups = d->waiters;
     report_error(pthread_cond_broadcast(&d->cond), "QWaitCondition::wakeAll()", "cv broadcast");
     report_error(pthread_mutex_unlock(&d->mutex), "QWaitCondition::wakeAll()", "mutex unlock");
 }
@@ -218,23 +223,39 @@ bool QWaitCondition::wait(QMutex *mutex, unsigned long time)
     }
 
     report_error(pthread_mutex_lock(&d->mutex), "QWaitCondition::wait()", "mutex lock");
+    ++d->waiters;
     mutex->unlock();
 
     int code;
-    if (time != ULONG_MAX) {
-        struct timeval tv;
-        gettimeofday(&tv, 0);
+    forever {
+        if (time != ULONG_MAX) {
+            struct timeval tv;
+            gettimeofday(&tv, 0);
 
-        timespec ti;
-        ti.tv_nsec = (tv.tv_usec + (time % 1000) * 1000) * 1000;
-        ti.tv_sec = tv.tv_sec + (time / 1000) + (ti.tv_nsec / 1000000000);
-        ti.tv_nsec %= 1000000000;
+            timespec ti;
+            ti.tv_nsec = (tv.tv_usec + (time % 1000) * 1000) * 1000;
+            ti.tv_sec = tv.tv_sec + (time / 1000) + (ti.tv_nsec / 1000000000);
+            ti.tv_nsec %= 1000000000;
 
-        code = pthread_cond_timedwait(&d->cond, &d->mutex, &ti);
-    } else {
-        code = pthread_cond_wait(&d->cond, &d->mutex);
+            code = pthread_cond_timedwait(&d->cond, &d->mutex, &ti);
+        } else {
+            code = pthread_cond_wait(&d->cond, &d->mutex);
+        }
+        if (code == 0 && d->wakeups == 0) {
+            // many vendors warn of spurios wakeups from
+            // pthread_cond_wait(), especially after signal delivery,
+            // even though POSIX doesn't allow for it... sigh
+            continue;
+        }
+        break;
     }
 
+    Q_ASSERT_X(d->waiters > 0, "QWaitCondition::wait", "internal error (waiters)");
+    --d->waiters;
+    if (code == 0) {
+        Q_ASSERT_X(d->wakeups > 0, "QWaitCondition::wait", "internal error (wakeups)");
+        --d->wakeups;
+    }
     report_error(pthread_mutex_unlock(&d->mutex), "QWaitCondition::wait()", "mutex unlock");
     mutex->lock();
 
