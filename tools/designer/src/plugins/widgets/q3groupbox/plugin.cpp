2994ba59c542fa751dde33e9896bb1a9a9d4a206
Roberto Raggi <rraggi@trolltech.com>
2005-04-14 16:02:07 +0100

    p4i integration Integrate 175502 from 4.0 to main: Details: added the plugin for Q3GroupBox
    [git-p4: depot-paths = "//depot/qt/main/": change = 175503]

---
+++ tools/designer/src/plugins/widgets/q3groupbox/plugin.cpp
@@ -0,0 +1,120 @@
+
+#include <QtDesigner/QDesignerCustomWidgetInterface>
+
+#include <Qt3Support/Q3GroupBox>
+#include <QtCore/qplugin.h>
+
+class Q3GroupBoxPlugin: public QObject, public QDesignerCustomWidgetInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QDesignerCustomWidgetInterface)
+public:
+    Q3GroupBoxPlugin(QObject *parent = 0);
+    virtual ~Q3GroupBoxPlugin();
+
+    virtual QString name() const;
+    virtual QString group() const;
+    virtual QString toolTip() const;
+    virtual QString whatsThis() const;
+    virtual QString includeFile() const;
+    virtual QIcon icon() const;
+
+    virtual bool isContainer() const;
+    virtual bool isForm() const;
+
+    virtual QWidget *createWidget(QWidget *parent);
+
+    virtual bool isInitialized() const;
+    virtual void initialize(QDesignerFormEditorInterface *core);
+
+    virtual QString domXml() const
+    { return QLatin1String("\
+        <widget class=\"Q3GroupBox\" name=\"buttonGroup\">\
+            <property name=\"geometry\">\
+                <rect>\
+                    <x>0</x>\
+                    <y>0</y>\
+                    <width>100</width>\
+                    <height>80</height>\
+                </rect>\
+            </property>\
+        </widget>\
+      "); }
+
+private:
+    bool m_initialized;
+};
+
+Q3GroupBoxPlugin::Q3GroupBoxPlugin(QObject *parent)
+    : QObject(parent),
+      m_initialized(false)
+{
+}
+
+Q3GroupBoxPlugin::~Q3GroupBoxPlugin()
+{
+}
+
+
+QString Q3GroupBoxPlugin::name() const
+{
+    return QLatin1String("Q3GroupBox");
+}
+
+QString Q3GroupBoxPlugin::group() const
+{
+    return QLatin1String("Compat");
+}
+
+QString Q3GroupBoxPlugin::toolTip() const
+{
+    return QString();
+}
+
+QString Q3GroupBoxPlugin::whatsThis() const
+{
+    return QString();
+}
+
+QString Q3GroupBoxPlugin::includeFile() const
+{
+    return QLatin1String("Qt3Support/Q3GroupBox");
+}
+
+QIcon Q3GroupBoxPlugin::icon() const
+{
+    return QIcon();
+}
+
+bool Q3GroupBoxPlugin::isContainer() const
+{
+    return true;
+}
+
+bool Q3GroupBoxPlugin::isForm() const
+{
+    return false;
+}
+
+QWidget *Q3GroupBoxPlugin::createWidget(QWidget *parent)
+{
+    Q3GroupBox *g = new Q3GroupBox(parent);
+    g->setColumnLayout(0, Qt::Vertical);
+    return g;
+}
+
+bool Q3GroupBoxPlugin::isInitialized() const
+{
+    return m_initialized;
+}
+
+void Q3GroupBoxPlugin::initialize(QDesignerFormEditorInterface *core)
+{
+    Q_UNUSED(core);
+    m_initialized = true;
+}
+
+
+Q_EXPORT_PLUGIN(Q3GroupBoxPlugin)
+
+#include "plugin.moc"
--- tools/designer/src/plugins/widgets/widgets.pro
+++ tools/designer/src/plugins/widgets/widgets.pro
@@ -5,6 +5,7 @@ SUBDIRS = \
     q3listview \
     q3widgetstack \
     q3buttongroup \
+    q3groupbox \
     q3frame
 
 
