1b8abca0b2daf4710dd0c9ca13b7c667938564dc
Volker Hilsheimer <vohi@trolltech.com>
2005-04-14 10:09:35 +0100

    p4i integration Integrate 175367 from 4.0 to main: Revert accidential submit :(
    [git-p4: depot-paths = "//depot/qt/main/": change = 175375]

--- src/gui/kernel/qapplication_win.cpp
+++ src/gui/kernel/qapplication_win.cpp
@@ -2977,23 +2977,8 @@ bool QETWidget::translateKeyEvent(const MSG &msg, bool grab)
                 if (!uch.isNull())
                     text += uch;
                 char a = uch.row() ? 0 : uch.cell();
+                store_key_rec( msg.wParam, a, state, text );
                 k0 = sendKeyEvent(QEvent::KeyPress, code, state, grab, text);
-
-                bool store = true;
-                // Alt+<alphanumerical> go to the Win32 menu system if unhandled by Qt
-                if (msg.message == WM_SYSKEYDOWN && !k0 && a) {
-                    HWND parent = GetParent(winId());
-                    while (parent) {
-                        parent = GetParent(parent);
-                        if (GetMenu(parent)) {
-                            SendMessage(winId(), WM_SYSCOMMAND, SC_KEYMENU, a);
-                            store = false;
-                            k0 = true;
-                        }
-                    }
-                }
-                if (store)
-                    store_key_rec( msg.wParam, a, state, text );
             }
         } else {
             // Must be KEYUP
@@ -3010,17 +2995,8 @@ bool QETWidget::translateKeyEvent(const MSG &msg, bool grab)
                 k0 = sendKeyEvent(QEvent::KeyRelease, code, state, grab, rec->text);
 
                 // don't pass Alt to Windows unless we are embedded in a non-Qt window
-                if ( code == Qt::Key_Alt ) {
+                if ( code == Qt::Key_Alt )
                     k0 = true;
-                    HWND parent = GetParent(winId());
-                    while (parent) {
-                        if (!QWidget::find(parent) && GetMenu(parent)) {
-                            k0 = false;
-                            break;
-                        }
-                        parent = GetParent(parent);
-                    }
-                }
             }
         }
     }
