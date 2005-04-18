cce33aa314d49a6e6497203f941fdf6a30a48f9d
Roberto Raggi <rraggi@trolltech.com>
2005-04-18 15:15:00 +0100

    Details: readded iconcache.cpp
    [git-p4: depot-paths = "//depot/qt/main/": change = 175874]

---
+++ tools/designer/src/components/formeditor/iconcache.cpp
@@ -0,0 +1,71 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "iconcache.h"
+
+#include <resourcefile.h>
+#include <QtCore/QDir>
+#include <QtCore/QFileInfo>
+
+using namespace qdesigner::components::formeditor;
+
+IconCache::IconCache(QObject *parent)
+    : QDesignerIconCacheInterface(parent)
+{
+}
+
+QIcon IconCache::nameToIcon(const QString &path, const QString &resourcePath)
+{ return m_icon_cache.keyToItem(path, resourcePath); }
+
+QString IconCache::iconToFilePath(const QIcon &pm) const
+{ return m_icon_cache.itemToFilePath(pm); }
+
+QString IconCache::iconToQrcPath(const QIcon &pm) const
+{ return m_icon_cache.itemToQrcPath(pm); }
+
+QPixmap IconCache::nameToPixmap(const QString &path, const QString &resourcePath)
+{ return m_pixmap_cache.keyToItem(path, resourcePath); }
+
+QString IconCache::pixmapToFilePath(const QPixmap &pm) const
+{ return m_pixmap_cache.itemToFilePath(pm); }
+
+QString IconCache::pixmapToQrcPath(const QPixmap &pm) const
+{ return m_pixmap_cache.itemToQrcPath(pm); }
+
+QList<QPixmap> IconCache::pixmapList() const
+{ return m_pixmap_cache.itemList(); }
+
+QList<QIcon> IconCache::iconList() const
+{ return m_icon_cache.itemList(); }
+
+QString IconCache::resolveQrcPath(const QString &filePath, const QString &qrcPath, const QString &wd) const
+{
+    QString workingDirectory = wd;
+    if (workingDirectory.isEmpty()) {
+        workingDirectory = QDir::currentPath();
+    }
+
+    QString icon_path = filePath;
+    QString qrc_path = qrcPath;
+
+    if (!qrc_path.isEmpty()) {
+        qrc_path = QFileInfo(QDir(workingDirectory), qrcPath).absoluteFilePath();
+        ResourceFile rf(qrc_path);
+        if (rf.load())
+            return rf.resolvePath(filePath);
+    } else {
+        return QFileInfo(QDir(workingDirectory), filePath).absoluteFilePath();
+    }
+
+    return QString();
+}
