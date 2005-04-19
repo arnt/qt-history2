7741973695d4b6ae13b020d881ab8738e3a91bcb
Roberto Raggi <rraggi@trolltech.com>
2005-04-19 11:07:05 +0100

    Details: manual integrate of extrainfo.h
    [git-p4: depot-paths = "//depot/qt/main/": change = 175982]

---
+++ tools/designer/src/lib/sdk/extrainfo.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef EXTRAINFO_H
+#define EXTRAINFO_H
+
+#include <QtDesigner/extension.h>
+
+class DomWidget;
+class DomUi;
+class QWidget;
+
+class QDesignerFormEditorInterface;
+
+class QDesignerExtraInfoExtension
+{
+public:
+    virtual ~QDesignerExtraInfoExtension() {}
+
+    virtual QDesignerFormEditorInterface *core() const = 0;
+    virtual QWidget *widget() const = 0;
+
+    virtual bool saveUiExtraInfo(DomUi *ui) = 0;
+    virtual bool loadUiExtraInfo(DomUi *ui) = 0;
+
+    virtual bool saveWidgetExtraInfo(DomWidget *ui_widget) = 0;
+    virtual bool loadWidgetExtraInfo(DomWidget *ui_widget) = 0;
+
+    QString workingDirectory() const;
+    void setWorkingDirectory(const QString &workingDirectory);
+
+private:
+    QString m_workingDirectory;
+};
+Q_DECLARE_EXTENSION_INTERFACE(QDesignerExtraInfoExtension, "http://trolltech.com/Qt/IDE/ExtraInfo")
+
+
+#endif // EXTRAINFO_H
