824ece10bcda8efd89690fc6906cfb5d10df8e0f
gunnar <gunnar@THAUMASIA>
2005-04-22 10:41:44 +0100

    p4i integration Integrate 176644 from 4.0 to main: Fixes:    compile
    [git-p4: depot-paths = "//depot/qt/main/": change = 176645]

--- src/gui/painting/qbrush.cpp
+++ src/gui/painting/qbrush.cpp
@@ -67,7 +67,8 @@ QPixmap qt_pixmapForBrush(int brushStyle, bool invert)
     QPixmap pm;
     QString key = "$qt-brush$" + QString::number(brushStyle) + QString::number((int)invert);
     if (!QPixmapCache::find(key, pm)) {
-        pm = QBitmap::fromData(QSize(8, 8), qt_patternForBrush(brushStyle, invert), QSysInfo::LittleEndian);
+        pm = QBitmap::fromData(QSize(8, 8), qt_patternForBrush(brushStyle, invert),
+                               QImage::Format_MonoLSB);
         QPixmapCache::insert(key, pm);
     }
 
