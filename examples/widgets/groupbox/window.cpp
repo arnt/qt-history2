3f9b212f0186b1959a8538b09255dfa110896952
Vivi Karlsen <vkarlsen@trolltech.com>
2005-04-25 10:59:49 +0100

    p4i integration Integrate 176921 from 4.0 to main: doc: removed parents in example
    [git-p4: depot-paths = "//depot/qt/main/": change = 176923]

--- examples/widgets/groupbox/window.cpp
+++ examples/widgets/groupbox/window.cpp
@@ -5,11 +5,12 @@
 Window::Window(QWidget *parent)
     : QWidget(parent)
 {
-    QGridLayout *grid = new QGridLayout(this);
+    QGridLayout *grid = new QGridLayout;
     grid->addWidget(createFirstExclusiveGroup(), 0, 0);
     grid->addWidget(createSecondExclusiveGroup(), 1, 0);
     grid->addWidget(createNonExclusiveGroup(), 0, 1);
     grid->addWidget(createPushButtonGroup(), 1, 1);
+    setLayout(grid);
 
     setWindowTitle(tr("Group Box"));
     resize(480, 320);
@@ -17,81 +18,84 @@ Window::Window(QWidget *parent)
 
 QGroupBox *Window::createFirstExclusiveGroup()
 {
-    QGroupBox *groupBox = new QGroupBox(tr("Exclusive Radio Buttons"), this);
+    QGroupBox *groupBox = new QGroupBox(tr("Exclusive Radio Buttons"));
 
-    QRadioButton *radio1 = new QRadioButton(tr("&Radio button 1"), groupBox);
-    QRadioButton *radio2 = new QRadioButton(tr("R&adio button 2"), groupBox);
-    QRadioButton *radio3 = new QRadioButton(tr("Ra&dio button 3"), groupBox);
+    QRadioButton *radio1 = new QRadioButton(tr("&Radio button 1"));
+    QRadioButton *radio2 = new QRadioButton(tr("R&adio button 2"));
+    QRadioButton *radio3 = new QRadioButton(tr("Ra&dio button 3"));
 
     radio1->setChecked(true);
 
-    QVBoxLayout *vbox = new QVBoxLayout(groupBox);
+    QVBoxLayout *vbox = new QVBoxLayout;
     vbox->addWidget(radio1);
     vbox->addWidget(radio2);
     vbox->addWidget(radio3);
     vbox->addStretch(1);
+    groupBox->setLayout(vbox);
 
     return groupBox;
 }
 
 QGroupBox *Window::createSecondExclusiveGroup()
 {
-    QGroupBox *groupBox = new QGroupBox(tr("E&xclusive Radio Buttons"), this);
+    QGroupBox *groupBox = new QGroupBox(tr("E&xclusive Radio Buttons"));
     groupBox->setCheckable(true);
     groupBox->setChecked(false);
 
-    QRadioButton *radio1 = new QRadioButton(tr("Rad&io button 1"), groupBox);
-    QRadioButton *radio2 = new QRadioButton(tr("Radi&o button 2"), groupBox);
-    QRadioButton *radio3 = new QRadioButton(tr("Radio &button 3"), groupBox);
+    QRadioButton *radio1 = new QRadioButton(tr("Rad&io button 1"));
+    QRadioButton *radio2 = new QRadioButton(tr("Radi&o button 2"));
+    QRadioButton *radio3 = new QRadioButton(tr("Radio &button 3"));
     radio1->setChecked(true);
-    QCheckBox *checkBox = new QCheckBox(tr("Ind&ependent checkbox"), groupBox);
+    QCheckBox *checkBox = new QCheckBox(tr("Ind&ependent checkbox"));
     checkBox->setChecked(true);
 
-    QVBoxLayout *vbox = new QVBoxLayout(groupBox);
+    QVBoxLayout *vbox = new QVBoxLayout;
     vbox->addWidget(radio1);
     vbox->addWidget(radio2);
     vbox->addWidget(radio3);
     vbox->addWidget(checkBox);
     vbox->addStretch(1);
+    groupBox->setLayout(vbox);
 
     return groupBox;
 }
 
 QGroupBox *Window::createNonExclusiveGroup()
 {
-    QGroupBox *groupBox = new QGroupBox(tr("Non-Exclusive Checkboxes"), this);
+    QGroupBox *groupBox = new QGroupBox(tr("Non-Exclusive Checkboxes"));
     groupBox->setFlat(true);
 
-    QCheckBox *checkBox1 = new QCheckBox(tr("&Checkbox 1"), groupBox);
-    QCheckBox *checkBox2 = new QCheckBox(tr("C&heckbox 2"), groupBox);
+    QCheckBox *checkBox1 = new QCheckBox(tr("&Checkbox 1"));
+    QCheckBox *checkBox2 = new QCheckBox(tr("C&heckbox 2"));
     checkBox2->setChecked(true);
-    QCheckBox *tristateBox = new QCheckBox(tr("Tri-&state button"), groupBox);
+    QCheckBox *tristateBox = new QCheckBox(tr("Tri-&state button"));
     tristateBox->setTristate(true);
     tristateBox->setCheckState(Qt::PartiallyChecked);
 
-    QVBoxLayout *vbox = new QVBoxLayout(groupBox);
+    QVBoxLayout *vbox = new QVBoxLayout;
     vbox->addWidget(checkBox1);
     vbox->addWidget(checkBox2);
     vbox->addWidget(tristateBox);
     vbox->addStretch(1);
+    groupBox->setLayout(vbox);
 
     return groupBox;
 }
 
 QGroupBox *Window::createPushButtonGroup()
 {
-    QGroupBox *groupBox = new QGroupBox(tr("&Push Buttons"), this);
+    QGroupBox *groupBox = new QGroupBox(tr("&Push Buttons"));
     groupBox->setCheckable(true);
     groupBox->setChecked(true);
 
-    QPushButton *pushButton = new QPushButton(tr("&Normal Button"), groupBox);
-    QPushButton *toggleButton = new QPushButton(tr("&Toggle Button"), groupBox);
+    QPushButton *pushButton = new QPushButton(tr("&Normal Button"));
+    QPushButton *toggleButton = new QPushButton(tr("&Toggle Button"));
     toggleButton->setCheckable(true);
     toggleButton->setChecked(true);
-    QPushButton *flatButton = new QPushButton(tr("&Flat Button"), groupBox);
+    QPushButton *flatButton = new QPushButton(tr("&Flat Button"));
     flatButton->setFlat(true);
 
-    QPushButton *popupButton = new QPushButton(tr("Pop&up Button"), groupBox);
+    QPushButton *popupButton = new QPushButton(tr("Pop&up Button"));
     QMenu *menu = new QMenu(this);
     menu->addAction(tr("&First Item"));
     menu->addAction(tr("&Second Item"));
@@ -99,12 +103,13 @@ QGroupBox *Window::createPushButtonGroup()
     menu->addAction(tr("F&ourth Item"));
     popupButton->setMenu(menu);
 
-    QVBoxLayout *vbox = new QVBoxLayout(groupBox);
+    QVBoxLayout *vbox = new QVBoxLayout;
     vbox->addWidget(pushButton);
     vbox->addWidget(toggleButton);
     vbox->addWidget(flatButton);
     vbox->addWidget(popupButton);
     vbox->addStretch(1);
+    groupBox->setLayout(vbox);
 
     return groupBox;
 }
