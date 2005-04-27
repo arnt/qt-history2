0456bffe91bfdb403986054faf7c654bf62064a1
Brad Hughes <bhughes@trolltech.com>
2005-04-27 14:16:13 +0100

    p4i integration Integrate 177400 from 4.0 to main: Fixes:    atomic on the arm Task:     65058 RevBy:    none, trust me (lars in spirit) AutoTest: existing tests Details:
    in the interest of space, use a trick on the arm that is similar to the
    one we do on parisc: use a global array of locks instead of a single
    global lock for all atomic operations...
    
    this is all done as function calls, which gives us the possibility of
    providing a binary compatible, optimized drop-in replacement for ARMv6,
    which implements load/store exclusive (similar to lwarx/stcwx. on ppc).
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 177401]

--- src/corelib/arch/arm/qatomic.cpp
+++ src/corelib/arch/arm/qatomic.cpp
@@ -1,27 +1,95 @@
 #include "arch/qatomic.h"
 
+static char locks[256];
+
+char *getLock(volatile void *addr)
+{ return &locks[(ulong(addr) >> 2) & 0xff]; }
+
 #ifdef Q_CC_GNU
 
+inline char q_atomic_swp(volatile char *ptr, char newval)
+{
+    register int ret;
+    asm volatile("swpb %0,%1,[%2]"
+                 : "=r"(ret)
+                 : "r"(newval), "r"(ptr)
+                 : "cc", "memory");
+    return ret;
+}
+
+#endif // Q_CC_GNU
+
 extern "C" {
+
+    int q_atomic_test_and_set_int(volatile int *ptr, int expected, int newval)
+    {
+        char *lock = getLock(ptr);
+        int returnValue = 0;
+        while (q_atomic_swp(lock, ~0) != 0)
+            ;
+        if (*ptr == expected) {
+            *ptr = newval;
+            returnValue = 1;
+        }
+        (void) q_atomic_swp(lock, 0);
+        return returnValue;
+    }
+
     int q_atomic_test_and_set_ptr(volatile void *ptr, void *expected, void *newval)
     {
-        static int lock = 0;
-        int ret = 0;
-        while (q_atomic_swp(&lock, ~0) != 0)
+        char *lock = getLock(ptr);
+        int returnValue = 0;
+        while (q_atomic_swp(lock, ~0) != 0)
             ;
         if (*reinterpret_cast<void * volatile *>(ptr) == expected) {
             *reinterpret_cast<void * volatile *>(ptr) = newval;
-            ret = 1;
+            returnValue = 1;
         }
-        (void) q_atomic_swp(&lock, 0);
-        return ret;
+        (void) q_atomic_swp(lock, 0);
+        return returnValue;
+    }
+
+    int q_atomic_increment(volatile int *ptr)
+    {
+        char *lock = getLock(ptr);
+        while (q_atomic_swp(lock, ~0) != 0)
+            ;
+        int originalValue = *ptr;
+        *ptr = originalValue + 1;
+        (void) q_atomic_swp(lock, 0);
+        return originalValue != -1;
+    }
+
+    int q_atomic_decrement(volatile int *ptr)
+    {
+        char *lock = getLock(ptr);
+        while (q_atomic_swp(lock, ~0) != 0)
+            ;
+        int originalValue = *ptr;
+        *ptr = originalValue - 1;
+        (void) q_atomic_swp(lock, 0);
+        return originalValue != 1;
+    }
+
+    int q_atomic_set_int(volatile int *ptr, int newval)
+    {
+        char *lock = getLock(ptr);
+        while (q_atomic_swp(lock, ~0) != 0)
+            ;
+        int originalValue = *ptr;
+        *ptr = newval;
+        (void) q_atomic_swp(lock, 0);
+        return originalValue;
     }
 
     void *q_atomic_set_ptr(volatile void *ptr, void *newval)
     {
-        return reinterpret_cast<void *>(q_atomic_swp(reinterpret_cast<volatile int *>(ptr),
-                                                     reinterpret_cast<int>(newval)));
+        char *lock = getLock(ptr);
+        while (q_atomic_swp(lock, ~0) != 0)
+            ;
+	void *originalValue = *reinterpret_cast<void * volatile *>(ptr);
+        *reinterpret_cast<void * volatile *>(ptr) = newval;
+        (void) q_atomic_swp(lock, 0);
+        return originalValue;
     }
 }
-
-#endif
