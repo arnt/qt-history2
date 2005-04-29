f799c3417a370bd67f7b23434d4b222d5f733932
Lars Knoll <lars@trolltech.com>
2005-04-29 11:47:04 +0100

    p4i integration Integrate 177826 from 4.0 to main: Fixes:    compat methods in QInputDialog Details:  They got removed by accident in my previous submit.
    [git-p4: depot-paths = "//depot/qt/main/": change = 177827]

--- src/gui/dialogs/qinputdialog.h
+++ src/gui/dialogs/qinputdialog.h
@@ -48,6 +48,28 @@ public:
     static QString getItem(QWidget *parent, const QString &title, const QString &label, const QStringList &list,
                            int current = 0, bool editable = true, bool *ok = 0,Qt::WFlags f = 0);
 
+#ifdef QT3_SUPPORT
+    inline static QT3_SUPPORT QString getText(const QString &title, const QString &label,
+                                              QLineEdit::EchoMode echo = QLineEdit::Normal,
+                                              const QString &text = QString::null, bool *ok = 0,
+                                              QWidget *parent = 0, const char * = 0, Qt::WFlags f = 0)
+        { return getText(parent, title, label, echo, text, ok, f); }
+    inline static QT3_SUPPORT int getInteger(const QString &title, const QString &label, int value = 0,
+                                             int minValue = -2147483647, int maxValue = 2147483647,
+                                             int step = 1, bool *ok = 0,
+                                             QWidget *parent = 0, const char * = 0, Qt::WFlags f = 0)
+        { return getInteger(parent, title, label, value, minValue, maxValue, step, ok, f); }
+    inline static QT3_SUPPORT double getDouble(const QString &title, const QString &label, double value = 0,
+                                               double minValue = -2147483647, double maxValue = 2147483647,
+                                               int decimals = 1, bool *ok = 0,
+                                               QWidget *parent = 0, const char * = 0, Qt::WFlags f = 0)
+        { return getDouble(parent, title, label, value, minValue, maxValue, decimals, ok, f); }
+    inline static QT3_SUPPORT QString getItem(const QString &title, const QString &label, const QStringList &list,
+                                              int current = 0, bool editable = true, bool *ok = 0,
+                                              QWidget *parent = 0, const char * = 0, Qt::WFlags f = 0)
+        { return getItem(parent, title, label, list, current, editable, ok, f); }
+#endif
+
 private:
     Q_DISABLE_COPY(QInputDialog)
 };
