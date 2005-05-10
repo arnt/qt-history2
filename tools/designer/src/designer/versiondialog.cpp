d07cddc8aaf33bd2f9beee7d6fa1b622e898be3f
jsurazsk <jsurazsk@bobo>
2005-05-10 11:47:07 +0100

    p4i integration Integrate 179340 from 4.0 to main: Fixes:    Wrong date in about dialog Task:     69465
    [git-p4: depot-paths = "//depot/qt/main/": change = 179341]

--- tools/designer/src/designer/versiondialog.cpp
+++ tools/designer/src/designer/versiondialog.cpp
@@ -129,7 +129,7 @@ VersionDialog::VersionDialog(QWidget *parent)
                     "<br/><br/>Version %2"
                     "<br/>Qt Designer is a graphical user interface designer "
                     "for Qt applications.<br/><br/>"
-                    "<br/>Copyright 2000-2004 Trolltech AS. All rights reserved."
+                    "<br/>Copyright 2000-2005 Trolltech AS. All rights reserved."
                     "<br/><br/>The program is provided AS IS with NO WARRANTY OF ANY KIND,"
                     " INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A"
                     " PARTICULAR PURPOSE.<br/> ").arg(tr("Qt Designer"))
