8efa6509537c3871eecfbc4527fa216fefb12954
Marius Storm-Olsen <marius@trolltech.com>
2005-05-19 10:15:01 +0100

    p4i integration Integrate 180717 from 4.0 to main: Change 180717 by marius@marius-dingo on 2005/05/19 11:14:55
    Fixes:    QLibraryInfo conflict when using PCH in qmake
    RevBy:    pascal
    Details:  Just remove the project header files from the PCH file,
              and only use Qt headers there.
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 180718]

--- qmake/qmake_pch.h
+++ qmake/qmake_pch.h
@@ -2,19 +2,19 @@
 #define QMAKE_PCH_H
 
 #include <stdio.h>
-#include "makefile.h"
-#include "meta.h"
+//#include "makefile.h"
+//#include "meta.h"
 #include <qfile.h>
-#include "winmakefile.h"
+//#include "winmakefile.h"
 #include <qtextstream.h>
-#include "project.h"
+//#include "project.h"
 #include <qstring.h>
 #include <qstringlist.h>
 #include <qhash.h>
 #include <time.h>
 #include <stdlib.h>
 #include <qregexp.h>
-#include <qdir.h>
-#include "option.h"
+//#include <qdir.h>
+//#include "option.h"
 
 #endif
