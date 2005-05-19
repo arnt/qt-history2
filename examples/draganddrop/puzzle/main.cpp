3146850344fb02625a8f3459a765386278715417
Dimitri Papadopoulos <dimitri@trolltech.com>
2005-05-19 21:07:46 +0100

    p4i integration Integrate 180772 from 4.0 to main: Fixes:    Memory leak. Task:     none RevBy:    TrustMe AutoTest: none Details:  Create the MainWindow on the stack.
    [git-p4: depot-paths = "//depot/qt/main/": change = 180931]

--- examples/draganddrop/puzzle/main.cpp
+++ examples/draganddrop/puzzle/main.cpp
@@ -18,8 +18,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    MainWindow *window = new MainWindow;
-    window->openImage(":/images/example.jpg");
-    window->show();
+    MainWindow window;
+    window.openImage(":/images/example.jpg");
+    window.show();
     return app.exec();
 }
