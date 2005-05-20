8d18b4501e259b1bf1a9f5e48eafafbe272c4233
Sam Magnuson <sam@trolltech.com>
2005-05-20 21:39:10 +0100

    p4i integration Integrate 181218 from 4.0 to main: Fixes:    fix cleanPath("//") AutoTest: 181215 Details:  pointed out on the preview mailing list
    [git-p4: depot-paths = "//depot/qt/main/": change = 181219]

--- src/corelib/io/qdir.cpp
+++ src/corelib/io/qdir.cpp
@@ -1795,7 +1795,7 @@ QString QDir::cleanPath(const QString &path)
                 eaten = true;
 #endif
                 last = -1;
-            } else if(i && i == len-1) {
+            } else if(last != -1 && i == len-1) {
                 eaten = true;
             } else {
                 levels++;
