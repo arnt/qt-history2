06cc313fd32cdc5d98fa99b2c128952d252079b8
Roberto Raggi <rraggi@trolltech.com>
2005-05-24 12:35:49 +0100

    p4i integration Integrate 181670 from 4.0 to main: Fixes:    use the _right_ color for the selection
    [git-p4: depot-paths = "//depot/qt/main/": change = 181671]

--- tools/designer/src/lib/shared/tree_widget.cpp
+++ tools/designer/src/lib/shared/tree_widget.cpp
@@ -30,31 +30,20 @@ public:
         : QItemDelegate(treeWidget) {}
 
 
-    virtual void paint(QPainter *painter, const QStyleOptionViewItem &opt,
+    virtual void paint(QPainter *painter, const QStyleOptionViewItem &option,
                         const QModelIndex &index) const
     {
-        QStyleOptionViewItem option = opt;
-
-        option.state &= ~(QStyle::State_Selected | QStyle::State_HasFocus);
-
-        if (opt.state & QStyle::State_Selected)
-            painter->fillRect(option.rect, QColor(230, 230, 230));
+        QItemDelegate::paint(painter, option, index);
 
         painter->drawLine(option.rect.x(), option.rect.bottom(),
                             option.rect.right(), option.rect.bottom());
 
         painter->drawLine(option.rect.right(), option.rect.y(),
                             option.rect.right(), option.rect.bottom());
-
-        QItemDelegate::paint(painter, option, index);
     }
 
-    virtual QSize sizeHint(const QStyleOptionViewItem &opt, const QModelIndex &index) const
+    virtual QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
     {
-        QStyleOptionViewItem option = opt;
-
-        option.state &= ~(QStyle::State_Selected | QStyle::State_HasFocus);
-
         return QItemDelegate::sizeHint(option, index) + QSize(4,4);
     }
 };
@@ -74,22 +63,19 @@ TreeWidget::~TreeWidget()
 
 void TreeWidget::drawBranches(QPainter *painter, const QRect &rect, const QModelIndex &index) const
 {
-    QStyleOptionViewItem opt = viewOptions();
-    QStyleOptionViewItem option = opt;
+    QStyleOptionViewItem option = viewOptions();
 
-    if (selectionModel()->isSelected(index))
-        painter->fillRect(rect, QColor(230, 230, 230));
-
-    painter->drawLine(rect.x(), rect.bottom(),
-                      rect.right(), rect.bottom());
+    painter->drawLine(rect.x(), rect.bottom(), rect.right(), rect.bottom());
 
     if (model()->hasChildren(index)) {
         static const int size = 9;
-        opt.state |= QStyle::State_Children;
-        opt.rect.setRect(rect.width() - (indentation() + size) / 2,
-                         rect.y() + (rect.height() - size) / 2, size, size);
+        option.state |= QStyle::State_Children;
+        option.rect.setRect(rect.width() - (indentation() + size) / 2,
+                            rect.y() + (rect.height() - size) / 2, size, size);
+
         if (isExpanded(index))
-            opt.state |= QStyle::State_Open;
-        style()->drawPrimitive(QStyle::PE_IndicatorBranch, &opt, painter, this);
+            option.state |= QStyle::State_Open;
+
+        style()->drawPrimitive(QStyle::PE_IndicatorBranch, &option, painter, this);
     }
 }
