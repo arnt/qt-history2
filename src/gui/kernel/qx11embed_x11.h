2ea1a5ce7e085cce697042981ff2ea79176ccafc
Trenton Schulz <twschulz@twschulz-rain>
2005-05-27 08:32:57 +0100

    p4i integration Integrate 182229 from 4.0 to main: Fixes:    XEmbed style(9) fixes. RevBy:    Andreas Details:  Only 3 people understand what style(9) is.
    [git-p4: depot-paths = "//depot/qt/main/": change = 182230]

--- src/gui/kernel/qx11embed_x11.h
+++ src/gui/kernel/qx11embed_x11.h
@@ -1,7 +1,20 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #ifndef QX11EMBED_H
 #define QX11EMBED_H
 
-#include <qwidget.h>
+#include <QtGui/qwidget.h>
 
 class QX11EmbedWidgetPrivate;
 class QX11EmbedWidget : public QWidget
