cd7b17ca5b1c37528fa9c9e13c5afbbf4d02d54c
gunnar <gunnar@THAUMASIA>
2005-05-30 09:32:49 +0100

    p4i integration Integrate 182394 from 4.0 to main: Fixes:    codesnippet for painterpath. RevBy:    trustme
    [git-p4: depot-paths = "//depot/qt/main/": change = 182395]

--- doc/src/snippets/painterpath/painterpath.cpp
+++ doc/src/snippets/painterpath/painterpath.cpp
@@ -1,16 +1,40 @@
 #include <QtGui/QtGui>
 
-int main()
+
+
+int main(int argc, char **argv)
 {
-    QPainter painter;
+    QApplication app(argc, argv);
+
+    QImage image(100, 100, QImage::Format_RGB32);
+    image.fill(0xffffffff);
+
+    QPainter painter(&image);
+    painter.fillRect(0, 0, 100, 100, Qt::white);
+
+    painter.save();
+    painter.translate(0.5, 0.5);
+    painter.setPen(QPen(QColor(103, 103, 150), 3, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin));
+    painter.setBrush(QColor(223, 223, 255));
+    painter.setRenderHint(QPainter::Antialiasing);
+
     // PATH
     QPainterPath path;
-    path.addRect(20, 20, 80, 80);
+    path.addRect(20, 20, 60, 60);
 
     path.moveTo(0, 0);
     path.cubicTo(99, 0,  50, 50,  99, 99);
     path.cubicTo(0, 99,  50, 50,  0, 0);
 
     painter.drawPath(path);
-    return 0;
+
+    painter.restore();
+    painter.setPen(Qt::black);
+    painter.drawRect(0, 0, 99, 99);
+    painter.end();
+
+    QLabel lab;
+    lab.setPixmap(QPixmap::fromImage(image));
+    lab.show();
+    return app.exec();
 }
