efcd1d0a5b63dfac91e98850c23835a77872dd6d
Simon Hausmann <shausman@trolltech.com>
2005-06-24 13:33:17 +0100

    p4i integration Integrate 185415 from 4.0 to main: Fixes:    Just extends the help page of the license key generator RevBy:    None, trust me, it's just printfs in an otherwise unimportant program ;-)
    [git-p4: depot-paths = "//depot/qt/main/": change = 185416]

--- util/licensekeys/generate/main.cpp
+++ util/licensekeys/generate/main.cpp
@@ -4,6 +4,31 @@ int main(int argc, char **argv)
 {
     if (argc != 7) {
         printf("usage: %s product platform license-schema license-features license-id expiry-date\n", argv[0]);
+
+        printf("\nproduct:\n");
+        for (int i = 0; i < NumberOfProducts; ++i)
+            if (Products[i])
+                printf("\t\t%s\n", Products[i]);
+
+        printf("\nplatform:\n ");
+        for (int i = 0; i < NumberOfPlatforms; ++i)
+            if (Platforms[i])
+                printf("\t\t%s\n", Platforms[i]);
+
+        printf("\nlicense schema:\n");
+        for (int i = 0; i < NumberOfLicenseSchemas; ++i)
+            if (LicenseSchemas[i])
+                printf("\t\t%s\n", LicenseSchemas[i]);
+
+        printf("\nlicense feature:\n");
+        for (int i = 0; i < NumberOfLicenseFeatures; ++i)
+            if (LicenseFeatures[i])
+                printf("\t\t%s\n", LicenseFeatures[i]);
+        printf("\t\tnone\n");
+
+        printf("\nlicense id: integer\n");
+        printf("\nexpiry-date: YYYY-MM-DD\n");
+
         return 2;
     }
     QByteArray licenseKey = generateLicenseKey(argv[1],
