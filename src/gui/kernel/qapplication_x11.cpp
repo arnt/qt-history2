53d3c3a4a2737fddba09aff65a0f1e96e3fd6f32
benjamin <bmeyer@trolltech.com>
2005-07-07 11:56:44 +0100

    Fixes:    Fix compile Details:  unsetenv isn't defined by posix.  And used qgetenv instead of getenv.
    [git-p4: depot-paths = "//depot/qt/main/": change = 186678]

--- src/gui/kernel/qapplication_x11.cpp
+++ src/gui/kernel/qapplication_x11.cpp
@@ -1852,8 +1852,8 @@ void qt_init(QApplicationPrivate *priv, int,
         }
 #endif // QT_NO_TABLET_SUPPORT
        
-        X11->startupId = getenv("DESKTOP_STARTUP_ID");
-        unsetenv("DESKTOP_STARTUP_ID");
+        X11->startupId = qgetenv("DESKTOP_STARTUP_ID");
+        setenv("DESKTOP_STARTUP_ID", 0);
 
     } else {
         // read some non-GUI settings when not using the X server...
