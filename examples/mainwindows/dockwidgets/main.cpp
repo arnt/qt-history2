4505a8180ce46b1dc28cd0bb45aaa049fbee3a7b
Jasmin Blanchette <jasmin@trolltech.com>
2005-07-11 17:11:40 +0100

    Fixes:    Link.
    [git-p4: depot-paths = "//depot/qt/main/": change = 187015]

--- examples/mainwindows/dockwidgets/main.cpp
+++ examples/mainwindows/dockwidgets/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    Q_INIT_RESOURCE(formletters);
+    Q_INIT_RESOURCE(dockwidgets);
     MainWindow mainWin;
     mainWin.show();
     return app.exec();
