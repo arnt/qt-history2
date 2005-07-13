eee8ec0c96fa5462292fd0d0a0e1afb536603ce4
harald <harald@harald-morpork>
2005-07-13 13:37:26 +0100

    p4i integration Integrate 187302 from 4.0 to main: Fixes:    Added missing license headers Details:            found by benjamin's script
    [git-p4: depot-paths = "//depot/qt/main/": change = 187303]

--- src/corelib/io/qbufferedfsfileengine.cpp
+++ src/corelib/io/qbufferedfsfileengine.cpp
@@ -1,3 +1,16 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #include <qdatetime.h>
 #include <stdio.h>
 #include <qdebug.h>
@@ -169,7 +182,7 @@ qint64 QBufferedFSFileEngine::read(char *data, qint64 maxlen)
 #ifdef Q_OS_UNIX
     size_t readBytes = 0;
     int oldFlags = fcntl(fileno(d->fh), F_GETFL);
-    
+
     for (int i = 0; i < 2; ++i) {
 	// Make the underlying file descriptor non-blocking
 	if (d->sequential) {
