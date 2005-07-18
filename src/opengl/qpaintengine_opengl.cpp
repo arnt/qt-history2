1384abe73ecab97bd893016072b078bf609ffcfa
gunnar <gunnar@THAUMASIA>
2005-07-18 15:10:02 +0100

    p4i integration Integrate 187840 from 4.0 to main: Fixes:    state of autoSwapBuffers was not restored on end(); Task:     79310 RevBy:    Eskil
    [git-p4: depot-paths = "//depot/qt/main/": change = 187841]

--- src/opengl/qpaintengine_opengl.cpp
+++ src/opengl/qpaintengine_opengl.cpp
@@ -139,6 +139,7 @@ public:
     uint has_clipping : 1;
     uint has_pen : 1;
     uint has_brush : 1;
+    uint has_autoswap : 1;
 
     QMatrix matrix;
     GLubyte pen_color[4];
@@ -167,6 +168,7 @@ bool QOpenGLPaintEngine::begin(QPaintDevice *pdev)
     Q_ASSERT(static_cast<const QGLWidget *>(pdev));
     d->drawable.setDevice(pdev);
     d->has_clipping = false;
+    d->has_autoswap = dgl->autoBufferSwap();
     d->drawable.setAutoBufferSwap(false);
     setActive(true);
     d->drawable.makeCurrent();
@@ -196,6 +198,7 @@ bool QOpenGLPaintEngine::end()
     glPopAttrib();
     glFlush();
     d->drawable.swapBuffers();
+    dgl->setAutoBufferSwap(d_func()->has_autoswap);
     setActive(false);
     return true;
 }
