5a7b29ac3218bc775e6f245275a545c3349acf88
benjamin <bmeyer@trolltech.com>
2005-07-21 10:24:22 +0100

    p4i integration Integrate 188245 from 4.0 to main: Fixes:    dos->unix format conversion & Remove non-ascii characters Task:     79640 RevBy:    TrustMe Details:  Converted ^M -> \n and copyright symbol to (c)
    [git-p4: depot-paths = "//depot/qt/main/": change = 188247]

--- util/mwerks_plugin/Headers/MWLangDefs.h
+++ util/mwerks_plugin/Headers/MWLangDefs.h
@@ -1,1 +1,25 @@
-/* *  MWLangDefs.h * *  Copyright © 1994-1996 metrowerks inc.  All rights reserved. * *	Language constants. */#ifndef MWLangDefs_H#define MWLangDefs_Henum ELanguage{	langUnknown,	langC,	langCPlus,	langPascal,	langObjectPascal,	langJava,	langAssembler,		// don't care which assembler	langFortran,	langRez};#endif	// !MWLangDefs_H
+/*
+ *  MWLangDefs.h
+ *
+ *  Copyright (c) 1994-1996 metrowerks inc.  All rights reserved.
+ *
+ *	Language constants.
+ */
+
+#ifndef MWLangDefs_H
+#define MWLangDefs_H
+
+enum ELanguage
+{
+	langUnknown,
+	langC,
+	langCPlus,
+	langPascal,
+	langObjectPascal,
+	langJava,
+	langAssembler,		// don't care which assembler
+	langFortran,
+	langRez
+};
+
+#endif	// !MWLangDefs_H
