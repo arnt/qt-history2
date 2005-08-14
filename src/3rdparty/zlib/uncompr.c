7f09423faeb3187d0ff85cea5b1decd722918cde
Dimitri Papadopoulos <dimitri@trolltech.com>
2005-08-14 12:55:32 +0100

    Fixes:    Trolltech modifications to zlib. Details:  Upgrading to zlib 1.2.3 as it natively fixes:           * CAN-2005-2096 (already fixed in change 186645)           * CAN-2005-1849 (already fixed in change 190327)           These Trolltech modifications were copied from change           161745 and prior changes in the qt/3 branch.
              Do we still need these modifications in Qt 4?
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 190332]

--- src/3rdparty/zlib/uncompr.c
+++ src/3rdparty/zlib/uncompr.c
@@ -23,7 +23,7 @@
    enough memory, Z_BUF_ERROR if there was not enough room in the output
    buffer, or Z_DATA_ERROR if the input data was corrupted.
 */
-int ZEXPORT uncompress (dest, destLen, source, sourceLen)
+int Q_ZEXPORT uncompress (dest, destLen, source, sourceLen)
     Bytef *dest;
     uLongf *destLen;
     const Bytef *source;
