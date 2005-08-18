b886192735bfb6b4591c508d64d12adada7ded36
Brad Hughes <bhughes@trolltech.com>
2005-08-18 09:04:27 +0100

    RevBy:    paul Details:
    make the ARM implementation of QAtomic inline assembler... this greatly
    improves performance (especially on the gumstix).
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 190637]

--- src/corelib/arch/arm/arch/qatomic.h
+++ src/corelib/arch/arm/arch/qatomic.h
@@ -16,13 +16,147 @@
 
 #include "QtCore/qglobal.h"
 
-extern "C" {
-    Q_CORE_EXPORT int q_atomic_test_and_set_int(volatile int *ptr, int expected, int newval);
-    Q_CORE_EXPORT int q_atomic_test_and_set_ptr(volatile void *ptr, void *expected, void *newval);
-    Q_CORE_EXPORT int q_atomic_increment(volatile int *ptr);
-    Q_CORE_EXPORT int q_atomic_decrement(volatile int *ptr);
-    Q_CORE_EXPORT int q_atomic_set_int(volatile int *ptr, int newval);
-    Q_CORE_EXPORT void *q_atomic_set_ptr(volatile void *ptr, void *newval);
+extern Q_CORE_EXPORT char q_atomic_lock;
+
+inline int q_atomic_test_and_set_int(volatile int *ptr, int expected, int newval)
+{
+    register int tmp = 1, ret = 0;
+    asm volatile(
+                 // lock
+                 "1:\n"
+                 "    swpb %0,%0,[%2]\n"
+                 "    teq %0,#0\n"
+                 "    bne 1b\n"
+                 // increment
+                 "    ldr %1,[%3]\n"
+                 "    teq %4,%1\n"
+                 "    streq %5,[%3]\n"
+                 "    moveq %1,#1\n"
+                 "    movne %1,#0\n"
+                 // unlock
+                 "    mov %0,#0\n"
+                 "    swpb %0,%0,[%2]\n"
+
+                 : "+r"(tmp), "+r"(ret)
+                 : "r"(&q_atomic_lock), "r"(ptr), "r"(expected), "r"(newval)
+                 : "cc", "memory");
+    return ret;
+}
+
+inline int q_atomic_test_and_set_ptr(volatile void *ptr, void *expected, void *newval)
+{
+    register int tmp = 1, ret = 0;
+    asm volatile(
+                 // lock
+                 "1:\n"
+                 "    swpb %0,%0,[%2]\n"
+                 "    teq %0,#0\n"
+                 "    bne 1b\n"
+                 // increment
+                 "    ldr %1,[%3]\n"
+                 "    teq %4,%1\n"
+                 "    streq %5,[%3]\n"
+                 "    moveq %1,#1\n"
+                 "    movne %1,#0\n"
+                 // unlock
+                 "    mov %0,#0\n"
+                 "    swpb %0,%0,[%2]\n"
+
+                 : "+r"(tmp), "+r"(ret)
+                 : "r"(&q_atomic_lock), "r"(ptr), "r"(expected), "r"(newval)
+                 : "cc", "memory");
+    return ret;
+}
+
+inline int q_atomic_increment(volatile int *ptr)
+{
+    register int tmp = 1, ret = 0;
+    asm volatile(
+                 // lock
+                 "1:\n"
+                 "    swpb %0,%0,[%2]\n"
+                 "    teq %0,#0\n"
+                 "    bne 1b\n"
+                 // increment
+                 "    ldr %1,[%3]\n"
+                 "    add %1,%1,#1\n"
+                 "    str %1,[%3]\n"
+                 // unlock
+                 "    mov %0,#0\n"
+                 "    swpb %0,%0,[%2]\n"
+
+                 : "+r"(tmp), "+r"(ret)
+                 : "r"(&q_atomic_lock), "r"(ptr)
+                 : "cc", "memory");
+    return ret;
+}
+
+inline int q_atomic_decrement(volatile int *ptr)
+{
+    register int tmp = 1, ret = 0;
+    asm volatile(
+                 // lock
+                 "1:\n"
+                 "    swpb %0,%0,[%2]\n"
+                 "    teq %0,#0\n"
+                 "    bne 1b\n"
+                 // increment
+                 "    ldr %1,[%3]\n"
+                 "    sub %1,%1,#1\n"
+                 "    str %1,[%3]\n"
+                 // unlock
+                 "    mov %0,#0\n"
+                 "    swpb %0,%0,[%2]\n"
+
+                 : "+r"(tmp), "+r"(ret)
+                 : "r"(&q_atomic_lock), "r"(ptr)
+                 : "cc", "memory");
+    return ret;
+}
+
+inline int q_atomic_set_int(volatile int *ptr, int newval)
+{
+    register int tmp = 1, ret = 0;
+    asm volatile(
+                 // lock
+                 "1:\n"
+                 "    swpb %0,%0,[%2]\n"
+                 "    teq %0,#0\n"
+                 "    bne 1b\n"
+                 // increment
+                 "    ldr %1,[%3]\n"
+                 "    str %4,[%3]\n"
+                 // unlock
+                 "    mov %0,#0\n"
+                 "    swpb %0,%0,[%2]\n"
+
+                 : "+r"(tmp), "+r"(ret)
+                 : "r"(&q_atomic_lock), "r"(ptr), "r"(newval)
+                 : "cc", "memory");
+    return ret;
+}
+
+inline void *q_atomic_set_ptr(volatile void *ptr, void *newval)
+{
+    register int tmp = 1;
+    register void *ret = 0;
+    asm volatile(
+                 // lock
+                 "1:\n"
+                 "    swpb %0,%0,[%2]\n"
+                 "    teq %0,#0\n"
+                 "    bne 1b\n"
+                 // increment
+                 "    ldr %1,[%3]\n"
+                 "    str %4,[%3]\n"
+                 // unlock
+                 "    mov %0,#0\n"
+                 "    swpb %0,%0,[%2]\n"
+
+                 : "+r"(tmp), "+r"(ret)
+                 : "r"(&q_atomic_lock), "r"(ptr), "r"(newval)
+                 : "cc", "memory");
+    return ret;
 }
 
 #endif // ARM_QATOMIC_H
