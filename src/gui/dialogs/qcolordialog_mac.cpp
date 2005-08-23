d1810a109f8b225ae77123f627f5ab065ea75a9b
Sam Magnuson <sam@trolltech.com>
2005-08-23 23:15:13 +0100

    Fixes:    compile
    [git-p4: depot-paths = "//depot/qt/main/": change = 191157]

--- src/gui/dialogs/qcolordialog_mac.cpp
+++ src/gui/dialogs/qcolordialog_mac.cpp
@@ -14,6 +14,7 @@
 #include "qcolordialog.h"
 #if !defined(QT_NO_COLORDIALOG) && defined(Q_WS_MAC)
 #include <qapplication.h>
+#include <private/qapplication_p.h>
 #include <qdesktopwidget.h>
 #include <private/qt_mac_p.h>
 #include <string.h>
