48e707402190ba7328283a261624a379ae5bc545
dboddie <dboddie@mikaeld-lurven>
2005-09-07 16:34:03 +0100

    Doc: Branching a snippet to help provide missing information about using drag and drop with item views.
    [git-p4: depot-paths = "//depot/qt/main/": change = 192337]

---
+++ doc/src/snippets/qlistwidget-dnd/mainwindow.cpp
@@ -0,0 +1,100 @@
+#include <QtGui>
+
+#include "mainwindow.h"
+
+MainWindow::MainWindow()
+{
+    QMenu *fileMenu = new QMenu(tr("&File"));
+
+    QAction *quitAction = fileMenu->addAction(tr("E&xit"));
+    quitAction->setShortcut(tr("Ctrl+Q"));
+
+    QMenu *itemsMenu = new QMenu(tr("&Items"));
+
+    insertAction = itemsMenu->addAction(tr("&Insert Item"));
+    removeAction = itemsMenu->addAction(tr("&Remove Item"));
+    QAction *ascendingAction = itemsMenu->addAction(tr("Sort in &Ascending Order"));
+    QAction *descendingAction = itemsMenu->addAction(tr("Sort in &Descending Order"));
+
+    menuBar()->addMenu(fileMenu);
+    menuBar()->addMenu(itemsMenu);
+
+/*  For convenient quoting:
+    QListWidget *listWidget = new QListWidget(this);
+*/
+    listWidget = new QListWidget(this);
+    listWidget->setSelectionMode(QAbstractItemView::SingleSelection);
+
+    connect(quitAction, SIGNAL(triggered()), this, SLOT(close()));
+    connect(ascendingAction, SIGNAL(triggered()), this, SLOT(sortAscending()));
+    connect(descendingAction, SIGNAL(triggered()), this, SLOT(sortDescending()));
+    connect(insertAction, SIGNAL(triggered()), this, SLOT(insertItem()));
+    connect(removeAction, SIGNAL(triggered()), this, SLOT(removeItem()));
+    connect(listWidget,
+            SIGNAL(currentItemChanged(QListWidgetItem *, QListWidgetItem *)),
+            this, SLOT(updateMenus(QListWidgetItem *)));
+
+    setupListItems();
+    updateMenus(listWidget->currentItem());
+
+    setCentralWidget(listWidget);
+    setWindowTitle(tr("List Widget"));
+}
+
+void MainWindow::setupListItems()
+{
+    new QListWidgetItem(tr("Oak"), listWidget);
+    new QListWidgetItem(tr("Fir"), listWidget);
+    new QListWidgetItem(tr("Pine"), listWidget);
+    new QListWidgetItem(tr("Birch"), listWidget);
+    new QListWidgetItem(tr("Hazel"), listWidget);
+    new QListWidgetItem(tr("Redwood"), listWidget);
+    new QListWidgetItem(tr("Sycamore"), listWidget);
+    new QListWidgetItem(tr("Chestnut"), listWidget);
+    new QListWidgetItem(tr("Mahogany"), listWidget);
+}
+
+void MainWindow::sortAscending()
+{
+    listWidget->sortItems(Qt::AscendingOrder);
+}
+
+void MainWindow::sortDescending()
+{
+    listWidget->sortItems(Qt::DescendingOrder);
+}
+
+void MainWindow::insertItem()
+{
+    if (!listWidget->currentItem())
+        return;
+
+    QString itemText = QInputDialog::getText(this, tr("Insert Item"),
+        tr("Input text for the new item:"));
+
+    if (itemText.isNull())
+        return;
+
+    QListWidgetItem *newItem = new QListWidgetItem;
+    newItem->setText(itemText);
+    int row = listWidget->row(listWidget->currentItem());
+    listWidget->insertItem(row, newItem);
+
+    QString toolTipText = tr("Tooltip:") + itemText;
+    QString statusTipText = tr("Status tip:") + itemText;
+    QString whatsThisText = tr("What's This?:") + itemText;
+    newItem->setToolTip(toolTipText);
+    newItem->setStatusTip(toolTipText);
+    newItem->setWhatsThis(whatsThisText);
+}
+
+void MainWindow::removeItem()
+{
+    listWidget->takeItem(listWidget->row(listWidget->currentItem()));
+}
+
+void MainWindow::updateMenus(QListWidgetItem *current)
+{
+    insertAction->setEnabled(current != 0);
+    removeAction->setEnabled(current != 0);
+}
