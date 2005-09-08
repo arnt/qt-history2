6c86706551124c54b22e8d0e682d32886941e2ef
dboddie <dboddie@mikaeld-lurven>
2005-09-08 18:28:15 +0100

    Doc: A model/view version of the qlistwidget-dnd snippet.
    [git-p4: depot-paths = "//depot/qt/main/": change = 192467]

---
+++ doc/src/snippets/qlistview-dnd/model.cpp
@@ -0,0 +1,108 @@
+/****************************************************************************
+**
+** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of an example program for Qt.
+** EDITIONS: NOLIMITS
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+/*
+  model.cpp
+
+  A simple model that uses a QStringList as its data source.
+*/
+
+#include <QtGui>
+
+#include "model.h"
+
+DragDropListModel::DragDropListModel(const QStringList &strings,
+                                     QObject *parent)
+    : QStringListModel(strings, parent)
+{
+}
+
+bool DragDropListModel::dropMimeData(const QMimeData *data,
+    Qt::DropAction action, int row, int column, const QModelIndex &parent)
+{
+    if (action == Qt::IgnoreAction)
+        return true;
+
+    if (!data->hasFormat("text/plain"))
+        return false;
+
+    if (column > 0)
+        return false;
+
+    int beginRow;
+
+    if (!parent.isValid())
+        beginRow = qMax(row, rowCount());
+    else
+        beginRow = parent.row();
+
+    int rows = 0;
+
+    QByteArray encodedData = data->data("text/plain");
+    QDataStream stream(&encodedData, QIODevice::ReadOnly);
+    QStringList newItems;
+
+    while (!stream.atEnd()) {
+        QString text;
+        stream >> text;
+        newItems << text;
+        ++rows;
+    }
+
+    insertRows(beginRow, rows, QModelIndex());
+    foreach (QString text, newItems) {
+        QModelIndex idx = index(beginRow, 0, QModelIndex());
+        setData(idx, text);
+    }
+
+    return true;
+}
+
+Qt::ItemFlags DragDropListModel::flags(const QModelIndex &index) const
+{
+    Qt::ItemFlags defaultFlags = QStringListModel::flags(index);
+
+    if (index.isValid())
+        return Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled | defaultFlags;
+    else
+        return Qt::ItemIsDropEnabled | defaultFlags;
+}
+
+QMimeData *DragDropListModel::mimeData(const QModelIndexList &indexes) const
+{
+    QMimeData *mimeData = new QMimeData();
+    QByteArray encodedData;
+
+    QDataStream stream(&encodedData, QIODevice::WriteOnly);
+
+    foreach (QModelIndex index, indexes) {
+        if (index.isValid()) {
+            QString text = data(index, Qt::DisplayRole).toString();
+            stream << text;
+        }
+    }
+
+    mimeData->setData("text/plain", encodedData);
+    return mimeData;
+}
+
+QStringList DragDropListModel::mimeTypes() const
+{
+    QStringList types;
+    types << "text/plain";
+    return types;
+}
+
+Qt::DropActions DragDropListModel::supportedDropActions() const
+{
+    return Qt::CopyAction | Qt::MoveAction;
+}
