68f93282def61b4be44c3826be4ed695d59c2244
qt <qt@manta>
2005-09-15 09:13:14 +0100

    Username: eblomfel Fixes:    QPrintDialog::exec() on ansi-based Windows Task:     84632 Details: The Windows function PrintDlg comes in W/A-versions, but we were only calling the unicode version. Thus the print dialog was never shown on e.g. Windows ME. This fix makes the correct function be called.
    [git-p4: depot-paths = "//depot/qt/main/": change = 192993]

--- src/gui/dialogs/qprintdialog_win.cpp
+++ src/gui/dialogs/qprintdialog_win.cpp
@@ -50,6 +50,161 @@ public:
     QWin32PrintEnginePrivate *ep;
 };
 
+#ifndef Q_OS_TEMP
+// If you change this function, make sure you also change the unicode equivalent
+static PRINTDLGA *qt_win_make_PRINTDLGA(QWidget *parent, QPrintDialogPrivate *d, HGLOBAL *tempDevNames)
+{
+    PRINTDLGA *pd = new PRINTDLGA;
+    memset(pd, 0, sizeof(PRINTDLGA));
+    pd->lStructSize = sizeof(PRINTDLGA);
+
+    pd->hDevMode = d->ep->devMode;       
+    pd->hDevNames  = tempDevNames;
+
+    pd->Flags = PD_RETURNDC;
+    pd->Flags |= PD_USEDEVMODECOPIESANDCOLLATE;
+
+    if (!(d->options & QPrintDialog::PrintSelection))
+        pd->Flags |= PD_NOSELECTION;
+    if (d->options & QPrintDialog::PrintPageRange) {
+        pd->nMinPage = d->minPage;
+        pd->nMaxPage = d->maxPage;
+    }
+
+    if(!(d->options & QPrintDialog::PrintToFile))
+        pd->Flags |= PD_DISABLEPRINTTOFILE;
+
+    if (d->printRange == QPrintDialog::Selection)
+        pd->Flags |= PD_SELECTION;
+    else if (d->printRange == QPrintDialog::PageRange)
+        pd->Flags |= PD_PAGENUMS;
+    else
+        pd->Flags |= PD_ALLPAGES;
+
+    // As stated by MSDN, to enable collate option when minpage==maxpage==0
+    // set the PD_NOPAGENUMS flag
+    if (pd->nMinPage==0 && pd->nMaxPage==0)
+        pd->Flags |= PD_NOPAGENUMS;
+
+    if (d->ep->printToFile)
+        pd->Flags |= PD_PRINTTOFILE;
+    pd->hwndOwner = parent ? parent->winId() : 0;
+    pd->nFromPage = qMax(d->fromPage, d->minPage);
+    pd->nToPage   = qMin(d->toPage, d->maxPage);
+    pd->nCopies = d->ep->devModeA()->dmCopies;
+
+    return pd;
+}
+
+// If you change this function, make sure you also change the unicode equivalent
+static void qt_win_clean_up_PRINTDLGA(PRINTDLGA **pd)
+{    
+    delete *pd;
+    *pd = 0;
+}
+
+// If you change this function, make sure you also change the unicode equivalent
+static void qt_win_read_back_PRINTDLGA(PRINTDLGA *pd, QPrintDialogPrivate *d)
+{
+    if (pd->Flags & PD_SELECTION) {
+        d->printRange = QPrintDialog::Selection;
+        d->fromPage = 0;
+        d->toPage = 0;
+    } else if (pd->Flags & PD_PAGENUMS) {
+        d->printRange = QPrintDialog::PageRange;
+        d->fromPage = pd->nFromPage;
+        d->toPage = pd->nToPage;
+    } else {
+        d->printRange = QPrintDialog::AllPages;
+        d->fromPage = d->minPage;
+        d->toPage = d->maxPage;
+    }
+
+    d->ep->printToFile = (pd->Flags & PD_PRINTTOFILE) != 0;
+
+    d->ep->readDevnames(pd->hDevNames);
+    d->ep->readDevmode(pd->hDevMode);
+}
+#endif // Q_OS_TEMP
+
+#ifdef UNICODE
+// If you change this function, make sure you also change the ansi equivalent
+static PRINTDLGW *qt_win_make_PRINTDLGW(QWidget *parent, QPrintDialogPrivate *d, HGLOBAL *tempDevNames)
+{
+    PRINTDLGW *pd = new PRINTDLGW;
+    memset(pd, 0, sizeof(PRINTDLGW));
+    pd->lStructSize = sizeof(PRINTDLGW);
+
+    pd->hDevMode = d->ep->devMode;
+    pd->hDevNames  = tempDevNames;
+
+    pd->Flags = PD_RETURNDC;
+    pd->Flags |= PD_USEDEVMODECOPIESANDCOLLATE;
+
+    if (!(d->options & QPrintDialog::PrintSelection))
+        pd->Flags |= PD_NOSELECTION;
+    if (d->options & QPrintDialog::PrintPageRange) {
+        pd->nMinPage = d->minPage;
+        pd->nMaxPage = d->maxPage;
+    }
+
+    if(!(d->options & QPrintDialog::PrintToFile))
+        pd->Flags |= PD_DISABLEPRINTTOFILE;
+
+    if (d->printRange == QPrintDialog::Selection)
+        pd->Flags |= PD_SELECTION;
+    else if (d->printRange == QPrintDialog::PageRange)
+        pd->Flags |= PD_PAGENUMS;
+    else
+        pd->Flags |= PD_ALLPAGES;
+
+    // As stated by MSDN, to enable collate option when minpage==maxpage==0
+    // set the PD_NOPAGENUMS flag
+    if (pd->nMinPage==0 && pd->nMaxPage==0)
+        pd->Flags |= PD_NOPAGENUMS;
+
+    if (d->ep->printToFile)
+        pd->Flags |= PD_PRINTTOFILE;
+    pd->hwndOwner = parent ? parent->winId() : 0;
+    pd->nFromPage = qMax(d->fromPage, d->minPage);
+    pd->nToPage   = qMin(d->toPage, d->maxPage);
+    pd->nCopies = d->ep->devModeW()->dmCopies;
+
+    return pd;
+}
+
+// If you change this function, make sure you also change the ansi equivalent
+static void qt_win_clean_up_PRINTDLGW(PRINTDLGW **pd)
+{
+    delete *pd;
+    *pd = 0;
+}
+
+// If you change this function, make sure you also change the ansi equivalent
+static void qt_win_read_back_PRINTDLGW(PRINTDLGW *pd, QPrintDialogPrivate *d)
+{
+    if (pd->Flags & PD_SELECTION) {
+        d->printRange = QPrintDialog::Selection;
+        d->fromPage = 0;
+        d->toPage = 0;
+    } else if (pd->Flags & PD_PAGENUMS) {
+        d->printRange = QPrintDialog::PageRange;
+        d->fromPage = pd->nFromPage;
+        d->toPage = pd->nToPage;
+    } else {
+        d->printRange = QPrintDialog::AllPages;
+        d->fromPage = d->minPage;
+        d->toPage = d->maxPage;
+    }
+
+    d->ep->printToFile = (pd->Flags & PD_PRINTTOFILE) != 0;
+
+    d->ep->readDevnames(pd->hDevNames);
+    d->ep->readDevmode(pd->hDevMode);
+
+}
+#endif // UNICODE
+
 QPrintDialog::QPrintDialog(QPrinter *printer, QWidget *parent)
     : QAbstractPrintDialog( *(new QPrintDialogPrivate), printer, parent)
 {
@@ -81,54 +236,23 @@ int QPrintDialog::exec()
         QApplicationPrivate::enterModal(parent);
     }
 
-    PRINTDLG pd;
-    memset(&pd, 0, sizeof(PRINTDLG));
-    pd.lStructSize = sizeof(PRINTDLG);
-
-    pd.hDevMode = d->ep->devMode;
-
     HGLOBAL *tempDevNames = d->ep->createDevNames();
-    pd.hDevNames  = tempDevNames;
 
-    pd.Flags = PD_RETURNDC;
-    pd.Flags |= PD_USEDEVMODECOPIESANDCOLLATE;
-
-    if (!(d->options & PrintSelection))
-        pd.Flags |= PD_NOSELECTION;
-    if (d->options & PrintPageRange) {
-        pd.nMinPage = d->minPage;
-        pd.nMaxPage = d->maxPage;
-    }
-
-    if(!(d->options & PrintToFile))
-        pd.Flags |= PD_DISABLEPRINTTOFILE;
-
-    if (d->printRange == Selection)
-        pd.Flags |= PD_SELECTION;
-    else if (d->printRange == PageRange)
-        pd.Flags |= PD_PAGENUMS;
-    else
-        pd.Flags |= PD_ALLPAGES;
-
-    // As stated by MSDN, to enable collate option when minpage==maxpage==0
-    // set the PD_NOPAGENUMS flag
-    if (pd.nMinPage==0 && pd.nMaxPage==0)
-        pd.Flags |= PD_NOPAGENUMS;
-
-    if (d->ep->printToFile)
-        pd.Flags |= PD_PRINTTOFILE;
-    pd.hwndOwner = parent ? parent->winId() : 0;
-    pd.nFromPage = qMax(d->fromPage, d->minPage);
-    pd.nToPage   = qMin(d->toPage, d->maxPage);
-    QT_WA( {
-        pd.nCopies = d->ep->devModeW()->dmCopies;
+    bool result;
+    void *voidp;
+    QT_WA({
+        PRINTDLGW *pd = qt_win_make_PRINTDLGW(parent, d, tempDevNames);
+        voidp = pd; // store until later
+        result = PrintDlgW(pd);
+        if (result && pd->hDC == 0)
+            result = false;
     }, {
-        pd.nCopies = d->ep->devModeA()->dmCopies;
-    } );
-
-    bool result = PrintDlg(&pd);
-    if (result && pd.hDC == 0)
-        result = false;
+        PRINTDLGA *pd = qt_win_make_PRINTDLGA(parent, d, tempDevNames);
+        voidp = pd; // store until later
+        result = PrintDlgA(pd);
+        if (result && pd->hDC == 0)
+            result = false;
+    });
 
     if (parent) {
         QApplicationPrivate::leaveModal(parent);
@@ -140,24 +264,15 @@ int QPrintDialog::exec()
 
     // write values back...
     if (result) {
-        if (pd.Flags & PD_SELECTION) {
-            d->printRange = Selection;
-            d->fromPage = 0;
-            d->toPage = 0;
-        } else if (pd.Flags & PD_PAGENUMS) {
-            d->printRange = PageRange;
-            d->fromPage = pd.nFromPage;
-            d->toPage = pd.nToPage;
-        } else {
-            d->printRange = AllPages;
-            d->fromPage = d->minPage;
-            d->toPage = d->maxPage;
-        }
-
-        d->ep->printToFile = (pd.Flags & PD_PRINTTOFILE) != 0;
-
-        d->ep->readDevnames(pd.hDevNames);
-        d->ep->readDevmode(pd.hDevMode);
+        QT_WA({
+            PRINTDLGW *pd = reinterpret_cast<PRINTDLGW *>(voidp);
+            qt_win_read_back_PRINTDLGW(pd, d);
+            qt_win_clean_up_PRINTDLGW(&pd);
+        }, {
+            PRINTDLGA *pd = reinterpret_cast<PRINTDLGA *>(voidp);
+            qt_win_read_back_PRINTDLGA(pd, d);
+            qt_win_clean_up_PRINTDLGA(&pd);
+        });
     }
 
     // Cleanup...
