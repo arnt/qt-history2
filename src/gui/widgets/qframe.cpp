8026a33e7ea254317e6ae630f0a26fef14fc28a2
hwall <hwall@hwall-extra>
2005-09-15 09:01:21 +0100

    Fixes:    Unnecessary repaints. Task:     none AutoTest: none Details:  Don't repaint if the paint rectangle don't intersects the rectangle.
    [git-p4: depot-paths = "//depot/qt/main/": change = 192989]

--- src/gui/widgets/qframe.cpp
+++ src/gui/widgets/qframe.cpp
@@ -471,8 +471,18 @@ QSize QFrame::sizeHint() const
 /*!\reimp
 */
 
-void QFrame::paintEvent(QPaintEvent *)
+void QFrame::paintEvent(QPaintEvent *event)
 {
+    Q_D(QFrame);
+    const QRect er = event->rect();
+    const QRect tr = this->rect();
+    const int fw = d->frameWidth;
+    if (er.left() >= tr.left() + fw
+        && er.top() >= tr.top() + fw
+        && er.right() <= tr.right() - fw
+        && er.bottom() <= tr.bottom() - fw)
+        return;
+
     QPainter paint(this);
     drawFrame(&paint);
 }
