36b7b7b406f5384179371f8bfdaeb80dd9a887d0
mmonsen <mmonsen@trolltech.com>
2005-09-16 14:54:07 +0100

    Handle more types when sorting.
    [git-p4: depot-paths = "//depot/qt/main/": change = 193153]

--- src/gui/itemviews/qsortingproxymodel_p.h
+++ src/gui/itemviews/qsortingproxymodel_p.h
@@ -28,6 +28,7 @@
 
 #include <qnamespace.h>
 #include <private/qmappingproxymodel_p.h>
+#include <qdatetime.h>
 
 class QSortingProxyModelPrivate : private QMappingProxyModelPrivate
 {
@@ -37,14 +38,66 @@ public:
 
     static bool defaultLessThan(const QModelIndex &left, const QModelIndex &right)
     {
-        return (left.model()->data(left, Qt::DisplayRole).toString()
-                < right.model()->data(right, Qt::DisplayRole).toString());
+        QVariant l = left.model()->data(left, Qt::DisplayRole);
+        QVariant r = right.model()->data(right, Qt::DisplayRole);
+        Q_ASSERT(l.type() == r.type());
+        switch (l.type()) {
+        case QVariant::Int:
+            return l.toInt() < r.toInt();
+        case QVariant::UInt:
+            return l.toUInt() < r.toUInt();
+        case QVariant::LongLong:
+            return l.toLongLong() < r.toLongLong();
+        case QVariant::ULongLong:
+            return l.toULongLong() < r.toULongLong();
+        case QVariant::Double:
+            return l.toDouble() < r.toDouble();
+        case QVariant::Char:
+            return l.toChar() < r.toChar();
+        case QVariant::Date:
+            return l.toDate() < r.toDate();
+        case QVariant::Time:
+            return l.toTime() < r.toTime();
+        case QVariant::DateTime:
+            return l.toDateTime() < r.toDateTime();
+        case QVariant::String:
+        default:
+            return l.toString() < r.toString();
+        }
+
+        return false;
     }
 
     static bool defaultGreaterThan(const QModelIndex &left, const QModelIndex &right)
     {
-        return (left.model()->data(left, Qt::DisplayRole).toString()
-                > right.model()->data(right, Qt::DisplayRole).toString());
+        QVariant l = left.model()->data(left, Qt::DisplayRole);
+        QVariant r = right.model()->data(right, Qt::DisplayRole);
+        Q_ASSERT(l.type() == r.type());
+        switch (l.type()) {
+        case QVariant::Int:
+            return l.toInt() > r.toInt();
+        case QVariant::UInt:
+            return l.toUInt() > r.toUInt();
+        case QVariant::LongLong:
+            return l.toLongLong() > r.toLongLong();
+        case QVariant::ULongLong:
+            return l.toULongLong() > r.toULongLong();
+        case QVariant::Double:
+            return l.toDouble() > r.toDouble();
+        case QVariant::Char:
+            return l.toChar() > r.toChar();
+        case QVariant::Date:
+            return l.toDate() > r.toDate();
+        case QVariant::Time:
+            return l.toTime() > r.toTime();
+        case QVariant::DateTime:
+            return l.toDateTime() > r.toDateTime();
+        case QVariant::String:
+        default:
+            return l.toString() > r.toString();
+        }
+
+        return false;
     }
     
     QSortingProxyModelPrivate()
