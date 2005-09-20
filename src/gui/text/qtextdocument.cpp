c7bcc3c9865d0dda1ce2ad8f604533ad94414c4e
Simon Hausmann <shausman@trolltech.com>
2005-09-20 09:34:07 +0100

    Fixes:    QTextDocument::clone() not preserving the default document font AutoTest: (see this submit)
    [git-p4: depot-paths = "//depot/qt/main/": change = 193329]

--- src/gui/text/qtextdocument.cpp
+++ src/gui/text/qtextdocument.cpp
@@ -277,6 +277,7 @@ QTextDocument *QTextDocument::clone(QObject *parent) const
     doc->d_func()->config()->title = d->config()->title;
     doc->d_func()->pageSize = d->pageSize;
     doc->d_func()->useDesignMetrics = d->useDesignMetrics;
+    doc->d_func()->setDefaultFont(d->defaultFont());
     return doc;
 }
 
