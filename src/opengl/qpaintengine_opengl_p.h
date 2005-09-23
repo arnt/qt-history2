e0c07fc0b3ff0114abd73c6405c472d92cc006a8
Trond Kjernaasen <trond@trolltech.com>
2005-09-23 09:52:19 +0100

    warnings
    [git-p4: depot-paths = "//depot/qt/main/": change = 193743]

--- src/opengl/qpaintengine_opengl_p.h
+++ src/opengl/qpaintengine_opengl_p.h
@@ -53,12 +53,23 @@ public:
     void drawLines(const QLineF *lines, int lineCount);
     void drawPoints(const QPointF *p, int pointCount);
 
+#ifdef Q_NO_USING_KEYWORD
+    inline void drawPoints(const QPoint *points, int pointCount) { QPaintEngine::drawPoints(points, pointCount); }
+    inline void drawPolygon(const QPoint *points, int pointCount, PolygonDrawMode mode)
+        { QPaintEngine::drawPolygon(points, pointCount, mode); }
+    inline void drawLines(const QLine *lines, int lineCount) { QPaintEngine::drawLines(lines, lineCount); }
+    inline void drawRects(const QRect *r, int rectCount) { QPaintEngine::drawRects(r, rectCount); }
+#else
+    using QPaintEngine::drawRects;
+    using QPaintEngine::drawPolygon;
+    using QPaintEngine::drawPoints;
+    using QPaintEngine::drawLines;
+#endif
+
     void drawPixmap(const QRectF &r, const QPixmap &pm, const QRectF &sr);
 
     void drawPath(const QPainterPath &path);
     void drawPolygon(const QPointF *points, int pointCount, PolygonDrawMode mode);
-    inline void drawPolygon(const QPoint *points, int pointCount, PolygonDrawMode mode)
-        { QPaintEngine::drawPolygon(points, pointCount, mode); }
     void drawTiledPixmap(const QRectF &r, const QPixmap &pixmap, const QPointF &s);
     void drawImage(const QRectF &r, const QImage &image, const QRectF &sr,
                    Qt::ImageConversionFlags conversionFlags);
