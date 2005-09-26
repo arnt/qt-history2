96c762d4bae28da6f1487dfb5c26fa9fdd4601f1
dboddie <dboddie@mikaeld-lurven>
2005-09-26 14:56:47 +0100

    Documentation and example about widget masks. Task:     ? There was one about the Tux example.
    [git-p4: depot-paths = "//depot/qt/main/": change = 194008]

---
+++ examples/widgets/shapedclock/shapedclock.cpp
@@ -0,0 +1,113 @@
+/****************************************************************************
+**
+** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QtGui>
+
+#include "shapedclock.h"
+
+ShapedClock::ShapedClock(QWidget *parent)
+    : QWidget(parent)
+{
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(update()));
+    timer->start(1000);
+
+    setMouseTracking(true);
+    setWindowFlags(windowFlags() | Qt::FramelessWindowHint);
+    setWindowTitle(tr("Shaped Analog Clock"));
+}
+
+void ShapedClock::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        dragPosition = event->globalPos() - frameGeometry().topLeft();
+        event->accept();
+    }
+}
+
+void ShapedClock::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() == Qt::LeftButton) {
+        move(event->globalPos() - dragPosition);
+        event->accept();
+    }
+}
+
+void ShapedClock::paintEvent(QPaintEvent *)
+{
+    static const QPoint hourHand[3] = {
+        QPoint(7, 8),
+        QPoint(-7, 8),
+        QPoint(0, -40)
+    };
+    static const QPoint minuteHand[3] = {
+        QPoint(7, 8),
+        QPoint(-7, 8),
+        QPoint(0, -70)
+    };
+
+    QColor hourColor(127, 0, 127);
+    QColor minuteColor(0, 127, 127, 191);
+
+    int side = qMin(width(), height());
+    QTime time = QTime::currentTime();
+
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.translate(width() / 2, height() / 2);
+    painter.scale(side / 200.0, side / 200.0);
+
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(hourColor);
+
+    painter.save();
+    painter.rotate(30.0 * ((time.hour() + time.minute() / 60.0)));
+    painter.drawConvexPolygon(hourHand, 3);
+    painter.restore();
+
+    painter.setPen(hourColor);
+
+    for (int i = 0; i < 12; ++i) {
+        painter.drawLine(88, 0, 96, 0);
+        painter.rotate(30.0);
+    }
+
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(minuteColor);
+
+    painter.save();
+    painter.rotate(6.0 * (time.minute() + time.second() / 60.0));
+    painter.drawConvexPolygon(minuteHand, 3);
+    painter.restore();
+
+    painter.setPen(minuteColor);
+
+    for (int j = 0; j < 60; ++j) {
+        if ((j % 5) != 0)
+            painter.drawLine(92, 0, 96, 0);
+        painter.rotate(6.0);
+    }
+}
+
+void ShapedClock::resizeEvent(QResizeEvent *event)
+{
+    int side = qMin(width(), height());
+    QRegion maskedRegion(width()/2 - side/2, height()/2 - side/2, side, side,
+                         QRegion::Ellipse);
+    setMask(maskedRegion);
+}
+
+QSize ShapedClock::sizeHint() const
+{
+    return QSize(100, 100);
+}
---
+++ examples/widgets/shapedclock/shapedclock.h
@@ -0,0 +1,37 @@
+/****************************************************************************
+**
+** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef SHAPEDCLOCK_H
+#define SHAPEDCLOCK_H
+
+#include <QWidget>
+
+class ShapedClock : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ShapedClock(QWidget *parent = 0);
+    QSize sizeHint() const;
+
+protected:
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void paintEvent(QPaintEvent *event);
+    void resizeEvent(QResizeEvent *event);
+
+private:
+    QPoint dragPosition;
+};
+
+#endif
