f15cc9dc24c37efd4a1152dd9caca406d07e62f9
Brad Hughes <bhughes@trolltech.com>
2005-09-27 09:26:51 +0100

    use config.tests/unix/compile.test to do the stl test... and instead of running the stltest output, we simply rely on the compilation result. this allows the stl test result to be used when cross-compiling
    [git-p4: depot-paths = "//depot/qt/main/": change = 194118]

--- config.tests/unix/stl/stltest.cpp
+++ config.tests/unix/stl/stltest.cpp
@@ -11,64 +11,50 @@ templates for common STL container classes.
 
 int main()
 {
-    int rval = 0;
-
     std::vector<int> v1;
     v1.push_back( 0 );
     v1.push_back( 1 );
     v1.push_back( 2 );
     v1.push_back( 3 );
     v1.push_back( 4 );
-    if ( v1.size() != 5 ) {
-	std::cout << "stltest: vector failed size check, expected 5, got " << v1.size();
-	++rval;
-    }
-    if ( v1.capacity() < 5 ) {
-	std::cout << "stltest: vector failed capacity check, expected >= 5, got " << v1.capacity();
-	++rval;
-    }
+    int v1size = v1.size();
+    v1size = 0;
+    int v1capacity = v1.capacity();
+    v1capacity = 0;
+
     std::vector<int>::iterator v1it = std::find( v1.begin(), v1.end(), 99 );
-    if ( v1it != v1.end() ) {
-	std::cout << "stltest: find failed, expected end(), got " << *v1it;
-	++rval;
-    }
+    bool v1notfound = (v1it == v1.end());
+    v1notfound = false;
+
     v1it = std::find( v1.begin(), v1.end(), 3 );
-    if ( v1it == v1.end() ) {
-	std::cout << "stltest: find failed, expected to find element, got end()";
-	++rval;
-    }
+    bool v1found = (v1it != v1.end());
+    v1found = false;
+
     std::vector<int> v2;
     std::copy( v1.begin(), v1it, std::back_inserter( v2 ) );
-    if ( v2.size() != 3 ) {
-	std::cout << "stltest: copy failed, expected 3 elements, got " << v2.size();
-	++rval;
-    }
+    int v2size = v2.size();
+    v2size = 0;
 
     std::map<int, double> m1;
     m1.insert( std::make_pair( 1, 2.0 ) );
     m1.insert( std::make_pair( 3, 2.0 ) );
     m1.insert( std::make_pair( 5, 2.0 ) );
     m1.insert( std::make_pair( 7, 2.0 ) );
-    if ( m1.size() != 4 ) {
-	std::cout << "stltest: map failed size check, expected 4, got " << m1.size();
-	++rval;
-    }
+    int m1size = m1.size();
+    m1size = 0;
     std::map<int,double>::iterator m1it = m1.begin();
     for ( ; m1it != m1.end(); ++m1it ) {
-	if ( (*m1it).second != 2 ) {
-	    std::cout << "stltest: iterator failed, expected 2, got " << (*m1it).second;
-	    ++rval;
-	}
+        int first = (*m1it).first;
+        first = 0;
+        double second = (*m1it).second;
+        second = 0.0;
     }
     std::map< int, double > m2( m1 );
-    if ( m2.size() != m1.size() ) {
-	std::cout << "stltest: map copy failed, expected " << m1.size() << " elements, got " << m2.size();
-	++rval;
-    }
+    int m2size = m2.size();
+    m2size = 0;
 
-    return rval;
+    return 0;
 }
-
 
 // something mean to see if the compiler and C++ standard lib are good enough
 template<class K, class T>
@@ -80,4 +66,3 @@ class DummyClass
     // typename implemented ?
     typedef typename std::map<K,T>::iterator MyIterator;
 };
-
--- config.tests/unix/stl/stltest.pro
+++
@@ -1,5 +0,0 @@
-TARGET=stltest
-CONFIG-=qt dylib
-mac:CONFIG -= app_bundle
-TEMPLATE=app
-SOURCES=stltest.cpp
