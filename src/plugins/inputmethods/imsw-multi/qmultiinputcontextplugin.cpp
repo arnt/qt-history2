6605f860526f4ad3b22a0eb60f6be82918547a23
Dimitri Papadopoulos <dimitri@trolltech.com>
2005-09-28 21:06:13 +0100

    Replace obsolete Trolltech copyright notice.
    [git-p4: depot-paths = "//depot/qt/main/": change = 194427]

--- src/plugins/inputmethods/imsw-multi/qmultiinputcontextplugin.cpp
+++ src/plugins/inputmethods/imsw-multi/qmultiinputcontextplugin.cpp
@@ -1,5 +1,17 @@
 /****************************************************************************
-** $Id$
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+/****************************************************************************
 **
 ** Implementation of QMultiInputContextPlugin class
 **
@@ -10,27 +22,6 @@
 ** description is copied from their original file headers. Contact
 ** immodule-qt@freedesktop.org if any conditions of this licensing are
 ** not clear to you.
-**
-****************************************************************************/
-
-/****************************************************************************
-**
-** Copyright (C) 1992-2004 Trolltech AS. All rights reserved.
-**
-** This file is part of the input method module of the Qt Toolkit.
-**
-** Licensees holding valid Qt Preview licenses may use this file in
-** accordance with the Qt Preview License Agreement provided with the
-** Software.
-**
-** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
-** information about Qt Commercial License Agreements.
-**
-** Contact info@trolltech.com if any conditions of this licensing are
-** not clear to you.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
 ****************************************************************************/
 
