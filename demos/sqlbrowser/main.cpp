d65b9eaeb086c49fff0ca60e0470850722ab24e7
dboddie <dboddie@mikaeld-lurven>
2005-09-30 14:40:27 +0100

    Fixes:    Incorrect or missing license information. Task:     88030
    [git-p4: depot-paths = "//depot/qt/main/": change = 194695]

--- demos/sqlbrowser/main.cpp
+++ demos/sqlbrowser/main.cpp
@@ -1,14 +1,15 @@
 /****************************************************************************
- **
- ** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
- **
- ** This file is an example program for the Qt SQL module.
- ** EDITIONS: NOLIMITS
- **
- ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- **
- ****************************************************************************/
+**
+** Copyright (C) 2004-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
 
 #include "browser.h"
 
