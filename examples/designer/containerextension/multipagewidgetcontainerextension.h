868554991ec60681e97789b0203f9b7139d792d8
Vivi Karlsen <vkarlsen@trolltech.com>
2005-09-30 14:30:37 +0100

    doc: removed unnecessary includes from the Designer extensions examples, and added licenses to all the example files.
    [git-p4: depot-paths = "//depot/qt/main/": change = 194690]

--- examples/designer/containerextension/multipagewidgetcontainerextension.h
+++ examples/designer/containerextension/multipagewidgetcontainerextension.h
@@ -1,8 +1,20 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #ifndef MULTIPAGEWIDGETCONTAINEREXTENSION_H
 #define MULTIPAGEWIDGETCONTAINEREXTENSION_H
 
 #include <QtDesigner/QDesignerContainerExtension>
-#include <QtDesigner/QExtensionFactory>
 
 class QExtensionManager;
 class MultiPageWidget;
--- examples/designer/containerextension/multipagewidgetextensionfactory.cpp
+++ examples/designer/containerextension/multipagewidgetextensionfactory.cpp
@@ -1,6 +1,15 @@
-#include <QtDesigner/QDesignerIconCacheInterface>
-#include <QtDesigner/QDesignerFormEditorInterface>
-#include <QtDesigner/ui4.h>
+/****************************************************************************
+**
+** Copyright (C) 2005-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
 
 #include "multipagewidgetextensionfactory.h"
 #include "multipagewidgetcontainerextension.h"
