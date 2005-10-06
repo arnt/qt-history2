029f207da3a84e2480c32c299229a7b7e70b6e07
gunnar <gunnar@THAUMASIA>
2005-10-06 15:14:33 +0100

    Demo plugins for designer
    [git-p4: depot-paths = "//depot/qt/main/": change = 195322]

---
+++ demos/arthurplugin/arthur_plugin.pro
@@ -0,0 +1,46 @@
+
+QTDIR       = $$(QTDIR)
+
+CONFIG      += designer plugin
+TEMPLATE    = lib
+DESTDIR     = $$QTDIR/plugins/designer
+
+SHARED_FOLDER = ../shared
+include(../shared/shared.pri)
+
+DEMO_DEFORM_DIR = $$QTDIR/demos/deform
+DEMO_AFFINE_DIR = $$QTDIR/demos/affine
+DEMO_GRADIENT_DIR = $$QTDIR/demos/gradients
+DEMO_STROKE_DIR = $$QTDIR/demos/pathstroke
+DEMO_COMPOSITION_DIR = $$QTDIR/demos/composition
+
+INCLUDEPATH += $$DEMO_DEFORM_DIR $$DEMO_AFFINE_DIR $$DEMO_GRADIENT_DIR $$DEMO_STROKE_DIR $$DEMO_COMPOSITION_DIR
+
+SOURCES = plugin.cpp \
+	$$DEMO_COMPOSITION_DIR/composition.cpp \
+        $$DEMO_AFFINE_DIR/xform.cpp \
+        $$DEMO_DEFORM_DIR/pathdeform.cpp \
+        $$DEMO_GRADIENT_DIR/gradients.cpp \
+        $$DEMO_STROKE_DIR/pathstroke.cpp \
+             
+
+HEADERS = \
+	$$DEMO_COMPOSITION_DIR/composition.h \
+        $$DEMO_AFFINE_DIR/xform.h \
+        $$DEMO_DEFORM_DIR/pathdeform.h \
+        $$DEMO_GRADIENT_DIR/gradients.h \
+        $$DEMO_STROKE_DIR/pathstroke.h \
+
+              
+# install
+target.path = $$[QT_INSTALL_PLUGINS]/designer
+sources.files = $$SOURCES $$HEADERS *.pro
+sources.path = $$[QT_INSTALL_EXAMPLES]/designer/customwidgetplugin
+INSTALLS += target sources
+
+RESOURCES += arthur_plugin.qrc
+
+win32-msvc.net {
+	QMAKE_CFLAGS += /Zm500
+	QMAKE_CXXFLAGS += /Zm500
+}
---
+++ demos/arthurplugin/plugin.cpp
@@ -0,0 +1,199 @@
+#include <QtDesigner/QDesignerContainerExtension>
+#include <QtDesigner/QDesignerCustomWidgetInterface>
+
+#include <QtCore/qplugin.h>
+#include <QtGui/QIcon>
+#include <QtGui/QLineEdit>
+
+#include "xform.h"
+#include "pathdeform.h"
+#include "gradients.h"
+#include "pathstroke.h"
+#include "hoverpoints.h"
+#include "composition.h"
+
+class QDesignerFormEditorInterface;
+
+class PathDeformRendererEx : public PathDeformRenderer
+{
+    Q_OBJECT
+public:
+    PathDeformRendererEx(QWidget *parent) : PathDeformRenderer(parent) { }
+    QSize sizeHint() const { return QSize(300, 200); }
+};
+
+class DemoPlugin : public QDesignerCustomWidgetInterface
+{
+    Q_INTERFACES(QDesignerCustomWidgetInterface)
+        public:
+    DemoPlugin() : m_initialized(false) { }
+    bool isContainer() const { return false; }
+    bool isInitialized() const { return m_initialized; }
+    QIcon icon() const { return QIcon(); }
+    QString codeTemplate() const { return QString(); }
+    QString whatsThis() const { return QString(); }
+    QString toolTip() const { return QString(); }
+    QString group() const { return "Arthur Widgets [Demo]"; }
+    void initialize(QDesignerFormEditorInterface *)
+    {
+        if (m_initialized)
+            return;
+        m_initialized = true;
+    }
+private:
+    bool m_initialized;
+};
+
+class DeformPlugin : public QObject, public DemoPlugin
+{
+    Q_OBJECT
+public:
+    DeformPlugin(QObject *parent = 0) : QObject(parent) { }
+
+    QString includeFile() const { return "deform.h"; }
+    QString name() const { return "PathDeformRendererEx"; }
+
+    QWidget *createWidget(QWidget *parent)
+    {
+        PathDeformRenderer *deform = new PathDeformRendererEx(parent);
+        deform->setRadius(70);
+        deform->setAnimated(false);
+        deform->setFontSize(20);
+        deform->setText("Arthur Widgets Demo");
+
+        return deform;
+    }
+
+};
+
+class XFormRendererEx : public XFormView
+{
+    Q_OBJECT
+public:
+    XFormRendererEx(QWidget *parent) : XFormView(parent) { textEditor = new QLineEdit; }
+    QSize sizeHint() const { return QSize(300, 200); }
+
+public slots:
+void setText(const QString &text) { textEditor->setText(text); }
+};
+
+class XFormPlugin : public QObject, public DemoPlugin
+{
+    Q_OBJECT
+public:
+    XFormPlugin(QObject *parent = 0) : QObject(parent) { }
+    QString includeFile() const { return "xform.h"; }
+    QString name() const { return "XFormRendererEx"; }
+
+    QWidget *createWidget(QWidget *parent)
+    {
+        XFormRendererEx *xform = new XFormRendererEx(parent);
+        xform->setText("Qt - Hello World!!");
+        return xform;
+    }
+};
+
+
+class GradientEditorPlugin : public QObject, public DemoPlugin
+{
+    Q_OBJECT
+public:
+    GradientEditorPlugin(QObject *parent = 0) : QObject(parent) { }
+    QString includeFile() const { return "gradients.h"; }
+    QString name() const { return "GradientEditor"; }
+
+    QWidget *createWidget(QWidget *parent)
+    {
+        GradientEditor *editor = new GradientEditor(parent);
+        return editor;
+    }
+};
+
+class GradientRendererEx : public GradientRenderer
+{
+    Q_OBJECT
+public:
+    GradientRendererEx(QWidget *p) : GradientRenderer(p) { }
+    QSize sizeHint() const { return QSize(300, 200); }
+};
+
+class GradientRendererPlugin : public QObject, public DemoPlugin
+{
+    Q_OBJECT
+public:
+    GradientRendererPlugin(QObject *parent = 0) : QObject(parent) { }
+    QString includeFile() const { return "gradients.h"; }
+    QString name() const { return "GradientRendererEx"; }
+
+    QWidget *createWidget(QWidget *parent)
+    {
+        GradientRenderer *renderer = new GradientRendererEx(parent);
+        renderer->setConicalGradient();
+        return renderer;
+    }
+};
+
+class PathStrokeRendererEx : public PathStrokeRenderer
+{
+    Q_OBJECT
+public:
+    PathStrokeRendererEx(QWidget *p) : PathStrokeRenderer(p) { }
+    QSize sizeHint() const { return QSize(300, 200); }
+};
+
+class StrokeRenderPlugin : public QObject, public DemoPlugin
+{
+    Q_OBJECT
+public:
+    StrokeRenderPlugin(QObject *parent = 0) : QObject(parent) { }
+    QString includeFile() const { return "pathstroke.h"; }
+    QString name() const { return "PathStrokeRendererEx"; }
+
+    QWidget *createWidget(QWidget *parent)
+    {
+        PathStrokeRenderer *stroke = new PathStrokeRendererEx(parent);
+        return stroke;
+    }
+};
+
+
+class CompositionModePlugin : public QObject, public DemoPlugin
+{
+    Q_OBJECT
+public:
+    CompositionModePlugin(QObject *parent = 0) : QObject(parent) { }
+    QString includeFile() const { return "composition.h"; }
+    QString name() const { return "CompositionRenderer"; }
+
+    QWidget *createWidget(QWidget *parent)
+    {
+        CompositionRenderer *renderer = new CompositionRenderer(parent);
+        renderer->setAnimationEnabled(false);
+        return renderer;
+    }
+};
+
+
+class ArthurPlugins : public QObject, public QDesignerCustomWidgetCollectionInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QDesignerCustomWidgetCollectionInterface)
+        public:
+    QList<QDesignerCustomWidgetInterface*> customWidgets() const
+    {
+        QList<QDesignerCustomWidgetInterface *> plugins;
+        plugins
+            << new DeformPlugin
+            << new XFormPlugin
+            << new GradientEditorPlugin
+            << new GradientRendererPlugin
+            << new StrokeRenderPlugin
+            << new CompositionModePlugin;
+        return plugins;
+    }
+};
+
+
+#include "plugin.moc"
+
+Q_EXPORT_PLUGIN(ArthurPlugins)
