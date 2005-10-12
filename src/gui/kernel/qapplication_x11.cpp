9a55b015e6b97187462ef43560ea7d7a44e744ad
benjamin <bmeyer@trolltech.com>
2005-10-12 15:43:43 +0100

    Fixes:    revert
    [git-p4: depot-paths = "//depot/qt/main/": change = 195980]

--- src/gui/kernel/qapplication_x11.cpp
+++ src/gui/kernel/qapplication_x11.cpp
@@ -1828,7 +1828,6 @@ void qt_init(QApplicationPrivate *priv, int,
 #endif // QT_NO_TABLET
 
         X11->startupId = getenv("DESKTOP_STARTUP_ID");
-        // On linux this "leaks" memory because putenv makes a copy while solaris (and others?) alias
         putenv(strdup("DESKTOP_STARTUP_ID="));
 
     } else {
