fcfe693b66cbf4d7651b47c7a368f31436b571d3
jasaethe <jasaethe@mike-macondo>
2005-10-13 09:13:25 +0100

    Fixes:    Use only Q_UINT64_C constructors for 64-bit endian conversion functions.                  Use C-style comments. Task:     <task number> AutoTest: <change number> Details:
    [git-p4: depot-paths = "//depot/qt/main/": change = 196041]

--- src/corelib/global/qendian.h
+++ src/corelib/global/qendian.h
@@ -20,7 +20,7 @@ QT_MODULE(Core)
 
 /*
  * ENDIAN FUNCTIONS
- */
+*/
 inline void qbswap_helper(const uchar *src, uchar *dest, int size)
 {
     for (int i = 0; i < size ; ++i) dest[i] = src[size - 1 - i];
@@ -31,7 +31,7 @@ inline void qbswap_helper(const uchar *src, uchar *dest, int size)
  * Changes the byte order of \a src from big endian to little endian or vice versa
  * and stores the result in \a dest.
  * There is no alignment requirements for \a dest.
- */
+*/
 template <typename T> inline void qbswap(const T src, uchar *dest)
 {
     qbswap_helper(static_cast<T>(&src), dest, sizeof(T));
@@ -48,7 +48,7 @@ template <typename T> inline void qToUnaligned(const T src, uchar *dest)
  * This function will read a little-endian encoded value from \a src
  * and return the value in host-endian encoding.
  * There is no requirement that \a src must be aligned.
- */
+*/
 #if defined Q_CC_MSVC && _MSC_VER < 1300 || defined Q_CC_SUN
 inline quint64 qFromLittleEndian_helper(const uchar *src, quint64 *dest)
 {
@@ -67,16 +67,16 @@ inline quint32 qFromLittleEndian_helper(const uchar *src, quint32 *dest)
 {
     return 0
         | src[0]
-        | src[1] * Q_UINT64_C(0x0000000000000100)
-        | src[2] * Q_UINT64_C(0x0000000000010000)
-        | src[3] * Q_UINT64_C(0x0000000001000000);
+        | src[1] * quint32(0x00000100)
+        | src[2] * quint32(0x00010000)
+        | src[3] * quint32(0x01000000);
 }
 
 inline quint16 qFromLittleEndian_helper(const uchar *src, quint16 *dest)
 {
     return 0
         | src[0]
-        | src[1] * Q_UINT64_C(0x0000000000000100);
+        | src[1] * 0x0100;
 }
 
 inline qint64 qFromLittleEndian_helper(const uchar *src, qint64 * dest)
@@ -110,16 +110,16 @@ template <> inline quint32 qFromLittleEndian(const uchar *src)
 {
     return 0
         | src[0]
-        | src[1] * Q_UINT64_C(0x0000000000000100)
-        | src[2] * Q_UINT64_C(0x0000000000010000)
-        | src[3] * Q_UINT64_C(0x0000000001000000);
+        | src[1] * quint32(0x00000100)
+        | src[2] * quint32(0x00010000)
+        | src[3] * quint32(0x01000000);
 }
 
 template <> inline quint16 qFromLittleEndian(const uchar *src)
 {
     return 0
         | src[0]
-        | src[1] * Q_UINT64_C(0x0000000000000100);
+        | src[1] * 0x0100;
 }
 
 // signed specializations
@@ -136,7 +136,7 @@ template <> inline qint16 qFromLittleEndian(const uchar *src)
 /* This function will read a big-endian (also known as network order) encoded value from \a src
  * and return the value in host-endian encoding.
  * There is no requirement that \a src must be aligned.
- */
+*/
 #if defined Q_CC_MSVC && _MSC_VER < 1300 || defined Q_CC_SUN
 inline quint64 qFromBigEndian_helper(const uchar *src, quint64 *dest)
 {
@@ -155,16 +155,16 @@ inline quint32 qFromBigEndian_helper(const uchar *src, quint32 * dest)
 {
     return 0
         | src[3]
-        | src[2] * Q_UINT64_C(0x0000000000000100)
-        | src[1] * Q_UINT64_C(0x0000000000010000)
-        | src[0] * Q_UINT64_C(0x0000000001000000);
+        | src[2] * quint32(0x00000100)
+        | src[1] * quint32(0x00010000)
+        | src[0] * quint32(0x01000000);
 }
 
 inline quint16 qFromBigEndian_helper(const uchar *src, quint16 * des)
 {
     return 0
         | src[1]
-        | src[0] * Q_UINT64_C(0x0000000000000100);
+        | src[0] * 0x0100;
 }
 
 
@@ -201,9 +201,9 @@ inline quint32 qFromBigEndian(const uchar *src)
 {
     return 0
         | src[3]
-        | src[2] * Q_UINT64_C(0x0000000000000100)
-        | src[1] * Q_UINT64_C(0x0000000000010000)
-        | src[0] * Q_UINT64_C(0x0000000001000000);
+        | src[2] * quint32(0x00000100)
+        | src[1] * quint32(0x00010000)
+        | src[0] * quint32(0x01000000);
 }
 
 template<>
@@ -211,7 +211,7 @@ inline quint16 qFromBigEndian(const uchar *src)
 {
     return 0
         | src[1]
-        | src[0] * Q_UINT64_C(0x0000000000000100);
+        | src[0] * quint16(0x0100);
 }
 
 
@@ -230,7 +230,7 @@ template <> inline qint16 qFromBigEndian(const uchar *src)
  * Changes the byte order of a value from big endian to little endian or vice versa.
  * This function can be used if you are not concerned about alignment issues,
  * and it is therefore a bit more convenient and in most cases more efficient.
- */
+*/
 template <typename T> T qbswap(T source);
 template <> inline quint64 qbswap(quint64 source)
 {
