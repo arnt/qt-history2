58295581ec54464520078a714621b56b8032ff08
dboddie <dboddie@mikaeld-lurven>
2005-10-17 15:37:41 +0100

    Doc: Changes to the QSvgWidget snippet.
    [git-p4: depot-paths = "//depot/qt/main/": change = 196365]

--- doc/src/snippets/qsvgwidget/main.cpp
+++ doc/src/snippets/qsvgwidget/main.cpp
@@ -4,7 +4,14 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QSvgWidget window(":/files/sunflower.svg");
+    QSvgWidget window(":/files/spheres.svg");
     window.show();
+    QSvgRenderer *renderer = window.renderer();
+    QImage image(150, 150, QImage::Format_RGB32);
+    QPainter painter;
+    painter.begin(&image);
+    renderer->render(&painter);
+    painter.end();
+    image.save("spheres.png", "PNG", 9);
     return app.exec();
 }
