830213b48f47f33d376181faa7efbbecd7f57b21
Sverre <sdjonne@trolltech.com>
2005-10-20 12:47:56 +0100

    Fixes:    custom widgets not exported in designer plugins Task:     77920 RevBy:    roberto, david AutoTest: none Details:  created a new header file with the export macro
    [git-p4: depot-paths = "//depot/qt/main/": change = 196818]

---
+++ tools/designer/src/lib/uilib/qdesignerexportwidget.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QDESIGNEREXPORTWIDGET_H
+#define QDESIGNEREXPORTWIDGET_H
+
+#include <QtCore/QtGlobal>
+
+#if 0
+// pragma for syncqt, don't remove.
+#pragma qt_class(QDesignerExportWidget)
+#endif
+
+#if defined(QDESIGNER_EXPORT_WIDGETS)
+#  define QDESIGNER_WIDGET_EXPORT Q_DECL_EXPORT
+#else
+#  define QDESIGNER_WIDGET_EXPORT Q_DECL_IMPORT
+#endif
+
+#endif //QDESIGNEREXPORTWIDGET_H
