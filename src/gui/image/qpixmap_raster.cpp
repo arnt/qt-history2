9a525e6f0d02ff6316ec5e90a2a4eef1faf015a5
Simon Hausmann <shausman@trolltech.com>
2005-10-29 18:56:41 +0100

    Fixes:    QPixmap::grabWidget when used on widgets with children on embedded and win32 RevBy:    Trust me Details:  grabWidget on x11 calls QWidgetPrivate::drawWidget with DrawRecursive, but                  the implementation qpixmap_raster.cpp missed that flag.
    [git-p4: depot-paths = "//depot/qt/main/": change = 197858]

--- src/gui/image/qpixmap_raster.cpp
+++ src/gui/image/qpixmap_raster.cpp
@@ -359,7 +359,7 @@ QPixmap QPixmap::grabWidget(QWidget *widget, const QRect &rect)
     QWidget *tlw = widget->window();
     QPoint tlwOffset = widget->mapTo(tlw, QPoint());
     r.translate(tlwOffset);
-    tlw->d_func()->drawWidget(&res, r, -r.topLeft(), QWidgetPrivate::DrawAsRoot | QWidgetPrivate::DrawPaintOnScreen | QWidgetPrivate::DrawInvisible);
+    tlw->d_func()->drawWidget(&res, r, -r.topLeft(), QWidgetPrivate::DrawRecursive | QWidgetPrivate::DrawAsRoot | QWidgetPrivate::DrawPaintOnScreen | QWidgetPrivate::DrawInvisible);
     return res;
 }
 
