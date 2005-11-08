1c82b1ef368b29c05d791d19d1adbe8361c5b22f
dboddie <dboddie@mikaeld-lurven>
2005-11-08 15:58:06 +0100

    p4i integration Integrate 198819 from 4.1 to main: Fixes:    Incorrect model/view documentation. RevBy:    Trust me
    [git-p4: depot-paths = "//depot/qt/main/": change = 198820]

---
+++ doc/src/snippets/qstringfiltermodel/main.cpp
@@ -0,0 +1,32 @@
+#include <QtGui>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QObject *parent = &app;
+
+    QStringList numbers;
+    numbers << "One" << "Two" << "Three" << "Four" << "Five";
+
+    QAbstractItemModel *stringListModel = new QStringListModel(numbers, parent);
+
+    QStringFilterModel *filterModel = new QStringFilterModel(parent);
+    filterModel->setSourceModel(stringListModel);
+
+    QWidget *window = new QWidget;
+
+    QListView *filteredView = new QListView(window);
+    filteredView->setModel(filterModel);
+    filteredView->setWindowTitle("Filtered view onto a string list model");
+
+    QLineEdit *patternEditor = new QLineEdit(window);
+    QObject::connect(patternEditor, SIGNAL(textChanged(const QString &)),
+                     filterModel, SLOT(setPattern(const QString &)));
+
+    QVBoxLayout *layout = new QVBoxLayout(window);
+    layout->addWidget(filteredView);
+    layout->addWidget(patternEditor);
+
+    window->show();
+    return app.exec();
+}
