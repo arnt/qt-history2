f19b4b6076424359f9abd0fa372ea3a53160d54f
jasaethe <jasaethe@mike-macondo>
2005-11-17 15:40:29 +0100

    Fixes:    Added tests for lupdate AutoTest: You wish... Details:  I am adding some stuff to the ts fileformat since I am adding some merging functionality,                  so before I submit I think it is a good idea that we make some autotests for lupdate.                  More tests will come, this is just the framework. It should be easier to add tests now.
    [git-p4: depot-paths = "//depot/qt/main/": change = 199961]

---
+++ tools/linguist/lupdate/tests/testdata/parseui/project.pro
@@ -0,0 +1,7 @@
+TEMPLATE = app
+LANGUAGE = C++
+
+FORMS += project.ui
+
+TRANSLATIONS        = project_no.ts
+
---
+++ tools/linguist/lupdate/tests/tst_lupdate.cpp
@@ -0,0 +1,148 @@
+#include <QtTest/QtTest>
+#include "testlupdate.h"
+#include <QtCore/QDir>
+class tst_lupdate : public QObject
+{
+    Q_OBJECT
+public:
+    tst_lupdate();
+
+private slots:
+    void parse_data();
+    void parse();
+    void merge_data();
+    void merge();
+    
+private:
+    TestLUpdate m_lupdate;
+    QString m_basePath;
+};
+
+tst_lupdate::tst_lupdate()
+{
+    
+    m_basePath = QDir::currentPath() + QLatin1String("/testdata/");
+    m_lupdate.setWorkingDirectory(QDir::currentPath());
+}
+
+void tst_lupdate::parse_data()
+{
+    QTest::addColumn<QString>("inputprofile");
+    QTest::addColumn<QString>("generatedtsfile");
+    QTest::addColumn<QString>("expectedtsfile");
+    
+    QTest::newRow("Parse UI file") << QString("parseui/project.pro") << QString() << QString();
+    //QTest::newRow("Merge UI file") << QString("parsecpp/project.pro") << QString() << QString();
+}
+
+void tst_lupdate::parse()
+{
+
+    QFETCH(QString, inputprofile);
+    QFETCH(QString, generatedtsfile);
+    QFETCH(QString, expectedtsfile);
+
+    if (inputprofile.startsWith("/")) inputprofile.remove(0,1);
+    QString inputpro = m_basePath + inputprofile;
+
+    // If the testcase did not give a filename, assume that is uses the default filenames.
+    if (generatedtsfile.isNull()) {
+        QString base = QFileInfo(inputpro).absolutePath();
+        generatedtsfile = base + QLatin1String("/project_no.ts");
+    } else {
+        if (generatedtsfile.startsWith("/")) generatedtsfile.remove(0,1);
+        generatedtsfile = m_basePath + generatedtsfile;
+    }
+
+    if (expectedtsfile.isNull()) {
+        QString base = QFileInfo(inputpro).absolutePath();
+        expectedtsfile = base + QLatin1String("/project_no.ts.result");
+    } else {
+        if (expectedtsfile.startsWith("/")) expectedtsfile.remove(0,1);
+        expectedtsfile = m_basePath + expectedtsfile;
+    }
+
+    m_lupdate.updateProFile(inputpro);
+    
+    QFile file1(generatedtsfile);
+    bool ok = file1.open(QIODevice::ReadOnly);
+    QVERIFY(ok);
+    QByteArray data1 = file1.readAll();
+    QString str1(data1);
+    
+    QFile file2(expectedtsfile);
+    ok = file2.open(QIODevice::ReadOnly);
+    QVERIFY(ok);
+    QByteArray data2 = file2.readAll();
+    QString str2(data2);
+
+    QCOMPARE(str1, str2);
+
+
+}
+
+void tst_lupdate::merge_data()
+{
+    QTest::addColumn<QString>("inputprofile");
+    QTest::addColumn<QString>("generatedtsfile");
+    QTest::addColumn<QString>("expectedtsfile");
+    
+    QTest::newRow("Merge UI file") << QString("mergeui/project.pro") << QString() << QString();
+    //QTest::newRow("Merge CPP file") << QString("mergecpp/project.pro") << QString() << QString();
+}
+
+void tst_lupdate::merge()
+{
+    QFETCH(QString, inputprofile);
+    QFETCH(QString, generatedtsfile);
+    QFETCH(QString, expectedtsfile);
+
+    if (inputprofile.startsWith("/")) inputprofile.remove(0,1);
+    QString inputpro = m_basePath + inputprofile;
+
+    // If the testcase did not give a filename, assume that is uses the default filenames.
+    if (generatedtsfile.isNull()) {
+        QString base = QFileInfo(inputpro).absolutePath();
+        generatedtsfile = base + QLatin1String("/project_no.ts");
+    } else {
+        if (generatedtsfile.startsWith("/")) generatedtsfile.remove(0,1);
+        generatedtsfile = m_basePath + generatedtsfile;
+    }
+
+    if (expectedtsfile.isNull()) {
+        QString base = QFileInfo(inputpro).absolutePath();
+        expectedtsfile = base + QLatin1String("/project_no.ts.result");
+    } else {
+        if (expectedtsfile.startsWith("/")) expectedtsfile.remove(0,1);
+        expectedtsfile = m_basePath + expectedtsfile;
+    }
+
+    // copy it to a temporary file, since we can't write to a read-only file (usually, its not checked out editable from the depot)
+    // This also enables us to run the test many times in a sequence, without changing the input data.
+    QString tmpTSFile = generatedtsfile + QLatin1String(".tmp");
+    QFile::copy(generatedtsfile, tmpTSFile);
+
+    m_lupdate.updateProFile(inputpro);
+    
+    QFile file1(tmpTSFile);
+    bool ok = file1.open(QIODevice::ReadOnly);
+    QVERIFY(ok);
+    QByteArray data1 = file1.readAll();
+    QString str1(data1);
+    file1.close();
+    
+    QFile file2(expectedtsfile);
+    ok = file2.open(QIODevice::ReadOnly);
+    QVERIFY(ok);
+    QByteArray data2 = file2.readAll();
+    QString str2(data2);
+    file2.close();
+
+    QCOMPARE(str1, str2);
+
+
+}
+
+
+QTEST_MAIN(tst_lupdate)
+#include "tst_lupdate.moc"
