b1f31df7b9c2200733bf0f93ed11c4b8d7cdb503
jasaethe <jasaethe@mike-macondo>
2005-11-22 14:45:33 +0100

    Fixes:    Putting in the encoding attribute in the xml prolog again. For best possible compatibility, it should always be utf-8. Task:     Added more tests to mergeui.
    [git-p4: depot-paths = "//depot/qt/main/": change = 200450]

--- tools/linguist/lupdate/tests/testdata/mergeui/project_no.ts
+++ tools/linguist/lupdate/tests/testdata/mergeui/project_no.ts
@@ -4,12 +4,18 @@
     <name>FindDialog</name>
     <message>
         <location filename="project.ui" line="27"/>
-        <source>Qt Assistant - Finn text</source>
-        <translation></translation>
+        <source>Qt Assistant - Find text</source>
+        <!--should be changed to unfinished, since we are changing the sourcetext in the ui file-->
+        <translation>Qt Assistant - Finn tekst</translation>
     </message>
     <message>
         <location filename="project.ui" line="30"/>
-        <source>Finn tekst</source>
+        <source>300px</source>
+        <translation>300px</translation>
+    </message>
+    <message>
+        <location filename="project.ui" line="33"/>
+        <source>400px</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
