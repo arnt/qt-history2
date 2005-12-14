04c09258e1111e75e294bab960a6d7ad46bd4338
Vivi Karlsen <vkarlsen@trolltech.com>
2005-12-14 13:05:51 +0100

    p4i integration Integrate 202658 from 4.1 to main: doc: improved the Brush Styles snippet's code.
    [git-p4: depot-paths = "//depot/qt/main/": change = 202659]

--- doc/src/snippets/brushstyles/stylewidget.cpp
+++ doc/src/snippets/brushstyles/stylewidget.cpp
@@ -5,156 +5,95 @@
 StyleWidget::StyleWidget(QWidget *parent)
         : QWidget(parent)
 {
-    path.addRect(0, 0, 130, 60);
-    pattern.addRect(0, 0, 410, 60);
+    solid = new RenderArea(new QBrush(Qt::SolidPattern));
+    dense1 = new RenderArea(new QBrush(Qt::Dense1Pattern));
+    dense2 = new RenderArea(new QBrush(Qt::Dense2Pattern));
+    dense3 = new RenderArea(new QBrush(Qt::Dense3Pattern));
+    dense4 = new RenderArea(new QBrush(Qt::Dense4Pattern));
+    dense5 = new RenderArea(new QBrush(Qt::Dense5Pattern));
+    dense6 = new RenderArea(new QBrush(Qt::Dense6Pattern));
+    dense7 = new RenderArea(new QBrush(Qt::Dense7Pattern));
+    no = new RenderArea(new QBrush(Qt::NoBrush));
+    hor = new RenderArea(new QBrush(Qt::HorPattern));
+    ver = new RenderArea(new QBrush(Qt::VerPattern));
+    cross = new RenderArea(new QBrush(Qt::CrossPattern));
+    bdiag = new RenderArea(new QBrush(Qt::BDiagPattern));
+    fdiag = new RenderArea(new QBrush(Qt::FDiagPattern));
+    diagCross = new RenderArea(new QBrush(Qt::DiagCrossPattern));
+    linear = new RenderArea(new QBrush(Qt::LinearGradientPattern));
+    radial = new RenderArea(new QBrush(Qt::RadialGradientPattern));
+    conical = new RenderArea(new QBrush(Qt::ConicalGradientPattern));
+    texture = new RenderArea(new QBrush(QPixmap("qt-logo.png")));
 
+    solidLabel = new QLabel("Qt::SolidPattern");
+    dense1Label = new QLabel("Qt::Dense1Pattern");
+    dense2Label = new QLabel("Qt::Dense2Pattern");
+    dense3Label = new QLabel("Qt::Dense3Pattern");
+    dense4Label = new QLabel("Qt::Dense4Pattern");
+    dense5Label = new QLabel("Qt::Dense5Pattern");
+    dense6Label = new QLabel("Qt::Dense6Pattern");
+    dense7Label = new QLabel("Qt::Dense7Pattern");
+    noLabel = new QLabel("Qt::NoPattern");
+    horLabel = new QLabel("Qt::HorPattern");
+    verLabel = new QLabel("Qt::VerPattern");
+    crossLabel = new QLabel("Qt::CrossPattern");
+    bdiagLabel = new QLabel("Qt::BDiagPattern");
+    fdiagLabel = new QLabel("Qt::FDiagPattern");
+    diagCrossLabel = new QLabel("Qt::DiagCrossPattern");
+    linearLabel = new QLabel("Qt::LinearGradientPattern");
+    radialLabel = new QLabel("Qt::RadialGradientPattern");
+    conicalLabel = new QLabel("Qt::ConicalGradientPattern");
+    textureLabel = new QLabel("Qt::TexturePattern");
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(solid, 0, 0);
+    layout->addWidget(dense1, 0, 1);
+    layout->addWidget(dense2, 0, 2);
+    layout->addWidget(solidLabel, 1, 0);
+    layout->addWidget(dense1Label, 1, 1);
+    layout->addWidget(dense2Label, 1, 2);
+
+    layout->addWidget(dense3, 2, 0 );
+    layout->addWidget(dense4, 2, 1);
+    layout->addWidget(dense5, 2, 2);
+    layout->addWidget(dense3Label, 3, 0);
+    layout->addWidget(dense4Label, 3, 1);
+    layout->addWidget(dense5Label, 3, 2);
+
+    layout->addWidget(dense6, 4, 0);
+    layout->addWidget(dense7, 4, 1);
+    layout->addWidget(no, 4, 2);
+    layout->addWidget(dense6Label, 5, 0);
+    layout->addWidget(dense7Label, 5, 1);
+    layout->addWidget(noLabel, 5, 2);
+
+    layout->addWidget(hor, 6, 0);
+    layout->addWidget(ver, 6, 1);
+    layout->addWidget(cross, 6, 2);
+    layout->addWidget(horLabel, 7, 0);
+    layout->addWidget(verLabel, 7, 1);
+    layout->addWidget(crossLabel, 7, 2);
+
+    layout->addWidget(bdiag, 8, 0);
+    layout->addWidget(fdiag, 8, 1);
+    layout->addWidget(diagCross, 8, 2);
+    layout->addWidget(bdiagLabel, 9, 0);
+    layout->addWidget(fdiagLabel, 9, 1);
+    layout->addWidget(diagCrossLabel, 9, 2);
+
+    layout->addWidget(linear, 10, 0);
+    layout->addWidget(radial, 10, 1);
+    layout->addWidget(conical, 10, 2);
+    layout->addWidget(linearLabel, 11, 0);
+    layout->addWidget(radialLabel, 11, 1);
+    layout->addWidget(conicalLabel, 11, 2);
+
+    layout->addWidget(texture, 12, 0, 1, 3);
+    layout->addWidget(textureLabel, 13, 0, 1, 3);
+
+    setLayout(layout);
     setWindowTitle(tr("Brush Styles"));
     resize(430, 605);
 }
 
-void StyleWidget::paintEvent(QPaintEvent *event)
-{
-    QPainter painter(this);
-    QFont font = painter.font();
-    font.setPointSize(9);
-    painter.setFont(font);
-    painter.setPen(Qt::NoPen);
-    painter.setRenderHint(QPainter::Antialiasing);
 
-    painter.translate(10, 10);
-    painter.save();
-    drawShape(&painter, QBrush(Qt::SolidPattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::Dense1Pattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::Dense2Pattern));
-
-    painter.restore();
-    painter.translate(0, 75);
-    painter.save();
-
-    drawText(&painter,  "Qt::SolidPattern", "Qt::Dense1Pattern",
-                    "Qt::Dense2Pattern");
-
-    painter.restore();
-    painter.translate(0, 10);
-    painter.save();
-
-    drawShape(&painter, QBrush(Qt::Dense3Pattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::Dense4Pattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::Dense5Pattern));
-
-    painter.restore();
-    painter.translate(0, 75);
-    painter.save();
-
-    drawText(&painter,  "Qt::Dense3Pattern", "Qt::Dense4Pattern",
-                    "Qt::Dense5Pattern");
-
-    painter.restore();
-    painter.translate(0, 10);
-    painter.save();
-
-    drawShape(&painter, QBrush(Qt::Dense6Pattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::Dense7Pattern));
-    painter.translate(130, 0);
-    drawShape(&painter, QBrush(Qt::NoBrush));
-
-    painter.restore();
-    painter.translate(0, 75);
-    painter.save();
-
-    drawText(&painter,  "Qt::Dense6Pattern", "Qt::Dense7Pattern",
-                    "Qt::NoBrush");
-
-    painter.restore();
-    painter.translate(0, 10);
-    painter.save();
-
-    drawShape(&painter, QBrush(Qt::HorPattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::VerPattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::CrossPattern));
-
-    painter.restore();
-    painter.translate(0, 75);
-    painter.save();
-
-    drawText(&painter, "Qt::HorPattern", "Qt::VerPattern",
-                   "Qt::CrossPattern");
-
-    painter.restore();
-    painter.translate(0, 10);
-    painter.save();
-
-    drawShape(&painter, QBrush(Qt::BDiagPattern));
-    painter.drawPath(path);
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::FDiagPattern));
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(Qt::DiagCrossPattern));
-
-    painter.restore();
-    painter.translate(0, 75);
-    painter.save();
-
-    drawText(&painter, "Qt::BDiagPattern", "Qt::FDiagPattern",
-                    "Qt::DiagCrossPattern");
-
-    painter.restore();
-    painter.translate(0, 10);
-    painter.save();
-
-    drawShape(&painter, QBrush(QLinearGradient(0, 0, 120, 120)));
-    painter.translate(140, 0);
-
-    QRadialGradient radial(65, 30, 65, 65, 30);
-    radial.setColorAt(0.0, Qt::white);
-    radial.setColorAt(1.0, Qt::black);
-    drawShape(&painter, QBrush(radial));
-
-    painter.translate(140, 0);
-    drawShape(&painter, QBrush(QConicalGradient(65, 30, 90)));
-
-    painter.restore();
-    painter.translate(0, 75);
-    painter.save();
-
-    drawText(&painter, "Qt::LinearGradientPattern",
-                    "Qt::ConicalGradientPattern", "Qt::RadialGradientPattern");
-
-    painter.restore();
-    painter.translate(0, 10);
-    painter.save();
-
-    drawShape(&painter, QBrush(QPixmap("qt-logo.png")));
-
-    painter.restore();
-    painter.translate(0, 75);
-
-    drawText(&painter, "Qt::TexturePattern");
-}
-
-void StyleWidget::drawShape(QPainter *painter, const QBrush &brush)
-{
-    painter->setBrush(brush);
-    if (brush.style() == Qt::TexturePattern )
-        painter->drawPath(pattern);
-    else
-        painter->drawPath(path);
-}
-
-void StyleWidget::drawText(QPainter *painter, const QString &first,
-                                         const QString &second,
-                                         const QString &third)
-{
-    painter->setPen(Qt::SolidLine);
-    painter->drawText(0, 0, first);
-    painter->drawText(140, 0, second);
-    painter->drawText(280, 0, third);
-    painter->setPen(Qt::NoPen);
-}
--- doc/src/snippets/brushstyles/stylewidget.h
+++ doc/src/snippets/brushstyles/stylewidget.h
@@ -3,6 +3,9 @@
 
 #include <QWidget>
 #include <QPainterPath>
+#include "renderarea.h"
+
+class QLabel;
 
 class StyleWidget : public QWidget
 {
@@ -11,15 +14,45 @@ class StyleWidget : public QWidget
 public:
     StyleWidget(QWidget *parent = 0);
 
-protected:
-    void paintEvent(QPaintEvent *event);
-
 private:
-    void drawShape(QPainter *painter, const QBrush &brush);
-    void drawText(QPainter *painter, const QString &first,
-                           const QString &second = 0, const QString &third = 0);
+    RenderArea *solid;
+    RenderArea *dense1;
+    RenderArea *dense2;
+    RenderArea *dense3;
+    RenderArea *dense4;
+    RenderArea *dense5;
+    RenderArea *dense6;
+    RenderArea *dense7;
+    RenderArea *no;
+    RenderArea *hor;
+    RenderArea *ver;
+    RenderArea *cross;
+    RenderArea *bdiag;
+    RenderArea *fdiag;
+    RenderArea *diagCross;
+    RenderArea *linear;
+    RenderArea *radial;
+    RenderArea *conical;
+    RenderArea *texture;
 
-    QPainterPath path;
-    QPainterPath pattern;
+    QLabel *solidLabel;
+    QLabel *dense1Label;
+    QLabel *dense2Label;
+    QLabel *dense3Label;
+    QLabel *dense4Label;
+    QLabel *dense5Label;
+    QLabel *dense6Label;
+    QLabel *dense7Label;
+    QLabel *noLabel;
+    QLabel *horLabel;
+    QLabel *verLabel;
+    QLabel *crossLabel;
+    QLabel *bdiagLabel;
+    QLabel *fdiagLabel;
+    QLabel *diagCrossLabel;
+    QLabel *linearLabel;
+    QLabel *radialLabel;
+    QLabel *conicalLabel;
+    QLabel *textureLabel;
 };
 #endif
