2507a3b94190b019a8cf1e88ade386a4edd711aa
Dimitri Papadopoulos <dimitri@trolltech.com>
2006-01-02 18:37:29 +0100

    p4i integration Integrate 204052 from 4.1 to main: Fixes:    Atomic operations for Alpha architecture and Tru64. RevBy:    bhughes Details:  Functions were missing for the Alpha architecture:           	q_atomic_test_and_set_acquire_int           	q_atomic_test_and_set_release_int:           This hasn't been run-time tested yet.
    [git-p4: depot-paths = "//depot/qt/main/": change = 204053]

--- src/corelib/arch/alpha/qatomic.s
+++ src/corelib/arch/alpha/qatomic.s
@@ -23,6 +23,44 @@ q_atomic_test_and_set_int:
 	.end q_atomic_test_and_set_int
 	.align 2
 	.align 4
+	.globl q_atomic_test_and_set_acquire_int
+	.ent q_atomic_test_and_set_acquire_int
+q_atomic_test_and_set_acquire_int:
+	.frame $30,0,$26,0
+	.prologue 0
+1:	ldl_l $0,0($16)
+	cmpeq $0,$17,$0
+	beq   $0,3f
+	mov   $18,$0
+	stl_c $0,0($16)
+	beq   $0,2f
+	br    3f
+2:	br    1b
+3:	mb
+	addl  $31,$0,$0
+	ret   $31,($26),1
+	.end q_atomic_test_and_set_acquire_int
+	.align 2
+	.align 4
+	.globl q_atomic_test_and_set_release_int
+	.ent q_atomic_test_and_set_release_int
+q_atomic_test_and_set_release_int:
+	.frame $30,0,$26,0
+	.prologue 0
+	mb
+1:	ldl_l $0,0($16)
+	cmpeq $0,$17,$0
+	beq   $0,3f
+	mov   $18,$0
+	stl_c $0,0($16)
+	beq   $0,2f
+	br    3f
+2:	br    1b
+3:	addl  $31,$0,$0
+	ret   $31,($26),1
+	.end q_atomic_test_and_set_release_int
+	.align 2
+	.align 4
 	.globl q_atomic_test_and_set_ptr
 	.ent q_atomic_test_and_set_ptr
 q_atomic_test_and_set_ptr:
