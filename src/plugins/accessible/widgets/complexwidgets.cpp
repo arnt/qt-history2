dbbeca404b84c3b894af892e2bd311e8f484656c
msorvig <msorvig@dole>
2006-01-04 16:29:04 +0100

    p4i integration Integrate 204281 from 4.1 to main:    sectionClicked(int, Qt::ButtonState) -> sectionClicked(int)    The signature for sectionClicked was changed and the accessibility plugin was not.
    [git-p4: depot-paths = "//depot/qt/main/": change = 204282]

--- src/plugins/accessible/widgets/complexwidgets.cpp
+++ src/plugins/accessible/widgets/complexwidgets.cpp
@@ -47,7 +47,7 @@ QAccessibleHeader::QAccessibleHeader(QWidget *w)
 : QAccessibleWidget(w)
 {
     Q_ASSERT(header());
-    addControllingSignal("sectionClicked(int, Qt::ButtonState)");
+    addControllingSignal("sectionClicked(int)");
 }
 
 /*! Returns the QHeaderView. */
