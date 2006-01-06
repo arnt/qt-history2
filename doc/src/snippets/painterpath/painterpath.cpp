a56a9e4292c0099d9d3fd8c2e63702e35cd22e41
Vivi Karlsen <vkarlsen@trolltech.com>
2006-01-06 14:55:39 +0100

    p4i integration Integrate 204568 from 4.1 to main: doc: minor cosmetic changes to QPainterPath snippet
    [git-p4: depot-paths = "//depot/qt/main/": change = 204569]

--- doc/src/snippets/painterpath/painterpath.cpp
+++ doc/src/snippets/painterpath/painterpath.cpp
@@ -7,18 +7,7 @@ int main(int argc, char **argv)
     QApplication app(argc, argv);
 
     QImage image(100, 100, QImage::Format_RGB32);
-    image.fill(0xffffffff);
 
-    QPainter painter(&image);
-    painter.fillRect(0, 0, 100, 100, Qt::white);
-
-    painter.save();
-    painter.translate(0.5, 0.5);
-    painter.setPen(QPen(QColor(103, 103, 150), 3, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin));
-    painter.setBrush(QColor(223, 223, 255));
-    painter.setRenderHint(QPainter::Antialiasing);
-
-    // PATH
     QPainterPath path;
     path.addRect(20, 20, 60, 60);
 
@@ -26,11 +15,18 @@ int main(int argc, char **argv)
     path.cubicTo(99, 0,  50, 50,  99, 99);
     path.cubicTo(0, 99,  50, 50,  0, 0);
 
+    QPainter painter(&image);
+    painter.fillRect(0, 0, 100, 100, Qt::white);
+
+    painter.save();
+    painter.translate(0.5, 0.5);
+    painter.setPen(QPen(QColor(79, 106, 25), 3, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin));
+    painter.setBrush(QColor(122, 163, 39));
+    painter.setRenderHint(QPainter::Antialiasing);
+
     painter.drawPath(path);
 
     painter.restore();
-    painter.setPen(Qt::black);
-    painter.drawRect(0, 0, 99, 99);
     painter.end();
 
     QLabel lab;
