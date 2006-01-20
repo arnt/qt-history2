50e47345a5b909c02aa576c843ffabb1fa338121
hwall <hwall@hwall-extra>
2006-01-20 14:02:49 +0100

    p4i integration Integrate 206663 from 4.1 to main: Fixes:    tslib plugin based on internal driver Task:     99067 RevBy:    paul
    [git-p4: depot-paths = "//depot/qt/main/": change = 206664]

---
+++ src/plugins/mousedrivers/tslib/main.cpp
@@ -0,0 +1,52 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <qmousedriverplugin_qws.h>
+#include <qmousetslib_qws.h>
+
+class TslibMouseDriver : public QMouseDriverPlugin
+{
+public:
+    TslibMouseDriver();
+
+    QStringList keys() const;
+    QWSMouseHandler* create(const QString &name);
+    QWSMouseHandler* create(const QString &driver, const QString &device);
+};
+
+TslibMouseDriver::TslibMouseDriver()
+    : QMouseDriverPlugin()
+{
+}
+
+QStringList TslibMouseDriver::keys() const
+{
+    return (QStringList() << "tslib");
+}
+
+QWSMouseHandler* TslibMouseDriver::create(const QString &name)
+{
+    return create(name, QString());
+}
+
+QWSMouseHandler* TslibMouseDriver::create(const QString &driver,
+                                          const QString &device)
+{
+    if (driver.toLower() != "tslib")
+        return 0;
+
+    return new QWSTslibMouseHandler(driver, device);
+}
+
+Q_EXPORT_STATIC_PLUGIN(TslibMouseDriver)
+Q_EXPORT_PLUGIN2(qwstslibmousehandler, TslibMouseDriver)
