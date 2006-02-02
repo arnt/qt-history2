d25540753b9639008b8e6f479bada0d3258443fc
janerik <janerik@janerik-cocktail>
2006-02-02 15:19:00 +0100

    p4i integration Integrate 208264 from 4.1 to main: Fixes:    QSqlQueryModel::setQuery() causes an assert in QAbstractItemModel::beginInsertRows() Task:     101953 RevBy:    Harald AutoTest: none Details:  QAbstractItemModel::beginInsertRows() should not be called with -1 as the "last" argument.
    [git-p4: depot-paths = "//depot/qt/main/": change = 208265]

--- src/sql/models/qsqlquerymodel.cpp
+++ src/sql/models/qsqlquerymodel.cpp
@@ -324,7 +324,7 @@ void QSqlQueryModel::setQuery(const QSqlQuery &query)
     }
     QModelIndex newBottom;
     if (hasQuerySize) {
-        beginInsertRows(QModelIndex(), 0, newBottom.row());
+        beginInsertRows(QModelIndex(), 0, qMax(0, newBottom.row()));
         newBottom = createIndex(d->query.size() - 1, d->rec.count() - 1);
         d->bottom = createIndex(d->query.size() - 1, columnsChanged ? 0 : d->rec.count() - 1);
         d->atEnd = true;
