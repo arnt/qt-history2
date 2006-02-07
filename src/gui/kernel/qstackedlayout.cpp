89436d2bda4f585de4c942d14b79a294f7914c3c
Dimitri Papadopoulos <dimitri@trolltech.com>
2006-02-07 22:34:16 +0100

    p4i integration Integrate 208733 from 4.1 to main: Fixes:    removeWidget should only change focus if you remove the widget that has focus. Task:     100368 RevBy:    simon AutoTest: 208730 Details:  Previously it could steal focus away from widgets that was not part of the QStackedLayout
    [git-p4: depot-paths = "//depot/qt/main/": change = 208849]

--- src/gui/kernel/qstackedlayout.cpp
+++ src/gui/kernel/qstackedlayout.cpp
@@ -264,7 +264,7 @@ void QStackedLayout::setCurrentIndex(int index)
 
     if (parent) {
         QWidget * fw = parent->window()->focusWidget();
-        if (fw && (!prev || prev->isAncestorOf(fw))) { // focus was on old page
+        if (fw && (prev && prev->isAncestorOf(fw))) { // focus was on old page
             // look for the best focus widget we can find
             if (QWidget *nfw = next->focusWidget())
                 nfw->setFocus();
