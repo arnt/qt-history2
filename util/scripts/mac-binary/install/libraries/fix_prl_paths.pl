8d886690322a2973f671b133ee5e431cb1b85ee0
Trenton Schulz <twschulz@twschulz-rain>
2006-02-09 15:03:22 +0100

    p4i integration Integrate 209160 from 4.1 to main: Fixes:    Fix the mysql path Details:  Strange that a universal mysql does this, but "oh well."
    [git-p4: depot-paths = "//depot/qt/main/": change = 209161]

--- util/scripts/mac-binary/install/libraries/fix_prl_paths.pl
+++ util/scripts/mac-binary/install/libraries/fix_prl_paths.pl
@@ -7,7 +7,7 @@ open(NEWFILE, ">" . "$ARGV[1]") || die "Couldn't open output file: $ARGV[1]";
 
 while (my $line = <OLDFILE>) {
     $line =~ s,-L/(\w+(\w|\s|/)*)/pgsql/lib,,g;
-    $line =~ s,-L/(\w+(\w|\s|/)*)/mysql/lib,,g;
+    $line =~ s,-L/(\w+(\w|\s|/)*)/mysql/lib/mysql,,g;
     $line =~ s,-F/private/tmp/qt-stuff/source/qt-mac-\w+-src-4\.\d\.\d(-rc\d)?/lib,,g;
     $line =~ s,-L/private/tmp/qt-stuff/source/qt-mac-\w+-src-4\.\d\.\d(-rc\d)?/lib,,g;
     print NEWFILE $line;
