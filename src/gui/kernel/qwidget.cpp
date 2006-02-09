87a5ad5412753526b774238d467474510a1a2579
Martin Jones <mjones@trolltech.com>
2006-02-09 07:57:28 +0100

    p4i integration Integrate 209051 from 4.1 to main: Fixes:    Keypad Navigation: clearing edit focus was forcing focus. RevBy:    Mark George Details:  Keypad Navigation: clearing edit focus was forcing focus.
    [git-p4: depot-paths = "//depot/qt/main/": change = 209052]

--- src/gui/kernel/qwidget.cpp
+++ src/gui/kernel/qwidget.cpp
@@ -237,11 +237,11 @@ void QWidget::setEditFocus(bool on)
     while (f->d_func()->extra && f->d_func()->extra->focus_proxy)
         f = f->d_func()->extra->focus_proxy;
 
-    if (!f->hasFocus())
-        f->setFocus();
-
     if (QWidgetPrivate::editingWidget && QWidgetPrivate::editingWidget != f)
         QWidgetPrivate::editingWidget->setEditFocus(false);
+
+    if (on && !f->hasFocus())
+        f->setFocus();
 
     if ((!on && !QWidgetPrivate::editingWidget)
         || (on && QWidgetPrivate::editingWidget == f)) {
