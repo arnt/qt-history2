31491fd71b54f8dbe36f7f9a72d644b5a572870f
harald <harald@harald-morpork>
2006-02-22 16:49:54 +0100

    p4i integration Integrate 210943 from 4.1 to main: Fixes:    don't show the crash-error box on windows Task:     104324 RevBy:    Stian Details:            before, it was only hidden when using msvc
    [git-p4: depot-paths = "//depot/qt/main/": change = 210944]

--- tools/qtestlib/src/qtestcase.cpp
+++ tools/qtestlib/src/qtestcase.cpp
@@ -27,7 +27,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifdef Q_OS_WIN32
+#ifdef Q_OS_WIN
 #include <windows.h> // for Sleep
 #endif
 #ifdef Q_OS_UNIX
@@ -998,7 +998,7 @@ int QTest::qExec(QObject *testObject, int argc, char **argv)
     try {
 #endif
 
-#if defined(Q_CC_MSVC)
+#if defined(Q_OS_WIN)
     SetErrorMode(SetErrorMode(0) | SEM_NOGPFAULTERRORBOX);
 #endif
 
@@ -1305,7 +1305,7 @@ void QTest::qSleep(int ms)
 {
     QTEST_ASSERT(ms > 0);
 
-#ifdef Q_OS_WIN32
+#ifdef Q_OS_WIN
     Sleep(uint(ms));
 #else
     struct timespec ts = { ms / 1000, (ms % 1000) * 1000 * 1000 };
