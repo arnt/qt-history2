c68d6ccebe5b83c17c7c696f79e395e5344fb5fb
Sverre <sdjonne@trolltech.com>
2006-02-27 11:56:36 +0100

    Fixes:    fixed eol errors... don't use mac newlines!!!
    [git-p4: depot-paths = "//depot/qt/main/": change = 211300]

--- qmake/generators/unix/unixmake.h
+++ qmake/generators/unix/unixmake.h
@@ -9,7 +9,7 @@
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
-****************************************************************************/\
+****************************************************************************/
 
 #ifndef UNIXMAKE_H
 #define UNIXMAKE_H
