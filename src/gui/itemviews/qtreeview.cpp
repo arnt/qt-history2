c971a3195acc1817c268333ba93b3009140090dc
qt <qt@manta>
2006-03-20 18:42:01 +0100

    Username: bmeyer Fixes:    re-intigrate manually 213562
    [git-p4: depot-paths = "//depot/qt/main/": change = 213628]

--- src/gui/itemviews/qtreeview.cpp
+++ src/gui/itemviews/qtreeview.cpp
@@ -1038,9 +1038,14 @@ void QTreeView::drawBranches(QPainter *painter, const QRect &rect,
         const bool children = (expanded // already layed out
                                ? d->viewItems.at(item).total // this also covers the hidden items
                                : d->model->hasChildren(index)); // not layed out yet, so we don't know
+        bool moreSiblings = false;
+        if (d->hiddenIndexes.isEmpty())
+            moreSiblings = (d->model->rowCount(parent) - 1 > index.row());
+        else
+            moreSiblings = ((d->viewItems.size() > item +1) && (d->viewItems.at(item + 1).index.parent() == parent));
+
         opt.state = QStyle::State_Item | extraFlags
-                    | (d->model->rowCount(parent) - 1 > index.row()
-                      ? QStyle::State_Sibling : QStyle::State_None)
+                    | (moreSiblings ? QStyle::State_Sibling : QStyle::State_None)
                     | (children ? QStyle::State_Children : QStyle::State_None)
                     | (expanded ? QStyle::State_Open : QStyle::State_None);
         style()->drawPrimitive(QStyle::PE_IndicatorBranch, &opt, painter, this);
