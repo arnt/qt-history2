0ff19416ca7952b2f519d70b1b5159ea102504cf
Brad Hughes <bhughes@trolltech.com>
2006-03-22 08:30:32 +0100

    p4i integration Integrate 213916 from 4.1 to main: Fixes:    Configure should define -no-fontconfig if fontconfig is too old Task:     99277 RevBy:    simon Details:
           We don't have a minimum version, so we instead look for
           FC_RGBA_UNKNOWN... if it's not found, turn off fontconfig support
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 213917]

--- config.tests/x11/fontconfig/fontconfig.cpp
+++ config.tests/x11/fontconfig/fontconfig.cpp
@@ -2,6 +2,10 @@
 #include FT_FREETYPE_H
 #include <fontconfig/fontconfig.h>
 
+#ifndef FC_RGBA_UNKNOWN
+#  error "This version of fontconfig is tool old, it is missing the FC_RGBA_UNKNOWN define"
+#endif
+
 int main(int, char **)
 {
     FT_Face face;
