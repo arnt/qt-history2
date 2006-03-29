9565fcc4a14d97aa0daf1011d86ea47fc15ebaa3
Girish Ramakrishnan <gramakri@trolltech.com>
2006-03-29 08:52:23 +0100

    p4i integration Integrate 214942 from 4.1-optimized to main: Integrate 214941 from 4.1 to 4.1-optimized: Fixes:    ToolTips on windows dont have drop shadows Task:     108576 RevBy:    marius AutoTest: none Details:  ToolTips on windows were created with no drop shadow and had the same class as a Tool window. Give ToolTips a separate window class of their own. But for their class name ("QToolTip"), they share the same attributes of a popup("QPopup").
    [git-p4: depot-paths = "//depot/qt/main/": change = 214943]

--- src/gui/kernel/qapplication_win.cpp
+++ src/gui/kernel/qapplication_win.cpp
@@ -731,15 +731,15 @@ const QString qt_reg_winclass(Qt::WFlags flags)        // register window class
         style |= CS_OWNDC;
 #endif
         icon  = true;
-    } else if (type == Qt::Tool || type == Qt::ToolTip){
+    } else if (type == Qt::Tool) {
 	cname = "QTool";
 	style = CS_DBLCLKS;
 #ifndef Q_OS_TEMP
 	style |= CS_SAVEBITS;
 #endif
 	icon = false;
-    } else if (type == Qt::Popup) {
-        cname = "QPopup";
+    } else if (type == Qt::Popup || type == Qt::ToolTip) {
+        cname = (type == Qt::ToolTip) ? "QToolTip" : "QPopup";
         style = CS_DBLCLKS;
 #ifndef Q_OS_TEMP
         style |= CS_SAVEBITS;
