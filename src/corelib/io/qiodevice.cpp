54a921a82ffa3bb672a52f9fb2e14b3694cce383
dboddie <dboddie@mikaeld-lurven>
2006-04-04 19:14:47 +0100

    p4i integration Integrate 215939 from 4.1-optimized to main: Integrate 215938 from 4.1 to 4.1-optimized: Doc:  Made the examples in the documentation less specific to avoid confusion. Task: 107210 Details: QFile doesn't have a readyRead() signal.
    [git-p4: depot-paths = "//depot/qt/main/": change = 215940]

--- src/corelib/io/qiodevice.cpp
+++ src/corelib/io/qiodevice.cpp
@@ -271,13 +271,14 @@ QIODevicePrivate::~QIODevicePrivate()
     \sa readyRead()
 */
 
-/*!     \fn QIODevice::readyRead()
+/*!     
+    \fn QIODevice::readyRead()
 
     This signal is emitted once every time new data is available for
     reading from the device. It will only be emitted again once new
     data is available, such as when a new payload of network data has
     arrived on your network socket, or when a new block of data has
-    been appended to your file.
+    been appended to your device.
 
     readyRead() is not emitted recursively; if you reenter the event loop or
     call waitForReadyRead() inside a slot connected to the readyRead() signal,
