90b1a27319f71add2caf118c4a09fbc64d145669
Vivi Karlsen <vkarlsen@trolltech.com>
2006-04-05 08:41:59 +0100

    p4i integration Integrate 215991 from 4.1-optimized to main: Integrate 215990 from 4.1 to 4.1-optimized: doc: reviewed the QSocketNotifier class documentation.
    [git-p4: depot-paths = "//depot/qt/main/": change = 215992]

--- src/corelib/kernel/qsocketnotifier.cpp
+++ src/corelib/kernel/qsocketnotifier.cpp
@@ -26,43 +26,48 @@
 
     \ingroup io
 
-    The QSocketNotifier makes it possible to integrate Qt's event
-    loop with other event loops based on file descriptors. For
-    example, the QtCorba Solution uses it to process CORBA events.
-    File descriptor action is detected in Qt's main event loop
+    The QSocketNotifier makes it possible to integrate Qt's event loop
+    with other event loops based on file descriptors. For example, the
+    \l
+    {http://www.trolltech.com/products/solutions/catalog/4/Utilities/qtcorba/}
+    {QtCorba Solution} uses it to process CORBA events.  File
+    descriptor action is detected in Qt's main event loop
     (QCoreApplication::exec()).
-
-    Once you have opened a device using a low-level (usually
-    platform-specific) API, you can create a socket notifier to
-    monitor the file descriptor. You can then connect the activated()
-    signal to the slot you want to be called whenever an event
-    occurs.
-
-    Although the class is called QSocketNotifier, it is normally used
-    for other types of devices than sockets. QTcpSocket and
-    QUdpSocket provide notification through signals, so there is
-    normally no need to use a QSocketNotifier on them.
-
-    \bold{Note for Windows users:} The socket passed to QSocketNotifier
-    will become non-blocking, even if it was created as a blocking socket.
 
     \target write notifiers
 
+    Once you have opened a device using a low-level (usually
+    platform-specific) API, you can create a socket notifier to
+    monitor the file descriptor. The socket notifier is enabled by
+    default, i.e. it emits the activated() signal whenever a socket
+    event corresponding to its type occurs. Connect the activated()
+    signal to the slot you want to be called when an event
+    corresponding to your socket notifier's type occurs.
+
     There are three types of socket notifiers: read, write, and
-    exception. You must specify one of these in the constructor.
+    exception. The type is described by the \l Type enum, and must be
+    specified when constructing the socket notifier. After
+    construction it can be determined using the type() function. Note
+    that if you need to monitor both reads and writes for the same
+    file descriptor, you must create two socket notifiers. Note also
+    that it is not possible to install two socket notifiers of the
+    same type (\l Read, \l Write, \l Exception) on the same socket.
 
-    The type specifies when the activated() signal is to be emitted:
-    \list 1
-    \o QSocketNotifier::Read - There is data to be read.
-    \o QSocketNotifier::Write - Data can be written.
-    \o QSocketNotifier::Exception - An exception has occurred.
-       We recommend against using this.
-    \endlist
+    The setEnabled() function allows you to disable as well as enable
+    the socket notifier. It is generally advisable to explicitly
+    enable or disable the socket notifier, especially for write
+    notifiers. A disabled notifier ignores socket events (the same
+    effect as not creating the socket notifier). Use the isEnabled()
+    function to determine the notifier's current status.
 
-    \bold{Note:} If you need to monitor both reads and writes for the
-    same file descriptor, you must create two socket notifiers. It is
-    not possible to install two socket notifiers of the same type
-    (\l Read, \l Write, \l Exception) on the same socket.
+    Finally, you can use the socket() function to retrieve the
+    socket identifier.  Although the class is called QSocketNotifier,
+    it is normally used for other types of devices than sockets.
+    QTcpSocket and QUdpSocket provide notification through signals, so
+    there is normally no need to use a QSocketNotifier on them.
+
+    \bold{Note for Windows users:} The socket passed to QSocketNotifier
+    will become non-blocking, even if it was created as a blocking socket.
 
     \sa QFile, QProcess, QTcpSocket, QUdpSocket
 */
@@ -70,12 +75,20 @@
 /*!
     \enum QSocketNotifier::Type
 
-    The socket notifier can be used to inform the application of the
-    following types of event:
+    This enum describes the various types of events that a socket
+    notifier can recognize. The type must be specified when
+    constructing the socket notifier.
 
-    \value Read       There is incoming data.
+    Note that if you need to monitor both reads and writes for the
+    same file descriptor, you must create two socket notifiers. Note
+    also that it is not possible to install two socket notifiers of
+    the same type (Read, Write, Exception) on the same socket.
+
+    \value Read      There is data to be read.
     \value Write      Data can be written.
-    \value Exception  An exception has occurred.
+    \value Exception  An exception has occurred. We recommend against using this.
+
+    \sa QSocketNotifier(), type()
 */
 
 /*!
@@ -116,14 +129,15 @@ QSocketNotifier::QSocketNotifier(int socket, Type type, QObject *parent)
 /*!
     \obsolete
 
-    Constructs a socket notifier called \a name, with the given
-    \a parent. It enables the \a socket, and watches for events of the
-    given \a type.
+    Use the QSocketNotifier() constructor combined with the
+    QObject::setObjectName() function instead.
 
-    It is generally advisable to explicitly enable or disable the
-    socket notifier, especially for write notifiers.
-
-    \sa setEnabled(), isEnabled()
+    \oldcode
+        QSocketNotifier *notifier = new QSocketNotifier(socket, type, parent, name);
+    \newcode
+        QSocketNotifier *notifier = new QSocketNotifier(socket, type, parent);
+        notifier->setObjectName(name);
+    \endcode
 */
 
 QSocketNotifier::QSocketNotifier(int socket, Type type, QObject *parent,
@@ -150,7 +164,7 @@ QSocketNotifier::QSocketNotifier(int socket, Type type, QObject *parent,
 }
 #endif
 /*!
-    Destroys the socket notifier.
+    Destroys this socket notifier.
 */
 
 QSocketNotifier::~QSocketNotifier()
@@ -162,16 +176,10 @@ QSocketNotifier::~QSocketNotifier()
 /*!
     \fn void QSocketNotifier::activated(int socket)
 
-    This signal is emitted under certain conditions specified by the
-    notifier type():
-    \list 1
-    \i QSocketNotifier::Read - There is data to be read (socket read event).
-    \i QSocketNotifier::Write - Data can be written (socket write event).
-    \i QSocketNotifier::Exception - An exception has occurred (socket
-    exception event).
-    \endlist
+    This signal is emitted whenever the socket notifier is enabled and
+    a socket event corresponding to its \l {Type}{type} occurs.
 
-    The \a socket is the socket identifier.
+    The socket identifier is passed in the \a socket parameter.
 
     \sa type(), socket()
 */
@@ -206,16 +214,13 @@ QSocketNotifier::~QSocketNotifier()
     If \a enable is true, the notifier is enabled; otherwise the notifier
     is disabled.
 
-    The notifier is enabled by default.
-
-    If the notifier is enabled, it emits the activated() signal
-    whenever a socket event corresponding to its \l{type()}{type}
-    occurs. If it is disabled, it ignores socket events (the same
-    effect as not creating the socket notifier).
+    The notifier is enabled by default, i.e. it emits the activated()
+    signal whenever a socket event corresponding to its
+    \l{type()}{type} occurs. If it is disabled, it ignores socket
+    events (the same effect as not creating the socket notifier).
 
     Write notifiers should normally be disabled immediately after the
-    activated() signal has been emitted; see discussion of write
-    notifiers in the \l{write notifiers}{class description} above.
+    activated() signal has been emitted
 
     \sa isEnabled(), activated()
 */
