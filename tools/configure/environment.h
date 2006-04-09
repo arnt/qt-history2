25f3904f404baecafaa920ca1d31fe9d77ebefd5
Dimitri Papadopoulos <dimitri@trolltech.com>
2006-04-09 22:36:09 +0100

    manual integration (1st pass) Integrate 216098 from 4.1-optimized to main: p4i integration Integrate 216095 from 4.1 to 4.1-optimized: Fixes:    Move configure.exe sourcecode into tools/configure RevBy:    TrustMe Details:  Since we need to distribute the sourcecode for configure.exe, we'll move it into tools.           This will ensure that we won't by mistake ship the other things in utils/, which is           purely internal stuff.
    [git-p4: depot-paths = "//depot/qt/main/": change = 216525]

---
+++ tools/configure/environment.h
@@ -0,0 +1,38 @@
+#include <qstring.h>
+#include <qt_windows.h>
+
+
+enum Compiler {
+    CC_UNKNOWN = 0,
+    CC_BORLAND = 0x01,
+    CC_MINGW   = 0x02,
+    CC_INTEL   = 0x03,
+    CC_MSVC4   = 0x40,
+    CC_MSVC5   = 0x50,
+    CC_MSVC6   = 0x60,
+    CC_NET2002 = 0x70,
+    CC_NET2003 = 0x71,
+    CC_NET2005 = 0x80
+};
+
+struct CompilerInfo;
+class Environment
+{
+public:
+    static Compiler detectCompiler();
+    static QString detectQMakeSpec();
+    static bool detectExecutable(const QString &executable);
+
+    static int execute(QStringList arguments, const QStringList &additionalEnv, const QStringList &removeEnv);
+    static bool cpdir(const QString &srcDir, const QString &destDir);
+    static bool rmdir(const QString &name);
+
+private:
+    static Compiler detectedCompiler;
+
+    static CompilerInfo *compilerInfo(Compiler compiler);
+    static QString keyPath(const QString &rKey);
+    static QString keyName(const QString &rKey);
+    static QString readRegistryKey(HKEY parentHandle, const QString &rSubkey);
+};
+
---
+++ tools/configure/main.cpp
@@ -0,0 +1,57 @@
+/*
+** Configure tool
+**
+*/
+
+#include "configureapp.h"
+
+int main( int argc, char** argv )
+{
+    Configure app( argc, argv );
+    if (!app.isOk())
+        return 3;
+
+    app.parseCmdLine();
+#if !defined(EVAL)
+    app.validateArgs();
+#endif
+    if( app.displayHelp() )
+	return 1;
+
+    // Read license now, and exit if it doesn't pass.
+    // This lets the user see the command-line options of configure
+    // without having to load and parse the license file.
+    app.readLicense();
+    if (!app.isOk())
+        return 3;
+
+    // Auto-detect modules and settings.
+    app.autoDetection();
+
+    // After reading all command-line arguments, and doing all the
+    // auto-detection, it's time to do some last minute validation.
+    // If the validation fails, we cannot continue.
+    if (!app.verifyConfiguration())
+        return 3;
+
+    app.generateOutputVars();
+
+#if !defined(EVAL)
+    if( !app.isDone() )
+	app.generateCachefile();
+    if( !app.isDone() )
+	app.generateConfigfiles();
+    if( !app.isDone() )
+	app.displayConfig();
+    if( !app.isDone() )
+	app.buildQmake();
+    if( !app.isOk() )
+	return 2;
+#endif
+    if( !app.isDone() )
+	app.generateMakefiles();
+    if( !app.isDone() )
+	app.showSummary();
+
+    return 0;
+}
