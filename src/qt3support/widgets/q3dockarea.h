d10abf8304ef0d0414dece14d950b4fe272b4e81
harald <harald@harald-morpork>
2006-04-11 10:28:22 +0100

    p4i integration Integrate 216717 from 4.1-optimized to main: Integrate 216714 from 4.1 to 4.1-optimized: Fixes:    uninitialized memory read RevBy:    Simon, Girish
    [git-p4: depot-paths = "//depot/qt/main/": change = 216718]

--- src/qt3support/widgets/q3dockarea.h
+++ src/qt3support/widgets/q3dockarea.h
@@ -40,7 +40,7 @@ class Q_COMPAT_EXPORT Q3DockAreaLayout : public QLayout
 
 public:
     Q3DockAreaLayout(QWidget* parent, Qt::Orientation o, QList<Q3DockWindow *> *wl, int space = -1, int margin = -1, const char *name = 0)
-        : QLayout(parent), orient(o), dockWindows(wl), parentWidget(parent)
+        : QLayout(parent), orient(o), dockWindows(wl), parentWidget(parent), dirty(true)
     {
         if (space != -1)
             setSpacing(space);
