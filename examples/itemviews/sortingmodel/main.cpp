1974e94471ba25a0fa3983c05d1eb195913c1bdf
Dimitri Papadopoulos <dimitri@trolltech.com>
2006-04-24 07:12:50 +0100

    manual integration (1st pass) Integrate 217238 from 4.1-optimized to main: Integrate 217237 from 4.1 to 4.1-optimized: Fixes:    Sort app doesn't sort in initial window Task:     110975 RevBy:    MBM
    [git-p4: depot-paths = "//depot/qt/main/": change = 217822]

---
+++ examples/itemviews/sortingmodel/main.cpp
@@ -0,0 +1,45 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QtGui>
+
+#include "treemodel.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QFile file(":/default.txt");
+    file.open(QIODevice::ReadOnly);
+    TreeModel model(file.readAll());
+    file.close();
+
+    QTreeView unsortedView;
+    unsortedView.setModel(&model);
+    unsortedView.setWindowTitle(QObject::tr("Unsorted Data"));
+    unsortedView.show();
+
+    QSortFilterProxyModel sortingModel;
+    sortingModel.setSourceModel(&model);
+    sortingModel.sort(1);
+
+    QTreeView sortedView;
+    sortedView.setModel(&sortingModel);
+    sortedView.setWindowTitle(QObject::tr("Sorted Data"));
+    sortedView.header()->setSortIndicator(1, Qt::AscendingOrder);
+    sortedView.header()->setSortIndicatorShown(true);
+    sortedView.header()->setClickable(true);
+    sortedView.show();
+
+    return app.exec();
+}
