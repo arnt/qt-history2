075dbf1c3c88ad057e3b09f19b1800ae7ceeabc6
dboddie <dboddie@mikaeld-lurven>
2006-04-25 13:32:28 +0100

    Doc: Some changes to Brad's original documentation.
    [git-p4: depot-paths = "//depot/qt/main/": change = 218110]

--- src/corelib/io/qfilesystemwatcher.cpp
+++ src/corelib/io/qfilesystemwatcher.cpp
@@ -193,34 +193,36 @@ void QFileSystemWatcherPrivate::_q_directoryChanged(const QString &path, bool re
 
 
 
-/*! \class QFileSystemWatcher
+/*! 
+    \class QFileSystemWatcher
     \brief The QFileSystemWatcher class provides an interface for monitoring files and directories for modications.
     \ingroup io
+    \since 4.2
     \reentrant
 
     QFileSystemWatcher monitors the file system for changes to files
-    and directories.
+    and directories by watching a list of specified paths.
 
-    To watch a particlar file or directory, call addPath(). Multiple
-    paths can be added using the addPaths() function. You can later
-    remove these paths using removePath() and removePaths().
+    Call addPath() to watch a particular file or directory. Multiple
+    paths can be added using the addPaths() function. Existing paths can
+    be removed by using the removePath() and removePaths() functions.
 
     QFileSystemWatcher examines each path added to it. Files that have
     been added to the QFileSystemWatcher can be accessed using the
-    files() function, and directories using the directories()
-    function.
+    files() function, and directories using the directories() function.
 
     The fileChanged() signal is emitted when a file has been modified
-    or removed from the disk. Similarly, the directoryChanged() signal
-    is emitted when a directory has been modified or removed from
-    disk. Note that QFileSystemWatcher stops monitoring files and
-    directories once they have been removed from disk.
+    or removed from disk. Similarly, the directoryChanged() signal
+    is emitted when a directory is modified or removed. Note that
+    QFileSystemWatcher stops monitoring files and directories once they
+    have been removed from disk.
+
+    \sa QFile, QDir
 */
 
 
 /*!
-    Constructs a new file system watcher object with the given \a
-    parent.
+    Constructs a new file system watcher object with the given \a parent.
 */
 QFileSystemWatcher::QFileSystemWatcher(QObject *parent)
     : QObject(*new QFileSystemWatcherPrivate, parent)
@@ -229,8 +231,8 @@ QFileSystemWatcher::QFileSystemWatcher(QObject *parent)
 }
 
 /*!
-    Constructs a new file system watcher object with the given \a
-    parent which monitors \a paths.
+    Constructs a new file system watcher object with the given \a parent
+    which monitors the specified \a paths list.
 */
 QFileSystemWatcher::QFileSystemWatcher(const QStringList &paths, QObject *parent)
     : QObject(*new QFileSystemWatcherPrivate, parent)
@@ -240,7 +242,7 @@ QFileSystemWatcher::QFileSystemWatcher(const QStringList &paths, QObject *parent
 }
 
 /*!
-    Destroyed the file system watcher object.
+    Destroys the file system watcher.
 */
 QFileSystemWatcher::~QFileSystemWatcher()
 {
@@ -260,13 +262,16 @@ QFileSystemWatcher::~QFileSystemWatcher()
 }
 
 /*!
-    Adds \a path to the file system watcher if \a path exists. If \a
-    path does not exist, this function does nothing.
+    Adds \a path to the file system watcher if \a path exists. The path is
+    not added if it does not exist, or if it is already being monitored by
+    the file system watcher.
 
-    If \a path specifies a directory, the diretoryChanged() signal
-    will be emitted when \a path is modified or removed from the disk;
+    If \a path specifies a directory, the directoryChanged() signal
+    will be emitted when \a path is modified or removed from disk;
     otherwise the fileChanged() signal is emitted when \a path is
-    modified or removed from disk.
+    modified or removed.
+
+    \sa addPaths(), removePath()
 */
 void QFileSystemWatcher::addPath(const QString &path)
 {
@@ -274,13 +279,16 @@ void QFileSystemWatcher::addPath(const QString &path)
 }
 
 /*!
-    Adds each path in \a paths to the file system watcher. If a path
-    does not exist, This function ignores paths that do not exist.
+    Adds each path in \a paths to the file system watcher. Paths are not
+    added if they not exist, or if they are already being monitored by the
+    file system watcher.
 
-    If a path specifies a directory, the diretoryChanged() signal will
-    be emitted when the path is modified or removed from the disk;
-    otherwise the fileChanged() signal is emitted when the path is
-    modified or removed from disk.
+    If a path specifies a directory, the directoryChanged() signal will
+    be emitted when the path is modified or removed from disk; otherwise
+    the fileChanged() signal is emitted when the path is modified or
+    removed.
+
+    \sa addPath(), removePaths()
 */
 void QFileSystemWatcher::addPaths(const QStringList &paths)
 {
@@ -318,7 +326,9 @@ void QFileSystemWatcher::addPaths(const QStringList &paths)
 }
 
 /*!
-    Removes \a path from the file system watcher.
+    Removes the specified \a path from the file system watcher.
+
+    \sa removePaths(), addPath()
 */
 void QFileSystemWatcher::removePath(const QString &path)
 {
@@ -326,7 +336,9 @@ void QFileSystemWatcher::removePath(const QString &path)
 }
 
 /*!
-    Removes \a paths from the file system watcher.
+    Removes the specified \a paths from the file system watcher.
+
+    \sa removePath(), addPaths()
 */
 void QFileSystemWatcher::removePaths(const QStringList &paths)
 {
@@ -339,17 +351,21 @@ void QFileSystemWatcher::removePaths(const QStringList &paths)
 }
 
 /*!
-    \fn void fileChanged(const QString &path)
+    \fn void QFileSystemWatcher::fileChanged(const QString &path)
 
-    Emitted when the file at \a path is modified or removed from the
-    disk.
+    This signal is emitted when the file at the specified \a path is modified
+    or removed from disk.
+
+    \sa directoryChanged()
 */
 
 /*!
-    \fn void directoryChanged(const QString &path)
+    \fn void QFileSystemWatcher::directoryChanged(const QString &path)
 
-    Emitted when the directory at \a path is modified or removed from
-    the disk.
+    This signal is emitted when the directory at the specified \a path is
+    modified or removed from disk.
+
+    \sa fileChanged()
 */
 
 #include "moc_qfilesystemwatcher.cpp"
