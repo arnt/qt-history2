f7a714a66a19b586f0798711a02673e67f99f5e4
harald <harald@harald-morpork>
2006-04-25 13:03:42 +0100

    p4i integration Integrate 218093 from 4.1-optimized to main: Integrate 218092 from 4.1 to 4.1-optimized:        ignore binary files
    [git-p4: depot-paths = "//depot/qt/main/": change = 218094]

--- util/fixnonlatin1/main.cpp
+++ util/fixnonlatin1/main.cpp
@@ -7,7 +7,24 @@ int main(int argc, char *argv[])
     if (argc <= 1)
         qFatal("Usage: %s FILES", argc ? argv[0] : "fixnonlatin1");
     for (int i = 1; i < argc; ++i) {
-        QFile file(QString::fromLocal8Bit(argv[i]));
+
+        QString fileName = QString::fromLocal8Bit(argv[i]);
+        if (fileName.endsWith(QLatin1String(".gif"))
+            || fileName.endsWith(QLatin1String(".jpg"))
+            || fileName.endsWith(QLatin1String(".png"))
+            || fileName.endsWith(QLatin1String(".mng"))
+            || fileName.endsWith(QLatin1String(".ico"))
+            || fileName.endsWith(QLatin1String(".zip"))
+            || fileName.endsWith(QLatin1String(".gz"))
+            || fileName.endsWith(QLatin1String(".qpf"))
+            || fileName.endsWith(QLatin1String(".ttf"))
+            || fileName.endsWith(QLatin1String(".pfb"))
+            || fileName.endsWith(QLatin1String(".exe"))
+            || fileName.endsWith(QLatin1String(".o"))
+            )
+            continue;
+
+        QFile file(fileName);
         if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
             qFatal("Cannot open '%s': %s", argv[i], qPrintable(file.errorString()));
 
@@ -28,8 +45,10 @@ int main(int argc, char *argv[])
             continue;
 
         qDebug("found non-latin1 characters in '%s'", argv[i]);
-        if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
-            qFatal("Cannot open '%s' for writing: %s", argv[i], qPrintable(file.errorString()));
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            qWarning("Cannot open '%s' for writing: %s", argv[i], qPrintable(file.errorString()));
+            continue;
+        }
         if (file.write(ba) < 0)
             qFatal("Error while writing into '%s': %s", argv[i], qPrintable(file.errorString()));
         file.close();
