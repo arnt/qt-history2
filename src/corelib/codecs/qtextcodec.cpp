d2747b21e3624561deeddf8d0b658349bdd6af98
Trenton Schulz <twschulz@twschulz-rain>
2006-04-26 11:08:21 +0100

    Fixes:    Now that Jaguar is gone we can use nl_langinfo as well :)
    [git-p4: depot-paths = "//depot/qt/main/": change = 218225]

--- src/corelib/codecs/qtextcodec.cpp
+++ src/corelib/codecs/qtextcodec.cpp
@@ -330,7 +330,7 @@ static void setupLocaleMapper()
     localeMapper = QTextCodec::codecForName("System");
 #else
 
-#if defined (_XOPEN_UNIX) && !defined(Q_OS_QNX6) && !defined(Q_OS_OSF) && !defined(Q_OS_MAC)
+#if defined (_XOPEN_UNIX) && !defined(Q_OS_QNX6) && !defined(Q_OS_OSF)
     char *charset = nl_langinfo (CODESET);
     if (charset)
       localeMapper = QTextCodec::codecForName(charset);
