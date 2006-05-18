29f4d8d2242e3e572e31ff32e1422082f6f7d148
Trond Kjernaasen <trond@trolltech.com>
2006-05-18 17:43:22 +0100

    Add an OpenGL framebuffer object example - it's weird, and I guess it will change in the near future.
    [git-p4: depot-paths = "//depot/qt/main/": change = 220288]

---
+++ examples/opengl/framebufferobject/glwidget.cpp
@@ -0,0 +1,187 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QtGui/QImage>
+#include "glwidget.h"
+
+#define WIDTH 12
+#define HEIGHT 11
+
+const char *logo[] =
+{
+    "..XX....X...",
+    ".X..X...X...",
+    "X....X.XXXXX",
+    "X....X..X...",
+    "X....X..X...",
+    "X....X..X...",
+    "X....X..X...",
+    ".X..X...X...",
+    "..XX.....XXX",
+    "...XX.......",
+    "....XX......"
+};
+GLWidget::GLWidget(QWidget *parent)
+    : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
+{
+    setWindowTitle(tr("OpenGL framebuffer objects"));
+    makeCurrent();
+    fbo = new QGLFramebufferObject(512, 512);
+    rot = 0.0;
+    scale = 1.2;
+    anim = new QTimeLine(750, this);
+    anim->setUpdateInterval(20);
+    connect(anim, SIGNAL(valueChanged(qreal)), SLOT(animate(qreal)));
+    connect(anim, SIGNAL(finished()), SLOT(animFinished()));
+
+    svg_renderer = new QSvgRenderer(QLatin1String(":/res/bubbles.svg"), this);
+    connect(svg_renderer, SIGNAL(repaintNeeded()), this, SLOT(draw()));
+    glClearColor(0.5, 0.5, 1.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+}
+
+GLWidget::~GLWidget()
+{
+    delete fbo;
+}
+
+void GLWidget::paintEvent(QPaintEvent *)
+{
+    draw();
+}
+
+void GLWidget::draw()
+{
+    QPainter p(this); // used for text overlay
+
+    // save the GL state QPainter expects
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+
+    // render the SVG file into our framebuffer object
+    QPainter fbo_painter(fbo);
+    svg_renderer->render(&fbo_painter);
+    fbo_painter.end();
+
+    // draw into the widget
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-1, 1, -1, 1, 10, 100);
+    glTranslatef(0.0f, 0.0f, -15.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glViewport(0, 0, width(), height());
+
+    glRotatef(rot, 0, 1, 0);
+
+    glScalef(scale/WIDTH, scale/WIDTH, scale/WIDTH);
+    glEnable(GL_MULTISAMPLE);
+    glEnable(GL_DEPTH_TEST);
+
+    glTranslatef(-WIDTH+1, -HEIGHT+1, 0);
+    for (int y=HEIGHT-1; y>=0; --y) {
+        for (int x=0; x<WIDTH; ++x) {
+            if (logo[y][x] == 'X') {
+                // front side
+                glEnable(GL_TEXTURE_2D);
+                glBindTexture(GL_TEXTURE_2D, fbo->texture());
+                glColor3f(1.0, 1.0, 1.0);
+                glBegin(GL_QUADS);
+                {
+                    glTexCoord2f(0, 0); glVertex3f(-1, -1, 0);
+                    glTexCoord2f(1, 0); glVertex3f(1, -1, 0);
+                    glTexCoord2f(1, 1); glVertex3f(1, 1, 0);
+                    glTexCoord2f(0, 1); glVertex3f(-1, 1, 0);
+                }
+                glEnd();
+                glDisable(GL_TEXTURE_2D);
+
+                // flip side
+                glColor4f(0.45, 0.45, 0.45, 0.9);
+                glBegin(GL_QUADS);
+                {
+                    glTexCoord2f(1, 0); glVertex3f(-1, -1, -0.01);
+                    glTexCoord2f(1, 1); glVertex3f(-1, 1, -0.01);
+                    glTexCoord2f(0, 1); glVertex3f(1, 1, -0.01);
+                    glTexCoord2f(0, 0); glVertex3f(1, -1, -0.01);
+                }
+                glEnd();
+            }
+            glTranslatef(2, 0, 0);
+        }
+        glTranslatef(-WIDTH*2, 2, 0);
+    }
+
+    // restore the GL state that QPainter expects
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glPopMatrix();
+    glPopAttrib();
+
+    // draw the instruction text
+    p.setPen(QColor(197, 197, 197, 157));
+    p.setBrush(QColor(197, 197, 197, 127));
+    p.drawRect(QRect(0, 0, width(), 50));
+    p.setBrush(Qt::NoBrush);
+    p.setPen(Qt::black);
+    const QString str1(tr("A simple OpenGL framebuffer object example."));
+    const QString str2(tr("Use the mouse wheel to zoom, press and move mouse to rotate the panel, double-click to flip the text."));
+    QFontMetrics fm(p.font());
+    p.drawText(width()/2 - fm.width(str1)/2, 20, str1);
+    p.drawText(width()/2 - fm.width(str2)/2, 20 + fm.lineSpacing(), str2);
+}
+
+void GLWidget::mousePressEvent(QMouseEvent *e)
+{
+    anchor = e->pos();
+}
+
+void GLWidget::mouseMoveEvent(QMouseEvent *e)
+{
+    QPoint diff = e->pos() - anchor;
+    rot += diff.x()/5.0;
+    anchor = e->pos();
+    draw();
+}
+
+void GLWidget::wheelEvent(QWheelEvent *e)
+{
+    e->delta() > 0 ? scale += 0.1 : scale -= 0.1;
+    draw();
+}
+
+void GLWidget::mouseDoubleClickEvent(QMouseEvent *)
+{
+    anim->start();
+}
+
+void GLWidget::animate(qreal val)
+{
+    rot = val * 180;
+    draw();
+}
+
+void GLWidget::animFinished()
+{
+    if (anim->direction() == QTimeLine::Forward)
+        anim->setDirection(QTimeLine::Backward);
+    else
+        anim->setDirection(QTimeLine::Forward);
+}
---
+++ examples/opengl/framebufferobject/glwidget.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QtOpenGL>
+#include <QTimeLine>
+#include <QSvgRenderer>
+
+class GLWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    GLWidget(QWidget *parent);
+    ~GLWidget();
+    void paintEvent(QPaintEvent *);
+    void mousePressEvent(QMouseEvent *);
+    void mouseDoubleClickEvent(QMouseEvent *);
+    void mouseMoveEvent(QMouseEvent *);
+    void wheelEvent(QWheelEvent *);
+
+
+public slots:
+    void animate(qreal);
+    void animFinished();
+    void draw();
+
+private:
+    QPoint anchor;
+    float scale;
+    float rot;
+
+    QTimeLine *anim;
+    QSvgRenderer *svg_renderer;
+    QGLFramebufferObject *fbo;
+};
+
