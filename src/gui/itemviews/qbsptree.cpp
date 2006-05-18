5cb79855749d6318981efdc94c95d0989d38a5ac
benjamin <bmeyer@trolltech.com>
2006-05-18 14:00:07 +0100

    p4i integration Integrate 220237 from 4.1-optimized to main: Integrate 220236 from 4.1 to 4.1-optimized: Fixes:    Assert if all rows are hidden in the list view (icon mode) Task:     111832 RevBy:    MBM
    [git-p4: depot-paths = "//depot/qt/main/": change = 220238]

--- src/gui/itemviews/qbsptree.cpp
+++ src/gui/itemviews/qbsptree.cpp
@@ -26,6 +26,7 @@ void QBspTree::create(int n, int d)
     } else {
         depth = d;
     }
+    depth = qMax(depth, 1);
 
     nodes.resize((1 << depth) - 1); // resize to number of nodes
     leaves.resize(1 << depth); // resize to number of leaves
