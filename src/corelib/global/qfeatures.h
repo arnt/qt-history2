c18cce851d26386840af1460f5b1a806213090fe
hwall <hwall@hwall-extra>
2006-05-23 13:12:16 +0100

    Fixes:    missing license header Task:     116098
    [git-p4: depot-paths = "//depot/qt/main/": change = 220691]

--- src/corelib/global/qfeatures.h
+++ src/corelib/global/qfeatures.h
@@ -1,7 +1,22 @@
-// All feature and their dependencies
-//
-// This list is generated from $QTDIR/src/corelib/global/qfeatures.txt
-//
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+/*
+ * All features and their dependencies.
+ *
+ * This list is generated from $QTDIR/src/corelib/global/qfeatures.txt
+ */
+
 // QAction
 //#define QT_NO_ACTION
 
