e547d9ed23c45e50b99c8b1d5da3fbb5a4fc3106
Trond Kjernaasen <trond@trolltech.com>
2006-06-06 15:11:25 +0100

    Port the framebuffer object example to use pbuffers. It's all part of Jasmins big plan. Task:    part of 117491
    [git-p4: depot-paths = "//depot/qt/main/": change = 221955]

---
+++ examples/opengl/pbuffers2/glwidget.h
@@ -0,0 +1,56 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QtOpenGL>
+#include <QImage>
+#include <QTimeLine>
+#include <QSvgRenderer>
+
+class GLWidget : public QGLWidget
+{
+    Q_OBJECT
+
+public:
+    GLWidget(QWidget *parent);
+    ~GLWidget();
+
+    void saveGLState();
+    void restoreGLState();
+
+    void paintEvent(QPaintEvent *);
+    void mousePressEvent(QMouseEvent *);
+    void mouseDoubleClickEvent(QMouseEvent *);
+    void mouseMoveEvent(QMouseEvent *);
+    void timerEvent(QTimerEvent *);
+    void wheelEvent(QWheelEvent *);
+
+public slots:
+    void animate(qreal);
+    void animFinished();
+    void draw();
+
+private:
+    QPoint anchor;
+    float scale;
+    float rot_x, rot_y, rot_z;
+    GLuint tile_list;
+    GLfloat *wave;
+
+    QImage logo;
+    QTimeLine *anim;
+    QSvgRenderer *svg_renderer;
+
+    GLuint dynamicTexture;
+    QGLPixelBuffer *pbuffer;
+};
+
