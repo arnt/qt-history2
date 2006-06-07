065065f696cf9a280755dd7e272cfbc89e3a8b25
dboddie <dboddie@mikaeld-lurven>
2006-06-07 10:39:15 +0100

    Doc/example: Forgot to add these.
    [git-p4: depot-paths = "//depot/qt/main/": change = 222019]

---
+++ examples/tools/completer/dirmodel.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+**
+** Copyright (C) 2006-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef DIRMODEL_H
+#define DIRMODEL_H
+
+#include <QDirModel>
+
+// With a QDirModel, set on a view, you will see "Program Files" in the view
+// But with this model, you will see "C:\Program Files" in the view.
+// We acheive this, by having the data() return the entire file path for
+// the display role. Note that the Qt::EditRole over which the QCompleter
+// looks for matches is left unchanged
+class DirModel : public QDirModel
+{
+public:
+    DirModel(QObject *parent = 0);
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+};
+
+#endif
