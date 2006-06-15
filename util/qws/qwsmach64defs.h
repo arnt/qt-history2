103866a8d9ceb6e76bbfe4cce2d7906683bc1bdb
dboddie <dboddie@mikaeld-lurven>
2006-06-15 18:18:52 +0100

    Fixes: Added missing license information.
    [git-p4: depot-paths = "//depot/qt/main/": change = 223744]

--- util/qws/qwsmach64defs.h
+++ util/qws/qwsmach64defs.h
@@ -6,6 +6,9 @@
 **
 ** $TROLLTECH_DUAL_LICENSE$
 **
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
 ****************************************************************************/
 
 #define GEN_TEST_CNTL 0x00d0
