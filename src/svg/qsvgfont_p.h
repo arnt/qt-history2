02a82f7c1b1a5e877ad6224cd8c670ce7143a3d8
harald <harald@harald-morpork>
2006-06-16 14:57:49 +0100

    Fixes:    license header consistency
    [git-p4: depot-paths = "//depot/qt/main/": change = 223890]

--- src/svg/qsvgfont_p.h
+++ src/svg/qsvgfont_p.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2005-$THISYEAR$ $TROLLTECH$. All rights reserved.
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
 **
 ** This file is part of the $MODULE$ of the Qt Toolkit.
 **
