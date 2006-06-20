d0a2a44d365be9112af3e0f66f471dac197a5362
Marius Storm-Olsen <marius@trolltech.com>
2006-06-20 10:22:21 +0100

    Fixes:    Add qkeymapper_qws.cpp code Details:  The integrate missed the code in qkeymapper_qws.cpp for some reason
    [git-p4: depot-paths = "//depot/qt/main/": change = 224233]

---
+++ src/gui/kernel/qkeymapper_qws.cpp
@@ -0,0 +1,43 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "qkeymapper_p.h"
+#include <qdebug.h>
+#include <private/qevent_p.h>
+#include <private/qlocale_p.h>
+
+
+QKeyMapperPrivate::QKeyMapperPrivate()
+{
+    keyboardInputLocale = QLocale::system();
+    keyboardInputDirection = Qt::RightToLeft;
+}
+
+QKeyMapperPrivate::~QKeyMapperPrivate()
+{
+    // clearMappings();
+}
+
+void QKeyMapperPrivate::clearMappings()
+{
+}
+
+QList<int> QKeyMapperPrivate::possibleKeys(QKeyEvent *e)
+{
+    QList<int> result;
+    if (e->key() && (e->key() != Qt::Key_unknown))
+        result << int(e->key() + e->modifiers());
+    else if (!e->text().isEmpty())
+        result << int(e->text().at(0).unicode() + e->modifiers());
+    return result;
+}
