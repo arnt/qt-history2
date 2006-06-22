5b5d1ca9b367099a552831131c7e16cd1629ded2
Trenton Schulz <twschulz@twschulz-rain>
2006-06-22 08:58:07 +0100

    p4i integration Integrate 224537 from 4.2 to main: Fixes:    I don't know what sort of stack/heap tricks we were trying to pull, but this doesn't cause as many memory errors.
    [git-p4: depot-paths = "//depot/qt/main/": change = 224602]

--- tests/auto/qfocusframe/tst_qfocusframe.cpp
+++ tests/auto/qfocusframe/tst_qfocusframe.cpp
@@ -40,16 +40,16 @@ tst_QFocusFrame::~tst_QFocusFrame()
 // Testing get/set functions
 void tst_QFocusFrame::getSetCheck()
 {
-    QFocusFrame obj1;
+    QFocusFrame *obj1 = new QFocusFrame();
     // QWidget * QFocusFrame::widget()
     // void QFocusFrame::setWidget(QWidget *)
-    QWidget *var1 = new QWidget();
-    QWidget *var2 = new QWidget(var1);
-    obj1.setWidget(var2);
-    QCOMPARE(var2, obj1.widget());
-    obj1.setWidget((QWidget *)0);
-    QCOMPARE((QWidget *)0, obj1.widget());
-    delete var1;
+    QWidget var1;
+    QWidget *var2 = new QWidget(&var1);
+    obj1->setWidget(var2);
+    QCOMPARE(var2, obj1->widget());
+    obj1->setWidget((QWidget *)0);
+    QCOMPARE((QWidget *)0, obj1->widget());
+    delete obj1;
 }
 
 QTEST_MAIN(tst_QFocusFrame)
