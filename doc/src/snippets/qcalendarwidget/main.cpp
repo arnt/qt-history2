14d275d69a860c9d7465a2c59cb6c87cb08a5762
dboddie <dboddie@mikaeld-lurven>
2006-06-26 15:18:34 +0100

    p4i integration Integrate 225210 from 4.2 to main: Doc: More changes to the QCalendarWidget screenshots.
    [git-p4: depot-paths = "//depot/qt/main/": change = 225243]

--- doc/src/snippets/qcalendarwidget/main.cpp
+++ doc/src/snippets/qcalendarwidget/main.cpp
@@ -10,5 +10,15 @@ int main(int argc, char **argv)
     calendar.setGridVisible(true);
     calendar.show();
 
+    QCalendarWidget calendarMin;
+    calendarMin.setMinimumDate(calendarMin.selectedDate().addDays(-7));
+    calendarMin.setGridVisible(true);
+    calendarMin.show();
+
+    QCalendarWidget calendarMax;
+    calendarMax.setMaximumDate(calendarMax.selectedDate().addDays(7));
+    calendarMax.setGridVisible(true);
+    calendarMax.show();
+
     return app.exec();
 }
