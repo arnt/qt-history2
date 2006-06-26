39820746318e902132005c707b5f7c7f95923c99
dboddie <dboddie@mikaeld-lurven>
2006-06-26 15:19:23 +0100

    p4i integration Integrate 225225 from 4.2 to main: Doc: CleanLooks -> Cleanlooks
    [git-p4: depot-paths = "//depot/qt/main/": change = 225247]

--- src/gui/styles/qcleanlooksstyle.cpp
+++ src/gui/styles/qcleanlooksstyle.cpp
@@ -539,7 +539,8 @@ static void qt_cleanlooks_draw_mdibutton(QPainter *painter, const QStyleOptionTi
     that closely resembles the Clearlooks style, introduced by Richard
     Stellingwerff and Daniel Borgmann.
 
-    \sa QWindowsXPStyle, QMacStyle, QWindowsStyle, QCDEStyle, QMotifStyle, QPlastiqueStyle.
+    \sa {Cleanlooks Style Widget Gallery}, QWindowsXPStyle, QMacStyle, QWindowsStyle,
+        QCDEStyle, QMotifStyle, QPlastiqueStyle
 */
 
 /*!
