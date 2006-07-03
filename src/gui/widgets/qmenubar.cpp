3c196ea531d9509a38051ff5a4faef64b095243f
Jasmin Blanchette <jasmin@trolltech.com>
2006-07-03 17:49:45 +0100

    p4i integration Integrate 226635 from 4.2 to main: Fixes:    Doc: Fix \sa link. Task:     111193
    [git-p4: depot-paths = "//depot/qt/main/": change = 226636]

--- src/gui/widgets/qmenubar.cpp
+++ src/gui/widgets/qmenubar.cpp
@@ -548,7 +548,7 @@ QStyleOptionMenuItem QMenuBarPrivate::getStyleOption(const QAction *action) cons
     QMenuBar and QMenu.
 
     \sa QMenu, QShortcut, QAction,
-        {http://developer.apple.com/documentation/UserExperience/Conceptual/OSXHIGuidelines/index.html}{Introduction to Apple Human Interface Guidelines}
+        {http://developer.apple.com/documentation/UserExperience/Conceptual/OSXHIGuidelines/index.html}{Introduction to Apple Human Interface Guidelines},
         {fowler}{GUI Design Handbook: Menu Bar}, {Menus Example}
 */
 
