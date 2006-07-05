3978364aa33047966149edd4538ad7c0301858c0
ahanssen <ahanssen@:w>
2006-07-05 10:36:22 +0100

    p4i integration Integrate 224689 from 4.2 to main: Fixes:    Test what is actually returned; we cannot change this behavior.
    [git-p4: depot-paths = "//depot/qt/main/": change = 226816]

--- tests/auto/q3progressdialog/tst_q3progressdialog.cpp
+++ tests/auto/q3progressdialog/tst_q3progressdialog.cpp
@@ -44,34 +44,36 @@ void tst_Q3ProgressDialog::getSetCheck()
     // bool Q3ProgressDialog::autoReset()
     // void Q3ProgressDialog::setAutoReset(bool)
     obj1.setAutoReset(false);
-    QCOMPARE(false, obj1.autoReset());
+    QVERIFY(!obj1.autoReset());
     obj1.setAutoReset(true);
-    QCOMPARE(true, obj1.autoReset());
+    QVERIFY(obj1.autoReset());
 
     // bool Q3ProgressDialog::autoClose()
     // void Q3ProgressDialog::setAutoClose(bool)
     obj1.setAutoClose(false);
-    QCOMPARE(false, obj1.autoClose());
+    QVERIFY(!obj1.autoClose());
     obj1.setAutoClose(true);
-    QCOMPARE(true, obj1.autoClose());
+    QVERIFY(obj1.autoClose());
 
     // int Q3ProgressDialog::totalSteps()
     // void Q3ProgressDialog::setTotalSteps(int)
     obj1.setTotalSteps(0);
-    QCOMPARE(0, obj1.totalSteps());
+    QCOMPARE(obj1.totalSteps(), 0);
     obj1.setTotalSteps(INT_MIN);
-    QCOMPARE(INT_MIN, obj1.totalSteps());
+    QCOMPARE(obj1.totalSteps(), INT_MIN);
     obj1.setTotalSteps(INT_MAX);
-    QCOMPARE(INT_MAX, obj1.totalSteps());
+    QCOMPARE(obj1.totalSteps(), INT_MAX);
 
     // int Q3ProgressDialog::minimumDuration()
     // void Q3ProgressDialog::setMinimumDuration(int)
     obj1.setMinimumDuration(0);
-    QCOMPARE(0, obj1.minimumDuration());
+    QCOMPARE(obj1.minimumDuration(), 0);
     obj1.setMinimumDuration(INT_MIN);
-    QCOMPARE(0, obj1.minimumDuration()); // Makes no sense with negative duration
+    // It makes no sense with negative duration, but we cannot change this
+    // behavior in a Qt3Support classs.
+    QCOMPARE(obj1.minimumDuration(), INT_MIN);
     obj1.setMinimumDuration(INT_MAX);
-    QCOMPARE(INT_MAX, obj1.minimumDuration());
+    QCOMPARE(obj1.minimumDuration(), INT_MAX);
 }
 
 QTEST_MAIN(tst_Q3ProgressDialog)
