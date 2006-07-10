5441838ea42f7142bc8b376bf874aa7f25b031a3
jkobus <jkobus@smatux-slaruell>
2006-07-10 10:19:11 +0100

    p4i integration Integrate 227186 from 4.2 to main: Fixes:    Doubles are rounded off in Designer Task:     119197
    [git-p4: depot-paths = "//depot/qt/main/": change = 227187]

--- tools/designer/src/lib/uilib/ui4.cpp
+++ tools/designer/src/lib/uilib/ui4.cpp
@@ -5460,7 +5460,7 @@ QDomElement DomProperty::write(QDomDocument &doc, const QString &tagName)
     }
     case Double: {
         QDomElement child = doc.createElement(QLatin1String("double"));
-        QDomText text = doc.createTextNode(QString::number(elementDouble()));
+        QDomText text = doc.createTextNode(QString::number(elementDouble(), 'g', 15));
         child.appendChild(text);
         e.appendChild(child);
         break;
