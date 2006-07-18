073fc8c4f670a7eed5a07f4d5ab7286667a3abe1
ahanssen <ahanssen@:w>
2006-07-18 10:49:45 +0100

    p4i integration Integrate 228278 from 4.2 to main: Fixes:    More tests for suffixes Task:     114525 Details:  Hidden files on Unix, not hidden on Windows.
    [git-p4: depot-paths = "//depot/qt/main/": change = 228279]

--- tests/auto/qfileinfo/tst_qfileinfo.cpp
+++ tests/auto/qfileinfo/tst_qfileinfo.cpp
@@ -408,7 +408,27 @@ void tst_QFileInfo::suffix_data()
     QTest::newRow("data3") << "/path/file.tar" << "tar";
     QTest::newRow("resource1") << ":/tst_qfileinfo/resources/file1.ext1" << "ext1";
     QTest::newRow("resource2") << ":/tst_qfileinfo/resources/file1.ext1.ext2" << "ext2";
-
+#ifdef Q_OS_UNIX
+    QTest::newRow("hidden1") << ".ext1" << "";
+    QTest::newRow("hidden1") << ".ext" << "";
+    QTest::newRow("hidden1") << ".ex" << "";
+    QTest::newRow("hidden1") << ".e" << "";
+    QTest::newRow("hidden2") << ".ext1.ext2" << "ext2";
+    QTest::newRow("hidden2") << ".ext.ext2" << "ext2";
+    QTest::newRow("hidden2") << ".ex.ext2" << "ext2";
+    QTest::newRow("hidden2") << ".e.ext2" << "ext2";
+    QTest::newRow("hidden2") << "..ext2" << "ext2";
+#else
+    QTest::newRow("hidden1") << ".ext1" << "ext1";
+    QTest::newRow("hidden1") << ".ext" << "ext";
+    QTest::newRow("hidden1") << ".ex" << "ex";
+    QTest::newRow("hidden1") << ".e" << "e";
+    QTest::newRow("hidden2") << ".ext1.ext2" << "ext2";
+    QTest::newRow("hidden2") << ".ext.ext2" << "ext2";
+    QTest::newRow("hidden2") << ".ex.ext2" << "ext2";
+    QTest::newRow("hidden2") << ".e.ext2" << "ext2";
+    QTest::newRow("hidden2") << "..ext2" << "ext2";
+#endif
 }
 
 void tst_QFileInfo::suffix()
