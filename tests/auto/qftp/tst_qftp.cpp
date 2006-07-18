514084ae7fa550f29bd3f5854ff5607a4abe9a93
ahanssen <ahanssen@:w>
2006-07-18 09:44:42 +0100

    p4i integration Integrate 228255 from 4.2 to main: Fixes:    Test for starting new commands in an error slot.
    [git-p4: depot-paths = "//depot/qt/main/": change = 228256]

--- tests/auto/qftp/tst_qftp.cpp
+++ tests/auto/qftp/tst_qftp.cpp
@@ -93,6 +93,7 @@ protected slots:
     void commandFinished( int, bool );
     void done( bool );
     void activeModeDone( bool );
+    void mkdir2Slot(int id, bool error);
 
 private:
     QFtp *newFtp();
@@ -835,32 +836,23 @@ QVERIFY( !dirExists( host, port, user, password, cdDir, dirToCreate ) );
 
 void tst_QFtp::mkdir2()
 {
-    QFtp ftp;
-    ftp.connectToHost("smokesignal.troll.no");
-    ftp.login();
-    ftp.cd("kake/test");
+    ftp = new QFtp;
+    ftp->connectToHost("smokesignal.troll.no");
+    ftp->login();
+    current_id = ftp->cd("kake/test");
 
     QEventLoop loop;
-    connect(&ftp, SIGNAL(done(bool)), &loop, SLOT(quit()));
+    connect(ftp, SIGNAL(done(bool)), &loop, SLOT(quit()));
+    connect(ftp, SIGNAL(commandFinished(int, bool)), this, SLOT(mkdir2Slot(int, bool)));
     QTimer::singleShot(5000, &loop, SLOT(quit()));
 
-    QSignalSpy commandStartedSpy(&ftp, SIGNAL(commandStarted(int)));
-    QSignalSpy commandFinishedSpy(&ftp, SIGNAL(commandFinished(int, bool)));
+    QSignalSpy commandStartedSpy(ftp, SIGNAL(commandStarted(int)));
+    QSignalSpy commandFinishedSpy(ftp, SIGNAL(commandFinished(int, bool)));
     
-    loop.exec();
-
-    QCOMPARE(commandStartedSpy.count(), 3); // connect, login, cd
-    QCOMPARE(commandFinishedSpy.count(), 3);
-
-    ftp.mkdir("kake/test");
     loop.exec();
 
     QCOMPARE(commandStartedSpy.count(), 4); // connect, login, cd, mkdir
     QCOMPARE(commandFinishedSpy.count(), 4);
-
-    QEventLoop loop2;
-    connect(&ftp, SIGNAL(done(bool)), &loop2, SLOT(quit()));
-    QTimer::singleShot(5000, &loop2, SLOT(quit()));
 
     for (int i = 0; i < 4; ++i)
         QCOMPARE(commandFinishedSpy.at(i).at(0), commandStartedSpy.at(i).at(0));
@@ -869,6 +861,12 @@ void tst_QFtp::mkdir2()
     QVERIFY(!commandFinishedSpy.at(1).at(1).toBool());
     QVERIFY(commandFinishedSpy.at(2).at(1).toBool());
     QVERIFY(commandFinishedSpy.at(3).at(1).toBool());
+}
+
+void tst_QFtp::mkdir2Slot(int id, bool error)
+{
+    if (id == current_id)
+        ftp->mkdir("kake/test");
 }
 
 void tst_QFtp::rmdir()
