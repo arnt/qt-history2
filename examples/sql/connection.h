286b771074fb922edd6eb5d06c28718c07f056f7
Girish Ramakrishnan <gramakri@trolltech.com>
2006-07-20 18:00:40 +0100

    p4i integration Integrate 228789 from 4.2 to main: Fixes:    compile
    [git-p4: depot-paths = "//depot/qt/main/": change = 228790]

--- examples/sql/connection.h
+++ examples/sql/connection.h
@@ -14,7 +14,7 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
-#include <QMessageBoxEx>
+#include <QMessageBox>
 #include <QSqlDatabase>
 #include <QSqlError>
 #include <QSqlQuery>
@@ -32,12 +32,12 @@ static bool createConnection()
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName(":memory:");
     if (!db.open()) {
-        QMessageBoxEx::critical(0, qApp->tr("Cannot open database"),
+        QMessageBox::critical(0, qApp->tr("Cannot open database"),
             qApp->tr("Unable to establish a database connection.\n"
                      "This example needs SQLite support. Please read "
                      "the Qt SQL driver documentation for information how "
                      "to build it.\n\n"
-                     "Click Cancel to exit."), QMessageBoxEx::Cancel);
+                     "Click Cancel to exit."), QMessageBox::Cancel);
         return false;
     }
 
