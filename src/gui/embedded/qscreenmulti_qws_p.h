546c0ce859a28bf224003626e5faef57b64fba20
hwall <hwall@hwall-extra>
2006-07-24 12:16:04 +0100

    p4i integration Integrate 229102 from 4.2 to main: Fixes:    missing license headers
    [git-p4: depot-paths = "//depot/qt/main/": change = 229103]

--- src/gui/embedded/qscreenmulti_qws_p.h
+++ src/gui/embedded/qscreenmulti_qws_p.h
@@ -14,6 +14,17 @@
 #ifndef QMULTISCREEN_QWS_P_H
 #define QMULTISCREEN_QWS_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of the QLibrary class.  This header file may change from
+// version to version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <qscreen_qws.h>
 
 class QMultiScreenPrivate;
