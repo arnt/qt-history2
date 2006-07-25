5a2576ecf44cbcf0399b3ecb113d2426b12970d6
msorvig <msorvig@dole>
2006-07-25 09:35:32 +0100

    p4i integration Integrate 229244 from 4.2 to main: Username: msorvig Enable Windows-specific code from change 229242.
    [git-p4: depot-paths = "//depot/qt/main/": change = 229251]

--- src/corelib/thread/qthread_win.cpp
+++ src/corelib/thread/qthread_win.cpp
@@ -63,8 +63,8 @@ QThreadData *QThreadData::current()
 
 void QAdoptedThread::init()
 {
-//    d_func()->handle = GetCurrentThread();
-//    d_func()->id = GetCurrentThreadId();
+    d_func()->handle = GetCurrentThread();
+    d_func()->id = GetCurrentThreadId();
 }
 
 /**************************************************************************
