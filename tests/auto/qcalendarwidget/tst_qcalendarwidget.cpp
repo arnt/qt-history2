7694bfb5e31dc324ea350003cdebc63de5e14780
pullatti <prasanth.ullattil@trolltech.com>
2006-07-28 13:19:21 +0100

    p4i integration Integrate 229826 from 4.2 to main: Fixes:    Autotest for calendar widget Task:     123538 Details:   GetSet Checking and mouse clicks on the navigation buttons
    [git-p4: depot-paths = "//depot/qt/main/": change = 229832]

--- tests/auto/qcalendarwidget/tst_qcalendarwidget.cpp
+++ tests/auto/qcalendarwidget/tst_qcalendarwidget.cpp
@@ -10,6 +10,9 @@
 #include <QtTest/QtTest>
 
 #include <qcalendarwidget.h>
+#include <qtoolbutton.h>
+#include <qspinbox.h>
+#include <qmenu.h>
 #include <qdebug.h>
 
 
@@ -23,15 +26,16 @@ class tst_QCalendarWidget : public QObject
 public:
     tst_QCalendarWidget();
     virtual ~tst_QCalendarWidget();
-
-
 public slots:
     void initTestCase();
     void cleanupTestCase();
     void init();
     void cleanup();
+
 private slots:
     void getSetCheck();
+    void buttonClickCheck();
+
 };
 
 // Testing get/set functions
@@ -84,8 +88,96 @@ void tst_QCalendarWidget::getSetCheck()
     QDate selectedDate(2005, 7, 3);
     object.setSelectedDate(selectedDate);
     QCOMPARE(selectedDate, object.selectedDate());
+    //month and year
+    object.setCurrentPage(2004, 1);
+    QCOMPARE(1, object.monthShown());
+    QCOMPARE(2004, object.yearShown());
+    object.showNextMonth();
+    QCOMPARE(2, object.monthShown());
+    object.showPreviousMonth();
+    QCOMPARE(1, object.monthShown());
+    object.showNextYear();
+    QCOMPARE(2005, object.yearShown());
+    object.showPreviousYear();
+    QCOMPARE(2004, object.yearShown());
+    //date range
+    minDate = QDate(2006,1,1);
+    maxDate = QDate(2010,12,31);
+    object.setDateRange(minDate, maxDate);
+    QCOMPARE(maxDate, object.maximumDate());
+    QCOMPARE(minDate, object.minimumDate());
+
+    //date should not go beyond the minimum.
+    selectedDate = minDate.addDays(-10);
+    object.setSelectedDate(selectedDate);
+    QCOMPARE(minDate, object.selectedDate());
+    QVERIFY(selectedDate != object.selectedDate());
+    //date should not go beyond the maximum.
+    selectedDate = maxDate.addDays(10);
+    object.setSelectedDate(selectedDate);
+    QCOMPARE(maxDate, object.selectedDate());
+    QVERIFY(selectedDate != object.selectedDate());
+    //show today
+    QDate today = QDate::currentDate();
+    object.showToday();
+    QCOMPARE(today.month(), object.monthShown());
+    QCOMPARE(today.year(), object.yearShown());
+    //slect a different date and move.
+    object.setSelectedDate(minDate);
+    object.showSelectedDate();
+    QCOMPARE(minDate.month(), object.monthShown());
+    QCOMPARE(minDate.year(), object.yearShown());
+}
+
+void tst_QCalendarWidget::buttonClickCheck()
+{
+    QCalendarWidget object;
+    QSize size = object.sizeHint();
+    object.setGeometry(0,0,size.width(), size.height());
+    object.show();
+
+    QRect rect = object.geometry();
+    QDate selectedDate(2005, 1, 1);
+    //click on the month buttons
+    int month = object.monthShown();
+    QToolButton *button = object.findChild<QToolButton *>("qt_calendar_prevmonth");
+    QTest::mouseClick(button, Qt::LeftButton);
+    QCOMPARE(month-1, object.monthShown());
+    button = object.findChild<QToolButton *>("qt_calendar_nextmonth");
+    QTest::mouseClick(button, Qt::LeftButton);
+    QCOMPARE(month, object.monthShown());
+
+    button = object.findChild<QToolButton *>("qt_calendar_yearbutton");
+    QTest::mouseClick(button, Qt::LeftButton);
+    QVERIFY(!button->isVisible());
+    QSpinBox *spinbox = object.findChild<QSpinBox *>("qt_calendar_yearedit");
+    QTest::keyClick(spinbox, '2');
+    QTest::keyClick(spinbox, '0');
+    QTest::keyClick(spinbox, '0');
+    QTest::keyClick(spinbox, '6');
+    QWidget *widget = object.findChild<QWidget *>("qt_calendar_calendarview");
+    QTest::mouseClick(widget, Qt::LeftButton);
+    QCOMPARE(2006, object.yearShown());
+    object.setSelectedDate(selectedDate);
+    object.showSelectedDate();
+    QTest::keyClick(widget, Qt::Key_Down);
+    QVERIFY(selectedDate != object.selectedDate());
+
+    object.setDateRange(QDate(2006,1,1), QDate(2006,2,28));
+    object.setSelectedDate(QDate(2006,1,1));
+    object.showSelectedDate();
+    button = object.findChild<QToolButton *>("qt_calendar_prevmonth");
+    QTest::mouseClick(button, Qt::LeftButton);
+    QCOMPARE(1, object.monthShown());
+
+    button = object.findChild<QToolButton *>("qt_calendar_nextmonth");
+    QTest::mouseClick(button, Qt::LeftButton);
+    QCOMPARE(2, object.monthShown());
+    QTest::mouseClick(button, Qt::LeftButton);
+    QCOMPARE(2, object.monthShown());
 
 }
+
 
 tst_QCalendarWidget::tst_QCalendarWidget()
 {
