0dd2ac3c92904e2364fc1802636b73db2ccadc3c
tlillesk <tlillesk@trolltech.com>
2006-08-07 14:56:40 +0100

    p4i integration Integrate 230688 from 4.2 to main: Fixes:    Now, the conical.frag and radial.frag are generated from OpenGL Shading Language code.                  Read README_GLSL for how to generate them manually from the .glsl files. Task:     None AutoTest: tested with research/painter/tools/lance Details:  This is the foundation for the OpenGL SL support/porting of the gradient code to work on embedded.
    [git-p4: depot-paths = "//depot/qt/main/": change = 230689]

---
+++ src/opengl/conical.glsl
@@ -0,0 +1,23 @@
+// conical gradient shader
+#define M_PI  3.14159265358979323846
+uniform sampler1D palette;
+uniform float angle;
+uniform vec4 inv_matrix;
+uniform vec2 inv_matrix_offset;
+
+void main()
+{
+    mat2 mat;
+    mat[0][0] = inv_matrix.x;
+    mat[0][1] = inv_matrix.y;
+    mat[1][0] = inv_matrix.z;
+    mat[1][1] = inv_matrix.w;
+
+    vec2 A = mat*gl_FragCoord.xy + inv_matrix_offset;
+/*     float val = fmod((atan2(-A.y, A.x) + angle) / (2.0 * M_PI), 1); */
+    if (abs(A.y) == abs(A.x))
+ 	A.y += 0.002;
+    float t = (atan2(-A.y, A.x) + angle) / (2.0 * M_PI);
+    float val = t - floor(t);
+    gl_FragColor = texture1D(palette, val);
+}
