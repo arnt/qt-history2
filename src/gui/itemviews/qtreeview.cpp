4b1e83e017751e57ffa3bee946c6c379fbfa46bc
jasaethe <jasaethe@mike-macondo>
2006-08-07 14:33:00 +0100

    p4i integration Integrate 230674 from 4.2 to main: Fixes:    Make sure that child items are layouted correctly if column 0 is hidden. Task:     124499 AutoTest: I wish... Details:  The viewItems structure contains only visible model indexes, so modelIndex(i) will return a model index that does not have children. This causes model->hasChildren(parent) and model->rowCount(parent) to not return the values that we are querying for. This should also fix it for the other places that we are using parent, since the rest of the code always assumes parent to be a (valid) parent in column 0.
    [git-p4: depot-paths = "//depot/qt/main/": change = 230675]

--- src/gui/itemviews/qtreeview.cpp
+++ src/gui/itemviews/qtreeview.cpp
@@ -2229,6 +2229,9 @@ void QTreeViewPrivate::layout(int i)
     Q_Q(QTreeView);
     QModelIndex current;
     QModelIndex parent = (i < 0) ? (QModelIndex)root : modelIndex(i);
+    // modelIndex() will return an index that don't have a parent if column 0 is hidden, 
+    // so we must make sure that parent points to the actual parent that has children.
+    parent = model->index(parent.row(), 0, parent.parent());
 
     int count = 0;
     if (model->hasChildren(parent))
