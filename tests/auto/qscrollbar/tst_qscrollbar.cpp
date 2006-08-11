0b3e70adac6e9b4a532b71a071dcab0103e2d66f
bnilsen <bnilsen@trolltech.com>
2006-08-11 14:40:44 +0100

    p4i integration Integrate 231338 from 4.2 to main: Fixes:    Test for QScrollBar.
    [git-p4: depot-paths = "//depot/qt/main/": change = 231339]

---
+++ tests/auto/qscrollbar/tst_qscrollbar.cpp
@@ -0,0 +1,69 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ Trolltech AS. All rights reserved.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QtTest/QtTest>
+#include <QScrollBar>
+#include <QStyleOptionSlider>
+
+class tst_QScrollBar : public QObject
+{
+    Q_OBJECT
+public slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void hideAndShow(int action);
+
+private slots:
+    void scrollSingleStep();
+
+private:
+    QScrollBar *testWidget;
+};
+
+void tst_QScrollBar::initTestCase()
+{
+    testWidget = new QScrollBar(Qt::Horizontal);
+    testWidget->resize(100, testWidget->height());
+    testWidget->show();
+}
+
+void tst_QScrollBar::cleanupTestCase()
+{
+    delete testWidget;
+    testWidget = 0;
+}
+
+void tst_QScrollBar::hideAndShow(int)
+{
+    testWidget->hide();
+    testWidget->show();
+}
+
+// Check that the scrollbar doesn't scroll after calling hide and show
+// from a slot connected to the scrollbar's actionTriggered signal.
+void tst_QScrollBar::scrollSingleStep()
+{
+    testWidget->setValue(testWidget->minimum());
+    QCOMPARE(testWidget->value(), testWidget->minimum());
+    connect(testWidget, SIGNAL(actionTriggered(int)), this, SLOT(hideAndShow(int)));
+
+    // Get rect for the area to click on
+    QStyleOptionSlider opt;
+    opt.initFrom(testWidget);
+    QRect sr = testWidget->style()->subControlRect(QStyle::CC_ScrollBar, &opt,
+                                                   QStyle::SC_ScrollBarAddLine, testWidget);
+
+    QTest::mouseClick(testWidget, Qt::LeftButton, Qt::NoModifier, QPoint(sr.x(), sr.y()));
+    QTest::qWait(510); // initial delay is 500 for setRepeatAction
+    disconnect(testWidget, SIGNAL(actionTriggered(int)), 0, 0);
+    QCOMPARE(testWidget->value(), testWidget->singleStep());
+}
+
+QTEST_MAIN(tst_QScrollBar)
+#include "tst_qscrollbar.moc"
