238a4eb0e2536734f27587af470ea8ba4b375578
harald <harald@harald-morpork>
2006-08-16 09:24:32 +0100

    p4i integration Integrate 231924 from 4.2 to main: Fixes:    shuttup
    [git-p4: depot-paths = "//depot/qt/main/": change = 231925]

--- src/opengl/qgl.h
+++ src/opengl/qgl.h
@@ -163,7 +163,7 @@ public:
         OpenGL_ES_CommonLite_Version_1_0  = 0x00000100,
         OpenGL_ES_Common_Version_1_1      = 0x00000200,
         OpenGL_ES_CommonLite_Version_1_1  = 0x00000400,
-        OpenGL_ES_Version_2_0             = 0x00000800,
+        OpenGL_ES_Version_2_0             = 0x00000800
     };
     Q_DECLARE_FLAGS(OpenGLVersionFlags, OpenGLVersionFlag)
 
