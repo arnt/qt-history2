3c1b65c04739e62fbcda5b7cf1dbfe345191ce54
Paul Olav Tvete <paul@trolltech.com>
2006-08-18 13:04:50 +0100

    p4i integration Integrate 232415 from 4.2 to main: Fixes:    Warn about the Qt::Tool mess on embedded Task:     126435 Details:  To be cross-platform compatible, we have to 	  break compatibility with previous versions. 	  For this release we issue a qWarning() for 	  the case that is going to break.
    [git-p4: depot-paths = "//depot/qt/main/": change = 232416]

--- src/gui/kernel/qwidget_qws.cpp
+++ src/gui/kernel/qwidget_qws.cpp
@@ -89,6 +89,15 @@ void QWidgetPrivate::create_sys(WId window, bool initializeWindow, bool /*destro
     bool desktop = (type == Qt::Desktop);
     bool tool = (type == Qt::Tool || type == Qt::SplashScreen || type == Qt::ToolTip);
 
+#ifndef QT_NO_WARNING_OUTPUT
+    static bool toolWarningShown = false;
+    if (!toolWarningShown && type == Qt::Tool && !(flags & Qt::FramelessWindowHint)) {
+        qWarning() << "Qtopia Core " QT_VERSION_STR " does not support tool windows with frames.\n"
+            "This behavior will change in a later release. To ensure compatibility with\n"
+            "future versions, use (Qt::Tool | Qt::FramelessWindowHint).";
+    }
+#endif
+
     // a popup stays on top
     if (popup)
         flags |= Qt::WindowStaysOnTopHint;
