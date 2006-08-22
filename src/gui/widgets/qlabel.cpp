2efa909e5dac8ca2f165250f6c00baf12ffa316c
Girish Ramakrishnan <gramakri@trolltech.com>
2006-08-22 12:14:40 +0100

    p4i integration Integrate 233003 from 4.2 to main: Fixes:    Refactor private into qlabel_p.h Description: Will come in handy for message box and other widgets that want to access a label'd document
    [git-p4: depot-paths = "//depot/qt/main/": change = 233004]

--- src/gui/widgets/qlabel.cpp
+++ src/gui/widgets/qlabel.cpp
@@ -11,95 +11,19 @@
 **
 ****************************************************************************/
 
-#include "qlabel.h"
 #include "qpainter.h"
 #include "qevent.h"
 #include "qdrawutil.h"
-#include "qmovie.h"
-#include "qimage.h"
-#include "qbitmap.h"
-#include "qpicture.h"
 #include "qapplication.h"
-#include "qtextdocument.h"
 #include "qabstractbutton.h"
 #include "qstyle.h"
 #include "qstyleoption.h"
-#include "qframe_p.h"
 #include <limits.h>
-#include "qtextdocumentfragment.h"
 #include "qaction.h"
-#include "qmenu.h"
 #include "qclipboard.h"
-#include "../text/qtextdocumentlayout_p.h"
-#include "private/qtextcontrol_p.h"
 #include <qdebug.h>
 #include <qurl.h>
-
-class QLabelPrivate : public QFramePrivate
-{
-    Q_DECLARE_PUBLIC(QLabel)
-public:
-    QLabelPrivate() {}
-
-    void init();
-    void clearContents();
-    void updateLabel();
-    QSize sizeForWidth(int w) const;
-
-    mutable QSize sh;
-    mutable QSize msh;
-    mutable bool valid_hints;
-    int margin;
-    QString text;
-    QPixmap  *pixmap;
-    QPixmap *scaledpixmap;
-    QImage *cachedimage;
-#ifndef QT_NO_PICTURE
-    QPicture *picture;
-#endif
-#ifndef QT_NO_MOVIE
-    QPointer<QMovie> movie;
-    void _q_movieUpdated(const QRect&);
-    void _q_movieResized(const QSize&);
-#endif
-#ifndef QT_NO_SHORTCUT
-    void updateShortcut();
-#endif
-    bool isRichText() const {
-        return textformat == Qt::RichText
-               || (textformat == Qt::AutoText && Qt::mightBeRichText(text));
-    }
-#ifndef QT_NO_SHORTCUT
-    QPointer<QWidget> buddy;
-    int shortcutId;
-#endif
-    ushort align;
-    short indent;
-    uint scaledcontents :1;
-    Qt::TextFormat textformat;
-    QTextDocument* doc;
-    QTextControl *control;
-    Qt::TextInteractionFlags textInteractionFlags;
-
-    void ensureTextControl();
-    void sendControlEvent(QEvent *e);
-
-    void _q_linkHovered(const QString &link);
-
-    QRect layoutRect() const;
-    QRect documentRect() const;
-    QPoint layoutPoint(const QPoint& p) const;
-#ifndef QT_NO_CONTEXTMENU
-    QMenu *createStandardContextMenu(const QPoint &pos);
-#endif
-
-    bool openExternalLinks;
-
-    bool hasCustomCursor;
-#ifndef QT_NO_CURSOR
-    QCursor cursor;
-#endif
-};
+#include "qlabel_p.h"
 
 /*!
     \class QLabel
---
+++ src/gui/widgets/qlabel_p.h
@@ -0,0 +1,109 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QLABEL_P_H
+#define QLABEL_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "qlabel.h"
+
+#include "../text/qtextdocumentlayout_p.h"
+#include "private/qtextcontrol_p.h"
+#include "qtextdocumentfragment.h"
+#include "qframe_p.h"
+#include "qtextdocument.h"
+#include "qmovie.h"
+#include "qimage.h"
+#include "qbitmap.h"
+#include "qpicture.h"
+#include "qmenu.h"
+
+class QLabelPrivate : public QFramePrivate
+{
+    Q_DECLARE_PUBLIC(QLabel)
+public:
+    QLabelPrivate() {}
+
+    void init();
+    void clearContents();
+    void updateLabel();
+    QSize sizeForWidth(int w) const;
+
+    mutable QSize sh;
+    mutable QSize msh;
+    mutable bool valid_hints;
+    int margin;
+    QString text;
+    QPixmap  *pixmap;
+    QPixmap *scaledpixmap;
+    QImage *cachedimage;
+#ifndef QT_NO_PICTURE
+    QPicture *picture;
+#endif
+#ifndef QT_NO_MOVIE
+    QPointer<QMovie> movie;
+    void _q_movieUpdated(const QRect&);
+    void _q_movieResized(const QSize&);
+#endif
+#ifndef QT_NO_SHORTCUT
+    void updateShortcut();
+#endif
+    bool isRichText() const {
+        return textformat == Qt::RichText
+               || (textformat == Qt::AutoText && Qt::mightBeRichText(text));
+    }
+#ifndef QT_NO_SHORTCUT
+    QPointer<QWidget> buddy;
+    int shortcutId;
+#endif
+    ushort align;
+    short indent;
+    uint scaledcontents :1;
+    Qt::TextFormat textformat;
+    QTextDocument* doc;
+    QTextControl *control;
+    Qt::TextInteractionFlags textInteractionFlags;
+
+    void ensureTextControl();
+    void sendControlEvent(QEvent *e);
+
+    void _q_linkHovered(const QString &link);
+
+    QRect layoutRect() const;
+    QRect documentRect() const;
+    QPoint layoutPoint(const QPoint& p) const;
+#ifndef QT_NO_CONTEXTMENU
+    QMenu *createStandardContextMenu(const QPoint &pos);
+#endif
+
+    bool openExternalLinks;
+
+    bool hasCustomCursor;
+#ifndef QT_NO_CURSOR
+    QCursor cursor;
+#endif
+
+    friend class QMessageBoxPrivate;
+};
+
+#endif // QLABEL_P_H
