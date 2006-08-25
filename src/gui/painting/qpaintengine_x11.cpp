2bda8d0632b01332a7ee3d707e13341e45da180d
Trond Kjernaasen <trond@trolltech.com>
2006-08-25 10:19:17 +0100

    p4i integration Integrate 233665 from 4.2 to main: Fixes:    compile
    [git-p4: depot-paths = "//depot/qt/main/": change = 233666]

--- src/gui/painting/qpaintengine_x11.cpp
+++ src/gui/painting/qpaintengine_x11.cpp
@@ -1554,11 +1554,12 @@ void QX11PaintEnginePrivate::fillPolygon_dev(const QPointF *polygonPoints, int p
         has_fill_pattern = true;
     }
 
+    bool antialias = render_hints & QPainter::Antialiasing;
+
     // don't take the XRender path unless we really, really have to
     if (has_fill_texture && !antialias && !fill.texture().hasAlpha())
         has_fill_texture = false;
 
-    bool antialias = render_hints & QPainter::Antialiasing;
     if (X11->use_xrender
         && picture
         && !has_fill_pattern
