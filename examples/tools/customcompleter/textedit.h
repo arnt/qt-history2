447df226dc98aef0b323685467444aace4642ee0
harald <harald@harald-morpork>
2006-08-27 13:23:19 +0100

    p4i integration Integrate 233922 from 4.2 to main: Fixes:    add missing license headers
    [git-p4: depot-paths = "//depot/qt/main/": change = 233923]

--- examples/tools/customcompleter/textedit.h
+++ examples/tools/customcompleter/textedit.h
@@ -1,3 +1,16 @@
+/****************************************************************************
+**
+** Copyright (C) 2006-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
 #ifndef TEXTEDIT_H
 #define TEXTEDIT_H
 
@@ -15,7 +28,7 @@ public:
 
     void setCompleter(QCompleter *c);
     QCompleter *completer() const;
-    
+
 protected:
     void keyPressEvent(QKeyEvent *e);
 
