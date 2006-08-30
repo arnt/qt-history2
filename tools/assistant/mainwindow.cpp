0e78d71c896cf111e0d639c7b8679339eda8c3bd
Trenton Schulz <twschulz@twschulz-rain>
2006-08-30 17:57:01 +0100

    p4i integration Integrate 234420 from 4.2 to main: Fixes:    about dialog in assitant looks awful Task:     127963 RevBy:	  Done with janarve Details:  The text was manually formatted. Fixed to use standard html formatting. Additionally, we splitted out the translations into more sensible parts.
    [git-p4: depot-paths = "//depot/qt/main/": change = 234479]

--- tools/assistant/mainwindow.cpp
+++ tools/assistant/mainwindow.cpp
@@ -249,32 +249,34 @@ void MainWindow::closeEvent(QCloseEvent *e)
 void MainWindow::about()
 {
     QMessageBox box(this);
-    QString edition =
 #if QT_EDITION == QT_EDITION_OPENSOURCE
-                   tr(" Open Source Edition</center><p>"
-                   "This version of Qt Assistant is part of the Qt Open Source Edition, for use "
-                   "in the development of Open Source applications. "
-                   "Qt is a comprehensive C++ framework for cross-platform application "
-                   "development.<br/><br/>"
-                   "You need a commercial Qt license for development of proprietary (closed "
-                   "source) applications. Please see <tt>http://www.trolltech.com/company/model"
-                   ".html</tt> for an overview of Qt licensing.");
+    QString edition = tr("Open Source Edition");
+    QString info = tr("This version of Qt Assistant is part of the Qt Open Source Edition, for use "
+                      "in the development of Open Source applications. "
+                      "Qt is a comprehensive C++ framework for cross-platform application "
+                      "development.");
+    QString moreInfo = tr("You need a commercial Qt license for development of proprietary (closed "
+                   "source) applications. Please see <a href=\"http://www.trolltech.com/company/model"
+                   ".html\">www.trolltech.com/company/model.html</a> for an overview of Qt licensing.");
 #else 
-                   tr("</center><p>This program is licensed to you under the terms of the "
+    QString edition;
+    QString info;
+    QString moreInfo(tr("This program is licensed to you under the terms of the "
                    "Qt Commercial License Agreement. For details, see the file LICENSE "
-                   "that came with this software distribution.");
+                   "that came with this software distribution."));
 
 #endif
 
-    box.setText(tr("<center><img src=\":/trolltech/assistant/images/assistant-128.png\">"
+    box.setText(QString("<center><img src=\":/trolltech/assistant/images/assistant-128.png\">"
                    "<h3>%1</h3>"
-                   "<br/>Version %2"
-                   "%3"
-                   "<br/><br/>Copyright (C) 2000-$THISYEAR$ $TROLLTECH$. All rights reserved."
-                   "<br/><br/>The program is provided AS IS with NO WARRANTY OF ANY KIND,"
+                   "<p>Version %2 %3</p></center>"
+                   "<p>%4</p>"
+                   "<p>%5</p>"
+                   "<p>Copyright (C) 2000-$THISYEAR$ $TROLLTECH$. All rights reserved.</p>"
+                   "<p>The program is provided AS IS with NO WARRANTY OF ANY KIND,"
                    " INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A"
-                   " PARTICULAR PURPOSE.<br/> ")
-                   .arg(tr("Qt Assistant")).arg(QT_VERSION_STR).arg(edition));
+                   " PARTICULAR PURPOSE.<p/>")
+                   .arg(tr("Qt Assistant")).arg(QT_VERSION_STR).arg(edition).arg(info).arg(moreInfo));
     box.setWindowTitle(tr("Qt Assistant"));
     box.setIcon(QMessageBox::NoIcon);
     box.exec();
