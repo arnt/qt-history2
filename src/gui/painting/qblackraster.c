f0b93879ab183258145fbfc8ed3d9222ebbcf670
gunnar <gunnar@THAUMASIA>
2006-09-04 13:44:22 +0100

    p4i integration Integrate 234811 from 4.2 to main: Fixes:    Precision problems in the black raster. Task:     128227 RevBy:    Trond Details: I had previously set the precision too high, so reduce it to a value that works...
    [git-p4: depot-paths = "//depot/qt/main/": change = 234813]

--- src/gui/painting/qblackraster.c
+++ src/gui/painting/qblackraster.c
@@ -370,8 +370,8 @@ typedef struct  TBand_
 
 typedef struct TRaster_Instance_  TRaster_Instance;
 
-#define precision_bits 16
-#define precision_step 16384
+#define precision_bits 12
+#define precision_step (1 << (precision_bits - 1))
 #define precision (1 << precision_bits)
 #define precision_shift (precision_bits - Pixel_Bits)
 
