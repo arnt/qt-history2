0098530c709a30ac12457f341883487a9f706928
khansen <khansen@khansen-scrabble>
2006-09-04 15:19:37 +0100

    p4i integration Integrate 234889 from 4.2 to main: Fixes:    compile
    [git-p4: depot-paths = "//depot/qt/main/": change = 234890]

--- tests/auto/qmovie/tst_qmovie.cpp
+++ tests/auto/qmovie/tst_qmovie.cpp
@@ -51,9 +51,9 @@ void tst_QMovie::getSetCheck()
     // void QMovie::setDevice(QIODevice *)
     QFile *var1 = new QFile;
     obj1.setDevice(var1);
-    QCOMPARE(var1, obj1.device());
+    QCOMPARE(obj1.device(), (QIODevice *)var1);
     obj1.setDevice((QIODevice *)0);
-    QCOMPARE((QIODevice *)0, obj1.device());
+    QCOMPARE(obj1.device(), (QIODevice *)0);
     delete var1;
 
     // CacheMode QMovie::cacheMode()
