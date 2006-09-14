772c8eefc0af5c5208b5f2c8a89d8a191cb5da94
harald <harald@harald-morpork>
2006-09-14 10:35:20 +0100

    p4i integration Integrate 236281 from 4.2 to main: Username: harald Fixes:    license headers
    [git-p4: depot-paths = "//depot/qt/main/": change = 236282]

--- src/opengl/qegl_qws_p.h
+++ src/opengl/qegl_qws_p.h
@@ -11,6 +11,17 @@
 **
 ****************************************************************************/
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of QEGL.  This header file may change from
+// version to version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #ifndef QEGL_H
 #define QEGL_H
 
