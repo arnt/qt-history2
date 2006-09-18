b1a43c5d912ca6831ef329c90388b16d3abebfbd
jsurazsk <jsurazsk@bobo>
2006-09-18 15:51:53 +0100

    p4i integration Integrate 236735 from 4.2 to main: Fixes:    tst_qdatetimeedit failures Task:     130629 RevBy:    jbache AutoTest: tst_qdatetimeedit Details:  the locale returned by QLocale::system() should be chosen by                  GetThreadLocale(), not LOCALE_USER_DEFAULT.
    [git-p4: depot-paths = "//depot/qt/main/": change = 236736]

--- src/corelib/tools/qlocale.cpp
+++ src/corelib/tools/qlocale.cpp
@@ -325,16 +325,17 @@ static QByteArray envVarLocale()
 */
 
 static const char *winLangCodeToIsoName(int code);
-static QString winIso639LangName(LCID id = LOCALE_USER_DEFAULT);
-static QString winIso3116CtryName(LCID id = LOCALE_USER_DEFAULT);
+static QString winIso639LangName(LCID id);
+static QString winIso3116CtryName(LCID id);
 
 static QString getWinLocaleInfo(LCTYPE type)
 {
     int cnt = 0;
+    LCID id = GetThreadLocale();
     QT_WA({
-        cnt = GetLocaleInfoW(LOCALE_USER_DEFAULT, type, 0, 0)*2;
+        cnt = GetLocaleInfoW(id, type, 0, 0)*2;
     } , {
-        cnt = GetLocaleInfoA(LOCALE_USER_DEFAULT, type, 0, 0);
+        cnt = GetLocaleInfoA(id, type, 0, 0);
     });
 
     if (cnt == 0) {
@@ -345,11 +346,11 @@ static QString getWinLocaleInfo(LCTYPE type)
     QByteArray buff(cnt, 0);
 
     QT_WA({
-        cnt = GetLocaleInfoW(LOCALE_USER_DEFAULT, type,
+        cnt = GetLocaleInfoW(id, type,
                                 reinterpret_cast<wchar_t*>(buff.data()),
                                 buff.size()/2);
     } , {
-        cnt = GetLocaleInfoA(LOCALE_USER_DEFAULT, type,
+        cnt = GetLocaleInfoA(id, type,
                                 buff.data(), buff.size());
     });
 
@@ -367,7 +368,7 @@ static QString getWinLocaleInfo(LCTYPE type)
     return result;
 }
 
-QByteArray getWinLocaleName(LCID id = LOCALE_USER_DEFAULT)
+QByteArray getWinLocaleName(LCID id)
 {
     QByteArray result;
     if (id == LOCALE_USER_DEFAULT) {
@@ -477,13 +478,15 @@ static QString winDateToString(const QDate &date, DWORD flags)
     st.wMonth = date.month();
     st.wDay = date.day();
 
+    LCID id = GetThreadLocale();
+
     QT_WA({
         TCHAR buf[255];
-        if (GetDateFormatW(LOCALE_USER_DEFAULT, flags, &st, 0, buf, 255))
+        if (GetDateFormatW(id, flags, &st, 0, buf, 255))
             return QString::fromUtf16((ushort*)buf);
     } , {
         char buf[255];
-        if (GetDateFormatA(LOCALE_USER_DEFAULT, flags, &st, 0, (char*)&buf, 255))
+        if (GetDateFormatA(id, flags, &st, 0, (char*)&buf, 255))
             return QString::fromLocal8Bit(buf);
     });
 
@@ -500,14 +503,15 @@ static QString winTimeToString(const QTime &time)
     st.wMilliseconds = 0;
 
     DWORD flags = 0;
+    LCID id = GetThreadLocale();
 
     QT_WA({
         TCHAR buf[255];
-        if (GetTimeFormatW(LOCALE_USER_DEFAULT, flags, &st, 0, buf, 255))
+        if (GetTimeFormatW(id, flags, &st, 0, buf, 255))
             return QString::fromUtf16((ushort*)buf);
     } , {
         char buf[255];
-        if (GetTimeFormatA(LOCALE_USER_DEFAULT, flags, &st, 0, (char*)&buf, 255))
+        if (GetTimeFormatA(id, flags, &st, 0, (char*)&buf, 255))
             return QString::fromLocal8Bit(buf);
     });
 
@@ -557,7 +561,7 @@ static QString winMonthName(int month, bool short_format)
 
 QLocale QSystemLocale::fallbackLocale() const
 {
-    return QLocale(getWinLocaleName());
+    return QLocale(getWinLocaleName(GetThreadLocale()));
 }
 
 QVariant QSystemLocale::query(QueryType type, QVariant in = QVariant()) const
@@ -609,7 +613,7 @@ QVariant QSystemLocale::query(QueryType type, QVariant in = QVariant()) const
 
     case LanguageId:
     case CountryId: {
-        QString locale = getWinLocaleName();
+        QString locale = getWinLocaleName(GetThreadLocale());
         QLocale::Language lang;
         QLocale::Country cntry;
         getLangAndCountry(locale, lang, cntry);
