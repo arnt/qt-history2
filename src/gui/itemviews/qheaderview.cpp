b1c5e835672db84fa81b97539f0a3646cd312bfd
mmonsen <mmonsen@trolltech.com>
2006-10-05 13:08:19 +0100

    p4i integration Integrate 238527 from 4.2 to main: Fixes:    doc fix Task:     132795 RevBy:    trustme Details:  explain that the stretchLastSection property overrides the 	  resize mode set on the last section
    [git-p4: depot-paths = "//depot/qt/main/": change = 238531]

--- src/gui/itemviews/qheaderview.cpp
+++ src/gui/itemviews/qheaderview.cpp
@@ -2397,8 +2397,9 @@ bool QHeaderViewPrivate::isSectionSelected(int section) const
   Interactive - the user decides the size
   Stretch - take up whatever space is left
   Fixed - the size is set programatically outside the header
+  ResizeToContentes - the size is set based on the contents of the row or column in the parent view
 
-  The globalMode will not affect the last section if stretchLastSection is true.
+  The resize mode will not affect the last section if stretchLastSection is true.
  */
 void QHeaderViewPrivate::resizeSections(QHeaderView::ResizeMode globalMode, bool useGlobalMode)
 {
