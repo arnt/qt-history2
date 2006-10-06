f258a0b36d18d25a051ea59c41a3138f6b026cbb
hwall <hwall@hwall-extra>
2006-10-06 15:25:14 +0100

    Fixes:    Add missing file in change 238712
    [git-p4: depot-paths = "//depot/qt/main/": change = 238713]

---
+++ src/gui/embedded/qscreenvnc_p.h
@@ -0,0 +1,243 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef QSCREENVNC_P_H
+#define QSCREENVNC_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of the QLibrary class.  This header file may change from version to version
+// without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "qscreenvnc_qws.h"
+
+#ifndef QT_NO_QWS_VNC
+
+#include <QtNetwork/qtcpsocket.h>
+#include <QtNetwork/qtcpserver.h>
+#include <private/qsharedmemory_p.h>
+
+#ifndef QT_NO_QWS_CURSOR
+class QVNCCursor : public QScreenCursor
+{
+public:
+    QVNCCursor(QVNCScreenPlugin *s) : screen(s) { hwaccel = true; }
+    ~QVNCCursor() {}
+
+    void hide();
+    void show();
+    void set(const QImage &image, int hotx, int hoty);
+    void move(int x, int y);
+
+private:
+    QVNCScreenPlugin *screen;
+};
+#endif // QT_NO_QWS_CURSOR
+
+#define MAP_TILE_SIZE 16
+#define MAP_WIDTH 1280 / MAP_TILE_SIZE
+#define MAP_HEIGHT 1024 / MAP_TILE_SIZE
+#define UPDATE_FREQUENCY 40
+
+struct QVNCHeader
+{
+    bool dirty;
+    uchar map[MAP_HEIGHT][MAP_WIDTH];
+};
+
+class QRfbRect
+{
+public:
+    QRfbRect() {}
+    QRfbRect(quint16 _x, quint16 _y, quint16 _w, quint16 _h) {
+        x = _x; y = _y; w = _w; h = _h;
+    }
+
+    void read(QTcpSocket *s);
+    void write(QTcpSocket *s);
+
+    quint16 x;
+    quint16 y;
+    quint16 w;
+    quint16 h;
+};
+
+class QRfbPixelFormat
+{
+public:
+    static int size() { return 16; }
+
+    void read(QTcpSocket *s);
+    void write(QTcpSocket *s);
+
+    int bitsPerPixel;
+    int depth;
+    bool bigEndian;
+    bool trueColor;
+    int redBits;
+    int greenBits;
+    int blueBits;
+    int redShift;
+    int greenShift;
+    int blueShift;
+};
+
+class QRfbServerInit
+{
+public:
+    QRfbServerInit() { name = 0; }
+    ~QRfbServerInit() { delete[] name; }
+
+    int size() const { return QRfbPixelFormat::size() + 8 + strlen(name); }
+    void setName(const char *n);
+
+    void read(QTcpSocket *s);
+    void write(QTcpSocket *s);
+
+    quint16 width;
+    quint16 height;
+    QRfbPixelFormat format;
+    char *name;
+};
+
+class QRfbSetEncodings
+{
+public:
+    bool read(QTcpSocket *s);
+
+    quint16 count;
+};
+
+class QRfbFrameBufferUpdateRequest
+{
+public:
+    bool read(QTcpSocket *s);
+
+    char incremental;
+    QRfbRect rect;
+};
+
+class QRfbKeyEvent
+{
+public:
+    bool read(QTcpSocket *s);
+
+    char down;
+    int  keycode;
+    int  unicode;
+};
+
+class QRfbPointerEvent
+{
+public:
+    bool read(QTcpSocket *s);
+
+    uint buttons;
+    quint16 x;
+    quint16 y;
+};
+
+class QRfbClientCutText
+{
+public:
+    bool read(QTcpSocket *s);
+
+    quint32 length;
+};
+
+class QVNCServerPlugin;
+
+class QVNCScreenPluginPrivate
+{
+public:
+    QVNCScreenPluginPrivate(QVNCScreenPlugin *parent);
+    ~QVNCScreenPluginPrivate();
+
+    bool doOnScreenSurface;
+    QVNCHeader hdr;
+    QVNCServerPlugin *vncServer;
+    QScreen *subscreen;
+
+#ifndef QT_NO_QWS_MULTIPROCESS
+    QSharedMemory *shm;
+#endif
+
+    QVNCScreenPlugin *q_ptr;
+};
+
+class QVNCServerPlugin : public QObject
+{
+    Q_OBJECT
+public:
+    QVNCServerPlugin(QVNCScreenPlugin *screen);
+    QVNCServerPlugin(QVNCScreenPlugin *screen, int id);
+    ~QVNCServerPlugin();
+
+    enum ClientMsg { SetPixelFormat = 0,
+                     FixColourMapEntries = 1,
+                     SetEncodings = 2,
+                     FramebufferUpdateRequest = 3,
+                     KeyEvent = 4,
+                     PointerEvent = 5,
+                     ClientCutText = 6 };
+
+    enum ServerMsg { FramebufferUpdate = 0,
+                     SetColourMapEntries = 1 };
+
+private:
+    void setPixelFormat();
+    void setEncodings();
+    void frameBufferUpdateRequest();
+    void pointerEvent();
+    void keyEvent();
+    void clientCutText();
+    bool checkFill(const uchar *data, int width, int height, int stride);
+    void convertPixels(char *dst, const char *src, int count);
+    void sendHextile();
+    void sendRaw();
+    inline QVNCHeader* header() { return &qvnc_screen->d_ptr->hdr; }
+
+private slots:
+    void newConnection();
+    void readClient();
+    void checkUpdate();
+    void discardClient();
+
+private:
+    void init(uint port);
+    enum ClientState { Protocol, Init, Connected };
+    QTimer *timer;
+    QTcpServer *serverSocket;
+    QTcpSocket *client;
+    ClientState state;
+    quint8 msgType;
+    bool handleMsg;
+    QRfbPixelFormat pixelFormat;
+    Qt::KeyboardModifiers keymod;
+    int encodingsPending;
+    int cutTextPending;
+    bool supportHextile;
+    bool wantUpdate;
+    int nibble;
+    bool sameEndian;
+    QVNCScreenPlugin *qvnc_screen;
+};
+
+#endif // QT_NO_QWS_VNC
+#endif // QSCREENVNC_P_H
