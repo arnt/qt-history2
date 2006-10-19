6395e3871faa80109cc99890e42ed68b1828236f
Brad Hughes <bhughes@trolltech.com>
2006-10-19 07:24:18 +0100

    test for task 123923 (works, yay!)
    [git-p4: depot-paths = "//depot/qt/main/": change = 239764]

--- tests/auto/qcoreapplication/tst_qcoreapplication.cpp
+++ tests/auto/qcoreapplication/tst_qcoreapplication.cpp
@@ -15,12 +15,43 @@ class tst_QCoreApplication: public QObject
     Q_OBJECT
 private slots:
     void qAppName();
+    void argc();
 };
 
 void tst_QCoreApplication::qAppName()
 {
+    int argc = 1;
+    char *argv[] = { "tst_qcoreapplication" };
+    QCoreApplication app(argc, argv);
     QVERIFY(!::qAppName().isEmpty());
 }
 
-QTEST_MAIN(tst_QCoreApplication)
+void tst_QCoreApplication::argc()
+{
+    {
+        int argc = 1;
+        char *argv[] = { "tst_qcoreapplication" };
+        QCoreApplication app(argc, argv);
+        QCOMPARE(argc, 1);
+        QCOMPARE(app.argc(), 1);
+    }
+
+    {
+        int argc = 4;
+        char *argv[] = { "tst_qcoreapplication", "arg1", "arg2", "arg3" };
+        QCoreApplication app(argc, argv);
+        QCOMPARE(argc, 4);
+        QCOMPARE(app.argc(), 4);
+    }
+
+    {
+        int argc = 0;
+        char **argv = 0;
+        QCoreApplication app(argc, argv);
+        QCOMPARE(argc, 0);
+        QCOMPARE(app.argc(), 0);
+    }
+}
+
+QTEST_APPLESS_MAIN(tst_QCoreApplication)
 #include "tst_qcoreapplication.moc"
