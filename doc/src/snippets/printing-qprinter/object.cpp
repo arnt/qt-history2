65640c450b7e0263915236c23ce8c0d052868fbe
dboddie <dboddie@mikaeld-lurven>
2006-10-27 16:02:56 +0100

    p4i integration Integrate 240666 from 4.2 to main: Fixes:    Doc: Added a code snippet to show the basics of printing. RevBy:    -
    [git-p4: depot-paths = "//depot/qt/main/": change = 240667]

---
+++ doc/src/snippets/printing-qprinter/object.cpp
@@ -0,0 +1,29 @@
+#include <QtGui>
+#include "object.h"
+
+Object::Object(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void Object::print()
+{
+    int numberOfPages = 10;
+    int lastPage = numberOfPages - 1;
+
+    QPrinter printer(QPrinter::HighResolution);
+    printer.setOutputFileName("print.ps");
+    QPainter painter;
+    painter.begin(&printer);
+
+    for (int page = 0; page < numberOfPages; ++page) {
+
+        // Use the painter to draw on the page.
+
+        if (page != lastPage)
+            printer.newPage();
+    }
+
+    painter.end();
+    qApp->quit();
+}
