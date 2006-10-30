d6ca90001022d132d75286215804ab22d85a1f7a
Trenton Schulz <twschulz@twschulz-rain>
2006-10-30 16:43:49 +0100

    Fixes:    Make macstyle compile in 64-bit mode
    [git-p4: depot-paths = "//depot/qt/main/": change = 240774]

--- src/gui/styles/qmacstyle_mac.cpp
+++ src/gui/styles/qmacstyle_mac.cpp
@@ -1359,6 +1359,7 @@ void QMacStyle::polish(QPalette &pal)
 {
     QPixmap px(200, 200);
     QColor pc(Qt::black);
+#if !__LP64__
     {
 #ifndef QT_MAC_NO_QUICKDRAW
         QMacSavedPortInfo port(&px);
@@ -1370,6 +1371,15 @@ void QMacStyle::polish(QPalette &pal)
         pc = QColor(c.red / 256, c.green / 256, c.blue / 256);
 #endif
     }
+#else
+    {
+        // Use the new stuff from 10.4.
+        QPainter painter(&px);
+        QMacCGContext cg(&painter);
+        HIThemeSetFill(kThemeBrushDialogBackgroundActive, 0, cg, kHIThemeOrientationNormal);
+        CGContextFillRect(cg, CGRectMake(0, 0, 200, 200));
+    }
+#endif
     QBrush background(pc, px);
     pal.setBrush(QPalette::All, QPalette::Window, background);
     pal.setBrush(QPalette::All, QPalette::Button, background);
