6abdd25694525b0eaeeb95908a746cbff13cc5de
ahanssen <ahanssen@:w>
2006-11-06 16:50:29 +0100

    Fixes:    QFtp tolerance for various FTP servers wrt the LIST command. Task:     107381 Details:  QFtp gets a new powerful LIST parser. This is as far as we can 	  go with the current architecture. A "proper" solution is to use 	  different list commands for different types of servers, to ensure 	  that we get the maximum amount of information that the server 	  can provide. This requires heavy refactoring though.
    	  Tested manually with a number of servers from ftp-sites.org,
    	  including the one in the task: cdhnow.com, and eps.unm.edu (DOS),
    	  aixpdslib.seas.ucla.edu (aix), ftp.arsc.edu (winnt), and
    	  ftp.dina.kvl.dk (bsd).
    
    [git-p4: depot-paths = "//depot/qt/main/": change = 241516]

--- src/network/qftp.cpp
+++ src/network/qftp.cpp
@@ -69,7 +69,7 @@ public:
 
     void abortConnection();
 
-    static bool parseDir(const QString &buffer, const QString &userName, QUrlInfo *info);
+    static bool parseDir(const QByteArray &buffer, const QString &userName, QUrlInfo *info);
 
 signals:
     void listInfo(const QUrlInfo&);
@@ -421,139 +421,175 @@ void QFtpDTP::abortConnection()
         socket->abort();
 }
 
-bool QFtpDTP::parseDir(const QString &buffer, const QString &userName, QUrlInfo *info)
+static void _q_fixupDateTime(QDateTime *dateTime)
 {
-    QStringList lst = buffer.simplified().split(QLatin1String(" "));
+    // Adjust for future tolerance.
+    const int futureTolerance = 86400;
+    if (dateTime->secsTo(QDateTime::currentDateTime()) < -futureTolerance) {
+        QDate d = dateTime->date();
+        d.setYMD(d.year() - 1, d.month(), d.day());
+        dateTime->setDate(d);
+    }
+}
 
-    if (lst.count() < 9)
-        return false;
+static void _q_parseUnixDir(const QStringList &tokens, const QString &userName, QUrlInfo *info)
+{
+    // Unix style, 7 + 1 entries
+    // -rw-r--r--    1 ftp      ftp      17358091 Aug 10  2004 qt-x11-free-3.3.3.tar.gz
+    // drwxr-xr-x    3 ftp      ftp          4096 Apr 14  2000 compiled-examples
+    // lrwxrwxrwx    1 ftp      ftp             9 Oct 29  2005 qtscape -> qtmozilla
+    if (tokens.size() != 8)
+        return;
 
-    QString tmp;
-
-    // permissions
-    tmp = lst[0];
-
-    if (tmp[0] == QLatin1Char('d')) {
+    char first = tokens.at(1).at(0).toLatin1();
+    if (first == 'd') {
         info->setDir(true);
         info->setFile(false);
         info->setSymLink(false);
-    } else if (tmp[0] == QLatin1Char('-')) {
+    } else if (first == '-') {
         info->setDir(false);
         info->setFile(true);
         info->setSymLink(false);
-    } else if (tmp[0] == QLatin1Char('l')) {
+    } else if (first == 'l') {
         info->setDir(true);
         info->setFile(false);
         info->setSymLink(true);
+    }
+
+    // Resolve filename
+    QString name = tokens.at(7);
+    if (info->isSymLink()) {
+        int linkPos = name.indexOf(QLatin1String(" ->"));
+        if (linkPos != -1)
+            name.resize(linkPos);
+    }
+    info->setName(name);
+
+    // Resolve owner & group
+    info->setOwner(tokens.at(3));
+    info->setGroup(tokens.at(4));
+
+    // Resolve size
+    info->setSize(tokens.at(5).toLongLong());
+
+    QStringList formats;
+    formats << "MMM dd  yyyy" << "MMM dd hh:mm" << "MMM  d  yyyy"
+            << "MMM  d hh:mm" << "MMM  d yyyy" << "MMM dd yyyy";
+
+    QString dateString = tokens.at(6);
+    dateString[0] = dateString[0].toUpper();
+
+    // Resolve the modification date by parsing all possible formats
+    QDateTime dateTime;
+    int n = 0;
+    do {
+        dateTime = QDateTime::fromString(dateString, formats.at(n++));
+    }  while (n < formats.size() && (!dateTime.isValid()));
+
+    if (n == 2 || n == 4) {
+        // Guess the year.
+        dateTime.setDate(QDate(QDate::currentDate().year(),
+                               dateTime.date().month(),
+                               dateTime.date().day()));
+        _q_fixupDateTime(&dateTime);
+    }
+    if (dateTime.isValid())
+        info->setLastModified(dateTime);
+
+    // Resolve permissions
+    int permissions = 0;
+    QString p = tokens.at(2);
+    permissions |= (p[0] == QLatin1Char('r') ? QUrlInfo::ReadOwner : 0);
+    permissions |= (p[1] == QLatin1Char('w') ? QUrlInfo::WriteOwner : 0);
+    permissions |= (p[2] == QLatin1Char('x') ? QUrlInfo::ExeOwner : 0);
+    permissions |= (p[3] == QLatin1Char('r') ? QUrlInfo::ReadGroup : 0);
+    permissions |= (p[4] == QLatin1Char('w') ? QUrlInfo::WriteGroup : 0);
+    permissions |= (p[5] == QLatin1Char('x') ? QUrlInfo::ExeGroup : 0);
+    permissions |= (p[6] == QLatin1Char('r') ? QUrlInfo::ReadOther : 0);
+    permissions |= (p[7] == QLatin1Char('w') ? QUrlInfo::WriteOther : 0);
+    permissions |= (p[8] == QLatin1Char('x') ? QUrlInfo::ExeOther : 0);
+    info->setPermissions(permissions);
+
+    bool isOwner = info->owner() == userName;
+    info->setReadable((permissions & QUrlInfo::ReadOther) || ((permissions & QUrlInfo::ReadOwner) && isOwner));
+    info->setWritable((permissions & QUrlInfo::WriteOther) || ((permissions & QUrlInfo::WriteOwner) && isOwner));
+}
+
+static void _q_parseDosDir(const QStringList &tokens, const QString &userName, QUrlInfo *info)
+{
+    // DOS style, 3 + 1 entries
+    // 01-16-02  11:14AM       <DIR>          epsgroup
+    // 06-05-03  03:19PM                 1973 readme.txt
+    if (tokens.size() != 4)
+        return;
+
+    Q_UNUSED(userName);
+
+    QString name = tokens.at(3);
+    info->setName(name);
+    info->setSymLink(name.toLower().endsWith(".lnk"));
+
+    if (tokens.at(2) == QLatin1String("<DIR>")) {
+        info->setFile(false);
+        info->setDir(true);
     } else {
+        info->setFile(true);
+        info->setDir(false);
+        info->setSize(tokens.at(2).toLongLong());
+    }
+
+    // Note: We cannot use QFileInfo; permissions are for the server-side
+    // machine, and QFileInfo's behavior depends on the local platform.
+    int permissions = QUrlInfo::ReadOwner | QUrlInfo::WriteOwner
+                      | QUrlInfo::ReadGroup | QUrlInfo::WriteGroup
+                      | QUrlInfo::ReadOther | QUrlInfo::WriteOther;
+    QString ext;
+    int extIndex = name.lastIndexOf(QLatin1Char('.'));
+    if (extIndex != -1)
+        ext = name.mid(extIndex + 1);
+    if (ext == QLatin1String("exe") || ext == QLatin1String("bat") || ext == QLatin1String("com"))
+        permissions |= QUrlInfo::ExeOwner | QUrlInfo::ExeGroup | QUrlInfo::ExeOther;
+    info->setPermissions(permissions);
+
+    info->setReadable(true);
+    info->setWritable(info->isFile());
+
+    QDateTime dateTime = QDateTime::fromString(tokens.at(1), QLatin1String("MM-dd-yy  hh:mmAP"));
+    if (dateTime.date().year() < 1971) {
+        dateTime.setDate(QDate(dateTime.date().year() + 100,
+                               dateTime.date().month(),
+                               dateTime.date().day()));
+    }
+
+    info->setLastModified(dateTime);
+    
+}
+
+bool QFtpDTP::parseDir(const QByteArray &buffer, const QString &userName, QUrlInfo *info)
+{
+    if (buffer.isEmpty())
         return false;
+
+    QString bufferStr = QString::fromLatin1(buffer).trimmed();
+
+    // Unix style FTP servers
+    QRegExp unixPattern(QLatin1String("^([\\-dl])([a-zA-Z\\-]{9,9})\\s+\\d+\\s+(\\S*)\\s+"
+                                      "(\\S*)\\s+(\\d+)\\s+(\\S+\\s+\\S+\\s+\\S+)\\s+(\\S.*)"));
+    if (unixPattern.indexIn(bufferStr) == 0) {
+        _q_parseUnixDir(unixPattern.capturedTexts(), userName, info);
+        return true;
     }
 
-    static int user = 0;
-    static int group = 1;
-    static int other = 2;
-    static int readable = 0;
-    static int writable = 1;
-    static int executable = 2;
-
-    bool perms[3][3];
-    perms[0][0] = (tmp[1] == QLatin1Char('r'));
-    perms[0][1] = (tmp[2] == QLatin1Char('w'));
-    perms[0][2] = (tmp[3] == QLatin1Char('x'));
-    perms[1][0] = (tmp[4] == QLatin1Char('r'));
-    perms[1][1] = (tmp[5] == QLatin1Char('w'));
-    perms[1][2] = (tmp[6] == QLatin1Char('x'));
-    perms[2][0] = (tmp[7] == QLatin1Char('r'));
-    perms[2][1] = (tmp[8] == QLatin1Char('w'));
-    perms[2][2] = (tmp[9] == QLatin1Char('x'));
-
-    // owner
-    tmp = lst[2];
-    info->setOwner(tmp);
-
-    // group
-    tmp = lst[3];
-    info->setGroup(tmp);
-
-    // detect permissions
-    info->setWritable((userName == info->owner() && perms[user][writable]) ||
-        perms[other][writable]);
-    info->setReadable((userName == info->owner() && perms[user][readable]) ||
-        perms[other][readable]);
-
-    int p = 0;
-    if (perms[user][readable])
-        p |= QUrlInfo::ReadOwner;
-    if (perms[user][writable])
-        p |= QUrlInfo::WriteOwner;
-    if (perms[user][executable])
-        p |= QUrlInfo::ExeOwner;
-    if (perms[group][readable])
-        p |= QUrlInfo::ReadGroup;
-    if (perms[group][writable])
-        p |= QUrlInfo::WriteGroup;
-    if (perms[group][executable])
-        p |= QUrlInfo::ExeGroup;
-    if (perms[other][readable])
-        p |= QUrlInfo::ReadOther;
-    if (perms[other][writable])
-        p |= QUrlInfo::WriteOther;
-    if (perms[other][executable])
-        p |= QUrlInfo::ExeOther;
-    info->setPermissions(p);
-
-    // size
-    tmp = lst[4];
-    info->setSize(tmp.toLongLong());
-
-    // date and time
-    QTime time;
-    QString dateStr;
-    dateStr += QLatin1String("Sun ");
-    lst[5][0] = lst[5][0].toUpper();
-    dateStr += lst[5];
-    dateStr += QLatin1Char(' ');
-    dateStr += lst[6];
-    dateStr += QLatin1Char(' ');
-
-    if (lst[7].contains(QLatin1Char(':'))) {
-        time = QTime(lst[7].left(2).toInt(), lst[7].right(2).toInt());
-        dateStr += QString::number(QDate::currentDate().year());
-    } else {
-        dateStr += lst[7];
+    // DOS style FTP servers
+    QRegExp dosPattern(QLatin1String("^(\\d\\d-\\d\\d-\\d\\d\\ \\ \\d\\d:\\d\\d[AP]M)\\s+"
+                                     "(<DIR>|\\d+)\\s+(\\S.*)$"));
+    if (dosPattern.indexIn(bufferStr) == 0) {
+        _q_parseDosDir(dosPattern.capturedTexts(), userName, info);
+        return true;
     }
 
-#ifndef QT_NO_DATESTRING
-    QDate date = QDate::fromString(dateStr);
-    info->setLastModified(QDateTime(date, time));
-#endif
-
-    if (lst[7].contains(QLatin1Char(':'))) {
-        // if the year-field is missing, check the modification date/time of
-        // the file and compare to "now". If the file was changed in the
-        // "future", also considering a possible 24 hour time zone gap, then
-        // we assume it was changed a year ago.
-        const int futureTolerance = 86400;
-        if(info->lastModified().secsTo(QDateTime::currentDateTime()) < -futureTolerance) {
-            QDateTime dt = info->lastModified();
-            QDate d = dt.date();
-            d.setYMD(d.year()-1, d.month(), d.day());
-            dt.setDate(d);
-            info->setLastModified(dt);
-        }
-    }
-
-    // name
-    if (info->isSymLink())
-        info->setName(lst[8].trimmed());
-    else {
-        QString n;
-        for (int i = 8; i < lst.count(); ++i)
-            n += lst[i] + QLatin1Char(' ');
-        n = n.trimmed();
-        info->setName(n);
-    }
-    return true;
+    // Unsupported
+    return false;
 }
 
 void QFtpDTP::socketConnected()
@@ -588,9 +624,9 @@ void QFtpDTP::socketReadyRead()
     if (pi->currentCommand().startsWith(QLatin1String("LIST"))) {
         while (socket->canReadLine()) {
             QUrlInfo i;
-            QString line = QString::fromAscii(socket->readLine());
+            QByteArray line = socket->readLine();
 #if defined(QFTPDTP_DEBUG)
-            qDebug("QFtpDTP read (list): '%s'", line.toLatin1().constData());
+            qDebug("QFtpDTP read (list): '%s'", line.constData());
 #endif
             if (parseDir(line, QLatin1String(""), &i)) {
                 emit listInfo(i);
@@ -598,8 +634,8 @@ void QFtpDTP::socketReadyRead()
                 // some FTP servers don't return a 550 if the file or directory
                 // does not exist, but rather write a text to the data socket
                 // -- try to catch these cases
-                if (line.endsWith(QLatin1String("No such file or directory\r\n")))
-                    err = line;
+                if (line.endsWith("No such file or directory\r\n"))
+                    err = QString::fromLatin1(line);
             }
         }
     } else {
