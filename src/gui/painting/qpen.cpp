71cb33bb0bb2b7e7b770d69482a64e88fcee8adb
dboddie <dboddie@mikaeld-lurven>
2006-11-30 12:28:02 +0100

    p4i integration Integrate 244173 from 4.2 to main: Fixes:    Doc: Fixed link. RevBy:    Mark Summerfield
    [git-p4: depot-paths = "//depot/qt/main/": change = 244174]

--- src/gui/painting/qpen.cpp
+++ src/gui/painting/qpen.cpp
@@ -75,7 +75,7 @@
     convenience functions to extract and set the color of the pen's
     brush, respectively. Pens may also be compared and streamed.
 
-    For more information about painting in general, see {The Paint
+    For more information about painting in general, see \l{The Paint
     System} documentation.
 
     \tableofcontents
