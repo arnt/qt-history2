d3e3120d4f6384e28bbd00ec1ed8afbc156e79e4
Girish Ramakrishnan <gramakri@trolltech.com>
2006-11-30 17:42:12 +0100

    Fixes:    relayout tabwidget when a corner widget is set Task:     124987
    [git-p4: depot-paths = "//depot/qt/main/": change = 244236]

--- src/gui/widgets/qtabwidget.cpp
+++ src/gui/widgets/qtabwidget.cpp
@@ -462,7 +462,8 @@ void QTabWidget::setTabEnabled(int index, bool enable)
   \fn void QTabWidget::setCornerWidget(QWidget *widget, Qt::Corner corner)
 
   Sets the given \a widget to be shown in the specified \a corner of the
-  tab widget.
+  tab widget. The geometry of the widget is determined based on the widget's
+  sizeHint() and the style().
 
   Only the horizontal element of the \a corner will be used.
 
@@ -480,6 +481,7 @@ void QTabWidget::setCornerWidget(QWidget * w, Qt::Corner corner)
         d->rightCornerWidget = w;
     else
         d->leftCornerWidget = w;
+    setUpLayout();
 }
 
 /*!
