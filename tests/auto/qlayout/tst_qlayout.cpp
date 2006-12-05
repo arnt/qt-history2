df5b6e888102b66a4f7cdd2f9a4e912f8f02df7e
jasaethe <jasaethe@mike-macondo>
2006-12-05 17:24:38 +0100

    p4i integration Integrate 244703 from 4.2 to main: Fixes:    Autotest for QWidgetItem::geometry()
    [git-p4: depot-paths = "//depot/qt/main/": change = 244704]

--- tests/auto/qlayout/tst_qlayout.cpp
+++ tests/auto/qlayout/tst_qlayout.cpp
@@ -13,6 +13,8 @@
 #include <qdebug.h>
 #include <qboxlayout.h>
 #include <qmenubar.h>
+#include <QtGui/QFrame>
+#include <QtGui/QWindowsStyle>
 
 //TESTED_CLASS=
 //TESTED_FILES=qlayout.h
@@ -27,6 +29,7 @@ public:
 
 private slots:
     void getSetCheck();
+    void geometry();
 };
 
 tst_QLayout::tst_QLayout()
@@ -50,6 +53,48 @@ void tst_QLayout::getSetCheck()
     QCOMPARE((QWidget *)0, obj1.menuBar());
     delete var1;
 }
+
+class SizeHinterFrame : public QFrame
+{
+public:
+    SizeHinterFrame(const QSize &s) 
+    : QFrame(0), sh(s) {
+        setFrameStyle(QFrame::Box | QFrame::Plain);
+    }
+
+
+
+    void setSizeHint(const QSize &s) { sh = s; }
+    QSize sizeHint() const { return sh; }
+
+private:
+    QSize sh;
+};
+
+
+void tst_QLayout::geometry()
+{
+    // For QWindowsStyle we know that QWidgetItem::geometry() and QWidget::geometry()
+    // should be the same.
+    QApplication::setStyle(new QWindowsStyle);
+    QWidget w;
+    QVBoxLayout layout(&w);
+    SizeHinterFrame widget(QSize(100,100));
+    layout.addWidget(&widget);
+    QLayoutItem *item = layout.itemAt(0);
+    w.show();
+    QApplication::processEvents();
+    QCOMPARE(item->geometry().size(), QSize(100,100));
+
+    widget.setMinimumSize(QSize(110,110));
+    QCOMPARE(item->geometry().size(), QSize(110,110));
+
+    widget.setMinimumSize(QSize(0,0));
+    widget.setMaximumSize(QSize(90,90));
+    widget.setSizeHint(QSize(100,100));
+    QCOMPARE(item->geometry().size(), QSize(90,90));
+}
+
 
 QTEST_MAIN(tst_QLayout)
 #include "tst_qlayout.moc"
