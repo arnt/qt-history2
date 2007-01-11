cc8df92da2f1b7f0464275ffb3aad3c1c734219b
Simon Hausmann <shausman@trolltech.com>
2007-01-11 22:50:07 +0100

    Details: Make the blockTextFlags a property
    [git-p4: depot-paths = "//depot/qt/main/": change = 247537]

--- src/gui/text/qtextdocumentlayout_p.h
+++ src/gui/text/qtextdocumentlayout_p.h
@@ -40,6 +40,7 @@ class Q_AUTOTEST_EXPORT QTextDocumentLayout : public QAbstractTextDocumentLayout
     Q_PROPERTY(double tabStopWidth READ tabStopWidth WRITE setTabStopWidth)
     Q_PROPERTY(int cursorWidth READ cursorWidth WRITE setCursorWidth)
     Q_PROPERTY(qreal idealWidth READ idealWidth)
+    Q_PROPERTY(int blockTextFlags READ blockTextFlags WRITE setBlockTextFlags)
 public:
     explicit QTextDocumentLayout(QTextDocument *doc);
 
