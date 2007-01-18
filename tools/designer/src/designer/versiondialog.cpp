d86b9f0c311bc81afcbf2dd1daed5ac640b79043
Stian Thomassen <sthomassen@trolltech.com>
2007-01-18 17:52:10 +0100

    Fixes:    Text in "About Qt Designer" dialog can now be translated properly Task:     none AutoTest: none Details:  ifdefs were used to create the text used in the "About Qt Designer" dialog box, without breaking it up in appropriate blocks of tr(), thus lupdate could not parse the source correctly.
    [git-p4: depot-paths = "//depot/qt/main/": change = 248218]

--- tools/designer/src/designer/versiondialog.cpp
+++ tools/designer/src/designer/versiondialog.cpp
@@ -136,31 +136,36 @@ VersionDialog::VersionDialog(QWidget *parent)
     QGridLayout *layout = new QGridLayout(this);
     VersionLabel *label = new VersionLabel(this);
     QLabel *lbl = new QLabel(this);
-    lbl->setText(tr("<h3>%1</h3>"
-                    "<br/><br/>Version %2"
+    QString version = tr("<h3>%1</h3><br/><br/>Version %2");
 #if QT_EDITION == QT_EDITION_OPENSOURCE
-                    " Open Source Edition"
+    QString open = tr(" Open Source Edition");
+    version.append(open);
 #endif
-                    "<br/>Qt Designer is a graphical user interface designer "
-                    "for Qt applications.<br/><br/>"
+    version = version.arg(tr("Qt Designer")).arg(QLatin1String(QT_VERSION_STR));
+    version.append(tr("<br/>Qt Designer is a graphical user interface designer for Qt applications.<br/>"));
+    
+    QString edition =
 #if QT_EDITION == QT_EDITION_OPENSOURCE
-                    "This version of Qt Designer is part of the Qt Open Source Edition, for use "
+                    tr("This version of Qt Designer is part of the Qt Open Source Edition, for use "
                     "in the development of Open Source applications. "
                     "Qt is a comprehensive C++ framework for cross-platform application "
                     "development.<br/><br/>"
                     "You need a commercial Qt license for development of proprietary (closed "
                     "source) applications. Please see <a href=\"http://www.trolltech.com/company/model.html\">http://www.trolltech.com/company/model"
-                    ".html</a> for an overview of Qt licensing.<br/>"
+                    ".html</a> for an overview of Qt licensing.<br/>");
 #else
-                    "This program is licensed to you under the terms of the "
+                    tr("This program is licensed to you under the terms of the "
                     "Qt Commercial License Agreement. For details, see the file LICENSE "
-                    "that came with this software distribution.<br/>"
+                    "that came with this software distribution.<br/>");
 #endif
+                    
+    lbl->setText(tr("%1"
+                    "<br/>%2"
                     "<br/>Copyright 2000-$THISYEAR$ $TROLLTECH$. All rights reserved."
                     "<br/><br/>The program is provided AS IS with NO WARRANTY OF ANY KIND,"
                     " INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A"
-                    " PARTICULAR PURPOSE.<br/> ").arg(tr("Qt Designer"))
-                    .arg(QLatin1String(QT_VERSION_STR)));
+                    " PARTICULAR PURPOSE.<br/> ").arg(version).arg(edition));
+    
     lbl->setWordWrap(true);
     lbl->setOpenExternalLinks(true);
     QPushButton *cmd = new QPushButton(QLatin1String("OK"), this);
