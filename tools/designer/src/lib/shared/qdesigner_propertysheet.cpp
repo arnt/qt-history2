1fe5a023b4531aa6ff17b99bed1d2f21dbcfcca4
fkleint <fkleint@trolltech.com>
2007-01-23 12:47:36 +0100

    p4i integration Integrate 248616 from 4.2 to main: Fixes:     Value of autoFillBackground between Designer and compiled program isn't consistent (main container) Task:     145879 RevBy:    Jarek Details:  Made autoFillBackground a fake property.
    [git-p4: depot-paths = "//depot/qt/main/": change = 248619]

--- tools/designer/src/lib/shared/qdesigner_propertysheet.cpp
+++ tools/designer/src/lib/shared/qdesigner_propertysheet.cpp
@@ -117,6 +117,7 @@ QDesignerPropertySheet::QDesignerPropertySheet(QObject *object, QObject *parent)
         createFakeProperty(QLatin1String("acceptDrops"));
         createFakeProperty(QLatin1String("dragEnabled"));
         createFakeProperty(QLatin1String("windowModality"));
+        createFakeProperty(QLatin1String("autoFillBackground"));
 
         if (m_canHaveLayoutAttributes) {
             int pindex = count();
