d8d546bc58985be8ac15ff2072c0f0a9b8591392
harald <harald@harald-morpork>
2007-01-26 17:07:44 +0100

    p4i integration Integrate 249209 from 4.2 to main: Fixes:    work with more than one plugin
    [git-p4: depot-paths = "//depot/qt/main/": change = 249210]

--- util/plugintest/main.cpp
+++ util/plugintest/main.cpp
@@ -6,17 +6,20 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    const QStringList args = app.arguments();
-    if (args.count() != 2) {
-        printf("Usage: ./plugintest libplugin.so\nThis tool loads a plugin and displays whether QPluginLoader could load it or not.\nIf the plugin could not be loaded, it'll display the error string.\n");
+    const QStringList args = app.arguments().mid(1);
+    if (args.isEmpty()) {
+        printf("Usage: ./plugintest libplugin.so...\nThis tool loads a plugin and displays whether QPluginLoader could load it or not.\nIf the plugin could not be loaded, it'll display the error string.\n");
         return 1;
     }
 
-    QPluginLoader loader(args.at(1));
-    if (loader.load())
-        printf("success!\n");
-    else
-        printf("failure: %s\n", qPrintable(loader.errorString()));
+    foreach (QString plugin, args) {
+        printf("%s: ", qPrintable(plugin));
+        QPluginLoader loader(plugin);
+        if (loader.load())
+            printf("success!\n");
+        else
+            printf("failure: %s\n", qPrintable(loader.errorString()));
+    }
 
     return 0;
 }
