127881322c37cd774c72f07cbfb0f12715d1e5f7
ahanssen <ahanssen@:w>
2007-01-31 10:57:04 +0100

    Fixes:    Make not crash. Insert a QFAIL instead.
    [git-p4: depot-paths = "//depot/qt/main/": change = 249563]

--- tests/auto/qvarlengtharray/tst_qvarlengtharray.cpp
+++ tests/auto/qvarlengtharray/tst_qvarlengtharray.cpp
@@ -113,6 +113,7 @@ void tst_QVarLengthArray::oldTests()
         QCOMPARE(sa3[5], 5);
     }
 
+    QFAIL("This test causes the machine to crash when allocating too much memory.");
     {
         QVarLengthArray<Foo> a;
         const int N = 0x7fffffff / sizeof(Foo);
