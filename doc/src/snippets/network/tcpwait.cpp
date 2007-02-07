9cd561a21eb10bf674d6b54d084f87f12e5e5c95
gvatteka <gvatteka@trolltech.com>
2007-02-07 13:55:11 +0100

    p4i integration Integrate 250462 from 4.2 to main: Added code snippet
    [git-p4: depot-paths = "//depot/qt/main/": change = 250463]

---
+++ doc/src/snippets/network/tcpwait.cpp
@@ -0,0 +1,27 @@
+#include <QtGui>
+#include <QTcpSocket>
+
+#include "server.h"
+
+int main(int argv, char **args)
+{
+    QCoreApplication app(argv, args);
+
+    QTcpSocket socket;
+    socket.connectToHost("localhost", 1025);
+    
+    int numRead = 0, numReadTotal = 0;
+    char buffer[50];
+
+    forever {
+	numRead  = socket.read(buffer, 50);
+
+	// do whatever with array
+	
+	numReadTotal += numRead;
+	if (numRead == 0 && !socket.waitForReadyRead()) 
+	    break;
+    }
+    
+    return app.exec();
+}
