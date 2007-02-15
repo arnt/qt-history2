a33d6c18899ae799428b287181aba3533af63e12
benjamin <bmeyer@trolltech.com>
2007-02-15 12:59:54 +0100

    Fixes:    Revert Details:  There might be a better fix at some point in the future.
    [git-p4: depot-paths = "//depot/qt/main/": change = 251505]

--- examples/graphicsview/dragdroprobot/main.cpp
+++ examples/graphicsview/dragdroprobot/main.cpp
@@ -43,7 +43,6 @@ int main(int argc, char **argv)
     view.setRenderHint(QPainter::Antialiasing);
     view.setBackgroundBrush(QColor(230, 200, 167));
     view.setWindowTitle("Drag and Drop Robot");
-    view.resize(450, 450);
     view.show();
 
     return app.exec();
