ce12039a5ff04c1be5185e55db8d5645af548bbe
Simon Hausmann <shausman@trolltech.com>
2007-02-23 12:44:33 +0100

    Details: Fix the license headers to use $DUAL_LICENSE$ and the like
    [git-p4: depot-paths = "//depot/qt/main/": change = 252594]

--- tools/qdbus/src/qdbuscontext.cpp
+++ tools/qdbus/src/qdbuscontext.cpp
@@ -1,20 +1,10 @@
 /****************************************************************************
 **
-** Copyright (C) 1992-2007 Trolltech ASA. All rights reserved.
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
 **
-** This file is part of the tools applications of the Qt Toolkit.
+** This file is part of the $MODULE$ of the Qt Toolkit.
 **
-** This file may be used under the terms of the GNU General Public
-** License version 2.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of
-** this file.  Please review the following information to ensure GNU
-** General Public Licensing requirements will be met:
-** http://www.trolltech.com/products/qt/opensource.html
-**
-** If you are unsure which license is appropriate for your use, please
-** review the following information:
-** http://www.trolltech.com/products/qt/licensing.html or contact the
-** sales department at sales@trolltech.com.
+** $TROLLTECH_DUAL_LICENSE$
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
@@ -205,4 +195,5 @@ void QDBusContext::sendErrorReply(QDBusError::ErrorType type, const QString &msg
     setDelayedReply(true);
     connection().send(message().createErrorReply(type, msg));
 }
+
 
