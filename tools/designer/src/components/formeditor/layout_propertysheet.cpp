b1984332b52bc1c461b60eecb051b567681edd9e
jkobus <jkobus@smatux-slaruell>
2007-02-26 11:43:49 +0100

    Fixes:    Forgot to add those files
    [git-p4: depot-paths = "//depot/qt/main/": change = 252751]

---
+++ tools/designer/src/components/formeditor/layout_propertysheet.cpp
@@ -0,0 +1,159 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "layout_propertysheet.h"
+
+// sdk
+#include <QtDesigner/QExtensionManager>
+
+// shared
+#include <QtGui/QLayout>
+#include <QtGui/QStyle>
+#include <qlayout_widget_p.h>
+
+using namespace qdesigner_internal;
+
+LayoutPropertySheet::LayoutPropertySheet(QLayout *object, QObject *parent)
+    : QDesignerPropertySheet(object, parent), m_layout(object)
+{
+    int pindex = count();
+    createFakeProperty(QLatin1String("leftMargin"), 0);
+    setPropertyGroup(pindex, tr("Layout"));
+
+    pindex = count();
+    createFakeProperty(QLatin1String("topMargin"), 0);
+    setPropertyGroup(pindex, tr("Layout"));
+
+    pindex = count();
+    createFakeProperty(QLatin1String("rightMargin"), 0);
+    setPropertyGroup(pindex, tr("Layout"));
+
+    pindex = count();
+    createFakeProperty(QLatin1String("bottomMargin"), 0);
+    setPropertyGroup(pindex, tr("Layout"));
+
+    setAttribute(indexOf(QLatin1String("margin")), true);
+}
+
+LayoutPropertySheet::~LayoutPropertySheet()
+{
+}
+
+void LayoutPropertySheet::setProperty(int index, const QVariant &value)
+{
+    QString pname = propertyName(index);
+    QLayoutWidget *lw = qobject_cast<QLayoutWidget *>(m_layout->parent());
+    if (lw) {
+        if (pname == QLatin1String("leftMargin")) {
+            lw->setLayoutLeftMargin(value.toInt());
+            return;
+        }
+        if (pname == QLatin1String("topMargin")) {
+            lw->setLayoutTopMargin(value.toInt());
+            return;
+        }
+        if (pname == QLatin1String("rightMargin")) {
+            lw->setLayoutRightMargin(value.toInt());
+            return;
+        }
+        if (pname == QLatin1String("bottomMargin")) {
+            lw->setLayoutBottomMargin(value.toInt());
+            return;
+        }
+    }
+    int left, top, right, bottom;
+    m_layout->getContentsMargins(&left, &top, &right, &bottom);
+    if (pname == QLatin1String("leftMargin"))
+        m_layout->setContentsMargins(value.toInt(), top, right, bottom);
+    else if (pname == QLatin1String("topMargin"))
+        m_layout->setContentsMargins(left, value.toInt(), right, bottom);
+    else if (pname == QLatin1String("rightMargin"))
+        m_layout->setContentsMargins(left, top, value.toInt(), bottom);
+    else if (pname == QLatin1String("bottomMargin"))
+        m_layout->setContentsMargins(left, top, right, value.toInt());
+    else
+        QDesignerPropertySheet::setProperty(index, value);
+}
+
+QVariant LayoutPropertySheet::property(int index) const
+{
+    QString pname = propertyName(index);
+    QLayoutWidget *lw = qobject_cast<QLayoutWidget *>(m_layout->parent());
+    if (lw) {
+        if (pname == QLatin1String("leftMargin"))
+            return lw->layoutLeftMargin();
+        if (pname == QLatin1String("topMargin"))
+            return lw->layoutTopMargin();
+        if (pname == QLatin1String("rightMargin"))
+            return lw->layoutRightMargin();
+        if (pname == QLatin1String("bottomMargin"))
+            return lw->layoutBottomMargin();
+    }
+    int left, top, right, bottom;
+    m_layout->getContentsMargins(&left, &top, &right, &bottom);
+    if (pname == QLatin1String("leftMargin"))
+        return left;
+    if (pname == QLatin1String("topMargin"))
+        return top;
+    if (pname == QLatin1String("rightMargin"))
+        return right;
+    if (pname == QLatin1String("bottomMargin"))
+        return bottom;
+    return QDesignerPropertySheet::property(index);
+}
+
+bool LayoutPropertySheet::reset(int index)
+{
+    QString pname = propertyName(index);
+    int left, top, right, bottom;
+    m_layout->getContentsMargins(&left, &top, &right, &bottom);
+    if (pname == QLatin1String("leftMargin"))
+        m_layout->setContentsMargins(-1, top, right, bottom);
+    else if (pname == QLatin1String("topMargin"))
+        m_layout->setContentsMargins(left, -1, right, bottom);
+    else if (pname == QLatin1String("rightMargin"))
+        m_layout->setContentsMargins(left, top, -1, bottom);
+    else if (pname == QLatin1String("bottomMargin"))
+        m_layout->setContentsMargins(left, top, right, -1);
+    else
+        return QDesignerPropertySheet::reset(index);
+    return true;
+}
+
+void LayoutPropertySheet::setChanged(int index, bool changed)
+{
+    QString pname = propertyName(index);
+    if (pname == QLatin1String("margin")) {
+        setChanged(indexOf(QLatin1String("leftMargin")), changed);
+        setChanged(indexOf(QLatin1String("topMargin")), changed);
+        setChanged(indexOf(QLatin1String("rightMargin")), changed);
+        setChanged(indexOf(QLatin1String("bottomMargin")), changed);
+    }
+    QDesignerPropertySheet::setChanged(index, changed);
+}
+
+LayoutPropertySheetFactory::LayoutPropertySheetFactory(QExtensionManager *parent)
+    : QExtensionFactory(parent)
+{
+}
+
+QObject *LayoutPropertySheetFactory::createExtension(QObject *object, const QString &iid, QObject *parent) const
+{
+    if (iid != Q_TYPEID(QDesignerPropertySheetExtension))
+        return 0;
+
+    if (QLayout *o = qobject_cast<QLayout*>(object))
+        return new LayoutPropertySheet(o, parent);
+
+    return 0;
+}
---
+++ tools/designer/src/components/formeditor/layout_propertysheet.h
@@ -0,0 +1,52 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef LAYOUT_PROPERTYSHEET_H
+#define LAYOUT_PROPERTYSHEET_H
+
+#include <qdesigner_propertysheet_p.h>
+
+class QLayout;
+
+namespace qdesigner_internal {
+
+class LayoutPropertySheet: public QDesignerPropertySheet
+{
+    Q_OBJECT
+    Q_INTERFACES(QDesignerPropertySheetExtension)
+public:
+    LayoutPropertySheet(QLayout *object, QObject *parent = 0);
+    virtual ~LayoutPropertySheet();
+
+    virtual void setProperty(int index, const QVariant &value);
+    virtual QVariant property(int index) const;
+    virtual bool reset(int index);
+    void setChanged(int index, bool changed);
+private:
+    QLayout *m_layout;
+};
+
+class LayoutPropertySheetFactory: public QExtensionFactory
+{
+    Q_OBJECT
+    Q_INTERFACES(QAbstractExtensionFactory)
+public:
+    LayoutPropertySheetFactory(QExtensionManager *parent = 0);
+
+protected:
+    virtual QObject *createExtension(QObject *object, const QString &iid, QObject *parent) const;
+};
+
+}  // namespace qdesigner_internal
+
+#endif // LAYOUT_PROPERTYSHEET_H
