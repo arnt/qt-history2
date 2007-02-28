f4fcc1a413c7b26459240bb501962ca21bba60ec
Alessandro Portale <aportale@trolltech.com>
2007-02-28 18:15:15 +0100

    Implemented a few layout corrections which dboddie suggested
    [git-p4: depot-paths = "//depot/qt/main/": change = 253287]

--- util/qdoc3/test/rssbox/rssboxswitcher.js
+++ util/qdoc3/test/rssbox/rssboxswitcher.js
@@ -1,8 +1,27 @@
 function init()
 {
-	$("#rssbox").css("display", "block");
+	$(window).resize(function() {recalculateRssBoxPosition();});
+	$("#rssbox").css({ display: "block", position: "absolute" });
+	recalculateRssBoxPosition();
 	$("#rssbox>ul>li>a").removeAttr("href").css("cursor", "pointer").click(function() {toggleRssLinksVisibility();});
 	setRssLinksVisible(getCookie("rsslinksvisible") == "true");
+}
+
+function recalculateRssBoxPosition()
+{
+	var logoWidth = $("img[@src*=trolltech-logo]").width();
+	var bodyMarginTop = cleanNumber($("body").css("margin-top"));
+	var bodyMarginLeft = cleanNumber($("body").css("margin-left"));
+	var logoHeight = $("img[@src*=trolltech-logo]").height();
+	var headerTableWidth = $("table:first").width();
+	$("#rssbox").css({ left: headerTableWidth - logoWidth + bodyMarginLeft, top: logoHeight + bodyMarginTop}).width(logoWidth);
+}
+
+function cleanNumber(dirtyNumber)
+{
+	while (dirtyNumber.search(/[^0-9]/) >= 0)
+		dirtyNumber = dirtyNumber.replace(/[^0-9]/, "");
+	return dirtyNumber * 1;
 }
 
 function toggleRssLinksVisibility()
