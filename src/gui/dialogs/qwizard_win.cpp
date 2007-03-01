f9663cec80fcfc9b55dd56ed66282a9d43b5ca23
ahanssen <ahanssen@:w>
2007-03-01 14:27:46 +0100

    p4i integration Integrate 253438 from 4.3 to main: Fixes: Fixup license headers.
    [git-p4: depot-paths = "//depot/qt/main/": change = 253439]

--- src/gui/dialogs/qwizard_win.cpp
+++ src/gui/dialogs/qwizard_win.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2004-$THISYEAR$ $TROLLTECH$. All rights reserved.
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
 **
 ** This file is part of the $MODULE$ of the Qt Toolkit.
 **
