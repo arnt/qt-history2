525dab43bee3722de2ed0132edbf813c04c7a0d6
dboddie <dboddie@mikaeld-lurven>
2007-03-01 16:12:29 +0100

    p4i integration Integrate 253519 from 4.3 to main: Fixed: Added missing license headers. RevBy: Trust me Task: 152806
    [git-p4: depot-paths = "//depot/qt/main/": change = 253520]

--- examples/xml/rsslisting/main.cpp
+++ examples/xml/rsslisting/main.cpp
@@ -2,8 +2,9 @@
 **
 ** Copyright (C) 2004-$THISYEAR$ $TROLLTECH$. All rights reserved.
 **
-** This file is part of an example program for Qt.
-** EDITIONS: NOLIMITS
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
