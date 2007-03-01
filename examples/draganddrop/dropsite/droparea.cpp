dd1278aa7f8268ead8cca7b63dfdfd58318717bf
Richard Moe Gustavsen <rmgustav@trolltech.com>
2007-03-01 14:31:41 +0100

    p4i integration Integrate 253443 from 4.3 to main: Show the URI if dropped as well...
    [git-p4: depot-paths = "//depot/qt/main/": change = 253446]

--- examples/draganddrop/dropsite/droparea.cpp
+++ examples/draganddrop/dropsite/droparea.cpp
@@ -52,6 +52,14 @@ void DropArea::dropEvent(QDropEvent *event)
     } else if (mimeData->hasText()) {
         setText(mimeData->text());    
         setTextFormat(Qt::PlainText);
+    } else if (mimeData->hasUrls()) {
+        QList<QUrl> urlList = mimeData->urls();
+        QString text;
+        for (int i = 0; i < urlList.size() && i < 32; ++i) {
+            QString url = urlList.at(i).path();
+            text += url + QString("\n");
+        }
+        setText(text);
     } else {
         setText(tr("Cannot display data"));
     }
