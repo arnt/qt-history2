9ccc8a454fd4203ae9783a9585435d8cd13f69af
Frans Englich <fenglich@trolltech.com>
2007-03-09 15:37:36 +0100

    p4i integration Integrate 254807 from 4.3 to main: Fixes:    Integrate the xmlstreamlint example from the research branch.
    [git-p4: depot-paths = "//depot/qt/main/": change = 254808]

---
+++ examples/xml/xmlstreamlint/main.cpp
@@ -0,0 +1,94 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include <QCoreApplication>
+#include <QFile>
+#include <QStringList>
+#include <QTextStream>
+#include <QXmlStreamReader>
+
+/*!
+ \short This class exists for the solve purpose of creating a translation context.
+ */
+class XmlStreamLint
+{
+public:
+    Q_DECLARE_TR_FUNCTIONS(XmlStreamLint);
+};
+
+int main(int argc, char *argv[])
+{
+    enum ExitCode
+    {
+        Success,
+        ParseFailure,
+        ArgumentError,
+        WriteError,
+        FileFailure
+    };
+
+    QCoreApplication app(argc, argv);
+
+    QTextStream errorStream(stderr);
+
+    if (argc != 2)
+    {
+        errorStream << XmlStreamLint::tr(
+                       "Usage: xmlstreamlint <path to XML file>\n");
+        return ArgumentError;
+    }
+
+    QString inputFilePath(QCoreApplication::arguments().at(1));
+    QFile inputFile(inputFilePath);
+
+    if (!QFile::exists(inputFilePath))
+    {
+        errorStream << XmlStreamLint::tr(
+                       "File %1 does not exist.\n").arg(inputFilePath);
+        return FileFailure;
+
+    } else if (!inputFile.open(QIODevice::ReadOnly)) {
+        errorStream << XmlStreamLint::tr(
+                       "Failed to open file %1.\n").arg(inputFilePath);
+        return FileFailure;
+    }
+
+    QFile outputFile;
+    if (!outputFile.open(stdout, QIODevice::WriteOnly))
+    {
+        QTextStream(stderr) << XmlStreamLint::tr("Failed to open stdout.");
+        return WriteError;
+    }
+
+    QXmlStreamReader reader(&inputFile);
+    QXmlStreamWriter writer(&outputFile);
+
+    while (!reader.atEnd())
+    {
+        reader.readNext();
+
+        if (reader.error())
+        {
+            errorStream << XmlStreamLint::tr(
+                           "Error: %1 in file %2 at line %3, column %4.\n").arg(
+                               reader.errorString(), inputFilePath,
+                               QString::number(reader.lineNumber()),
+                               QString::number(reader.columnNumber()));
+            return ParseFailure;
+
+        } else
+            writer.writeCurrentToken(reader);
+    }
+
+    return Success;
+}
