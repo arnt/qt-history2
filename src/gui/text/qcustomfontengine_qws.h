ca79b2ca3d87b2f077b15c24dd1eb2bf9842dac9
Simon Hausmann <shausman@trolltech.com>
2007-03-13 15:01:14 +0100

    p4i integration Integrate 255257 from 4.3 to main: Task:     144103 RevBy:    Done with Paul Details:  Another minor comment :)
    [git-p4: depot-paths = "//depot/qt/main/": change = 255300]

--- src/gui/text/qcustomfontengine_qws.h
+++ src/gui/text/qcustomfontengine_qws.h
@@ -131,7 +131,7 @@ public:
         MaxCharWidth,
         MinLeftBearing,
         MinRightBearing,
-        TotalGlyphCount,
+        TotalGlyphCount, // GlyphCount?
 
         // hints
         GlyphShareHint,
