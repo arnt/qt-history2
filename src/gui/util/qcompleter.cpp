0496b0b254ef77bacb2c3aa6933ab1afebdb5b52
benjamin <bmeyer@trolltech.com>
2007-03-21 12:18:08 +0100

    p4i integration Integrate 256595 from 4.3 to main: Fixes:    reset the model if there is nothing in it rather then doing the more expensive calls. RevBy:    gerish Details:  Most relavent on startup where this will be hit a number of times
    [git-p4: depot-paths = "//depot/qt/main/": change = 256596]

--- src/gui/util/qcompleter.cpp
+++ src/gui/util/qcompleter.cpp
@@ -389,6 +389,11 @@ void QCompletionModel::filter(const QStringList& parts)
 
 void QCompletionModel::resetModel()
 {
+    if (rowCount() == 0) {
+        reset();
+        return;
+    }
+
     emit layoutAboutToBeChanged();
     QModelIndexList piList = persistentIndexList();
     QModelIndexList empty;
