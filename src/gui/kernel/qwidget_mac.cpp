213a4e4fb26cc1aac0d85cf9ae335fc182eb9e7b
msorvig <msorvig@dole>
2007-04-02 11:33:52 +0100

    p4i integration Integrate 258288 from 4.3 to main: Remove outdated comment, this has now been fixed with change 252926.
    [git-p4: depot-paths = "//depot/qt/main/": change = 258289]

--- src/gui/kernel/qwidget_mac.cpp
+++ src/gui/kernel/qwidget_mac.cpp
@@ -2872,10 +2872,6 @@ QPaintEngine *QWidget::paintEngine() const
 
 void QWidgetPrivate::setModal_sys()
 {
-// The HIWindowChangeClass function does not work correctly. It fails to remove the jewels (e.g close button)
-// and writes the window text over it when going from document class to movable modal. So it looks crappy.
-// Bug reported to apple. When fixed, the lines below should work (task 142893).
-
     Q_Q(QWidget);
 
     if (!q->testAttribute(Qt::WA_WState_Created))
