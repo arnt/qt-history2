7bed7ceaa20d9e9862e3bf3625fe904839f2d3ec
hfroilan <hfroilan@trolltech.com>
2007-04-03 16:18:56 +0100

    p4i integration Integrate 258534 from 4.3 to main: Fixes:    Keep encoding (utf8 flag) when merging RevBy:    Jan-Arve
    [git-p4: depot-paths = "//depot/qt/main/": change = 258535]

--- tools/linguist/lupdate/merge.cpp
+++ tools/linguist/lupdate/merge.cpp
@@ -65,7 +65,7 @@ void merge( const MetaTranslator *tor, const MetaTranslator *virginTor, MetaTran
                     // ### The QString() cast is evil
                     if (getSimilarityScore(QLatin1String(m.sourceText()), mv.sourceText()) >= textSimilarityThreshold) {
                         // It is just slightly modified, assume that it is the same string
-                        m = MetaTranslatorMessage(m.context(), mv.sourceText(), m.comment(), m.fileName(), m.lineNumber(), m.translations());
+                        m = MetaTranslatorMessage(m.context(), mv.sourceText(), m.comment(), m.fileName(), m.lineNumber(), m.translations(), mv.utf8());
                         m.setPlural(mv.isPlural());
 
                         // Mark it as unfinished. (Since the source text was changed it might require re-translating...)
