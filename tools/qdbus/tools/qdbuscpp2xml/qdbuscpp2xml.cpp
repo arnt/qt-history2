ce10b2e71a5df0314eb1a33ac483064ca8ce5b36
Simon Hausmann <shausman@trolltech.com>
2007-04-12 12:20:32 +0100

    p4i integration Integrate 259415 from 4.3 to main: Fixes:    Fix qdbuscpp2xml on Windows part 2. Task:     144663 RevBy:    Done with Mbm Details:  Also pass QIODevice::Text to the moc process we start.
    [git-p4: depot-paths = "//depot/qt/main/": change = 259416]

--- tools/qdbus/tools/qdbuscpp2xml/qdbuscpp2xml.cpp
+++ tools/qdbus/tools/qdbuscpp2xml/qdbuscpp2xml.cpp
@@ -366,7 +366,7 @@ int main(int argc, char **argv)
         else {
             // run moc on this file
             QProcess proc;
-            proc.start(QLibraryInfo::location(QLibraryInfo::BinariesPath) + QLatin1String("/moc"), QStringList() << QFile::decodeName(argv[i]));
+            proc.start(QLibraryInfo::location(QLibraryInfo::BinariesPath) + QLatin1String("/moc"), QStringList() << QFile::decodeName(argv[i]), QIODevice::ReadOnly | QIODevice::Text);
 
             if (!proc.waitForStarted()) {
                 fprintf(stderr, PROGRAMNAME ": could not execute moc! Aborting.\n");
