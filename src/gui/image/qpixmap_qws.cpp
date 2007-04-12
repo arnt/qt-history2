6f303db0887f75335f40c4d400311b4a516528ed
hwall <hwall@hwall-extra>
2007-04-12 15:07:30 +0100

    p4i integration Integrate 259486 from 4.3 to main: Fixes:    QPixmap::grabWindow() on 18/24 bit screens Task:     156704 RevBy:    paul
    [git-p4: depot-paths = "//depot/qt/main/": change = 259487]

--- src/gui/image/qpixmap_qws.cpp
+++ src/gui/image/qpixmap_qws.cpp
@@ -45,12 +45,60 @@ QPixmap QPixmap::grabWindow(WId window, int x, int y, int w, int h)
         h = grabRect.height() - y;
     grabRect &= QRect(x, y, w, h);
 
+    QPixmap pixmap;
+
     QWSDisplay::grab(false);
 
-    QImage img(qt_screen->base(),
-               qt_screen->width(), qt_screen->height(),
-               qt_screen->linestep(), qt_screen->pixelFormat());
-    QPixmap pixmap = fromImage(img.copy(grabRect));
+    if (qt_screen->pixelFormat() != QImage::Format_Invalid) {
+        QImage img(qt_screen->base(),
+                   qt_screen->width(), qt_screen->height(),
+                   qt_screen->linestep(), qt_screen->pixelFormat());
+        pixmap = fromImage(img.copy(grabRect));
+    } else if (qt_screen->base()) {
+
+        // temporary workaraounds for screen depths supported by the linuxfb
+        // driver. Should be replaced by some QScreen::toImage() function
+        // or something...
+        switch (qt_screen->depth()) {
+#ifdef QT_QWS_DEPTH_24
+        case 24: {
+            QImage img(grabRect.width(), grabRect.height(), QImage::Format_RGB32);
+            const uchar *src = qt_screen->base()
+                               + qt_screen->linestep() * grabRect.y()
+                               + sizeof(quint24) * grabRect.x();
+            qt_rectconvert<quint32, quint24>(reinterpret_cast<quint32*>(img.bits()),
+                                             reinterpret_cast<const quint24*>(src),
+                                             0, 0, grabRect.width(), grabRect.height(),
+                                             img.bytesPerLine(),
+                                             qt_screen->linestep());
+            pixmap = fromImage(img);
+            break;
+        }
+#endif // QT_QWS_DEPTH_24
+#ifdef QT_QWS_DEPTH_18
+        case 18: {
+            QImage img(grabRect.width(), grabRect.height(), QImage::Format_RGB32);
+            const uchar *src = qt_screen->base()
+                               + qt_screen->linestep() * grabRect.y()
+                               + sizeof(quint18) * grabRect.x();
+            qt_rectconvert<quint32, quint18>(reinterpret_cast<quint32*>(img.bits()),
+                                             reinterpret_cast<const quint18*>(src),
+                                             0, 0, grabRect.width(), grabRect.height(),
+                                             img.bytesPerLine(),
+                                             qt_screen->linestep());
+            pixmap = fromImage(img);
+            break;
+        }
+#endif // QT_QWS_DEPTH_18
+        default:
+            qWarning("QPixmap::grabWindow(): Unsupported screen depth %d",
+                     qt_screen->depth());
+            break;
+        }
+    } else {
+        qWarning("QPixmap::grabWindow(): Unable to copy pixels from framebuffer");
+    }
+
     QWSDisplay::ungrab();
 
     return pixmap;
