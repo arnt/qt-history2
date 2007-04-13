85ea9442f4dde4474aad8da01d02758f772319e0
jasaethe <jasaethe@mike-macondo>
2007-04-13 14:20:28 +0100

    p4i integration Integrate 259717 from 4.3 to main: Fixes:    Use the unused variable. RevBy:    bnilsen AutoTest: disableSaveButton
    [git-p4: depot-paths = "//depot/qt/main/": change = 259718]

--- src/gui/dialogs/qfiledialog_p.h
+++ src/gui/dialogs/qfiledialog_p.h
@@ -124,8 +124,8 @@ public:
 #elif defined(Q_OS_WIN)
         DWORD maxLength;
         QString drive = path.left(3);
-        if (QT_WA_INLINE(::GetVolumeInformationW(reinterpret_cast<const WCHAR *>(path.utf16()), NULL, 0, NULL, &maxLength, NULL, NULL, 0),
-                         ::GetVolumeInformationA(path.toAscii().constData(), NULL, 0, NULL, &maxLength, NULL, NULL, 0)) == FALSE)
+        if (QT_WA_INLINE(::GetVolumeInformationW(reinterpret_cast<const WCHAR *>(drive.utf16()), NULL, 0, NULL, &maxLength, NULL, NULL, 0),
+            ::GetVolumeInformationA(drive.toLocal8Bit().constData(), NULL, 0, NULL, &maxLength, NULL, NULL, 0)) == FALSE)
             return -1;
         return maxLength;
 #else
