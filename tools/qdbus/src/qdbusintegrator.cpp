216aa1886d1fb8f20237d187ca4f44826a639c55
Simon Hausmann <shausman@trolltech.com>
2007-04-13 15:24:56 +0100

    p4i integration Integrate 259736 from 4.3 to main: Fixes:    Compiler warning (unused variable)
    [git-p4: depot-paths = "//depot/qt/main/": change = 259762]

--- tools/qdbus/src/qdbusintegrator.cpp
+++ tools/qdbus/src/qdbusintegrator.cpp
@@ -835,6 +835,7 @@ QDBusConnectionPrivate::QDBusConnectionPrivate(QObject *p)
     extern bool qDBusInitThreads();
     static const bool threads = qDBusInitThreads();
     static const bool debugging = !qgetenv("QDBUS_DEBUG").isEmpty();
+    Q_UNUSED(threads)
 
     ::isDebugging = debugging;
 
