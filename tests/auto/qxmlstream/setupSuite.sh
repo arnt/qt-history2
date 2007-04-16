b9571165403e6de3afd2c32de51a1ed84dc5f349
Frans Englich <fenglich@trolltech.com>
2007-04-16 17:11:05 +0100

    p4i integration Integrate 260023 from 4.3 to main: Fixes:    Adjust the scripts so they run more easily.
    [git-p4: depot-paths = "//depot/qt/main/": change = 260025]

--- tests/auto/qxmlstream/setupSuite.sh
+++ tests/auto/qxmlstream/setupSuite.sh
@@ -4,14 +4,25 @@
 #
 # You don't have to run this script unless you are actually updating the test suite.
 # For precaution, we therefore have this exit call.
-exit 0
 
-cvs up ¥XML-Test-Suite
+
+# CVS is retarded when it comes to reverting changes. Remove files it has moved.
+find XML-Test-Suite/ -name ".*.?.*" | xargs rm
+
+cd XML-Test-Suite
+
+export CVSROOT=":pserver:anonymous@dev.w3.org:/sources/public"
+cvs -q up -C
 
 # We're working-around a bug in the test suite here.
-sed -i 's+<TESTCASES PROFILE+<TESTCASES xml:base="eduni/namespaces/1.0/" PROFILE+' xmlconf/eduni/namespaces/1.0/rmt-ns10.xml
-sed -i 's+<TESTCASES PROFILE+<TESTCASES xml:base="eduni/errata-2e/" PROFILE+' xmlconf/eduni/errata-2e/errata2e.xml
+#sed -i 's+<TESTCASES PROFILE+<TESTCASES xml:base="eduni/namespaces/1.0/" PROFILE+' xmlconf/eduni/namespaces/1.0/rmt-ns10.xml
+#sed -i 's+<TESTCASES PROFILE+<TESTCASES xml:base="eduni/errata-2e/" PROFILE+' xmlconf/eduni/errata-2e/errata2e.xml
 
+sed -i 's+XML1.0-errata2e+XML1.0-errata2e|XML1.0-errata3e|NS1.0-errata1e+' xmlconf/testcases.dtd
+
+#sed -i 's+NS1.0-errata1+XML1.0-errata2e+' xmlconf/eduni/namespaces/errata-1e/errata1e.xml
+
+p4 edit ...
+p4 revert `find -name "Entries"` # They only contain CVS timestamps.
 xmllint --valid --noent xmlconf/xmlconf.xml --output xmlconf/finalCatalog.xml
-
-
+p4 revert -a ...
