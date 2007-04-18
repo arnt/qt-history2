12588b51334bb8f54bca031f7771b8c48bf68e98
jkobus <jkobus@smatux-slaruell>
2007-04-18 09:25:50 +0100

    p4i integration Integrate 260353 from 4.3 to main: Fixes:    extract images moved to cpp Details:  compile juic
    [git-p4: depot-paths = "//depot/qt/main/": change = 260355]

---
+++ src/tools/uic/cpp/cppextractimages.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+**
+** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef CPPEXTRACTIMAGES_H
+#define CPPEXTRACTIMAGES_H
+
+#include "treewalker.h"
+#include <QtCore/QDir>
+
+class QTextStream;
+class Driver;
+class Uic;
+
+struct Option;
+
+namespace CPP {
+
+class ExtractImages : public TreeWalker
+{
+public:
+    ExtractImages(const Option &opt);
+
+    void acceptUI(DomUI *node);
+    void acceptImages(DomImages *images);
+    void acceptImage(DomImage *image);
+
+private:
+    QTextStream *m_output;
+    const Option &m_option;
+    QDir m_imagesDir;
+};
+
+} // namespace CPP
+
+#endif // CPPEXTRACTIMAGES_H
