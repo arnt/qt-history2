fad5d410822df8e16d0aa877b94410df9ecd8164
Jo Asplin <jasplin@trolltech.com>
2007-04-20 15:09:14 +0100

    p4i integration Integrate 260874 from 4.3 to main: Fixes:    Compile on Solaris RevBy:    Morten Details:  Removed empty template specifier on Solaris. It could 	  possibly be removed on all platforms, but we're a 	  bit careful for now since 4.3.0 is just branched.
    [git-p4: depot-paths = "//depot/qt/main/": change = 260875]

--- src/gui/painting/qdrawhelper_p.h
+++ src/gui/painting/qdrawhelper_p.h
@@ -1,10 +1,20 @@
 /****************************************************************************
 **
-** Copyright (C) 1992-$THISYEAR$ $TROLLTECH$. All rights reserved.
+** Copyright (C) 1992-2007 Trolltech ASA. All rights reserved.
 **
-** This file is part of the $MODULE$ of the Qt Toolkit.
+** This file is part of the QtGui module of the Qt Toolkit.
 **
-** $TROLLTECH_DUAL_LICENSE$
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.  Please review the following information to ensure GNU
+** General Public Licensing requirements will be met:
+** http://www.trolltech.com/products/qt/opensource.html
+**
+** If you are unsure which license is appropriate for your use, please
+** review the following information:
+** http://www.trolltech.com/products/qt/licensing.html or contact the
+** sales department at sales@trolltech.com.
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
@@ -224,7 +234,10 @@ inline DST qt_colorConvert(SRC color)
     return color;
 }
 
+
+#ifndef Q_OS_SOLARIS
 template <>
+#endif
 inline quint32 qt_colorConvert(quint16 color)
 {
     const int r = (color & 0xf800);
@@ -237,7 +250,9 @@ inline quint32 qt_colorConvert(quint16 color)
     return qRgb(tr, tg, tb);
 }
 
+#ifndef Q_OS_SOLARIS
 template <>
+#endif
 inline quint16 qt_colorConvert(quint32 color)
 {
     const int r = qRed(color) << 8;
