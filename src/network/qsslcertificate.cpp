27db6fe25337123c9c3d3df91beac69de4f36f59
Trenton Schulz <twschulz@twschulz-rain>
2007-04-23 15:07:04 +0100

    p4i integration Integrate 261134 from 4.3 to main: Fixes:    Convert to a QString as quick as possible. RevBy:    Jo AutoTest: alternateFileNames        Details:  The QLatin1String doesn't behave nicely if it actually is an        object we use, so convert it to a QString.
    [git-p4: depot-paths = "//depot/qt/main/": change = 261135]

--- src/network/qsslcertificate.cpp
+++ src/network/qsslcertificate.cpp
@@ -107,6 +107,8 @@
 #include <QtCore/qstring.h>
 #include <QtCore/qstringlist.h>
 
+#include <QtCore/qdebug.h>
+
 /*!
     Constructs a QSslCertificate, and reads \a format encoded data from \a
     device to find the first available certificate. You can later call
@@ -325,9 +327,8 @@ QMultiMap<QSsl::AlternateNameEntry, QString> QSslCertificate::alternateSubjectNa
     if (altNames) {
         for (int i = 0; i < q_sk_GENERAL_NAME_num(altNames); ++i) {
             const GENERAL_NAME *genName = q_sk_GENERAL_NAME_value(altNames, i);
-            const QLatin1String altName(
-                QByteArray(
-                    (const char *)q_ASN1_STRING_data(genName->d.ia5),
+            const QString altName = QLatin1String(
+                    QByteArray(reinterpret_cast<const char *>(q_ASN1_STRING_data(genName->d.ia5)),
                     q_ASN1_STRING_length(genName->d.ia5)));
             if (genName->type == GEN_DNS)
                 result.insert(QSsl::DnsEntry, altName);
