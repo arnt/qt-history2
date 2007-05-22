4463ec90b42ff34a947a6c3ede7337a9148c1a2a
dboddie <dboddie@mikaeld-lurven>
2007-05-22 13:56:04 +0100

    Fixes: Translation: Manual integrate of change 264922.
    [git-p4: depot-paths = "//depot/qt/main/": change = 264946]

---
+++ translations/qvfb_pl.ts
@@ -0,0 +1,252 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl">
+<context>
+    <name>AnimationSaveWidget</name>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="929"/>
+        <source>Record</source>
+        <translation>Nagrywaj</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="728"/>
+        <source>Reset</source>
+        <translation>Zeruj</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="730"/>
+        <source>Save</source>
+        <translation>Zapisz</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="748"/>
+        <source>Save in MPEG format (requires netpbm package installed)</source>
+        <translation>Zapisz w formacie MPEG (wymaga zainstalowanego pakietu netpbm)</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="961"/>
+        <source>Click record to begin recording.</source>
+        <translation>Kliknij przycisk aby rozpocząć nagrywanie.</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="993"/>
+        <source>Finished saving.</source>
+        <translation>Zapisywanie zakończone.</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="930"/>
+        <source>Paused. Click record to resume, or save if done.</source>
+        <translation>Wstrzymany. Kliknij nagrywaj aby powrócić lub zapisać jeżeli zakończono.</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="936"/>
+        <source>Pause</source>
+        <translation>Wstrzymaj</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="937"/>
+        <source>Recording...</source>
+        <translation>Nagrywanie...</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="977"/>
+        <source>Saving... </source>
+        <translation>Zapisywanie... </translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="985"/>
+        <source>Save animation...</source>
+        <translation>Zapisz animacje...</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="987"/>
+        <source>Save canceled.</source>
+        <translation>Zapisywanie anulowane.</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="996"/>
+        <source>Save failed!</source>
+        <translation>Zapisywanie zakończone błędem!</translation>
+    </message>
+</context>
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="37"/>
+        <source>Configure</source>
+        <translation>Konfiguruj</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="68"/>
+        <source>Size</source>
+        <translation>Rozmiar</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="80"/>
+        <source>176x220 &quot;SmartPhone&quot;</source>
+        <translation>176x220 &quot;SmartPhone&quot;</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="87"/>
+        <source>240x320 &quot;PDA&quot;</source>
+        <translation>240x320 &quot;PDA&quot;</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="94"/>
+        <source>320x240 &quot;TV&quot; / &quot;QVGA&quot;</source>
+        <translation>320x240 &quot;TV&quot; / &quot;QVGA&quot;</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="101"/>
+        <source>640x480 &quot;VGA&quot;</source>
+        <translation>640x480 &quot;VGA&quot;</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="108"/>
+        <source>800x600</source>
+        <translation>800x600</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="115"/>
+        <source>1024x768</source>
+        <translation>1024x768</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="138"/>
+        <source>Custom</source>
+        <translation>Niestandardowy</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="182"/>
+        <source>Depth</source>
+        <translation>Głębokość</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="194"/>
+        <source>1 bit monochrome</source>
+        <translation>1 bit czarno-biały</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="201"/>
+        <source>4 bit grayscale</source>
+        <translation>4 bit poziom szarości</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="208"/>
+        <source>8 bit</source>
+        <translation>8 bit</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="215"/>
+        <source>12 (16) bit</source>
+        <translation>12 (16) bit</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="222"/>
+        <source>16 bit</source>
+        <translation>16 bit</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="229"/>
+        <source>18 bit</source>
+        <translation>18 bit</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="236"/>
+        <source>24 bit</source>
+        <translation>24 bit</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="243"/>
+        <source>32 bit</source>
+        <translation>32 bit</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="263"/>
+        <source>Skin</source>
+        <translation>Skórka</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="279"/>
+        <source>None</source>
+        <translation>Brak</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="289"/>
+        <source>Emulate touch screen (no mouse move)</source>
+        <translation>Emuluj ekran dotykowy (brak ruchu myszy)</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="296"/>
+        <source>Emulate LCD screen (Only with fixed zoom of 3.0 times magnification)</source>
+        <translation>Emuluj ekran LCD (tylko z zastosowanym powiększeniem x 3)</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="324"/>
+        <source>&lt;p&gt;Note that any applications using the virtual framebuffer will be terminated if you change the Size or Depth &lt;i&gt;above&lt;/i&gt;. You may freely modify the Gamma &lt;i&gt;below&lt;/i&gt;.</source>
+        <translation>&lt;p&gt;Uwaga, każda aplikacja korzystająca z virtual framebuffer zostanie zakończona przy zmianie Rozmiaru bądź Głębokości &lt;i&gt;powyżej&lt;/i&gt;. Można swobodnie zmieniać paramter Gamma &lt;i&gt;poniżej&lt;/i&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="334"/>
+        <source>Gamma</source>
+        <translation>Gamma</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="346"/>
+        <source>Blue</source>
+        <translation>Niebieski</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="1229"/>
+        <source>1.0</source>
+        <translation>1.0</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="638"/>
+        <source>Green</source>
+        <translation>Zielony</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="930"/>
+        <source>All</source>
+        <translation>Wszystkie</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="1222"/>
+        <source>Red</source>
+        <translation>Czerowny</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="1514"/>
+        <source>Set all to 1.0</source>
+        <translation>Ustaw wszystkie do 1.0</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="1548"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="1561"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Anuluj</translation>
+    </message>
+</context>
+<context>
+    <name>QVFb</name>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="531"/>
+        <source>Browse...</source>
+        <translation>Przeglądaj...</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="645"/>
+        <source>Load Custom Skin...</source>
+        <translation>Wczytaj skórki użytkownika...</translation>
+    </message>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="646"/>
+        <source>All QVFB Skins (*.skin)</source>
+        <translation>Wszystkie skórki QVFB (*.skin)</translation>
+    </message>
+</context>
+</TS>
