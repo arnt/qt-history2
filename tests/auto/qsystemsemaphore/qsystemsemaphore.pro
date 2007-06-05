0513b6cb3713c04ab845842df241bc44bcabffe6
benjamin <bmeyer@trolltech.com>
2007-06-05 11:26:36 +0100

    AutoTest: compile / runtime fixes
    [git-p4: depot-paths = "//depot/qt/main/": change = 266538]

--- tests/auto/qsystemsemaphore/qsystemsemaphore.pro
+++ tests/auto/qsystemsemaphore/qsystemsemaphore.pro
@@ -1,7 +1,7 @@
 CONFIG += qttest_p4
 #QT = core
 
-include(../src/qsharedmemory.pri)
+include(../qsharedmemory/src/qsharedmemory.pri)
 win32: CONFIG += console
 mac:CONFIG -= app_bundle
 
--- tests/auto/qsystemsemaphore/tst_qsystemsemaphore.cpp
+++ tests/auto/qsystemsemaphore/tst_qsystemsemaphore.cpp
@@ -19,6 +19,8 @@
 
 #define EXISTING_SHARE "existing"
 
+#define LACKYLOC "../qsharedmemory/lackey"
+
 class tst_QSystemSemaphore : public QObject
 {
     Q_OBJECT
@@ -117,19 +119,20 @@ void tst_QSystemSemaphore::basicProcesses()
 {
     QSystemSemaphore sem("store", 0);
 
-    QStringList acquireArguments = QStringList() << "../lackey/scripts/systemsemaphore_acquire.js";
+    QStringList acquireArguments = QStringList() << LACKYLOC "/scripts/systemsemaphore_acquire.js";
     QProcess acquire;
+    qDebug() << acquireArguments;
     acquire.setProcessChannelMode(QProcess::ForwardedChannels);
 
-    QStringList releaseArguments = QStringList() << "../lackey/scripts/systemsemaphore_release.js";
+    QStringList releaseArguments = QStringList() << LACKYLOC "/scripts/systemsemaphore_release.js";
     QProcess release;
     release.setProcessChannelMode(QProcess::ForwardedChannels);
 
-    acquire.start("../lackey/lackey", acquireArguments);
+    acquire.start(LACKYLOC "/lackey", acquireArguments);
     QVERIFY(!acquire.waitForFinished(100));
     QVERIFY(acquire.state()== QProcess::Running);
     acquire.kill();
-    release.start("../lackey/lackey", releaseArguments);
+    release.start(LACKYLOC "/lackey", releaseArguments);
     QVERIFY(acquire.waitForFinished(100));
     QVERIFY(acquire.state()== QProcess::NotRunning);
 }
@@ -151,7 +154,7 @@ void tst_QSystemSemaphore::processes()
     QFETCH(int, processes);
     QStringList scripts;
     for (int i = 0; i < processes; ++i)
-        scripts.append("../lackey/scripts/systemsemaphore_acquirerelease.js");
+        scripts.append(LACKYLOC "/scripts/systemsemaphore_acquirerelease.js");
 
     QList<QProcess*> consumers;
     for (int i = 0; i < scripts.count(); ++i) {
@@ -159,7 +162,7 @@ void tst_QSystemSemaphore::processes()
         QProcess *p = new QProcess;
         p->setProcessChannelMode(QProcess::ForwardedChannels);
         consumers.append(p);
-        p->start("../lackey/lackey", arguments);
+        p->start(LACKYLOC "/lackey", arguments);
     }
 
     while (!consumers.isEmpty()) {
@@ -174,24 +177,24 @@ void tst_QSystemSemaphore::undo()
 #ifdef Q_OS_WIN
     QSKIP("This test only checks a unix behavior", SkipSingle);
 #endif
-    
+
     QSystemSemaphore sem("store", 1);
 
-    QStringList acquireArguments = QStringList() << "../lackey/scripts/systemsemaphore_acquire.js";
+    QStringList acquireArguments = QStringList() << LACKYLOC "/scripts/systemsemaphore_acquire.js";
     QProcess acquire;
     acquire.setProcessChannelMode(QProcess::ForwardedChannels);
 
-    QStringList releaseArguments = QStringList() << "../lackey/scripts/systemsemaphore_release.js";
+    QStringList releaseArguments = QStringList() << LACKYLOC "/scripts/systemsemaphore_release.js";
     QProcess release;
     release.setProcessChannelMode(QProcess::ForwardedChannels);
 
-    acquire.start("../lackey/lackey", acquireArguments);
+    acquire.start(LACKYLOC "/lackey", acquireArguments);
     QVERIFY(acquire.waitForFinished(100));
     QVERIFY(acquire.state()== QProcess::NotRunning);
 
     // At process exit the kernel should auto undo
 
-    acquire.start("../lackey/lackey", acquireArguments);
+    acquire.start(LACKYLOC "/lackey", acquireArguments);
     QVERIFY(acquire.waitForFinished(100));
     QVERIFY(acquire.state()== QProcess::NotRunning);
 }
@@ -200,24 +203,24 @@ void tst_QSystemSemaphore::initialValue()
 {
     QSystemSemaphore sem("store", 1);
 
-    QStringList acquireArguments = QStringList() << "../lackey/scripts/systemsemaphore_acquire.js";
+    QStringList acquireArguments = QStringList() << LACKYLOC "/scripts/systemsemaphore_acquire.js";
     QProcess acquire;
     acquire.setProcessChannelMode(QProcess::ForwardedChannels);
 
-    QStringList releaseArguments = QStringList() << "../lackey/scripts/systemsemaphore_release.js";
+    QStringList releaseArguments = QStringList() << LACKYLOC "/scripts/systemsemaphore_release.js";
     QProcess release;
     release.setProcessChannelMode(QProcess::ForwardedChannels);
 
-    acquire.start("../lackey/lackey", acquireArguments);
+    acquire.start(LACKYLOC "/lackey", acquireArguments);
     QVERIFY(acquire.waitForFinished(100));
     QVERIFY(acquire.state()== QProcess::NotRunning);
 
-    acquire.start("../lackey/lackey", acquireArguments << "2");
+    acquire.start(LACKYLOC "/lackey", acquireArguments << "2");
     QVERIFY(!acquire.waitForFinished(100));
     QVERIFY(acquire.state()== QProcess::Running);
     acquire.kill();
 
-    release.start("../lackey/lackey", releaseArguments);
+    release.start(LACKYLOC "/lackey", releaseArguments);
     QVERIFY(acquire.waitForFinished(100));
     QVERIFY(acquire.state()== QProcess::NotRunning);
 }
