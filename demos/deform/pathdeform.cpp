881affd7c4ed4b71fbc79181b18ab8598d1a9b69
hwall <hwall@hwall-extra>
2007-06-08 13:41:36 +0100

    p4i integration Integrate 266991 from 4.3 to main: Fixes:    deform demo performance Task:     166248 RevBy:    TrustMe Details:  Don't repaint more than necessary.
    [git-p4: depot-paths = "//depot/qt/main/": change = 266992]

--- demos/deform/pathdeform.cpp
+++ demos/deform/pathdeform.cpp
@@ -267,7 +267,7 @@ void PathDeformRenderer::timerEvent(QTimerEvent *e)
             m_direction *= 0.995;
         double time = m_repaintTracker.restart();
 
-        QRect rectBefore = circle_bounds(m_pos, m_radius, m_fontSize);
+        QRect rectBefore = circle_bounds(m_pos, m_radius, LENS_EXTENT);
 
         double dx = m_direction.x();
         double dy = m_direction.y();
@@ -302,7 +302,7 @@ void PathDeformRenderer::timerEvent(QTimerEvent *e)
         } else
 #endif
         {
-            QRect rectAfter = circle_bounds(m_pos, m_radius, m_fontSize);
+            QRect rectAfter = circle_bounds(m_pos, m_radius, LENS_EXTENT);
             update(rectAfter | rectBefore);
             QApplication::syncX();
         }
