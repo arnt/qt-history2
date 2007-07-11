5e8f63da881b9f7d96416f43278a250a1e4b77ad
Girish Ramakrishnan <gramakri@trolltech.com>
2007-07-11 14:22:56 +0100

    p4i integration Integrate 270025 from 4.3 to main: Fixes:    Add "we mean it" header
    [git-p4: depot-paths = "//depot/qt/main/": change = 270027]

--- src/opengl/util/fragmentprograms_p.h
+++ src/opengl/util/fragmentprograms_p.h
@@ -13,6 +13,17 @@
 #ifndef FRAGMENTPROGRAMS_H
 #define FRAGMENTPROGRAMS_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 enum FragmentVariable {
     VAR_BRUSH_TEXTURE,
     VAR_LINEAR,
