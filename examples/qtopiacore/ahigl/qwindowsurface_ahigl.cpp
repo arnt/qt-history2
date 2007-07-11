a04e3fd8e48505f96735525e9fedbb22085e6f43
msmith <msmith@trolltech.com>
2007-07-11 12:23:37 +0100

    p4i integration Integrate 269994 from 4.3 to main: Fixes:    doc: The ahigl example is done for now.
    [git-p4: depot-paths = "//depot/qt/main/": change = 269995]

--- examples/qtopiacore/ahigl/qwindowsurface_ahigl.cpp
+++ examples/qtopiacore/ahigl/qwindowsurface_ahigl.cpp
@@ -20,10 +20,20 @@
 #include <QtOpenGL/private/qglpaintdevice_qws_p.h>
 #include <QtOpenGL/private/qgl_p.h>
 
+/*!
+  \class QAhiGLWindowSurfacePrivate
+  \internal
+
+  \brief The QAhiGLWindowSurfacePrivate class is the private implementation
+  class for class QAhiGLWindowSurface.
+
+  This class contains only state variables. 
+  */
 class QAhiGLWindowSurfacePrivate
 {
 public:
-    QAhiGLWindowSurfacePrivate(EGLDisplay eglDisplay, EGLSurface eglSurface,
+    QAhiGLWindowSurfacePrivate(EGLDisplay eglDisplay,
+			       EGLSurface eglSurface,
                                EGLContext eglContext);
 
     QPaintDevice *device;
@@ -40,6 +50,10 @@ public:
     EGLContext context;
 };
 
+/*!
+  The construct just sets statwe variables using the ones
+  provided. 
+ */
 QAhiGLWindowSurfacePrivate::QAhiGLWindowSurfacePrivate(EGLDisplay eglDisplay,
                                                        EGLSurface eglSurface,
                                                        EGLContext eglContext)
@@ -60,6 +74,67 @@ inline static int nextPowerOfTwo(uint v)
     return v;
 }
 
+/*!
+    \class QAhiGLWindowSurface
+    \preliminary
+    \internal
+
+    \brief The QAhiGLWindowSurface class provides the drawing area
+    for top-level windows using OpenGL for drawing on an ATI handheld
+    device.
+
+    In \l {Qtopia Core}, the default behavior for each client is to
+    render widgets into an area of memory. The server then displays
+    the contents of that memory on the screen. For ATI handheld
+    devices using OpenGL, QAhiGLWindowSurface is the window surface
+    class that allocates and manages the memory areas in the clients
+    and the server.
+
+    When a screen update is required, the server runs through all the
+    top-level windows that intersect with the region being updated,
+    ensuring that the clients have updated their window surfaces. Then
+    the server uses the screen driver to copy the contents of the
+    affected window surfaces into its composition and then display the
+    composition on the screen.  
+
+    \tableofcontents
+
+    \section1 Pure Virtual Functions
+
+    There are two window surface instances for each top-level window.
+    One is used by the application when drawing a window, and the
+    other is used by the server application to make its copy for
+    building a window composition to send to the screen.
+
+    The key() function is implemented to uniquely identify this window
+    surface class as "ahigl", and the isValid() function is
+    implemented to determine whether the associated window is still
+    acceptable for representation by this window surface class.  It
+    must either be a window using an \l {QGLWidget} {OpenGL widget},
+    or it must be a window whose frame is no bigger than 256 x 256.
+
+    The setGeometry() function is implemented to change the geometry
+    of the frame buffer whenever the geometry of the associated
+    top-level window changes. The image() function is called by the
+    window system when building window compositions to return an image
+    of the top-level window.
+
+    The paintDevice() function is implemented to return the appropriate
+    paint device.
+
+    \section1 Virtual Functions
+
+    When painting onto the surface, the window system will always call
+    the beginPaint() function before any painting operations are
+    performed. It ensures that the correct frame buffer will be used
+    by the OpenGL library for painting operations. Likewise, the
+    endPaint() function is automatically called when the painting is
+    done, but it isn't implemented for this example.
+*/
+
+/*!
+  This is the client side constructor.
+ */
 QAhiGLWindowSurface::QAhiGLWindowSurface(QWidget *widget,
                                          EGLDisplay eglDisplay,
                                          EGLSurface eglSurface,
@@ -72,6 +147,9 @@ QAhiGLWindowSurface::QAhiGLWindowSurface(QWidget *widget,
     setSurfaceFlags(QWSWindowSurface::Buffered);
 }
 
+/*!
+  This is the server side constructor.
+ */
 QAhiGLWindowSurface::QAhiGLWindowSurface(EGLDisplay eglDisplay,
                                          EGLSurface eglSurface,
                                          EGLContext eglContext)
@@ -80,6 +158,11 @@ QAhiGLWindowSurface::QAhiGLWindowSurface(EGLDisplay eglDisplay,
     setSurfaceFlags(QWSWindowSurface::Buffered);
 }
 
+/*!
+  The destructor deletes the OpenGL structures held by the
+  private implementation class, and then it deletes the
+  private implementation class.
+ */
 QAhiGLWindowSurface::~QAhiGLWindowSurface()
 {
     if (d_ptr->texture)
@@ -92,6 +175,14 @@ QAhiGLWindowSurface::~QAhiGLWindowSurface()
     delete d_ptr;
 }
 
+/*!
+  This function changes the geometry of the frame buffer
+  to the geometry in \a rect. It is called whenever the
+  geometry of the associated top-level window changes. It
+  also rebuilds the window surface's texture and binds
+  the OpenGL identifier to the texture for use by the
+  OpenGL library.
+ */
 void QAhiGLWindowSurface::setGeometry(const QRect &rect)
 {
     QSize size = rect.size();
@@ -147,6 +238,9 @@ void QAhiGLWindowSurface::setGeometry(const QRect &rect)
     QWSGLWindowSurface::setGeometry(rect);
 }
 
+/*!
+  Returns the window  surface's texture as a QByteArray.
+ */
 QByteArray QAhiGLWindowSurface::permanentState() const
 {
     QByteArray array;
@@ -157,22 +251,37 @@ QByteArray QAhiGLWindowSurface::permanentState() const
     return array;
 }
 
+/*!
+  Sets the window surface's texture to \a data.
+ */
 void QAhiGLWindowSurface::setPermanentState(const QByteArray &data)
 {
     const char *ptr = data.constData();
     d_ptr->texture = reinterpret_cast<const GLuint*>(ptr)[0];
 }
 
+/*!
+  Returns the paint device being used for this window surface.
+ */
 QPaintDevice *QAhiGLWindowSurface::paintDevice()
 {
     return d_ptr->device;
 }
 
+/*!
+  Returns the window surface's texture.
+ */
 GLuint QAhiGLWindowSurface::textureId() const
 {
     return d_ptr->texture;
 }
 
+/*!
+  The \l {QWSServer} {window system} always calls this function
+  before any painting operations begin for this window surface.
+  It ensures that the correct frame buffer will be used by the
+  OpenGL library for painting operations. 
+ */
 void QAhiGLWindowSurface::beginPaint(const QRegion &region)
 {
     QWSGLWindowSurface::beginPaint(region);
@@ -181,6 +290,13 @@ void QAhiGLWindowSurface::beginPaint(const QRegion &region)
         glBindFramebufferOES(GL_FRAMEBUFFER_EXT, d_ptr->frameBufferObject);
 }
 
+/*!
+  This function returns true if the window associated with
+  this window surface can still rendered using this window
+  surface class. Either the window's top-level widget must
+  be an \l {QGLWidget} {OpenGL widget}, or the window's
+  frame must be no bigger than 256 x 256.
+ */
 bool QAhiGLWindowSurface::isValid() const
 {
     if (!qobject_cast<QGLWidget*>(window())) {
