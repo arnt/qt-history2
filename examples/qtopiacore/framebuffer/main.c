14d98917f835c9e56b6970bfd048caa473bc9f01
hwall <hwall@hwall-extra>
2007-07-12 14:38:42 +0100

    p4i integration Integrate 270174 from 4.3 to main: Fixes:    Add support for 8bpp and 4bpp Task:     152914
    [git-p4: depot-paths = "//depot/qt/main/": change = 270175]

--- examples/qtopiacore/framebuffer/main.c
+++ examples/qtopiacore/framebuffer/main.c
@@ -131,6 +131,133 @@ void restoreTextMode(long oldMode)
     close(ttyFd);
 }
 
+struct fb_cmap oldPalette;
+struct fb_cmap palette;
+int paletteSize = 0;
+
+void initPalette_16()
+{
+    if (finfo.type == FB_TYPE_PACKED_PIXELS) {
+	// We'll setup a grayscale map for 4bpp linear
+	int val = 0;
+        int i;
+	for (i = 0; i < 16; ++i) {
+	    palette.red[i] = (val << 8) | val;
+	    palette.green[i] = (val << 8) | val;
+	    palette.blue[i] = (val << 8) | val;
+            val += 17;
+	}
+	return;
+    }
+
+    // Default 16 colour palette
+    unsigned char reds[16]   = { 0x00, 0x7F, 0xBF, 0xFF, 0xFF, 0xA2,
+				 0x00, 0xFF, 0xFF, 0x00, 0x7F, 0x7F,
+				 0x00, 0x00, 0x00, 0x82 };
+    unsigned char greens[16] = { 0x00, 0x7F, 0xBF, 0xFF, 0x00, 0xC5,
+				 0x00, 0xFF, 0x00, 0xFF, 0x00, 0x00,
+				 0x00, 0x7F, 0x7F, 0x7F };
+    unsigned char blues[16]  = { 0x00, 0x7F, 0xBF, 0xFF, 0x00, 0x11,
+				 0xFF, 0x00, 0xFF, 0xFF, 0x00, 0x7F,
+				 0x7F, 0x7F, 0x00, 0x00 };
+
+    int i;
+    for (i = 0; i < 16; ++i) {
+	palette.red[i] = ((reds[i]) << 8) | reds[i];
+	palette.green[i] = ((greens[i]) << 8) | greens[i];
+	palette.blue[i] = ((blues[i]) << 8) | blues[i];
+	palette.transp[i] = 0;
+    }
+}
+
+void initPalette_256()
+{
+    if (vinfo.grayscale) {
+        int i;
+	for (i = 0; i < 256; ++i) {
+	    unsigned short c = (i << 8) | i;
+	    palette.red[i] = c;
+	    palette.green[i] = c;
+	    palette.blue[i] = c;
+	    palette.transp[i] = 0;
+	}
+	return;
+    }
+
+    // 6x6x6 216 color cube
+    int i = 0;
+    int ir, ig, ib;
+    for (ir = 0x0; ir <= 0xff; ir += 0x33) {
+	for (ig = 0x0; ig <= 0xff; ig += 0x33) {
+	    for (ib = 0x0; ib <= 0xff; ib += 0x33) {
+		palette.red[i] = (ir << 8)|ir;
+		palette.green[i] = (ig << 8)|ig;
+		palette.blue[i] = (ib << 8)|ib;
+		palette.transp[i] = 0;
+		++i;
+	    }
+	}
+    }
+}
+
+void initPalette()
+{
+    switch (vinfo.bits_per_pixel) {
+    case 8: paletteSize = 256; break;
+    case 4: paletteSize = 16; break;
+    default: break;
+    }
+
+    if (!paletteSize)
+	return; /* not using a palette */
+
+    /* read old palette */
+    oldPalette.start = 0;
+    oldPalette.len = paletteSize;
+    oldPalette.red = (unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    oldPalette.green = (unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    oldPalette.blue=(unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    oldPalette.transp=(unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    if (ioctl(ttyFd, FBIOGETCMAP, &oldPalette) == -1)
+	perror("initPalette: error reading palette");
+
+    /* create new palette */
+    palette.start = 0;
+    palette.len = paletteSize;
+    palette.red = (unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    palette.green = (unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    palette.blue = (unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    palette.transp = (unsigned short*)malloc(sizeof(unsigned short)*paletteSize);
+    switch (paletteSize) {
+    case 16: initPalette_16(); break;
+    case 256: initPalette_256(); break;
+    default: break;
+    }
+
+    /* set new palette */
+    if (ioctl(ttyFd, FBIOPUTCMAP, &palette) == -1)
+	perror("initPalette: error setting palette");
+}
+
+void resetPalette()
+{
+    if (paletteSize == 0)
+	return;
+
+    if (ioctl(ttyFd, FBIOPUTCMAP, &oldPalette) == -1)
+	perror("resetPalette");
+
+    free(oldPalette.red);
+    free(oldPalette.green);
+    free(oldPalette.blue);
+    free(oldPalette.transp);
+
+    free(palette.red);
+    free(palette.green);
+    free(palette.blue);
+    free(palette.transp);
+}
+
 void drawRect_rgb32(int x0, int y0, int width, int height, int color)
 {
     const int bytesPerPixel = 4;
@@ -171,6 +298,25 @@ void drawRect_rgb16(int x0, int y0, int width, int height, int color)
     }
 }
 
+void drawRect_palette(int x0, int y0, int width, int height, int color)
+{
+    const int bytesPerPixel = 1;
+    const int stride = finfo.line_length / bytesPerPixel;
+    const unsigned char color8 = color;
+
+    unsigned char *dest = (unsigned char*)(frameBuffer)
+                          + (y0 + vinfo.yoffset) * stride
+                          + (x0 + vinfo.xoffset);
+
+    int x, y;
+    for (y = 0; y < height; ++y) {
+        for (x = 0; x < width; ++x) {
+            dest[x] = color8;
+        }
+        dest += stride;
+    }
+}
+
 void drawRect(int x0, int y0, int width, int height, int color)
 {
     switch (vinfo.bits_per_pixel) {
@@ -180,6 +326,12 @@ void drawRect(int x0, int y0, int width, int height, int color)
     case 16:
         drawRect_rgb16(x0, y0, width, height, color);
         break;
+    case 8:
+	drawRect_palette(x0, y0, width, height, color);
+	break;
+    case 4:
+	drawRect_palette(x0, y0, width, height, color);
+	break;
     default:
         printf("Warning: drawRect() not implemented for color depth %i\n",
                vinfo.bits_per_pixel);
@@ -246,20 +398,37 @@ int main(int argc, char **argv)
 
     printVariableInfo();
 
-    printf("Will draw 3 rectangles on the screen,\n"
-           "they should be colored red, green and blue (in that order).\n");
+    initPalette();
 
-    drawRect(vinfo.xres / 8, vinfo.yres / 8,
-             vinfo.xres / 4, vinfo.yres / 4,
-             0xffff0000);
-    drawRect(vinfo.xres * 3 / 8, vinfo.yres * 3 / 8,
-             vinfo.xres / 4, vinfo.yres / 4,
-             0xff00ff00);
-    drawRect(vinfo.xres * 5 / 8, vinfo.yres * 5 / 8,
-             vinfo.xres / 4, vinfo.yres / 4,
-             0xff0000ff);
+    if (paletteSize == 0) {
+        printf("Will draw 3 rectangles on the screen,\n"
+               "they should be colored red, green and blue (in that order).\n");
+        drawRect(vinfo.xres / 8, vinfo.yres / 8,
+                 vinfo.xres / 4, vinfo.yres / 4,
+                 0xffff0000);
+        drawRect(vinfo.xres * 3 / 8, vinfo.yres * 3 / 8,
+                 vinfo.xres / 4, vinfo.yres / 4,
+                 0xff00ff00);
+        drawRect(vinfo.xres * 5 / 8, vinfo.yres * 5 / 8,
+                 vinfo.xres / 4, vinfo.yres / 4,
+                 0xff0000ff);
+    } else {
+        printf("Will rectangles from the 16 first entries in the color palette"
+               " on the screen\n");
+        int y;
+        int x;
+        for (y = 0; y < 4; ++y) {
+            for (x = 0; x < 4; ++x) {
+                drawRect(vinfo.xres / 4 * x, vinfo.yres / 4 * y,
+                         vinfo.xres / 4, vinfo.yres / 4,
+                         4 * y + x);
+            }
+        }
+    }
 
     sleep(5);
+
+    resetPalette();
 
     printf("  Done.\n");
 
