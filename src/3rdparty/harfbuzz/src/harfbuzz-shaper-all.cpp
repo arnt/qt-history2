4229ad594466b6227c349851a332f2828fc41818
Simon Hausmann <shausman@trolltech.com>
2007-08-07 13:02:00 +0100

    Fixes:    <changelog entry> Task:     <task number> AutoTest: <change number> Details:  add missing file
    [git-p4: depot-paths = "//depot/qt/main/": change = 272582]

---
+++ src/3rdparty/harfbuzz/src/harfbuzz-shaper-all.cpp
@@ -0,0 +1,10 @@
+
+#include "harfbuzz-shaper.cpp"
+#include "harfbuzz-tibetan.cpp"
+#include "harfbuzz-khmer.cpp"
+#include "harfbuzz-indic.cpp"
+#include "harfbuzz-hebrew.cpp"
+#include "harfbuzz-arabic.cpp"
+#include "harfbuzz-hangul.cpp"
+#include "harfbuzz-myanmar.cpp"
+
