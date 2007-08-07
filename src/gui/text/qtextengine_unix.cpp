8a53e5e30483158084159b2d9ffdc0b1df646395
Simon Hausmann <shausman@trolltech.com>
2007-08-07 13:01:50 +0100

    Fixes:    <changelog entry> Task:     <task number> AutoTest: <change number> Details:  Link
    [git-p4: depot-paths = "//depot/qt/main/": change = 272569]

--- src/gui/text/qtextengine_unix.cpp
+++ src/gui/text/qtextengine_unix.cpp
@@ -18,6 +18,33 @@
 #  define FT_KERNING_UNFITTED ft_kerning_unfitted
 #endif
 
+#include <harfbuzz-shaper.h>
+
+HB_LineBreakClass HB_GetLineBreakClass(HB_UChar32 ch)
+{
+    return (HB_LineBreakClass)QUnicodeTables::lineBreakClass(ch);
+}
+
+void HB_GetUnicodeCharProperties(HB_UChar32 ch, HB_CharCategory *category, int *combiningClass)
+{
+    *category = (HB_CharCategory)QChar::category(ch);
+    *combiningClass = QChar::combiningClass(ch);
+}
+
+HB_CharCategory HB_GetUnicodeCharCategory(HB_UChar32 ch)
+{
+    return (HB_CharCategory)QChar::category(ch);
+}
+
+int HB_GetUnicodeCharCombiningClass(HB_UChar32 ch)
+{
+    return QChar::combiningClass(ch);
+}
+
+HB_UChar16 HB_GetMirroredChar(HB_UChar16 ch)
+{
+    return QChar::mirroredChar(ch);
+}
 
 void QTextEngine::shapeText(int item) const
 {
