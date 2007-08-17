26fe8a7e10354714e3cc454d0e60c837d770937b
Volker Hilsheimer <vohi@trolltech.com>
2007-08-17 11:13:59 +0100

    p4i integration Integrate 273821 from 4.3 to main: Fixes:    Compilare necessus est. Details:  needs to be checked with other compilers.
    [git-p4: depot-paths = "//depot/qt/main/": change = 273822]

--- src/corelib/io/qfsfileengine_win.cpp
+++ src/corelib/io/qfsfileengine_win.cpp
@@ -39,6 +39,15 @@
 #define SECURITY_WIN32
 #include <security.h>
 
+#ifndef _INTPTR_T_DEFINED
+#ifdef  _WIN64
+typedef __int64             intptr_t;
+#else
+typedef _W64 int            intptr_t;
+#endif
+#define _INTPTR_T_DEFINED
+#endif
+
 #ifndef INVALID_FILE_ATTRIBUTES
 #  define INVALID_FILE_ATTRIBUTES (DWORD (-1))
 #endif
