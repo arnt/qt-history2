ddd10877ee3aee600520c9ed8af865cf684e8ca4
benjamin <bmeyer@trolltech.com>
2007-08-23 16:09:47 +0100

    Fixes:    Set buffer back to 0 :) and fix autotest
    [git-p4: depot-paths = "//depot/qt/main/": change = 274383]

--- tests/auto/qlocalsocket/src/qlocalserver_win.cpp
+++ tests/auto/qlocalsocket/src/qlocalserver_win.cpp
@@ -23,7 +23,7 @@
 // The buffer size need to be 0 otherwise data could be
 // lost if the socket that has written data closes the connection
 // before it is read.
-#define BUFSIZE 4096
+#define BUFSIZE 0
 
 QLocalServerThread::QLocalServerThread(QObject *parent) : QThread(parent),
        	maxPendingConnections(1),
