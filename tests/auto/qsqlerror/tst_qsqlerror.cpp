3e0d2f5dbefc51d9dbd0d4c70fdcd554c7b76174
Anders Larsen <alarsen@trolltech.com>
2007-08-24 10:53:42 +0100

    p4i integration Integrate 274461 from 4.3 to main: Fixes:    Autotest for constructor and assignment operator
    [git-p4: depot-paths = "//depot/qt/main/": change = 274462]

--- tests/auto/qsqlerror/tst_qsqlerror.cpp
+++ tests/auto/qsqlerror/tst_qsqlerror.cpp
@@ -27,6 +27,7 @@ public:
 
 private slots:
     void getSetCheck();
+    void construction();
 };
 
 tst_QSqlError::tst_QSqlError()
@@ -62,6 +63,33 @@ void tst_QSqlError::getSetCheck()
     QCOMPARE(INT_MIN, obj1.number());
     obj1.setNumber(INT_MAX);
     QCOMPARE(INT_MAX, obj1.number());
+}
+
+void tst_QSqlError::construction()
+{
+   QSqlError obj1("drivertext", "databasetext", QSqlError::UnknownError, 123);
+   QCOMPARE(obj1.driverText(), QString("drivertext"));
+   QCOMPARE(obj1.databaseText(), QString("databasetext"));
+   QCOMPARE(obj1.type(), QSqlError::UnknownError);
+   QCOMPARE(obj1.number(), 123);
+   obj1.isValid();
+
+   QSqlError obj2(obj1);
+   QCOMPARE(obj2.driverText(), obj1.driverText());
+   QCOMPARE(obj2.databaseText(), obj1.databaseText());
+   QCOMPARE(obj2.type(), obj1.type());
+   QCOMPARE(obj2.number(), obj1.number());
+   QVERIFY(obj2.isValid());
+
+   QSqlError obj3 = obj2;
+   QCOMPARE(obj3.driverText(), obj2.driverText());
+   QCOMPARE(obj3.databaseText(), obj2.databaseText());
+   QCOMPARE(obj3.type(), obj2.type());
+   QCOMPARE(obj3.number(), obj2.number());
+   QVERIFY(obj3.isValid());
+
+   QSqlError obj4;
+   QVERIFY(!obj4.isValid());
 }
 
 QTEST_MAIN(tst_QSqlError)
