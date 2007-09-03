1e9fb4f854c12001e9105493ffe0502f71f5457e
Dimitri Papadopoulos <dimitri@trolltech.com>
2007-09-03 20:44:04 +0100

    p4i integration Integrate 275375 from 4.3 to main: Fixes:    Qtopia performance improvement RevBy:    Haavard Details:  don't use a QRegExp to check for mmWidth and mmHeight 	  in the display spec. Cuts off around 300.000 instructions                  from the QApplication constructor.
    [git-p4: depot-paths = "//depot/qt/main/": change = 275465]

--- src/gui/embedded/qscreenvfb_qws.cpp
+++ src/gui/embedded/qscreenvfb_qws.cpp
@@ -199,21 +199,37 @@ bool QVFbScreen::connect(const QString &displaySpec)
     lstep = d_ptr->hdr->linestep;
 
     // Handle display physical size spec.
-    QRegExp mmWidthRx(QLatin1String("mmWidth=?(\\d+)"));
-    int dimIdxW = displayArgs.indexOf(mmWidthRx);
-    QRegExp mmHeightRx(QLatin1String("mmHeight=?(\\d+)"));
-    int dimIdxH = displayArgs.indexOf(mmHeightRx);
+    int dimIdxW = -1;
+    int dimIdxH = -1;
+    for (int i = 0; i < displayArgs.size(); ++i) {
+        if (displayArgs.at(i).startsWith(QLatin1String("mmWidth="))) {
+            dimIdxW = i;
+            break;
+        }
+    }
+    for (int i = 0; i < displayArgs.size(); ++i) {
+        if (displayArgs.at(i).startsWith(QLatin1String("mmHeight="))) {
+            dimIdxH = i;
+            break;
+        }
+    }
     if (dimIdxW >= 0) {
-        mmWidthRx.exactMatch(displayArgs.at(dimIdxW));
-        physWidth = mmWidthRx.cap(1).toInt();
-        if (dimIdxH < 0)
-            physHeight = dh*physWidth/dw;
+        bool ok;
+        int pw = displayArgs.at(dimIdxW).mid(8).toInt(&ok);
+        if (ok) {
+            physWidth = pw;
+            if (dimIdxH < 0)
+                physHeight = dh*physWidth/dw;
+        }
     }
     if (dimIdxH >= 0) {
-        mmHeightRx.exactMatch(displayArgs.at(dimIdxH));
-        physHeight = mmHeightRx.cap(1).toInt();
-        if (dimIdxW < 0)
-            physWidth = dw*physHeight/dh;
+        bool ok;
+        int pw = displayArgs.at(dimIdxH).mid(8).toInt(&ok);
+        if (ok) {
+            physWidth = pw;
+            if (dimIdxH < 0)
+                physHeight = dh*physWidth/dw;
+        }
     }
     if (dimIdxW < 0 && dimIdxH < 0) {
         const int dpi = 72;
@@ -222,7 +238,7 @@ bool QVFbScreen::connect(const QString &displaySpec)
     }
 
     qDebug("Connected to VFB server %s: %d x %d x %d %dx%dmm (%dx%ddpi)", displaySpec.toLatin1().data(),
-        w, h, d, physWidth, physHeight, int(dw*25.4/physWidth), int(dh*25.4/physHeight) );
+        w, h, d, physWidth, physHeight, qRound(dw*25.4/physWidth), qRound(dh*25.4/physHeight) );
 
     size = lstep * h;
     mapsize = size;
