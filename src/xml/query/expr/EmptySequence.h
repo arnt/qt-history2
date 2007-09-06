7ba9a8a5c6495ddadb959a01a6ed34ac6edaf601
Frans Englich <fenglich@trolltech.com>
2007-09-06 17:21:29 +0100

    Fixes:    Small syntactical difference in license headers.
    [git-p4: depot-paths = "//depot/qt/main/": change = 276049]

--- src/xml/query/expr/EmptySequence.h
+++ src/xml/query/expr/EmptySequence.h
@@ -9,8 +9,7 @@
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 **
-***************************************************************************
-*/
+****************************************************************************/
 
 #ifndef Patternist_EmptySequence_H
 #define Patternist_EmptySequence_H
