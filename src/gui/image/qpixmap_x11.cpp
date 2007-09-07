c9946c107188d000f3d226360fcd3f6b74bdb46f
Trond Kjernaasen <trond@trolltech.com>
2007-09-07 13:00:44 +0100

    p4i integration Integrate 276158 from 4.3 to main: Fixes:    endianess issues with QPixmap/QImage conversions under X11 Task:     related to 170792 RevBy:    Simon
    [git-p4: depot-paths = "//depot/qt/main/": change = 276159]

--- src/gui/image/qpixmap_x11.cpp
+++ src/gui/image/qpixmap_x11.cpp
@@ -740,6 +740,30 @@ QImage QPixmap::toImage() const
         QImage image(data->w, data->h, QImage::Format_ARGB32_Premultiplied);
         memcpy(image.bits(), xi->data, xi->bytes_per_line * xi->height);
 
+        // we may have to swap the byte order
+        if ((QSysInfo::ByteOrder == QSysInfo::LittleEndian && xi->byte_order == MSBFirst)
+            || (QSysInfo::ByteOrder == QSysInfo::BigEndian))
+        {
+            for (int i=0; i < image.height(); i++) {
+                uint *p = (uint*) image.scanLine(i);
+                uint *end = p + image.width();
+                if ((xi->byte_order == LSBFirst && QSysInfo::ByteOrder == QSysInfo::BigEndian)
+                    || (xi->byte_order == MSBFirst && QSysInfo::ByteOrder == QSysInfo::LittleEndian)) {
+                    while (p < end) {
+                        *p = ((*p << 24) & 0xff000000) | ((*p << 8) & 0x00ff0000)
+                             | ((*p >> 8) & 0x0000ff00) | ((*p >> 24) & 0x000000ff);
+                        p++;
+                    }
+                } else if (xi->byte_order == MSBFirst && QSysInfo::ByteOrder == QSysInfo::BigEndian) {
+                    while (p < end) {
+                        *p = ((*p << 16) & 0x00ff0000) | ((*p >> 16) & 0x000000ff)
+                             | ((*p ) & 0xff00ff00);
+                        p++;
+                    }
+                }
+            }
+        }
+
         // throw away image data
         qSafeXDestroyImage(xi);
 
